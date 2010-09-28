101ba4487be9ede9e9dd4e041be119a35f6de389
Julian Walker <julianreidwalker@gmail.com>
2010-09-28 15:24:14 -0700

    Matrix4.lookAt now initializes W row (omitted previously).

--- src/core/Matrix4.js
+++ src/core/Matrix4.js
@@ -46,7 +46,7 @@ THREE.Matrix4.prototype = {
 		this.n11 = x.x; this.n12 = x.y; this.n13 = x.z; this.n14 = - x.dot( eye );
 		this.n21 = y.x; this.n22 = y.y; this.n23 = y.z; this.n24 = - y.dot( eye );
 		this.n31 = z.x; this.n32 = z.y; this.n33 = z.z; this.n34 = - z.dot( eye );
-
+		this.n41 = 0; this.n42 = 0; this.n43 = 0; this.n44 = 1;
 	},
 
 	transform: function ( v ) {
