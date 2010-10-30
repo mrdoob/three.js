8ee8c9baf91342189ead30ec917f3909935f439c
Mr.doob <info@mrdoob.com>
2010-10-30 05:19:44 +0100

    * three.js: Fixed terrain example (was still using Scene.add())

--- src/core/Matrix4.js
+++ src/core/Matrix4.js
@@ -435,10 +435,10 @@ THREE.Matrix4.makeOrtho = function( left, right, top, bottom, near, far ) {
 
 	m = new THREE.Matrix4();
 	w = right - left;
-	h = top - bottom; // bottom - top
+	h = top - bottom;
 	p = far - near;
 	x = ( right + left ) / w;
-	y = ( top + bottom ) / h; // ( bottom + top ) / h
+	y = ( top + bottom ) / h;
 	z = ( far + near ) / p;
 
 	m.n11 = 2 / w; m.n12 = 0;     m.n13 = 0;      m.n14 = -x;
