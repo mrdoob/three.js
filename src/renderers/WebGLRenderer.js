4b606714a2afc155c816852c802527bda61fa755
Mr.doob <info@mrdoob.com>
2010-11-21 18:04:53 +0000

    Fixed non-seamless panorama in WebGLRenderer. I can't see any other WebGL example being affected by this change (for bad).

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -469,6 +469,8 @@ THREE.WebGLRenderer = function ( scene ) {
 				material.__webGLTexture = _gl.createTexture();
 				_gl.bindTexture( _gl.TEXTURE_2D, material.__webGLTexture );
 				_gl.texImage2D( _gl.TEXTURE_2D, 0, _gl.RGBA, _gl.RGBA, _gl.UNSIGNED_BYTE, material.map.image ) ;
+				_gl.texParameteri( _gl.TEXTURE_2D, _gl.TEXTURE_WRAP_S, _gl.CLAMP_TO_EDGE );
+				_gl.texParameteri( _gl.TEXTURE_2D, _gl.TEXTURE_WRAP_T, _gl.CLAMP_TO_EDGE );
 				_gl.texParameteri( _gl.TEXTURE_2D, _gl.TEXTURE_MAG_FILTER, _gl.LINEAR );
 				_gl.texParameteri( _gl.TEXTURE_2D, _gl.TEXTURE_MIN_FILTER, _gl.LINEAR_MIPMAP_LINEAR );
 				_gl.generateMipmap( _gl.TEXTURE_2D );
