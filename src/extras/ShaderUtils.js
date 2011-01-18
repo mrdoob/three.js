0b8323f4267223c2ca544ea71eb6a056aeaf7913
Mr.doob <info@mrdoob.com>
2011-01-18 05:48:50 +0000

    Removed hatching commented code from ShaderUtils.

--- src/extras/ShaderUtils.js
+++ src/extras/ShaderUtils.js
@@ -262,119 +262,6 @@ var ShaderUtils = {
 			].join("\n")
 
 		},
-		/*
-		'hatching' : {
-
-			uniforms: {
-
-				"uSampler": { type: "t", value: 2, texture: null },
-
-				"uDirLightPos":	{ type: "v3", value: new THREE.Vector3() },
-				"uDirLightColor": { type: "c", value: new THREE.Color( 0xeeeeee ) },
-
-				"uAmbientLightColor": { type: "c", value: new THREE.Color( 0x050505 ) }
-
-			},
-
-			vertex_shader: [
-
-				"varying vec3 vNormal;",
-
-				"void main() {",
-
-					"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-					"vNormal = normalize( normalMatrix * normal );",
-
-				"}"
-
-			].join("\n"),
-
-			fragment_shader: [
-
-				"uniform vec3 uDirLightPos;",
-				"uniform vec3 uDirLightColor;",
-
-				"uniform vec3 uAmbientLightColor;",
-
-				"uniform sampler2D uSampler;",
-
-				"varying vec3 vNormal;",
-
-				"void main() {",
-
-					"float directionalLightWeighting = max(dot(normalize(vNormal), uDirLightPos), 0.0);",
-					"vec3 lightWeighting = uAmbientLightColor + uDirLightColor * directionalLightWeighting;",
-
-					"gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);",
-
-					"if (length(lightWeighting) < 1.00) {",
-
-						"if (mod(gl_FragCoord.x + gl_FragCoord.y, 10.0) == 0.0) {",
-
-							"gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
-
-						"}",
-
-					"}",
-
-					"if (length(lightWeighting) < 0.75) {",
-
-						"if (mod(gl_FragCoord.x - gl_FragCoord.y, 10.0) == 0.0) {",
-
-							"gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
-
-						"}",
-					"}",
-
-					"if (length(lightWeighting) < 0.50) {",
-
-						"if (mod(gl_FragCoord.x + gl_FragCoord.y - 5.0, 10.0) == 0.0) {",
-
-							"gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
-
-						"}",
-					"}",
-
-					"if (length(lightWeighting) < 0.3465) {",
-
-						"if (mod(gl_FragCoord.x - gl_FragCoord.y - 5.0, 10.0) == 0.0) {",
-
-							"gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
-
-						"}",
-					"}",
-
-				"}"
-
-			].join("\n")
-
-		},
-		*/
-		'basic': {
-
-			uniforms: {},
-
-			vertex_shader: [
-
-				"void main() {",
-
-					"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-				"}"
-
-			].join("\n"),
-
-			fragment_shader: [
-
-				"void main() {",
-
-					"gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);",
-
-				"}"
-
-			].join("\n")
-
-		},
 
 		'cube': {
 
@@ -405,6 +292,32 @@ var ShaderUtils = {
 
 					"vec3 wPos = cameraPosition - vViewPosition;",
 					"gl_FragColor = textureCube( tCube, vec3( - wPos.x, wPos.yz ) );",
+
+				"}"
+
+			].join("\n")
+
+		},
+
+		'basic': {
+
+			uniforms: {},
+
+			vertex_shader: [
+
+				"void main() {",
+
+					"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+				"}"
+
+			].join("\n"),
+
+			fragment_shader: [
+
+				"void main() {",
+
+					"gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);",
 
 				"}"
 
