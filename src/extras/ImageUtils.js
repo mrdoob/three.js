3ad1f77774ff9c4eb64e07b5e0bc0a568414f166
Mr.doob <info@mrdoob.com>
2011-03-19 19:37:40 +0000

    ImageUtils blank spaces clean up.

--- src/extras/ImageUtils.js
+++ src/extras/ImageUtils.js
@@ -4,14 +4,14 @@ var ImageUtils = {
 
 		var image = new Image(),
 			texture = new THREE.Texture( image, mapping );
-		
+
 		image.onload = function () { texture.needsUpdate = true; if( callback ) callback( this ); };
 		image.src = path;
 
 		return texture;
 
 	},
-	
+
 	loadTextureCube: function ( array, mapping, callback ) {
 
 		var i, l, 
@@ -24,11 +24,11 @@ var ImageUtils = {
 
 			images[ i ] = new Image();
 			images[ i ].onload = function () {
-				
+
 					images.loadCount += 1; 
 					if( images.loadCount == 6 ) texture.needsUpdate = true; 
 					if( callback ) callback( this ); 
-				
+
 				};
 
 			images[ i ].src = array[ i ];
