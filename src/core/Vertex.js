1faa8ab964a9cfcd54027e96bc0a6b03164b48cd
Mr.doob <info@mrdoob.com>
2011-03-22 01:33:04 +0000

    Minor clean up.

--- src/core/Vertex.js
+++ src/core/Vertex.js
@@ -2,21 +2,8 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-THREE.Vertex = function ( position /*, normal*/ ) {
+THREE.Vertex = function ( position ) {
 
 	this.position = position || new THREE.Vector3();
-
-	/*
-	this.positionWorld = new THREE.Vector3();
-	this.positionScreen = new THREE.Vector4();
-
-	this.normal = normal || new THREE.Vector3();
-	this.normalWorld = new THREE.Vector3();
-	this.normalScreen = new THREE.Vector3();
-
-	this.tangent = new THREE.Vector4();
-
-	this.__visible = true;
-	*/
 
 };
