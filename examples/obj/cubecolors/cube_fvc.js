d9a930caa449beb29714d4cb9c17cea88454b3e0
alteredq <postfilter@gmail.com>
2011-03-22 04:29:08 +0100

    Added second cube for Blender vertex color example (courtesy of mrdoob).

---
+++ examples/obj/cubecolors/cube_fvc.js
@@ -0,0 +1,47 @@
+/*
+ * File generated with Blender 2.56 Exporter
+ * https://github.com/mrdoob/three.js/tree/master/utils/exporters/blender/
+ *
+ * vertices: 8
+ * faces: 6
+ * normals: 8
+ * uvs: 0
+ * colors: 22
+ * materials: 1
+ *
+ */
+
+var model = {
+
+    'version' : 2,
+    
+    'materials': [	{
+	"DbgColor" : 0xeeeeee,
+	"DbgIndex" : 0,
+	"DbgName" : "Material",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [1, 1, 1],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"shading" : "Basic",
+	"specularCoef" : 50,
+	"transparency" : 1.0,
+	"vertexColors" : true
+	}],
+
+    'vertices': [1.000000,-1.000000,-1.000000,1.000000,-1.000000,1.000000,-1.000000,-1.000000,1.000000,-1.000000,-1.000000,-1.000000,1.000000,1.000000,-1.000000,0.999999,1.000000,1.000000,-1.000000,1.000000,0.999999,-1.000000,1.000000,-1.000000],
+
+    'morphTargets': [],
+
+    'normals': [0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,-0.577349,-0.577349,0.577349,-0.577349,-0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349,0.577349,-0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349],
+
+    'colors': [0xffffff,0xffe18d,0xffe190,0x80fff7,0xe7df1b,0x6f62ff,0xfd17db,0xff0007,0x101d0e,0x5c00a3,0xd1ffb4,0x8a0079,0x797eed,0x6758ff,0xfff5de,0x3f3cff,0xb6b4ff,0x1dffef,0x101e1d,0x8a9190,0xd0ffaf,0x4c5555],
+
+    'uvs': [[]],
+
+    'faces': [163,0,1,2,3,0,0,1,2,3,0,1,2,3,163,4,7,6,5,0,4,5,6,7,4,5,6,7,163,0,4,5,1,0,0,4,7,1,0,8,9,10,163,1,5,6,2,0,1,7,6,2,0,11,12,13,163,2,6,7,3,0,2,6,5,3,14,15,16,17,163,4,0,3,7,0,4,0,3,5,18,19,20,21],
+
+    'end': (new Date).getTime()
+
+}
+
+postMessage( model );
