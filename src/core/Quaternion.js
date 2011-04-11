99ac869b24e96ae18f4cce4748445af8f85696a0
alteredq <postfilter@gmail.com>
2011-04-11 21:01:54 +0200

    Added forgotten return to Quaternion.multiply

--- src/core/Quaternion.js
+++ src/core/Quaternion.js
@@ -155,6 +155,8 @@ THREE.Quaternion.prototype = {
 		this.y = -q1.x * q2.z + q1.y * q2.w + q1.z * q2.x + q1.w * q2.y;
 		this.z =  q1.x * q2.y - q1.y * q2.x + q1.z * q2.w + q1.w * q2.z;
 		this.w = -q1.x * q2.x - q1.y * q2.y - q1.z * q2.z + q1.w * q2.w;
+		
+		return this;
 
 	},
 
