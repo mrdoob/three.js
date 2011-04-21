e457b4f8aefcb2582def3112a4f1d8ed39bc6d86
Mr.doob <info@mrdoob.com>
2011-04-22 00:35:43 +0200

    Added drawCalls property to WebGLRenderer::data object.

--- src/renderers/WebGLRenderTarget.js
+++ src/renderers/WebGLRenderTarget.js
@@ -17,7 +17,7 @@ THREE.WebGLRenderTarget = function ( width, height, options ) {
 
 	this.format = options.format !== undefined ? options.format : THREE.RGBAFormat;
 	this.type = options.type !== undefined ? options.type : THREE.UnsignedByteType;
-	
+
 	this.depthBuffer = options.depthBuffer !== undefined ? options.depthBuffer : true;
 	this.stencilBuffer = options.stencilBuffer !== undefined ? options.stencilBuffer : true;
 
