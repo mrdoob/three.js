3ce4e9090957bd7c31f771a557590f0fbba7b705
alteredq <postfilter@gmail.com>
2011-07-27 01:49:30 +0200

    Added forgotten SpotLight.js

---
+++ src/lights/SpotLight.js
@@ -0,0 +1,20 @@
+/**
+ * @author alteredq / http://alteredqualia.com/
+ */
+
+THREE.SpotLight = function ( hex, intensity, distance, castShadow ) {
+
+	THREE.Light.call( this, hex );
+
+	this.position = new THREE.Vector3( 0, 1, 0 );
+	this.target = new THREE.Object3D();
+
+	this.intensity = intensity || 1;
+	this.distance = distance || 0;
+
+	this.castShadow = castShadow !== undefined ? castShadow : false;
+
+};
+
+THREE.SpotLight.prototype = new THREE.Light();
+THREE.SpotLight.prototype.constructor = THREE.SpotLight;
