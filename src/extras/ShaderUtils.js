32f0f884bdc9e621691628c75e27d720e7bf040b
alteredq <postfilter@gmail.com>
2011-08-30 12:38:32 +0200

    Added sepia shader / effect.

--- src/extras/ShaderUtils.js
+++ src/extras/ShaderUtils.js
@@ -562,6 +562,58 @@ THREE.ShaderUtils = {
 
 		},
 
+
+		/* -------------------------------------------------------------------------
+		//	Sepia tone shader
+		//  - based on glfx.js sepia shader
+		//		https://github.com/evanw/glfx.js
+		 ------------------------------------------------------------------------- */
+
+		'sepia': {
+
+			uniforms: {
+
+				tDiffuse: { type: "t", value: 0, texture: null },
+				amount: { type: "f", value: 1.0 }
+
+			},
+
+			vertexShader: [
+
+				"varying vec2 vUv;",
+
+				"void main() {",
+
+					"vUv = vec2( uv.x, 1.0 - uv.y );",
+					"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+				"}"
+
+			].join("\n"),
+
+			fragmentShader: [
+
+				"varying vec2 vUv;",
+				"uniform sampler2D tDiffuse;",
+				"uniform float amount;",
+
+				"void main() {",
+
+					"vec4 color = texture2D( tDiffuse, vUv );",
+					"vec3 c = color.rgb;",
+
+					"color.r = dot( c, vec3( 1.0 - 0.607 * amount, 0.769 * amount, 0.189 * amount ) );",
+					"color.g = dot( c, vec3( 0.349 * amount, 1.0 - 0.314 * amount, 0.168 * amount ) );",
+					"color.b = dot( c, vec3( 0.272 * amount, 0.534 * amount, 1.0 - 0.869 * amount ) );",
+
+					"gl_FragColor = vec4( min( vec3( 1.0 ), color.rgb ), color.a );",
+
+				"}"
+
+			].join("\n")
+
+		},
+
 		/* -------------------------------------------------------------------------
 		//	Full-screen textured quad shader
 		 ------------------------------------------------------------------------- */
