0bd23ca9734273989ce02882edbecd0b959158b7
alteredq <postfilter@gmail.com>
2011-09-02 13:40:31 +0200

    Added scene override material to RenderPass. Added mipmapped depth-of-field shader.

--- examples/js/postprocessing/RenderPass.js
+++ examples/js/postprocessing/RenderPass.js
@@ -2,10 +2,11 @@
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.RenderPass = function ( scene, camera ) {
+THREE.RenderPass = function ( scene, camera, overrideMaterial ) {
 
 	this.scene = scene;
 	this.camera = camera;
+	this.overrideMaterial = overrideMaterial;
 
 	this.clear = true;
 	this.needsSwap = false;
@@ -16,7 +17,11 @@ THREE.RenderPass.prototype = {
 
 	render: function ( renderer, writeBuffer, readBuffer, delta ) {
 
+		this.scene.overrideMaterial = this.overrideMaterial;
+
 		renderer.render( this.scene, this.camera, readBuffer, this.clear );
+
+		this.scene.overrideMaterial = null;
 
 	}
 
