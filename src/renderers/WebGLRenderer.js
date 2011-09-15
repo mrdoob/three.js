0d8f3572f059836852e49a635b4b1f01f795a1f4
alteredq <postfilter@gmail.com>
2011-09-16 00:10:34 +0200

    Fixed shadowmap camera aspect ratio (thanks to @kig).

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -3426,7 +3426,7 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 		if ( ! _cameraLight ) {
 
-			_cameraLight = new THREE.Camera( _this.shadowCameraFov, camera.aspect, _this.shadowCameraNear, _this.shadowCameraFar );
+			_cameraLight = new THREE.Camera( _this.shadowCameraFov, _this.shadowMapWidth / _this.shadowMapHeight, _this.shadowCameraNear, _this.shadowCameraFar );
 
 		}
 
