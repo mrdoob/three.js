b406568dce1713e201fc0e55d4779ef538dbe11a
Mr.doob <info@mrdoob.com>
2011-11-07 02:34:38 +0100

    Refactored SphereGeometry Now uses Face4 where posible. Adde phiStart, phiLenght, thetaStart and thetaLength as extra parameters (useful for doing google-earth-like stuff).

--- src/extras/geometries/SphereGeometry.js
+++ src/extras/geometries/SphereGeometry.js
@@ -1,112 +1,91 @@
 /**
  * @author mr.doob / http://mrdoob.com/
- * based on http://papervision3d.googlecode.com/svn/trunk/as3/trunk/src/org/papervision3d/objects/primitives/Sphere.as
  */
 
-THREE.SphereGeometry = function ( radius, segmentsWidth, segmentsHeight ) {
+THREE.SphereGeometry = function ( radius, segmentsWidth, segmentsHeight, phiStart, phiLength, thetaStart, thetaLength ) {
 
 	THREE.Geometry.call( this );
 
-	var radius = radius || 50,
-	gridX = segmentsWidth || 8,
-	gridY = segmentsHeight || 6;
+	var radius = radius || 50;
+	var segmentsX = segmentsWidth || 8;
+	var segmentsY = segmentsHeight || 6;
 
-	var i, j, pi = Math.PI;
-	var iHor = Math.max( 3, gridX );
-	var iVer = Math.max( 2, gridY );
-	var aVtc = [];
+	var phiStart = phiStart != undefined ? phiStart : 0;
+	var phiLength = phiLength != undefined ? phiLength : Math.PI * 2;
 
-	for ( j = 0; j < ( iVer + 1 ) ; j++ ) {
+	var thetaStart = thetaStart != undefined ? thetaStart : 0;
+	var thetaLength = thetaLength != undefined ? thetaLength : Math.PI;
 
-		var fRad1 = j / iVer;
-		var fZ = radius * Math.cos( fRad1 * pi );
-		var fRds = radius * Math.sin( fRad1 * pi );
-		var aRow = [];
-		var oVtx = 0;
+	var x, y, vertices = [], uvs = [];
 
-		for ( i = 0; i < iHor; i++ ) {
+	for ( y = 0; y <= segmentsY; y ++ ) {
 
-			var fRad2 = 2 * i / iHor;
-			var fX = fRds * Math.sin( fRad2 * pi );
-			var fY = fRds * Math.cos( fRad2 * pi );
+		var verticesRow = [];
+		var uvsRow = [];
 
-			if ( !( ( j == 0 || j == iVer ) && i > 0 ) ) {
+		for ( x = 0; x <= segmentsX; x ++ ) {
 
-				oVtx = this.vertices.push( new THREE.Vertex( new THREE.Vector3( fY, fZ, fX ) ) ) - 1;
+			var u = x / segmentsX;
+			var v = y / segmentsY;
 
-			}
+			var xpos = - radius * Math.cos( phiStart + u * phiLength ) * Math.sin( thetaStart + v * thetaLength );
+			var ypos = radius * Math.cos( thetaStart + v * thetaLength );
+			var zpos = radius * Math.sin( phiStart + u * phiLength ) * Math.sin( thetaStart + v * thetaLength );
 
-			aRow.push( oVtx );
+			this.vertices.push( new THREE.Vertex( new THREE.Vector3( xpos, ypos, zpos ) ) );
+
+			verticesRow.push( this.vertices.length - 1 );
+			uvsRow.push( new THREE.UV( u, v ) );
 
 		}
 
-		aVtc.push( aRow );
+		vertices.push( verticesRow );
+		uvs.push( uvsRow );
 
 	}
 
-	var n1, n2, n3, iVerNum = aVtc.length;
+	for ( y = 0; y < segmentsY; y ++ ) {
 
-	for ( j = 0; j < iVerNum; j++ ) {
+		for ( x = 0; x < segmentsX; x ++ ) {
 
-		var iHorNum = aVtc[ j ].length;
+			var v1 = vertices[ y ][ x + 1 ];
+			var v2 = vertices[ y ][ x ];
+			var v3 = vertices[ y + 1 ][ x ];
+			var v4 = vertices[ y + 1 ][ x + 1 ];
 
-		if ( j > 0 ) {
+			var n1 = this.vertices[ v1 ].position.clone().normalize();
+			var n2 = this.vertices[ v2 ].position.clone().normalize();
+			var n3 = this.vertices[ v3 ].position.clone().normalize();
+			var n4 = this.vertices[ v4 ].position.clone().normalize();
 
-			for ( i = 0; i < iHorNum; i++ ) {
+			var uv1 = uvs[ y ][ x + 1 ].clone();
+			var uv2 = uvs[ y ][ x ].clone();
+			var uv3 = uvs[ y + 1 ][ x ].clone();
+			var uv4 = uvs[ y + 1 ][ x + 1 ].clone();
 
-				var bEnd = i == ( iHorNum - 1 );
-				var aP1 = aVtc[ j ][ bEnd ? 0 : i + 1 ];
-				var aP2 = aVtc[ j ][ ( bEnd ? iHorNum - 1 : i ) ];
-				var aP3 = aVtc[ j - 1 ][ ( bEnd ? iHorNum - 1 : i ) ];
-				var aP4 = aVtc[ j - 1 ][ bEnd ? 0 : i + 1 ];
+			if ( Math.abs( this.vertices[ v1 ].position.y ) == radius ) {
 
-				var fJ0 = j / ( iVerNum - 1 );
-				var fJ1 = ( j - 1 ) / ( iVerNum - 1 );
-				var fI0 = ( i + 1 ) / iHorNum;
-				var fI1 = i / iHorNum;
+				this.faces.push( new THREE.Face3( v1, v3, v4, [ n1, n3, n4 ] ) );
+				this.faceVertexUvs[ 0 ].push( [ uv1, uv3, uv4 ] );
 
-				var aP1uv = new THREE.UV( 1 - fI0, fJ0 );
-				var aP2uv = new THREE.UV( 1 - fI1, fJ0 );
-				var aP3uv = new THREE.UV( 1 - fI1, fJ1 );
-				var aP4uv = new THREE.UV( 1 - fI0, fJ1 );
+			} else if ( Math.abs( this.vertices[ v3 ].position.y ) ==  radius ) {
 
-				if ( j < ( aVtc.length - 1 ) ) {
+				this.faces.push( new THREE.Face3( v1, v2, v3, [ n1, n2, n3 ] ) );
+				this.faceVertexUvs[ 0 ].push( [ uv1, uv2, uv3 ] );
 
-					n1 = this.vertices[ aP1 ].position.clone();
-					n2 = this.vertices[ aP2 ].position.clone();
-					n3 = this.vertices[ aP3 ].position.clone();
-					n1.normalize();
-					n2.normalize();
-					n3.normalize();
+			} else {
 
-					this.faces.push( new THREE.Face3( aP1, aP2, aP3, [ new THREE.Vector3( n1.x, n1.y, n1.z ), new THREE.Vector3( n2.x, n2.y, n2.z ), new THREE.Vector3( n3.x, n3.y, n3.z ) ] ) );
-
-					this.faceVertexUvs[ 0 ].push( [ aP1uv, aP2uv, aP3uv ] );
-
-				}
-
-				if ( j > 1 ) {
-
-					n1 = this.vertices[aP1].position.clone();
-					n2 = this.vertices[aP3].position.clone();
-					n3 = this.vertices[aP4].position.clone();
-					n1.normalize();
-					n2.normalize();
-					n3.normalize();
-
-					this.faces.push( new THREE.Face3( aP1, aP3, aP4, [ new THREE.Vector3( n1.x, n1.y, n1.z ), new THREE.Vector3( n2.x, n2.y, n2.z ), new THREE.Vector3( n3.x, n3.y, n3.z ) ] ) );
-
-					this.faceVertexUvs[ 0 ].push( [ aP1uv, aP3uv, aP4uv ] );
-
-				}
+				this.faces.push( new THREE.Face4( v1, v2, v3, v4, [ n1, n2, n3, n4 ] ) );
+				this.faceVertexUvs[ 0 ].push( [ uv1, uv2, uv3, uv4 ] );
 
 			}
+
 		}
+
 	}
 
 	this.computeCentroids();
 	this.computeFaceNormals();
-	this.computeVertexNormals();
 
 	this.boundingSphere = { radius: radius };
 
