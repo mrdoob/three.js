ec287d5bc6106ebfc48a2154c297bd4642684c74
Erik Kitson <erik.kitson@gmail.com>
2012-01-03 11:32:06 -0800

    Fixed bug where depth buffer is not cleared if last object rendered on previous render cycle disabled depth mask.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -3178,6 +3178,11 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 		}
 
+		// Ensure depth buffer writing is enabled so it can be cleared on next render
+
+		this.setDepthTest( true );
+		setDepthWrite( true );
+
 		//_gl.finish();
 
 	};
