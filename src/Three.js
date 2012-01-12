b74342a4d6e27e4f05d84abc1ba99e82f608b289
Mr.doob <info@mrdoob.com>
2012-01-12 16:20:51 +0100

    Updated RequestAnimationFrame polyfill.

--- src/Three.js
+++ src/Three.js
@@ -11,23 +11,33 @@ if ( ! self.Int32Array ) {
 
 }
 
-// Provides requestAnimationFrame in a cross browser way.
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
 
-if ( ! self.requestAnimationFrame ) {
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
 
-	self.requestAnimationFrame = ( function () {
-
-		return self.webkitRequestAnimationFrame ||
-		self.mozRequestAnimationFrame ||
-		self.oRequestAnimationFrame ||
-		self.msRequestAnimationFrame ||
-		function ( callback, element ) {
-
-			self.setTimeout( callback, 1000 / 60 );
-
-		};
-
-	} )();
-
-}
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'RequestCancelAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
