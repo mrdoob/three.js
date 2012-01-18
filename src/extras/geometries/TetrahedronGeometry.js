53816d94ccc2299e68c3ba98bf029a8abf8d9461
Mr.doob <info@mrdoob.com>
2012-01-18 22:44:13 +0100

    Implemented @timothypratley 's Polyhedron code. Keeping the old IcosahadrenGeometry and OctahedronGeometry to double check results later.

---
+++ src/extras/geometries/TetrahedronGeometry.js
@@ -0,0 +1,19 @@
+/**
+ * @author timothypratley / https://github.com/timothypratley
+ */
+
+THREE.TetrahedronGeometry = function ( radius, detail ) {
+
+	var vertices = [
+		[ 1,  1,  1 ], [ -1, -1, 1 ], [ -1, 1, -1 ], [ 1, -1, -1 ]
+	];
+
+	var faces = [
+		[ 2, 1, 0 ], [ 0, 3, 2 ], [ 1, 3, 0 ], [ 2, 3, 1 ]
+	];
+
+	THREE.PolyhedronGeometry.call( this, vertices, faces, radius, detail );
+
+};
+THREE.TetrahedronGeometry.prototype = new THREE.Geometry();
+THREE.TetrahedronGeometry.prototype.constructor = THREE.TetrahedronGeometry;
