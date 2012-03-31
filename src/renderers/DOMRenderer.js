5ffc9d4f0ef08b7b60331107d7a2ecfcb7bd8e47
alteredq <postfilter@gmail.com>
2012-03-31 07:06:56 +0200

    Fixed runaway globals.
    Thanks Closure compiler.

--- src/renderers/DOMRenderer.js
+++ src/renderers/DOMRenderer.js
@@ -5,27 +5,38 @@
 THREE.DOMRenderer = function () {
 
 	var _renderData, _elements,
-	_projector = new THREE.Projector(),
-	_width, _height, _widthHalf, _heightHalf, _transformProp;
+	_width, _height, _widthHalf, _heightHalf, _transformProp,
+	_projector = new THREE.Projector();
 
-	var getSupportedProp = function (proparray) {
+	var getSupportedProp = function ( proparray ) {
+
 		var root = document.documentElement
-		for ( var i = 0; i < proparray.length; i++ ) { 
-			if (typeof root.style[proparray[i]]=="string") { 
-				return proparray[i]
+
+		for ( var i = 0; i < proparray.length; i ++ ) {
+
+			if ( typeof root.style[ proparray[ i ] ] === "string" ) {
+
+				return proparray[i];
+
 			}
+
 		}
+
 		return null;
+
 	};
-	
-	_transformProp = getSupportedProp(['transform', 'MozTransform', 'WebkitTransform', 'msTransform', 'OTransform']);
-	
+
+	_transformProp = getSupportedProp( [ 'transform', 'MozTransform', 'WebkitTransform', 'msTransform', 'OTransform' ] );
+
 	this.domElement = document.createElement( 'div' );
 
 	this.setSize = function ( width, height ) {
 
-		_width = width; _height = height;
-		_widthHalf = _width / 2; _heightHalf = _height / 2;
+		_width = width;
+		_height = height;
+
+		_widthHalf = _width / 2;
+		_heightHalf = _height / 2;
 
 	};
 
@@ -43,25 +54,28 @@ THREE.DOMRenderer = function () {
 			if ( element instanceof THREE.RenderableParticle && element.material instanceof THREE.ParticleDOMMaterial ) {
 
 				dom = element.material.domElement;
-				
-				v1x = element.x * _widthHalf + _widthHalf - (dom.offsetWidth >> 1); 
-				v1y = element.y * _heightHalf + _heightHalf - (dom.offsetHeight >> 1);
-				
+
+				v1x = element.x * _widthHalf + _widthHalf - ( dom.offsetWidth >> 1 );
+				v1y = element.y * _heightHalf + _heightHalf - ( dom.offsetHeight >> 1 );
+
 				dom.style.left = v1x + 'px';
 				dom.style.top = v1y + 'px';
-				dom.style.zIndex = Math.abs(Math.floor((1 - element.z) * camera.far / camera.near))
+				dom.style.zIndex = Math.abs( Math.floor( ( 1 - element.z ) * camera.far / camera.near ) )
 
-				if(_transformProp) {
-					scaleX = element.scale.x * _widthHalf;
-					scaleY = element.scale.y * _heightHalf;
-					scaleVal = "scale(" + scaleX + "," + scaleY + ")";
-					dom.style[_transformProp] = scaleVal;
+				if ( _transformProp ) {
+
+					var scaleX = element.scale.x * _widthHalf;
+					var scaleY = element.scale.y * _heightHalf;
+					var scaleVal = "scale(" + scaleX + "," + scaleY + ")";
+
+					dom.style[ _transformProp ] = scaleVal;
+
 				}
-				
+
 			}
 
 		}
 
 	};
 
-}
+};
