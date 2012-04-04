2a335b902de11ec67520c243b019c487c2e4567b
alteredq <postfilter@gmail.com>
2012-04-04 20:08:25 +0200

    Removed heightOffset from CubeCamera (as per @mrdoob's suggestion).

--- src/extras/cameras/CubeCamera.js
+++ src/extras/cameras/CubeCamera.js
@@ -5,10 +5,9 @@
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.CubeCamera = function ( near, far, heightOffset, cubeResolution ) {
+THREE.CubeCamera = function ( near, far, cubeResolution ) {
 
-	this.heightOffset = heightOffset;
-	this.position = new THREE.Vector3( 0, heightOffset, 0 );
+	this.position = new THREE.Vector3( 0, 0, 0 );
 
 	// cameras
 
@@ -59,8 +58,6 @@ THREE.CubeCamera = function ( near, far, heightOffset, cubeResolution ) {
 	this.updatePosition = function ( position ) {
 
 		this.position.copy( position );
-
-		this.position.y += this.heightOffset;
 
 		this.targetPX.copy( this.position );
 		this.targetNX.copy( this.position );
