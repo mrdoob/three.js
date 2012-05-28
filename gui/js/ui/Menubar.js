62fbc80258143d91aabcd6907c44ed0803837547
Mr.doob <info@mrdoob.com>
2012-05-28 18:15:20 +0200

    GUI: Menubar tweaks.

--- gui/js/ui/Menubar.js
+++ gui/js/ui/Menubar.js
@@ -1,4 +1,4 @@
-var MenuBar = function ( signals ) {
+var Menubar = function ( signals ) {
 
 	var container = new UI.Panel( 'absolute' );
 	container.setBackgroundColor( '#eee' );
@@ -6,6 +6,7 @@ var MenuBar = function ( signals ) {
 	var options = new UI.Panel();
 	options.setMargin( '8px' );
 	options.add( new UI.Text().setText( 'File' ).setColor( '#666' ).setMarginRight( '10px' ) );
+	options.add( new UI.Text().setText( 'Edit' ).setColor( '#666' ).setMarginRight( '10px' ) );
 	options.add( new UI.Text().setText( 'About' ).setColor( '#666' ).setMarginRight( '10px' ) );
 	container.add( options );
 
