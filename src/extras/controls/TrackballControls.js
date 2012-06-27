7f13327b0203197e0ef6b0563153347424f61b13
minglars <minglars@yahoo.com>
2012-06-26 21:29:28 -0700

    Replaced distanceTo with distanceToSquared on line 245 in TrackballControls.js

--- src/extras/controls/TrackballControls.js
+++ src/extras/controls/TrackballControls.js
@@ -253,7 +253,7 @@ THREE.TrackballControls = function ( object, domElement ) {
 
 		_this.object.lookAt( _this.target );
 
-		if ( lastPosition.distanceTo( _this.object.position ) > 0 ) {
+		if ( lastPosition.distanceToSquared( _this.object.position ) > 0 ) {
 
 			_this.dispatchEvent( changeEvent );
 
