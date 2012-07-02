fddb8ca03a1b5212a131d824f15b1bfc46997d1b
Alvaro Gil <zevarito@gmail.com>
2012-07-02 16:01:29 -0300

    fix createGeometry ignoring point.z

--- src/extras/core/CurvePath.js
+++ src/extras/core/CurvePath.js
@@ -204,7 +204,7 @@ THREE.CurvePath.prototype.createGeometry = function( points ) {
 
 	for ( var i = 0; i < points.length; i ++ ) {
 
-		geometry.vertices.push( new THREE.Vector3( points[ i ].x, points[ i ].y, 0 ) );
+		geometry.vertices.push( new THREE.Vector3( points[ i ].x, points[ i ].y, points[ i ].z ) );
 
 	}
 
