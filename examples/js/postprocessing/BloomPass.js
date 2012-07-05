7a9ac49a9785f52bbb86f4fa6091064f1305984a
Mr.doob <info@mrdoob.com>
2012-07-05 03:36:36 +0200

    Minor formatting tweaks.

--- examples/js/postprocessing/BloomPass.js
+++ examples/js/postprocessing/BloomPass.js
@@ -2,7 +2,7 @@
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.BloomPass = function( strength, kernelSize, sigma, resolution ) {
+THREE.BloomPass = function ( strength, kernelSize, sigma, resolution ) {
 
 	strength = ( strength !== undefined ) ? strength : 1;
 	kernelSize = ( kernelSize !== undefined ) ? kernelSize : 25;
