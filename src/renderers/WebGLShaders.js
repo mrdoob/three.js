93d96da07e483029d93785e39a9da6b06dc22bcc
alteredq <postfilter@gmail.com>
2012-07-05 19:28:09 +0200

    Unflipped particles.
    For some reason point sprites in "natural" orientation are upside down :S

--- src/renderers/WebGLShaders.js
+++ src/renderers/WebGLShaders.js
@@ -156,7 +156,7 @@ THREE.ShaderChunk = {
 
 		"#ifdef USE_MAP",
 
-			"gl_FragColor = gl_FragColor * texture2D( map, gl_PointCoord );",
+			"gl_FragColor = gl_FragColor * texture2D( map, vec2( gl_PointCoord.x, 1.0 - gl_PointCoord.y ) );",
 
 		"#endif"
 
