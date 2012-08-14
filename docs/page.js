a1e901d83b15027a6e7a015256b472daf422510d
alteredq <postfilter@gmail.com>
2012-08-14 03:51:55 +0200

    Added syntax highlighting for code snippets in the documentation.
    This is using Prettify highlighter: http://code.google.com/p/google-code-prettify/
    
    Added "threejs.css" for doing custom styling (if somebody would like to do this). See for example here:
    
    http://google-code-prettify.googlecode.com/svn/trunk/styles/index.html
    http://code.google.com/p/google-code-prettify/source/browse/trunk/styles/sunburst.css

--- docs/page.js
+++ docs/page.js
@@ -29,6 +29,38 @@ var onDocumentLoad = function ( event ) {
 
 	document.body.appendChild( button );
 
+	// Syntax highlighting
+
+	var styleBase = document.createElement( 'link' );
+	styleBase.href = '../../prettify/prettify.css';
+	styleBase.rel = 'stylesheet';
+
+	var styleCustom = document.createElement( 'link' );
+	styleCustom.href = '../../prettify/threejs.css';
+	styleCustom.rel = 'stylesheet';
+
+	document.head.appendChild( styleBase );
+	document.head.appendChild( styleCustom );
+
+	var prettify = document.createElement( 'script' );
+	prettify.src = '../../prettify/prettify.js';
+
+	prettify.onload = function () {
+
+		var elements = document.getElementsByTagName( 'code' );
+
+		for ( var i = 0; i < elements.length; i ++ ) {
+
+			var e = elements[ i ];
+			e.className += ' prettyprint';
+
+		}
+
+		prettyPrint();
+
+	}
+
+	document.head.appendChild( prettify );
 
 };
 
