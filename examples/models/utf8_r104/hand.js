1e24a04f6f4abff00066521f15822aa810aa4a2c
alteredq <postfilter@gmail.com>
2012-09-10 03:10:43 +0200

    Added example of how to use new version of UTF8 format with UTF8v2Loader.
    See #2399

---
+++ examples/models/utf8_r104/hand.js
@@ -0,0 +1,27 @@
+{
+  "materials": {
+    "preview": { "Kd": [184, 136, 234] },
+    "nails": { "map_Kd": "../../textures/ash_uvgrid01.jpg" },
+    "skin": { "map_Kd": "../../textures/ash_uvgrid01.jpg" }
+  },
+  "decodeParams": {
+    "decodeOffsets": [-7473,-239,-8362,0,0,-511,-511,-511],
+    "decodeScales": [0.000050,0.000050,0.000050,0.000978,0.000978,0.001957,0.001957,0.001957]
+  },
+  "urls": {
+    "hand.utf8": [
+      { "material": "nails",
+        "attribRange": [0, 261],
+        "codeRange": [2088, 817, 404]
+      },
+      { "material": "preview",
+        "attribRange": [2905, 688],
+        "codeRange": [8409, 2570, 1280]
+      },
+      { "material": "skin",
+        "attribRange": [10979, 8899],
+        "codeRange": [82171, 31026, 15451]
+      }
+    ]
+  }
+}
