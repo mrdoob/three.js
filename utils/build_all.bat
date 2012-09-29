8707cc2b0649e0f9dbe6a4dbf8232525f7dba853
Mr.doob <info@mrdoob.com>
2012-09-29 15:10:46 +0200

    Reverted to always build unminified and minified as suggested in #2465.

--- utils/build_all.bat
+++ utils/build_all.bat
@@ -1,3 +1,4 @@
+python build.py --include common --include extras --output ../build/three.js
 python build.py --include common --include extras --minify --output ../build/three.min.js
 python build.py --include canvas --minify --output ../build/three-canvas.min.js
 python build.py --include css3d --minify --output ../build/three-css3d.min.js
