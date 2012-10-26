92850e05b6c9a62d69e4127901dbf0b7b5874c92
Mr.doob <info@mrdoob.com>
2012-10-26 12:56:38 +0200

    Editor: Reverting Menubar styling that was strangely opening when clicking on dropdowns and confirm()s.

--- editor/js/ui/Menubar.js
+++ editor/js/ui/Menubar.js
@@ -6,11 +6,11 @@ var Menubar = function ( signals ) {
 
 	var options = new UI.Panel();
 	options.setBackgroundColor( '#eee' );
-	options.setPadding( '0px' ).setMargin( '0px' );
+	options.setPadding( '8px' );
 
 	// File
 
-	var optionFile = new UI.Text().setValue( 'File' ).setDisplay( 'inline-block' ).setColor( '#666' ).setPadding( '8px 15px' ).onMouseOver( onOptionFileClick ).onMouseOver( onFileMouseOver ).onMouseOut( onFileMouseOut );
+	var optionFile = new UI.Text().setValue( 'File' ).setColor( '#666' ).setMarginRight( '20px' ).onMouseOver( onOptionFileMouseOver );
 	options.add( optionFile );
 
 	var optionFileMenu = new Menubar.File().setTop( '32px' ).setDisplay( 'none' ).onMouseOut( closeAll );
@@ -18,7 +18,7 @@ var Menubar = function ( signals ) {
 
 	// Edit
 
-	var optionEdit = new UI.Text().setValue( 'Edit' ).setDisplay( 'inline-block' ).setColor( '#666' ).setPadding( '8px 15px' ).onMouseOver( onOptionEditClick ).onMouseOver( onEditMouseOver ).onMouseOut( onEditMouseOut );
+	var optionEdit = new UI.Text().setValue( 'Edit' ).setColor( '#666' ).setMarginRight( '20px' ).onMouseOver( onOptionEditMouseOver );
 	options.add( optionEdit );
 
 	var optionEditMenu = new Menubar.Edit().setTop( '32px' ).setLeft( '50px' ).setDisplay( 'none' ).onMouseOut( closeAll );
@@ -26,7 +26,7 @@ var Menubar = function ( signals ) {
 
 	// Add
 
-	var optionAdd = new UI.Text().setValue( 'Add' ).setDisplay( 'inline-block' ).setColor( '#666' ).setPadding( '8px 15px' ).onMouseOver( onOptionAddClick ).onMouseOver( onAddMouseOver ).onMouseOut( onAddMouseOut );
+	var optionAdd = new UI.Text().setValue( 'Add' ).setColor( '#666' ).setMarginRight( '20px' ).onMouseOver( onOptionAddMouseOver );
 	options.add( optionAdd );
 
 	var optionAddMenu = new Menubar.Add().setTop( '32px' ).setLeft( '90px' ).setDisplay( 'none' ).onMouseOut( closeAll );
@@ -35,7 +35,7 @@ var Menubar = function ( signals ) {
 
 	// Help
 
-	var optionHelp = new UI.Text().setValue( 'Help' ).setDisplay( 'inline-block' ).setColor( '#666' ).setPadding( '8px 15px' ).onMouseOver( onOptionHelpClick ).onMouseOver( onHelpMouseOver ).onMouseOut( onHelpMouseOut );
+	var optionHelp = new UI.Text().setValue( 'Help' ).setColor( '#666' ).onMouseOver( onOptionHelpMouseOver );
 	options.add( optionHelp );
 
 	var optionHelpMenu = new Menubar.Help().setTop( '32px' ).setLeft( '140px' ).setDisplay( 'none' ).onMouseOut( closeAll );
@@ -55,93 +55,31 @@ var Menubar = function ( signals ) {
 
 	}
 
-	function onOptionFileClick() {
+	function onOptionFileMouseOver() {
 
 		closeAll();
 		optionFileMenu.setDisplay( '' );
 
 	}
 
-	function onOptionEditClick() {
+	function onOptionEditMouseOver() {
 
 		closeAll();
 		optionEditMenu.setDisplay( '' );
 
 	}
 
-	function onOptionAddClick() {
+	function onOptionAddMouseOver() {
 
 		closeAll();
 		optionAddMenu.setDisplay( '' );
 
 	}
 
-	function onOptionHelpClick() {
+	function onOptionHelpMouseOver() {
 
 		closeAll();
 		optionHelpMenu.setDisplay( '' );
-
-	}
-
-	function setStyleOver( item ) {
-
-		item.dom.style.background = "#444";
-		item.dom.style.color = "#eee";
-
-	}
-
-	function setStyleOut( item ) {
-
-		item.dom.style.background = "transparent";
-		item.dom.style.color = "#000";
-
-	}
-
-	function onFileMouseOver() {
-
-		setStyleOver( optionFile );
-
-	}
-
-	function onFileMouseOut() {
-
-		setStyleOut( optionFile );
-
-	}
-
-	function onEditMouseOver() {
-
-		setStyleOver( optionEdit );
-
-	}
-
-	function onEditMouseOut() {
-
-		setStyleOut( optionEdit );
-
-	}
-
-	function onAddMouseOver() {
-
-		setStyleOver( optionAdd );
-
-	}
-
-	function onAddMouseOut() {
-
-		setStyleOut( optionAdd );
-
-	}
-
-	function onHelpMouseOver() {
-
-		setStyleOver( optionHelp );
-
-	}
-
-	function onHelpMouseOut() {
-
-		setStyleOut( optionHelp );
 
 	}
 
