b7d916e760879e04d5b3065f187ecbe0c7685620
alteredq <postfilter@gmail.com>
2012-12-03 15:16:04 +0100

    WebGLDeferredRenderer: removed unused unlit shader.

--- examples/js/ShaderDeferred.js
+++ examples/js/ShaderDeferred.js
@@ -262,55 +262,6 @@ THREE.ShaderDeferred = {
 
 	},
 
-	"unlit" : {
-
-		uniforms: {
-
-			samplerDepth: { type: "t", value: null },
-			viewWidth:    { type: "f", value: 800 },
-			viewHeight:   { type: "f", value: 600 },
-			lightColor:   { type: "c", value: new THREE.Color( 0x000000 ) }
-
-		},
-
-		fragmentShader : [
-
-			"varying vec4 clipPos;",
-			"uniform sampler2D samplerDepth;",
-
-			"uniform float viewHeight;",
-			"uniform float viewWidth;",
-
-			"uniform vec3 lightColor;",
-
-			"void main() {",
-
-				"vec2 texCoord = gl_FragCoord.xy / vec2( viewWidth, viewHeight );",
-				"float z = texture2D( samplerDepth, texCoord ).x;",
-				"vec4 color = vec4( lightColor, 1.0 );",
-				"float depth = clipPos.z / clipPos.w;",
-				"if( depth > z && z > 0.0 ) color.w = 0.0;",
-				"gl_FragColor = color;",
-
-			"}"
-
-		].join("\n"),
-
-		vertexShader : [
-
-			"varying vec4 clipPos;",
-
-			"void main() {",
-
-				"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-				"clipPos = gl_Position;",
-
-			"}"
-
-		].join("\n")
-
-	},
-
 	"composite" : {
 
 		uniforms: {
