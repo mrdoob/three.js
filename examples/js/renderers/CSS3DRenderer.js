17f6d3cd640ec67f2063636247ee173953e5f91a
Ben Adams <thundercat@illyriad.co.uk>
2012-12-06 18:48:33 +0000

    Slightly faster string construction

--- examples/js/renderers/CSS3DRenderer.js
+++ examples/js/renderers/CSS3DRenderer.js
@@ -91,24 +91,24 @@ THREE.CSS3DRenderer = function () {
 
 		var elements = matrix.elements;
 
-		return 'matrix3d(' +
-			epsilon( elements[ 0 ] ) + ',' +
-			epsilon( - elements[ 1 ] ) + ',' +
-			epsilon( elements[ 2 ] ) + ',' +
-			epsilon( elements[ 3 ] ) + ',' +
-			epsilon( elements[ 4 ] ) + ',' +
-			epsilon( - elements[ 5 ] ) + ',' +
-			epsilon( elements[ 6 ] ) + ',' +
-			epsilon( elements[ 7 ] ) + ',' +
-			epsilon( elements[ 8 ] ) + ',' +
-			epsilon( - elements[ 9 ] ) + ',' +
-			epsilon( elements[ 10 ] ) + ',' +
-			epsilon( elements[ 11 ] ) + ',' +
-			epsilon( elements[ 12 ] ) + ',' +
-			epsilon( - elements[ 13 ] ) + ',' +
-			epsilon( elements[ 14 ] ) + ',' +
-			epsilon( elements[ 15 ] ) +
-		')';
+		return ['matrix3d(' ,
+			epsilon(elements[0]) , ',' ,
+			epsilon(-elements[1]) , ',' ,
+			epsilon(elements[2]) , ',' ,
+			epsilon(elements[3]) , ',' ,
+			epsilon(elements[4]) , ',' ,
+			epsilon(-elements[5]) , ',' ,
+			epsilon(elements[6]) , ',' ,
+			epsilon(elements[7]) , ',' ,
+			epsilon(elements[8]) , ',' ,
+			epsilon(-elements[9]) , ',' ,
+			epsilon(elements[10]) , ',' ,
+			epsilon(elements[11]) , ',' ,
+			epsilon(elements[12]) , ',' ,
+			epsilon(-elements[13]) , ',' ,
+			epsilon(elements[14]) , ',' ,
+			epsilon(elements[15]) ,
+		')'].join('');
 
 	}
 
@@ -116,24 +116,24 @@ THREE.CSS3DRenderer = function () {
 
 		var elements = matrix.elements;
 
-		return 'translate3d(-50%,-50%,0) matrix3d(' +
-			epsilon( elements[ 0 ] ) + ',' +
-			epsilon( elements[ 1 ] ) + ',' +
-			epsilon( elements[ 2 ] ) + ',' +
-			epsilon( elements[ 3 ] ) + ',' +
-			epsilon( - elements[ 4 ] ) + ',' +
-			epsilon( - elements[ 5 ] ) + ',' +
-			epsilon( - elements[ 6 ] ) + ',' +
-			epsilon( - elements[ 7 ] ) + ',' +
-			epsilon( elements[ 8 ] ) + ',' +
-			epsilon( elements[ 9 ] ) + ',' +
-			epsilon( elements[ 10 ] ) + ',' +
-			epsilon( elements[ 11 ] ) + ',' +
-			epsilon( elements[ 12 ] ) + ',' +
-			epsilon( elements[ 13 ] ) + ',' +
-			epsilon( elements[ 14 ] ) + ',' +
-			epsilon( elements[ 15 ] ) +
-		')';
+		return ['translate3d(-50%,-50%,0) matrix3d(' ,
+			epsilon( elements[ 0 ] ) , ',' ,
+			epsilon( elements[ 1 ] ) , ',' ,
+			epsilon( elements[ 2 ] ) , ',' ,
+			epsilon( elements[ 3 ] ) , ',' ,
+			epsilon( - elements[ 4 ] ) , ',' ,
+			epsilon( - elements[ 5 ] ) , ',' ,
+			epsilon( - elements[ 6 ] ) , ',' ,
+			epsilon( - elements[ 7 ] ) , ',' ,
+			epsilon( elements[ 8 ] ) , ',' ,
+			epsilon( elements[ 9 ] ) , ',' ,
+			epsilon( elements[ 10 ] ) , ',' ,
+			epsilon( elements[ 11 ] ) , ',' ,
+			epsilon( elements[ 12 ] ) , ',' ,
+			epsilon( elements[ 13 ] ) , ',' ,
+			epsilon( elements[ 14 ] ) , ',' ,
+			epsilon( elements[ 15 ] ) ,
+		')'].join('');
 
 	}
 
