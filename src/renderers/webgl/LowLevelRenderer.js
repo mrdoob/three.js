441ca41ddcf6530611094ad3620f4cb37331c7e6
OpenShift guest <jeroen-876@hotmail.com>
2013-01-29 04:33:25 -0500

    This makes sure getShaderPrecisionFormat exists on the context

--- src/renderers/webgl/LowLevelRenderer.js
+++ src/renderers/webgl/LowLevelRenderer.js
@@ -168,6 +168,19 @@ THREE.WebGLRenderer2.LowLevelRenderer = function(parameters){
 			console.log( 'THREE.WebGLRenderer: S3TC compressed textures not supported.' );
 
 		}
+		
+		if (! _gl.getShaderPrecisionFormat ) {
+			
+			_gl.getShaderPrecisionFormat = function() { 
+				
+				return {
+					"rangeMin"  : 1,
+					"rangeMax"  : 1,
+					"precision" : 1
+				};
+				
+			}
+		}
 
 	};
 	
