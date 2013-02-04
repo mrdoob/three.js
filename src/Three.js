53169124d9474a319092a7858a845e5e8b230a61
Ben Houston <ben@exocortex.com>
2013-02-04 08:11:41 -0500

    THREE.extend improvements: adopt safe {}.hasOwnProperty method source.  Optimize Object.keys() based on @paulmillr suggestion.

--- src/Three.js
+++ src/Three.js
@@ -51,18 +51,23 @@ THREE.extend = function ( obj, source ) {
 	// ECMAScript5 compatibility based on: http://www.nczonline.net/blog/2012/12/11/are-your-mixins-ecmascript-5-compatible/
 	if ( Object.keys ) {
 
-        Object.keys( source ).forEach( 
-			function ( prop ) {
-					Object.defineProperty( obj, prop, Object.getOwnPropertyDescriptor( source, prop ) );
-				}
-			);
+		var keys = Object.keys( source );
+
+		for (var i = 0, il = keys.length; i < il; i++) {
+
+			var prop = keys[i];
+			Object.defineProperty( obj, prop, Object.getOwnPropertyDescriptor( source, prop ) );
+
+		}
 
     }
     else {
 
+		var safeHasOwnProperty = {}.hasOwnProperty;
+
 		for ( var prop in source ) {
 
-			if ( source.hasOwnProperty( prop ) ) {
+			if ( safeHasOwnProperty.call( source, prop ) ) {
 
 				obj[prop] = source[prop];
 
