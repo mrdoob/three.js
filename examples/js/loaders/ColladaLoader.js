f3801d8246b6df0097a4c06a3a8bf34b4a2cce77
Jihoon Lee <jihoonlee.in@gmail.com>
2013-02-15 14:28:11 +0900

    Unit conversion for collada loader

--- examples/js/loaders/ColladaLoader.js
+++ examples/js/loaders/ColladaLoader.js
@@ -47,7 +47,6 @@ THREE.ColladaLoader = function () {
 
 	};
 
-	// TODO: support unit conversion as well
 	var colladaUnit = 1.0;
 	var colladaUp = 'Y';
 	var upConversion = null;
@@ -846,6 +845,9 @@ THREE.ColladaLoader = function () {
 		obj.quaternion = props[ 1 ];
 		obj.useQuaternion = true;
 		obj.scale = props[ 2 ];
+
+    // unit conversion
+    obj.scale.multiplyScalar(colladaUnit);
 
 		if ( options.centerGeometry && obj.geometry ) {
 
