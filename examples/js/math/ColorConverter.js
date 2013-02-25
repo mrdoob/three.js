c34e87a558b519de93c0266f20a96f499cf715fc
zz85 <zz85nus@gmail.com>
2013-02-25 14:21:41 +0800

    THREE.ColorConverter.fromHSV() -> THREE.ColorConverter.setHSV() THREE.ColorConverter.toHSV() -> THREE.ColorConverter.getHSV()
    Set and get seems to be natural since THREE.ColorConverter is a static class.

--- examples/js/math/ColorConverter.js
+++ examples/js/math/ColorConverter.js
@@ -5,14 +5,14 @@
 
 THREE.ColorConverter = {
 
-	fromHSV: function ( color, h, s, v ) {
+	setHSV: function ( color, h, s, v ) {
 
 		// https://gist.github.com/xpansive/1337890#file-index-js
 		return color.setHSL( h, ( s * v ) / ( ( h = ( 2 - s ) * v ) < 1 ? h : ( 2 - h ) ), h * 0.5 );
 
 	},
 
-	toHSV: function( color ) {
+	getHSV: function( color ) {
 
 		var hsl = color.getHSL();
 
