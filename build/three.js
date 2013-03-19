9e594cb112964715c73dbf3476473977e990a66e
Mr.doob <info@mrdoob.com>
2013-03-19 14:49:04 +0100

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -3724,6 +3724,50 @@ THREE.Matrix4.prototype = {
 
 	},
 
+	copyPosition: function ( m ) {
+
+		var te = this.elements;
+		var me = m.elements;
+
+		te[12] = me[12];
+		te[13] = me[13];
+		te[14] = me[14];
+
+		return this;
+
+	},
+
+	copyRotation: function () {
+
+		var v1 = new THREE.Vector3();
+
+		return function ( m ) {
+
+			var te = this.elements;
+			var me = m.elements;
+
+			var scaleX = 1 / v1.set( me[0], me[1], me[2] ).length();
+			var scaleY = 1 / v1.set( me[4], me[5], me[6] ).length();
+			var scaleZ = 1 / v1.set( me[8], me[9], me[10] ).length();
+
+			te[0] = me[0] * scaleX;
+			te[1] = me[1] * scaleX;
+			te[2] = me[2] * scaleX;
+
+			te[4] = me[4] * scaleY;
+			te[5] = me[5] * scaleY;
+			te[6] = me[6] * scaleY;
+
+			te[8] = me[8] * scaleZ;
+			te[9] = me[9] * scaleZ;
+			te[10] = me[10] * scaleZ;
+
+			return this;
+
+		};
+
+	}(),
+
 	setRotationFromEuler: function ( v, order ) {
 
 		var te = this.elements;
@@ -4235,47 +4279,17 @@ THREE.Matrix4.prototype = {
 
 	extractPosition: function ( m ) {
 
-		var te = this.elements;
-		var me = m.elements;
-
-		te[12] = me[12];
-		te[13] = me[13];
-		te[14] = me[14];
-
-		return this;
+		console.warn( 'DEPRECATED: Matrix4\'s .extractPosition() has been renamed to .copyPosition().' );
+		return this.copyPosition( m );
 
 	},
 
-	extractRotation: function() {
+	extractRotation: function ( m ) {
 
-		var v1 = new THREE.Vector3();
+		console.warn( 'DEPRECATED: Matrix4\'s .extractRotation() has been renamed to .copyRotation().' );
+		return this.copyRotation( m );
 
-		return function ( m ) {
-
-			var te = this.elements;
-			var me = m.elements;
-
-			var scaleX = 1 / v1.set( me[0], me[1], me[2] ).length();
-			var scaleY = 1 / v1.set( me[4], me[5], me[6] ).length();
-			var scaleZ = 1 / v1.set( me[8], me[9], me[10] ).length();
-
-			te[0] = me[0] * scaleX;
-			te[1] = me[1] * scaleX;
-			te[2] = me[2] * scaleX;
-
-			te[4] = me[4] * scaleY;
-			te[5] = me[5] * scaleY;
-			te[6] = me[6] * scaleY;
-
-			te[8] = me[8] * scaleZ;
-			te[9] = me[9] * scaleZ;
-			te[10] = me[10] * scaleZ;
-
-			return this;
-
-		};
-
-	}(),
+	},
 
 	translate: function ( v ) {
 
@@ -5978,7 +5992,7 @@ THREE.EventDispatcher = function () {
 			var a, b, c, d;
 			var precision = raycaster.precision;
 
-			object.matrixRotationWorld.extractRotation( object.matrixWorld );
+			object.matrixRotationWorld.copyRotation( object.matrixWorld );
 
 			inverseMatrix.getInverse( object.matrixWorld );
 
@@ -6192,7 +6206,7 @@ THREE.Object3D.prototype = {
 
 			this.scale.getScaleFromMatrix( this.matrix );
 
-			m1.extractRotation( this.matrix );
+			m1.copyRotation( this.matrix );
 
 			if ( this.useQuaternion === true )  {
 
