aa7567f46e2adaefd02f6a9f540dc52f91f636bd
zz85 <zz85nus@gmail.com>
2013-05-03 10:59:46 +0800

    Refactored Curve.js to individual classes (#3378)
    Split /src/extras/core/Curve.js to
    /src/extra/curves/*
    
    LineCurve
    QuadraticBezierCurve
    CubicBezierCurve
    SplineCurve
    EllipseCurve
    ArcCurve
    LineCurve3
    QuadraticBezierCurve3
    CubicBezierCurve3
    SplineCurve3
    ClosedSplineCurve3

---
+++ src/extras/curves/LineCurve.js
@@ -0,0 +1,37 @@
+/**************************************************************
+ *	Line
+ **************************************************************/
+
+THREE.LineCurve = function ( v1, v2 ) {
+
+	this.v1 = v1;
+	this.v2 = v2;
+
+};
+
+THREE.LineCurve.prototype = Object.create( THREE.Curve.prototype );
+
+THREE.LineCurve.prototype.getPoint = function ( t ) {
+
+	var point = this.v2.clone().sub(this.v1);
+	point.multiplyScalar( t ).add( this.v1 );
+
+	return point;
+
+};
+
+// Line curve is linear, so we can overwrite default getPointAt
+
+THREE.LineCurve.prototype.getPointAt = function ( u ) {
+
+	return this.getPoint( u );
+
+};
+
+THREE.LineCurve.prototype.getTangent = function( t ) {
+
+	var tangent = this.v2.clone().sub(this.v1);
+
+	return tangent.normalize();
+
+};
