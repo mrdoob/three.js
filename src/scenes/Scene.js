14f5298d777fee32743b12f106a6fd40f65d4b23
Mr.doob <info@mrdoob.com>
2013-05-31 10:23:23 +0200

    Spaces to tabs.

--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -110,21 +110,18 @@ THREE.Scene.prototype.__removeObject = function ( object ) {
 
 };
 
-THREE.Scene.prototype.clone = function (object) {
-    if ( object === undefined ) object = new THREE.Scene();
+THREE.Scene.prototype.clone = function ( object ) {
 
-    THREE.Object3D.prototype.clone.call(this, object);
-    
-    if(this.fog !== null) {
-        object.fog = this.fog.clone();
-    }
+	if ( object === undefined ) object = new THREE.Scene();
 
-    if(this.overrideMaterial !== null) {
-        object.overrideMaterial = this.overrideMaterial.clone();
-    }
-    
-    object.autoUpdate = this.autoUpdate;
-    object.matrixAutoUpdate = this.matrixAutoUpdate;
+	THREE.Object3D.prototype.clone.call(this, object);
 
-    return object;
+	if ( this.fog !== null ) object.fog = this.fog.clone();
+	if ( this.overrideMaterial !== null ) object.overrideMaterial = this.overrideMaterial.clone();
+
+	object.autoUpdate = this.autoUpdate;
+	object.matrixAutoUpdate = this.matrixAutoUpdate;
+
+	return object;
+
 };
