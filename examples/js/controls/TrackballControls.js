a92c98f47893968ac2fd302b6c84c2d16533db08
Patrick Fuller <patrickfuller@gmail.com>
2013-06-10 11:52:20 -0500

    From #1454, small edit to TrackballControls.js to zoom properly with an OrthographicCamera

--- examples/js/controls/TrackballControls.js
+++ examples/js/controls/TrackballControls.js
@@ -194,7 +194,19 @@ THREE.TrackballControls = function ( object, domElement ) {
 
 			if ( factor !== 1.0 && factor > 0.0 ) {
 
-				_eye.multiplyScalar( factor );
+				if ( this.object instanceof THREE.PerspectiveCamera ) {
+
+					_eye.multiplyScalar( factor );
+
+				} else {
+
+					this.object.left *= factor;
+					this.object.right *= factor;
+					this.object.top *= factor;
+					this.object.bottom *= factor;
+
+					this.object.updateProjectionMatrix();
+				}
 
 				if ( _this.staticMoving ) {
 
