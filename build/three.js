c1763e8e2b21dc159292c9f786311331beec3efa
Mr.doob <info@mrdoob.com>
2013-08-13 21:07:59 -0700

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -767,7 +767,7 @@ THREE.Quaternion.prototype = {
 
 		if ( euler instanceof THREE.Euler === false ) {
 
-			console.error( 'ERROR: Quaternion\'s .setFromEuler() now expects a Euler rotation rather than a Vector3 and order.  Please update your code.' );
+			throw new Error( 'ERROR: Quaternion\'s .setFromEuler() now expects a Euler rotation rather than a Vector3 and order.  Please update your code.' );
 		}
 
 		// http://www.mathworks.com/matlabcentral/fileexchange/
