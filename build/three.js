9074c152c2d3d12a094e692a62e72c11bd3217ad
Mr.doob <info@mrdoob.com>
2013-09-25 19:03:58 -0700

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -17565,7 +17565,7 @@ THREE.ShaderChunk = {
 
 		"#ifdef USE_COLOR",
 
-			"gl_FragColor = gl_FragColor * vec4( vColor, opacity );",
+			"gl_FragColor = gl_FragColor * vec4( vColor, 1.0 );",
 
 		"#endif"
 
