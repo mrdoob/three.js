ab0edec3daf492b5ba0f246c01a8bba4f2120b67
Paul Tondeur <paul@paultondeur.nl>
2013-11-03 15:38:04 +0100

    Gruntfile previous build commands cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,3 @@
-//python build.py --include common --include extras --output ../../build/three.js
-//python build.py --include common --include extras --minify --output ../../build/three.min.js
-//python build.py --include canvas --minify --output ../../build/three-canvas.min.js
-//python build.py --include css3d --minify --output ../../build/three-css3d.min.js
-//python build.py --include webgl --minify --output ../../build/three-webgl.min.js
-//python build.py --include extras --externs externs/extras.js --minify --output ../../build/three-extras.min.js
-//python build.py --include math --output ../../build/three-math.js
-//python build.py --include math --minify --output ../../build/three-math.min.js
-
 module.exports = function (grunt) {
 	var common = grunt.file.readJSON('utils/build/includes/common.json'),
 		extras = grunt.file.readJSON('utils/build/includes/extras.json'),
