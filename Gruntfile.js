ed951c9f53480dd0d9f1efea5a7753ba2309c304
Paul Tondeur <paul@paultondeur.nl>
2013-11-03 16:54:00 +0100

    Added bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function (grunt) {
 
 		bump: {
 			options: {
-				files: ['package.json'],
+				files: ['package.json','bower.json'],
 				updateConfigs: ["pkg"],
 				commit: true,
 				commitMessage: 'Release v%VERSION%',
