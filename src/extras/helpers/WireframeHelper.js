13ce7cc165f4474a12998599727b1483753811b2
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2013-12-09 17:17:34 +0100

    Improve THREE.WireframeHelper to allow processing buffer geometries

--- src/extras/helpers/WireframeHelper.js
+++ src/extras/helpers/WireframeHelper.js
@@ -10,32 +10,67 @@ THREE.WireframeHelper = function ( object ) {
 	var keys = [ 'a', 'b', 'c', 'd' ];
 	var geometry = new THREE.Geometry();
 
-	var vertices = object.geometry.vertices;
-	var faces = object.geometry.faces;
+	if (object.geometry.vertices) {
 
-	for ( var i = 0, l = faces.length; i < l; i ++ ) {
+		var vertices = object.geometry.vertices;
+		var faces = object.geometry.faces;
 
-		var face = faces[ i ];
+		for ( var i = 0, l = faces.length; i < l; i ++ ) {
 
-		for ( var j = 0; j < 3; j ++ ) {
+			var face = faces[ i ];
 
-			edge[ 0 ] = face[ keys[ j ] ];
-			edge[ 1 ] = face[ keys[ ( j + 1 ) % 3 ] ];
-			edge.sort( sortFunction );
+			for ( var j = 0; j < 3; j ++ ) {
 
-			var key = edge.toString();
+				edge[ 0 ] = face[ keys[ j ] ];
+				edge[ 1 ] = face[ keys[ ( j + 1 ) % 3 ] ];
+				edge.sort( sortFunction );
 
-			if ( hash[ key ] === undefined ) {
+				var key = edge.toString();
 
-				geometry.vertices.push( vertices[ edge[ 0 ] ] );
-				geometry.vertices.push( vertices[ edge[ 1 ] ] );
+				if ( hash[ key ] === undefined ) {
 
-				hash[ key ] = true;
+					geometry.vertices.push( vertices[ edge[ 0 ] ] );
+					geometry.vertices.push( vertices[ edge[ 1 ] ] );
+
+					hash[ key ] = true;
+
+				}
 
 			}
 
 		}
 
+    } else {
+		vertices = object.geometry.attributes.position.array;
+		faces = object.geometry.attributes.index.array;
+
+		for ( i = 0, l = faces.length / 3; i < l; i ++ ) {
+
+			for ( j = 0; j < 3; j ++ ) {
+                var index = i * 3;
+				edge[ 0 ] = faces[ index + j ];
+				edge[ 1 ] = faces[ index + (j + 1) % 3 ];
+				edge.sort( sortFunction );
+
+				key = edge.toString();
+
+				if ( hash[ key ] === undefined ) {
+
+                    for (var k = 0; k < 2; k ++) {
+
+                        var vertex = new THREE.Vector3(vertices[ 3 * edge[ k ] ],
+                            vertices[ 3 * edge[ k ] + 1 ],
+                            vertices[ 3 * edge[ k ] + 2 ]);
+    
+                        geometry.vertices.push( vertex );
+                        hash[ key ] = true;
+                    }
+
+				}
+
+			}
+
+		}
 	}
 
 	THREE.Line.call( this, geometry, new THREE.LineBasicMaterial( { color: 0xffffff } ), THREE.LinePieces );
