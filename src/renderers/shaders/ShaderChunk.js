c9f5f1e458e55955f211f1efb6d5434bede3d940
Mr.doob <info@mrdoob.com>
2014-01-27 17:27:30 +0100

    Implemented fix in #4152. All the skinning examples seem to be working fine still.

--- src/renderers/shaders/ShaderChunk.js
+++ src/renderers/shaders/ShaderChunk.js
@@ -1270,6 +1270,8 @@ THREE.ShaderChunk = {
 
 			"mat4 boneMatX = getBoneMatrix( skinIndex.x );",
 			"mat4 boneMatY = getBoneMatrix( skinIndex.y );",
+			"mat4 boneMatZ = getBoneMatrix( skinIndex.z );",
+			"mat4 boneMatW = getBoneMatrix( skinIndex.w );",
 
 		"#endif"
 
@@ -1290,7 +1292,9 @@ THREE.ShaderChunk = {
 			"#endif",
 
 			"vec4 skinned  = boneMatX * skinVertex * skinWeight.x;",
-			"skinned 	  += boneMatY * skinVertex * skinWeight.y;",
+			"skinned      += boneMatY * skinVertex * skinWeight.y;",
+			"skinned      += boneMatZ * skinVertex * skinWeight.z;",
+			"skinned      += boneMatW * skinVertex * skinWeight.w;",
 
 		"#endif"
 
