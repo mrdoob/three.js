30a4eaea880c909dd6633795d3d0641cc2a8d5d7
Mr.doob <info@mrdoob.com>
2014-01-29 19:30:21 +0100

    More perPixel removal.

--- src/materials/MeshPhongMaterial.js
+++ src/materials/MeshPhongMaterial.js
@@ -111,7 +111,6 @@ THREE.MeshPhongMaterial.prototype.clone = function () {
 	material.shininess = this.shininess;
 
 	material.metal = this.metal;
-	material.perPixel = this.perPixel;
 
 	material.wrapAround = this.wrapAround;
 	material.wrapRGB.copy( this.wrapRGB );
