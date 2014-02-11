e7e7e1ad9be520f8a04b1238cf083cd482226508
Mr.doob <info@mrdoob.com>
2014-02-10 19:31:50 -0500

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -8191,6 +8191,22 @@ THREE.Projector = function () {
 
 					}
 
+				} else if ( geometry instanceof THREE.Geometry2 ) {
+
+					vertices = geometry.vertices;
+
+					for ( var i = 0, l = vertices.length; i < l; i += 3 ) {
+
+						renderList.pushVertex( vertices[ i ], vertices[ i + 1 ], vertices[ i + 2 ] );
+
+					}
+
+					for ( var i = 0, l = vertices.length / 3; i < l; i += 3 ) {
+
+						renderList.pushTriangle( i, i + 1, i + 2 );
+
+					}
+
 				} else if ( geometry instanceof THREE.Geometry ) {
 
 					vertices = geometry.vertices;
@@ -9336,6 +9352,88 @@ THREE.Geometry.prototype = {
 THREE.EventDispatcher.prototype.apply( THREE.Geometry.prototype );
 
 THREE.GeometryIdCount = 0;
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+THREE.Geometry2 = function ( size ) {
+
+	this.id = THREE.GeometryIdCount ++;
+	this.uuid = THREE.Math.generateUUID();
+
+	this.name = '';
+
+	this.vertices = size !== undefined ? new Float32Array( size * 3 ) : [];
+	this.normals = size !== undefined ? new Float32Array( size * 3 ) : [];
+	this.uvs = size !== undefined ? new Float32Array( size * 2 ) : [];
+
+	this.boundingBox = null;
+	this.boundingSphere = null;
+
+};
+
+THREE.Geometry2.prototype = {
+
+	constructor: THREE.Geometry2,
+
+	applyMatrix: function ( matrix ) {
+
+		matrix.multiplyVector3Array( this.vertices );
+
+	},
+
+	computeBoundingSphere: function () {
+
+		var box = new THREE.Box3();
+		var vector = new THREE.Vector3();
+
+		return function () {
+
+			if ( this.boundingSphere === null ) {
+
+				this.boundingSphere = new THREE.Sphere();
+
+			}
+
+			box.makeEmpty();
+
+			var vertices = this.vertices;
+			var center = this.boundingSphere.center;
+
+			for ( var i = 0, il = vertices.length; i < il; i += 3 ) {
+
+				vector.set( vertices[ i ], vertices[ i + 1 ], vertices[ i + 2 ] );
+				box.addPoint( vector );
+
+			}
+
+			box.center( center );
+
+			var maxRadiusSq = 0;
+
+			for ( var i = 0, il = vertices.length; i < il; i += 3 ) {
+
+				vector.set( vertices[ i ], vertices[ i + 1 ], vertices[ i + 2 ] );
+				maxRadiusSq = Math.max( maxRadiusSq, center.distanceToSquared( vector ) );
+
+			}
+
+			this.boundingSphere.radius = Math.sqrt( maxRadiusSq );
+
+		}
+
+	}(),
+
+	dispose: function () {
+
+		this.dispatchEvent( { type: 'dispose' } );
+
+	}
+
+};
+
+THREE.EventDispatcher.prototype.apply( THREE.Geometry2.prototype );
 
 /**
  * @author alteredq / http://alteredqualia.com/
