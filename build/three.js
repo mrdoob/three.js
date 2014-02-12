4b30560b9e8e4ef9766f687aa23bb20fd1deecda
Mr.doob <info@mrdoob.com>
2014-02-12 18:14:24 -0500

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -7863,7 +7863,7 @@ THREE.Projector = function () {
 	_line, _lineCount, _linePool = [], _linePoolLength = 0,
 	_sprite, _spriteCount, _spritePool = [], _spritePoolLength = 0,
 
-	_renderData = { objects: [], sprites: [], lights: [], elements: [] },
+	_renderData = { objects: [], lights: [], elements: [] },
 
 	_vA = new THREE.Vector3(),
 	_vB = new THREE.Vector3(),
@@ -7933,28 +7933,6 @@ THREE.Projector = function () {
 
 	};
 
-	var getObject = function ( object ) {
-
-		_object = getNextObjectInPool();
-		_object.id = object.id;
-		_object.object = object;
-
-		if ( object.renderDepth !== null ) {
-
-			_object.z = object.renderDepth;
-
-		} else {
-
-			_vector3.setFromMatrixPosition( object.matrixWorld );
-			_vector3.applyProjection( _viewProjectionMatrix );
-			_object.z = _vector3.z;
-
-		}
-
-		return _object;
-
-	};
-
 	var projectObject = function ( object ) {
 
 		if ( object.visible === false ) return;
@@ -7963,17 +7941,29 @@ THREE.Projector = function () {
 
 			_renderData.lights.push( object );
 
-		} else if ( object instanceof THREE.Mesh || object instanceof THREE.Line ) {
+		} else if ( object instanceof THREE.Mesh || object instanceof THREE.Line || object instanceof THREE.Sprite ) {
 
 			if ( object.frustumCulled === false || _frustum.intersectsObject( object ) === true ) {
 
-				_renderData.objects.push( getObject( object ) );
+				_object = getNextObjectInPool();
+				_object.id = object.id;
+				_object.object = object;
+
+				if ( object.renderDepth !== null ) {
+
+					_object.z = object.renderDepth;
+
+				} else {
+
+					_vector3.setFromMatrixPosition( object.matrixWorld );
+					_vector3.applyProjection( _viewProjectionMatrix );
+					_object.z = _vector3.z;
+
+				}
+
+				_renderData.objects.push( _object );
 
 			}
-
-		} else if ( object instanceof THREE.Sprite ) {
-
-			_renderData.sprites.push( getObject( object ) );
 
 		}
 
@@ -7990,7 +7980,6 @@ THREE.Projector = function () {
 		_objectCount = 0;
 
 		_renderData.objects.length = 0;
-		_renderData.sprites.length = 0;
 		_renderData.lights.length = 0;
 
 		projectObject( root );
@@ -8476,40 +8465,36 @@ THREE.Projector = function () {
 
 				}
 
-			}
+			} else if ( object instanceof THREE.Sprite ) {
 
-		}
+				_modelMatrix = object.matrixWorld;
 
-		for ( o = 0, ol = _renderData.sprites.length; o < ol; o++ ) {
+				_vector4.set( _modelMatrix.elements[12], _modelMatrix.elements[13], _modelMatrix.elements[14], 1 );
+				_vector4.applyMatrix4( _viewProjectionMatrix );
 
-			object = _renderData.sprites[ o ].object;
+				var invW = 1 / _vector4.w;
 
-			_modelMatrix = object.matrixWorld;
+				_vector4.z *= invW;
 
-			_vector4.set( _modelMatrix.elements[12], _modelMatrix.elements[13], _modelMatrix.elements[14], 1 );
-			_vector4.applyMatrix4( _viewProjectionMatrix );
+				if ( _vector4.z >= -1 && _vector4.z <= 1 ) {
 
-			var invW = 1 / _vector4.w;
+					_sprite = getNextSpriteInPool();
+					_sprite.id = object.id;
+					_sprite.x = _vector4.x * invW;
+					_sprite.y = _vector4.y * invW;
+					_sprite.z = _vector4.z;
+					_sprite.object = object;
 
-			_vector4.z *= invW;
+					_sprite.rotation = object.rotation;
 
-			if ( _vector4.z >= -1 && _vector4.z <= 1 ) {
+					_sprite.scale.x = object.scale.x * Math.abs( _sprite.x - ( _vector4.x + camera.projectionMatrix.elements[0] ) / ( _vector4.w + camera.projectionMatrix.elements[12] ) );
+					_sprite.scale.y = object.scale.y * Math.abs( _sprite.y - ( _vector4.y + camera.projectionMatrix.elements[5] ) / ( _vector4.w + camera.projectionMatrix.elements[13] ) );
 
-				_sprite = getNextSpriteInPool();
-				_sprite.id = object.id;
-				_sprite.x = _vector4.x * invW;
-				_sprite.y = _vector4.y * invW;
-				_sprite.z = _vector4.z;
-				_sprite.object = object;
+					_sprite.material = object.material;
 
-				_sprite.rotation = object.rotation;
+					_renderData.elements.push( _sprite );
 
-				_sprite.scale.x = object.scale.x * Math.abs( _sprite.x - ( _vector4.x + camera.projectionMatrix.elements[0] ) / ( _vector4.w + camera.projectionMatrix.elements[12] ) );
-				_sprite.scale.y = object.scale.y * Math.abs( _sprite.y - ( _vector4.y + camera.projectionMatrix.elements[5] ) / ( _vector4.w + camera.projectionMatrix.elements[13] ) );
-
-				_sprite.material = object.material;
-
-				_renderData.elements.push( _sprite );
+				}
 
 			}
 
@@ -15534,6 +15519,7 @@ THREE.Sprite = function ( material ) {
 
 	THREE.Object3D.call( this );
 
+	this.geometry = { boundingSphere: new THREE.Sphere( new THREE.Vector3(), 0.5 ) };
 	this.material = ( material !== undefined ) ? material : new THREE.SpriteMaterial();
 
 };
@@ -16199,13 +16185,6 @@ THREE.CanvasRenderer = function ( parameters ) {
 		var dist = 0.5 * Math.sqrt( scaleX * scaleX + scaleY * scaleY ); // allow for rotated sprite
 		_elemBox.min.set( v1.x - dist, v1.y - dist );
 		_elemBox.max.set( v1.x + dist, v1.y + dist );
-
-		if ( _clipBox.isIntersectionBox( _elemBox ) === false ) {
-
-			_elemBox.makeEmpty();
-			return;
-
-		}
 
 		if ( material instanceof THREE.SpriteMaterial ||
 			 material instanceof THREE.ParticleSystemMaterial ) { // Backwards compatibility
