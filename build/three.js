84ef5f9076563a5c416335807ef2277d895743c5
Mr.doob <info@mrdoob.com>
2014-02-12 20:15:23 -0500

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -15509,14 +15509,21 @@ THREE.LOD.prototype.clone = function ( object ) {
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.Sprite = function ( material ) {
+THREE.Sprite = ( function () {
 
-	THREE.Object3D.call( this );
+	var geometry = new THREE.Geometry2( 3 );
+	geometry.vertices.set( [ - 0.5, - 0.5, 0, 0.5, - 0.5, 0, 0.5, 0.5, 0 ] );
 
-	this.geometry = { boundingSphere: new THREE.Sphere( new THREE.Vector3(), 0.5 ), addEventListener: function () {} /* WebGLRenderer dependency */ };
-	this.material = ( material !== undefined ) ? material : new THREE.SpriteMaterial();
+	return function ( material ) {
 
-};
+		THREE.Object3D.call( this );
+
+		this.geometry = geometry;
+		this.material = ( material !== undefined ) ? material : new THREE.SpriteMaterial();
+
+	};
+
+} )();
 
 THREE.Sprite.prototype = Object.create( THREE.Object3D.prototype );
 
