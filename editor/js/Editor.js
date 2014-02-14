ffe96c174cc4fe219dc27a43f971263686a7dadc
Mr.doob <info@mrdoob.com>
2014-02-14 00:03:58 -0500

    Editor: Geometry2 support.

--- editor/js/Editor.js
+++ editor/js/Editor.js
@@ -357,6 +357,7 @@ Editor.prototype = {
 			'TorusKnotGeometry': THREE.TorusKnotGeometry,
 			'TubeGeometry': THREE.TubeGeometry,
 			'Geometry': THREE.Geometry,
+			'Geometry2': THREE.Geometry2,
 			'BufferGeometry': THREE.BufferGeometry
 
 		};
