8a5670055962067d7f7a93f8384478481fe8f3d1
srifqi <muhammadrifqipriyosusanto@gmail.com>
2014-02-17 19:44:02 +0700

    Update CubeGeometry.js
    warning the user

--- src/extras/geometries/CubeGeometry.js
+++ src/extras/geometries/CubeGeometry.js
@@ -1,3 +1,6 @@
 // DEPRECATED
 
-THREE.CubeGeometry = THREE.BoxGeometry;
+THREE.CubeGeometry = function(){
+	console.warn( 'DEPRECATED: THREE.CubeGeometry is deprecated. Use THREE.BoxGeometry instead.' );
+	return new THREE.BoxGeometry();
+};
