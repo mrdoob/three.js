d61d5f7a3e6b70402b9c23f15ca41a87846c352f
Mr.doob <info@mrdoob.com>
2014-02-16 23:19:43 -0500

    Fixed DoubleSide regression in Projector.

--- src/core/Projector.js
+++ src/core/Projector.js
@@ -470,7 +470,8 @@ THREE.Projector = function () {
 
 						var visible = renderList.checkTriangleVisibility( v1, v2, v3 );
 
-						if ( visible === ( side === THREE.BackSide ) ) continue;
+						if ( ( visible === false && side === THREE.FrontSide ) ||
+							 ( visible === true && side === THREE.BackSide ) ) continue;
 
 						_face = getNextFaceInPool();
 
