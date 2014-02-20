f59ab84051840bbbc3ec0539846885547d710dd1
Mr.doob <info@mrdoob.com>
2014-02-20 16:14:29 -0500

    Finished CubeGeometry deprecated change.

--- src/extras/geometries/CubeGeometry.js
+++ src/extras/geometries/CubeGeometry.js
@@ -1,6 +1,6 @@
 // DEPRECATED
 
-THREE.CubeGeometry = function(){
+THREE.CubeGeometry = function ( width, height, depth, widthSegments, heightSegments, depthSegments ) {
 	console.warn( 'DEPRECATED: THREE.CubeGeometry is deprecated. Use THREE.BoxGeometry instead.' );
-	return new THREE.BoxGeometry();
-};
+ 	return new THREE.BoxGeometry( width, height, depth, widthSegments, heightSegments, depthSegments );
+ };
