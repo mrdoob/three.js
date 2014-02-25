c44c885d79af59621f8c1c35367c87d779687085
Mr.doob <info@mrdoob.com>
2014-02-25 17:59:37 -0500

    WebGLRenderer: Generating easier to read shaders.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -5512,39 +5512,39 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 			"#ifdef USE_COLOR",
 
-				"attribute vec3 color;",
+			"	attribute vec3 color;",
 
 			"#endif",
 
 			"#ifdef USE_MORPHTARGETS",
 
-				"attribute vec3 morphTarget0;",
-				"attribute vec3 morphTarget1;",
-				"attribute vec3 morphTarget2;",
-				"attribute vec3 morphTarget3;",
+			"	attribute vec3 morphTarget0;",
+			"	attribute vec3 morphTarget1;",
+			"	attribute vec3 morphTarget2;",
+			"	attribute vec3 morphTarget3;",
 
-				"#ifdef USE_MORPHNORMALS",
+			"	#ifdef USE_MORPHNORMALS",
 
-					"attribute vec3 morphNormal0;",
-					"attribute vec3 morphNormal1;",
-					"attribute vec3 morphNormal2;",
-					"attribute vec3 morphNormal3;",
+			"		attribute vec3 morphNormal0;",
+			"		attribute vec3 morphNormal1;",
+			"		attribute vec3 morphNormal2;",
+			"		attribute vec3 morphNormal3;",
 
-				"#else",
+			"	#else",
 
-					"attribute vec3 morphTarget4;",
-					"attribute vec3 morphTarget5;",
-					"attribute vec3 morphTarget6;",
-					"attribute vec3 morphTarget7;",
+			"		attribute vec3 morphTarget4;",
+			"		attribute vec3 morphTarget5;",
+			"		attribute vec3 morphTarget6;",
+			"		attribute vec3 morphTarget7;",
 
-				"#endif",
+			"	#endif",
 
 			"#endif",
 
 			"#ifdef USE_SKINNING",
 
-				"attribute vec4 skinIndex;",
-				"attribute vec4 skinWeight;",
+			"	attribute vec4 skinIndex;",
+			"	attribute vec4 skinWeight;",
 
 			"#endif",
 
