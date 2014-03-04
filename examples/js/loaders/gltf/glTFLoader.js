aec75c71e09145bd21f9c334f364ea29403e7d82
Mr.doob <info@mrdoob.com>
2014-03-04 16:44:01 -0500

    Fixed glTFLoader breakage cause by recent matrixWorldNeedsUpdate default change. /cc @tparisi

--- examples/js/loaders/gltf/glTFLoader.js
+++ examples/js/loaders/gltf/glTFLoader.js
@@ -1093,13 +1093,14 @@ THREE.glTFLoader.prototype.load = function( url, callback ) {
 
                 var m = description.matrix;
                 if(m) {
-                    threeNode.matrixAutoUpdate = false;
                     threeNode.applyMatrix(new THREE.Matrix4(
                         m[0],  m[4],  m[8],  m[12],
                         m[1],  m[5],  m[9],  m[13],
                         m[2],  m[6],  m[10], m[14],
                         m[3],  m[7],  m[11], m[15]
-                    ));                    
+                    ));
+                    threeNode.matrixAutoUpdate = false;
+                    threeNode.matrixWorldNeedsUpdate = true;
                 }
                 else {
                 	var t = description.translation;
@@ -1119,6 +1120,7 @@ THREE.glTFLoader.prototype.load = function( url, callback ) {
                 	var matrix = new THREE.Matrix4;
                 	matrix.compose(position, rotation, scale);
                     threeNode.matrixAutoUpdate = false;
+                    threeNode.matrixWorldNeedsUpdate = true;
                     threeNode.applyMatrix(matrix);                    
                 }
 
