51d22d83bd5ce110b9b65ee2a01d30697a6d28a3
Mr.doob <info@mrdoob.com>
2014-03-15 09:21:05 -0600

    ShaderLib: Implemented @forest-ka shadow fix. See #4341.

--- src/renderers/shaders/ShaderLib.js
+++ src/renderers/shaders/ShaderLib.js
@@ -1282,7 +1282,7 @@ THREE.ShaderLib = {
 
 			"	const vec4 bit_shift = vec4( 256.0 * 256.0 * 256.0, 256.0 * 256.0, 256.0, 1.0 );",
 			"	const vec4 bit_mask  = vec4( 0.0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0 );",
-			"	vec4 res = fract( depth * bit_shift );",
+			"	vec4 res = mod( depth * bit_shift * vec4( 255 ), vec4( 256 ) ) / vec4( 255 );", // "	vec4 res = fract( depth * bit_shift );",
 			"	res -= res.xxyz * bit_mask;",
 			"	return res;",
 
