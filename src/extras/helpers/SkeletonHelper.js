c43140f01422b280fd30e5917440dd7969317e59
Mr.doob <info@mrdoob.com>
2014-04-11 23:09:02 -0600

    Tab indentations.

--- src/extras/helpers/SkeletonHelper.js
+++ src/extras/helpers/SkeletonHelper.js
@@ -5,48 +5,48 @@
 
 THREE.SkeletonHelper = function ( skeleton, jointBoxSize, scaleRatio ) {
 
-  THREE.Object3D.call( this );
+	THREE.Object3D.call( this );
 
-  this.scaleRatio = ( scaleRatio !== undefined ) ? scaleRatio : 1;
-  this.skeleton = skeleton;
+	this.scaleRatio = ( scaleRatio !== undefined ) ? scaleRatio : 1;
+	this.skeleton = skeleton;
 
-  if ( jointBoxSize === undefined ) jointBoxSize = 1;
-  
-  var boxSize = jointBoxSize * this.scaleRatio;
+	if ( jointBoxSize === undefined ) jointBoxSize = 1;
+	
+	var boxSize = jointBoxSize * this.scaleRatio;
 
-  for ( var i = 0; i < skeleton.bones.length; ++i ) {
+	for ( var i = 0; i < skeleton.bones.length; ++i ) {
 
-    var bone = skeleton.bones[ i ];
-    var boxGeometry = new THREE.BoxGeometry( boxSize, boxSize, boxSize );
-    var boxMaterial = new THREE.MeshBasicMaterial();
+		var bone = skeleton.bones[ i ];
+		var boxGeometry = new THREE.BoxGeometry( boxSize, boxSize, boxSize );
+		var boxMaterial = new THREE.MeshBasicMaterial();
 
-    bone.helper = {};
-    bone.helper.box = new THREE.Mesh( boxGeometry, boxMaterial );
-    bone.helper.axes = new THREE.AxisHelper( jointBoxSize * 3 );
+		bone.helper = {};
+		bone.helper.box = new THREE.Mesh( boxGeometry, boxMaterial );
+		bone.helper.axes = new THREE.AxisHelper( jointBoxSize * 3 );
 
-    this.add( bone.helper.box );
-    this.add( bone.helper.axes );
+		this.add( bone.helper.box );
+		this.add( bone.helper.axes );
 
-    if ( bone.parent instanceof THREE.Bone ) {
+		if ( bone.parent instanceof THREE.Bone ) {
 
-      var lineMaterial = new THREE.LineBasicMaterial();
-      var lineGeometry = new THREE.Geometry();
+			var lineMaterial = new THREE.LineBasicMaterial();
+			var lineGeometry = new THREE.Geometry();
 
-      lineMaterial.vertexColors = true;
+			lineMaterial.vertexColors = true;
 
-      lineGeometry.vertices.push( new THREE.Vector3() );
-      lineGeometry.vertices.push( new THREE.Vector3() );
-      lineGeometry.colors.push( new THREE.Color( 1, 1, 0 ) );
-      lineGeometry.colors.push( new THREE.Color( 0, 0, 0 ) );
+			lineGeometry.vertices.push( new THREE.Vector3() );
+			lineGeometry.vertices.push( new THREE.Vector3() );
+			lineGeometry.colors.push( new THREE.Color( 1, 1, 0 ) );
+			lineGeometry.colors.push( new THREE.Color( 0, 0, 0 ) );
 
-      bone.helper.line = new THREE.Line( lineGeometry, lineMaterial );
-      this.add( bone.helper.line);
+			bone.helper.line = new THREE.Line( lineGeometry, lineMaterial );
+			this.add( bone.helper.line);
 
-    }
+		}
 
-  }
+	}
 
-  this.update();
+	this.update();
 };
 
 
@@ -54,50 +54,50 @@ THREE.SkeletonHelper.prototype = Object.create( THREE.Object3D.prototype );
 
 THREE.SkeletonHelper.prototype.update = function () {
 
-  for ( var i = 0; i < this.skeleton.bones.length; ++i ) {
+	for ( var i = 0; i < this.skeleton.bones.length; ++i ) {
 
-    var bone = this.skeleton.bones[ i ];
+		var bone = this.skeleton.bones[ i ];
 
-    if ( this.visible && bone.parent instanceof THREE.Bone ) {
+		if ( this.visible && bone.parent instanceof THREE.Bone ) {
 
-      bone.skinMatrix.decompose( bone.helper.box.position, bone.helper.box.quaternion, bone.helper.box.scale );
-      bone.helper.box.position.multiplyScalar( this.scaleRatio );
+			bone.skinMatrix.decompose( bone.helper.box.position, bone.helper.box.quaternion, bone.helper.box.scale );
+			bone.helper.box.position.multiplyScalar( this.scaleRatio );
 
-      bone.helper.axes.quaternion = bone.helper.box.quaternion;
-      bone.helper.axes.position = bone.helper.box.position;
-      bone.helper.axes.scale = bone.helper.box.scale;
+			bone.helper.axes.quaternion = bone.helper.box.quaternion;
+			bone.helper.axes.position = bone.helper.box.position;
+			bone.helper.axes.scale = bone.helper.box.scale;
 
-      bone.helper.line.geometry.vertices[0].setFromMatrixPosition( bone.skinMatrix );
-      bone.helper.line.geometry.vertices[0].multiplyScalar( this.scaleRatio );
+			bone.helper.line.geometry.vertices[0].setFromMatrixPosition( bone.skinMatrix );
+			bone.helper.line.geometry.vertices[0].multiplyScalar( this.scaleRatio );
 
-      bone.helper.line.geometry.vertices[1].setFromMatrixPosition( bone.parent.skinMatrix );
-      bone.helper.line.geometry.vertices[1].multiplyScalar( this.scaleRatio );
+			bone.helper.line.geometry.vertices[1].setFromMatrixPosition( bone.parent.skinMatrix );
+			bone.helper.line.geometry.vertices[1].multiplyScalar( this.scaleRatio );
 
-      bone.helper.line.geometry.verticesNeedUpdate = true;
+			bone.helper.line.geometry.verticesNeedUpdate = true;
 
-    }
+		}
 
-  }
+	}
 
 };
 
 THREE.SkeletonHelper.prototype.setVisible = function ( shouldBeVisible ) {
 
-  for ( var i = 0; i < this.skeleton.bones.length; ++i ) {
+	for ( var i = 0; i < this.skeleton.bones.length; ++i ) {
 
-    var bone = this.skeleton.bones[ i ];
-    if ( bone.helper ) {
+		var bone = this.skeleton.bones[ i ];
+		if ( bone.helper ) {
 
-      bone.helper.box.visible = shouldBeVisible;
-      bone.helper.axes.visible = shouldBeVisible;
+			bone.helper.box.visible = shouldBeVisible;
+			bone.helper.axes.visible = shouldBeVisible;
 
-      if ( bone.parent instanceof THREE.Bone ) {
+			if ( bone.parent instanceof THREE.Bone ) {
 
-         bone.helper.line.visible = shouldBeVisible;
+				 bone.helper.line.visible = shouldBeVisible;
 
-      }
+			}
 
-    }
+		}
 
-  }
+	}
 }
--- src/objects/Skeleton.js
+++ src/objects/Skeleton.js
@@ -6,96 +6,96 @@
 
 THREE.Skeleton = function ( boneList, useVertexTexture ) {
 
-  this.useVertexTexture = useVertexTexture !== undefined ? useVertexTexture : true;
+	this.useVertexTexture = useVertexTexture !== undefined ? useVertexTexture : true;
 
-  // init bones
+	// init bones
 
-  this.bones = [];
-  this.boneMatrices = [];
+	this.bones = [];
+	this.boneMatrices = [];
 
-  var bone, gbone, p, q, s;
+	var bone, gbone, p, q, s;
 
-  if ( boneList !== undefined ) {
+	if ( boneList !== undefined ) {
 
-    for ( var b = 0; b < boneList.length; ++b ) {
+		for ( var b = 0; b < boneList.length; ++b ) {
 
-      gbone = boneList[ b ];
+			gbone = boneList[ b ];
 
-      p = gbone.pos;
-      q = gbone.rotq;
-      s = gbone.scl;
+			p = gbone.pos;
+			q = gbone.rotq;
+			s = gbone.scl;
 
-      bone = this.addBone();
+			bone = this.addBone();
 
-      bone.name = gbone.name;
-      bone.position.set( p[0], p[1], p[2] );
-      bone.quaternion.set( q[0], q[1], q[2], q[3] );
+			bone.name = gbone.name;
+			bone.position.set( p[0], p[1], p[2] );
+			bone.quaternion.set( q[0], q[1], q[2], q[3] );
 
-      if ( s !== undefined ) {
+			if ( s !== undefined ) {
 
-        bone.scale.set( s[0], s[1], s[2] );
+				bone.scale.set( s[0], s[1], s[2] );
 
-      } else {
+			} else {
 
-        bone.scale.set( 1, 1, 1 );
+				bone.scale.set( 1, 1, 1 );
 
-      }
+			}
 
-    }
+		}
 
-    for ( var b = 0; b < boneList.length; ++b ) {
+		for ( var b = 0; b < boneList.length; ++b ) {
 
-      gbone = boneList[ b ];
+			gbone = boneList[ b ];
 
-      if ( gbone.parent !== -1 ) {
+			if ( gbone.parent !== -1 ) {
 
-        this.bones[ gbone.parent ].add( this.bones[ b ] );
+				this.bones[ gbone.parent ].add( this.bones[ b ] );
 
-      }
+			}
 
-    }
+		}
 
-    //
+		//
 
-    var nBones = this.bones.length;
+		var nBones = this.bones.length;
 
-    if ( this.useVertexTexture ) {
+		if ( this.useVertexTexture ) {
 
-      // layout (1 matrix = 4 pixels)
-      //  RGBA RGBA RGBA RGBA (=> column1, column2, column3, column4)
-      //  with  8x8  pixel texture max   16 bones  (8 * 8  / 4)
-      //     16x16 pixel texture max   64 bones (16 * 16 / 4)
-      //     32x32 pixel texture max  256 bones (32 * 32 / 4)
-      //     64x64 pixel texture max 1024 bones (64 * 64 / 4)
+			// layout (1 matrix = 4 pixels)
+			//  RGBA RGBA RGBA RGBA (=> column1, column2, column3, column4)
+			//  with  8x8  pixel texture max   16 bones  (8 * 8  / 4)
+			//     16x16 pixel texture max   64 bones (16 * 16 / 4)
+			//     32x32 pixel texture max  256 bones (32 * 32 / 4)
+			//     64x64 pixel texture max 1024 bones (64 * 64 / 4)
 
-      var size;
+			var size;
 
-      if ( nBones > 256 )
-        size = 64;
-      else if ( nBones > 64 )
-        size = 32;
-      else if ( nBones > 16 )
-        size = 16;
-      else
-        size = 8;
+			if ( nBones > 256 )
+				size = 64;
+			else if ( nBones > 64 )
+				size = 32;
+			else if ( nBones > 16 )
+				size = 16;
+			else
+				size = 8;
 
-      this.boneTextureWidth = size;
-      this.boneTextureHeight = size;
+			this.boneTextureWidth = size;
+			this.boneTextureHeight = size;
 
-      this.boneMatrices = new Float32Array( this.boneTextureWidth * this.boneTextureHeight * 4 ); // 4 floats per RGBA pixel
-      this.boneTexture = new THREE.DataTexture( this.boneMatrices, this.boneTextureWidth, this.boneTextureHeight, THREE.RGBAFormat, THREE.FloatType );
-      this.boneTexture.minFilter = THREE.NearestFilter;
-      this.boneTexture.magFilter = THREE.NearestFilter;
-      this.boneTexture.generateMipmaps = false;
-      this.boneTexture.flipY = false;
+			this.boneMatrices = new Float32Array( this.boneTextureWidth * this.boneTextureHeight * 4 ); // 4 floats per RGBA pixel
+			this.boneTexture = new THREE.DataTexture( this.boneMatrices, this.boneTextureWidth, this.boneTextureHeight, THREE.RGBAFormat, THREE.FloatType );
+			this.boneTexture.minFilter = THREE.NearestFilter;
+			this.boneTexture.magFilter = THREE.NearestFilter;
+			this.boneTexture.generateMipmaps = false;
+			this.boneTexture.flipY = false;
 
-    } else {
+		} else {
 
-      this.boneMatrices = new Float32Array( 16 * nBones );
+			this.boneMatrices = new Float32Array( 16 * nBones );
 
-    }
+		}
 
-  }
+	}
 
 };
 
@@ -105,31 +105,31 @@ THREE.Skeleton.prototype = Object.create( THREE.Mesh.prototype );
 
 THREE.Skeleton.prototype.addBone = function( bone ) {
 
-  if ( bone === undefined ) {
+	if ( bone === undefined ) {
 
-    bone = new THREE.Bone( this );
+		bone = new THREE.Bone( this );
 
-  }
+	}
 
-  this.bones.push( bone );
+	this.bones.push( bone );
 
-  return bone;
+	return bone;
 
 };
 
 
 THREE.Skeleton.prototype.calculateInverses = function( bone ) {
 
-  this.boneInverses = [];
+	this.boneInverses = [];
 
-  for ( var b = 0, bl = this.bones.length; b < bl; ++b ) {
+	for ( var b = 0, bl = this.bones.length; b < bl; ++b ) {
 
-    var inverse = new THREE.Matrix4();
+		var inverse = new THREE.Matrix4();
 
-    inverse.getInverse( this.bones[ b ].skinMatrix );
+		inverse.getInverse( this.bones[ b ].skinMatrix );
 
-    this.boneInverses.push( inverse );
+		this.boneInverses.push( inverse );
 
-  }
+	}
 
 };
