422b7118e6dee3b8bb0133e7700ddb66f2a55789
Mr.doob <info@mrdoob.com>
2014-04-25 17:45:24 -0400

    BufferGeometry: Check and report NaN cases in computeBoundingBox/computeBoundinSphere.

--- src/core/BufferGeometry.js
+++ src/core/BufferGeometry.js
@@ -149,6 +149,12 @@ THREE.BufferGeometry.prototype = {
 
 		}
 
+		if ( isNaN( this.boundingBox.min.x ) || isNaN( this.boundingBox.min.y ) || isNaN( this.boundingBox.min.z ) ) {
+
+			console.error( 'THREE.BufferGeometry.computeBoundingBox()', 'Computed min/max have NaN values. The "position" attribute is likely to have NaN values.' );
+
+		}
+
 	},
 
 	computeBoundingSphere: function () {
@@ -194,6 +200,12 @@ THREE.BufferGeometry.prototype = {
 				}
 
 				this.boundingSphere.radius = Math.sqrt( maxRadiusSq );
+
+				if ( isNaN( this.boundingSphere.radius ) ) {
+
+					console.error( 'THREE.BufferGeometry.computeBoundingSphere()', 'Computed radius is NaN. The "position" attribute is likely to have NaN values.' );
+
+				}
 
 			}
 
