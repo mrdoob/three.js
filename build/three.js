88a104b6c430853e5aac0e9353e3764b06309290
Mr.doob <info@mrdoob.com>
2014-05-02 18:54:40 -0400

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -7096,6 +7096,34 @@ THREE.EventDispatcher.prototype = {
 
 			} );
 
+
+		} else if ( object instanceof THREE.PointCloud ) {
+
+			var vertices = object.geometry.vertices;
+
+			for ( var i = 0; i < vertices.length; i ++ ) {
+
+				var v = vertices[ i ];
+
+				matrixPosition.copy( v ).applyMatrix4( object.matrixWorld );
+
+				var distance = raycaster.ray.distanceToPoint( matrixPosition );
+
+				if ( distance < 1 ) { // needs a better test; particle size?
+
+					intersects.push( {
+
+						distance: distance,
+						index: i,
+						face: null,
+						object: object
+
+					} );
+
+				}
+
+			}
+
 		} else if ( object instanceof THREE.LOD ) {
 
 			matrixPosition.setFromMatrixPosition( object.matrixWorld );
