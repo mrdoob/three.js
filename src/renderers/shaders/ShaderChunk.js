e16ec4be7a850398027da378b981e4f76c557888
Mr.doob <info@mrdoob.com>
2014-05-10 17:47:35 +0200

    Fixed glsl typo.

--- src/renderers/shaders/ShaderChunk.js
+++ src/renderers/shaders/ShaderChunk.js
@@ -1202,7 +1202,7 @@ THREE.ShaderChunk = {
 
 		"	#endif",
 
-		"	vec4 skinned = vec4( 0.0 )'",
+		"	vec4 skinned = vec4( 0.0 );",
 		"	skinned += boneMatX * skinVertex * skinWeight.x;",
 		"	skinned += boneMatY * skinVertex * skinWeight.y;",
 		"	skinned += boneMatZ * skinVertex * skinWeight.z;",
