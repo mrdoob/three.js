b6a788b3403c6f2f3c0eaae96a56f71cab2de2bc
Charlie <charlie.hoey@gmail.com>
2014-06-23 12:45:35 -0400

    added link to example

--- examples/js/shaders/TechnicolorShader.js
+++ examples/js/shaders/TechnicolorShader.js
@@ -3,8 +3,8 @@
  *
  * Technicolor Shader
  * Simulates the look of the two-strip technicolor process popular in early 20th century films.
- * More info here: http://www.widescreenmuseum.com/oldcolor/technicolor1.htm
- * 
+ * More historical info here: http://www.widescreenmuseum.com/oldcolor/technicolor1.htm
+ * Demo here: http://charliehoey.com/technicolor_shader/shader_test.html
  */
 
 THREE.TechnicolorShader = {
