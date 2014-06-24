699782e779692bb45ac0a00f2edef94a668ab69e
gero3 <jeroen-876@hotmail.com>
2014-06-24 16:49:00 +0200

    Throw a decent error on missing font.
    It throws an error when the font is missing.
    Ex: "The font helvetiker with normal weight and normal style is missing.
    
    Fixes #4923.

--- src/extras/FontUtils.js
+++ src/extras/FontUtils.js
@@ -32,8 +32,16 @@ THREE.FontUtils = {
 	divisions : 10,
 
 	getFace : function() {
-
-		return this.faces[ this.face ][ this.weight ][ this.style ];
+		
+		try {
+			
+			return this.faces[ this.face ][ this.weight ][ this.style ];
+			
+		} catch (e) {
+			
+			throw "The font " + this.face + " with " + this.weight + " weight and " + this.style + " style is missing."
+			 
+		};
 
 	},
 
