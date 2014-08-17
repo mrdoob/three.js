7be6e2a4e53302f0fdaf56642f7911667fa36a80
Mr.doob <info@mrdoob.com>
2014-08-16 22:35:38 -0700

    Editor: Fixed Script panel behaviour.

--- editor/js/Sidebar.Script.js
+++ editor/js/Sidebar.Script.js
@@ -4,12 +4,27 @@ Sidebar.Script = function ( editor ) {
 
 	var container = new UI.CollapsiblePanel();
 	container.setCollapsed( true );
+	container.setDisplay( 'none' );
 
 	container.addStatic( new UI.Text( 'Script' ).setTextTransform( 'uppercase' ) );
 	container.add( new UI.Break() );
 
 	var scriptsRow = new UI.Panel();
 	container.add( scriptsRow );
+
+	signals.objectSelected.add( function ( object ) {
+
+		if ( object !== null ) {
+
+			container.setDisplay( 'block' );
+
+		} else {
+
+			container.setDisplay( 'none' );
+
+		}
+
+	} );
 
 	return container;
 
