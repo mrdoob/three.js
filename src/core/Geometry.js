3daf0b234ca62915125737240f9c47145fda2e2d
Mr.doob <info@mrdoob.com>
2014-09-03 14:22:22 -0400

    Geometry.fromBufferGeometry() also cloning colors.

--- src/core/Geometry.js
+++ src/core/Geometry.js
@@ -137,7 +137,7 @@ THREE.Geometry.prototype = {
 		var addFace = function ( a, b, c ) {
 
 			var vertexNormals = normals !== undefined ? [ tempNormals[ a ].clone(), tempNormals[ b ].clone(), tempNormals[ c ].clone() ] : [];
-			var vertexColors = colors !== undefined ? [ scope.colors[ a ], scope.colors[ b ], scope.colors[ c ] ] : [];
+			var vertexColors = colors !== undefined ? [ scope.colors[ a ].clone(), scope.colors[ b ].clone(), scope.colors[ c ].clone() ] : [];
 
 			scope.faces.push( new THREE.Face3( a, b, c, vertexNormals, vertexColors ) );
 			scope.faceVertexUvs[ 0 ].push( [ tempUVs[ a ], tempUVs[ b ], tempUVs[ c ] ] );
