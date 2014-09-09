123d95a3db60ded47f801b6254c351a2efa48969
Mr.doob <info@mrdoob.com>
2014-09-08 19:08:50 -0700

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -14966,6 +14966,38 @@ THREE.DataTexture.prototype.clone = function () {
 
 };
 
+// File:src/textures/VideoTexture.js
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+THREE.VideoTexture = function ( video, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy ) {
+
+	THREE.Texture.call( this, video, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy );
+
+	this.generateMipmaps = false;
+
+	var scope = this;
+
+	var update = function () {
+
+		requestAnimationFrame( update );
+
+		if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+
+			scope.needsUpdate = true;
+
+		}
+
+	};
+
+	update();
+
+};
+
+THREE.VideoTexture.prototype = Object.create( THREE.Texture.prototype );
+
 // File:src/objects/Group.js
 
 /**
