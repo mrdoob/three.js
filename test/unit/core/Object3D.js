71e96a2ffeccdbbd6e2d10b450189232ac8ffb95
Daniel Taub <dan@cemi.org>
2014-09-10 19:34:11 -0400

    Adding custom mesh example, and changing parse to accept materials and geometries

--- test/unit/core/Object3D.js
+++ test/unit/core/Object3D.js
@@ -12,16 +12,41 @@ THREE.CustomObject = function (){
 
 THREE.CustomObject.prototype = Object.create( THREE.Object3D.prototype );
 
-THREE.CustomObject.parse = function (data) {
+THREE.CustomObject.parse = function (data, geometries, materials) {
 
-	return (new THREE.CustomObject());
+	return ( new THREE.CustomObject() );
 	// nothing special to initialize this one
 
 };
 
-THREE.CustomTypes = { 'CustomObject': THREE.CustomObject };
+THREE.CustomMesh = function ( geometry, material ){
+	THREE.Mesh.call( this, geometry, material );
+	this.type = "CustomMesh";
+};
 
-test( "custom type initializes", function() {
+THREE.CustomMesh.prototype = Object.create( THREE.Mesh.prototype );
+
+THREE.CustomMesh.parse = function (data, geometries, materials) {
+
+	var geometry = geometries[ data.geometry ];
+	var material = materials[ data.material ];
+
+	if ( ( geometry === undefined ) || ( material === undefined ) )
+		throw "missing geometry or material"
+
+	return ( new THREE.CustomMesh(data, geometry, material) );
+	// nothing special to initialize this one
+
+};
+
+
+
+THREE.CustomTypes = { 
+	'CustomObject': THREE.CustomObject,
+	'CustomMesh': THREE.CustomMesh
+ };
+
+test( "custom objects initialize", function() {
 
 	var object = new THREE.CustomObject();
 
@@ -32,19 +57,36 @@ test( "custom type initializes", function() {
 
 
 
-test( "custom type serializes", function() {
+test( "custom object serializes/deserializes", function() {
 
 	var object = new THREE.CustomObject();
-	window.o=object;
+
 	var strObj = object.toJSON();
 
 	var loader = new THREE.ObjectLoader();
 
 	var deserialized = loader.parse(strObj); 
 
-	window.d=deserialized;
-
 	ok( deserialized instanceof THREE.Object3D, "Passed!" );
 	ok( deserialized instanceof THREE.CustomObject, "Passed!" );
+	
+});
+
+
+test( "custom mesh serializes/deserializes", function() {
+
+	var geometry = new THREE.SphereGeometry();
+	var material = new THREE.MeshBasicMaterial();
+
+	var object = new THREE.CustomMesh(geometry, material);
+
+	var strObj = object.toJSON();
+
+	var loader = new THREE.ObjectLoader();
+
+	var deserialized = loader.parse(strObj); 
+
+	ok( deserialized instanceof THREE.Mesh, "Passed!" );
+	ok( deserialized instanceof THREE.CustomMesh, "Passed!" );
 	
 });
