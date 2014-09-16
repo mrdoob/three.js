9dee6be6e5939afde06319757905572abd3fb254
Mr.doob <info@mrdoob.com>
2014-09-16 17:25:59 -0400

    Removed WebGLRenderer3.

--- editor/js/Sidebar.Renderer.js
+++ editor/js/Sidebar.Renderer.js
@@ -5,7 +5,6 @@ Sidebar.Renderer = function ( editor ) {
 	var rendererTypes = {
 
 		'WebGLRenderer': THREE.WebGLRenderer,
-		'WebGLRenderer3': THREE.WebGLRenderer3,
 		'CanvasRenderer': THREE.CanvasRenderer,
 		'SVGRenderer': THREE.SVGRenderer,
 		'SoftwareRenderer': THREE.SoftwareRenderer,
