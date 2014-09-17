20bc6f2b57e086761194fec666735e048effbeb0
alexan0308 <alvild@mail.ru>
2014-09-18 00:38:29 +0600

    argument_mesh_to_replace_on_geometry

--- examples/js/modifiers/DentModifier.js
+++ examples/js/modifiers/DentModifier.js
@@ -23,28 +23,26 @@ THREE.DentModifier.prototype = {
         return this
     },
 
-    modify: function ( mesh ) {
+    modify: function ( geometry ) {
 
 		var R_Squared = this.radius * this.radius;
-        var M = new THREE.Matrix4().getInverse( mesh.matrixWorld );
-        var origin = this.origin.applyMatrix4( M ) ;
 
         var normal = new THREE.Vector3(); normal.copy( this.direction );
         normal.multiplyScalar( -this.radius*( 1 - this.depth ) ); 
 
-        var centerSphere = new THREE.Vector3(); centerSphere.addVectors( origin, normal );  
+        var centerSphere = new THREE.Vector3(); centerSphere.addVectors( this.origin, normal );  
         var Sphere = new THREE.Sphere( centerSphere, this.radius );
 
 		var NewVertexNormals = [];
 
-		for (var i=0; i< mesh.geometry.vertices.length; i++) {       
+		for (var i=0; i< geometry.vertices.length; i++) {       
 
 			NewVertexNormals[i] = null;
-            if ( centerSphere.distanceToSquared( mesh.geometry.vertices[i] ) < R_Squared ) {
+            if ( centerSphere.distanceToSquared( geometry.vertices[i] ) < R_Squared ) {
 
-                    var Ray = new THREE.Ray( mesh.geometry.vertices[i], this.direction );
+                    var Ray = new THREE.Ray( geometry.vertices[i], this.direction );
                     var punct = Ray.intersectSphere( Sphere );
-                    mesh.geometry.vertices[i] = punct;
+                    geometry.vertices[i] = punct;
 					var newNormal = new THREE.Vector3(); newNormal.subVectors( punct, centerSphere );
 					NewVertexNormals[i] = newNormal.normalize(); 
 
@@ -54,9 +52,9 @@ THREE.DentModifier.prototype = {
 		// compute Vertex Normals
 		var fvNames = [ 'a', 'b', 'c', 'd' ];
 
-		for ( var f = 0, fl = mesh.geometry.faces.length; f < fl; f ++ ) {
+		for ( var f = 0, fl = geometry.faces.length; f < fl; f ++ ) {
 
-			var face = mesh.geometry.faces[ f ];
+			var face = geometry.faces[ f ];
 			if ( face.vertexNormals === undefined ) {
 				continue;
 			}
@@ -70,9 +68,9 @@ THREE.DentModifier.prototype = {
 			}
 		// end compute Vertex Normals
 
-		mesh.geometry.computeFaceNormals();
-		mesh.geometry.verticesNeedUpdate = true;
-		mesh.geometry.normalsNeedUpdate = true; 
+		geometry.computeFaceNormals();
+		geometry.verticesNeedUpdate = true;
+		geometry.normalsNeedUpdate = true; 
         return this
 
     },
