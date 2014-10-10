598ac122d96dc2e342fa326621250248293dcdde
Mr.doob <info@mrdoob.com>
2014-10-10 02:36:46 +0200

    BufferGeometry: Added center placeholder.

--- src/core/BufferGeometry.js
+++ src/core/BufferGeometry.js
@@ -86,6 +86,12 @@ THREE.BufferGeometry.prototype = {
 
 	},
 
+	center: function () {
+
+		// TODO
+
+	},
+
 	fromGeometry: function ( geometry, settings ) {
 
 		settings = settings || { 'vertexColors': THREE.NoColors };
