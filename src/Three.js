ca84ec2b4e3078b9b07e831555b07c6ae91764e0
Mr.doob <info@mrdoob.com>
2014-11-12 11:45:17 +0000

    THREE: Moved Projector fallback code.

--- src/Three.js
+++ src/Three.js
@@ -181,6 +181,32 @@ THREE.RGBA_PVRTC_2BPPV1_Format = 2103;
 
 // DEPRECATED
 
+THREE.Projector = function () {
+
+	console.error( 'THREE.Projector has been moved to /examples/js/renderers/Projector.js.' );
+
+	this.projectVector = function ( vector, camera ) {
+
+		console.warn( 'THREE.Projector: .projectVector() is now vector.project().' );
+		vector.project( camera );
+
+	};
+
+	this.unprojectVector = function ( vector, camera ) {
+
+		console.warn( 'THREE.Projector: .unprojectVector() is now vector.unproject().' );
+		vector.unproject( camera );
+
+	};
+
+	this.pickingRay = function ( vector, camera ) {
+
+		console.error( 'THREE.Projector: .pickingRay() has been removed.' );
+
+	};
+
+};
+
 THREE.CanvasRenderer = function () {
 
 	console.error( 'THREE.CanvasRenderer has been moved to /examples/js/renderers/CanvasRenderer.js' );
