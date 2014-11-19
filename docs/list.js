39940c853b8d1242818e4e33c957db0797c3d4e7
Vincent Lark <vincent.lark@gmail.com>
2014-11-19 19:35:40 +0100

    footnotes for not bundled classes, an other way

--- docs/list.js
+++ docs/list.js
@@ -46,7 +46,7 @@ var list = {
 
 
 		"Loaders": [
-			[ "BabylonLoader", "api/loaders/BabylonLoader" ],
+			[ "BabylonLoader *", "api/loaders/BabylonLoader" ],
 			[ "BufferGeometryLoader", "api/loaders/BufferGeometryLoader" ],
 			[ "Cache", "api/loaders/Cache" ],
 			[ "ColladaLoader", "api/loaders/ColladaLoader" ],
@@ -264,10 +264,16 @@ for ( var section in list ) {
 		for ( var i = 0; i < list[ section ][ category ].length; i ++ ) {
 
 			var page = list[ section ][ category ][ i ];
-			pages[ section ][ category ][ page[ 0 ] ] = page[ 1 ];
+			pages[ section ][ category ][ cleanPageName( page[ 0 ] ) ] = page[ 1 ];
 
 		}
 
 	}
+
+}
+
+function cleanPageName( name ) {
+
+	return name.replace(/\ \*/g, '');
 
 }
