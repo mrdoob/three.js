6fb091a0707d1afaf5c9e1d8b4207664f8798ba6
Martin Naumann <martin@geekonaut.de>
2014-12-09 00:06:22 +0100

    Adjusting lightmap shaders for enhanced lightmaps

--- src/renderers/shaders/ShaderChunk/lightmap_pars_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/lightmap_pars_fragment.glsl
@@ -1,6 +1,18 @@
 #ifdef USE_LIGHTMAP
 
-	varying vec2 vUv2;
-	uniform sampler2D lightMap;
+   varying vec2 vUv2;
+   uniform sampler2D lightMap;
+
+   #ifdef USE_ENHANCED_LIGHTMAP
+	 	  //  <0, 2> the degree to which lightmap modifies the original texture color
+      uniform float lm_Intensity;
+
+	    // <0, 1> neutral point in the light map color range where the original texture color is unchanged
+      uniform float lm_Center;
+
+	    // <0, 1> quadratic rate of intensity decrease for extreme lightmap values
+      uniform float lm_Falloff;
+
+   #endif
 
 #endif
