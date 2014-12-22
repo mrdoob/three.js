0eabadc4e0d84f3ef706b7bb65f1af14a838ede1
walden- <walden-@users.noreply.github.com>
2014-12-22 05:25:59 +0100

    add fullscreen mode (from view menu)

---
+++ editor/js/Fullscreen.js
@@ -0,0 +1,11 @@
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
