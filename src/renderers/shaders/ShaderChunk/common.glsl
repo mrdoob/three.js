145c3895b00cd7cff0f1cda50f4fd64d124d48fa
Ben Houston <ben@exocortex.com>
2014-12-22 08:56:46 -0500

    Address @zz85's concern about using the constant 0.33333 insteaed of a division: https://github.com/mrdoob/three.js/pull/5785#discussion_r22146657

--- src/renderers/shaders/ShaderChunk/common.glsl
+++ src/renderers/shaders/ShaderChunk/common.glsl
@@ -14,7 +14,7 @@ vec3  saturate( in vec3 a )  { return clamp( a, 0.0, 1.0 ); }
 vec4  saturate( in vec4 a )  { return clamp( a, 0.0, 1.0 ); }
 float average( in float a ) { return a; }
 float average( in vec2 a )  { return ( a.x + a.y) * 0.5; }
-float average( in vec3 a )  { return ( a.x + a.y + a.z) * 0.3333333333; }
+float average( in vec3 a )  { return ( a.x + a.y + a.z) / 3.0; }
 float average( in vec4 a )  { return ( a.x + a.y + a.z + a.w) * 0.25; }
 float whiteCompliment( in float a ) { return saturate( 1.0 - a ); }
 vec2  whiteCompliment( in vec2 a )  { return saturate( vec2(1.0) - a ); }
