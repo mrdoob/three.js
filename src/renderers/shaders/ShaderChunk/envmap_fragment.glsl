0479681978b3f0e599e43285525b25100ca3783b
Ben Houston <ben@exocortex.com>
2014-12-30 22:01:21 -0500

    fix bug in ENVMAP_TYPE_SPHERE mapping.

--- src/renderers/shaders/ShaderChunk/envmap_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/envmap_fragment.glsl
@@ -39,7 +39,7 @@
 		vec4 envColor = texture2D( envMap, sampleUV );
 		
 	#elif defined( ENVMAP_TYPE_SPHERE )
-		vec3 reflectView = flipNormal * ( transformDirection( reflectVec, viewMatrix ) + vec3(0.0,0.0,1.0) );
+		vec3 reflectView = flipNormal * normalize((viewMatrix * vec4( reflectVec, 0.0 )).xyz + vec3(0.0,0.0,1.0));
 		vec4 envColor = texture2D( envMap, reflectView.xy * 0.5 + 0.5 );
 	#endif
 
