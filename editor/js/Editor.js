991f253e0bf765b340b130495d1c7e9cbbdc940f
walden- <walden-@users.noreply.github.com>
2015-01-01 23:51:56 +0100

    get scene name from URL paramter

--- editor/js/Editor.js
+++ editor/js/Editor.js
@@ -400,6 +400,23 @@ Editor.prototype = {
 
 		};
 
-	}
+	},
+
+        getUrlParamByName: function ( name ) {
+
+                name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+                var regexS = "[\\?&]"+name+"=([^&#]*)";
+                var regex = new RegExp( regexS );
+                var results = regex.exec( document.URL );
+
+                if ( results == null ) {
+                        return null;
+                }
+                else {
+                        return results[1];
+                }
+
+        }
+
 
 }
