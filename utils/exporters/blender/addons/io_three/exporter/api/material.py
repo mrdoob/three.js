acdbbeb576379723c4bbd08519584e8d110a5f9d
yomotsu <oyamada@pxgrid.com>
2015-01-19 05:05:46 +0900

    opacity value of the Blender exporter is wrong

--- utils/exporters/blender/addons/io_three/exporter/api/material.py
+++ utils/exporters/blender/addons/io_three/exporter/api/material.py
@@ -128,7 +128,7 @@ def normal_map(material):
 @_material
 def opacity(material):
     logger.debug('material.opacity(%s)', material)
-    return round(material.alpha - 1.0, 2);
+    return round(1.0 - material.alpha, 2);
 
 
 @_material
