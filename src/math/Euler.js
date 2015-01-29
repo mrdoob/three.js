ca5dfc040f7eb2d3aff669be24764b76a3556a27
Wandalen <wandalen.me@gmail.com>
2015-01-29 10:14:44 +0200

    Color/Euler: fix 'toArray' broken expectated behavior #5

--- src/math/Euler.js
+++ src/math/Euler.js
@@ -280,7 +280,7 @@ THREE.Euler.prototype = {
 
 	},
 
-	toArray: function ( array, offset, withoutOrder ) {
+	toArray: function ( array, offset ) {
 
 		if ( array === undefined ) array = [];
 		if ( offset === undefined ) offset = 0;
@@ -288,6 +288,7 @@ THREE.Euler.prototype = {
 		array[ offset ] = this._x;
 		array[ offset + 1 ] = this._y;
 		array[ offset + 2 ] = this._z;
+		array[ offset + 3 ] = this._order;
 
 		return array;
 	},
