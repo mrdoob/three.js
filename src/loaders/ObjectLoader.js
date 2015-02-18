6f9491163d129e526664550b9a6dc5513baa26af
Mr.doob <info@mrdoob.com>
2015-02-18 14:53:52 -0800

    ObjectLoader: Fixed empty images. See fcf54cd2ed3b9097126c3f6b4fc5764a07f5195e.

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -289,17 +289,13 @@ THREE.ObjectLoader.prototype = {
 			var loader = new THREE.ImageLoader( manager );
 			loader.setCrossOrigin( this.crossOrigin );
 
-			var loadImage = function ( data ) {
-
-				var url = scope.texturePath + data.url;
+			var loadImage = function ( url ) {
 
 				scope.manager.itemStart( url );
 
-				loader.load( url, function ( image ) {
+				return loader.load( url, function () {
 
 					scope.manager.itemEnd( url );
-
-					images[ data.uuid ] = image;
 
 				} );
 
@@ -307,7 +303,9 @@ THREE.ObjectLoader.prototype = {
 
 			for ( var i = 0, l = json.length; i < l; i ++ ) {
 
-				loadImage( json[ i ] );
+				var image = json[ i ];
+
+				images[ image.uuid ] = loadImage( scope.texturePath + image.url );
 
 			}
 
