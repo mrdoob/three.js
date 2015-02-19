469c2e39aab7c8c031fa577c63e6a207c8bfd54c
Mr.doob <info@mrdoob.com>
2015-02-19 14:18:44 -0800

    Moved ShadowMesh to examples/js/objects by now.

--- examples/js/objects/ShadowMesh.js
+++ examples/js/objects/ShadowMesh.js
@@ -1,28 +1,25 @@
-// File:src/objects/ShadowMesh.js
-
 /**
  * @author erichlof /  http://github.com/erichlof
+ *
+ * A shadow Mesh that follows a shadow-casting Mesh in the scene, but is confined to a single plane.
  */
 
-// a shadow Mesh that follows a shadow-casting Mesh in the scene, but is confined to a single plane.
-
 THREE.ShadowMesh = function ( mesh ) {
-	
+
 	var shadowMaterial = new THREE.MeshBasicMaterial( {
-		
+
 		color: 0x000000,
 		transparent: true,
 		opacity: 0.6,
 		depthWrite: false
-		
+
 	} );
-	
+
 	THREE.Mesh.call( this, mesh.geometry, shadowMaterial );
-	
+
 	this.meshMatrix = mesh.matrixWorld;
-	
+
 	this.frustumCulled = false;
-	
 	this.matrixAutoUpdate = false;
 
 };
@@ -32,11 +29,12 @@ THREE.ShadowMesh.prototype.constructor = THREE.ShadowMesh;
 
 THREE.ShadowMesh.prototype.update = function () {
 
-	// based on https://www.opengl.org/archives/resources/features/StencilTalk/tsld021.htm
 	var shadowMatrix = new THREE.Matrix4();
 
 	return function ( plane, lightPosition4D ) {
-		
+
+		// based on https://www.opengl.org/archives/resources/features/StencilTalk/tsld021.htm
+
 		var dot = plane.normal.x * lightPosition4D.x +
 			  plane.normal.y * lightPosition4D.y +
 			  plane.normal.z * lightPosition4D.z +
@@ -63,7 +61,7 @@ THREE.ShadowMesh.prototype.update = function () {
 		sme[ 7 ]  = - lightPosition4D.w * plane.normal.y;
 		sme[ 11 ] = - lightPosition4D.w * plane.normal.z;
 		sme[ 15 ] = dot - lightPosition4D.w * -plane.constant;
-		
+
 		this.matrix.multiplyMatrices( shadowMatrix, this.meshMatrix );
 
 	};
