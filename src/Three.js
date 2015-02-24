e5cc53358c619c8aa7c9c26de137236d00b1fbf0
humbletim <humbletim@gmail.com>
2015-02-24 10:53:11 -0500

    Fix 'Math.sign' polyfill to match ES6 specs

--- src/Three.js
+++ src/Three.js
@@ -16,9 +16,11 @@ if ( typeof module === 'object' ) {
 
 if ( Math.sign === undefined ) {
 
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign 
+
 	Math.sign = function ( x ) {
 
-		return ( x < 0 ) ? - 1 : ( x > 0 ) ? 1 : 0;
+		return ( x < 0 ) ? - 1 : ( x > 0 ) ? 1 : +x;
 
 	};
 
