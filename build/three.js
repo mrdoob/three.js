703420f775ca4f08b55bf36e09fa09ad5352b8e9
Mr.doob <info@mrdoob.com>
2015-03-08 17:42:59 +0100

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -11472,10 +11472,14 @@ THREE.Loader.prototype = {
 			mpars.blending = THREE[ m.blending ];
 
 		}
-
-		if ( m.transparent !== undefined || m.opacity < 1.0 ) {
+		
+		if ( m.transparent !== undefined ) {
 
 			mpars.transparent = m.transparent;
+
+		} else if ( m.opacity < 1.0 ) {
+
+			mpars.transparent = true;
 
 		}
 
@@ -11555,14 +11559,14 @@ THREE.Loader.prototype = {
 
 		// modifiers
 
-		if ( m.transparency ) {
+		if ( m.transparency !== undefined ) {
 
 			console.warn( 'THREE.Loader: transparency has been renamed to opacity' );
-			mpars.opacity = m.transparency;
+			m.opacity = m.transparency;
 
 		}
 
-		if ( m.opacity ) {
+		if ( m.opacity !== undefined ) {
 
 			mpars.opacity = m.opacity;
 
