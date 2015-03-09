6db71a67bd48144b4b0631967493d1ac0ed2e70d
Mr.doob <info@mrdoob.com>
2015-03-10 00:28:30 +0100

    ObjectLoader: Added PlaneBufferGeometry support. Fixes #6203.

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -85,8 +85,9 @@ THREE.ObjectLoader.prototype = {
 				switch ( data.type ) {
 
 					case 'PlaneGeometry':
+					case 'PlaneBufferGeometry':
 
-						geometry = new THREE.PlaneGeometry(
+						geometry = new THREE[ data.type ](
 							data.width,
 							data.height,
 							data.widthSegments,
