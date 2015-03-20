91cf2ae5fa9ecbf02e22d11360c1ec6415ff953e
Ben Houston <ben@exocortex.com>
2015-03-20 16:01:08 -0400

    initial implementation of a shader pre-processor - not tested but to show the idea.

---
+++ src/renderers/shaders/ShaderPreprocessor.js
@@ -0,0 +1,59 @@
+/**
+ * GLSL Preprocessor with Caching.
+ *
+ * @author bhouston / http://clara.io/
+ */
+
+
+THREE.ShaderPreprocessor._shaderCache = {};
+
+THREE.ShaderPreprocessor.preprocess( shaderCode, shaderName ) {
+
+	if( shaderName.indexOf( '.glsl') < 0 ) {
+		shaderName = shaderName + '.glsl';
+	}
+
+	if( ! shaderCode ) {
+		if( ! THREE.ShaderChunks[shaderName] ) {
+			throw new Error( 'no shader chunk named: ' + shaderName );
+		}
+		shaderCode = THREE.ShaderChunks[shaderName];
+	}
+
+	if( THREE.ShaderPreprocessor._shaderCache[ shaderName ] ) {
+		return THREE.ShaderPreprocessor._shaderCache[ shaderName ];
+	}
+
+	var inputLines = shaderCode.match(/[^\r\n]+/g);
+
+	var outputLines = [];
+
+	for( var i = 0; i < inputLines.length; i ++ ) {
+
+		var inputLine = inputLines[i];
+
+		var results = inputLine.match(^[\s]+#include[\s]+["<]([a-zA-Z0-9_\.]+)[">]/gmi);
+
+		console.log( inputLine, results ); // for debugging
+
+		if( results && results.length == 1 ) {
+
+			var includeName = results[0];
+
+			// recursively apply preprocessor. :)
+			var includeCode = THREE.ShaderPreprocessor.preprocess( null, includeName );
+
+			outputLines.concat( includeCode );
+		}
+		else {
+			outputLines.concat( inputLine );
+		}
+
+	}
+
+	var outputCode = outputLines.join();
+	THREE.ShaderPreprocessor._shaderCache[ shaderName ] = outputCode;
+
+	return outputCode;
+
+};
