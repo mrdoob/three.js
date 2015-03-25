6b55c3ecfc6be3e59cd62bba8a11cbfa5c47e238
Sébastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2015-03-25 17:09:36 +0100

    Do not generate image texture if image is not loaded
    this gets rid of 'WebGL: INVALID_VALUE: texImage2D: invalid image' error messages

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -5505,6 +5505,8 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 		var mipmap, mipmaps = texture.mipmaps;
 
+		var imageNotComplete = false;
+
 		if ( texture instanceof THREE.DataTexture ) {
 
 			// use manually created mipmaps if available
@@ -5573,11 +5575,21 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 			} else {
 
-				_gl.texImage2D( _gl.TEXTURE_2D, 0, glFormat, glFormat, glType, texture.image );
+				if (texture.image.complete) {
+
+					_gl.texImage2D( _gl.TEXTURE_2D, 0, glFormat, glFormat, glType, texture.image );
+
+				} else {
+
+					imageNotComplete = true;
+
+				}
 
 			}
 
 		}
+
+		if (imageNotComplete) return;
 
 		if ( texture.generateMipmaps && isImagePowerOfTwo ) _gl.generateMipmap( _gl.TEXTURE_2D );
 
