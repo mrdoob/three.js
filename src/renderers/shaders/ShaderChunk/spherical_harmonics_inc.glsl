8e578385892f26c69e22b987844070a2c389507f
Ben Houston <ben@exocortex.com>
2015-04-15 12:50:20 -0400

    simplify shader code.

--- src/renderers/shaders/ShaderChunk/spherical_harmonics_inc.glsl
+++ src/renderers/shaders/ShaderChunk/spherical_harmonics_inc.glsl
@@ -1,28 +1,22 @@
 // normal is assumed to be unit length!
 vec3 shGetRadianceAt( vec3 shCoefficients[9], vec3 normal )
 {
-
-    float x = normal.x, y = normal.y, z = normal.z;
-
-    vec3 result = vec3( 0 );
-
     // band 0
-    result += shCoefficients[0];
+    vec3 result = shCoefficients[0];
 
     // band 1
-    result += shCoefficients[1] * y;
-    result += shCoefficients[2] * z;
-    result += shCoefficients[3] * x;
+    result += shCoefficients[1] * normal.y;
+    result += shCoefficients[2] * normal.z;
+    result += shCoefficients[3] * normal.x;
 
     // band 2
-    result += shCoefficients[4] * ( x*y );
-    result += shCoefficients[5] * ( y*z );
-    result += shCoefficients[6] * ( 3.0 * z*z - 1.0 );
-    result += shCoefficients[7] * ( x*z );
-    result += shCoefficients[8] * ( x*x - y*y );
+    result += shCoefficients[4] * ( normal.x*normal.y );
+    result += shCoefficients[5] * ( normal.y*normal.z );
+    result += shCoefficients[6] * ( 3.0 * normal.z*normal.z - 1.0 );
+    result += shCoefficients[7] * ( normal.x*normal.z );
+    result += shCoefficients[8] * ( normal.x*normal.x - normal.y*normal.y );
 
     return result;
-
 }
 
 // constants to convert from radiance to hemispheric irradiance
@@ -36,26 +30,20 @@ vec3 shGetRadianceAt( vec3 shCoefficients[9], vec3 normal )
 // normal is assumed to be unit length!
 vec3 shGetIrradianceAt( vec3 shCoefficients[9], vec3 normal )
 {
-
-    float x = normal.x, y = normal.y, z = normal.z;
-
-    vec3 result = vec3( 0 );
-
     // band 0
-    result += shCoefficients[0] * C4;
+    vec3 result = shCoefficients[0] * C4;
 
     // band 1
-    result += shCoefficients[1] * ( 2.0 * C2 * y );
-    result += shCoefficients[2] * ( 2.0 * C2 * z );
-    result += shCoefficients[3] * ( 2.0 * C2 * x );
+    result += shCoefficients[1] * ( 2.0 * C2 * normal.y );
+    result += shCoefficients[2] * ( 2.0 * C2 * normal.z );
+    result += shCoefficients[3] * ( 2.0 * C2 * normal.x );
 
     // band 2
-    result += shCoefficients[4] * ( 2.0 * C1 * x*y );
-    result += shCoefficients[5] * ( 2.0 * C1 * y*z );
-    result += shCoefficients[6] * ( C3 * z*z - C5 );
-    result += shCoefficients[7] * ( 2.0 * C1 * x*z );
-    result += shCoefficients[8] * ( C1 * ( x*x - y*y ) );
+    result += shCoefficients[4] * ( 2.0 * C1 * normal.x*normal.y );
+    result += shCoefficients[5] * ( 2.0 * C1 * normal.y*normal.z );
+    result += shCoefficients[6] * ( C3 * normal.z*normal.z - C5 );
+    result += shCoefficients[7] * ( 2.0 * C1 * normal.x*normal.z );
+    result += shCoefficients[8] * ( C1 * ( normal.x*normal.x - normal.y*normal.y ) );
 
     return result;
-
 }
