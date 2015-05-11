d9891c047d3fb86995dacd14534b97deb4b98ffa
Mr.doob <info@mrdoob.com>
2015-05-11 14:50:38 -0400

    MeshFaceMaterial: More robust check.

--- src/materials/MeshFaceMaterial.js
+++ src/materials/MeshFaceMaterial.js
@@ -6,7 +6,7 @@ THREE.MeshFaceMaterial = function ( materials ) {
 
 	console.error( 'THREE.MeshFaceMaterial has been removed.' );
 
-	var material = materials !== undefined ? materials[ 0 ] : new THREE.MeshBasicMaterial();
+	var material = Array.isArray( materials ) ? materials[ 0 ] : new THREE.MeshBasicMaterial();
 	material.materials = []; // temporal workaround
 
 	return material;
