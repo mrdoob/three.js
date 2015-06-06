7d1e856b6559e35368f02fd1c4bbb8b2013419ab
Mr.doob <info@mrdoob.com>
2015-06-06 14:53:28 +0100

    Clean up.

--- utils/servers/python_server.sh
+++ utils/servers/python_server.sh
@@ -3,11 +3,9 @@
 cd `dirname $0`/../../
 
 ret=`python -c 'import sys; print("%i" % (sys.version_info[0]))'`
-if [ $ret -eq 2 ]; then 
-	# Python 2
-    python -m SimpleHTTPServer
-else 
-	# Python 3
-    python -m http.server
-fi
 
+if [ $ret -eq 2 ]; then
+	python -m SimpleHTTPServer # Python 2
+else
+	python -m http.server # Python 3
+fi
