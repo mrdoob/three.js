5cc93b76e6861bb8f77ec8610b199849d40419d4
dubejf <dubejf@gmail.com>
2015-07-01 20:25:12 -0400

    Revert "Raycaster: Option to skip invisible objects"
    This reverts commit 0504ededcb924a70253bc1d8f293f1966f8c91e9.

--- src/core/Raycaster.js
+++ src/core/Raycaster.js
@@ -13,7 +13,6 @@
 
 		this.near = near || 0;
 		this.far = far || Infinity;
-		this.visibleOnly = false;
 
 		this.params = {
 			Sprite: {},
@@ -33,8 +32,6 @@
 
 	var intersectObject = function ( object, raycaster, intersects, recursive ) {
 
-		if ( raycaster.visibleOnly === true && object.visible === false ) return;
-	
 		object.raycast( raycaster, intersects );
 
 		if ( recursive === true ) {
