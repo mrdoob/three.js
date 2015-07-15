efc7554e7b1b62a708ea201b8699f8bc0ae24a81
Mr.doob <info@mrdoob.com>
2015-07-14 21:13:00 -0400

    WebGLObjects: Removed unneeded function.

--- src/renderers/webgl/WebGLObjects.js
+++ src/renderers/webgl/WebGLObjects.js
@@ -42,24 +42,9 @@ THREE.WebGLObjects = function ( gl, properties, info ) {
 
 	}
 
-	function removeInstances( objlist, object ) {
-
-		for ( var o = objlist.length - 1; o >= 0; o -- ) {
-
-			if ( objlist[ o ].object === object ) {
-
-				objlist.splice( o, 1 );
-
-			}
-
-		}
-
-	}
-
 	//
 
 	this.objects = objects;
-
 	this.geometries = geometries;
 
 	this.init = function ( object ) {
