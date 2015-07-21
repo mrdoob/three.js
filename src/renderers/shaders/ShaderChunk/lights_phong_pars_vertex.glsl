cc37640c37447c8cae0e7946e3339a5d7dfe0cd3
nn4e <support@prowebmake.ru>
2015-07-21 16:12:51 +0300

    internal shader modification and custom uniforms

--- src/renderers/shaders/ShaderChunk/lights_phong_pars_vertex.glsl
+++ src/renderers/shaders/ShaderChunk/lights_phong_pars_vertex.glsl
@@ -1,4 +1,4 @@
-#if MAX_SPOT_LIGHTS > 0 || defined( USE_BUMPMAP ) || defined( USE_ENVMAP )
+#if MAX_SPOT_LIGHTS > 0 || defined( USE_BUMPMAP ) || defined( USE_ENVMAP ) || defined( USE_WORLD_POS )
 
 	varying vec3 vWorldPosition;
 
