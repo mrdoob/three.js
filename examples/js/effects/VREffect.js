406ca50ebf64553a61a28c5ccfe5ec5260500249
Tee Jia Hen <wizztjh@gmail.com>
2015-07-30 11:23:27 +0800

    Fix VR Effect by using the new webVR spec

--- examples/js/effects/VREffect.js
+++ examples/js/effects/VREffect.js
@@ -47,10 +47,11 @@ THREE.VREffect = function ( renderer, done ) {
 				if ( devices[i] instanceof HMDVRDevice ) {
 					vrHMD = devices[i];
 					self._vrHMD = vrHMD;
-					self.leftEyeTranslation = vrHMD.getEyeTranslation( "left" );
-					self.rightEyeTranslation = vrHMD.getEyeTranslation( "right" );
-					self.leftEyeFOV = vrHMD.getRecommendedEyeFieldOfView( "left" );
-					self.rightEyeFOV = vrHMD.getRecommendedEyeFieldOfView( "right" );
+					self.leftEyeTranslation = vrHMD.getEyeParameters("left").eyeTranslation;
+					self.rightEyeTranslation = vrHMD.getEyeParameters("right").eyeTranslation;
+					self.leftEyeFOV = vrHMD.getEyeParameters("left").recommendedFieldOfView;
+					self.rightEyeFOV = vrHMD.getEyeParameters("right").recommendedFieldOfView;
+
 					break; // We keep the first we encounter
 				}
 			}
