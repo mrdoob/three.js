acced26a144888498c79f9ea6453c437dab3d565
Evgeny Bovykin <missingdays@etersoft.ru>
2015-07-31 14:26:12 +0300

    Fix typo

--- src/Three.js
+++ src/Three.js
@@ -9,6 +9,7 @@ if (typeof define === 'function' && define.amd) {
     define("three", THREE);
 } else if ('undefined' !== typeof exports && 'undefined' !== typeof module) {
     module.exports = THREE;
+}
  
 
 // polyfills
