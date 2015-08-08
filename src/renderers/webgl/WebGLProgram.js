cbfa37db3652e496974b171d337b42cb85a15324
Mr.doob <info@mrdoob.com>
2015-08-07 19:21:08 -0700

    WebGLProgram: Fixed gsls warning.

--- src/renderers/webgl/WebGLProgram.js
+++ src/renderers/webgl/WebGLProgram.js
@@ -281,6 +281,7 @@ THREE.WebGLProgram = ( function () {
 			prefixFragment = [
 
 				( parameters.bumpMap || parameters.normalMap || parameters.flatShading || material.derivatives ) ? '#extension GL_OES_standard_derivatives : enable' : '',
+				parameters.logarithmicDepthBuffer && renderer.extensions.get( 'EXT_frag_depth' ) ? '#extension GL_EXT_frag_depth : enable' : '',
 
 				'precision ' + parameters.precision + ' float;',
 				'precision ' + parameters.precision + ' int;',
