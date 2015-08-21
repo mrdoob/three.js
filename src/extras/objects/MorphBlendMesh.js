f2491f3dc009a74a9ba5cc0c4e4995e7bdbd7306
Mr.doob <info@mrdoob.com>
2015-08-21 00:10:20 -0700

    Clean up.

--- src/extras/objects/MorphBlendMesh.js
+++ src/extras/objects/MorphBlendMesh.js
@@ -299,11 +299,15 @@ THREE.MorphBlendMesh.prototype.update = function ( delta ) {
 
 		if ( animation.directionBackwards ) mix = 1 - mix;
 
-		if (animation.currentFrame !== animation.lastFrame) {
-			this.morphTargetInfluences[animation.currentFrame] = mix * weight;
-			this.morphTargetInfluences[animation.lastFrame] = ( 1 - mix ) * weight;
+		if ( animation.currentFrame !== animation.lastFrame ) {
+
+			this.morphTargetInfluences[ animation.currentFrame ] = mix * weight;
+			this.morphTargetInfluences[ animation.lastFrame ] = ( 1 - mix ) * weight;
+
 		} else {
-			this.morphTargetInfluences[animation.currentFrame] = weight;
+
+			this.morphTargetInfluences[ animation.currentFrame ] = weight;
+
 		}
 
 	}
