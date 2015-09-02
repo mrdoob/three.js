63f58304c49a9a641af6c9f81458bb05647dcae1
Mr.doob <info@mrdoob.com>
2015-09-02 21:00:18 +0200

    Editor: Move Convert to BufferGeometry to Actions.

--- editor/js/Sidebar.Geometry.Modifiers.js
+++ editor/js/Sidebar.Geometry.Modifiers.js
@@ -31,26 +31,6 @@ Sidebar.Geometry.Modifiers = function ( signals, object ) {
 
 	container.add( button );
 
-	// Convert to Geometry/BufferGeometry
-
-	var isBufferGeometry = geometry instanceof THREE.BufferGeometry;
-
-	if ( geometry instanceof THREE.Geometry ) {
-
-		var button = new UI.Button( 'Convert to BufferGeometry' );
-		button.onClick( function () {
-
-			if ( confirm( 'Are you sure?' ) === false ) return;
-
-			object.geometry = new THREE.BufferGeometry().fromGeometry( object.geometry );
-
-			signals.geometryChanged.dispatch( object );
-
-		} );
-		container.add( button );
-
-	}
-
 	//
 
 	return container;
