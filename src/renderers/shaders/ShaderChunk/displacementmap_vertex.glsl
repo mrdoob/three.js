ebc3f773618721e61441166246e954fdd72ea8bc
Mr.doob <info@mrdoob.com>
2015-09-05 06:26:31 +0200

    Fixed displacementmap_vertex. I think...

--- src/renderers/shaders/ShaderChunk/displacementmap_vertex.glsl
+++ src/renderers/shaders/ShaderChunk/displacementmap_vertex.glsl
@@ -1,5 +1,5 @@
 #ifdef USE_DISPLACEMENTMAP
 
-	transformed += objectNormal * texture2D( displacementMap, uv ).x * displacementScale;
+	transformed += normal * texture2D( displacementMap, uv ).x * displacementScale;
 
 #endif
