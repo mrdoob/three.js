aa7aac30757a13c075e557cfa28ae72e7b6c3f7c
Simon Thiele <simon.thiele@codecentric.de>
2015-09-07 15:00:44 +0200

    use tab instead of whitespace

--- test/unit/cameras/Camera.js
+++ test/unit/cameras/Camera.js
@@ -5,8 +5,8 @@
 module( "Camera" );
 
 test( "lookAt", function() {
-  var obj = new THREE.Camera();
-  obj.lookAt(new THREE.Vector3(0, 1, -1));
+	var obj = new THREE.Camera();
+	obj.lookAt(new THREE.Vector3(0, 1, -1));
 
 	ok( obj.rotation.x * (180 / Math.PI) === 45 , "x is equal" );
 });
