ef4da53e52ce6c207cc8dc50364467ae79b4e2c3
Mr.doob <info@mrdoob.com>
2015-09-23 15:45:16 -0400

    Minor clean up.

--- src/core/Geometry.js
+++ src/core/Geometry.js
@@ -257,7 +257,7 @@ THREE.Geometry.prototype = {
 
 		}
 
-		var addFace = function ( a, b, c ) {
+		function addFace( a, b, c ) {
 
 			var vertexNormals = normals !== undefined ? [ tempNormals[ a ].clone(), tempNormals[ b ].clone(), tempNormals[ c ].clone() ] : [];
 			var vertexColors = colors !== undefined ? [ scope.colors[ a ].clone(), scope.colors[ b ].clone(), scope.colors[ c ].clone() ] : [];
