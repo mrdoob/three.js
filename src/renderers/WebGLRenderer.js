27f7880fd8aaf18c921411fdb1dd444b342bc9ac
Mr.doob <info@mrdoob.com>
2015-10-26 11:29:11 -0400

    WebGLRenderer: Apply pixelRatio to Points size. See #7451.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1869,7 +1869,7 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 		uniforms.psColor.value = material.color;
 		uniforms.opacity.value = material.opacity;
-		uniforms.size.value = material.size;
+		uniforms.size.value = material.size * pixelRatio;
 		uniforms.scale.value = _canvas.height / 2.0; // TODO: Cache this.
 
 		uniforms.map.value = material.map;
