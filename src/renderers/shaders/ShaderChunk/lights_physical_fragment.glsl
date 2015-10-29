d489707f4b98b4bd50e037dd742e06abc6100315
Ben Houston <ben@exocortex.com>
2015-10-29 11:21:13 -0400

    dielectric Fresnel F0 (specularity) controlled via monochromatic reflectivity in both envMap and in direct lighting.

--- src/renderers/shaders/ShaderChunk/lights_physical_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/lights_physical_fragment.glsl
@@ -1,4 +1,4 @@
 PhysicalMaterial material;
 material.diffuseColor = diffuseColor.rgb * ( 1.0 - metalnessFactor );
 material.specularRoughness = roughnessFactor * 0.5 + 0.5; // disney's remapping of [ 0, 1 ] roughness to [ 0.5, 1 ]
-material.specularColor = mix( vec3( 0.04 ), diffuseColor.rgb, metalnessFactor );
+material.specularColor = mix( vec3( 0.04 ) * reflectivity, diffuseColor.rgb, metalnessFactor );
