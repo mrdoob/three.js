daa33c972876ea6ac427996b74660abeeb60147c
Ben Houston <ben@exocortex.com>
2015-10-29 10:16:37 -0400

    undo reflectivity integration into specular (a model change.)

--- src/renderers/shaders/ShaderChunk/lights_physical_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/lights_physical_fragment.glsl
@@ -1,5 +1,5 @@
 PhysicalMaterial material;
 material.diffuseColor = diffuseColor.rgb * ( 1.0 - metalnessFactor );
 material.specularRoughness = roughnessFactor * 0.5 + 0.5; // disney's remapping of [ 0, 1 ] roughness to [ 0.5, 1 ]
-material.specularColor = mix( vec3( 0.04 ) * reflectivity, diffuseColor.rgb, metalnessFactor );
+material.specularColor = mix( vec3( 0.04 ), diffuseColor.rgb, metalnessFactor );
 material.grazingColor = mix( vec3( 1.0 ), diffuseColor.rgb, metalnessFactor );
