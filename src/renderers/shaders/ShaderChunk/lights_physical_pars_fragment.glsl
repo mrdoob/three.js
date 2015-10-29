b2d94fc015431a5054be9ab1e6ecb226d49082b3
Ben Houston <ben@exocortex.com>
2015-10-29 09:35:02 -0400

    adopt unified ReflectedLight structure.

--- src/renderers/shaders/ShaderChunk/lights_physical_pars_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/lights_physical_pars_fragment.glsl
@@ -11,8 +11,8 @@ void PhysicalMaterial_RE_DirectLight( const in IncidentLight directLight, const 
 
 	float dotNL = saturate( dot( geometry.normal, directLight.direction ) );
 
-	directReflectedLight.diffuse += dotNL * directLight.color * BRDF_Diffuse_Lambert( material.diffuseColor );
-	directReflectedLight.specular += dotNL * directLight.color * BRDF_Specular_GGX( directLight, geometry, material.specularColor, material.grazingColor, material.specularRoughness );
+	reflectedLight.directDiffuse += dotNL * directLight.color * BRDF_Diffuse_Lambert( material.diffuseColor );
+	reflectedLight.directSpecular += dotNL * directLight.color * BRDF_Specular_GGX( directLight, geometry, material.specularColor, material.grazingColor, material.specularRoughness );
 	
 }
 #define Material_RE_DirectLight    PhysicalMaterial_RE_DirectLight
@@ -29,7 +29,7 @@ void PhysicalMaterial_RE_DiffuseIndirectLight( const in vec3 indirectDiffuseColo
 
 void PhysicalMaterial_RE_SpecularIndirectLight( const in vec3 indirectSpecularColor, const in GeometricContext geometry, const in PhysicalMaterial material, inout ReflectedLight reflectedLight ) {
 
-    indirectReflectedLight.specular += indirectSpecularColor * BRDF_Specular_GGX_Environment( geometry, material.specularColor, material.grazingColor, material.specularRoughness );
+    reflectedLight.indirectSpecular += indirectSpecularColor * BRDF_Specular_GGX_Environment( geometry, material.specularColor, material.grazingColor, material.specularRoughness );
 
 }
 
