b648f0d261f1c75d76d66423d037f1213708bcd0
Mr.doob <info@mrdoob.com>
2015-10-31 23:02:06 -0400

    XHRLoader: Clean up.

--- src/loaders/XHRLoader.js
+++ src/loaders/XHRLoader.js
@@ -46,10 +46,14 @@ THREE.XHRLoader.prototype = {
 			THREE.Cache.add( url, response );
 
 			if ( this.status == 200 && this.readyState == 4 ) {
-				if ( onLoad ) onLoad( response );	
+
+				if ( onLoad ) onLoad( response );
+
 			} else {
+
 				if ( onError ) onError( event );
-			};
+
+			}
 
 			scope.manager.itemEnd( url );
 
