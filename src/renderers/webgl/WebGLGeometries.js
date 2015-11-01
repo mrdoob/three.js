a8fb86c9f4c4ab14858648f56797f13f8dec9d6d
Pontus Leitzler <leitzler@users.noreply.github.com>
2015-11-01 22:08:18 +0100

    Added deletion of geometry properties.
    Geometry were not deleted from properties on dispose, causing memory leak.

--- src/renderers/webgl/WebGLGeometries.js
+++ src/renderers/webgl/WebGLGeometries.js
@@ -64,6 +64,8 @@ THREE.WebGLGeometries = function ( gl, properties, info ) {
 		var property = properties.get( geometry );
 		if ( property.wireframe ) deleteAttribute( property.wireframe );
 
+		properties.delete( geometry );
+
 		info.memory.geometries --;
 
 	}
