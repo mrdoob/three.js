58e96bd759e7c5f8b6199fbaa0b6cd7de393e557
Mr.doob <info@mrdoob.com>
2015-11-02 18:30:45 -0500

    convert_obj_three: clean up

--- utils/converters/obj/convert_obj_three.py
+++ utils/converters/obj/convert_obj_three.py
@@ -15,7 +15,6 @@ Notes:
         -a center|centerxz|top|bottom|none model alignment
         -s smooth|flat			smooth = export vertex normals, flat = no normals (face normals computed in loader)
         -t ascii|binary			export ascii or binary format (ascii has more features, binary just supports vertices, faces, normals, uvs and materials)
-        -d invert|normal		invert transparency
         -b						bake material colors into face colors
         -x 10.0                 scale and truncate
         -f 2                    morph frame sampling step
@@ -23,7 +22,6 @@ Notes:
     - by default:
         use smooth shading (if there were vertex normals in the original model)
         will be in ASCII format
-        original model is assumed to use non-inverted transparency / dissolve (0.0 fully transparent, 1.0 fully opaque)
         no face colors baking
         no scale and truncate
         morph frame step = 1 (all files will be processed)
@@ -1539,7 +1537,7 @@ if __name__ == "__main__":
 
     # get parameters from the command line
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hbi:m:c:b:o:a:s:t:d:x:f:", ["help", "bakecolors", "input=", "morphs=", "colors=", "output=", "align=", "shading=", "type=", "dissolve=", "truncatescale=", "framestep="])
+        opts, args = getopt.getopt(sys.argv[1:], "hbi:m:c:b:o:a:s:t:d:x:f:", ["help", "bakecolors", "input=", "morphs=", "colors=", "output=", "align=", "shading=", "type=", "truncatescale=", "framestep="])
 
     except getopt.GetoptError:
         usage()
