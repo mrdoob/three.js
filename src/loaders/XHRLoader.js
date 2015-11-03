mrdoob/three.js

commit 302ac826216449dbb38879dcfdec75b3c4934525

Update XHRLoader.js

src/loaders/XHRLoader.js
@@ -45,7 +45,11 @@ THREE.XHRLoader.prototype = {
 
 			THREE.Cache.add( url, response );
 
-			if ( onLoad ) onLoad( response );
+			if ( this.status == 200 && this.readyState == 4 ) {
+				if ( onLoad ) onLoad( response );	
+			} else {
+				if ( onError ) onError( event );
+			};
 
 			scope.manager.itemEnd( url );
 

commit 4d69eb25f7943689ebc1af229510f30e21195dc2

Check for status 0 as well as status 200
When opening a file locally (via `file://`) the status code seems to report as `0`. Checking if the status code is `0` fixes the issue. This bug was introduced in 302ac826216449dbb38879dcfdec75b3c4934525

Also, substitute `==` with `===`

src/loaders/XHRLoader.js
@@ -45,7 +45,7 @@ THREE.XHRLoader.prototype = {
 
 			THREE.Cache.add( url, response );
 
-			if ( this.status == 200 && this.readyState == 4 ) {
+			if ( ( this.status === 0 || this.status === 200 )  && this.readyState === 4 ) {
 
 				if ( onLoad ) onLoad( response );
 