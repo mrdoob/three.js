04f2c86a8db1c50fbd6a3aea9724eff9ce753bac
Mr.doob <info@mrdoob.com>
2015-11-13 15:17:32 -0500

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -16134,7 +16134,7 @@ THREE.Audio = function ( listener ) {
 	this.source.onended = this.onEnded.bind( this );
 
 	this.gain = this.context.createGain();
-	this.gain.connect( listener.getOutputNode() );
+	this.gain.connect( listener.getInput() );
 
 	this.autoplay = false;
 
@@ -16143,6 +16143,7 @@ THREE.Audio = function ( listener ) {
 	this.isPlaying = false;
 	this.hasPlaybackControl = true;
 	this.sourceType = 'empty';
+
 	this.filter = null;
 
 };
@@ -16282,7 +16283,15 @@ THREE.Audio.prototype.disconnect = function () {
 
 };
 
+THREE.Audio.prototype.getFilter = function () {
+
+	return this.filter;
+
+};
+
 THREE.Audio.prototype.setFilter = function ( value ) {
+
+	if ( value === undefined ) value = null;
 
 	if ( this.isPlaying === true ) {
 
@@ -16295,12 +16304,6 @@ THREE.Audio.prototype.setFilter = function ( value ) {
 		this.filter = value;
 
 	}
-
-};
-
-THREE.Audio.prototype.getFilter = function () {
-
-	return this.filter;
 
 };
 
@@ -16536,8 +16539,10 @@ THREE.AudioListener = function () {
 	this.type = 'AudioListener';
 
 	this.context = new ( window.AudioContext || window.webkitAudioContext )();
-	this.masterGain =  this.context.createGain();
-	this.masterGain.connect(this.context.destination);
+
+	this.gain = this.context.createGain();
+	this.gain.connect( this.context.destination );
+
 	this.filter = null;
 
 };
@@ -16545,9 +16550,9 @@ THREE.AudioListener = function () {
 THREE.AudioListener.prototype = Object.create( THREE.Object3D.prototype );
 THREE.AudioListener.prototype.constructor = THREE.AudioListener;
 
-THREE.AudioListener.prototype.getOutputNode = function () {
+THREE.AudioListener.prototype.getInput = function () {
 
-	return this.masterGain;
+	return this.gain;
 
 };
 
@@ -16555,9 +16560,9 @@ THREE.AudioListener.prototype.removeFilter = function ( ) {
 
 	if ( this.filter !== null ) {
 
-		this.masterGain.disconnect( this.filter );
+		this.gain.disconnect( this.filter );
 		this.filter.disconnect( this.context.destination );
-		this.masterGain.connect( this.context.destination );
+		this.gain.connect( this.context.destination );
 		this.filter = null;
 
 	}
@@ -16568,17 +16573,17 @@ THREE.AudioListener.prototype.setFilter = function ( value ) {
 
 	if ( this.filter !== null ) {
 
-		this.masterGain.disconnect( this.filter );
+		this.gain.disconnect( this.filter );
 		this.filter.disconnect( this.context.destination );
 
 	} else {
 
-		this.masterGain.disconnect( this.context.destination );
+		this.gain.disconnect( this.context.destination );
 
 	}
 
 	this.filter = value;
-	this.masterGain.connect( this.filter );
+	this.gain.connect( this.filter );
 	this.filter.connect( this.context.destination );
 
 };
@@ -16591,13 +16596,13 @@ THREE.AudioListener.prototype.getFilter = function () {
 
 THREE.AudioListener.prototype.setMasterVolume = function ( value ) {
 
-	this.masterGain.gain.value = value;
+	this.gain.gain.value = value;
 
 };
 
 THREE.AudioListener.prototype.getMasterVolume = function () {
 
-	return this.masterGain.gain.value;
+	return this.gain.gain.value;
 
 };
 
