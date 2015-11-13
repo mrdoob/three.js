3131be124cc185054b2df4e3dd0728a1344d4095
Mr.doob <info@mrdoob.com>
2015-11-13 18:47:47 -0500

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -16377,6 +16377,36 @@ THREE.Audio.prototype.getVolume = function () {
 
 };
 
+// File:src/audio/AudioAnalyser.js
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+THREE.AudioAnalyser = function ( audio, fftSize ) {
+
+	this.analyser = audio.context.createAnalyser();
+	this.analyser.fftSize = fftSize !== undefined ? fftSize : 2048;
+
+	this.data = new Uint8Array( this.analyser.frequencyBinCount );
+
+	audio.getOutput().connect( this.analyser );
+
+};
+
+THREE.AudioAnalyser.prototype = {
+
+	constructor: THREE.AudioAnalyser,
+
+	getData: function () {
+
+		this.analyser.getByteFrequencyData( this.data );
+		return this.data;
+
+	}
+
+};
+
 // File:src/audio/AudioBuffer.js
 
 /**
