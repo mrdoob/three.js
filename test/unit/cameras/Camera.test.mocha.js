d3df5c21609ca9eeb408c66f66107e0eaf206b1a
simonThiele <simon.thiele@instana.com>
2015-12-07 09:59:18 +0100

    use mocha and chai test infrastructure for simple camera test

--- test/unit/cameras/Camera.test.mocha.js
+++ test/unit/cameras/Camera.test.mocha.js
@@ -2,8 +2,10 @@
  * @author simonThiele / https://github.com/simonThiele
  */
 
-var chai = require('chai');
-var expect = chai.expect;
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+self = global;
 var THREE = require('../../../build/three');
 
 
@@ -14,34 +16,27 @@ describe('Camera', function() {
 		it('should calculate the correct x rotation value', function () {
 
 		  var cam = new THREE.Camera();
-			cam.lookAt(new THREE.Vector3(0, 1, -1));
+			cam.lookAt( new THREE.Vector3( 0, 1, -1 ) );
 
 			expect( cam.rotation.x * (180 / Math.PI) ).to.equal( 45 );
+
+    });
+
+		it('should clone', function () {
+
+				var cam = new THREE.Camera();
+
+				// fill the matrices with any nonsense values just to see if they get copied
+				cam.matrixWorldInverse.set( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 );
+				cam.projectionMatrix.set( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 );
+
+				var clonedCam = cam.clone();
+
+				assert.equal(cam.matrixWorldInverse.equals( clonedCam.matrixWorldInverse ), true, "matrixWorldInverse is equal");
+				assert.equal(cam.projectionMatrix.equals(clonedCam.projectionMatrix), true, "projectionMatrix is equal");
 
     });
 
   });
 
 });
-
-// module( "Camera" );
-//
-// test( "lookAt", function() {
-// 	var cam = new THREE.Camera();
-// 	cam.lookAt(new THREE.Vector3(0, 1, -1));
-//
-// 	ok( cam.rotation.x * (180 / Math.PI) === 45 , "x is equal" );
-// });
-//
-// test( "clone", function() {
-// 	var cam = new THREE.Camera();
-//
-// 	// fill the matrices with any nonsense values just to see if they get copied
-// 	cam.matrixWorldInverse.set( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 );
-// 	cam.projectionMatrix.set( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 );
-//
-// 	var clonedCam = cam.clone();
-//
-// 	ok( cam.matrixWorldInverse.equals(clonedCam.matrixWorldInverse) , "matrixWorldInverse is equal" );
-// 	ok( cam.projectionMatrix.equals(clonedCam.projectionMatrix) , "projectionMatrix is equal" );
-// });
