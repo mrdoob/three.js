f16ce1221330746d1a21d096aa5c2ef243289dce
Mr.doob <info@mrdoob.com>
2015-12-07 18:38:28 -0500

    Editor: Changed default camera near.

--- editor/js/Editor.js
+++ editor/js/Editor.js
@@ -6,7 +6,7 @@ var Editor = function () {
 
 	var SIGNALS = signals;
 
-	this.DEFAULT_CAMERA = new THREE.PerspectiveCamera( 50, 1, 1, 10000 );
+	this.DEFAULT_CAMERA = new THREE.PerspectiveCamera( 50, 1, 0.1, 10000 );
 	this.DEFAULT_CAMERA.name = 'Camera';
 	this.DEFAULT_CAMERA.position.set( 20, 10, 20 );
 	this.DEFAULT_CAMERA.lookAt( new THREE.Vector3() );
