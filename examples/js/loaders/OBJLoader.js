294f550e05ee1f17b965cff8a38cabf38f053170
Samuel Carlsson <samuel.carlsson@volumental.com>
2015-12-16 15:16:06 +0100

    OBJLoader now reports unknown lines
    Now the OBJLoader reports unknown lines via the `onError` function. This
    allows apps to either display the error for building robust apps, or
    just ignore it if you don't care for some reason.

--- examples/js/loaders/OBJLoader.js
+++ examples/js/loaders/OBJLoader.js
@@ -14,7 +14,7 @@ THREE.OBJLoader.prototype = {
 
 	constructor: THREE.OBJLoader,
 
-	load: function ( url, onLoad, onProgress, onError ) {
+	load: function ( url, onLoad, onProgress, onError, onParseFail ) {
 
 		var scope = this;
 
@@ -22,7 +22,7 @@ THREE.OBJLoader.prototype = {
 		loader.setPath( this.path );
 		loader.load( url, function ( text ) {
 
-			onLoad( scope.parse( text ) );
+			onLoad( scope.parse( text, onParseFail || onError ) );
 
 		}, onProgress, onError );
 
@@ -40,7 +40,7 @@ THREE.OBJLoader.prototype = {
 
 	},
 
-	parse: function ( text ) {
+	parse: function ( text, onParseFail ) {
 
 		console.time( 'OBJLoader' );
 
@@ -336,8 +336,8 @@ THREE.OBJLoader.prototype = {
 				object.material.smooth = result[ 1 ] === "1" || result[ 1 ] === "on";
 
 			} else {
-
-				// console.log( "THREE.OBJLoader: Unhandled line " + line );
+				
+				onParseFail("THREE.OBJLoader: Unknown line", line)
 
 			}
 
