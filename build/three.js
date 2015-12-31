a46d7393209f4bce1772e76b667eeb6509c7e882
Mr.doob <info@mrdoob.com>
2015-12-31 01:58:24 +0000

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -17914,6 +17914,35 @@ THREE.XHRLoader.prototype = {
 
 };
 
+// File:src/loaders/FontLoader.js
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+THREE.FontLoader = function ( manager ) {
+
+	this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
+
+};
+
+THREE.FontLoader.prototype = {
+
+	constructor: THREE.FontLoader,
+
+	load: function ( url, onLoad, onProgress, onError ) {
+
+		var loader = new THREE.XHRLoader( this.manager );
+		loader.load( url, function ( text ) {
+
+			onLoad( new THREE.Font( JSON.parse( text ) ) );
+
+		}, onProgress, onError );
+
+	}
+
+};
+
 // File:src/loaders/ImageLoader.js
 
 /**
@@ -32942,13 +32971,13 @@ THREE.ImageUtils = {
 
 	loadCompressedTexture: function () {
 
-		console.error( 'THREE.ImageUtils.loadCompressedTexture has been removed. Use THREE.DDSLoader instead.' )
+		console.error( 'THREE.ImageUtils.loadCompressedTexture has been removed. Use THREE.DDSLoader instead.' );
 
 	},
 
 	loadCompressedTextureCube: function () {
 
-		console.error( 'THREE.ImageUtils.loadCompressedTextureCube has been removed. Use THREE.DDSLoader instead.' )
+		console.error( 'THREE.ImageUtils.loadCompressedTextureCube has been removed. Use THREE.DDSLoader instead.' );
 
 	}
 
@@ -32993,15 +33022,6 @@ THREE.CanvasRenderer = function () {
 	this.render = function () {};
 	this.setClearColor = function () {};
 	this.setSize = function () {};
-
-};
-
-//
-
-THREE.TextGeometry = function () {
-
-	console.error( 'THREE.TextGeometry has been moved to /examples/js/geometries/TextGeometry.js' );
-	console.error( 'THREE.FontUtils has been moved to /examples/js/utils/FontUtils.js' );
 
 };
 
@@ -34125,9 +34145,7 @@ THREE.Curve.prototype = {
 
 	}
 
-}
-
-THREE.Curve.Utils = THREE.CurveUtils; // backwards compatibility
+};
 
 // TODO: Transformation for Curves?
 
@@ -34323,6 +34341,193 @@ THREE.CurvePath.prototype.createGeometry = function( points ) {
 	}
 
 	return geometry;
+
+};
+
+// File:src/extras/core/Font.js
+
+/**
+ * @author zz85 / http://www.lab4games.net/zz85/blog
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+THREE.Font = function ( data ) {
+
+	this.data = data;
+
+};
+
+THREE.Font.prototype = {
+
+	constructor: THREE.Font,
+
+	generateShapes: function ( text, size, curveSegments ) {
+
+		function createPaths( text ) {
+
+			var chars = String( text ).split( '' );
+			var scale = size / data.resolution;
+			var offset = 0;
+
+			var paths = [];
+
+			for ( var i = 0; i < chars.length; i ++ ) {
+
+				var path = new THREE.Path();
+
+				var ret = extractGlyphPoints( chars[ i ], scale, offset, path );
+				offset += ret.offset;
+
+				paths.push( ret.path );
+
+			}
+
+			return paths;
+
+		}
+
+		function extractGlyphPoints( c, scale, offset, path ) {
+
+			var pts = [];
+
+			var b2 = THREE.ShapeUtils.b2;
+			var b3 = THREE.ShapeUtils.b3;
+
+			var i, i2, divisions,
+				outline, action, length,
+				scaleX, scaleY,
+				x, y, cpx, cpy, cpx0, cpy0, cpx1, cpy1, cpx2, cpy2,
+				laste,
+				glyph = data.glyphs[ c ] || data.glyphs[ '?' ];
+
+			if ( ! glyph ) return;
+
+			if ( glyph.o ) {
+
+				outline = glyph._cachedOutline || ( glyph._cachedOutline = glyph.o.split( ' ' ) );
+				length = outline.length;
+
+				scaleX = scale;
+				scaleY = scale;
+
+				for ( i = 0; i < length; ) {
+
+					action = outline[ i ++ ];
+
+					//console.log( action );
+
+					switch ( action ) {
+
+					case 'm':
+
+						// Move To
+
+						x = outline[ i ++ ] * scaleX + offset;
+						y = outline[ i ++ ] * scaleY;
+
+						path.moveTo( x, y );
+						break;
+
+					case 'l':
+
+						// Line To
+
+						x = outline[ i ++ ] * scaleX + offset;
+						y = outline[ i ++ ] * scaleY;
+						path.lineTo( x, y );
+						break;
+
+					case 'q':
+
+						// QuadraticCurveTo
+
+						cpx  = outline[ i ++ ] * scaleX + offset;
+						cpy  = outline[ i ++ ] * scaleY;
+						cpx1 = outline[ i ++ ] * scaleX + offset;
+						cpy1 = outline[ i ++ ] * scaleY;
+
+						path.quadraticCurveTo( cpx1, cpy1, cpx, cpy );
+
+						laste = pts[ pts.length - 1 ];
+
+						if ( laste ) {
+
+							cpx0 = laste.x;
+							cpy0 = laste.y;
+
+							for ( i2 = 1, divisions = this.divisions; i2 <= divisions; i2 ++ ) {
+
+								var t = i2 / divisions;
+								b2( t, cpx0, cpx1, cpx );
+								b2( t, cpy0, cpy1, cpy );
+
+							}
+
+						}
+
+						break;
+
+					case 'b':
+
+						// Cubic Bezier Curve
+
+						cpx  = outline[ i ++ ] * scaleX + offset;
+						cpy  = outline[ i ++ ] * scaleY;
+						cpx1 = outline[ i ++ ] * scaleX + offset;
+						cpy1 = outline[ i ++ ] * scaleY;
+						cpx2 = outline[ i ++ ] * scaleX + offset;
+						cpy2 = outline[ i ++ ] * scaleY;
+
+						path.bezierCurveTo( cpx1, cpy1, cpx2, cpy2, cpx, cpy );
+
+						laste = pts[ pts.length - 1 ];
+
+						if ( laste ) {
+
+							cpx0 = laste.x;
+							cpy0 = laste.y;
+
+							for ( i2 = 1, divisions = this.divisions; i2 <= divisions; i2 ++ ) {
+
+								var t = i2 / divisions;
+								b3( t, cpx0, cpx1, cpx2, cpx );
+								b3( t, cpy0, cpy1, cpy2, cpy );
+
+							}
+
+						}
+
+						break;
+
+					}
+
+				}
+
+			}
+
+			return { offset: glyph.ha * scale, path: path };
+
+		}
+
+		//
+
+		if ( size === undefined ) size = 100;
+		if ( curveSegments === undefined ) curveSegments = 4;
+
+		var data = this.data;
+
+		var paths = createPaths( text );
+		var shapes = [];
+
+		for ( var p = 0, pl = paths.length; p < pl; p ++ ) {
+
+			Array.prototype.push.apply( shapes, paths[ p ].toShapes() );
+
+		}
+
+		return shapes;
+
+	}
 
 };
 
@@ -35130,8 +35335,6 @@ THREE.Shape.prototype.extractPoints = function ( divisions ) {
 
 };
 
-THREE.Shape.Utils = THREE.ShapeUtils; // backwards compatibility
-
 // File:src/extras/curves/LineCurve.js
 
 /**************************************************************
@@ -35837,7 +36040,7 @@ THREE.BoxGeometry = function ( width, height, depth, widthSegments, heightSegmen
 THREE.BoxGeometry.prototype = Object.create( THREE.Geometry.prototype );
 THREE.BoxGeometry.prototype.constructor = THREE.BoxGeometry;
 
-THREE.CubeGeometry = THREE.BoxGeometry; // backwards compatibility
+THREE.CubeGeometry = THREE.BoxGeometry;
 
 // File:src/extras/geometries/CircleGeometry.js
 
@@ -37511,6 +37714,61 @@ THREE.SphereBufferGeometry = function ( radius, widthSegments, heightSegments, p
 
 THREE.SphereBufferGeometry.prototype = Object.create( THREE.BufferGeometry.prototype );
 THREE.SphereBufferGeometry.prototype.constructor = THREE.SphereBufferGeometry;
+
+// File:src/extras/geometries/TextGeometry.js
+
+/**
+ * @author zz85 / http://www.lab4games.net/zz85/blog
+ * @author alteredq / http://alteredqualia.com/
+ *
+ * Text = 3D Text
+ *
+ * parameters = {
+ *  font: <THREE.Font>, // font
+ *
+ *  size: <float>, // size of the text
+ *  height: <float>, // thickness to extrude text
+ *  curveSegments: <int>, // number of points on the curves
+ *
+ *  bevelEnabled: <bool>, // turn on bevel
+ *  bevelThickness: <float>, // how deep into text bevel goes
+ *  bevelSize: <float> // how far from text outline is bevel
+ * }
+ */
+
+THREE.TextGeometry = function ( text, parameters ) {
+
+	parameters = parameters || {};
+
+	var font = parameters.font;
+
+	if ( font instanceof THREE.Font === false ) {
+
+		console.error( 'THREE.TextGeometry: font parameter is not an instance of THREE.Font.' );
+		return new THREE.Geometry();
+
+	}
+
+	var shapes = font.generateShapes( text, parameters.size, parameters.curveSegments );
+
+	// translate parameters to ExtrudeGeometry API
+
+	parameters.amount = parameters.height !== undefined ? parameters.height : 50;
+
+	// defaults
+
+	if ( parameters.bevelThickness === undefined ) parameters.bevelThickness = 10;
+	if ( parameters.bevelSize === undefined ) parameters.bevelSize = 8;
+	if ( parameters.bevelEnabled === undefined ) parameters.bevelEnabled = false;
+
+	THREE.ExtrudeGeometry.call( this, shapes, parameters );
+
+	this.type = 'TextGeometry';
+
+};
+
+THREE.TextGeometry.prototype = Object.create( THREE.ExtrudeGeometry.prototype );
+THREE.TextGeometry.prototype.constructor = THREE.TextGeometry;
 
 // File:src/extras/geometries/TorusGeometry.js
 
