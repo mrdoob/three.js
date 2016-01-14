f66b4d3f5b963ceafa46c33bbd24bc24cff3a036
Yaroslav Sivakov <yar3333@gmail.com>
2016-01-14 22:49:19 +0300

    Loader/material: ignore DbgColor if colorDiffuse is defined

--- src/loaders/Loader.js
+++ src/loaders/Loader.js
@@ -126,7 +126,7 @@ THREE.Loader.prototype = {
 
 				switch ( name ) {
 					case 'DbgColor':
-						json.color = value;
+						if (!m.hasOwnProperty('colorDiffuse')) json.color = value;
 						break;
 					case 'DbgIndex':
 					case 'opticalDensity':
