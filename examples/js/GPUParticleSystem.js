a84b64b1d7efb31ba36a1b10d11a1d1f9f7b7c26
Mr.doob <info@mrdoob.com>
2016-02-02 05:31:54 +0900

    GPUParticleSystem: Using alternative pack() code. Hopefully fixes #7804.

--- examples/js/GPUParticleSystem.js
+++ examples/js/GPUParticleSystem.js
@@ -83,7 +83,7 @@ THREE.GPUParticleSystem = function(options) {
       '{',
       'const vec4 bit_shift = vec4(256.0*256.0*256.0, 256.0*256.0, 256.0, 1.0);',
       'const vec4 bit_mask  = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);',
-      'vec4 res = fract(depth * bit_shift);',
+      'vec4 res = mod(depth*bit_shift*vec4(255), vec4(256))/vec4(255);',
       'res -= res.xxyz * bit_mask;',
       'return res;',
       '}',
