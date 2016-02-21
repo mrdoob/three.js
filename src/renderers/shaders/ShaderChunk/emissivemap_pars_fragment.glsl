7ea1c679b30ff6b4117ae62e938121382b462d86
Ben Houston <ben@exocortex.com>
2016-02-20 21:49:06 -0500

    fix bug with regards to emissiveMapTexelToLinear...

--- src/renderers/shaders/ShaderChunk/emissivemap_pars_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/emissivemap_pars_fragment.glsl
@@ -3,7 +3,7 @@
 	uniform sampler2D emissiveMap;
 
 	vec4 emissiveMapTexelToLinear( vec4 value ) {
-		#define DECODE_MACRO EMISSIVEMAP_ENCODING
+		#define MACRO_DECODE EMISSIVEMAP_ENCODING
 			#include <encoding_template>
 		#undef MACRO_DECODE
 	}
