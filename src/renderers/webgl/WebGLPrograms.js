8f15583b5fa548c7c827df45dd10764f5c1357c4
Mr.doob <info@mrdoob.com>
2016-02-26 15:19:23 +0900

    WebGLPrograms: Clean up.

--- src/renderers/webgl/WebGLPrograms.js
+++ src/renderers/webgl/WebGLPrograms.js
@@ -67,6 +67,38 @@ THREE.WebGLPrograms = function ( renderer, capabilities ) {
 
 	}
 
+	function getTextureEncodingFromMap( map ) {
+
+		if ( ! map ) return false;
+
+		var encoding;
+
+		if ( map instanceof THREE.Texture ) {
+
+			encoding = map.encoding;
+
+		} else if ( map instanceof THREE.WebGLRenderTarget ) {
+
+			encoding = map.texture.encoding;
+
+		} else {
+
+			throw new Error( "can not determine texture encoding from map: " + map );
+
+		}
+
+		// add backwards compatibility for WebGLRenderer.gammaInput parameter, should probably be removed at some point.
+
+		if ( encoding === THREE.LinearEncoding && renderer.gammaInput ) {
+
+			encoding = THREE.GammaEncoding;
+
+		}
+
+		return encoding;
+
+	}
+
 	this.getParameters = function ( material, lights, fog, object ) {
 
 		var shaderID = shaderIDs[ material.type ];
@@ -86,27 +118,6 @@ THREE.WebGLPrograms = function ( renderer, capabilities ) {
 
 			}
 
-		}
-
-		var getTextureEncodingFromMap = function( map ) {
-			if( ! map ) { // no texture
-				return false;
-			}
-			var encoding;
-			if( map instanceof THREE.Texture ) {
-				encoding = map.encoding;
-			}
-			else if( map instanceof THREE.WebGLRenderTarget ) {
-				encoding = map.texture.encoding;
-			}
-			else {
-				throw new Error( "can not determine texture encoding from map: " + map );
-			}
-			// add backwards compatibility for WebGLRenderer.gammaInput parameter, should probably be removed at some point.
-			if( encoding === THREE.LinearEncoding && renderer.gammaInput ) {
-				encoding = THREE.GammaEncoding;
-			}
-			return encoding;
 		}
 
 		var parameters = {
