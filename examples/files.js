00fd2eef38b24ad6653c440b7034204fa9090aa8
Mr.doob <info@mrdoob.com>
2016-02-27 18:45:58 +0900

    Removed example from list.

--- examples/files.js
+++ examples/files.js
@@ -138,7 +138,6 @@ var files = {
 		"webgl_materials_variations_lambert",
 		"webgl_materials_variations_phong",
 		"webgl_materials_variations_standard",
-		"webgl_materials_variations_standard2",
 		"webgl_materials_video",
 		"webgl_materials_wireframe",
 		"webgl_mirror",
