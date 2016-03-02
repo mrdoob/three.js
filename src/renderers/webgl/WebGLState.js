0f1e9b4331d0314d8eb2c625419ef9825d576c4f
Ben Houston <ben@exocortex.com>
2016-03-02 11:14:09 -0500

    restore old fallthrough behavior for blending modes not recognized.

--- src/renderers/webgl/WebGLState.js
+++ src/renderers/webgl/WebGLState.js
@@ -250,15 +250,10 @@ THREE.WebGLState = function ( gl, extensions, paramThreeToGL ) {
 				gl.blendEquationSeparate( gl.FUNC_ADD, gl.FUNC_ADD );
 				gl.blendFuncSeparate( gl.ZERO, gl.ZERO, gl.SRC_COLOR, gl.SRC_ALPHA );
 
-			} else if( blending === THREE.NormalBlending || blending === THREE.NoBlending ) {
+			} else {
 
 				gl.blendEquationSeparate( gl.FUNC_ADD, gl.FUNC_ADD );
 				gl.blendFuncSeparate( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA );
-
-			}
-			else {
-
-				console.error( "Unsupported blending type: " + blending );
 
 			}
 
