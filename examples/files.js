94290c538caffeac12f8c644d8d867907232f63a
Ben Houston <ben@exocortex.com>
2016-03-03 13:18:58 -0500

    add slots to example file list.

--- examples/files.js
+++ examples/files.js
@@ -127,6 +127,7 @@ var files = {
 		"webgl_materials_parallaxmap",
 		"webgl_materials_shaders_fresnel",
 		"webgl_materials_skin",
+		"webgl_materials_slots",
 		"webgl_materials_standard",
 		"webgl_materials_texture_anisotropy",
 		"webgl_materials_texture_compressed",
