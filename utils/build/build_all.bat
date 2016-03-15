162b849082f196b3f7cc04aead80c93a32bb611c
Ben Houston <ben@exocortex.com>
2016-03-14 21:54:23 -0400

    adopt build.js instead of build.py

--- utils/build/build_all.bat
+++ utils/build/build_all.bat
@@ -1,7 +1,7 @@
-python build.py --include common --include extras --output ../../build/three.js
-python build.py --include common --include extras --minify --output ../../build/three.min.js
-python build.py --include canvas --minify --output ../../build/three-canvas.min.js
-python build.py --include css3d --minify --output ../../build/three-css3d.min.js
-python build.py --include extras --externs externs/extras.js --minify --output ../../build/three-extras.min.js
-python build.py --include math --output ../../build/three-math.js
-python build.py --include math --minify --output ../../build/three-math.min.js
+@node build.js --include common --include extras --output ../../build/three.js
+@node build.js --include common --include extras --minify --output ../../build/three.min.js
+@node build.js --include canvas --minify --output ../../build/three-canvas.min.js
+@node build.js --include css3d --minify --output ../../build/three-css3d.min.js
+@node build.js --include extras --externs externs/extras.js --minify --output ../../build/three-extras.min.js
+@node build.js --include math --output ../../build/three-math.js
+@node build.js --include math --minify --output ../../build/three-math.min.js
--- utils/build/build_debug.bat
+++ utils/build/build_debug.bat
@@ -1,1 +1,1 @@
-python build.py --include common --include extras --output ../../build/three.min.js
+@node build.js --include common --include extras --output ../../build/three.min.js
