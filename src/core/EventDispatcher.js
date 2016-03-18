1023f9d5ed73959482c14c9706862878998de7c4
Master James <masterjames@master-domain.net>
2016-03-18 06:20:53 -0700

    Revert "Revert "This is adding optional direct indexing to the parent's child array manipulators.""
    This reverts commit 048bb247b436a511a7e0772a1496e2c4f7e16083.

--- src/core/EventDispatcher.js
+++ src/core/EventDispatcher.js
@@ -78,25 +78,17 @@ THREE.EventDispatcher.prototype = {
 
 		if ( this._listeners === undefined ) return;
 
-		var listeners = this._listeners;
-		var listenerArray = listeners[ event.type ];
+		var listenerArray = this._listeners[ event.type ];
 
 		if ( listenerArray !== undefined ) {
 
 			event.target = this;
 
-			var array = [];
 			var length = listenerArray.length;
 
 			for ( var i = 0; i < length; i ++ ) {
 
-				array[ i ] = listenerArray[ i ];
-
-			}
-
-			for ( var i = 0; i < length; i ++ ) {
-
-				array[ i ].call( this, event );
+				listenerArray[ i ].call( this, event );
 
 			}
 
