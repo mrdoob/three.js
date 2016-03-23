ecbc44fc6a40d7e218f50d898d5d49256b10f2d6
Mr.doob <info@mrdoob.com>
2016-03-23 23:23:00 +0100

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -16147,6 +16147,41 @@ THREE.BooleanKeyframeTrack.prototype =
 
 } );
 
+// File:src/animation/tracks/ColorKeyframeTrack.js
+
+/**
+ *
+ * A Track of keyframe values that represent color.
+ *
+ *
+ * @author Ben Houston / http://clara.io/
+ * @author David Sarno / http://lighthaus.us/
+ * @author tschw
+ */
+
+THREE.ColorKeyframeTrack = function ( name, times, values, interpolation ) {
+
+	THREE.KeyframeTrack.call( this, name, times, values, interpolation );
+
+};
+
+THREE.ColorKeyframeTrack.prototype =
+		Object.assign( Object.create( THREE.KeyframeTrack.prototype ), {
+
+	constructor: THREE.ColorKeyframeTrack,
+
+	ValueTypeName: 'color'
+
+	// ValueBufferType is inherited
+
+	// DefaultInterpolation is inherited
+
+
+	// Note: Very basic implementation and nothing special yet.
+	// However, this is the place for color space parameterization.
+
+} );
+
 // File:src/animation/tracks/NumberKeyframeTrack.js
 
 /**
