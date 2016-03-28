0f57208ca5066ed65f912955c7620a6af8905ced
Mr.doob <info@mrdoob.com>
2016-03-28 15:30:52 +0100

    Fixed DirectionalLights shadow breakage.

--- src/renderers/webgl/WebGLShadowMap.js
+++ src/renderers/webgl/WebGLShadowMap.js
@@ -171,6 +171,8 @@ THREE.WebGLShadowMap = function ( _renderer, _lights, _objects ) {
 
 				shadow.map = new THREE.WebGLRenderTarget( _shadowMapSize.x, _shadowMapSize.y, pars );
 
+				shadowCamera.updateProjectionMatrix();
+
 			}
 
 			if ( shadow instanceof THREE.SpotLightShadow ) {
