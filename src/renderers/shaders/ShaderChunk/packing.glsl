a95d4827c3c96a785507b6027064cd4606c25bfc
Ben Houston <ben@exocortex.com>
2016-04-06 14:24:07 -0400

    Revert "adopt fract because theoretically it should be faster, no divide."
    This reverts commit 8dfb3f1d2a53d389540bb514d5fc183605e9b133.

--- src/renderers/shaders/ShaderChunk/packing.glsl
+++ src/renderers/shaders/ShaderChunk/packing.glsl
@@ -10,7 +10,7 @@ vec3 unpackRGBToNormal( const in vec3 rgb ) {
 vec4 packLinearUnitToRGBA( const in float value ) {
 	const vec4 bit_shift = vec4( 256.0 * 256.0 * 256.0, 256.0 * 256.0, 256.0, 1.0 );
 	const vec4 bit_mask = vec4( 0.0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0 );
-	vec4 res = fract( value * bit_shift );
+	vec4 res = mod( value * bit_shift * vec4( 255 ), vec4( 256 ) ) / vec4( 255 );
 	res -= res.xxyz * bit_mask;
 	return res;
 }
