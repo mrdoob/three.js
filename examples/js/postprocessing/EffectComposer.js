0fbc8afb348198e4924d9805d1d4be5869264418
Michael Herzog <michael.herzog@human-interactive.org>
2016-04-06 02:14:10 +0200

    Post-Processing: Introducing base class for Passes
    * Post-Processing: Introducing base class for Passes
    
    * Post-Processing: move pass to effect composer

--- examples/js/postprocessing/EffectComposer.js
+++ examples/js/postprocessing/EffectComposer.js
@@ -134,3 +134,32 @@ THREE.EffectComposer.prototype = {
 	}
 
 };
+
+
+THREE.Pass = function () {
+
+  // if set to true, the pass is processed by the composer
+  this.enabled = true;
+
+  // if set to true, the pass indicates to swap read and write buffer after rendering
+  this.needsSwap = true;
+
+  // if set to true, the pass clears its buffer before rendering
+  this.clear = false;
+
+  // if set to true, the result of the pass is rendered to screen
+  this.renderToScreen = false;
+
+};
+
+THREE.Pass.prototype = {
+
+  constructor: THREE.Pass,
+
+  render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+
+		console.error( "THREE.Pass: .render() must be implemented in derived pass." );
+
+  }
+
+};
