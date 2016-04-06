8dfb3f1d2a53d389540bb514d5fc183605e9b133
Ben Houston <ben@exocortex.com>
2016-04-06 13:52:27 -0400

    adopt fract because theoretically it should be faster, no divide.

--- src/renderers/shaders/ShaderChunk/packing.glsl
+++ src/renderers/shaders/ShaderChunk/packing.glsl
@@ -11,7 +11,7 @@ vec4 packLinearUnitToRGBA( const in float value ) {
 
 	const vec4 bit_shift = vec4( 256.0 * 256.0 * 256.0, 256.0 * 256.0, 256.0, 1.0 );
 	const vec4 bit_mask = vec4( 0.0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0 );
-	vec4 res = mod( value * bit_shift * vec4( 255 ), vec4( 256 ) ) / vec4( 255 );
+	vec4 res = fract( value * bit_shift );
 	res -= res.xxyz * bit_mask;
 	return res;
 
