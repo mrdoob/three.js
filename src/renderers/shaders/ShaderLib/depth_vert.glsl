7b2ff27e1589da04830113ef9294bc39c889bf63
Ben Houston <ben@exocortex.com>
2016-04-07 11:55:50 -0400

    fix MeshDepthMaterial.

--- src/renderers/shaders/ShaderLib/depth_vert.glsl
+++ src/renderers/shaders/ShaderLib/depth_vert.glsl
@@ -3,6 +3,8 @@
 #include <logdepthbuf_pars_vertex>
 #include <clipping_planes_pars_vertex>
 
+varying vec3 vViewPosition;
+
 void main() {
 
 	#include <begin_vertex>
@@ -10,5 +12,7 @@ void main() {
 	#include <project_vertex>
 	#include <logdepthbuf_vertex>
 	#include <clipping_planes_vertex>
+
+	vViewPosition = - mvPosition.xyz;
 
 }
