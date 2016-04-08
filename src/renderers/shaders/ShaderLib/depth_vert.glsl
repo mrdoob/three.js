9035745c899bd7c0981bf8fe92d79e4d510796e9
Ben Houston <ben@exocortex.com>
2016-04-08 11:37:27 -0400

    enable skinning in MeshDepthMaterial, as it was enabled in depthRGBA material.

--- src/renderers/shaders/ShaderLib/depth_vert.glsl
+++ src/renderers/shaders/ShaderLib/depth_vert.glsl
@@ -1,5 +1,6 @@
 #include <common>
 #include <morphtarget_pars_vertex>
+#include <skinning_pars_vertex>
 #include <logdepthbuf_pars_vertex>
 #include <clipping_planes_pars_vertex>
 
@@ -11,8 +12,11 @@
 
 void main() {
 
+	#include <skinbase_vertex>
+
 	#include <begin_vertex>
 	#include <morphtarget_vertex>
+	#include <skinning_vertex>
 	#include <project_vertex>
 	#include <logdepthbuf_vertex>
 	#include <clipping_planes_vertex>
