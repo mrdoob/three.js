ae16b59a164c1c0fe2ae714a594dd0138dfde27d
takahiro <hogehoge@gachapin.jp>
2016-04-10 01:10:33 -0700

    Support Japanese Texture Filename for MMDLoader

--- examples/js/loaders/MMDLoader.js
+++ examples/js/loaders/MMDLoader.js
@@ -456,7 +456,7 @@ THREE.MMDLoader.prototype.parsePmd = function ( buffer ) {
 			p.toonIndex = dv.getInt8();
 			p.edgeFlag = dv.getUint8();
 			p.faceCount = dv.getUint32() / 3;
-			p.fileName = dv.getChars( 20 );
+			p.fileName = dv.getSjisStringsAsUnicode( 20 );
 			return p;
 
 		};
