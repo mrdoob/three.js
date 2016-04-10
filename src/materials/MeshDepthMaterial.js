dc9ddfae6807966479053d50e41ecfcda0753a38
WestLangley <WestLangley@users.noreply.github.com>
2016-04-10 07:11:58 -0400

    MeshDepthMaterial: Added displacement map support (#8586)

--- src/materials/MeshDepthMaterial.js
+++ src/materials/MeshDepthMaterial.js
@@ -6,6 +6,10 @@
  * parameters = {
  *  opacity: <float>,
  *
+ *  displacementMap: new THREE.Texture( <Image> ),
+ *  displacementScale: <float>,
+ *  displacementBias: <float>,
+ *
  *  wireframe: <boolean>,
  *  wireframeLinewidth: <float>
  * }
@@ -22,6 +26,10 @@ THREE.MeshDepthMaterial = function ( parameters ) {
 
 	this.skinning = false;
 	this.morphTargets = false;
+
+	this.displacementMap = null;
+	this.displacementScale = 1;
+	this.displacementBias = 0;
 
 	this.wireframe = false;
 	this.wireframeLinewidth = 1;
@@ -42,6 +50,10 @@ THREE.MeshDepthMaterial.prototype.copy = function ( source ) {
 
 	this.skinning = source.skinning;
 	this.morphTargets = source.morphTargets;
+
+	this.displacementMap = source.displacementMap;
+	this.displacementScale = source.displacementScale;
+	this.displacementBias = source.displacementBias;
 
 	this.wireframe = source.wireframe;
 	this.wireframeLinewidth = source.wireframeLinewidth;
