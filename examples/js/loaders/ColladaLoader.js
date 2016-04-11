f2ff4e0b16653df4875085ec26e7b14664d251fc
Niels <nielspausma@gmail.com>
2016-04-11 16:35:02 +0200

    Set Face3 materialIndex when using MultiMaterial
    I noticed this was missing after updating from an older version of ColladaLoader.js. This fixes the face materialIndex not being set.

--- examples/js/loaders/ColladaLoader.js
+++ examples/js/loaders/ColladaLoader.js
@@ -1170,6 +1170,13 @@ THREE.ColladaLoader = function () {
 				if ( num_materials > 1 ) {
 
 					material = new THREE.MultiMaterial( used_materials_array );
+					
+					for ( j = 0; j < geom.faces.length; j ++ ) {
+
+						var face = geom.faces[ j ];
+						face.materialIndex = used_materials[ face.daeMaterial ]
+
+					}
 
 				}
 
