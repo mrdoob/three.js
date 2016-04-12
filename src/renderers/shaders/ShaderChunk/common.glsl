b3518df17f82ec2458221cf9580c24a042f884e4
Ben Houston <ben@exocortex.com>
2016-04-12 10:04:28 -0400

    more adoption of tabs.

--- src/renderers/shaders/ShaderChunk/common.glsl
+++ src/renderers/shaders/ShaderChunk/common.glsl
@@ -15,8 +15,8 @@ float average( const in vec3 color ) { return dot( color, vec3( 0.3333 ) ); }
 // expects values in the range of [0,1]x[0,1], returns values in the [0,1] range.
 // do not collapse into a single function per: http://byteblacksmith.com/improvements-to-the-canonical-one-liner-glsl-rand-for-opengl-es-2-0/
 highp float rand( const in vec2 uv ) {
-  const highp float a = 12.9898, b = 78.233, c = 43758.5453;
-  highp float dt = dot( uv.xy, vec2( a,b ) ), sn = mod( dt, PI );
+	const highp float a = 12.9898, b = 78.233, c = 43758.5453;
+	highp float dt = dot( uv.xy, vec2( a,b ) ), sn = mod( dt, PI );
 	return fract(sin(sn) * c);
 }
 
