2e5a360587ae0a0bddc80c638a4e87d997fb3eab
WestLangley <dat1234@gmail.com>
2016-04-11 23:08:36 -0400

    Packing nomenclature mods

--- src/renderers/shaders/ShaderChunk/packing.glsl
+++ src/renderers/shaders/ShaderChunk/packing.glsl
@@ -7,14 +7,14 @@ vec3 unpackRGBToNormal( const in vec3 rgb ) {
 }
 
 
-vec4 packLinearUnitToRGBA( const in float value ) {
+vec4 packDepthToRGBA( const in float value ) {
 	const vec4 bit_shift = vec4( 256.0 * 256.0 * 256.0, 256.0 * 256.0, 256.0, 1.0 );
 	const vec4 bit_mask = vec4( 0.0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0 );
 	vec4 res = mod( value * bit_shift * vec4( 255 ), vec4( 256 ) ) / vec4( 255 );
 	res -= res.xxyz * bit_mask;
 	return res;
 }
-float unpackRGBAToLinearUnit( const in vec4 rgba ) {
+float unpackRGBAToDepth( const in vec4 rgba ) {
 	const vec4 bitSh = vec4( 1.0 / ( 256.0 * 256.0 * 256.0 ), 1.0 / ( 256.0 * 256.0 ), 1.0 / 256.0, 1.0 );
 	return dot( rgba, bitSh );
 }
