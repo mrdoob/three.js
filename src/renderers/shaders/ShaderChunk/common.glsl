b4dd7142b3b9d3accd837f0ab430730664a18bf9
Ben Houston <ben@exocortex.com>
2016-04-12 09:23:09 -0400

    avoid collapsing into a single function - @WestLangley recommendations

--- src/renderers/shaders/ShaderChunk/common.glsl
+++ src/renderers/shaders/ShaderChunk/common.glsl
@@ -13,7 +13,12 @@ float pow3( const in float x ) { return x*x*x; }
 float pow4( const in float x ) { float x2 = x*x; return x2*x2; }
 float average( const in vec3 color ) { return dot( color, vec3( 0.3333 ) ); }
 // expects values in the range of [0,1]x[0,1], returns values in the [0,1] range.
-highp float rand( const in vec2 uv ) { return fract(sin(mod(dot(uv,vec2(12.9898,78.233)), PI)) * 43758.5453); }
+// do not collapse into a single function per: http://byteblacksmith.com/improvements-to-the-canonical-one-liner-glsl-rand-for-opengl-es-2-0/
+highp float rand( const in vec2 uv ) {
+  const highp float a = 12.9898, b = 78.233, c = 43758.5453;
+  highp float dt = dot( uv.xy, vec2( a,b ) ), sn = mod( dt, PI );
+	return fract(sin(sn) * c);
+}
 
 struct IncidentLight {
 	vec3 color;
