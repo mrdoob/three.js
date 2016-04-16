4d8fe42657f0e7cff0989722c4418180b52a8b9d
Ben Houston <ben@exocortex.com>
2016-04-15 22:04:54 -0400

    simplify blur shader.

--- examples/js/shaders/BlurShader.js
+++ examples/js/shaders/BlurShader.js
@@ -4,8 +4,6 @@
  * For a horizontal blur, use X_STEP 1, Y_STEP 0
  * For a vertical blur, use X_STEP 0, Y_STEP 1
  *
- * For speed, this shader precomputes uv offsets in vert shader to allow for prefetching
- *
  */
 
 THREE.BlurShader = {
@@ -29,15 +27,11 @@ THREE.BlurShader = {
 
 		"#include <common>",
 
-		"uniform vec2 size;",
-
 		"varying vec2 vUv;",
-		"varying vec2 vInvSize;",
 
 		"void main() {",
 
 			"vUv = uv;",
-			"vInvSize = 1.0 / size;",
 
 			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
 
@@ -48,22 +42,24 @@ THREE.BlurShader = {
 	fragmentShader: [
 
 		"uniform sampler2D tDiffuse;",
+		"uniform vec2 size;",
 
 		"uniform vec2 sampleUvOffsets[ KERNEL_RADIUS + 1 ];",
 		"uniform float sampleWeights[ KERNEL_RADIUS + 1 ];",
 
 		"varying vec2 vUv;",
-		"varying vec2 vInvSize;",
 
 		"void main() {",
 
-		"float weightSum = sampleWeights[0];",
-		"vec4 diffuseSum = texture2D( tDiffuse, vUv ) * weightSum;",
+			"vec2 invSize = 1.0 / size;",
+
+			"float weightSum = sampleWeights[0];",
+			"vec4 diffuseSum = texture2D( tDiffuse, vUv ) * weightSum;",
 
 			"for( int i = 1; i <= KERNEL_RADIUS; i ++ ) {",
 
 				"float weight = sampleWeights[i];",
-				"vec2 sampleUvOffset = sampleUvOffsets[i] * vInvSize;",
+				"vec2 sampleUvOffset = sampleUvOffsets[i] * invSize;",
 				"diffuseSum += ( texture2D( tDiffuse, vUv + sampleUvOffset ) + texture2D( tDiffuse, vUv - sampleUvOffset ) ) * weight;",
 				"weightSum += 2.0 * weight;",
 
