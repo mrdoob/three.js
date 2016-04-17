a41375c4a494526816ba8063d20fedc147087b6e
aardgoose <angus.sawyer@gmail.com>
2016-04-17 16:22:55 +0100

    documentation: remove gyroscope object no longer in library

--- docs/list.js
+++ docs/list.js
@@ -173,7 +173,6 @@ var list = {
 		"Extras / Core": [
 			[ "Curve", "api/extras/core/Curve" ],
 			[ "CurvePath", "api/extras/core/CurvePath" ],
-			[ "Gyroscope", "api/extras/core/Gyroscope" ],
 			[ "Path", "api/extras/core/Path" ],
 			[ "Shape", "api/extras/core/Shape" ]
 		],
