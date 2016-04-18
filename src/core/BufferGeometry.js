af1d26419bf8a35b2a9b895810d77a3b8d288a9b
Chadwick <chadwickr@vacolab.adlnet.org>
2016-04-18 13:19:35 -0400

    serialize the normalized flag

--- src/core/BufferGeometry.js
+++ src/core/BufferGeometry.js
@@ -921,7 +921,8 @@ THREE.BufferGeometry.prototype = {
 			data.data.attributes[ key ] = {
 				itemSize: attribute.itemSize,
 				type: attribute.array.constructor.name,
-				array: array
+				array: array,
+				normalized: attribute.normalized
 			};
 
 		}
