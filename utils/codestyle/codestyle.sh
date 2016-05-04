cd116400ae4a04759c522088943a7ef29fef0eaa
aallison <aallison@yahoo-inc.com>
2016-05-04 10:36:32 -0700

    codeformat script can take custom paths

--- utils/codestyle/codestyle.sh
+++ utils/codestyle/codestyle.sh
@@ -3,8 +3,12 @@
 # get dir of this script file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# allow passing custom paths
+DEFAULT_PATHS="${DIR}/../../src/**/*.js"
+PATHS="${1:-${DEFAULT_PATHS}}"
+
 ${DIR}/../../node_modules/.bin/esformatter \
     --config ${DIR}/esformatter.config.json \
     -i \
-    "${DIR}/../../src/**/*.js"
+    ${PATHS}
 
