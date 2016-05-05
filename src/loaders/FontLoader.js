df758b8a2c89b38f955a70dbc81db013988c03f7
Mr.doob <info@mrdoob.com>
2016-05-05 17:47:30 -0400

    FontLoader: Added parse(). See #8817.

--- src/loaders/FontLoader.js
+++ src/loaders/FontLoader.js
@@ -14,12 +14,22 @@ THREE.FontLoader.prototype = {
 
 	load: function ( url, onLoad, onProgress, onError ) {
 
+		var scope = this;
+
 		var loader = new THREE.XHRLoader( this.manager );
 		loader.load( url, function ( text ) {
 
-			onLoad( new THREE.Font( JSON.parse( text.substring( 65, text.length - 2 ) ) ) );
+			var font = scope.parse( JSON.parse( text.substring( 65, text.length - 2 ) ) );
+
+			if ( onLoad ) onLoad( font );
 
 		}, onProgress, onError );
+
+	},
+
+	parse: function ( json ) {
+
+		return new THREE.Font( json );
 
 	}
 
