3b59daf656889c6aef207e5c00432386089427b0
godlzr <godlzr@gmail.com>
2016-05-06 13:33:09 -0400

    add Color.sub()

--- src/math/Color.js
+++ src/math/Color.js
@@ -380,6 +380,16 @@ THREE.Color.prototype = {
 
 	},
 
+	sub: function( color ) {
+
+	this.r -= color.r;
+	this.g -= color.g;
+	this.b -= color.b;
+
+	return this;
+
+	},
+
 	add: function ( color ) {
 
 		this.r += color.r;
