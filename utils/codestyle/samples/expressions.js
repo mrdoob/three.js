15e7ebb63b7b8e9f366fe5bc6fe8e7ccf619dc04
aallison <aallison@yahoo-inc.com>
2016-05-10 10:09:55 -0700

    address unary sign operator issue

--- utils/codestyle/samples/expressions.js
+++ utils/codestyle/samples/expressions.js
@@ -12,11 +12,17 @@ x ++;
 
 a ? b : c;
 
+a + ( - b ) + c;
+
 new ( a ? b : c )();
 delete a[ 'prop' ];
 
 obj[ 'key' ];
 obj.key;
+
+foo( aaa, bbb, ccc );
+foo( a,   b,   c );
+foo(   a,   b,   c );
 
 // BAD
 
@@ -28,6 +34,9 @@ x++;
 
 a?b:c;
 a ? b: c;
+
+// unary sign operators should have space after them
+-a + b + ( -c );
 
 obj['key'];
 obj[ 'key'];
