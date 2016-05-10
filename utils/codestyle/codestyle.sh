56d2f51f6bfe4a1eeee4ce6dd6564faef3b0f05d
aallison <aallison@yahoo-inc.com>
2016-05-10 00:07:03 -0700

    revamp codestyle script adding esformatter and test fixtures

--- utils/codestyle/codestyle.sh
+++ utils/codestyle/codestyle.sh
@@ -2,13 +2,28 @@
 
 # Get absolute path to script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE=${DIR}/../..
 
 # Allow formatting specific file paths
 # default to all files
-FILES=${@:-${DIR}/../../}
+FILES=${@:-${BASE}/}
 
-${DIR}/../../node_modules/.bin/jscs \
+${BASE}/node_modules/.bin/esformatter \
+   --config ${DIR}/esformatter.json \
+   -i \
+   "${@}"
+
+# sleep 1;
+
+# apply rules that jscs can auto-fix
+${BASE}/node_modules/.bin/jscs \
     --fix \
     --config=${DIR}/config.json \
-    "${@}" 
+    "${@}"
 
+# sleep 1;
+
+# check rules that jscs will not fix
+# ${BASE}/node_modules/.bin/jscs \
+#     --config=${DIR}/config_strict.json \
+#     "${@}"
