4c247af33c4453a0d3047ce5b89485205a1348ba
aallison <aallison@yahoo-inc.com>
2016-05-11 16:43:20 -0700

    removing old shell files

--- utils/codestyle/filesChangedBetweenBranches.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-UPSTREAM=${1:-dev}
-CURRENT=${2:-$(git rev-parse --abbrev-ref HEAD)}
-
-# echo "comparing ${UPSTREAM} <- ${CURRENT}" 1>&2
-
-git --no-pager diff --name-only ${UPSTREAM}..${CURRENT} \
-    | grep -E ".*\.js$"
