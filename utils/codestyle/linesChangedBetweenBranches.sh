8df683b938dd815c156ba879ebdcf42a78710136
aallison <aallison@yahoo-inc.com>
2016-05-11 10:08:13 -0700

    adding comments

--- utils/codestyle/linesChangedBetweenBranches.sh
+++ utils/codestyle/linesChangedBetweenBranches.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Determine lines modified by feature branch
 git --no-pager diff dev..cleanup \
     | grep -E "^(diff|index|---|\+\+\+|@@)" \
     | while read line; do
@@ -16,3 +17,10 @@ git --no-pager diff dev..cleanup \
 
         fi
     done
+
+# approach
+# 1. `diff dev..current` to figure out which lines are modified by feature branch
+# 2. execute formatter
+# 3. `git diff HEAD..HEAD~1` to see changes made by formatter
+# 4. generate patch from git previous output, filtering out changes not included
+#    in the line ranges found in (1)
