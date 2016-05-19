bd2caba22dd070654d447ba28c5216a44832c0e3
Ben Houston <ben@exocortex.com>
2016-05-19 08:36:33 -0400

    tabify EffectComposer.js

--- examples/js/postprocessing/EffectComposer.js
+++ examples/js/postprocessing/EffectComposer.js
@@ -149,30 +149,30 @@ THREE.EffectComposer.prototype = {
 
 THREE.Pass = function () {
 
-  // if set to true, the pass is processed by the composer
-  this.enabled = true;
+	// if set to true, the pass is processed by the composer
+	this.enabled = true;
 
-  // if set to true, the pass indicates to swap read and write buffer after rendering
-  this.needsSwap = true;
+	// if set to true, the pass indicates to swap read and write buffer after rendering
+	this.needsSwap = true;
 
-  // if set to true, the pass clears its buffer before rendering
-  this.clear = false;
+	// if set to true, the pass clears its buffer before rendering
+	this.clear = false;
 
-  // if set to true, the result of the pass is rendered to screen
-  this.renderToScreen = false;
+	// if set to true, the result of the pass is rendered to screen
+	this.renderToScreen = false;
 
 };
 
 THREE.Pass.prototype = {
 
-  constructor: THREE.Pass,
+	constructor: THREE.Pass,
 
 	setSize: function( width, height ) {},
 
-  render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+	render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
 
 		console.error( "THREE.Pass: .render() must be implemented in derived pass." );
 
-  }
+	}
 
 };
