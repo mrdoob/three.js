eeb750fb4525f9147be3cc021ccbfc7527d973e1
Mr.doob <info@mrdoob.com>
2016-05-31 14:51:57 -0700

    Material: Less aggressive roughness/metalness toJSON.

--- src/materials/Material.js
+++ src/materials/Material.js
@@ -149,8 +149,8 @@ THREE.Material.prototype = {
 
 		if ( this.color instanceof THREE.Color ) data.color = this.color.getHex();
 
-		if ( this.roughness !== 0.5 ) data.roughness = this.roughness;
-		if ( this.metalness !== 0.5 ) data.metalness = this.metalness;
+		if ( this.roughness !== undefined ) data.roughness = this.roughness;
+		if ( this.metalness !== undefined ) data.metalness = this.metalness;
 
 		if ( this.emissive instanceof THREE.Color ) data.emissive = this.emissive.getHex();
 		if ( this.specular instanceof THREE.Color ) data.specular = this.specular.getHex();
