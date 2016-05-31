0576c7c80c472beb67c855031975aa7708175370
Mr.doob <info@mrdoob.com>
2016-05-31 16:20:13 -0700

    ImageLoader: Fixed data: links breakage.

--- src/loaders/ImageLoader.js
+++ src/loaders/ImageLoader.js
@@ -13,22 +13,29 @@ Object.assign( THREE.ImageLoader.prototype, {
 	load: function ( url, onLoad, onProgress, onError ) {
 
 		var image = document.createElement( 'img' );
+		image.onload = function () {
 
-		var loader = new THREE.XHRLoader( this.manager );
-		loader.setPath( this.path );
-		loader.setResponseType( 'blob' );
-		loader.load( url, function ( blob ) {
+			URL.revokeObjectURL( image.src );
+			if ( onLoad ) onLoad( image );
 
-			image.onload = function () {
+		};
 
-				URL.revokeObjectURL( image.src );
-				if ( onLoad ) onLoad( image );
+		if ( url.indexOf( 'data:' ) === 0 ) {
 
-			};
+			image.src = url;
 
-			image.src = URL.createObjectURL( blob );
+		} else {
 
-		}, onProgress, onError );
+			var loader = new THREE.XHRLoader( this.manager );
+			loader.setPath( this.path );
+			loader.setResponseType( 'blob' );
+			loader.load( url, function ( blob ) {
+
+				image.src = URL.createObjectURL( blob );
+
+			}, onProgress, onError );
+
+		}
 
 		return image;
 
