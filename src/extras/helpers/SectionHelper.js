f24e7073643d63ffcc76e6da73b5d331a882dd1f
Wilt <wilt.johansson@gmail.com>
2016-07-01 11:28:05 +0200

    THREE.SectionHelper class

---
+++ src/extras/helpers/SectionHelper.js
@@ -0,0 +1,33 @@
+/**
+ * Will draw a mesh with backside material or color set to the passed value.
+ * Can be used for faking a sliced geometry.
+ * A demo can be found here:
+ *
+ * @author wilt
+ * Based on THREE.EdgesHelper by WestLangley / http://github.com/WestLangley
+ * @param {THREE.Mesh} object THREE.Mesh whose geometry will be used
+ * @param {string|THREE.Material} hexOrMaterial line color
+ */
+THREE.SectionHelper = function( object, hexOrMaterial ) {
+
+    var material;
+    if ( hexOrMaterial instanceof THREE.MeshBasicMaterial ) {
+
+        material = hexOrMaterial;
+
+    } else {
+
+        var color = ( hexOrMaterial !== undefined ) ? hexOrMaterial : 0xffffff;
+        material = new THREE.MeshBasicMaterial( { color: color, side: THREE.BackSide } );
+
+    }
+
+    THREE.Mesh.call( this, object.geometry, material );
+
+    this.matrix = object.matrixWorld;
+    this.matrixAutoUpdate = false;
+
+};
+
+THREE.SectionHelper.prototype = Object.create( THREE.Mesh.prototype );
+THREE.SectionHelper.prototype.constructor = THREE.SectionHelper;
