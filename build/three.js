ce9022d5375c4153398c04d3dadebea917f626eb
Mr.doob <info@mrdoob.com>
2016-07-08 06:22:14 +0900

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -29654,6 +29654,7 @@ THREE.WebGLShadowMap = function ( _renderer, _lights, _objects, capabilities ) {
 	_lightShadows = _lights.shadows,
 
 	_shadowMapSize = new THREE.Vector2(),
+	_maxShadowMapSize = new THREE.Vector2( capabilities.maxTextureSize, capabilities.maxTextureSize ),
 
 	_lookTarget = new THREE.Vector3(),
 	_lightPositionWorld = new THREE.Vector3(),
@@ -29767,7 +29768,7 @@ THREE.WebGLShadowMap = function ( _renderer, _lights, _objects, capabilities ) {
 			var shadowCamera = shadow.camera;
 
 			_shadowMapSize.copy( shadow.mapSize );
-			_shadowMapSize.min( capabilities.maxTextureSize, capabilities.maxTextureSize );
+			_shadowMapSize.min( _maxShadowMapSize );
 
 			if ( light instanceof THREE.PointLight ) {
 
