e1e70f377276f4fe84a928fdb0085023148a1101
q4w56 <q4w5566@gmail.com>
2016-07-25 14:07:21 +0800

    SceneUtils.attach bug fix

--- src/extras/SceneUtils.js
+++ src/extras/SceneUtils.js
@@ -41,6 +41,52 @@ SceneUtils = {
 		scene.remove( child );
 		parent.add( child );
 
+	},
+
+	updateMatrixWorldOfAncestors: function ( obj ) {
+
+		var ancestors = [];
+
+		for ( var a = obj; a !== null; a = a.parent) ancestors.push( a );
+
+		for ( var a = ancestors.pop() ; a !== undefined ; a = ancestors.pop() ){
+
+			a.matrix.compose( a.position, a.quaternion, a.scale );
+
+			if ( a.parent === null) a.matrixWorld.copy ( a.matrix );
+
+			if ( a.parent !== null) a.matrixWorld.multiplyMatrices( a.parent.matrixWorld, a.matrix );
+
+		}
+
+	},
+
+	detach: function ( child ) {
+
+		if (child.parent == null) return;
+
+		SceneUtils.updateMatrixWorldOfAncestors( child );
+		
+		child.matrix.copy( child.matrixWorld );
+
+		child.matrix.decompose( child.position, child.quaternion, child.scale );
+
+		child.parent.remove( child )
+	
+	},
+
+	attach: function ( child, parent ) {
+
+		SceneUtils.updateMatrixWorldOfAncestors( child );
+
+		SceneUtils.updateMatrixWorldOfAncestors( parent );
+
+		child.matrix.getInverse( parent.matrixWorld ).multiply( child.matrixWorld  );
+		 
+		child.matrix.decompose( child.position, child.quaternion, child.scale );
+
+		parent.add( child )
+
 	}
 
 };
