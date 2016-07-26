e0c8209ba26cd36c18f1eea72500504e30a964a8
Ayman Habib <ahabib@stanford.edu>
2016-07-26 11:13:43 -0700

    Remove debugging lines

--- editor/websocket.js
+++ editor/websocket.js
@@ -9,31 +9,29 @@ var websocket = new WebSocket(wsUri);
 websocket.onerror = function(evt) { onError(evt) };
 
 function onError(evt) {
-    //alert('<span style="color: red;">ERROR:</span> ' + evt.data);
+
 }
-// For testing purposes
-//var output = document.getElementById("output");
+
 websocket.onopen = function(evt) { onOpen(evt) };
 
 function writeToScreen(message) {
-    //output.innerHTML += message + "<br>";
+
 };
 
 function onOpen() {
-    //alert("Connected to " + wsUri);
+
 }
 websocket.onmessage = function(evt) { onMessage(evt) };
 
 function sendText(json) {
     console.log("sending text: " + json);
-    //alert('Sending msg '+json);
     websocket.send(json);
 }
                 
 function onMessage(evt) {
     console.log("received: " + evt.data);
     msg = JSON.parse(evt.data);
-    //alert("received: " + msg);
+
     if (msg.Op=="Select"){
         editor.selectByUuid( msg.UUID, true );
     }
@@ -66,8 +64,8 @@ function onMessage(evt) {
             o.updateMatrix();
         } */
         onWindowResize();
-        //alert("mat after: " + o.matrix);
+
    }
-    //drawImageText(evt.data);
+
 }
 // End test functions
