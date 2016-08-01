f824a62e88d7e72273ca106da2707fbe5f4b0267
Ayman Habib <ahabib@stanford.edu>
2016-08-01 15:07:43 -0700

    Implement view from +/- X,Y,Z directions

--- editor/js/OpenSimToolbar.js
+++ editor/js/OpenSimToolbar.js
@@ -23,26 +23,21 @@ var OpenSimToolbar = function ( editor ) {
 	buttons.add(viewx);
 
 	function viewfromPlusX() {
+		var bbox = computeModelBbox();
 		var center = new THREE.Vector3();
-		var modelObject = editor.scene.getObjectByName('OpenSimModel');
-		var bbox = new THREE.Box3().setFromObject(modelObject);
 		bbox.center(center);
-		//var helper = new THREE.BoundingBoxHelper(modelObject, 0xff0000);
+	    //var helper = new THREE.BoundingBoxHelper(modelObject, 0xff0000);
 		//helper.update();
 	    // If you want a visible bounding box
 		//editor.scene.add(helper);
 	    // create a sphere at new CameraPos
-		var newpos = new THREE.Vector3();
-		newpos.copy(center);
+		var newpos = new THREE.Vector3().copy(center);
 		var fov = editor.camera.fov * (Math.PI / 180);
 	    // Calculate the camera distance
-		var objectSize = Math.max(bbox.max.y - bbox.min.y, bbox.max.x - bbox.min.x);
+		var objectSize = Math.max(bbox.max.y - bbox.min.y, bbox.max.z - bbox.min.z);
 		var distance = Math.abs(objectSize / Math.sin(fov / 2));
 		newpos.x = bbox.max.x + distance;
-		editor.camera.position.copy(newpos);
-		editor.camera.lookAt(center.x, center.y, center.z);
-		//editor.camera.fov = 2 * Math.atan((bbox.max.y - bbox.min.y) / 1000) * (180 / Math.PI);
-		editor.camera.updateProjectionMatrix();
+		updateCamera(newpos, center);
 	};
 
 	var viewminx = new UI.Button('-X').onClick(function () {
@@ -50,26 +45,16 @@ var OpenSimToolbar = function ( editor ) {
 	});
 	buttons.add(viewminx);
 	function viewfromMinusX() {
-	    var modelObject = editor.scene.getObjectByName('OpenSimModel');
-	    var bbox = new THREE.Box3().setFromObject(modelObject);
+	    var bbox = computeModelBbox();
 	    var center = new THREE.Vector3();
 	    bbox.center(center);
-	    //var helper = new THREE.BoundingBoxHelper(modelObject, 0xff0000);
-	    //helper.update();
-	    // If you want a visible bounding box
-	    //editor.scene.add(helper);
-	    // create a sphere at new CameraPos
-	    var newpos = new THREE.Vector3();
-	    newpos.copy(center);
+	    var newpos = new THREE.Vector3().copy(center);
 	    var fov = editor.camera.fov * (Math.PI / 180);
 	    // Calculate the camera distance
-	    var objectSize = Math.max(bbox.max.y - bbox.min.y, bbox.max.x - bbox.min.x);
+	    var objectSize = Math.max(bbox.max.y - bbox.min.y, bbox.max.z - bbox.min.z);
 	    var distance = Math.abs(objectSize / Math.sin(fov / 2));
 	    newpos.x = bbox.min.x - distance;
-	    editor.camera.position.copy(newpos);
-	    editor.camera.lookAt(center.x, center.y, center.z);
-	    //editor.camera.fov = 2 * Math.atan((bbox.max.y - bbox.min.y) / 1000) * (180 / Math.PI);
-	    editor.camera.updateProjectionMatrix();
+	    updateCamera(newpos, center);
 	};
 
 	var viewminy = new UI.Button('-Y').onClick(function () {
@@ -77,26 +62,76 @@ var OpenSimToolbar = function ( editor ) {
 	});
 	buttons.add(viewminy);
 	function viewfromMinusY() {
+	    var bbox = computeModelBbox();
+	    var center = new THREE.Vector3();
+	    bbox.center(center);
+	    var newpos = new THREE.Vector3().copy(center);
+	    var fov = editor.camera.fov * (Math.PI / 180);
+	    // Calculate the camera distance
+	    var objectSize = Math.max(bbox.max.x - bbox.min.x, bbox.max.z - bbox.min.z);
+	    var distance = Math.abs(objectSize / Math.sin(fov / 2));
+	    newpos.y = bbox.min.y - distance;
+	    updateCamera(newpos, center);
 	};
 
 	var viewplusy = new UI.Button('+Y').onClick(function () {
 	    viewfromPlusY();
 	});
 	buttons.add(viewplusy);
-	function viewfromMinusY() {
+	function viewfromPlusY() {
+	    var bbox = computeModelBbox();
+	    var center = new THREE.Vector3();
+	    bbox.center(center);
+	    var newpos = new THREE.Vector3().copy(center);
+	    var fov = editor.camera.fov * (Math.PI / 180);
+	    // Calculate the camera distance
+	    var objectSize = Math.max(bbox.max.x - bbox.min.x, bbox.max.z - bbox.min.z);
+	    var distance = Math.abs(objectSize / Math.sin(fov / 2));
+	    newpos.y = bbox.max.y + distance;
+	    updateCamera(newpos, center);
 	};
 	var viewminz = new UI.Button('-Z').onClick(function () {
 	    viewfromMinusZ();
 	});
 	buttons.add(viewminz);
 	function viewfromMinusZ() {
+	    var bbox = computeModelBbox();
+	    var center = new THREE.Vector3();
+	    bbox.center(center);
+	    var newpos = new THREE.Vector3().copy(center);
+	    var fov = editor.camera.fov * (Math.PI / 180);
+	    // Calculate the camera distance
+	    var objectSize = Math.max(bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y);
+	    var distance = Math.abs(objectSize / Math.sin(fov / 2));
+	    newpos.z = bbox.min.z - distance;
+	    updateCamera(newpos, center);
 	};
 	var viewplusz = new UI.Button('+Z').onClick(function () {
 	    viewfromPlusZ();
 	});
 	buttons.add(viewplusz);
 	function viewfromPlusZ() {
+	    var bbox = computeModelBbox();
+	    var center = new THREE.Vector3();
+	    bbox.center(center);
+	    var newpos = new THREE.Vector3().copy(center);
+	    var fov = editor.camera.fov * (Math.PI / 180);
+	    // Calculate the camera distance
+	    var objectSize = Math.max(bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y);
+	    var distance = Math.abs(objectSize / Math.sin(fov / 2));
+	    newpos.z = bbox.max.z + distance;
+	    updateCamera(newpos, center);
 	};
+	function updateCamera(newposition, viewCenter) {
+	    editor.camera.position.copy(newposition);
+	    editor.camera.lookAt(viewCenter.x, viewCenter.y, viewCenter.z);
+	    editor.camera.updateProjectionMatrix();
+
+	};
+	function computeModelBbox() {
+	    var modelObject = editor.scene.getObjectByName('OpenSimModel');
+	    return (new THREE.Box3().setFromObject(modelObject));
+	}
 	return container;
 
 }
