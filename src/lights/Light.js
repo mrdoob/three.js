df9a9242792bb8a492d6ca3f6160c47d2538904a
satori99 <satori99@users.noreply.github.com>
2016-08-05 01:31:20 +1000

    Serialize and load JSON Light .shadow property (#9416)
    * Serialize and load JSON Light .shadow property
    
    * Add .toJSON() method to LightShadow
    * Light .toJSON() writes .shadow property
    * Parse .shadow property in DirectionLight and Spotlight JSON data
    
    * Remove .castShadow condition when exporting .shadow property to JSON

--- src/lights/Light.js
+++ src/lights/Light.js
@@ -50,6 +50,8 @@ Light.prototype = Object.assign( Object.create( Object3D.prototype ), {
 		if ( this.decay !== undefined ) data.object.decay = this.decay;
 		if ( this.penumbra !== undefined ) data.object.penumbra = this.penumbra;
 
+		if ( this.shadow !== undefined ) data.object.shadow = this.shadow.toJSON();
+
 		return data;
 
 	}
