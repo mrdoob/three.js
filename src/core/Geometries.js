b284377bc104c7ff93b12540c53ff7712eb926e8
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2016-08-18 11:38:08 +1000

    Removed all references to the THREE namespace

---
+++ src/core/Geometries.js
@@ -0,0 +1,4 @@
+export { InstancedBufferGeometry } from './InstancedBufferGeometry.js';
+export { BufferGeometry } from './BufferGeometry.js';
+export { DirectGeometry } from './DirectGeometry.js';
+export { Geometry } from './Geometry.js';
