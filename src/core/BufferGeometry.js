6f9dffc3ac4bd59421ef2967aa7a5929a3530899
Kyle Maxwell <kyle@flux.io>
2016-09-15 17:26:36 -0700

    Moved back to member function called join.

--- src/core/BufferGeometry.js
+++ src/core/BufferGeometry.js
@@ -820,6 +820,93 @@ Object.assign( BufferGeometry.prototype, EventDispatcher.prototype, {
 
 	},
 
+
+	/**
+	 * Join a list of buffer geometries into a new one.
+	 * The old ones (including this) will be disposed, so they can be garbage collected.
+	 * @param  {Array.<THREE.Object3D>} geometries  A list of geometries containing geometry to merge
+	 * @return {THREE.BufferGeometry}       	The merged result.
+	 */
+	join: function ( geomOrList ) {
+
+		var isObject = geomOrList != null && typeof geomOrList === 'object';
+		if ( ! isObject || ( geomOrList.isBufferGeometry === false && geomOrList.constructor !== Array ) ) {
+
+			console.error( 'THREE.BufferGeometry.join(): geometry not an instance of THREE.BufferGeometry or Array.', geomOrList );
+			return this;
+
+		}
+		var geometries;
+		if ( geomOrList.isBufferGeometry ) {
+
+			geometries = [ geomOrList, this ];
+
+		} else {
+
+			// already an  array
+			geometries = geomOrList;
+			geometries.push( this );
+
+		}
+
+		var i, m, geom2;
+
+		var geometry = new THREE.BufferGeometry();
+		var geom1 = this;
+
+		// Split all indexed geometry so we don't need it anymore
+		for ( m = 0; m < geometries.length; m ++ ) {
+
+			geom2 = geometries[ m ];
+			if ( geom2.index ) {
+
+				geom2 = geom2.toNonIndexed();
+				geometries[ m ] = geom2;
+
+			}
+
+		}
+
+		// for each attribute
+		for ( var key in geom1.attributes ) {
+
+			if ( geom1.attributes[ key ].array.constructor !== Float32Array ) continue;
+
+			var data = [];
+			// for each geometry
+			for ( m = 0; m < geometries.length; m ++ ) {
+
+				geom2 = geometries[ m ];
+				if ( ! geom2.attributes[ key ] ) {
+
+					console.error( 'Mismatched geometry attributes: ' + key );
+					return this;
+
+				}
+
+				var attributeArray2 = geom2.attributes[ key ].array;
+				for ( i = 0; i < attributeArray2.length; i ++ ) {
+
+					data.push( attributeArray2[ i ] );
+
+				}
+
+			} // end for each geom
+			geometry.addAttribute( key, new THREE.BufferAttribute( new Float32Array( data ), geom1.attributes[ key ].itemSize ) );
+
+		} // end for each attr
+
+		for ( m = 0; m < geometries.length; m ++ ) {
+
+			geometries[ m ].dispose();
+
+		}
+
+		return geometry;
+
+	},
+
+
 	normalizeNormals: function () {
 
 		var normals = this.attributes.normal.array;
@@ -1043,70 +1130,5 @@ Object.assign( BufferGeometry.prototype, EventDispatcher.prototype, {
 } );
 
 BufferGeometry.MaxIndex = 65535;
-
-
-/**
- * Merge a list of buffer geometries into a new one.
- * The old ones will be disposed, so they can be garbage collected.
- * @param  {Array.<THREE.Object3D>} geometries  A list of geometries containing geometry to merge
- * @return {THREE.BufferGeometry}       	The merged result.
- */
-BufferGeometry.merge = function( geometries ) {
-
-	var i, m, geom2;
-	var geometry = new THREE.BufferGeometry();
-	var geom1 = geometries[ 0 ];
-
-	// Split all indexed geometry so we don't need it anymore
-	for ( m = 0; m < geometries.length; m ++ ) {
-
-		geom2 = geometries[ m ];
-		if ( geom2.index ) {
-
-			geom2 = geom2.toNonIndexed();
-			geometries[ m ] = geom2;
-
-		}
-
-	}
-
-	// for each attribute
-	for ( var key in geom1.attributes ) {
-
-		if ( geom1.attributes[ key ].array.constructor !== Float32Array ) continue;
-
-		var data = [];
-		// for each geometry
-		for ( m = 0; m < geometries.length; m ++ ) {
-
-			geom2 = geometries[ m ];
-			if ( ! geom2.attributes[ key ] ) {
-
-				console.warn( 'Mismatched geometry attributes: ' + key );
-				continue;
-
-			}
-
-			var attributeArray2 = geom2.attributes[ key ].array;
-			for ( i = 0; i < attributeArray2.length; i ++ ) {
-
-				data.push( attributeArray2[ i ] );
-
-			}
-
-		} // end for each geom
-		geometry.addAttribute( key, new THREE.BufferAttribute( new Float32Array( data ), geom1.attributes[ key ].itemSize ) );
-
-	} // end for each attr
-
-	for ( m = 0; m < geometries.length; m ++ ) {
-
-		geometries[ m ].dispose();
-
-	}
-
-	return geometry;
-
-};
 
 export { BufferGeometry };
