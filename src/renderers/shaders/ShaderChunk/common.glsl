e9122654d2803577f96bbf75b72a7555d3c2bf59
Mr.doob <info@mrdoob.com>
2016-11-15 09:03:35 -0800

    Removed gaussianPdf from common.glsl. Fixes #10113

--- src/renderers/shaders/ShaderChunk/common.glsl
+++ src/renderers/shaders/ShaderChunk/common.glsl
@@ -73,12 +73,6 @@ vec3 linePlaneIntersect( in vec3 pointOnLine, in vec3 lineDirection, in vec3 poi
 
 }
 
-float gaussianPdf(in float x, in float sigma) {
-
-	return 0.39894 * exp( -0.5 * x * x/( sigma * sigma))/sigma;
-
-}
-
 mat3 transpose( const in mat3 v ) {
 
     mat3 tmp;
