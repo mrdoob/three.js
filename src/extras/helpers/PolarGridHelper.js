cb1aa41d8ba0de5b9a19ec7f70f58103d8d7b6d4
Mr.doob <info@mrdoob.com>
2016-11-22 16:26:27 -0800

    PolarGridHelper clean up.

--- src/extras/helpers/PolarGridHelper.js
+++ src/extras/helpers/PolarGridHelper.js
@@ -13,79 +13,79 @@ import { Color } from '../../math/Color';
 
 function PolarGridHelper( radius, radials, circles, divisions, color1, color2 ) {
 
-    radius = radius || 10;
-    radials = radials || 16;
-    circles = circles || 8;
-    divisions = divisions || 64;
-    color1 = new Color( color1 !== undefined ? color1 : 0x444444 );
-    color2 = new Color( color2 !== undefined ? color2 : 0x888888 );
+	radius = radius || 10;
+	radials = radials || 16;
+	circles = circles || 8;
+	divisions = divisions || 64;
+	color1 = new Color( color1 !== undefined ? color1 : 0x444444 );
+	color2 = new Color( color2 !== undefined ? color2 : 0x888888 );
 
-    var vertices = [];
-    var colors = [];
+	var vertices = [];
+	var colors = [];
 
-    var x, z;
-    var v, i, j, r, color;
+	var x, z;
+	var v, i, j, r, color;
 
-    // create the radials
+	// create the radials
 
-    for ( i = 0; i <= radials; i++ ) {
+	for ( i = 0; i <= radials; i ++ ) {
 
-        v = ( i / radials ) * ( Math.PI * 2 );
+		v = ( i / radials ) * ( Math.PI * 2 );
 
-        x = Math.sin( v ) * radius;
-        z = Math.cos( v ) * radius;
+		x = Math.sin( v ) * radius;
+		z = Math.cos( v ) * radius;
 
-        vertices.push( 0, 0, 0 );
-        vertices.push( x, 0, z );
+		vertices.push( 0, 0, 0 );
+		vertices.push( x, 0, z );
 
-        color = ( i & 1 ) ? color1 : color2;
+		color = ( i & 1 ) ? color1 : color2;
 
-        colors.push( color.r, color.g, color.b );
-        colors.push( color.r, color.g, color.b );
+		colors.push( color.r, color.g, color.b );
+		colors.push( color.r, color.g, color.b );
 
-    }
+	}
 
-    // create the circles
+	// create the circles
 
-    for ( i = 0; i <= circles; i++ ) {
+	for ( i = 0; i <= circles; i ++ ) {
 
-        color = ( i & 1 ) ? color1 : color2;
+		color = ( i & 1 ) ? color1 : color2;
 
-        r = radius - ( radius / circles * i )
+		r = radius - ( radius / circles * i );
 
-        for ( j = 0; j < divisions; j ++ ) {
+		for ( j = 0; j < divisions; j ++ ) {
 
-            // first vertex
+			// first vertex
 
-            v = ( j / divisions ) * ( Math.PI * 2 );
+			v = ( j / divisions ) * ( Math.PI * 2 );
 
-            x = Math.sin( v ) * r;
-            z = Math.cos( v ) * r;
+			x = Math.sin( v ) * r;
+			z = Math.cos( v ) * r;
 
-            vertices.push( x, 0, z );
-            colors.push( color.r, color.g, color.b );
+			vertices.push( x, 0, z );
+			colors.push( color.r, color.g, color.b );
 
-            // second vertex
+			// second vertex
 
-            v = ( ( j + 1 ) / divisions ) * ( Math.PI * 2 );
+			v = ( ( j + 1 ) / divisions ) * ( Math.PI * 2 );
 
-            x = Math.sin( v ) * r;
-            z = Math.cos( v ) * r;
+			x = Math.sin( v ) * r;
+			z = Math.cos( v ) * r;
 
-            vertices.push( x, 0, z );
-            colors.push( color.r, color.g, color.b );
+			vertices.push( x, 0, z );
+			colors.push( color.r, color.g, color.b );
 
-        }
+		}
 
-    }
+	}
 
-    var geometry = new BufferGeometry();
-    geometry.addAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
-    geometry.addAttribute( 'color', new Float32BufferAttribute( colors, 3 ) );
+	var geometry = new BufferGeometry();
+	geometry.addAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
+	geometry.addAttribute( 'color', new Float32BufferAttribute( colors, 3 ) );
 
-    var material = new LineBasicMaterial( { vertexColors: VertexColors } );
+	var material = new LineBasicMaterial( { vertexColors: VertexColors } );
 
-    LineSegments.call( this, geometry, material );
+	LineSegments.call( this, geometry, material );
 
 }
 
