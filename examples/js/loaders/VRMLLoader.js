44b402878e70634e042124340d87683488b07e55
Bart McLeod <mcleod@spaceweb.nl>
2016-12-05 18:44:47 +0100

    integrate VrmlParser with ThreeJs using the VRMLLoader

--- examples/js/loaders/VRMLLoader.js
+++ examples/js/loaders/VRMLLoader.js
@@ -10,51 +10,58 @@
  * @param VrmlParser.Renderer.ThreeJs renderer
  * @constructor
  */
-THREE.VRMLLoader = function (parser, manager) {
+THREE.VRMLLoader = function (parser, manager, debug) {
 
-  this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
-  this.parser = parser;
-
+	this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
+	this.parser  = parser;
+	this.debug   = debug;
 };
 
 THREE.VRMLLoader.prototype = {
 
-  constructor: THREE.VRMLLoader,
+	constructor: THREE.VRMLLoader,
 
-  parse: function (data) {
-    try {
-      var tree = this.parser.parse(data);
-      console.log(tree);
-      return tree;
-    } catch ( e ) {
-      console.log('Exception with message ' + e.message);
+	log: function (obj) {
+		if ( this.debug ) {
+			console.log(obj);
+		}
+	},
 
-      if ( undefined !== e.location ) {
-        console.log('Exception at location start: offset: ' + e.location.start.offset + ' line: ' + e.location.start.line + ' column: ' + e.location.start.column);
-        console.log('Exception at location end: offset: ' + e.location.end.offset + ' line: ' + e.location.end.line + ' column: ' + e.location.end.column);
-      }
+	parse: function (data) {
+		try {
+			var tree = this.parser.parse(data);
 
-      return;
-    }
-  },
+			this.log(tree);
+			return tree;
+		} catch ( e ) {
+			this.log('Exception with message ' + e.message);
 
-  load: function (url, onLoad, onProgress, onError) {
+			if ( undefined !== e.location ) {
+				this.log('Exception at location start: offset: ' + e.location.start.offset + ' line: ' + e.location.start.line + ' column: ' + e.location.start.column);
+				this.log('Exception at location end: offset: ' + e.location.end.offset + ' line: ' + e.location.end.line + ' column: ' + e.location.end.column);
+			}
 
-    var scope = this;
+			return;
+		}
+	},
 
-    var loader = new THREE.XHRLoader(this.manager);
-    loader.load(url, function (text) {
+	load: function (url, onLoad, onProgress, onError) {
 
-      onLoad(scope.parse(text));
+		var scope = this;
 
-    }, onProgress, onError);
+		var loader = new THREE.FileLoader(this.manager);
+		loader.load(url, function (text) {
 
-  },
+			onLoad(scope.parse(text));
 
-  setCrossOrigin: function (value) {
+		}, onProgress, onError);
 
-    this.crossOrigin = value;
+	},
 
-  },
+	setCrossOrigin: function (value) {
+
+		this.crossOrigin = value;
+
+	},
 
 };
