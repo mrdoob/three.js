5eb5486e5a137b67dcbe509310aacb178f4296d1
nicolasrannou <npm.rannou@gmail.com>
2016-12-05 13:17:09 +0100

    Allow user to define state in trackball constructor

--- examples/js/controls/OrthographicTrackballControls.js
+++ examples/js/controls/OrthographicTrackballControls.js
@@ -5,10 +5,12 @@
  * @author Max Smolens / https://github.com/msmolens
  */
 
-THREE.OrthographicTrackballControls = function ( object, domElement ) {
+THREE.OrthographicTrackballControls = function ( object, domElement, state ) {
 
 	var _this = this;
-	var STATE = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
+
+	var defaultState = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
+	var STATE = ( typeof state !== undefined ) ? state : defaultState;
 
 	this.object = object;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
--- examples/js/controls/TrackballControls.js
+++ examples/js/controls/TrackballControls.js
@@ -5,10 +5,11 @@
  * @author Luca Antiga 	/ http://lantiga.github.io
  */
 
-THREE.TrackballControls = function ( object, domElement ) {
+THREE.TrackballControls = function ( object, domElement, state ) {
 
 	var _this = this;
-	var STATE = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
+	var defaultState = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
+	var STATE = ( typeof state !== undefined ) ? state : defaultState;
 
 	this.object = object;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
