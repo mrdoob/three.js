c7e6ea127c2137337d02623a569195e55ecdbbf2
aardgoose <angus.sawyer@gmail.com>
2016-12-07 18:32:55 +0000

    make value versions - value for plain numbers

--- src/core/ParameterSource.js
+++ src/core/ParameterSource.js
@@ -1,8 +1,30 @@
 
-function MaterialParameter( value ) {
+function MaterialParameter( value, func ) {
 
 	this.value = value;
-	this.version = ++MaterialParameter.version;
+
+	if ( typeof value === "number" ) {
+
+		this.version = value;
+
+	} else {
+
+		this.version = ++MaterialParameter.version;
+	}
+
+	if ( func === undefined ) {
+
+		Object.defineProperty( this, 'rendererValue', {
+			value: this.value
+		} );
+
+	} else {
+
+		Object.defineProperty( this, 'rendererValue', {
+			value: func( this.value )
+		} );
+
+	}
 
 }
 
@@ -14,22 +36,34 @@ MaterialParameter.prototype = {
 
 	setValue: function ( value ) {
 
-//		console.log(" setting value ", value );
+		this.value = value;
+		this.version = value;
+
+	},
+
+	getValue: function () {
+
+		return this.value;
+
+	},
+
+	setObjectValue: function ( value ) {
 
 		this.value = value;
 		this.version = ++MaterialParameter.version;
 
 	},
 
-	getValue: function () {
+	getObjectValue: function () {
 
 		this.version = ++MaterialParameter.version;
 
 		return this.value;
 
-	}
+	},
 
 }
+
 
 function ParameterSource() {}
 
@@ -40,6 +74,51 @@ Object.assign( ParameterSource.prototype, {
 		if ( this._parameters === undefined ) this._parameters = {};
 
 		var parameter = new MaterialParameter( value );
+		var shaderName = alias === undefined ? name : alias;
+
+		this._parameters[ shaderName ] = parameter;
+
+		var get;
+
+		if ( typeof value === "number" ) {
+
+			Object.defineProperty( this, name, {
+				set: function( value ) { parameter.setValue( value ) },
+				get: function () { return parameter.getValue() }
+			} );
+
+		} else {
+
+			Object.defineProperty( this, name, {
+				set: function( value ) { parameter.setObjectValue( value ) },
+				get: function () { return parameter.getObjectValue() }
+			} );
+
+		}
+
+
+	},
+
+	addParameterRO: function ( name, value, alias ) {
+
+		if ( this._parameters === undefined ) this._parameters = {};
+
+		var parameter = new MaterialParameter( value );
+		var shaderName = alias === undefined ? name : alias;
+
+		this._parameters[ shaderName ] = parameter;
+
+		Object.defineProperty( this, name, {
+			get: function() { return parameter.getObjectValue() }
+		} );
+
+	},
+
+	addDerivedParameter: function ( name, value, func ) {
+
+		if ( this._parameters === undefined ) this._parameters = {};
+
+		var parameter = new MaterialParameter( value, func );
 		var shaderName = alias === undefined ? name : alias;
 
 		this._parameters[ shaderName ] = parameter;
