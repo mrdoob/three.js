79701264d33b89521d08ab348140b956de360f1a
Mr.doob <info@mrdoob.com>
2016-12-15 15:53:33 +0900

    Revert "Fix Loading an SVG with TextureLoader"

--- src/loaders/FileLoader.js
+++ src/loaders/FileLoader.js
@@ -184,7 +184,7 @@ Object.assign( FileLoader.prototype, {
 			if ( this.responseType !== undefined ) request.responseType = this.responseType;
 			if ( this.withCredentials !== undefined ) request.withCredentials = this.withCredentials;
 
-			if ( this.mimeType && request.overrideMimeType ) request.overrideMimeType( this.mimeType );
+			if ( request.overrideMimeType ) request.overrideMimeType( this.mimeType !== undefined ? this.mimeType : 'text/plain' );
 
 			request.send( null );
 
