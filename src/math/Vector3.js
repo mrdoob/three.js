b23734b85a635beb3fdb93764263c181f1ea2cf5
Franklin Ta <fta2012@gmail.com>
2016-12-20 14:17:25 -0800

    Fix typo, add a unit test

--- src/math/Vector3.js
+++ src/math/Vector3.js
@@ -299,9 +299,9 @@ Vector3.prototype = {
 		this.x = e[ 0 ] * x + e[ 4 ] * y + e[ 8 ]  * z + e[ 12 ];
 		this.y = e[ 1 ] * x + e[ 5 ] * y + e[ 9 ]  * z + e[ 13 ];
 		this.z = e[ 2 ] * x + e[ 6 ] * y + e[ 10 ] * z + e[ 14 ];
-		var w = e[ 3 ] * x + e[ 7 ] * y + e[ 11 ] * z + e[ 15 ];
+		var w =  e[ 3 ] * x + e[ 7 ] * y + e[ 11 ] * z + e[ 15 ];
 
-		return this.divideScalar( 1 / w );
+		return this.divideScalar( w );
 
 	},
 
