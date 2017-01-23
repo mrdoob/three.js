9ee0f7dddb7b38f2706acb450d2539cd5dc615ee
Tristan VALCKE <tristan.valcke@gmail.com>
2017-01-23 22:04:05 +0100

    Fix Texture accessors

--- src/textures/Texture.js
+++ src/textures/Texture.js
@@ -59,17 +59,17 @@ function Texture( image, mapping, wrapS, wrapT, magFilter, minFilter, format, ty
 Texture.DEFAULT_IMAGE = undefined;
 Texture.DEFAULT_MAPPING = UVMapping;
 
+Object.defineProperty( Texture.prototype, "needsUpdate", {
+
+	set: function(value) { if ( value === true ) this.version ++; }
+
+});
+
 Object.assign( Texture.prototype, EventDispatcher.prototype, {
 
 	constructor: Texture,
 
 	isTexture: true,
-
-	set needsUpdate( value ) {
-
-		if ( value === true ) this.version ++;
-
-	},
 
 	clone: function () {
 
