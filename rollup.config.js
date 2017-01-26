e2b027637e69dfb421bf2cffe7ddef10d9ebe17a
Tristan VALCKE <tristan.valcke@gmail.com>
2017-01-26 18:30:13 +0100

    Remove rollup.unit.config.js file and implement conditional environment in rollup.config.js (Available env are "production" and "test" for the moment)

--- rollup.config.js
+++ rollup.config.js
@@ -24,21 +24,66 @@ function glsl() {
 }
 
 export default {
-	entry: 'src/Three.js',
+
+	entry: (function () {
+
+		var env = process.env.BUILD;
+
+		if( env === 'production' ) {
+
+			return 'src/Three.js';
+
+		} else if( env === 'test' ){
+
+			return 'test/Three.Unit.js';
+
+		} else {
+
+			console.error("Invalid BUILD environment variable: " + env);
+			return null;
+
+		}
+
+	})(),
 	indent: '\t',
 	plugins: [
 		glsl()
 	],
 	// sourceMap: true,
-	targets: [
-		{
-			format: 'umd',
-			moduleName: 'THREE',
-			dest: 'build/three.js'
-		},
-		{
-			format: 'es',
-			dest: 'build/three.module.js'
+	targets: (function () {
+
+		var env = process.env.BUILD;
+
+		if( env === 'production' ) {
+
+			return [
+				{
+					format: 'umd',
+					moduleName: 'THREE',
+					dest: 'build/three.js'
+				},
+				{
+					format: 'es',
+					dest: 'build/three.module.js'
+				}
+			];
+
+		} else if( env === 'test' ){
+
+			return [
+				{
+					format: 'umd',
+					moduleName: 'THREE',
+					dest: 'test/unit/three.unit.js'
+				}
+			];
+
+		} else {
+
+			console.error("Invalid BUILD environment variable: " + env);
+			return null;
+
 		}
-	]
+
+	})()
 };
