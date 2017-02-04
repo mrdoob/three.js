cd140d94cd9961cc1557645adb686bf4942f6950
Steven Vergenz <vergenzs@gmail.com>
2017-02-04 14:03:32 -0800

    Replace magic numbers with descriptive var names

--- examples/js/loaders/GLTFLoader.js
+++ examples/js/loaders/GLTFLoader.js
@@ -1090,10 +1090,10 @@ THREE.GLTFLoader = ( function () {
 
 								var sampler = json.samplers[ texture.sampler ];
 
-								_texture.magFilter = WEBGL_FILTERS[ sampler.magFilter || 9729 ];
-								_texture.minFilter = WEBGL_FILTERS[ sampler.minFilter || 9986 ];
-								_texture.wrapS = WEBGL_WRAPPINGS[ sampler.wrapS || 10497 ];
-								_texture.wrapT = WEBGL_WRAPPINGS[ sampler.wrapT || 10497 ];
+								_texture.magFilter = WEBGL_FILTERS[ sampler.magFilter ] || THREE.LinearFilter;
+								_texture.minFilter = WEBGL_FILTERS[ sampler.minFilter ] || THREE.NearestMipMapLinearFilter;
+								_texture.wrapS = WEBGL_WRAPPINGS[ sampler.wrapS ] || THREE.RepeatWrapping;
+								_texture.wrapT = WEBGL_WRAPPINGS[ sampler.wrapT ] || THREE.RepeatWrapping;
 
 							}
 
