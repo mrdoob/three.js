ea51ba73630970f1e66d988dd9556e9396c3b3c4
Tristan VALCKE <tristan.valcke@gmail.com>
2017-02-11 22:52:56 +0100

    Normalize /curve/LineCurve, and apply MrDoob eslint style

--- src/extras/curves/LineCurve.js
+++ src/extras/curves/LineCurve.js
@@ -1,6 +1,5 @@
 import { Curve } from '../core/Curve';
 
-
 function LineCurve( v1, v2 ) {
 
 	this.v1 = v1;
@@ -8,41 +7,43 @@ function LineCurve( v1, v2 ) {
 
 }
 
-LineCurve.prototype = Object.create( Curve.prototype );
-LineCurve.prototype.constructor = LineCurve;
+LineCurve.prototype = Object.assign( Object.create( Curve.prototype ), {
 
-LineCurve.prototype.isLineCurve = true;
+	constructor: LineCurve,
 
-LineCurve.prototype.getPoint = function ( t ) {
+	isLineCurve: true,
 
-	if ( t === 1 ) {
+	getPoint: function ( t ) {
 
-		return this.v2.clone();
+		if ( t === 1 ) {
+
+			return this.v2.clone();
+
+		}
+
+		var point = this.v2.clone().sub( this.v1 );
+		point.multiplyScalar( t ).add( this.v1 );
+
+		return point;
+
+	},
+
+	// Line curve is linear, so we can overwrite default getPointAt
+
+	getPointAt: function ( u ) {
+
+		return this.getPoint( u );
+
+	},
+
+	getTangent: function () {
+
+		var tangent = this.v2.clone().sub( this.v1 );
+
+		return tangent.normalize();
 
 	}
 
-	var point = this.v2.clone().sub( this.v1 );
-	point.multiplyScalar( t ).add( this.v1 );
-
-	return point;
-
-};
-
-// Line curve is linear, so we can overwrite default getPointAt
-
-LineCurve.prototype.getPointAt = function ( u ) {
-
-	return this.getPoint( u );
-
-};
-
-LineCurve.prototype.getTangent = function ( t ) {
-
-	var tangent = this.v2.clone().sub( this.v1 );
-
-	return tangent.normalize();
-
-};
-
+} );
 
 export { LineCurve };
