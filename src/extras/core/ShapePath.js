028534c9a61e5573015ef295606bc340fa100744
Tristan VALCKE <tristan.valcke@gmail.com>
2017-02-11 22:49:23 +0100

    Normalize /core/ShapePath, and apply MrDoob eslint style

--- src/extras/core/ShapePath.js
+++ src/extras/core/ShapePath.js
@@ -80,7 +80,7 @@ Object.assign( ShapePath.prototype, {
 			var inside = false;
 			for ( var p = polyLen - 1, q = 0; q < polyLen; p = q ++ ) {
 
-				var edgeLowPt  = inPolygon[ p ];
+				var edgeLowPt = inPolygon[ p ];
 				var edgeHighPt = inPolygon[ q ];
 
 				var edgeDx = edgeHighPt.x - edgeLowPt.x;
@@ -91,22 +91,40 @@ Object.assign( ShapePath.prototype, {
 					// not parallel
 					if ( edgeDy < 0 ) {
 
-						edgeLowPt  = inPolygon[ q ]; edgeDx = - edgeDx;
-						edgeHighPt = inPolygon[ p ]; edgeDy = - edgeDy;
+						edgeLowPt = inPolygon[ q ];
+						edgeDx = - edgeDx;
+						edgeHighPt = inPolygon[ p ];
+						edgeDy = - edgeDy;
 
 					}
-					if ( ( inPt.y < edgeLowPt.y ) || ( inPt.y > edgeHighPt.y ) ) 		continue;
+					if ( ( inPt.y < edgeLowPt.y ) || ( inPt.y > edgeHighPt.y ) ) {
+
+						continue;
+
+					}
 
 					if ( inPt.y === edgeLowPt.y ) {
 
-						if ( inPt.x === edgeLowPt.x )		return	true;		// inPt is on contour ?
+						if ( inPt.x === edgeLowPt.x ) {
+
+							return true;
+
+						}		// inPt is on contour ?
 						// continue;				// no intersection or edgeLowPt => doesn't count !!!
 
 					} else {
 
 						var perpEdge = edgeDy * ( inPt.x - edgeLowPt.x ) - edgeDx * ( inPt.y - edgeLowPt.y );
-						if ( perpEdge === 0 )				return	true;		// inPt is on contour ?
-						if ( perpEdge < 0 ) 				continue;
+						if ( perpEdge === 0 ) {
+
+							return true;
+
+						}		// inPt is on contour ?
+						if ( perpEdge < 0 ) {
+
+							continue;
+
+						}
 						inside = ! inside;		// true intersection left of inPt
 
 					}
@@ -114,27 +132,42 @@ Object.assign( ShapePath.prototype, {
 				} else {
 
 					// parallel or collinear
-					if ( inPt.y !== edgeLowPt.y ) 		continue;			// parallel
+					if ( inPt.y !== edgeLowPt.y ) {
+
+						continue;
+
+					}			// parallel
 					// edge lies on the same horizontal line as inPt
 					if ( ( ( edgeHighPt.x <= inPt.x ) && ( inPt.x <= edgeLowPt.x ) ) ||
-						 ( ( edgeLowPt.x <= inPt.x ) && ( inPt.x <= edgeHighPt.x ) ) )		return	true;	// inPt: Point on contour !
+						( ( edgeLowPt.x <= inPt.x ) && ( inPt.x <= edgeHighPt.x ) ) ) {
+
+						return true;
+
+					}	// inPt: Point on contour !
 					// continue;
 
 				}
 
 			}
 
-			return	inside;
+			return inside;
 
 		}
 
 		var isClockWise = ShapeUtils.isClockWise;
 
 		var subPaths = this.subPaths;
-		if ( subPaths.length === 0 ) return [];
+		if ( subPaths.length === 0 ) {
 
-		if ( noHoles === true )	return	toShapesNoHoles( subPaths );
+			return [];
 
+		}
+
+		if ( noHoles === true ) {
+
+			return toShapesNoHoles( subPaths );
+
+		}
 
 		var solid, tmpPath, tmpShape, shapes = [];
 
@@ -171,19 +204,33 @@ Object.assign( ShapePath.prototype, {
 
 			if ( solid ) {
 
-				if ( ( ! holesFirst ) && ( newShapes[ mainIdx ] ) )	mainIdx ++;
+				if ( ( ! holesFirst ) && ( newShapes[ mainIdx ] ) ) {
 
-				newShapes[ mainIdx ] = { s: new Shape(), p: tmpPoints };
+					mainIdx ++;
+
+				}
+
+				newShapes[ mainIdx ] = {
+					s: new Shape(),
+					p: tmpPoints
+				};
 				newShapes[ mainIdx ].s.curves = tmpPath.curves;
 
-				if ( holesFirst )	mainIdx ++;
+				if ( holesFirst ) {
+
+					mainIdx ++;
+
+				}
 				newShapeHoles[ mainIdx ] = [];
 
 				//console.log('cw', i);
 
 			} else {
 
-				newShapeHoles[ mainIdx ].push( { h: tmpPath, p: tmpPoints[ 0 ] } );
+				newShapeHoles[ mainIdx ].push( {
+					h: tmpPath,
+					p: tmpPoints[ 0 ]
+				} );
 
 				//console.log('ccw', i);
 
@@ -192,8 +239,11 @@ Object.assign( ShapePath.prototype, {
 		}
 
 		// only Holes? -> probably all Shapes with wrong orientation
-		if ( ! newShapes[ 0 ] )	return	toShapesNoHoles( subPaths );
+		if ( ! newShapes[ 0 ] ) {
 
+			return toShapesNoHoles( subPaths );
+
+		}
 
 		if ( newShapes.length > 1 ) {
 
@@ -219,7 +269,15 @@ Object.assign( ShapePath.prototype, {
 
 						if ( isPointInsidePolygon( ho.p, newShapes[ s2Idx ].p ) ) {
 
-							if ( sIdx !== s2Idx )	toChange.push( { froms: sIdx, tos: s2Idx, hole: hIdx } );
+							if ( sIdx !== s2Idx ) {
+
+								toChange.push( {
+									froms: sIdx,
+									tos: s2Idx,
+									hole: hIdx
+								} );
+
+							}
 							if ( hole_unassigned ) {
 
 								hole_unassigned = false;
@@ -247,7 +305,11 @@ Object.assign( ShapePath.prototype, {
 			if ( toChange.length > 0 ) {
 
 				// console.log("to change: ", toChange);
-				if ( ! ambiguous )	newShapeHoles = betterShapeHoles;
+				if ( ! ambiguous ) {
+
+					newShapeHoles = betterShapeHoles;
+
+				}
 
 			}
 
@@ -276,6 +338,5 @@ Object.assign( ShapePath.prototype, {
 	}
 
 } );
-
 
 export { ShapePath };
