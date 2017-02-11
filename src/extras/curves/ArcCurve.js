0ac17edbe39b521469cf68b93812c7ac616ff19b
Tristan VALCKE <tristan.valcke@gmail.com>
2017-02-11 22:49:54 +0100

    Normalize /curve/ArcCurve, and apply MrDoob eslint style

--- src/extras/curves/ArcCurve.js
+++ src/extras/curves/ArcCurve.js
@@ -1,6 +1,5 @@
 import { EllipseCurve } from './EllipseCurve';
 
-
 function ArcCurve( aX, aY, aRadius, aStartAngle, aEndAngle, aClockwise ) {
 
 	EllipseCurve.call( this, aX, aY, aRadius, aRadius, aStartAngle, aEndAngle, aClockwise );
@@ -9,6 +8,5 @@ function ArcCurve( aX, aY, aRadius, aStartAngle, aEndAngle, aClockwise ) {
 
 ArcCurve.prototype = Object.create( EllipseCurve.prototype );
 ArcCurve.prototype.constructor = ArcCurve;
-
 
 export { ArcCurve };
