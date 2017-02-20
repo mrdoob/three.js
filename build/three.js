ff0c85921f6748de03a9dda6fe5e9d8ea1100af9
Mr.doob <info@mrdoob.com>
2017-02-19 21:08:29 -0800

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -29590,18 +29590,17 @@
 
 		load: function ( url, onLoad, onProgress, onError ) {
 
-			var texture = new Texture();
-
 			var loader = new ImageLoader( this.manager );
 			loader.setCrossOrigin( this.crossOrigin );
 			loader.setPath( this.path );
-			loader.load( url, function ( image ) {
+
+			var texture = new Texture();
+			texture.image = loader.load( url, function () {
 
 				// JPEGs can't have an alpha channel, so memory can be saved by storing them as RGB.
 				var isJPEG = url.search( /\.(jpg|jpeg)$/ ) > 0 || url.search( /^data\:image\/jpeg/ ) === 0;
 
 				texture.format = isJPEG ? RGBFormat : RGBAFormat;
-				texture.image = image;
 				texture.needsUpdate = true;
 
 				if ( onLoad !== undefined ) {
@@ -30025,7 +30024,9 @@
 
 			if ( AnimationUtils.isTypedArray( array ) ) {
 
-				return new array.constructor( array.subarray( from, to ) );
+				// in ios9 array.subarray(from, undefined) will return empty array
+				// but array.subarray(from) or array.subarray(from, len) is correct
+				return new array.constructor( array.subarray( from, to || array.length ) );
 
 			}
 
