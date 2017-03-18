0d5f58ab2aceafa5144a7c7d807ecb14866a492b
Mugen87 <michael.herzog@human-interactive.org>
2017-03-18 18:48:56 +0100

    MeshLine: More comments and better variable names

--- src/renderers/shaders/ShaderChunk/meshline_pars_vertex.glsl
+++ src/renderers/shaders/ShaderChunk/meshline_pars_vertex.glsl
@@ -3,11 +3,17 @@
 	uniform vec2 resolution;
 	uniform float lineWidth;
 
-	vec2 convertToNDC( vec4 coord, float aspect ) {
+	vec2 convertToNDC( vec4 clipCoords, float aspect ) {
 
-		vec2 ndc = coord.xy / coord.w;
-		ndc.x *= aspect;
-		return ndc;
+		// the prespective divide does the acutal conversion
+
+		vec2 ndcCoords = clipCoords.xy / clipCoords.w;
+
+		// correct for aspect ratio
+
+		ndcCoords.x *= aspect;
+
+		return ndcCoords;
 
 	}
 
--- src/renderers/shaders/ShaderChunk/meshline_vertex.glsl
+++ src/renderers/shaders/ShaderChunk/meshline_vertex.glsl
@@ -1,13 +1,20 @@
 #ifdef USE_MESHLINE
 
+	// the following code creates so called 'Screen-Space Projected Lines'
+	// see https://mattdesl.svbtle.com/drawing-lines-is-hard#screenspace-projected-lines_2
+
 	float aspect = resolution.x / resolution.y;
 	float pixelWidthRatio = 1.0 / ( resolution.x * projectionMatrix[ 0 ][ 0 ] );
 
 	mat4 m = projectionMatrix * modelViewMatrix;
 
+	// calculate positions in clip space
+
 	vec4 currentPosition = m * vec4( position, 1.0 );
-	vec4 prevPosition =  m * vec4( prev, 1.0 );
-	vec4 nextPosition =  m * vec4( next, 1.0 );
+	vec4 prevPosition = m * vec4( prev, 1.0 );
+	vec4 nextPosition = m * vec4( next, 1.0 );
+
+	// convert our points from clip space to NDC space
 
 	vec2 c = convertToNDC( currentPosition, aspect );
 	vec2 p = convertToNDC( prevPosition, aspect );
@@ -15,6 +22,8 @@
 
 	float pixelWidth = currentPosition.w * pixelWidthRatio;
 	float w = 1.8 * lineWidth * width * pixelWidth;
+
+	// compute direction vector for extrusion
 
 	vec2 dir;
 
@@ -37,6 +46,8 @@
 	vec2 offsetDirection = vec2( - dir.y, dir.x );
 	offsetDirection.x /= aspect;
 	offsetDirection *= side;
+
+	// perfrom extrusion from the center point
 
 	vec2 offset = offsetDirection * 0.5 * w;
 	currentPosition.xy += offset;
