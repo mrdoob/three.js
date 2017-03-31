9ec395da59da98220738b8fd3bf27e707c8aa028
Jost Schmithals <jost.schmithals@musikalisches-zentrum-hx.de>
2017-03-31 11:19:42 +0200

    tab indentation

--- docs/page.js
+++ docs/page.js
@@ -1,25 +1,25 @@
 if ( !window.frameElement ) {
 
-    // If the page is not yet displayed as an iframe of the index page (navigation panel/working links),
-    // redirect to the index page (using the current URL without extension as the new fragment).
-    // If this URL itself has a fragment, append it with a dot (since '#' in an URL fragment is not allowed).
+	// If the page is not yet displayed as an iframe of the index page (navigation panel/working links),
+	// redirect to the index page (using the current URL without extension as the new fragment).
+	// If this URL itself has a fragment, append it with a dot (since '#' in an URL fragment is not allowed).
 
-    var href = window.location.href;
-    var splitIndex = href.lastIndexOf( '/docs/' ) + 6;
-    var docsBaseURL = href.substr( 0, splitIndex );
+	var href = window.location.href;
+	var splitIndex = href.lastIndexOf( '/docs/' ) + 6;
+	var docsBaseURL = href.substr( 0, splitIndex );
 
-    var hash = window.location.hash;
+	var hash = window.location.hash;
 
-    if ( hash !== '' ) {
+	if ( hash !== '' ) {
 
-        href = href.replace( hash, '' );
-        hash = hash.replace( '#', '.' );
+		href = href.replace( hash, '' );
+		hash = hash.replace( '#', '.' );
 
-    }
+	}
 
-    var pathSnippet = href.slice( splitIndex, -5 );
+	var pathSnippet = href.slice( splitIndex, -5 );
 
-    window.location.replace( docsBaseURL + '#' + pathSnippet + hash );
+	window.location.replace( docsBaseURL + '#' + pathSnippet + hash );
 
 }
 
