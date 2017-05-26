9898e0269a05fdcaf85ab1838d0914c9d7952963
WestLangley <dat1234@gmail.com>
2017-05-26 00:02:32 -0400

    Box3Helper: .update() -> .onBeforeRender()

--- src/helpers/Box3Helper.js
+++ src/helpers/Box3Helper.js
@@ -2,6 +2,7 @@ import { Box3 } from '../math/Box3';
 import { LineSegments } from '../objects/LineSegments';
 import { LineBasicMaterial } from '../materials/LineBasicMaterial';
 import { BufferAttribute } from '../core/BufferAttribute';
+import { Float32BufferAttribute } from '../core/BufferAttribute';
 import { BufferGeometry } from '../core/BufferGeometry';
 
 /**
@@ -18,45 +19,37 @@ function Box3Helper( box, hex ) {
 	var color = ( hex !== undefined ) ? hex : 0xffff00;
 
 	var indices = new Uint16Array( [ 0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7 ] );
-	var positions = new Float32Array( 8 * 3 );
+
+	var positions = [ 1, 1, 1, - 1, 1, 1, - 1, - 1, 1, 1, - 1, 1, 1, 1, - 1, - 1, 1, - 1, - 1, - 1, - 1, 1, - 1, - 1 ];
 
 	var geometry = new BufferGeometry();
+
 	geometry.setIndex( new BufferAttribute( indices, 1 ) );
-	geometry.addAttribute( 'position', new BufferAttribute( positions, 3 ) );
+
+	geometry.addAttribute( 'position', new Float32BufferAttribute( positions, 3 ) );
 
 	LineSegments.call( this, geometry, new LineBasicMaterial( { color: color } ) );
 
-	this.update();
+	this.geometry.computeBoundingSphere();
+
+	this.onBeforeRender();
 
 }
 
 Box3Helper.prototype = Object.create( LineSegments.prototype );
 Box3Helper.prototype.constructor = Box3Helper;
 
-Box3Helper.prototype.update = function () {
+Box3Helper.prototype.onBeforeRender = function () {
 
 	var box = this.box;
 
 	if ( box.isEmpty() ) return;
 
-	var min = box.min;
-	var max = box.max;
+	box.getCenter( this.position );
 
-	var position = this.geometry.attributes.position;
-	var array = position.array;
+	box.getSize( this.scale );
 
-	array[  0 ] = max.x; array[  1 ] = max.y; array[  2 ] = max.z;
-	array[  3 ] = min.x; array[  4 ] = max.y; array[  5 ] = max.z;
-	array[  6 ] = min.x; array[  7 ] = min.y; array[  8 ] = max.z;
-	array[  9 ] = max.x; array[ 10 ] = min.y; array[ 11 ] = max.z;
-	array[ 12 ] = max.x; array[ 13 ] = max.y; array[ 14 ] = min.z;
-	array[ 15 ] = min.x; array[ 16 ] = max.y; array[ 17 ] = min.z;
-	array[ 18 ] = min.x; array[ 19 ] = min.y; array[ 20 ] = min.z;
-	array[ 21 ] = max.x; array[ 22 ] = min.y; array[ 23 ] = min.z;
-
-	position.needsUpdate = true;
-
-	this.geometry.computeBoundingSphere();
+	this.scale.multiplyScalar( 0.5 );
 
 };
 
