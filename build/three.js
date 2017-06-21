1c4f5d749ac6a527509268d9d01d4f390cb7b9ca
Mr.doob <info@mrdoob.com>
2017-06-21 00:26:31 -0700

    r86

--- build/three.js
+++ build/three.js
@@ -187,7 +187,7 @@
 
 	} );
 
-	var REVISION = '86dev';
+	var REVISION = '86';
 	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
--- build/three.module.js
+++ build/three.module.js
@@ -181,7 +181,7 @@ Object.assign( EventDispatcher.prototype, {
 
 } );
 
-var REVISION = '86dev';
+var REVISION = '86';
 var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 var CullFaceNone = 0;
 var CullFaceBack = 1;
