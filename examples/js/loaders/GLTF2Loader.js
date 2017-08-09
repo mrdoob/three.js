3997aa477a3f6c7b034d1bd5a3db92d184ac8465
Wittmann Tobias <twittmann@cadenas.internal>
2017-08-09 17:31:17 +0200

    copy material extras

--- examples/js/loaders/GLTF2Loader.js
+++ examples/js/loaders/GLTF2Loader.js
@@ -2107,7 +2107,9 @@ THREE.GLTF2Loader = ( function () {
 				// Normal map textures use OpenGL conventions:
 				// https://github.com/KhronosGroup/glTF/tree/master/specification/2.0#materialnormaltexture
 				_material.normalScale.x = -1;
-
+				
+				_material.userData = material.extras;
+				
 				return _material;
 
 			} );
