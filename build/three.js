9b938861f00c8fb6b4df6624c16854e83bfaabe4
Mr.doob <info@mrdoob.com>
2017-09-12 18:46:09 -0700

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -40125,47 +40125,19 @@
 
 		isInstancedBufferGeometry: true,
 
-		addGroup: function ( start, count, materialIndex ) {
+		copy: function ( source ) {
 
-			this.groups.push( {
+			BufferGeometry.prototype.copy.call( this, source );
 
-				start: start,
-				count: count,
-				materialIndex: materialIndex
+			this.maxInstancedCount = source.maxInstancedCount;
 
-			} );
+			return this;
 
 		},
 
-		copy: function ( source ) {
+		clone: function () {
 
-			var index = source.index;
-
-			if ( index !== null ) {
-
-				this.setIndex( index.clone() );
-
-			}
-
-			var attributes = source.attributes;
-
-			for ( var name in attributes ) {
-
-				var attribute = attributes[ name ];
-				this.addAttribute( name, attribute.clone() );
-
-			}
-
-			var groups = source.groups;
-
-			for ( var i = 0, l = groups.length; i < l; i ++ ) {
-
-				var group = groups[ i ];
-				this.addGroup( group.start, group.count, group.materialIndex );
-
-			}
-
-			return this;
+			return new this.constructor().copy( this );
 
 		}
 
