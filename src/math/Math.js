31e8e610b6ad86aca97c24c71fd4ebc6b5bfa259
Mr.doob <info@mrdoob.com>
2017-09-15 16:02:53 -0700

    Math: Changed nearestPowerOfTwo to be smaller or equal. See #12208.

--- src/math/Math.js
+++ src/math/Math.js
@@ -144,7 +144,7 @@ var _Math = {
 
 	nearestPowerOfTwo: function ( value ) {
 
-		return Math.pow( 2, Math.round( Math.log( value ) / Math.LN2 ) );
+		return Math.pow( 2, Math.floor( Math.log( value ) / Math.LN2 ) );
 
 	},
 
