a59b9e6e2096d500fb3dc5bbe6cd6a005dd9e655
Don McCurdy <donmccurdy@google.com>
2017-10-10 10:20:43 -0700

    LoadingManager: Add unit test.

--- test/unit/src/loaders/LoadingManager.js
+++ test/unit/src/loaders/LoadingManager.js
@@ -1,6 +1,22 @@
 /**
- * @author TristanVALCKE / https://github.com/TristanVALCKE
+ * @author Don McCurdy / https://github.com/donmccurdy
  */
 
-//Todo
-console.warn("Todo: Unit tests of LoadingManager")
+QUnit.module( "LoadingManager" );
+
+QUnit.test( "setResourceTransform", function( assert ) {
+
+  var manager = new THREE.LoadingManager();
+  var suffix = '?transformed=true';
+
+  manager.setResourceTransform( function ( url ) {
+
+    return url + suffix;
+
+  } );
+
+  var url = 'https://foo.bar/baz';
+  var resolvedURL = manager.resolveResourceURL( url );
+  assert.equal( resolvedURL, url + suffix, 'URL transform is applied' );
+
+});
