ab668156445fd19d27e137273d284172733db8e7
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-16 22:53:16 +0200

    Units: rename AxisHelper to AxesHelper

---
+++ test/unit/src/helpers/AxesHelper.js
@@ -0,0 +1,16 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global module, test */
+
+module.todo( 'AxesHelper', () => {
+
+	test( 'write me !', assert => {
+
+		assert.ok( false, "everything's gonna be alright" );
+
+	} );
+
+} );
+//Todo
+console.warn("Todo: Unit tests of AxisHelper")
