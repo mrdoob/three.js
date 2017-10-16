b2f44f4f638482cd45b73a6920a0e7334c8ff14a
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-16 22:01:22 +0200

    Unit utils: update to es6 syntax, and add explanation, plus some missing edge case

--- test/unit/src/utils.js
+++ test/unit/src/utils.js
@@ -1,25 +1,34 @@
 /**
  * @author alemures / https://github.com/alemures
+ * @author TristanVALCKE / https://github.com/Itee
  */
+/* global module, test */
 
-QUnit.module( 'utils' );
+module( 'utils', () => {
 
-QUnit.test( 'arrayMax' , function( assert ) {
+	test( 'arrayMax', assert => {
 
-	assert.equal( THREE.arrayMax( [] ), - Infinity );
-	assert.equal( THREE.arrayMax( [ 5 ] ), 5 );
-	assert.equal( THREE.arrayMax( [ 1, 5, 10 ] ), 10 );
-	assert.equal( THREE.arrayMax( [ 1, 10, 5 ] ), 10 );
-	assert.equal( THREE.arrayMax( [ 10, 5, 1 ] ), 10 );
+		assert.equal( THREE.arrayMax( [] ), - Infinity, 'Empty array return negative infinit' );
+		assert.equal( THREE.arrayMax( [ 5 ] ), 5, 'Single valued array should return the unique value as maximum' );
+		assert.equal( THREE.arrayMax( [ 10, 5, 1 ] ), 10, 'The array [ 10, 5, 1 ] return 10' );
+		assert.equal( THREE.arrayMax( [ 1, 10, 5 ] ), 10, 'The array [ 1, 10, 5 ] return 10' );
+		assert.equal( THREE.arrayMax( [ 1, 5, 10 ] ), 10, 'The array [ 1, 5, 10 ] return 10' );
+		assert.equal( THREE.arrayMax( [ - 0, 0 ] ), 0, 'The array [ - 0, 0 ] return 0' );
+		assert.equal( THREE.arrayMax( [ - Infinity, 0, Infinity ] ), Infinity, 'The array [ - Infinity, 0, Infinity ] return Infinity' );
 
-});
+	} );
 
-QUnit.test( 'arrayMin' , function( assert ) {
+	test( 'arrayMin', assert => {
 
-	assert.equal( THREE.arrayMin( [] ), Infinity );
-	assert.equal( THREE.arrayMin( [ 5 ] ), 5 );
-	assert.equal( THREE.arrayMin( [ 1, 5, 10 ] ), 1 );
-	assert.equal( THREE.arrayMin( [ 5, 1, 10 ] ), 1 );
-	assert.equal( THREE.arrayMin( [ 10, 5, 1 ] ), 1 );
+		assert.equal( THREE.arrayMin( [] ), Infinity, 'Empty array return positive infinit' );
+		assert.equal( THREE.arrayMin( [ 5 ] ), 5, 'Single valued array should return the unique value as minimum' );
+		assert.equal( THREE.arrayMin( [ 1, 5, 10 ] ), 1, 'The array [ 1, 5, 10 ] return 1' );
+		assert.equal( THREE.arrayMin( [ 5, 1, 10 ] ), 1, 'The array [ 5, 1, 10 ] return 1' );
+		assert.equal( THREE.arrayMin( [ 10, 5, 1 ] ), 1, 'The array [ 10, 5, 1 ] return 1' );
+		assert.equal( THREE.arrayMax( [ - 0, 0 ] ), - 0, 'The array [ - 0, 0 ] return -0' );
+		assert.equal( THREE.arrayMin( [ - Infinity, 0, Infinity ] ), - Infinity, 'The array [ - Infinity, 0, Infinity ] return -Infinity' );
 
-});
+	} );
+
+} );
+
