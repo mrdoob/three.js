164ec11847adc44036adb58e07e72b9af9afe257
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-19 01:54:58 +0200

    Add missings editors test

---
+++ test/unit/editor/Command.tests.js
@@ -0,0 +1,20 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global QUnit */
+
+import { NothingsIsExportedYet } from '../../../editor/js/Command';
+
+export default QUnit.module( 'Editor', () => {
+
+	QUnit.module.todo( 'Command', () => {
+
+		QUnit.test( 'write me !', ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+	} );
+
+} );
