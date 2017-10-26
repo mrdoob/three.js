b9bcf38635807ff92ade89c841707eda399efd15
Mr.doob <info@mrdoob.com>
2017-10-26 16:18:50 -0700

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -326,35 +326,37 @@
 		generateUUID: function () {
 
 			// http://www.broofa.com/Tools/Math.uuid.htm
+			// Replaced .join with string concatenation (@takahirox)
 
 			var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split( '' );
-			var uuid = new Array( 36 );
 			var rnd = 0, r;
 
 			return function generateUUID() {
+
+				var uuid = '';
 
 				for ( var i = 0; i < 36; i ++ ) {
 
 					if ( i === 8 || i === 13 || i === 18 || i === 23 ) {
 
-						uuid[ i ] = '-';
+						uuid += '-';
 
 					} else if ( i === 14 ) {
 
-						uuid[ i ] = '4';
+						uuid += '4';
 
 					} else {
 
 						if ( rnd <= 0x02 ) rnd = 0x2000000 + ( Math.random() * 0x1000000 ) | 0;
 						r = rnd & 0xf;
 						rnd = rnd >> 4;
-						uuid[ i ] = chars[ ( i === 19 ) ? ( r & 0x3 ) | 0x8 : r ];
+						uuid += chars[ ( i === 19 ) ? ( r & 0x3 ) | 0x8 : r ];
 
 					}
 
 				}
 
-				return uuid.join( '' );
+				return uuid;
 
 			};
 
