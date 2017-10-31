7011e96ad34f7dad830d2a1e3a4be3e3799425d9
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-31 22:31:56 +0100

    Add textures es6 unit tests

---
+++ test/unit/src/textures/CompressedTexture.tests.js
@@ -0,0 +1,35 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global QUnit */
+
+import { CompressedTexture } from '../../../../src/textures/CompressedTexture';
+
+export default QUnit.module( 'Textures', () => {
+
+	QUnit.module.todo( 'CompressedTexture', () => {
+
+		// INHERITANCE
+		QUnit.test( "Extending", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// INSTANCING
+		QUnit.test( "Instancing", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// PUBLIC STUFF
+		QUnit.test( "isCompressedTexture", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+	} );
+
+} );
