0d9a07af79f2e73ff2e82aa2122b8b526092121c
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-31 22:33:13 +0100

    Add mains unit test files and associated builds with rollup

---
+++ test/unit/three.example.unit.js
@@ -0,0 +1,15 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+	QUnit.module( "Example", () => {
+
+	/**
+	 * @author TristanVALCKE / https://github.com/Itee
+	 */
+
+	} );
+
+})));
