0c456ed85473fdb2c89634baa60402fb890298c7
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-31 22:28:26 +0100

    Add extras es6 unit tests

---
+++ test/unit/src/extras/core/Curve.tests.js
@@ -0,0 +1,92 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global QUnit */
+
+import { Curve } from '../../../../../src/extras/core/Curve';
+
+export default QUnit.module( 'Extras', () => {
+
+	QUnit.module( 'Core', () => {
+
+		QUnit.module.todo( 'Curve', () => {
+
+			// INSTANCING
+			QUnit.test( "Instancing", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			// PUBLIC STUFF
+			QUnit.test( "getPoint", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getPointAt", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getPoints", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getSpacedPoints", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getLength", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getLengths", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "updateArcLengths", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getUtoTmapping", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getTangent", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "getTangentAt", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			QUnit.test( "computeFrenetFrames", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+		} );
+
+	} );
+
+} );
---
+++ test/unit/src/extras/curves/ArcCurve.tests.js
@@ -0,0 +1,39 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global QUnit */
+
+import { ArcCurve } from '../../../../../src/extras/curves/ArcCurve';
+
+export default QUnit.module( 'Extras', () => {
+
+	QUnit.module( 'Curves', () => {
+
+		QUnit.module.todo( 'ArcCurve', () => {
+
+			// INHERITANCE
+			QUnit.test( "Extending", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			// INSTANCING
+			QUnit.test( "Instancing", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+			// PUBLIC STUFF
+			QUnit.test( "isArcCurve", ( assert ) => {
+
+				assert.ok( false, "everything's gonna be alright" );
+
+			} );
+
+		} );
+
+	} );
+
+} );
