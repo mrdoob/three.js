de8470ab3479586b55cd8cefbae24f066223528d
Tristan VALCKE <tristan.valcke@gmail.com>
2017-10-31 22:30:23 +0100

    Add materials es6 unit tests

---
+++ test/unit/src/materials/MeshStandardMaterial.tests.js
@@ -0,0 +1,41 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global QUnit */
+
+import { MeshStandardMaterial } from '../../../../src/materials/MeshStandardMaterial';
+
+export default QUnit.module( 'Materials', () => {
+
+	QUnit.module.todo( 'MeshStandardMaterial', () => {
+
+		// INHERITANCE
+		QUnit.test( "Extending", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// INSTANCING
+		QUnit.test( "Instancing", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// PUBLIC STUFF
+		QUnit.test( "isMeshStandardMaterial", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		QUnit.test( "copy", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+	} );
+
+} );
---
+++ test/unit/src/materials/MeshToonMaterial.tests.js
@@ -0,0 +1,41 @@
+/**
+ * @author TristanVALCKE / https://github.com/Itee
+ */
+/* global QUnit */
+
+import { MeshToonMaterial } from '../../../../src/materials/MeshToonMaterial';
+
+export default QUnit.module( 'Materials', () => {
+
+	QUnit.module.todo( 'MeshToonMaterial', () => {
+
+		// INHERITANCE
+		QUnit.test( "Extending", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// INSTANCING
+		QUnit.test( "Instancing", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// PUBLIC STUFF
+		QUnit.test( "isMeshToonMaterial", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		QUnit.test( "copy", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+	} );
+
+} );
