ef1efaac04bd07dffb114854bae198a8af4fdafb
Tyler Lindberg <tlindberg@ucla.edu>
2017-11-01 20:03:03 -0700

    Added input fields for transform controls

--- editor/js/Sidebar.Controls.js
+++ editor/js/Sidebar.Controls.js
@@ -8,10 +8,10 @@ Sidebar.Controls = function ( editor ) {
 	var signals = editor.signals;
 
 	var container = new UI.Panel();
-	container.setBorderTop( '0' );
-	container.setPaddingTop( '20px' );
-
 	container.add( new UI.Text( 'CONTROLS' ) );
+
+	// Use this to add a line break above the panel
+	container.add( new UI.Break(), new UI.Break() );
 
 	var controlNames = [
 		'translate',
@@ -19,11 +19,33 @@ Sidebar.Controls = function ( editor ) {
 		'scale'
 	];
 
-	// Create rows here
+	for ( var i = 0; i < controlNames.length; i++ ) {
 
-	if ( config.getKey( 'controls/translate' ) !== undefined ) {
+		let name = controlNames[ i ];
+		let configName = 'controls/' + name;
+		let controlRow = new UI.Row();
 
+		let controlInput = new UI.Input().setWidth( '150px' ).setFontSize( '12px' ).onChange( function () {
 
+			config.setKey( configName, controlInput.getValue()[ 0 ] );
+
+		} );
+		controlInput.dom.addEventListener( 'focus', function () {
+
+			controlInput.dom.select();
+
+		} );
+
+		if( config.getKey( configName ) !== undefined ) {
+
+			controlInput.setValue( config.getKey( configName ) );
+
+		}
+
+		controlInput.dom.maxLength = 1;
+		controlRow.add( new UI.Text( name.charAt( 0 ).toUpperCase() + name.slice( 1 ) ).setWidth( '90px' ) );
+		controlRow.add( controlInput );
+		container.add( controlRow );
 
 	}
 
