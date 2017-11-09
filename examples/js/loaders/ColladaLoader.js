4ebc74b868ea87e106f8d453222d5258918253fe
Pascal Häusler <p.haeusler@cadenas.de>
2017-11-09 12:21:43 +0100

    Added comment for unsupported tags

--- examples/js/loaders/ColladaLoader.js
+++ examples/js/loaders/ColladaLoader.js
@@ -1760,6 +1760,7 @@ THREE.ColladaLoader.prototype = {
 
 			var mesh = getElementsByTagName( xml, 'mesh' )[ 0 ];
 			
+			// the following tags inside geometry are not supported yet (see https://github.com/mrdoob/three.js/pull/12606): convex_mesh, spline, brep
 			if ( mesh === undefined ) return;
 
 			for ( var i = 0; i < mesh.childNodes.length; i ++ ) {
