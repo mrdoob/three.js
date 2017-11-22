d79f72f1067ebe74a46133a2b0ea9eb38dfec901
moraxy <moraxy@users.noreply.github.com>
2017-11-22 12:10:52 +0100

    Fixes #12722 by partially reverting db347605ed00cdf045f88120289056f3f299f088

--- src/renderers/shaders/ShaderChunk/worldpos_vertex.glsl
+++ src/renderers/shaders/ShaderChunk/worldpos_vertex.glsl
@@ -1,4 +1,4 @@
-#if defined( USE_ENVMAP ) || defined( DISTANCE ) || defined ( USE_SHADOWMAP )
+#if defined( USE_ENVMAP ) || defined( DISTANCE ) || defined ( USE_SHADOWMAP ) || defined ( PHYSICAL )
 
 	vec4 worldPosition = modelMatrix * vec4( transformed, 1.0 );
 
