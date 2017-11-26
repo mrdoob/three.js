1bc987e707cb1b2d9007d1e6fa334c0791bb588f
Takahiro <hogehoge@gachapin.jp>
2017-11-26 16:00:58 +0900

    Fix glTF Specular-Glossiness material by converting specularMap in sRGB space

--- examples/js/loaders/GLTFLoader.js
+++ examples/js/loaders/GLTFLoader.js
@@ -529,6 +529,7 @@ THREE.GLTFLoader = ( function () {
 					'vec3 specularFactor = specular;',
 					'#ifdef USE_SPECULARMAP',
 					'	vec4 texelSpecular = texture2D( specularMap, vUv );',
+					'	texelSpecular = sRGBToLinear( texelSpecular );',
 					'	// reads channel RGB, compatible with a glTF Specular-Glossiness (RGBA) texture',
 					'	specularFactor *= texelSpecular.rgb;',
 					'#endif'
