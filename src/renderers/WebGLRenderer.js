ec0bd8ad6d8d75233f883c10f3ee8bc50639b243
Mugen87 <michael.herzog@human-interactive.org>
2017-11-27 17:58:22 +0100

    VideoTexture: Refactor update

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1124,6 +1124,10 @@ function WebGLRenderer( parameters ) {
 
 		//
 
+		textures.updateVideoTextures();
+
+		//
+
 		if ( _clippingEnabled ) _clipping.beginShadows();
 
 		shadowMap.render( shadowsArray, scene, camera );
