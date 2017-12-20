1293a71ef4502eb480e1f6bac6e8954562f1283b
Don McCurdy <donmccurdy@google.com>
2017-12-20 11:48:01 -0800

    GLTFLoader: Update TODO list.

--- examples/js/loaders/GLTFLoader.js
+++ examples/js/loaders/GLTFLoader.js
@@ -376,10 +376,7 @@ THREE.GLTFLoader = ( function () {
 	 *
 	 * Specification: https://github.com/KhronosGroup/glTF/pull/874
 	 *
-	 * TODO:
-	 * - Support additional uncompressed attributes alongside those provided by Draco.
-	 * - Support fallback to uncompressed data if decoder is not unavailable.
-	 * - `primitive.attributes` should be passed to DRACOLoader, but isn't yet supported.
+	 * TODO: Support fallback to uncompressed data if decoder is unavailable.
 	 */
 	function GLTFDracoMeshCompressionExtension ( dracoLoader ) {
 
