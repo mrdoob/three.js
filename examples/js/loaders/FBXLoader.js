2c6e319712008e639cb5358ac96f6cf9a7f59bcf
Bryan Wong <bryanw0ng@ucla.edu>
2017-12-20 18:53:58 -0800

    changed fbx loader to new version

--- examples/js/loaders/FBXLoader.js
+++ examples/js/loaders/FBXLoader.js
@@ -3650,11 +3650,15 @@
 
 		getString: function ( size ) {
 
-			var s = THREE.LoaderUtils.decodeText( this.getUint8Array( size ) );
+			var a = new Uint8Array( this.getUint8Array( size ) );
 
-			this.skip( size );
+			// for ( var i = 0; i < size; i ++ ) {
 
-			return s;
+			// 	a[ i ] = this.getUint8();
+
+			// }
+
+			return THREE.LoaderUtils.decodeText( a );
 
 		}
 
