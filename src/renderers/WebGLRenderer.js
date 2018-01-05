86d299d368eb75cf37abcc911e2d2ad04ac2dca8
Yannis Gravezas <wizgrav@gmail.com>
2018-01-05 22:39:43 +0100

    use Scene.onBeforeRender and .onAfterRender

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1105,6 +1105,8 @@ function WebGLRenderer( parameters ) {
 
 		}
 
+		scene.onBeforeRender( _this, scene, camera, renderTarget );
+
 		_projScreenMatrix.multiplyMatrices( camera.projectionMatrix, camera.matrixWorldInverse );
 		_frustum.setFromMatrix( _projScreenMatrix );
 
@@ -1206,6 +1208,8 @@ function WebGLRenderer( parameters ) {
 		state.buffers.color.setMask( true );
 
 		state.setPolygonOffset( false );
+
+		scene.onAfterRender( _this, scene, camera, renderTarget );
 
 		if ( vr.enabled ) {
 
