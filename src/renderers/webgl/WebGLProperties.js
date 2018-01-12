f8c1beeb094c58528a8b5847facf8bdc5c9232c6
aardgoose <angus.sawyer@gmail.com>
2018-01-12 21:24:04 +0000

    use WeakMap in WebGL(Properties|Attributes)

--- src/renderers/webgl/WebGLProperties.js
+++ src/renderers/webgl/WebGLProperties.js
@@ -4,17 +4,16 @@
 
 function WebGLProperties() {
 
-	var properties = {};
+	var properties = new WeakMap();
 
 	function get( object ) {
 
-		var uuid = object.uuid;
-		var map = properties[ uuid ];
+		var map = properties.get( object );
 
 		if ( map === undefined ) {
 
 			map = {};
-			properties[ uuid ] = map;
+			properties.set( object, map);
 
 		}
 
@@ -24,13 +23,13 @@ function WebGLProperties() {
 
 	function remove( object ) {
 
-		delete properties[ object.uuid ];
+		properties.delete( object );
 
 	}
 
 	function dispose() {
 
-		properties = {};
+		properties = new WeakMap();
 
 	}
 
