07d757661ebd729eb38a18ab2f0394dd3fa61d9b
Don McCurdy <don.r.mccurdy@gmail.com>
2018-01-13 12:29:56 -0800

    Fix links to Frostbite PBR document.

--- src/lights/SpotLight.js
+++ src/lights/SpotLight.js
@@ -21,14 +21,14 @@ function SpotLight( color, intensity, distance, angle, penumbra, decay ) {
 		get: function () {
 
 			// intensity = power per solid angle.
-			// ref: equation (17) from http://www.frostbite.com/wp-content/uploads/2014/11/course_notes_moving_frostbite_to_pbr.pdf
+			// ref: equation (17) from https://seblagarde.files.wordpress.com/2015/07/course_notes_moving_frostbite_to_pbr_v32.pdf
 			return this.intensity * Math.PI;
 
 		},
 		set: function ( power ) {
 
 			// intensity = power per solid angle.
-			// ref: equation (17) from http://www.frostbite.com/wp-content/uploads/2014/11/course_notes_moving_frostbite_to_pbr.pdf
+			// ref: equation (17) from https://seblagarde.files.wordpress.com/2015/07/course_notes_moving_frostbite_to_pbr_v32.pdf
 			this.intensity = power / Math.PI;
 
 		}
