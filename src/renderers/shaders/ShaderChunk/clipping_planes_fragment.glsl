d7e56f1512a5ceecc2951765569768213977e0a1
Mr.doob <info@mrdoob.com>
2018-02-05 17:56:12 -0800

    Clean up.

--- src/renderers/shaders/ShaderChunk/clipping_planes_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/clipping_planes_fragment.glsl
@@ -1,11 +1,9 @@
 #if NUM_CLIPPING_PLANES > 0
 
-	vec4 plane;
-
 	#pragma unroll_loop
 	for ( int i = 0; i < UNION_CLIPPING_PLANES; i ++ ) {
 
-		plane = clippingPlanes[ i ];
+		vec4 plane = clippingPlanes[ i ];
 		if ( dot( vViewPosition, plane.xyz ) > plane.w ) discard;
 
 	}
@@ -17,7 +15,7 @@
 		#pragma unroll_loop
 		for ( int i = UNION_CLIPPING_PLANES; i < NUM_CLIPPING_PLANES; i ++ ) {
 
-			plane = clippingPlanes[ i ];
+			vec4 plane = clippingPlanes[ i ];
 			clipped = ( dot( vViewPosition, plane.xyz ) > plane.w ) && clipped;
 
 		}
