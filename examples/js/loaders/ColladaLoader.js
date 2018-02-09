97da62cc221782b7b881ffcb94ec602591411996
Mr.doob <info@mrdoob.com>
2018-02-09 14:29:30 -0800

    ColladaLoader: Clean up.

--- examples/js/loaders/ColladaLoader.js
+++ examples/js/loaders/ColladaLoader.js
@@ -1062,10 +1062,10 @@ THREE.ColladaLoader.prototype = {
 					case 'technique':
 						data.technique = parseEffectTechnique( child );
 						break;
-						
+
 					case 'extra':
-                        			data.extra = parseEffectExtra( child );
-                        			break;
+						data.extra = parseEffectExtra( child );
+						break;
 
 				}
 
@@ -1332,54 +1332,54 @@ THREE.ColladaLoader.prototype = {
 			}
 
 		}
-		
+
 		function parseEffectExtra( xml ) {
- 
-            		var data = {};
- 
-            		for ( var i = 0, l = xml.childNodes.length; i < l; i ++ ) {
- 
-                		var child = xml.childNodes[ i ];
- 
-                		if ( child.nodeType !== 1 ) continue;
- 
-                		switch ( child.nodeName ) {
- 
-                    		case 'technique':
-                        		data.technique = parseEffectExtraTechnique( child );
-                        		break;
- 
-                		}
- 
-            		}
- 
-            		return data;
- 
-        	}
-        
-        	function parseEffectExtraTechnique( xml ) {
- 
-            		var data = {};
- 
-            		for ( var i = 0, l = xml.childNodes.length; i < l; i ++ ) {
- 
-                		var child = xml.childNodes[ i ];
- 
-                		if ( child.nodeType !== 1 ) continue;
- 
-                		switch ( child.nodeName ) {
- 
-                    		case 'double_sided':
-                    			data[ child.nodeName ] = parseInt( child.textContent );
-                    			break;
- 
-                		}
- 
-            		}
- 
-            		return data;
- 
-        	}
+
+			var data = {};
+
+			for ( var i = 0, l = xml.childNodes.length; i < l; i ++ ) {
+
+				var child = xml.childNodes[ i ];
+
+				if ( child.nodeType !== 1 ) continue;
+
+				switch ( child.nodeName ) {
+
+					case 'technique':
+						data.technique = parseEffectExtraTechnique( child );
+						break;
+
+				}
+
+			}
+
+			return data;
+
+		}
+
+		function parseEffectExtraTechnique( xml ) {
+
+			var data = {};
+
+			for ( var i = 0, l = xml.childNodes.length; i < l; i ++ ) {
+
+				var child = xml.childNodes[ i ];
+
+				if ( child.nodeType !== 1 ) continue;
+
+				switch ( child.nodeName ) {
+
+					case 'double_sided':
+						data[ child.nodeName ] = parseInt( child.textContent );
+						break;
+
+				}
+
+			}
+
+			return data;
+
+		}
 
 		function buildEffect( data ) {
 
@@ -1426,7 +1426,7 @@ THREE.ColladaLoader.prototype = {
 			var effect = getEffect( data.url );
 			var technique = effect.profile.technique;
 			var extra = effect.profile.extra;
-			
+
 			var material;
 
 			switch ( technique.type ) {
@@ -1597,16 +1597,14 @@ THREE.ColladaLoader.prototype = {
 				}
 
 			}
-			
-			if ( extra !== undefined && extra.technique !== undefined && isEmpty( extra.technique ) === false ) {
-                
-                		if ( extra.technique.double_sided !== undefined && extra.technique.double_sided === 1 ) {
-                    			
-					material.side = THREE.DoubleSide;
- 
-                		}
- 
-            		}
+
+			//
+
+			if ( extra !== undefined && extra.technique !== undefined && extra.technique.double_sided === 1 ) {
+
+				material.side = THREE.DoubleSide;
+
+			}
 
 			return material;
 
