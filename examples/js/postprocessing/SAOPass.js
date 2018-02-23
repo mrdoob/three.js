00f4dca82f7724e4d1515d341a502fa99760e439
Pascal Häusler <p.haeusler@cadenas.de>
2018-02-23 09:46:15 +0100

    SAOPass: cleanup
    Since we don't use MRT there is no need for using the WEBGL_draw_buffers extension.
    check https://github.com/mrdoob/three.js/issues/13370

--- examples/js/postprocessing/SAOPass.js
+++ examples/js/postprocessing/SAOPass.js
@@ -76,7 +76,6 @@ THREE.SAOPass = function ( scene, camera, depthTexture, useNormals, resolution )
 
 	this.saoMaterial = new THREE.ShaderMaterial( THREE.SAOShader );
 	this.saoMaterial.extensions.derivatives = true;
-	this.saoMaterial.extensions.drawBuffers = true;
 	this.saoMaterial.defines[ 'DEPTH_PACKING' ] = this.supportsDepthTextureExtension ? 0 : 1;
 	this.saoMaterial.defines[ 'NORMAL_TEXTURE' ] = this.supportsNormalTexture ? 1 : 0;
 	this.saoMaterial.defines[ 'PERSPECTIVE_CAMERA' ] = this.camera.isPerspectiveCamera ? 1 : 0;
