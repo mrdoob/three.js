f26e7cc082ce442720a4420981dac9a5a7aa55d3
WestLangley <WestLangley@users.noreply.github.com>
2018-03-02 22:23:01 -0500

    Remove getWorldRotation()

--- src/Three.Legacy.js
+++ src/Three.Legacy.js
@@ -865,6 +865,11 @@ Object.assign( Object3D.prototype, {
 		console.warn( 'THREE.Object3D: .translate() has been removed. Use .translateOnAxis( axis, distance ) instead.' );
 		return this.translateOnAxis( axis, distance );
 
+	},
+	getWorldRotation: function () {
+
+		console.error( 'THREE.Object3D: .getWorldRotation() has been removed. Use THREE.Object3D.getWorldQuaternion( target ) instead.' );
+
 	}
 
 } );
