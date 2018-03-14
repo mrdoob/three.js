db6f9338525e6972a16305ea48baf5f590aaea10
Don McCurdy <donmccurdy@google.com>
2018-03-14 08:53:19 -0700

    LegacyGLTFLoader: Fix parsing textures in GLB files.

--- examples/js/loaders/deprecated/LegacyGLTFLoader.js
+++ examples/js/loaders/deprecated/LegacyGLTFLoader.js
@@ -394,16 +394,6 @@ THREE.LegacyGLTFLoader = ( function () {
 
 	};
 
-	GLTFBinaryExtension.prototype.loadTextureSourceUri = function ( source, bufferViews ) {
-
-		var metadata = source.extensions[ EXTENSIONS.KHR_BINARY_GLTF ];
-		var bufferView = bufferViews[ metadata.bufferView ];
-		var stringData = THREE.LoaderUtils.decodeText( new Uint8Array( bufferView ) );
-
-		return 'data:' + metadata.mimeType + ';base64,' + btoa( stringData );
-
-	};
-
 	/*********************************/
 	/********** INTERNALS ************/
 	/*********************************/
@@ -1071,10 +1061,15 @@ THREE.LegacyGLTFLoader = ( function () {
 
 						var source = json.images[ texture.source ];
 						var sourceUri = source.uri;
+						var isObjectURL = false;
 
 						if ( source.extensions && source.extensions[ EXTENSIONS.KHR_BINARY_GLTF ] ) {
 
-							sourceUri = extensions[ EXTENSIONS.KHR_BINARY_GLTF ].loadTextureSourceUri( source, dependencies.bufferViews );
+							var metadata = source.extensions[ EXTENSIONS.KHR_BINARY_GLTF ];
+							var bufferView = dependencies.bufferViews[ metadata.bufferView ];
+							var blob = new Blob( [ bufferView ], { type: metadata.mimeType } );
+							sourceUri = URL.createObjectURL( blob );
+							isObjectURL = true;
 
 						}
 
@@ -1089,6 +1084,8 @@ THREE.LegacyGLTFLoader = ( function () {
 						textureLoader.setCrossOrigin( options.crossOrigin );
 
 						textureLoader.load( resolveURL( sourceUri, options.path ), function ( _texture ) {
+
+							if ( isObjectURL ) URL.revokeObjectURL( sourceUri );
 
 							_texture.flipY = false;
 
@@ -1119,6 +1116,8 @@ THREE.LegacyGLTFLoader = ( function () {
 							resolve( _texture );
 
 						}, undefined, function () {
+
+							if ( isObjectURL ) URL.revokeObjectURL( sourceUri );
 
 							resolve();
 
