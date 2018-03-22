295eaa5cb78a356d606a8c8eee01ea910993f8d1
Mugen87 <michael.herzog@human-interactive.org>
2018-03-22 01:43:06 +0100

    ObjectLoader: Fix loading of ShapeBufferGeometry

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -360,9 +360,9 @@ Object.assign( ObjectLoader.prototype, {
 
 						var geometryShapes = [];
 
-						for ( var i = 0, l = data.shapes.length; i < l; i ++ ) {
+						for ( var j = 0, jl = data.shapes.length; j < jl; j ++ ) {
 
-							var shape = shapes[ data.shapes[ i ] ];
+							var shape = shapes[ data.shapes[ j ] ];
 
 							geometryShapes.push( shape );
 
