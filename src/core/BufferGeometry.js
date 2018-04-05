fbd50e891e63d06b628a88df9d13c23e0e8edb1d
aardgoose <angus.sawyer@gmail.com>
2018-04-05 11:18:25 +0100

    comment out diagnostic output

--- src/core/BufferGeometry.js
+++ src/core/BufferGeometry.js
@@ -948,7 +948,7 @@ BufferGeometry.prototype = Object.assign( Object.create( EventDispatcher.prototy
 
 		}
 
-		console.log( 'THREE.BufferGeometry: old count:', groups.length, 'new count:', this.groups.length );
+		// console.log( 'THREE.BufferGeometry: old count:', groups.length, 'new count:', this.groups.length );
 
 		return this;
 
