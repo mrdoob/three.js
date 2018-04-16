8f194ec566b40b9b9d7bb9a885b11d3b42737c94
Mugen87 <michael.herzog@human-interactive.org>
2018-04-16 15:28:14 +0200

    Examples: Clean up

--- examples/js/postprocessing/SAOPass.js
+++ examples/js/postprocessing/SAOPass.js
@@ -161,7 +161,7 @@ THREE.SAOPass = function ( scene, camera, depthTexture, useNormals, resolution )
 
 	this.quadCamera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
 	this.quadScene = new THREE.Scene();
-	this.quad = new THREE.Mesh( new THREE.PlaneGeometry( 2, 2 ), null );
+	this.quad = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), null );
 	this.quadScene.add( this.quad );
 
 };
