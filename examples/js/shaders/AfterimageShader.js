10df2978ab18a9e187ac16e6e458961b0d7aef69
HypnosNova <601670314@qq.com>
2018-04-22 18:38:26 +0800

    add afterimage effect
    a postprocessing of afterimage effect. Looks pretty cool.

---
+++ examples/js/shaders/AfterimageShader.js
@@ -0,0 +1,65 @@
+/**
+ * @author HypnosNova / https://www.threejs.org.cn/gallery/
+ * I'm lucky to get this domain name.(threejs.org.cn)
+ *
+ * Afterimage shader
+ * I created this effect inspired by a demo on codepen. (Unfortunately, I forgot the url)
+ */
+
+THREE.AfterimageShader = {
+
+	uniforms: {
+
+		"damp": { value: 0.96 },
+		"tOld": { value: null },
+		"tNew": { value: null }
+
+	},
+
+	vertexShader: [
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vUv = uv;",
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"uniform float damp;",
+
+		"uniform sampler2D tOld;",
+		"uniform sampler2D tNew;",
+
+		"varying vec2 vUv;",
+		
+		"float when_gt( float x, float y ) {",
+
+			"return max( sign( x - y ), 0.0 );",
+
+		"}",
+
+		"void main() {",
+
+			"vec4 texelOld = texture2D( tOld, vUv );",
+			"vec4 texelNew = texture2D( tNew, vUv );",
+			
+			"texelOld *= damp;",
+			
+			"texelOld.r *= when_gt( texelOld.r, 0.2 );",
+    		"texelOld.g *= when_gt( texelOld.g, 0.2 );",
+    		"texelOld.b *= when_gt( texelOld.b, 0.2 );",
+    		"texelOld.a *= when_gt( texelOld.a, 0.2 );",
+
+			"gl_FragColor = texelOld + texelNew;",
+
+		"}"
+
+	].join( "\n" )
+
+};
