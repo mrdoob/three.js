5401c99f1b808f124627f95cf9d82aaef597dcd6
Takahiro <hogehoge@gachapin.jp>
2018-04-25 03:41:55 +0900

    Deserialize .userData only for BufferGeometry in parseGeometries of ObjectLoader

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -398,7 +398,7 @@ Object.assign( ObjectLoader.prototype, {
 				geometry.uuid = data.uuid;
 
 				if ( data.name !== undefined ) geometry.name = data.name;
-				if ( data.userData !== undefined ) geometry.userData = data.userData;
+				if ( geometry.isBufferGeometry === true && data.userData !== undefined ) geometry.userData = data.userData;
 
 				geometries[ data.uuid ] = geometry;
 
