c0298ea85dfd5ef94aecc64e67d3f31578839a9d
oguz <eroglu.oguz93@gmail.com>
2018-05-18 23:12:14 +0300

    Prevention of unecessary calls of blend functions

--- src/renderers/webgl/WebGLState.js
+++ src/renderers/webgl/WebGLState.js
@@ -537,7 +537,7 @@ function WebGLState( gl, extensions, utils ) {
 
 		if ( blending !== CustomBlending ) {
 
-			if ( blending !== currentBlending || premultipliedAlpha !== currentPremultipledAlpha ) {
+			if ( ( blending !== NoBlending ) && ( blending !== currentBlending || premultipliedAlpha !== currentPremultipledAlpha ) ) {
 
 				switch ( blending ) {
 
@@ -639,8 +639,12 @@ function WebGLState( gl, extensions, utils ) {
 
 		}
 
-		currentBlending = blending;
-		currentPremultipledAlpha = premultipliedAlpha;
+		if ( blending !== NoBlending ) {
+
+			currentBlending = blending;
+			currentPremultipledAlpha = premultipliedAlpha;
+
+		}
 
 	}
 
