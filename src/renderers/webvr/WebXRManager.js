8914853ba04d252c1763f0207c529d0c9a26c43a
Mr.doob <info@mrdoob.com>
2018-05-25 17:18:58 -0400

    WebXRManager: Expose frameOfReferenceType.

--- src/renderers/webvr/WebXRManager.js
+++ src/renderers/webvr/WebXRManager.js
@@ -58,7 +58,7 @@ function WebXRManager( renderer ) {
 
 	//
 
-	this.setSession = function ( value ) {
+	this.setSession = function ( value, options ) {
 
 		session = value;
 
@@ -72,7 +72,7 @@ function WebXRManager( renderer ) {
 			} );
 
 			session.baseLayer = new XRWebGLLayer( session, gl );
-			session.requestFrameOfReference( 'stage' ).then( function ( value ) {
+			session.requestFrameOfReference( options.frameOfReferenceType ).then( function ( value ) {
 
 				frameOfRef = value;
 
