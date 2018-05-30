78f45193151b72d9562c6b935607159450c8edb9
Administrator <nicolas.dierick@peoplbrain.com>
2018-05-30 16:32:39 +0200

    Fix FBXLoader For Internet Explorer (indexOf and Slice undefined)

--- examples/js/loaders/FBXLoader.js
+++ examples/js/loaders/FBXLoader.js
@@ -3624,8 +3624,29 @@
 
 			}
 
-			var nullByte = a.indexOf( 0 );
-			if ( nullByte >= 0 ) a = a.slice( 0, nullByte );
+            if( a.indexOf ){
+			    var nullByte = a.indexOf( 0 );
+			    if ( nullByte >= 0 ) a = a.slice( 0, nullByte );
+            }else{
+
+                if(!Uint8Array.prototype.slice){
+                    Uint8Array.prototype.slice = function(a,b){
+                        var Uint8ArraySlice = new Uint8Array(this.buffer.slice(a,b));
+                        return Uint8ArraySlice;
+                    }
+                }
+
+                var ikk = -1;
+                for( var kkk in a ){
+                    if( a[kkk] === 0 ){
+                        ikk = kkk;
+                        break;
+                    }
+                }
+                if( ikk >= 0 ){
+                    a = a.slice( 0, ikk );
+                }
+            }
 
 			return THREE.LoaderUtils.decodeText( a );
 
