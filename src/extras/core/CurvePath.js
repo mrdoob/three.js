5408bd8978e9be03ef7ca70d8f57963bdb780878
luis Wang <lovemormot@gmail.com>
2018-06-14 23:06:58 +0700

    Add inLineCurve3 check

--- src/extras/core/CurvePath.js
+++ src/extras/core/CurvePath.js
@@ -171,8 +171,9 @@ CurvePath.prototype = Object.assign( Object.create( Curve.prototype ), {
 			var curve = curves[ i ];
 			var resolution = ( curve && curve.isEllipseCurve ) ? divisions * 2
 				: ( curve && curve.isLineCurve ) ? 1
-					: ( curve && curve.isSplineCurve ) ? divisions * curve.points.length
-						: divisions;
+					: ( curve && curve.isLineCurve3 ) ? 1
+						: ( curve && curve.isSplineCurve ) ? divisions * curve.points.length
+							: divisions;
 
 			var pts = curve.getPoints( resolution );
 
