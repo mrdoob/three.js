23901e86eb9930456896802b805fcd3b6a0b8c85
Marshall Quander <marshall@quander.me>
2018-06-15 14:37:20 -0700

    Add Object3D.applyMatrices

--- src/core/Object3D.js
+++ src/core/Object3D.js
@@ -109,11 +109,22 @@ Object3D.prototype = Object.assign( Object.create( EventDispatcher.prototype ), 
 
 	applyMatrix: function ( matrix ) {
 
-		this.matrix.multiplyMatrices( matrix, this.matrix );
+		this.applyMatrices ( matrix );
+
+	},
+
+	applyMatrices: function ( /* matrices */ ) {
+
+		for ( var i = 0; i < arguments.length; i++ ) {
+
+			this.matrix.multiplyMatrices( arguments[i], this.matrix );
+
+		}
 
 		this.matrix.decompose( this.position, this.quaternion, this.scale );
 
 	},
+
 
 	applyQuaternion: function ( q ) {
 
