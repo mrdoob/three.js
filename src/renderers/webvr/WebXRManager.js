2c480de9d50224714af23b9316f31fc9142e561d
Mr.doob <info@mrdoob.com>
2018-06-22 23:07:21 -0700

    WebXRManager: Use pointerMatrix instead of gripMatrix (Cardboard support).

--- src/renderers/webvr/WebXRManager.js
+++ src/renderers/webvr/WebXRManager.js
@@ -241,7 +241,7 @@ function WebXRManager( renderer ) {
 
 				if ( inputPose !== null ) {
 
-					controller.matrix.elements = inputPose.gripMatrix;
+					controller.matrix.elements = inputPose.pointerMatrix;
 					controller.matrix.decompose( controller.position, controller.rotation, controller.scale );
 					controller.visible = true;
 
