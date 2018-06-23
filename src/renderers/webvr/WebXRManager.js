522afd0c7031d4d6ef6032b8de95b90d54fb7c71
Mr.doob <info@mrdoob.com>
2018-06-22 23:25:39 -0700

    Revert "WebXRManager: Hide controller when using Cardboard."
    This reverts commit 684fbcbc7831582eb2055cc7df758a3c5b1238f2.

--- src/renderers/webvr/WebXRManager.js
+++ src/renderers/webvr/WebXRManager.js
@@ -243,7 +243,7 @@ function WebXRManager( renderer ) {
 
 					controller.matrix.elements = inputPose.pointerMatrix;
 					controller.matrix.decompose( controller.position, controller.rotation, controller.scale );
-					controller.visible = inputPose.emulatedPosition === false;
+					controller.visible = true;
 
 					continue;
 
