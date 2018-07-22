fae355c1c4946a150892e909d489ed740c1ada6c
Lewy Blue <looeee@gmail.com>
2018-07-22 09:32:25 +0100

    don't return top level double nested group

--- examples/js/loaders/FBXLoader.js
+++ examples/js/loaders/FBXLoader.js
@@ -1655,6 +1655,14 @@
 
 		setupMorphMaterials( sceneGraph );
 
+		// if all the models where already combined in a single group, just return that
+		if ( sceneGraph.children.length === 1 && sceneGraph.children[ 0 ].isGroup ) {
+
+			sceneGraph.children[ 0 ].animations = sceneGraph.animations;
+			return sceneGraph.children[ 0 ];
+
+		}
+
 		return sceneGraph;
 
 	}
