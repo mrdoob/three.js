484426d108ccb7f80349ea7bc254181572cc5077
Robert Long <robert@robertlong.me>
2018-07-26 17:33:39 -0700

    Error handling in processInput & postProcessBuffers works for 0 buffers

--- examples/js/exporters/GLTFExporter.js
+++ examples/js/exporters/GLTFExporter.js
@@ -2028,11 +2028,21 @@ THREE.GLTFExporter.prototype = {
 
 				}
 
+				resolve();
+
 			} );
 
 		}
 
-		processInput( input );
+		try {
+
+			processInput( input );
+
+		} catch ( e ) {
+
+			onError( e );
+
+		}
 
 		Promise.all( pending ).then( postProcessBuffers ).then( function () {
 
