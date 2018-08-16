8bfe292a79be1ee02bf6b522bb45413a5b2d33d8
Robert Long <robert@robertlong.me>
2018-08-16 11:44:53 -0700

    Fix getFileNameFromUri

--- examples/js/exporters/GLTFExporter.js
+++ examples/js/exporters/GLTFExporter.js
@@ -333,7 +333,13 @@ THREE.GLTFExporter.prototype = {
 		function getFileNameFromUri( uri ) {
 
 			var parts = uri.split( '#' ).shift().split( '?' ).shift().split( '/' ).pop().split( '.' );
-			parts.pop();
+
+			if ( parts.length > 1 ) {
+
+				parts.pop();
+
+			}
+
 			return parts.join();
 
 		}
