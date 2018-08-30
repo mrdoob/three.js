63f6d20200fcfced70fee3bf9865445e5a2e9cfd
Mr.doob <info@mrdoob.com>
2018-08-30 00:43:01 -0700

    Reverted distance fog. See #14736 and #14786.

--- src/renderers/shaders/ShaderChunk/fog_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/fog_fragment.glsl
@@ -1,12 +1,5 @@
 #ifdef USE_FOG
 
-	// float fogDepth = length( vFogPosition );
-	// mediump support: divide then multiply by max component
-
-	vec3 fogPositionAbs = abs( vFogPosition );
-	float fogMaxComponent = max( fogPositionAbs.x, max( fogPositionAbs.y, fogPositionAbs.z ) );
-	float fogDepth = length( vFogPosition / fogMaxComponent ) * fogMaxComponent;
-
 	#ifdef FOG_EXP2
 
 		float fogFactor = whiteCompliment( exp2( - fogDensity * fogDensity * fogDepth * fogDepth * LOG2 ) );
