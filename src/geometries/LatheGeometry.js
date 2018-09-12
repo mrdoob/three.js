d73727a6e8a74779eb8ca7e3eeacdecd7c6be8a2
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 17:52:12 +0200

    LatheGeometry: Fix wrong removal

--- src/geometries/LatheGeometry.js
+++ src/geometries/LatheGeometry.js
@@ -181,6 +181,7 @@ function LatheBufferGeometry( points, segments, phiStart, phiLength ) {
 }
 
 LatheBufferGeometry.prototype = Object.create( BufferGeometry.prototype );
+LatheBufferGeometry.prototype.constructor = LatheBufferGeometry;
 LatheBufferGeometry.prototype.isLatheBufferGeometry = true;
 
 
