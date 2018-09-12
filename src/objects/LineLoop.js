0b62484f2ff937d7e978808575a9c356ed7c697f
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:08:58 +0200

    LineLoop: Remove hard coded type

--- src/objects/LineLoop.js
+++ src/objects/LineLoop.js
@@ -8,8 +8,6 @@ function LineLoop( geometry, material ) {
 
 	Line.call( this, geometry, material );
 
-	this.type = 'LineLoop';
-
 }
 
 LineLoop.prototype = Object.assign( Object.create( Line.prototype ), {
