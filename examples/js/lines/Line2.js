3e8ca8f9ace0b0e8559586b6519efa27088b6a82
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:10:36 +0200

    Line2: Remove hard coded type

--- examples/js/lines/Line2.js
+++ examples/js/lines/Line2.js
@@ -7,8 +7,6 @@ THREE.Line2 = function ( geometry, material ) {
 
 	THREE.LineSegments2.call( this );
 
-	this.type = 'Line2';
-
 	this.geometry = geometry !== undefined ? geometry : new THREE.LineGeometry();
 	this.material = material !== undefined ? material : new THREE.LineMaterial( { color: Math.random() * 0xffffff } );
 
