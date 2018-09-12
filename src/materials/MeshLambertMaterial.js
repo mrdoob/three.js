5ffc031b7d8e74aae12c8ea0cf64e641368b3a89
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:06:48 +0200

    MeshLambertMaterial: Remove hard coded type

--- src/materials/MeshLambertMaterial.js
+++ src/materials/MeshLambertMaterial.js
@@ -44,8 +44,6 @@ function MeshLambertMaterial( parameters ) {
 
 	Material.call( this );
 
-	this.type = 'MeshLambertMaterial';
-
 	this.color = new Color( 0xffffff ); // diffuse
 
 	this.map = null;
