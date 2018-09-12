f1daec43b487fec747391db86dca13d3423f6d82
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:05:47 +0200

    RectAreaLight: Remove hard coded type

--- src/lights/RectAreaLight.js
+++ src/lights/RectAreaLight.js
@@ -8,8 +8,6 @@ function RectAreaLight( color, intensity, width, height ) {
 
 	Light.call( this, color, intensity );
 
-	this.type = 'RectAreaLight';
-
 	this.width = ( width !== undefined ) ? width : 10;
 	this.height = ( height !== undefined ) ? height : 10;
 
