8d55ba8f52179ad211969de0482684f5078cb32a
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:05:26 +0200

    HemisphereLight: Remove hard coded type

--- src/lights/HemisphereLight.js
+++ src/lights/HemisphereLight.js
@@ -10,8 +10,6 @@ function HemisphereLight( skyColor, groundColor, intensity ) {
 
 	Light.call( this, skyColor, intensity );
 
-	this.type = 'HemisphereLight';
-
 	this.castShadow = undefined;
 
 	this.position.copy( Object3D.DefaultUp );
