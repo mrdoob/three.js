9387d4ddb6c9669648c68204b92ee85b246617bb
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:09:26 +0200

    Points: Remove hard coded type

--- src/objects/Points.js
+++ src/objects/Points.js
@@ -14,8 +14,6 @@ function Points( geometry, material ) {
 
 	Object3D.call( this );
 
-	this.type = 'Points';
-
 	this.geometry = geometry !== undefined ? geometry : new BufferGeometry();
 	this.material = material !== undefined ? material : new PointsMaterial( { color: Math.random() * 0xffffff } );
 
