10c9015107d454e1b9dcf690b5ad3ec30b5c6ff9
Tristan VALCKE <valcketristan@gmail.com>
2018-09-12 13:00:49 +0200

    QuadraticBezierCurve3: Remove hard coded type

--- src/extras/curves/QuadraticBezierCurve3.js
+++ src/extras/curves/QuadraticBezierCurve3.js
@@ -7,8 +7,6 @@ function QuadraticBezierCurve3( v0, v1, v2 ) {
 
 	Curve.call( this );
 
-	this.type = 'QuadraticBezierCurve3';
-
 	this.v0 = v0 || new Vector3();
 	this.v1 = v1 || new Vector3();
 	this.v2 = v2 || new Vector3();
