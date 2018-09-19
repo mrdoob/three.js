5c70ba0441a1cbad58c6195d7e993114b0736e3a
06wj <06wj@163.com>
2018-09-19 11:10:02 +0800

    fixed sprite raycast bug when sizeAttenuation is false

--- src/objects/Sprite.js
+++ src/objects/Sprite.js
@@ -104,6 +104,12 @@ Sprite.prototype = Object.assign( Object.create( Object3D.prototype ), {
 			viewWorldMatrix.getInverse( this.modelViewMatrix ).premultiply( this.matrixWorld );
 			mvPosition.setFromMatrixPosition( this.modelViewMatrix );
 
+			if ( ! this.material.sizeAttenuation ) {
+
+				worldScale.multiplyScalar( - mvPosition.z );
+
+			}
+
 			var rotation = this.material.rotation;
 			var sin, cos;
 			if ( rotation !== 0 ) {
