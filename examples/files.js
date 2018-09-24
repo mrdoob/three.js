73d77bece263753f87ad7823013ee72953d03aae
WestLangley <WestLangley@users.noreply.github.com>
2018-09-23 23:29:46 -0400

    Correct typo

--- examples/files.js
+++ examples/files.js
@@ -173,7 +173,7 @@ var files = {
 		"webgl_materials_texture_manualmipmap",
 		"webgl_materials_texture_partialupdate",
 		"webgl_materials_texture_rotation",
-		"webgl_materials_texture3d_volume1",
+		"webgl_materials_texture3d_volume",
 		"webgl_materials_translucency",
 		"webgl_materials_transparency",
 		"webgl_materials_variations_basic",
