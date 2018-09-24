67eb3c4f6bba237fe517cccf8a48af22774a708d
Dukuo <dilip.ramirez@gmail.com>
2018-09-24 17:08:10 -0300

    Better vr variable handling. Refactor of #14914

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -300,7 +300,13 @@ function WebGLRenderer( parameters ) {
 
 	// vr
 
-	var vr = ( typeof navigator !== 'undefined' && 'xr' in navigator ) ? new WebXRManager( _this ) : new WebVRManager( _this );
+	var vr = null;
+
+	if ( typeof navigator !== 'undefined' ) {
+
+		vr = ( 'xr' in navigator ) ? new WebXRManager( _this ) : new WebVRManager( _this );
+
+	}
 
 	this.vr = vr;
 
