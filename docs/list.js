dca696c783b2bc62154f48aa13dcdcc7007d1b54
gogoend <gogoend@qq.com>
2018-10-16 00:28:02 +0800

    Translate the the page about creacting VR scene,and fix the link of this page.

--- docs/list.js
+++ docs/list.js
@@ -447,7 +447,7 @@ var list = {
 
 			"进阶": {
 				"如何更新场景": "manual/zh/introduction/How-to-update-things",
-				"如何创建VR内容": "manual/en/introduction/How-to-create-VR-content",
+				"如何创建VR内容": "manual/zh/introduction/How-to-create-VR-content",
 				"矩阵变换": "manual/zh/introduction/Matrix-transformations",
 				"动画系统": "manual/zh/introduction/Animation-system"
 			},
