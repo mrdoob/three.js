f7cb409b54ae40dc69cee02040b178af483fe0f0
Mr.doob <info@mrdoob.com>
2018-11-01 17:59:12 -0400

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -26394,6 +26394,11 @@
 
 		Texture.call( this, video, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy );
 
+		this.format = format !== undefined ? format : RGBFormat;
+
+		this.minFilter = minFilter !== undefined ? minFilter : LinearFilter;
+		this.magFilter = magFilter !== undefined ? magFilter : LinearFilter;
+
 		this.generateMipmaps = false;
 
 	}
--- build/three.module.js
+++ build/three.module.js
@@ -26388,6 +26388,11 @@ function VideoTexture( video, mapping, wrapS, wrapT, magFilter, minFilter, forma
 
 	Texture.call( this, video, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy );
 
+	this.format = format !== undefined ? format : RGBFormat;
+
+	this.minFilter = minFilter !== undefined ? minFilter : LinearFilter;
+	this.magFilter = magFilter !== undefined ? magFilter : LinearFilter;
+
 	this.generateMipmaps = false;
 
 }
