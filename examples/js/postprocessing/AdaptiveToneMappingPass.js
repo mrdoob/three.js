e57bfd219684287ee9f812a12cbf93fd3288d141
Mugen87 <michael.herzog@human-interactive.org>
2018-11-15 16:19:02 +0100

    Examples: Fix mimpapping usage of rendertargets

--- examples/js/postprocessing/AdaptiveToneMappingPass.js
+++ examples/js/postprocessing/AdaptiveToneMappingPass.js
@@ -211,6 +211,7 @@ THREE.AdaptiveToneMappingPass.prototype = Object.assign( Object.create( THREE.Pa
 
 		// We only need mipmapping for the current luminosity because we want a down-sampled version to sample in our adaptive shader
 		pars.minFilter = THREE.LinearMipMapLinearFilter;
+		pars.generateMipmaps = true;
 		this.currentLuminanceRT = new THREE.WebGLRenderTarget( this.resolution, this.resolution, pars );
 		this.currentLuminanceRT.texture.name = "AdaptiveToneMappingPass.cl";
 
