72b90d5f8331916b2a28b4ff2df67e803a38be5b
ChenX <maple6@vip.qq.com>
2018-11-19 16:18:42 +0800

    add ACESFilmToneMapping

--- src/constants.js
+++ src/constants.js
@@ -55,6 +55,7 @@ export var LinearToneMapping = 1;
 export var ReinhardToneMapping = 2;
 export var Uncharted2ToneMapping = 3;
 export var CineonToneMapping = 4;
+export var ACESFilmToneMapping = 5;
 export var UVMapping = 300;
 export var CubeReflectionMapping = 301;
 export var CubeRefractionMapping = 302;
