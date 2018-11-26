9175199cbf646c5278919b69a3a127ed3fa81762
Mr.doob <info@mrdoob.com>
2018-11-25 18:46:17 -0800

    SkinnedMesh: Removed THREE.Geometry support.

--- src/Three.Legacy.js
+++ src/Three.Legacy.js
@@ -71,6 +71,7 @@ import { LOD } from './objects/LOD.js';
 import { Points } from './objects/Points.js';
 import { Sprite } from './objects/Sprite.js';
 import { Skeleton } from './objects/Skeleton.js';
+import { SkinnedMesh } from './objects/SkinnedMesh.js';
 import { WebGLRenderer } from './renderers/WebGLRenderer.js';
 import { WebGLRenderTarget } from './renderers/WebGLRenderTarget.js';
 import { WebGLShadowMap } from './renderers/webgl/WebGLShadowMap.js';
@@ -1011,6 +1012,12 @@ Object.defineProperty( Skeleton.prototype, 'useVertexTexture', {
 	}
 
 } );
+
+SkinnedMesh.prototype.initBones = function () {
+
+	console.error( 'THREE.SkinnedMesh: initBones() has been removed.' );
+
+};
 
 Object.defineProperty( Curve.prototype, '__arcLengthDivisions', {
 
