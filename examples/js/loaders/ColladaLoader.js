ccd4a59e9ae1e8976452d834f8739fcc36b2e1f1
James Baicoianu <james_github@baicoianu.com>
2018-12-07 11:10:34 -0800

    Fixed whitespace

--- examples/js/loaders/ColladaLoader.js
+++ examples/js/loaders/ColladaLoader.js
@@ -2131,7 +2131,7 @@ THREE.ColladaLoader.prototype = {
 						var semantic = child.getAttribute( 'semantic' );
 						var offset = parseInt( child.getAttribute( 'offset' ) );
 						var set = parseInt( child.getAttribute( 'set' ) );
-						var inputname = (set > 0 ? semantic + set : semantic);
+						var inputname = ( set > 0 ? semantic + set : semantic );
 						primitive.inputs[ inputname ] = { id: id, offset: offset };
 						primitive.stride = Math.max( primitive.stride, offset + 1 );
 						if ( semantic === 'TEXCOORD' ) primitive.hasUV = true;
