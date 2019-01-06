1727c90eacbe605bd2a7e669d8aeb99a3ea232d6
Andrej <anhr@mail.ru>
2019-01-06 13:35:06 +0700

    remove examples/js/gui/StereoEffect.js, examples/js/gui/gui.js

--- examples/js/effects/StereoEffect.js
+++ examples/js/effects/StereoEffect.js
@@ -6,6 +6,8 @@
  * @authod anhr / https://github.com/anhr/
 */
 
+//Attenttion!!! Save this file as UTF-8 for localization.
+
 THREE.StereoEffectParameters = {
 
 	//spatialMultiplex
@@ -134,4 +136,239 @@ THREE.StereoEffect = function (renderer, options) {
 
 		renderer.setScissorTest( false );
 	};
+};
+
+//Some functions, you can use for localization of dat.GUI and saving user settings.
+//See https://github.com/dataarts/dat.gui/blob/master/API.md about dat.GUI API.
+THREE.gui = {
+	controllerNameAndTitle: function (controller, name, title) {
+		var elPropertyName = controller.__li.querySelector(".property-name");
+		elPropertyName.innerHTML = name;
+		if (title != undefined)
+			elPropertyName.title = title;
+	},
+
+	//returns the "primary language" subtag of the version of the browser.
+	//See the "Syntax" paragraph of RFC 4646 https://tools.ietf.org/html/rfc4646#section-2.1 for details.
+	getLanguageCode: function () {
+
+		//returns the language version of the browser.
+		function getLocale() {
+			if (!navigator) {
+				console.error("getLocale() failed! !navigator");
+				return "";
+			}
+
+			if (
+				(typeof navigator.languages != 'undefined')
+				&& (typeof navigator.languages != 'unknown')//for IE6
+				&& (navigator.languages.length > 0)
+				)
+				return navigator.languages[0];//Chrome
+
+			//IE
+			if (navigator.language) {
+				return navigator.language;
+			}
+			else if (navigator.browserLanguage) {
+				return navigator.browserLanguage;
+			}
+			else if (navigator.systemLanguage) {
+				return navigator.systemLanguage;
+			}
+			else if (navigator.userLanguage) {
+				return navigator.userLanguage;
+			}
+
+			console.error("getLocale() failed!");
+			return "";
+		}
+
+		var parts = getLocale().toLowerCase().match(/([a-z]+)(?:-([a-z]+))?/),
+			lang = parts[1],
+			locale = parts[2];
+		return lang;
+	},
+
+	get_cookie: function (cookie_name, defaultValue) {
+		if (!navigator.cookieEnabled) {
+			console.error('navigator.cookieEnabled = ' + navigator.cookieEnabled);
+			//Enable cookie
+			//Chrome: Settings/Show advanced settings.../Privacy/Content settings.../Cookies/Allow local data to be set
+			return;
+		}
+		var results = document.cookie.match('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
+
+		if (results)
+			return (unescape(results[2]));
+		if (typeof defaultValue == 'undefined')
+			return '';
+		return defaultValue;
+	},
+
+	SetCookie: function (name, value, settings) {
+		if (!navigator.cookieEnabled) {
+			console.error('navigator.cookieEnabled = ' + navigator.cookieEnabled);
+			//Enable cookie
+			//Chrome: Settings/Show advanced settings.../Privacy/Content settings.../Cookies/Allow local data to be set
+			return;
+		}
+
+		value = value.toString();
+		var cookie_date = new Date();
+		cookie_date.setTime(cookie_date.getTime() + 1000 * 60 * 60 * 24 * 365);
+		document.cookie = name + "=" + value + ((typeof settings == 'undefined') ? '' : settings) + "; expires=" + cookie_date.toGMTString();
+		return 0;
+	}
+}
+
+//Adds StereoEffects folder into dat.GUI.
+//See https://github.com/dataarts/dat.gui/blob/master/API.md about dat.GUI API.
+//gui: dat.GUI object.
+//options: See options of StereoEffect above for details.
+//guiParams:
+//{
+//	getLanguageCode: Your custom getLanguageCode() function. 
+//		returns the "primary language" subtag of the language version of the browser.
+//		Examples: "en" - English language, "ru" Russian.
+//		See the "Syntax" paragraph of RFC 4646 https://tools.ietf.org/html/rfc4646#section-2.1 for details.
+//		Default returns the 'en' is English language.
+//	SetCookie: Your custom SetCookie(name, value) function for saving of the StereoEffects settings
+//			name: name of current setting
+//			value: current setting
+//			Default function is nothing saving.
+//	lang: Object with localized language values
+//	scale: scale of allowed values. Default is 1.
+//}
+THREE.gui.stereoEffect = function (gui, options, guiParams) {
+
+	if (guiParams == undefined) guiParams = {};
+	guiParams.scale = guiParams.scale || 1;
+
+	function getLanguageCode() { return 'en'; }//Default language is English
+	if (guiParams.getLanguageCode != undefined) getLanguageCode = guiParams.getLanguageCode;
+
+	function SetCookie(name, value, settings) { }
+	if (guiParams.SetCookie != undefined) SetCookie = guiParams.SetCookie;
+
+	//Localization
+
+	var lang = {
+		stereoEffects: 'Stereo effects',
+
+		spatialMultiplexName: 'Spatial  multiplex',
+		spatialMultiplexTitle: 'Choose a way to do spatial multiplex.',
+
+		spatialMultiplexs: {
+			'Mono': THREE.StereoEffectParameters.spatialMultiplexsIndexs.Mono,
+			'Side by side': THREE.StereoEffectParameters.spatialMultiplexsIndexs.SbS, //https://en.wikipedia.org/wiki/DVB_3D-TV#Side_by_side
+			'Top and bottom': THREE.StereoEffectParameters.spatialMultiplexsIndexs.TaB, //https://en.wikipedia.org/wiki/DVB_3D-TV#Top_and_bottom
+		},
+
+		eyeSeparationName: 'Eye separation',
+		eyeSeparationTitle: 'The distance between left and right cameras.',
+
+		zeroParallaxName: 'Zero parallax',
+		zeroParallaxTitle: 'Distance to objects with zero parallax.',
+
+		defaultButton: 'Default',
+		defaultTitle: 'Restore default stereo effects settings.',
+	}
+
+	var languageCode = getLanguageCode();
+	switch (languageCode) {
+		case 'ru'://Russian language
+			lang.stereoEffects = 'Стерео эффекты';//'Stereo effects'
+
+			lang.spatialMultiplexName = 'Мультиплекс';//'Spatial  multiplex'
+			lang.spatialMultiplexTitle = 'Выберите способ создания пространственного мультиплексирования.';
+
+			lang.spatialMultiplexs = {
+				'Моно': THREE.StereoEffectParameters.spatialMultiplexsIndexs.Mono,//Mono
+				'Слева направо': THREE.StereoEffectParameters.spatialMultiplexsIndexs.SbS, //https://en.wikipedia.org/wiki/DVB_3D-TV#Side_by_side
+				'Сверху вниз': THREE.StereoEffectParameters.spatialMultiplexsIndexs.TaB, //https://en.wikipedia.org/wiki/DVB_3D-TV#Top_and_bottom
+			};
+
+			lang.eyeSeparationName = 'Развод камер';
+			lang.eyeSeparationTitle = 'Расстояние между левой и правой камерами.';
+
+			lang.zeroParallaxName = 'Параллакс 0';
+			lang.zeroParallaxTitle = 'Расстояние до объектов с нулевым параллаксом.';
+
+			lang.defaultButton = 'Восстановить';
+			lang.defaultTitle = 'Восстановить настройки стерео эффектов по умолчанию.';
+			break;
+		default://Custom language
+			if ((guiParams.lang == undefined) || (guiParams.lang.languageCode != languageCode))
+				break;
+
+			Object.keys(guiParams.lang).forEach(function (key) {
+				if (lang[key] === undefined)
+					return;
+				lang[key] = guiParams.lang[key];
+			});
+	}
+
+	//
+
+	gui.remember(options);
+
+	function displayControllers(value) {
+		var display = value == THREE.StereoEffectParameters.spatialMultiplexsIndexs.Mono ? 'none' : 'block';
+		controllerEyeSep.__li.style.display = display;
+		controllerDefaultF.__li.style.display = display;
+		controllerZeroParallax.__li.style.display = display;
+	}
+
+	var fStereoEffects = gui.addFolder(lang.stereoEffects);//Stero effects folder
+
+	function controllerNameAndTitle(controller, name, title) {
+		var elPropertyName = controller.__li.querySelector(".property-name");
+		elPropertyName.innerHTML = name;
+		elPropertyName.title = title;
+	}
+
+	//Spatial  multiplex
+	var controllerSpatialMultiplex = fStereoEffects.add(options, 'spatialMultiplex',
+		lang.spatialMultiplexs).onChange(function (value) {
+
+			value = parseInt(value);
+
+			displayControllers(value);
+
+			SetCookie('spatialMultiplex', value);
+		});
+	controllerNameAndTitle(controllerSpatialMultiplex, lang.spatialMultiplexName, lang.spatialMultiplexTitle);
+
+	//eyeSeparation
+	//http://paulbourke.net/papers/vsmm2007/stereoscopy_workshop.pdf
+	var controllerEyeSep = fStereoEffects.add(options.stereo, 'eyeSep', 0, 1 * guiParams.scale, 0.001 * guiParams.scale)
+		.onChange(function (value) {
+			SetCookie('eyeSeparation', value);
+		});
+	controllerNameAndTitle(controllerEyeSep, lang.eyeSeparationName, lang.eyeSeparationTitle);
+
+	//Zero parallax
+	//http://paulbourke.net/papers/vsmm2007/stereoscopy_workshop.pdf
+	var minMax = (60 - (400 / 9)) * guiParams.scale + 400 / 9;
+	var controllerZeroParallax = fStereoEffects.add(options, 'zeroParallax', -minMax, minMax)
+		.onChange(function (value) {
+			SetCookie('zeroParallax', value);
+		});
+	controllerNameAndTitle(controllerZeroParallax, lang.zeroParallaxName, lang.zeroParallaxTitle);
+
+	//default button
+	var defaultParams = {
+		defaultF: function (value) {
+			options.stereo.eyeSep = new THREE.StereoCamera().eyeSep;
+			controllerEyeSep.setValue(options.stereo.eyeSep);
+
+			options.zeroParallax = THREE.StereoEffectParameters.zeroParallaxDefault;
+			controllerZeroParallax.setValue(THREE.StereoEffectParameters.zeroParallaxDefault);
+		},
+	}
+	var controllerDefaultF = fStereoEffects.add(defaultParams, 'defaultF');
+	controllerNameAndTitle(controllerDefaultF, lang.defaultButton, lang.defaultTitle);
+
+	displayControllers(options.spatialMultiplex);
 };
