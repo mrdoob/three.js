a635b9cc07184f3a3ef300b3fade4e6326ccd2e1
Andrej <anhr@mail.ru>
2019-01-07 10:38:50 +0700

    Cosmetic changes
    Cosmetic changes according
    https://github.com/mrdoob/three.js/wiki/Mr.doob%27s-Code-Style%E2%84%A2
    rules

--- examples/js/effects/StereoEffect.js
+++ examples/js/effects/StereoEffect.js
@@ -46,7 +46,7 @@ THREE.StereoEffectParameters = {
 //	zeroParallax: Distance to objects with zero parallax.
 //		See http://paulbourke.net/papers/vsmm2007/stereoscopy_workshop.pdf for details.
 //		Default is THREE.StereoEffectParameters.zeroParallaxDefault
-//	get_cookie: Your custom get_cookie(name, defaultValue) function for loading of the StereoEffects settings
+//	getCookie: Your custom getCookie(name, defaultValue) function for loading of the StereoEffects settings
 //			name: name of current setting
 //			defaultValue: default setting
 //		returns a StereoEffects setting, saved before or defaultValue
@@ -57,21 +57,21 @@ THREE.StereoEffect = function ( renderer, options ) {
 	options.stereo = new THREE.StereoCamera();
 	options.stereo.aspect = 0.5;
 
-	function get_cookie( cookie_name, defaultValue ) {
+	function getCookie( cookieName, defaultValue ) {
 
 		return defaultValue;
 
 	}
 
 	options = options || {};
-	if ( options.get_cookie != undefined )
-		get_cookie = options.get_cookie;
-	if ( options.spatialMultiplex == undefined )
-		options.spatialMultiplex = get_cookie( 'spatialMultiplex', THREE.StereoEffectParameters.spatialMultiplexsIndexs.SbS );//Use 'Side by side' for compability with previous version of THREE.StereoEffect
-	if ( options.zeroParallax == undefined )
-		options.zeroParallax = parseInt( get_cookie( 'zeroParallax', THREE.StereoEffectParameters.zeroParallaxDefault ) );
+	if ( options.getCookie !== undefined )
+		getCookie = options.getCookie;
+	if ( options.spatialMultiplex === undefined )
+		options.spatialMultiplex = getCookie( 'spatialMultiplex', THREE.StereoEffectParameters.spatialMultiplexsIndexs.SbS );//Use 'Side by side' for compability with previous version of THREE.StereoEffect
+	if ( options.zeroParallax === undefined )
+		options.zeroParallax = parseInt( getCookie( 'zeroParallax', THREE.StereoEffectParameters.zeroParallaxDefault ) );
 
-	options.stereo.eyeSep = ( get_cookie( 'eyeSeparation', new THREE.StereoCamera().eyeSep ) * 10000 ) / 10000;
+	options.stereo.eyeSep = ( getCookie( 'eyeSeparation', new THREE.StereoCamera().eyeSep ) * 10000 ) / 10000;
 
 	this.setEyeSeparation = function ( eyeSep ) {
 
@@ -91,53 +91,53 @@ THREE.StereoEffect = function ( renderer, options ) {
 
 		if ( camera.parent === null ) camera.updateMatrixWorld();
 
-		var size = renderer.getSize();
+		var _size = renderer.getSize();
 
 		if ( renderer.autoClear ) renderer.clear();
 		renderer.setScissorTest( true );
 
-		var xL, yL, widthL, heightL,
-			xR, yR, widthR, heightR,
-			parallax = options.zeroParallax,
-			spatialMultiplex = parseInt( options.spatialMultiplex ),
-			spatialMultiplexsIndexs = THREE.StereoEffectParameters.spatialMultiplexsIndexs;
+		var _xL, _yL, _widthL, _heightL,
+			_xR, _yR, _widthR, _heightR,
+			_parallax = options.zeroParallax,
+			_spatialMultiplex = parseInt( options.spatialMultiplex ),
+			_spatialMultiplexsIndexs = THREE.StereoEffectParameters.spatialMultiplexsIndexs;
 
-		switch ( spatialMultiplex ) {
+		switch ( _spatialMultiplex ) {
 
-			case spatialMultiplexsIndexs.Mono://Mono
-				renderer.setScissor( 0, 0, size.width, size.height );
-				renderer.setViewport( 0, 0, size.width, size.height );
+			case _spatialMultiplexsIndexs.Mono://Mono
+				renderer.setScissor( 0, 0, _size.width, _size.height );
+				renderer.setViewport( 0, 0, _size.width, _size.height );
 				renderer.render( scene, camera );
 				renderer.setScissorTest( false );
 				return;
 
-			case spatialMultiplexsIndexs.SbS://'Side by side'
+			case _spatialMultiplexsIndexs.SbS://'Side by side'
 
-				var width = size.width / 2;
+				var _width = _size.width / 2;
 
-				xL = 0 + parallax;		yL = 0; widthL = width; heightL = size.height;
-				xR = width - parallax;	yR = 0; widthR = width; heightR = size.height;
-
-				break;
-
-			case spatialMultiplexsIndexs.TaB://'Top and bottom'
-
-				xL = 0 + parallax; yL = 0;					widthL = size.width; heightL = size.height / 2;
-				xR = 0 - parallax; yR = size.height / 2;	widthR = size.width; heightR = size.height / 2;
+				_xL = 0 + _parallax;		_yL = 0; _widthL = _width; _heightL = _size.height;
+				_xR = _width - _parallax;	_yR = 0; _widthR = _width; _heightR = _size.height;
 
 				break;
-			default: console.error( 'THREE.StereoEffect.render: Invalid "Spatial  multiplex" parameter: ' + spatialMultiplex );
+
+			case _spatialMultiplexsIndexs.TaB://'Top and bottom'
+
+				_xL = 0 + _parallax; _yL = 0;					_widthL = _size.width; _heightL = _size.height / 2;
+				_xR = 0 - _parallax; _yR = _size.height / 2;	_widthR = _size.width; _heightR = _size.height / 2;
+
+				break;
+			default: console.error( 'THREE.StereoEffect.render: Invalid "Spatial  multiplex" parameter: ' + _spatialMultiplex );
 
 		}
 
 		options.stereo.update( camera );
 
-		renderer.setScissor( xL, yL, widthL, heightL );
-		renderer.setViewport( xL, yL, widthL, heightL );
+		renderer.setScissor( _xL, _yL, _widthL, _heightL );
+		renderer.setViewport( _xL, _yL, _widthL, _heightL );
 		renderer.render( scene, options.stereo.cameraL );
 
-		renderer.setScissor( xR, yR, widthR, heightR );
-		renderer.setViewport( xR, yR, widthR, heightR );
+		renderer.setScissor( _xR, _yR, _widthR, _heightR );
+		renderer.setViewport( _xR, _yR, _widthR, _heightR );
 		renderer.render( scene, options.stereo.cameraR );
 
 		renderer.setScissorTest( false );
@@ -151,10 +151,10 @@ THREE.StereoEffect = function ( renderer, options ) {
 THREE.gui = {
 	controllerNameAndTitle: function ( controller, name, title ) {
 
-		var elPropertyName = controller.__li.querySelector( ".property-name" );
-		elPropertyName.innerHTML = name;
-		if ( title != undefined )
-			elPropertyName.title = title;
+		var _elPropertyName = controller.__li.querySelector( ".property-name" );
+		_elPropertyName.innerHTML = name;
+		if ( title !== undefined )
+			_elPropertyName.title = title;
 
 	},
 
@@ -173,8 +173,8 @@ THREE.gui = {
 			}
 
 			if (
-				( typeof navigator.languages != 'undefined' )
-				&& ( typeof navigator.languages != 'unknown' )//for IE6
+				( navigator.languages !== undefined )
+				&& ( typeof navigator.languages !== 'unknown' )//for IE6
 				&& ( navigator.languages.length > 0 )
 			)
 				return navigator.languages[ 0 ];//Chrome
@@ -204,13 +204,16 @@ THREE.gui = {
 		}
 
 		var parts = getLocale().toLowerCase().match( /([a-z]+)(?:-([a-z]+))?/ ),
-			lang = parts[ 1 ],
+			_lang = parts[ 1 ],
 			locale = parts[ 2 ];
-		return lang;
+		return _lang;
 
 	},
 
-	get_cookie: function ( cookie_name, defaultValue ) {
+	// Loading settings saved by THREE.gui.setCookie.
+	// cookieName: name of current setting
+	// defaultValue: default setting
+	getCookie: function ( cookieName, defaultValue ) {
 
 		if ( ! navigator.cookieEnabled ) {
 
@@ -220,17 +223,21 @@ THREE.gui = {
 			return;
 
 		}
-		var results = document.cookie.match( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
+		var _results = document.cookie.match( '(^|;) ?' + cookieName + '=([^;]*)(;|$)' );
 
-		if ( results )
-			return ( unescape( results[ 2 ] ) );
-		if ( typeof defaultValue == 'undefined' )
+		if ( _results )
+			return ( unescape( _results[ 2 ] ) );
+		if ( defaultValue === undefined )
 			return '';
 		return defaultValue;
 
 	},
 
-	SetCookie: function ( name, value, settings ) {
+	// Saving settings.
+	// name: name of current setting
+	// value: current setting
+	// settings: additional settings
+	setCookie: function ( name, value, settings ) {
 
 		if ( ! navigator.cookieEnabled ) {
 
@@ -242,9 +249,9 @@ THREE.gui = {
 		}
 
 		value = value.toString();
-		var cookie_date = new Date();
-		cookie_date.setTime( cookie_date.getTime() + 1000 * 60 * 60 * 24 * 365 );
-		document.cookie = name + "=" + value + ( ( typeof settings == 'undefined' ) ? '' : settings ) + "; expires=" + cookie_date.toGMTString();
+		var _cookieDate = new Date();
+		_cookieDate.setTime( _cookieDate.getTime() + 1000 * 60 * 60 * 24 * 365 );
+		document.cookie = name + "=" + value + ( ( settings === undefined ) ? '' : settings ) + "; expires=" + _cookieDate.toGMTString();
 		return 0;
 
 	}
@@ -262,7 +269,7 @@ THREE.gui = {
 //		Examples: "en" - English language, "ru" Russian.
 //		See the "Syntax" paragraph of RFC 4646 https://tools.ietf.org/html/rfc4646#section-2.1 for details.
 //		Default returns the 'en' is English language.
-//	SetCookie: Your custom SetCookie(name, value) function for saving of the StereoEffects settings
+//	setCookie: Your custom setCookie(name, value) function for saving of the StereoEffects settings
 //			name: name of current setting
 //			value: current setting
 //			Default function is nothing saving.
@@ -271,7 +278,7 @@ THREE.gui = {
 //}
 THREE.gui.stereoEffect = function ( gui, options, guiParams ) {
 
-	if ( guiParams == undefined ) guiParams = {};
+	if ( guiParams === undefined ) guiParams = {};
 	guiParams.scale = guiParams.scale || 1;
 
 	function getLanguageCode() {
@@ -279,14 +286,14 @@ THREE.gui.stereoEffect = function ( gui, options, guiParams ) {
 		return 'en';//Default language is English
 
 	}
-	if ( guiParams.getLanguageCode != undefined ) getLanguageCode = guiParams.getLanguageCode;
+	if ( guiParams.getLanguageCode !== undefined ) getLanguageCode = guiParams.getLanguageCode;
 
-	function SetCookie( name, value, settings ) { }
-	if ( guiParams.SetCookie != undefined ) SetCookie = guiParams.SetCookie;
+	function setCookie( name, value, settings ) { }
+	if ( guiParams.setCookie !== undefined ) setCookie = guiParams.setCookie;
 
 	//Localization
 
-	var lang = {
+	var _lang = {
 		stereoEffects: 'Stereo effects',
 
 		spatialMultiplexName: 'Spatial  multiplex',
@@ -309,39 +316,39 @@ THREE.gui.stereoEffect = function ( gui, options, guiParams ) {
 
 	};
 
-	var languageCode = getLanguageCode();
-	switch ( languageCode ) {
+	var _languageCode = getLanguageCode();
+	switch ( _languageCode ) {
 
 		case 'ru'://Russian language
-			lang.stereoEffects = 'Стерео эффекты';//'Stereo effects'
+			_lang.stereoEffects = 'Стерео эффекты';//'Stereo effects'
 
-			lang.spatialMultiplexName = 'Мультиплекс';//'Spatial  multiplex'
-			lang.spatialMultiplexTitle = 'Выберите способ создания пространственного мультиплексирования.';
+			_lang.spatialMultiplexName = 'Мультиплекс';//'Spatial  multiplex'
+			_lang.spatialMultiplexTitle = 'Выберите способ создания пространственного мультиплексирования.';
 
-			lang.spatialMultiplexs = {
+			_lang.spatialMultiplexs = {
 				'Моно': THREE.StereoEffectParameters.spatialMultiplexsIndexs.Mono, //Mono
 				'Слева направо': THREE.StereoEffectParameters.spatialMultiplexsIndexs.SbS, //https://en.wikipedia.org/wiki/DVB_3D-TV#Side_by_side
 				'Сверху вниз': THREE.StereoEffectParameters.spatialMultiplexsIndexs.TaB, //https://en.wikipedia.org/wiki/DVB_3D-TV#Top_and_bottom
 			};
 
-			lang.eyeSeparationName = 'Развод камер';
-			lang.eyeSeparationTitle = 'Расстояние между левой и правой камерами.';
+			_lang.eyeSeparationName = 'Развод камер';
+			_lang.eyeSeparationTitle = 'Расстояние между левой и правой камерами.';
 
-			lang.zeroParallaxName = 'Параллакс 0';
-			lang.zeroParallaxTitle = 'Расстояние до объектов с нулевым параллаксом.';
+			_lang.zeroParallaxName = 'Параллакс 0';
+			_lang.zeroParallaxTitle = 'Расстояние до объектов с нулевым параллаксом.';
 
-			lang.defaultButton = 'Восстановить';
-			lang.defaultTitle = 'Восстановить настройки стерео эффектов по умолчанию.';
+			_lang.defaultButton = 'Восстановить';
+			_lang.defaultTitle = 'Восстановить настройки стерео эффектов по умолчанию.';
 			break;
 		default://Custom language
-			if ( ( guiParams.lang == undefined ) || ( guiParams.lang.languageCode != languageCode ) )
+			if ( ( guiParams.lang === undefined ) || ( guiParams.lang._languageCode != _languageCode ) )
 				break;
 
 			Object.keys( guiParams.lang ).forEach( function ( key ) {
 
-				if ( lang[ key ] === undefined )
+				if ( _lang[ key ] === undefined )
 					return;
-				lang[ key ] = guiParams.lang[ key ];
+				_lang[ key ] = guiParams.lang[ key ];
 
 			} );
 
@@ -353,64 +360,62 @@ THREE.gui.stereoEffect = function ( gui, options, guiParams ) {
 
 	function displayControllers( value ) {
 
-		var display = value == THREE.StereoEffectParameters.spatialMultiplexsIndexs.Mono ? 'none' : 'block';
-		controllerEyeSep.__li.style.display = display;
-		controllerDefaultF.__li.style.display = display;
-		controllerZeroParallax.__li.style.display = display;
+		var _display = value == THREE.StereoEffectParameters.spatialMultiplexsIndexs.Mono ? 'none' : 'block';
+		_controllerEyeSep.__li.style.display = _display;
+		controllerDefaultF.__li.style.display = _display;
+		_controllerZeroParallax.__li.style.display = _display;
 
 	}
 
-	var fStereoEffects = gui.addFolder(lang.stereoEffects);//Stero effects folder
+	var _fStereoEffects = gui.addFolder( _lang.stereoEffects );//Stero effects folder
 
 	//Spatial multiplex
-	var controllerSpatialMultiplex = fStereoEffects.add( options, 'spatialMultiplex',
-		lang.spatialMultiplexs ).onChange( function ( value ) {
+	THREE.gui.controllerNameAndTitle( _fStereoEffects.add( options, 'spatialMultiplex',
+		_lang.spatialMultiplexs ).onChange( function ( value ) {
 
 		value = parseInt( value );
 
 		displayControllers( value );
 
-		SetCookie( 'spatialMultiplex', value );
+		setCookie( 'spatialMultiplex', value );
 
-	} );
-	THREE.gui.controllerNameAndTitle(controllerSpatialMultiplex, lang.spatialMultiplexName, lang.spatialMultiplexTitle);
+	} ), _lang.spatialMultiplexName, _lang.spatialMultiplexTitle );
 
 	//eyeSeparation
 	//http://paulbourke.net/papers/vsmm2007/stereoscopy_workshop.pdf
-	var controllerEyeSep = fStereoEffects.add( options.stereo, 'eyeSep', 0, 1 * guiParams.scale, 0.001 * guiParams.scale )
+	var _controllerEyeSep = _fStereoEffects.add( options.stereo, 'eyeSep', 0, 1 * guiParams.scale, 0.001 * guiParams.scale )
 		.onChange( function ( value ) {
 
-			SetCookie( 'eyeSeparation', value );
+			setCookie( 'eyeSeparation', value );
 
 		} );
-	THREE.gui.controllerNameAndTitle( controllerEyeSep, lang.eyeSeparationName, lang.eyeSeparationTitle );
+	THREE.gui.controllerNameAndTitle( _controllerEyeSep, _lang.eyeSeparationName, _lang.eyeSeparationTitle );
 
 	//Zero parallax
 	//http://paulbourke.net/papers/vsmm2007/stereoscopy_workshop.pdf
-	var minMax = ( 60 - ( 400 / 9 ) ) * guiParams.scale + 400 / 9;
-	var controllerZeroParallax = fStereoEffects.add( options, 'zeroParallax', - minMax, minMax )
+	var _minMax = ( 60 - ( 400 / 9 ) ) * guiParams.scale + 400 / 9;
+	var _controllerZeroParallax = _fStereoEffects.add( options, 'zeroParallax', - _minMax, _minMax )
 		.onChange( function ( value ) {
 
-			SetCookie( 'zeroParallax', value );
+			setCookie( 'zeroParallax', value );
 
 		} );
-	THREE.gui.controllerNameAndTitle( controllerZeroParallax, lang.zeroParallaxName, lang.zeroParallaxTitle );
+	THREE.gui.controllerNameAndTitle( _controllerZeroParallax, _lang.zeroParallaxName, _lang.zeroParallaxTitle );
 
 	//default button
-	var defaultParams = {
+	var controllerDefaultF = _fStereoEffects.add( {
 		defaultF: function ( value ) {
 
 			options.stereo.eyeSep = new THREE.StereoCamera().eyeSep;
-			controllerEyeSep.setValue( options.stereo.eyeSep );
+			_controllerEyeSep.setValue( options.stereo.eyeSep );
 
 			options.zeroParallax = THREE.StereoEffectParameters.zeroParallaxDefault;
-			controllerZeroParallax.setValue( THREE.StereoEffectParameters.zeroParallaxDefault );
+			_controllerZeroParallax.setValue( THREE.StereoEffectParameters.zeroParallaxDefault );
 
 		},
 
-	};
-	var controllerDefaultF = fStereoEffects.add( defaultParams, 'defaultF' );
-	THREE.gui.controllerNameAndTitle( controllerDefaultF, lang.defaultButton, lang.defaultTitle );
+	}, 'defaultF' );
+	THREE.gui.controllerNameAndTitle( controllerDefaultF, _lang.defaultButton, _lang.defaultTitle );
 
 	displayControllers( options.spatialMultiplex );
 
