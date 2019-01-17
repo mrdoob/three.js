c0060d7a56befd89ab37acd675696b87952464d7
Andrej <anhr@mail.ru>
2019-01-17 19:08:17 +0700

    Cosmetic changes

--- examples/js/effects/StereoEffect.js
+++ examples/js/effects/StereoEffect.js
@@ -48,7 +48,7 @@ THREE.StereoEffectParameters = {
 //		Default is THREE.StereoEffectParameters.zeroParallaxDefault
 //	camera: THREE.PerspectiveCamera. Use the camera key if you want control cameras focus.
 //	far: Camera frustum far plane. The far key uses for correct calculation default values of Eye separation. Default is 10.
-//	cookie: Your custom cookie function for saving and loading of the StereoEffects settings.
+//	cookie: Your custom cookie function for saving and loading of the StereoEffects settings. Default cookie is not saving settings.
 //}
 THREE.StereoEffect = function ( renderer, options ) {
 
