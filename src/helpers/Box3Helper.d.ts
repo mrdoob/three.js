39e681e090f8e378d7cdae2baacfb7f4f4546851
lu wang <Canada_wl@aliyun.com>
2019-01-24 11:09:25 -0500

    Adding Box3Helper.d.ts

---
+++ src/helpers/Box3Helper.d.ts
@@ -0,0 +1,8 @@
+import { Object3D } from './../core/Object3D';
+import { Box3 } from './../math/Box3';
+import { Color } from './../math/Color';
+import { LineSegments } from './../objects/LineSegments';
+
+export class Box3Helper {
+  constructor(object?: Object3D, color?: Color);
+}
