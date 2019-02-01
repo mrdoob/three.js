6e2df6a4b6da6ad409a5df1bbcf10f523fab5d58
Olli Etuaho <olli.etuaho@gmail.com>
2019-02-02 01:01:54 +0200

    Port UnrealBloomPass to EffectComposer2
    The bloom pass is a bit of a special case since the same buffer acts as both an input and output buffer for it, and the pass needs to be able to sample it as a texture. Passing the default framebuffer to the UnrealBloomPass2 doesn't work.
    
    So EffectComposer2 now supports having a pass that's only able to render to a texture. In case it's the final pass when rendering to the default framebuffer, then EffectComposer2 will copy the pass output from the texture to the default framebuffer.

--- examples/js/postprocessing2/EffectComposer2.js
+++ examples/js/postprocessing2/EffectComposer2.js
@@ -92,6 +92,29 @@ Object.assign( THREE.EffectComposer2.prototype, {
 
 	},
 
+	getColorOutputBuffer: function ( bufferConfig, writeToFinalColorTarget, finalColorRenderTarget ) {
+
+		// finalColorRenderTarget null or undefined means rendering to the default framebuffer.
+		// If the buffer must be a texture then we can't render directly to that but will do an extra copy
+		// step afterwards.
+		var canUseFinalColorTarget = finalColorRenderTarget || !bufferConfig.mustBeTexture;
+		if ( writeToFinalColorTarget && canUseFinalColorTarget ) {
+
+			return finalColorRenderTarget;
+
+		} else if ( bufferConfig.isInput ) {
+
+			// The output buffer is also used as a color input in the pass.
+			return this.colorReadBuffer;
+
+		} else {
+
+			return this.colorWriteBuffer;
+
+		}
+
+	},
+
 	gatherBuffersForPass: function ( pass, writeToFinalColorTarget, finalColorRenderTarget, buffers ) {
 
 		var writesToColorWriteBuffer = false;
@@ -103,20 +126,14 @@ Object.assign( THREE.EffectComposer2.prototype, {
 
 				if ( bufferConfig.isOutput ) {
 
-					var buffer;
-					if ( writeToFinalColorTarget ) {
-						buffer = finalColorRenderTarget;
-						if ( bufferConfig.isInput && !bufferConfig.clear )
-						{
-							// The output buffer is also used as a color input in the pass.
-							// Copy the color buffer from the previous pass to the final target before executing the pass.
-							this.copyPass.render( this.renderer, [ buffer, this.colorReadBuffer ], 0 );
-						}
-					} else if ( bufferConfig.isInput ) {
+					var buffer = this.getColorOutputBuffer( bufferConfig, writeToFinalColorTarget, finalColorRenderTarget );
+					if ( buffer == finalColorRenderTarget && bufferConfig.isInput && !bufferConfig.clear )
+					{
 						// The output buffer is also used as a color input in the pass.
-						buffer = this.colorReadBuffer;
-					} else {
-						buffer = this.colorWriteBuffer;
+						// Copy the color buffer from the previous pass to the final target before executing the pass.
+						this.copyPass.render( this.renderer, [ buffer, this.colorReadBuffer ], 0 );
+					}
+					if ( buffer == this.colorWriteBuffer ) {
 						writesToColorWriteBuffer = true;
 					}
 					buffers.push( buffer );
@@ -133,6 +150,24 @@ Object.assign( THREE.EffectComposer2.prototype, {
 		}
 
 		return writesToColorWriteBuffer;
+
+	},
+
+	passWroteToBuffer: function( pass, writeToFinalColorTarget, finalColorRenderTarget ) {
+
+		for ( i = 0; i < pass.bufferConfigs.length; ++i ) {
+
+			var bufferConfig = pass.bufferConfigs[i];
+			if ( bufferConfig.content == THREE.EffectComposer2.BufferContent.Color ) {
+
+				if ( bufferConfig.isOutput ) {
+					return this.getColorOutputBuffer( bufferConfig, writeToFinalColorTarget, finalColorRenderTarget );
+				}
+
+			}
+
+		}
+		return null;
 
 	},
 
@@ -162,14 +197,23 @@ Object.assign( THREE.EffectComposer2.prototype, {
 
 			if ( pass.enabled === false ) continue;
 
-			var writeToFinalRenderTarget = this.isLastEnabledPass( i );
+			var writeToFinalColorTarget = this.isLastEnabledPass( i );
 
 			var buffers = [];
-			var writesToColorWriteBuffer = this.gatherBuffersForPass( pass, writeToFinalRenderTarget, finalColorRenderTarget, buffers );
+			var writesToColorWriteBuffer = this.gatherBuffersForPass( pass, writeToFinalColorTarget, finalColorRenderTarget, buffers );
 
 			pass.render( this.renderer, buffers, deltaTime );
 
-			if ( writesToColorWriteBuffer ) {
+			// Check if we need to copy the final output from a texture to the default framebuffer.
+			if ( writeToFinalColorTarget && !finalColorRenderTarget ) {
+
+				var wroteToBuffer = this.passWroteToBuffer( pass, writeToFinalColorTarget, finalColorRenderTarget );
+				if ( wroteToBuffer ) {
+					// In case the pass is only able to use a texture as its write buffer, copy the result to screen here.
+					this.copyPass.render( this.renderer, [ null, wroteToBuffer ], 0 );
+				}
+
+			} else if ( writesToColorWriteBuffer ) {
 
 				this.swapBuffers();
 
@@ -229,15 +273,18 @@ THREE.IntermediateBufferConfig = function() {
 	// color format.
 	this.content = THREE.EffectComposer2.BufferContent.Color;
 
-	// Set to true if this buffer is read by the pass.
+	// Set to true if the pass intends to either blend new things on top of this buffer or reads it as a texture.
 	this.isInput = false;
 
-	// Set to true if this buffer is written by the pass.
+	// Set to true if this buffer is written to by the pass.
 	this.isOutput = true;
 
-	// Set to true if the pass clears this buffer before writing to it. Should only be set if isOutput is also true.
-	// If this is set then isInput is essentially ignored and the contents of the buffer are undefined when it is
-	// given to the pass.
+	// Set to true if this buffer needs to be stored in a texture. Should only be set if isInput is also true.
+	this.mustBeTexture = false;
+
+	// Set to true if the pass clears this buffer before reading from or writing to it. Should only be set if isOutput
+	// is also true. If this is set then isInput is essentially ignored and the contents of the buffer are undefined
+	// when it is given to the pass.
 	this.clear = false;
 
 };
