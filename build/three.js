580af89dff86d31636568d748c8adaff0927f011
Mr.doob <info@mrdoob.com>
2019-02-12 12:35:07 -0800

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -185,7 +185,7 @@
 
 	} );
 
-	var REVISION = '101';
+	var REVISION = '102dev';
 	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
--- build/three.module.js
+++ build/three.module.js
@@ -179,7 +179,7 @@ Object.assign( EventDispatcher.prototype, {
 
 } );
 
-var REVISION = '101';
+var REVISION = '102dev';
 var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 var CullFaceNone = 0;
 var CullFaceBack = 1;
