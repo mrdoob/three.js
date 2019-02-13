e248e7f557617f9eb81013b75222f8c39f2ae334
Marc-Sefan Cassola <maccesch@synphonyte.com>
2019-02-13 10:45:44 +0000

    fixed scoping issue

--- src/renderers/webgl/WebGLShadowMap.js
+++ src/renderers/webgl/WebGLShadowMap.js
@@ -100,6 +100,8 @@ function WebGLShadowMap( _renderer, _objects, maxTextureSize ) {
 
 		if ( lights.length === 0 ) return;
 
+		var currentRenderTarget = _renderer.getRenderTarget();
+
 		var _state = _renderer.state;
 
 		// Set GL state for depth map.
