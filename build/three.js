420b6eeeae979b2b81f2c7c75bf4c372e472c78e
Mr.doob <info@mrdoob.com>
2019-03-04 19:11:06 -0800

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -14753,7 +14753,8 @@
 			// Ignore background in AR
 			// TODO: Reconsider this.
 
-			var session = renderer.vr.getSession();
+			var vr = renderer.vr;
+			var session = vr.getSession && vr.getSession();
 
 			if ( session && session.environmentBlendMode === 'additive' ) {
 
