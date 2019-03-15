471b9404eef23a4ec11bb6504de74cb9a1f196bc
Nathan Bierema <nbierema@gmail.com>
2019-03-15 16:30:31 -0400

    Add TypeScript definitions for some utils

---
+++ examples/jsm/utils/GeometryUtils.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @deprecated
+ */
+export namespace GeometryUtils {
+	/**
+	 * @deprecated Use {@link Geometry#merge geometry.merge( geometry2, matrix, materialIndexOffset )} instead.
+	 */
+	export function merge(geometry1: any, geometry2: any, materialIndexOffset?: any): any;
+	/**
+	 * @deprecated Use {@link Geometry#center geometry.center()} instead.
+	 */
+	export function center(geometry: any): any;
+}
