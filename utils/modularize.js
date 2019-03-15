817323f932d826f14206c5ae88049100406e86a5
Mr.doob <info@mrdoob.com>
2019-03-14 20:23:09 -0700

    JSM: Added GLTFExporter and MTLLoader to modularize.

--- utils/modularize.js
+++ utils/modularize.js
@@ -11,9 +11,11 @@ var files = [
 	{ path: 'controls/OrbitControls.js', ignoreList: [] },
 	{ path: 'controls/MapControls.js', ignoreList: [] },
 	{ path: 'controls/TrackballControls.js', ignoreList: [] },
-	// { file: 'controls/TransformControls.js', ignoreList: [] },
+	// { path: 'controls/TransformControls.js', ignoreList: [] },
+	{ path: 'exporters/GLTFExporter.js', ignoreList: [] },
 	{ path: 'loaders/GLTFLoader.js', ignoreList: [ 'NoSide', 'Matrix2', 'DDSLoader', 'DRACOLoader', 'BufferGeometryUtils' ] },
-	{ path: 'loaders/OBJLoader.js', ignoreList: [] }
+	{ path: 'loaders/OBJLoader.js', ignoreList: [] },
+	{ path: 'loaders/MTLLoader.js', ignoreList: [] }
 ];
 
 for ( var i = 0; i < files.length; i ++ ) {
