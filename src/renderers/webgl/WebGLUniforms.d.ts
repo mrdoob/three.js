08ce26f24064662e1bdccea07592c0c0b2672111
Mr.doob <info@mrdoob.com>
2019-03-26 21:03:09 -0700

    WebGLUniforms: Remove WebGLRenderer dependency.

--- src/renderers/webgl/WebGLUniforms.d.ts
+++ src/renderers/webgl/WebGLUniforms.d.ts
@@ -1,17 +1,13 @@
 import { WebGLProgram } from './WebGLProgram';
-import { WebGLRenderer } from './../WebGLRenderer';
 import { WebGLTextures } from './WebGLTextures';
 
 export class WebGLUniforms {
-  constructor(gl: any, program: WebGLProgram, renderer: WebGLRenderer, textures: WebGLTextures);
+  constructor(gl: any, program: WebGLProgram);
 
-  renderer: WebGLRenderer;
-
-  setValue(gl: any, name: string, value: any): void;
-  set(gl: any, object: any, name: string): void;
+  setValue(gl: any, name: string, value: any, textures: WebGLTextures): void;
   setOptional(gl: any, object: any, name: string): void;
 
-  static upload(gl: any, seq: any, values: any[], renderer: WebGLRenderer, textures: WebGLTextures): void;
+  static upload(gl: any, seq: any, values: any[], textures: WebGLTextures): void;
   static seqWithValue(seq: any, values: any[]): any[];
   static splitDynamic(seq: any, values: any[]): any[];
   static evalDynamic(seq: any, values: any[], object: any, camera: any): any[];
