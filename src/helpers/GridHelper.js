0ddc8153705a9dd16a9b0e0b1307bdbcf1ae8c71
Mugen87 <michael.herzog@human-interactive.org>
2019-03-27 11:10:26 +0100

    GridHelper: Simplify .clone()

--- src/helpers/GridHelper.js
+++ src/helpers/GridHelper.js
@@ -72,9 +72,7 @@ GridHelper.prototype = Object.assign( Object.create( LineSegments.prototype ), {
 
 	clone: function () {
 
-		var parameters = this.parameters;
-
-		return new this.constructor( parameters.size, parameters.divisions, parameters.color1, parameters.color2 );
+		return new this.constructor().copy( this );
 
 	}
 
