7bd030b3fe11a0e86cf7b45e3dc4bce826ce7a00
Temdog007 <temdog007@yahoo.com>
2019-04-14 15:27:56 -0700

    Use abs when using pow on negative number

--- src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl.js
@@ -14,7 +14,7 @@ vec4 LinearToGamma( in vec4 value, in float gammaFactor ) {
 }
 
 vec4 sRGBToLinear( in vec4 value ) {
-	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
+	return vec4( mix( pow( abs( value.rgb * 0.9478672986 + vec3( 0.0521327014 ) ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
 }
 
 vec4 LinearTosRGB( in vec4 value ) {
