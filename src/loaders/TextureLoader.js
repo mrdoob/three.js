7016e1758b2c940c4a48312807a66ff99553754d
aardgoose <angus.sawyer@gmail.com>
2019-04-20 17:07:42 +0100

    use optimal image loader if available for textures

--- src/loaders/TextureLoader.js
+++ src/loaders/TextureLoader.js
@@ -4,6 +4,7 @@
 
 import { RGBAFormat, RGBFormat } from '../constants.js';
 import { ImageLoader } from './ImageLoader.js';
+import { ImageBitmapLoader } from './ImageBitmapLoader.js';
 import { Texture } from '../textures/Texture.js';
 import { DefaultLoadingManager } from './LoadingManager.js';
 
@@ -22,7 +23,8 @@ Object.assign( TextureLoader.prototype, {
 
 		var texture = new Texture();
 
-		var loader = new ImageLoader( this.manager );
+		var loader = ( typeof createImageBitmap === undefined ) ? new ImageLoader( this.manager ) : new ImageBitmapLoader( this.manager );
+
 		loader.setCrossOrigin( this.crossOrigin );
 		loader.setPath( this.path );
 
