9023205af7b0b0630043ecf9ed117e4217663d46
lin onetwo <linonetwo012@gmail.com>
2019-04-28 21:36:57 +0800

    refactor: put two class into a single .d.ts file

--- examples/jsm/renderers/CSS2DRenderer.d.ts
+++ examples/jsm/renderers/CSS2DRenderer.d.ts
@@ -2,7 +2,13 @@
  * @author mrdoob / http://mrdoob.com/
  */
 import * as THREE from 'three';
-export default class CSS2DRenderer {
+
+export class CSS2DObject extends THREE.Object3D {
+    public element: HTMLElement;
+    constructor(element: HTMLElement);
+}
+
+export class CSS2DRenderer {
     public domElement: HTMLDivElement;
     private width;
     private height;
