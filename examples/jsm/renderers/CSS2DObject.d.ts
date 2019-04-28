90514e90eeeb4297e60a9df87d2a3aa8bdc14642
lin onetwo <linonetwo012@gmail.com>
2019-04-28 21:36:09 +0800

    Delete CSS2DObject.d.ts

--- examples/jsm/renderers/CSS2DObject.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @author mrdoob / http://mrdoob.com/
- */
-import * as THREE from 'three';
-export default class CSS2DObject extends THREE.Object3D {
-    public element: HTMLElement;
-    constructor(element: HTMLElement);
-}
