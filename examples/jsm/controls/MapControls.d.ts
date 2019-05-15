f8355b07158ef76d8a380154c35cc389064b724e
linbingquan <695601626@qq.com>
2019-05-16 00:58:37 +0800

    JSM: Clean up.

--- examples/jsm/controls/MapControls.d.ts
+++ examples/jsm/controls/MapControls.d.ts
@@ -1,71 +1,71 @@
 import {
-  Camera,
-  EventDispatcher,
-  MOUSE,
-  Object3D,
-  Vector3
-} from '../../../src/Three';
+	Camera,
+	EventDispatcher,
+	MOUSE,
+	Object3D,
+	Vector3
+} from "../../../src/Three";
 
 export class MapControls extends EventDispatcher {
-  constructor(object: Camera, domElement?: HTMLElement);
+	constructor(object: Camera, domElement?: HTMLElement);
 
-  object: Camera;
-  domElement: HTMLElement | HTMLDocument;
+	object: Camera;
+	domElement: HTMLElement | HTMLDocument;
 
-  // API
-  enabled: boolean;
-  target: Vector3;
+	// API
+	enabled: boolean;
+	target: Vector3;
 
-  enableZoom: boolean;
-  zoomSpeed: number;
-  minDistance: number;
-  maxDistance: number;
-  enableRotate: boolean;
-  rotateSpeed: number;
-  enablePan: boolean;
-  keyPanSpeed: number;
-  maxZoom: number;
-  minZoom: number;
-  panSpeed: number;
-  autoRotate: boolean;
-  autoRotateSpeed: number;
-  minPolarAngle: number;
-  maxPolarAngle: number;
-  minAzimuthAngle: number;
-  maxAzimuthAngle: number;
-  enableKeys: boolean;
-  screenSpacePanning: boolean;
-  keys: { LEFT: number; UP: number; RIGHT: number; BOTTOM: number };
-  mouseButtons: { LEFT: MOUSE; MIDDLE: MOUSE; RIGHT: MOUSE };
-  enableDamping: boolean;
-  dampingFactor: number;
-  target0: Vector3;
-  position0: Vector3;
-  zoom0: number;
+	enableZoom: boolean;
+	zoomSpeed: number;
+	minDistance: number;
+	maxDistance: number;
+	enableRotate: boolean;
+	rotateSpeed: number;
+	enablePan: boolean;
+	keyPanSpeed: number;
+	maxZoom: number;
+	minZoom: number;
+	panSpeed: number;
+	autoRotate: boolean;
+	autoRotateSpeed: number;
+	minPolarAngle: number;
+	maxPolarAngle: number;
+	minAzimuthAngle: number;
+	maxAzimuthAngle: number;
+	enableKeys: boolean;
+	screenSpacePanning: boolean;
+	keys: { LEFT: number; UP: number; RIGHT: number; BOTTOM: number };
+	mouseButtons: { LEFT: MOUSE; MIDDLE: MOUSE; RIGHT: MOUSE };
+	enableDamping: boolean;
+	dampingFactor: number;
+	target0: Vector3;
+	position0: Vector3;
+	zoom0: number;
 
-  rotateLeft(angle?: number): void;
+	rotateLeft(angle?: number): void;
 
-  rotateUp(angle?: number): void;
+	rotateUp(angle?: number): void;
 
-  panLeft(distance?: number): void;
+	panLeft(distance?: number): void;
 
-  panUp(distance?: number): void;
+	panUp(distance?: number): void;
 
-  pan(deltaX: number, deltaY: number): void;
+	pan(deltaX: number, deltaY: number): void;
 
-  dollyIn(dollyScale: number): void;
+	dollyIn(dollyScale: number): void;
 
-  dollyOut(dollyScale: number): void;
+	dollyOut(dollyScale: number): void;
 
-  saveState(): void;
+	saveState(): void;
 
-  update(): boolean;
+	update(): boolean;
 
-  reset(): void;
+	reset(): void;
 
-  dispose(): void;
+	dispose(): void;
 
-  getPolarAngle(): number;
+	getPolarAngle(): number;
 
-  getAzimuthalAngle(): number;
+	getAzimuthalAngle(): number;
 }
