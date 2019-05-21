3d15c64cf550816716fdccc251b3e17f98ebfa20
Mugen87 <michael.herzog@human-interactive.org>
2019-05-21 13:38:32 +0200

    JSM: Added module and TS file for AssimpJSONLoader.

--- utils/modularize.js
+++ utils/modularize.js
@@ -32,6 +32,7 @@ var files = [
 	{ path: 'exporters/STLExporter.js', dependencies: [], ignoreList: [] },
 	{ path: 'exporters/TypedGeometryExporter.js', dependencies: [], ignoreList: [] },
 
+	{ path: 'loaders/AssimpJSONLoader.js', dependencies: [], ignoreList: [] },
 	{ path: 'loaders/AssimpLoader.js', dependencies: [], ignoreList: [] },
 	{ path: 'loaders/BabylonLoader.js', dependencies: [], ignoreList: [] },
 	{ path: 'loaders/BVHLoader.js', dependencies: [], ignoreList: [ 'Bones' ] },
