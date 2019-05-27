e879da10e2ccc9d05200c9f36259638e3b02ac8a
De Cuyper <jeroen.decuyper@orbitus.be>
2019-05-27 20:17:03 +0200

    fix lgtm error

--- examples/js/renderers/WebGLDeferredRenderer.js
+++ examples/js/renderers/WebGLDeferredRenderer.js
@@ -606,7 +606,7 @@ THREE.WebGLDeferredRenderer = function ( parameters ) {
 
 		}
 
-		updateDeferredColorUniforms( renderer, scene, camera, geometry, material, group );
+		updateDeferredColorUniforms( renderer, scene, camera, geometry, material );
 
 		material.uniforms.samplerLight.value = _compLight.renderTarget2.texture;
 
