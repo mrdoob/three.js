6459e0c99dea6c6988029b64aa0f8e1b0563cdc2
Mugen87 <michael.herzog@human-interactive.org>
2019-05-31 14:24:54 +0200

    Examples: Restore reference

--- examples/jsm/shaders/TranslucentShader.js
+++ examples/jsm/shaders/TranslucentShader.js
@@ -1,6 +1,11 @@
 /**
  * @author daoshengmu / http://dsmu.me/
  *
+ * ------------------------------------------------------------------------------------------
+ * Subsurface Scattering shader
+ * Base on GDC 2011 – Approximating Translucency for a Fast, Cheap and Convincing Subsurface Scattering Look
+ * https://colinbarrebrisebois.com/2011/03/07/gdc-2011-approximating-translucency-for-a-fast-cheap-and-convincing-subsurface-scattering-look/
+ *------------------------------------------------------------------------------------------
  */
 
 import {
