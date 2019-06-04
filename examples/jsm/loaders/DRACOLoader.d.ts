ec27ecbbbb7172360edf599a1132353c59a9094d
Peter Varga <soadzoor@gmail.com>
2019-06-04 16:12:28 +0200

    JSM: Added module and TS file for DRACOLoader

---
+++ examples/jsm/loaders/DRACOLoader.d.ts
@@ -0,0 +1,9 @@
+import {
+  LoadingManager
+} from '../../../src/Three';
+
+export class DRACOLoader {
+  constructor(manager?: LoadingManager);
+
+  public static setDecoderPath(path: string): void;
+}
