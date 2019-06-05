ee0a5239f75457ebe0e97eb5a15bbb158f93ef34
Peter Varga <soadzoor@gmail.com>
2019-06-05 14:02:43 +0200

    Added public functions to d.ts

--- examples/jsm/loaders/DRACOLoader.d.ts
+++ examples/jsm/loaders/DRACOLoader.d.ts
@@ -1,9 +1,22 @@
 import {
-  LoadingManager
-} from '../../../src/Three';
+  LoadingManager,
+  BufferGeometry,
+  TrianglesDrawMode,
+  TriangleStripDrawMode,
+} from '../../../build/three.module.js';
 
 export class DRACOLoader {
   constructor(manager?: LoadingManager);
 
-  public static setDecoderPath(path: string): void;
+  static setDecoderPath(path: string): void;
+  static setDecoderConfig(config: object): void;
+  static getDecoderModule(): Promise<any>;
+  static releaseDecoderModule(): void;
+
+  load(url: string, onLoad: (geometry: BufferGeometry) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void): void;
+  setPath(path: string): DRACOLoader;
+  setVerbosity(level: number): DRACOLoader;
+  setDrawMode(drawMode: TrianglesDrawModes): DRACOLoader;
+  setSkipDequantization(attributeName: 'position', skip?: boolean): DRACOLoader;
+  isVersionSupported(version: number, callback: (isVersionSupported: boolean) => any);
 }
