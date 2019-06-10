61941ecc4b3e428de0f1129a3456e34fa0250934
Rik Cabanier <rcabanier@magicleap.com>
2019-06-10 14:18:46 -0700

    disable reference space

--- examples/js/vr/HelioWebXRPolyfill.js
+++ examples/js/vr/HelioWebXRPolyfill.js
@@ -149,7 +149,7 @@ if ( /(Helio)/g.test( navigator.userAgent ) && "xr" in navigator ) {
 
 							return tempRequestReferenceSpace( {
 								type: "stationary",
-								subtype: "identity"
+								subtype: "position-disabled"
 							} );
 
 						};
