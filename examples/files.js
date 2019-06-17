4c7d6f467a2a44ec18612692498932379f10472d
sunag <sunag@users.noreply.github.com>
2019-06-17 20:02:55 -0300

    add example in file list

--- examples/files.js
+++ examples/files.js
@@ -159,6 +159,7 @@ var files = {
 		"webgl_materials_envmaps",
 		"webgl_materials_envmaps_exr",
 		"webgl_materials_envmaps_hdr",
+		"webgl_materials_envmaps_hdr_nodes",
 		"webgl_materials_envmaps_parallax",
 		"webgl_materials_grass",
 		"webgl_materials_lightmap",
