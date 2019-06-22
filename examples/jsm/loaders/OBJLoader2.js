d6989d5e7c559d5150c4baef0b017e229a6990b5
Kai Salmen <myself@kaisalmen.de>
2019-06-22 22:26:34 +0200

    Transformed basic obj2 example to jsm

--- examples/jsm/loaders/OBJLoader2.js
+++ examples/jsm/loaders/OBJLoader2.js
@@ -1,16 +1,17 @@
 /**
- * @author Kai Salmen / www.kaisalmen.de
+ * @author Kai Salmen / https://kaisalmen.de
+ * Development repository: https://github.com/kaisalmen/WWOBJLoader
  */
 
 import {
 	DefaultLoadingManager,
 	FileLoader,
 	Group
-} from "../../node_modules/three/build/three.module.js";
+} from "../../../build/three.module.js";
 
-import { OBJLoader2Parser } from "./worker/parallel/OBJLoader2Parser.js";
-import { MeshReceiver } from "./shared/MeshReceiver.js";
-import { MaterialHandler } from "./shared/MaterialHandler.js";
+import { OBJLoader2Parser } from "./obj2/worker/parallel/OBJLoader2Parser.js";
+import { MeshReceiver } from "./obj2/shared/MeshReceiver.js";
+import { MaterialHandler } from "./obj2/shared/MaterialHandler.js";
 
 /**
  * Use this class to load OBJ data from files or to parse OBJ data from an arraybuffer
