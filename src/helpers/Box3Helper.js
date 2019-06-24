fb0e0475a732465f3aab5fe56ff19b63dde0df47
Mugen87 <michael.herzog@human-interactive.org>
2019-06-24 10:43:31 +0200

    Box3Helper: Fix ctor color parameter.

--- src/helpers/Box3Helper.js
+++ src/helpers/Box3Helper.js
@@ -9,13 +9,13 @@ import { Float32BufferAttribute } from '../core/BufferAttribute.js';
 import { BufferGeometry } from '../core/BufferGeometry.js';
 import { Object3D } from '../core/Object3D.js';
 
-function Box3Helper( box, hex ) {
+function Box3Helper( box, color ) {
 
 	this.type = 'Box3Helper';
 
 	this.box = box;
 
-	var color = ( hex !== undefined ) ? hex : 0xffff00;
+	color = color || 0xffff00;
 
 	var indices = new Uint16Array( [ 0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7 ] );
 
