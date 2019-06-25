9908299a06ba0b3622452fefd127264dd31f60cb
ranbuch <ranbuch@gmail.com>
2019-06-25 20:47:05 +0300

    ready for review

--- src/renderers/webgl/WebGLRenderLists.js
+++ src/renderers/webgl/WebGLRenderLists.js
@@ -130,10 +130,10 @@ function WebGLRenderList() {
 
 	}
 
-	function sort() {
+	function sort( costumeOpaqueSort, costumeTransparentSort ) {
 
-		if ( opaque.length > 1 ) opaque.sort( painterSortStable );
-		if ( transparent.length > 1 ) transparent.sort( reversePainterSortStable );
+		if ( opaque.length > 1 ) opaque.sort( typeof costumeOpaqueSort === 'function' ? costumeOpaqueSort : painterSortStable );
+		if ( transparent.length > 1 ) transparent.sort( typeof costumeTransparentSort === 'function' ? costumeTransparentSort : reversePainterSortStable );
 
 	}
 
