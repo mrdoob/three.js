e62e9cc8a4250c4436df5d3c86cacd236ee733fa
Daniel Sturk <danielsturk1@gmail.com>
2019-07-08 16:31:23 -0400

    Removed unnecessary varying 'vViewPosition'

--- src/renderers/shaders/ShaderLib/skybox_vert.glsl.js
+++ src/renderers/shaders/ShaderLib/skybox_vert.glsl.js
@@ -3,13 +3,6 @@ varying vec3 vWorldPosition;
 
 #include <common>
 
-#if NUM_CLIPPING_PLANES == 0
-	#if ! defined( VARYING_VVIEWPOSITION )
-   		varying vec3 vViewPosition;
-   		#define VARYING_VVIEWPOSITION 1
-   #endif
-#endif
-
 void main() {
 
 	vWorldPosition = transformDirection( position, modelMatrix );
@@ -17,7 +10,6 @@ void main() {
 	#include <begin_vertex>
 	#include <project_vertex>
 
-	vViewPosition = - mvPosition.xyz;
 	gl_Position.z = gl_Position.w; // set z to camera.far
 	// important because otherwise the cube used for rendering the cubeMap might
 	// get clipped
