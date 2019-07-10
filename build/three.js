297e59a654f2949dea753c15cf3fc7157f23f986
Mr.doob <info@mrdoob.com>
2019-07-10 23:28:56 +0200

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -186,7 +186,8 @@
 	} );
 
 	var REVISION = '107dev';
-	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
+	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
+	var TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
 	var CullFaceFront = 2;
@@ -49233,6 +49234,7 @@
 	exports.StringKeyframeTrack = StringKeyframeTrack;
 	exports.SubtractEquation = SubtractEquation;
 	exports.SubtractiveBlending = SubtractiveBlending;
+	exports.TOUCH = TOUCH;
 	exports.TangentSpaceNormalMap = TangentSpaceNormalMap;
 	exports.TetrahedronBufferGeometry = TetrahedronBufferGeometry;
 	exports.TetrahedronGeometry = TetrahedronGeometry;
