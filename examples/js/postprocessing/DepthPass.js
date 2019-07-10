d42d4d46f87e950a8275e4b570540f482a1c6308
Jeff Johnson <jjxtra@gmail.com>
2019-07-10 13:14:27 -0600

    Format

--- examples/js/postprocessing/DepthPass.js
+++ examples/js/postprocessing/DepthPass.js
@@ -2,29 +2,30 @@
  * @author jjxtra / https://www.digitalruby.com
  */
 
-THREE.DepthPass = function(scene, camera)
-{
-	THREE.Pass.call(this);
+THREE.DepthPass = function ( scene, camera ) {
 
-	this.scene = scene;
+	THREE.Pass.call( this );
+
+ 	this.scene = scene;
 	this.camera = camera;
 	this.needsSwap = false;
+
 };
 
 THREE.DepthPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
 
-	constructor: THREE.DepthPass,
+ 	constructor: THREE.DepthPass,
 
-	render: function ( renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */ ) {
+ 	render: function ( renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */ ) {
 
-		this.scene.overrideMaterial = null;
+ 		this.scene.overrideMaterial = null;
 		renderer.clearDepth();
-		renderer.setRenderTarget(this.renderToScreen ? null : readBuffer);
-		renderer.context.colorMask(false, false, false, false);
-		renderer.context.depthMask(true);
-		renderer.render(this.scene, this.camera);
-		renderer.context.colorMask(true, true, true, true);
-		
-	}
+		renderer.setRenderTarget( this.renderToScreen ? null : readBuffer );
+		renderer.context.colorMask( false, false, false, false );
+		renderer.context.depthMask( true );
+		renderer.render( this.scene, this.camera );
+		renderer.context.colorMask( true, true, true, true );
+
+ 	}
 
 } );
