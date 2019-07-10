481b061e4065e1254304a72479da1f56138b59c5
Jeff Johnson <jjxtra@gmail.com>
2019-07-09 18:48:57 -0600

    Add DepthPass shader
    Render the depth buffer, useful when you post process only part of the geometry in your application, you can then draw that geometry over the depth buffer from the non-post processed geometry.

---
+++ examples/js/postprocessing/DepthPass.js
@@ -0,0 +1,30 @@
+/**
+ * @author jjxtra / https://www.digitalruby.com
+ */
+
+THREE.DepthPass = function(scene, camera)
+{
+	THREE.Pass.call(this);
+
+	this.scene = scene;
+	this.camera = camera;
+	this.needsSwap = false;
+};
+
+THREE.DepthPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+
+	constructor: THREE.DepthPass,
+
+	render: function ( renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */ ) {
+
+		this.scene.overrideMaterial = null;
+		renderer.clearDepth();
+		renderer.setRenderTarget(this.renderToScreen ? null : readBuffer);
+		renderer.context.colorMask(false, false, false, false);
+		renderer.context.depthMask(true);
+		renderer.render(this.scene, this.camera);
+		renderer.context.colorMask(true, true, true, true);
+		
+	}
+
+} );
