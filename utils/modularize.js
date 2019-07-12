74fdf547679316e954b126aa4385f91db5d047ce
Mugen87 <michael.herzog@human-interactive.org>
2019-07-12 11:28:53 +0200

    JSM: Added module and TS file for WebVR.

--- utils/modularize.js
+++ utils/modularize.js
@@ -254,6 +254,7 @@ var files = [
 	{ path: 'vr/deprecated/GearVRController.js', dependencies: [], ignoreList: [] },
 	{ path: 'vr/PaintViveController.js', dependencies: [ { name: 'ViveController', path: 'vr/ViveController.js' } ], ignoreList: [] },
 	{ path: 'vr/ViveController.js', dependencies: [], ignoreList: [] },
+	{ path: 'vr/WebVR.js', dependencies: [], ignoreList: [] },
 
 	{ path: 'WebGL.js', dependencies: [], ignoreList: [] },
 ];
