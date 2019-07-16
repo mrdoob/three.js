84215c0d748793bf4adbd433d0630cb7d0c27d97
Mugen87 <michael.herzog@human-interactive.org>
2019-07-16 20:02:05 +0200

    JSM: Added TS files for NodeMaterial core classes.

---
+++ examples/jsm/nodes/core/AttributeNode.d.ts
@@ -0,0 +1,14 @@
+import { TempNode } from './TempNode';
+import { NodeBuilder } from './NodeBuilder';
+
+export class AttributeNode extends TempNode {
+  constructor(name: string, type?: string);
+
+  name: string;
+  nodeType: string;
+
+  getAttributeType(builder: NodeBuilder): string;
+  getType(builder: NodeBuilder): string;
+  generate(builder: NodeBuilder, output: string): string;
+  copy(source: AttributeNode): this;
+}
---
+++ examples/jsm/nodes/core/NodeLib.d.ts
@@ -0,0 +1,23 @@
+import { Node } from './Node';
+
+export interface NodeLibKeyword {
+  callback: () => void;
+  cache?: object;
+}
+
+export namespace NodeLib {
+
+  export const nodes: object;
+  export const keywords: object;
+
+  export function add(node: Node): void;
+  export function addKeyword(name: string, callback: () => void, cache?: object): void;
+  export function remove(node: Node): void;
+  export function removeKeyword(name: string): void;
+  export function get(name: string): Node;
+  export function getKeyword(name: string, material: object): any;
+  export function getKeywordData(name: string): NodeLibKeyword;
+  export function contains(name: string): boolean;
+  export function containsKeyword(name: string): boolean;
+
+}
