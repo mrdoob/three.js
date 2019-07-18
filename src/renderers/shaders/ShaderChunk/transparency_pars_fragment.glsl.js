3aa73a4f20125a727251a1ca6f03635e322ec439
Daniel Sturk <danielsturk1@gmail.com>
2019-07-18 10:26:33 -0400

    Fixed diffuseLight having fresnel factored in twice

--- src/renderers/shaders/ShaderChunk/transparency_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/transparency_pars_fragment.glsl.js
@@ -4,19 +4,16 @@ uniform float transparency;
 	// makes surface transparent without hiding the specular term
 	vec4 combineLight( const in vec3 diffuseLight, const in vec3 specularLight, const in GeometricContext geometry, const in PhysicalMaterial material ) {
 
-	vec4 diffuseFinal = vec4( diffuseLight * ( 1. - transparency ), 1. - transparency );
+	float diffuseAlpha = 1. - transparency;
 
 	vec3 fresnel = BRDF_Specular_GGX_Environment( geometry, material.specularColor, material.specularRoughness );
 	float fresnelApprox = linearToRelativeLuminance( fresnel ); // since we can't have per-channel opacity blending, we must approximate with a single blending factor
 
-	vec4 specularFinal = vec4( specularLight, fresnelApprox ); // specularLight is already premultiplied by fresnel (TODO: factor fresnel out of BSDF)
+	vec3 finalLight = diffuseLight * diffuseAlpha + specularLight;
+	float finalAlpha = mix( diffuseAlpha, 1., fresnelApprox );
 
-	vec4 fragColor = mix( diffuseFinal, specularFinal, fresnelApprox );
-
-	// the above math is performed in premultiplied alpha
-	fragColor.rgb /= fragColor.a;
-
-	return fragColor;
+	if( finalAlpha < EPSILON ) return vec4( 0 );
+	else return vec4( finalLight / finalAlpha, finalAlpha );
 
 }
 `;
