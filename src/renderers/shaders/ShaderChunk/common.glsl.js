969c06d471235b0122a55106b96eb2259a95d25b
Mr.doob <info@mrdoob.com>
2019-08-19 15:41:41 -0700

    ShaderChunk: Fixed typo.

--- src/renderers/shaders/ShaderChunk/common.glsl.js
+++ src/renderers/shaders/ShaderChunk/common.glsl.js
@@ -8,7 +8,7 @@ export default /* glsl */`
 #define EPSILON 1e-6
 
 #define saturate(a) clamp( a, 0.0, 1.0 )
-#define whiteCompliment(a) ( 1.0 - saturate( a ) )
+#define whiteComplement(a) ( 1.0 - saturate( a ) )
 
 float pow2( const in float x ) { return x*x; }
 float pow3( const in float x ) { return x*x*x; }
