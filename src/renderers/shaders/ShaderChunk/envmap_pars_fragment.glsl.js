935b583f3f9f6d1feb8418c1cba57f7c1ea0f190
arobertson0 <arobertson@threekit.com>
2019-08-20 16:59:23 -0400

    simplified envmap defines

--- src/renderers/shaders/ShaderChunk/envmap_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/envmap_pars_fragment.glsl.js
@@ -5,6 +5,12 @@ export default /* glsl */`
 
 	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( USE_CLEARCOAT_NORMALMAP ) || defined( PHONG )
 
+		#define ENV_WORLDPOS
+
+	#endif
+
+	#ifdef ENV_WORLDPOS
+
 		varying vec3 vWorldPosition;
 		uniform float refractionRatio;
 	#else
--- src/renderers/shaders/ShaderChunk/envmap_pars_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/envmap_pars_vertex.glsl.js
@@ -1,7 +1,14 @@
 export default /* glsl */`
 #ifdef USE_ENVMAP
 
-	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( USE_CLEARCOAT_NORMALMAP ) || defined( PHONG )
+	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( USE_CLEARCOAT_NORMALMAP ) ||defined( PHONG )
+
+		#define ENV_WORLDPOS
+
+	#endif
+
+	#ifdef ENV_WORLDPOS
+		
 		varying vec3 vWorldPosition;
 
 	#else
