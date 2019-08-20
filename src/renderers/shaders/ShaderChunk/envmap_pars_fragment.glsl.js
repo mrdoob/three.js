1cf9d016db1eb642278881abbdd3892d1ff3e77a
arobertson0 <arobertson@threekit.com>
2019-08-20 17:00:25 -0400

    removed unused clearcoat check from regular envmap

--- src/renderers/shaders/ShaderChunk/envmap_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/envmap_pars_fragment.glsl.js
@@ -3,7 +3,7 @@ export default /* glsl */`
 
 	uniform float reflectivity;
 
-	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( USE_CLEARCOAT_NORMALMAP ) || defined( PHONG )
+	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( PHONG )
 
 		#define ENV_WORLDPOS
 
--- src/renderers/shaders/ShaderChunk/envmap_pars_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/envmap_pars_vertex.glsl.js
@@ -1,7 +1,7 @@
 export default /* glsl */`
 #ifdef USE_ENVMAP
 
-	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( USE_CLEARCOAT_NORMALMAP ) ||defined( PHONG )
+	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) ||defined( PHONG )
 
 		#define ENV_WORLDPOS
 
