3d4846579838c00ae419497ce45245f51e39bd65
Mugen87 <michael.herzog@human-interactive.org>
2019-08-24 10:11:19 +0200

    ImageLoader: Remove url parameter check in load().

--- src/loaders/ImageLoader.js
+++ src/loaders/ImageLoader.js
@@ -17,8 +17,6 @@ ImageLoader.prototype = Object.assign( Object.create( Loader.prototype ), {
 
 	load: function ( url, onLoad, onProgress, onError ) {
 
-		if ( url === undefined ) url = '';
-
 		if ( this.path !== undefined ) url = this.path + url;
 
 		url = this.manager.resolveURL( url );
