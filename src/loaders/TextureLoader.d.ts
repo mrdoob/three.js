3ffa8d4bbc1410de88bb0eaf10927ca3c9c6068d
Mugen87 <michael.herzog@human-interactive.org>
2019-08-24 11:35:17 +0200

    TextureLoader: Inherit from Loader.

--- src/loaders/TextureLoader.d.ts
+++ src/loaders/TextureLoader.d.ts
@@ -1,3 +1,4 @@
+import { Loader } from './Loader';
 import { LoadingManager } from './LoadingManager';
 import { Texture } from './../textures/Texture';
 
@@ -5,28 +6,15 @@ import { Texture } from './../textures/Texture';
  * Class for loading a texture.
  * Unlike other loaders, this one emits events instead of using predefined callbacks. So if you're interested in getting notified when things happen, you need to add listeners to the object.
  */
-export class TextureLoader {
+export class TextureLoader extends Loader {
 
 	constructor( manager?: LoadingManager );
 
-	manager: LoadingManager;
-	crossOrigin: string;
-	withCredentials: string;
-	path: string;
-
-	/**
-	 * Begin loading from url
-	 *
-	 * @param url
-	 */
 	load(
 		url: string,
 		onLoad?: ( texture: Texture ) => void,
 		onProgress?: ( event: ProgressEvent ) => void,
 		onError?: ( event: ErrorEvent ) => void
 	): Texture;
-	setCrossOrigin( crossOrigin: string ): TextureLoader;
-	setWithCredentials( value: string ): TextureLoader;
-	setPath( path: string ): TextureLoader;
 
 }
