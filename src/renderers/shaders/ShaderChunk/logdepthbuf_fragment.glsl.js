4c1fdc2fad18c0f167b910c5d7918d2beec0326d
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-08-29 21:17:40 -0700

    remove logarithmic fragment chunks

--- src/renderers/shaders/ShaderChunk/logdepthbuf_fragment.glsl.js
+++
@@ -1,7 +0,0 @@
-export default /* glsl */`
-#if defined( USE_LOGDEPTHBUF ) && defined( USE_LOGDEPTHBUF_EXT )
-
-	gl_FragDepthEXT = log2( vFragDepth ) * logDepthBufFC * 0.5;
-
-#endif
-`;
--- src/renderers/shaders/ShaderChunk/logdepthbuf_pars_fragment.glsl.js
+++
@@ -1,8 +0,0 @@
-export default /* glsl */`
-#if defined( USE_LOGDEPTHBUF ) && defined( USE_LOGDEPTHBUF_EXT )
-
-	uniform float logDepthBufFC;
-	varying float vFragDepth;
-
-#endif
-`;
