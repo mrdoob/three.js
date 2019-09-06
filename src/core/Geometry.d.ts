e7df45389370858bb728fd605b8cb6736b71be56
Takahiro <hogehoge@gachapin.jp>
2019-09-06 11:25:20 -0700

    Remove unnecessary EventDispatcher methods definitions from Geometry.d.ts

--- src/core/Geometry.d.ts
+++ src/core/Geometry.d.ts
@@ -1,6 +1,6 @@
 import { Vector3 } from './../math/Vector3';
 import { Color } from './../math/Color';
-import { Face3, Event } from './Face3';
+import { Face3 } from './Face3';
 import { Vector2 } from './../math/Vector2';
 import { Vector4 } from './../math/Vector4';
 import { Box3 } from './../math/Box3';
@@ -251,11 +251,5 @@ export class Geometry extends EventDispatcher {
 	bones: Bone[];
 	animation: AnimationClip;
 	animations: AnimationClip[];
-
-	// EventDispatcher mixins
-	addEventListener( type: string, listener: ( event: Event ) => void ): void;
-	hasEventListener( type: string, listener: ( event: Event ) => void ): boolean;
-	removeEventListener( type: string, listener: ( event: Event ) => void ): void;
-	dispatchEvent( event: { type: string; [attachment: string]: any } ): void;
 
 }
