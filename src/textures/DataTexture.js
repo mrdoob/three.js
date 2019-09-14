bf8383d0d4bf44a9c144ecf60de4a16a6ba26304
Elias Hasle <elias.hasle@gmail.com>
2019-09-14 10:14:12 +0200

    Always init true, default data to null

--- src/textures/DataTexture.js
+++ src/textures/DataTexture.js
@@ -9,7 +9,7 @@ function DataTexture( data, width, height, format, type, mapping, wrapS, wrapT, 
 
 	Texture.call( this, null, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy, encoding );
 
-	this.image = { data: data, width: width, height: height };
+	this.image = { data: data || null, width: width || 1, height: height || 1 };
 
 	this.magFilter = magFilter !== undefined ? magFilter : NearestFilter;
 	this.minFilter = minFilter !== undefined ? minFilter : NearestFilter;
@@ -18,11 +18,8 @@ function DataTexture( data, width, height, format, type, mapping, wrapS, wrapT, 
 	this.flipY = false;
 	this.unpackAlignment = 1;
 
-	if ( data ) {
+	this.needsUpdate = true;
 
-		this.needsUpdate = true;
-
-	}
 }
 
 DataTexture.prototype = Object.create( Texture.prototype );
