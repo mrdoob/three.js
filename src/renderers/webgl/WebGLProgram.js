715f6f3c6cd67bf2eb95b9c6663dbf300022aaa2
aardgoose <angus.sawyer@gmail.com>
2019-09-16 11:06:07 +0100

    use getContext()

--- src/renderers/webgl/WebGLProgram.js
+++ src/renderers/webgl/WebGLProgram.js
@@ -686,7 +686,7 @@ function WebGLProgram( renderer, extensions, code, material, shader, parameters,
 	this.pending = false;
 	this.numMultiviewViews = numMultiviewViews;
 
-	if ( parallelShaderExt !== null && material.parallelCompile ) {
+	if ( parallelShaderExt !== null && renderer.parallelCompile ) {
 
 		if ( currentParallel < maxParallel ) {
 
@@ -766,7 +766,7 @@ Object.assign( WebGLProgram.prototype, {
 
 	compileAndLink: function ( renderer, material ) {
 
-		var gl = renderer.context;
+		var gl = renderer.getContext();
 
 		var program = this.program;
 		var vertexGlsl = this.prefixVertex + this.vertexShader;
@@ -805,7 +805,7 @@ Object.assign( WebGLProgram.prototype, {
 
 	isLinked: function ( renderer, material, sync ) {
 
-		var gl = renderer.context;
+		var gl = renderer.getContext();
 
 		if ( this.pending && currentParallel < maxParallel ) {
 
@@ -833,7 +833,7 @@ Object.assign( WebGLProgram.prototype, {
 
 		// check for link errors
 
-		var gl = renderer.context;
+		var gl = renderer.getContext();
 		var program = this.program;
 		var glVertexShader = this.vertexShader;
 		var glFragmentShader = this.fragmentShader;
