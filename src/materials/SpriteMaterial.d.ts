e2047f4f1d748c30134ec9725ef2e14bfedcf673
WestLangley <WestLangley@users.noreply.github.com>
2019-10-07 01:14:58 -0400

    SpriteMaterial: add alphaMap support

--- src/materials/SpriteMaterial.d.ts
+++ src/materials/SpriteMaterial.d.ts
@@ -5,6 +5,7 @@ import { MaterialParameters, Material } from './Material';
 export interface SpriteMaterialParameters extends MaterialParameters {
 	color?: Color | string | number;
 	map?: Texture | null;
+	alphaMap?: Texture | null;
 	rotation?: number;
 	sizeAttenuation?: boolean;
 }
@@ -15,6 +16,7 @@ export class SpriteMaterial extends Material {
 
 	color: Color;
 	map: Texture | null;
+	alphaMap: Texture | null;
 	rotation: number;
 	sizeAttenuation: boolean;
 	isSpriteMaterial: true;
