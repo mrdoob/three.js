40f015ffaf94421e1ac6378de9eed56bd5027161
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2019-10-12 09:27:06 -0700

    Add GLSL comments to explain the math a bit

--- src/renderers/shaders/ShaderChunk/morphtarget_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/morphtarget_vertex.glsl.js
@@ -1,6 +1,9 @@
 export default /* glsl */`
 #ifdef USE_MORPHTARGETS
 
+	// morphTargetBaseInfluence is set based on BufferGeometry.morphTargetsRelative value:
+	// When morphTargetsRelative is false, this is set to 1 - sum(influences); this results in position = sum((target - base) * influence)
+	// When morphTargetsRelative is true, this is set to 1; as a result, all morph targets are simply added to the base after weighting
 	transformed *= morphTargetBaseInfluence;
 	transformed += morphTarget0 * morphTargetInfluences[ 0 ];
 	transformed += morphTarget1 * morphTargetInfluences[ 1 ];
