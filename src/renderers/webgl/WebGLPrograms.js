f7875d63437229dd267ec539f3669bd41a30684f
Guilherme Avila <guilhermebav@gmail.com>
2019-10-15 14:28:19 -0300

    WebGLPrograms: lint

--- src/renderers/webgl/WebGLPrograms.js
+++ src/renderers/webgl/WebGLPrograms.js
@@ -150,7 +150,7 @@ function WebGLPrograms( renderer, extensions, capabilities ) {
 			precision: precision,
 
 			instancing: object.isInstancedMesh === true,
-			orthographic: camera.isOrthographicCamera === true, 
+			orthographic: camera.isOrthographicCamera === true,
 
 			supportsVertexTextures: vertexTextures,
 			numMultiviewViews: numMultiviewViews,
