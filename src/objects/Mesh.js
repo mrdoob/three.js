c7f05e76f926ffc6ba5e9156275ab2802cf81744
Mugen87 <michael.herzog@human-interactive.org>
2019-10-29 12:00:07 +0100

    Mesh: Add drawMode warning in .raycast().

--- src/objects/Mesh.js
+++ src/objects/Mesh.js
@@ -165,6 +165,15 @@ Mesh.prototype = Object.assign( Object.create( Object3D.prototype ), {
 
 		}
 
+		// check unsupported draw modes
+
+		if ( this.drawMode !== TrianglesDrawMode ) {
+
+			console.warn( 'THREE.Mesh: TriangleStripDrawMode and TriangleFanDrawMode are not supported by .raycast().' );
+			return;
+
+		}
+
 		var intersection;
 
 		if ( geometry.isBufferGeometry ) {
