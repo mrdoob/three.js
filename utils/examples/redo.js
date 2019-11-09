398138cacff48cd513e2b12fdc970d9502fc5d36
Callum Prentice <callum@gmail.com>
2019-11-08 18:50:37 -0800

    First pass at generating thumbnails for visual example page using Google Puppeteer. https://github.com/mrdoob/three.js/issues/17692

---
+++ utils/examples/redo.js
@@ -0,0 +1,9 @@
+// An easy way to redo poor captured examples.
+// Add an entry here, update the tweak accordingly
+// (more time before capture, inject click etc.)
+// and then run the script with this file as a parameter
+var files = {
+    "redo": [
+        "webgl_animation_cloth",
+    ]
+}
