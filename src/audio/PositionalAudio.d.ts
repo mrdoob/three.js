ce1145f11c8348ce8c156c07190398848d78d79d
Atsushi <niconyan713@gmail.com>
2019-11-28 22:34:16 +0900

    Update for PositionalAudio.d.ts

--- src/audio/PositionalAudio.d.ts
+++ src/audio/PositionalAudio.d.ts
@@ -7,12 +7,13 @@ export class PositionalAudio extends Audio {
 
 	panner: PannerNode;
 
+	getOutput(): PannerNode;
 	setRefDistance( value: number ): this;
 	getRefDistance(): number;
 	setRolloffFactor( value: number ): this;
 	getRolloffFactor(): number;
-	setDistanceModel( value: string ): this;
-	getDistanceModel(): string;
+	setDistanceModel( value: DistanceModelType ): this;
+	getDistanceModel(): DistanceModelType;
 	setMaxDistance( value: number ): this;
 	getMaxDistance(): number;
 	setDirectionalCone(
@@ -20,5 +21,6 @@ export class PositionalAudio extends Audio {
 		coneOuterAngle: number,
 		coneOuterGain: number
 	): this;
+	updateMatrixWorld( force?: boolean ): void;
 
 }
