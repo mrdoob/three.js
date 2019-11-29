6896276313bde7f9717ef1912ebcbe5690ed2ad0
Jaswant Singh Ranawat <ranawat@adobe.com>
2019-11-29 13:59:44 +0530

    adding space before paranthesis

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1286,7 +1286,7 @@ function WebGLRenderer( parameters ) {
 
 	function projectObject( object, camera, groupOrder, sortObjects ) {
 
-		if ( object.scale.x === 0 || object.scale.y === 0 || object.scale.z === 0) return;
+		if ( object.scale.x === 0 || object.scale.y === 0 || object.scale.z === 0 ) return;
 		if ( object.visible === false ) return;
 
 		var visible = object.layers.test( camera.layers );
