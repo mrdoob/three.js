ff942eaa246c92d1652db5bdcaa56c9efad6d08b
Benjamin MICHEL <benjisbrk@gmail.com>
2019-12-03 14:36:47 +0100

    Added missing semicolon

--- src/renderers/shaders/ShaderChunk/lightmap_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/lightmap_fragment.glsl.js
@@ -1,7 +1,7 @@
 export default /* glsl */`
 #ifdef USE_LIGHTMAP
 
-	vec4 lightMapTexel= texture2D( lightMap, vUv2 )
+	vec4 lightMapTexel= texture2D( lightMap, vUv2 );
 	reflectedLight.indirectDiffuse += PI * lightMapTexelToLinear( lightMapTexel ).rgb * lightMapIntensity; // factor of PI should not be present; included here to prevent breakage
 
 #endif
