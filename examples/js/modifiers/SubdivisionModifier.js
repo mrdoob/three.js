0a66d7c7c9bbcd7e5fad33943cbfa4a2400ef6e7
Mugen87 <michael.herzog@human-interactive.org>
2019-12-21 13:33:34 +0100

    SubdivisionModifier: Fix runtime error with empty uvs.

--- examples/js/modifiers/SubdivisionModifier.js
+++ examples/js/modifiers/SubdivisionModifier.js
@@ -169,7 +169,7 @@ THREE.SubdivisionModifier.prototype.modify = function ( geometry ) {
 		oldFaces = geometry.faces; // { a: oldVertex1, b: oldVertex2, c: oldVertex3 }
 		oldUvs = geometry.faceVertexUvs;
 
-		var hasUvs = oldUvs !== undefined && oldUvs.length > 0;
+		var hasUvs = oldUvs[ 0 ] !== undefined && oldUvs[ 0 ].length > 0;
 
 		if ( hasUvs ) {
 
