7752728a7e612762684f95c8c7c8992b247167c9
Don McCurdy <don.r.mccurdy@gmail.com>
2019-12-22 20:37:56 -0800

    GLTFLoader: Use depthWrite=false for transparent materials

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -2162,6 +2162,9 @@ var GLTFLoader = ( function () {
 
 			materialParams.transparent = true;
 
+			// See: https://github.com/mrdoob/three.js/issues/17706
+			materialParams.depthWrite = false;
+
 		} else if ( alphaMode === ALPHA_MODES.MASK ) {
 
 			materialParams.alphaTest = materialDef.alphaCutoff !== undefined ? materialDef.alphaCutoff : 0.5;
