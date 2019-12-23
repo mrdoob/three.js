5f81936ea4c0d56da82d53a8528c0d23064913ac
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-12-23 17:42:48 +0300

    Added an ability to reset current state of Orbit controls

--- examples/js/controls/OrbitControls.js
+++ examples/js/controls/OrbitControls.js
@@ -112,6 +112,12 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	};
 
+	this.resetState = function () {
+
+		state = STATE.NONE;
+
+	};
+
 	this.reset = function () {
 
 		scope.target.copy( scope.target0 );
