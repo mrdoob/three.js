8b74cbcb9424aa008fba61db64df264134badc5c
WestLangley <WestLangley@users.noreply.github.com>
2020-01-02 13:25:40 -0500

    LightProbeGenerator: rename method

--- examples/jsm/lights/LightProbeGenerator.d.ts
+++ examples/jsm/lights/LightProbeGenerator.d.ts
@@ -8,6 +8,6 @@ import {
 export namespace LightProbeGenerator {
 
 	export function fromCubeTexture( cubeTexture: CubeTexture ): LightProbe;
-	export function fromRenderTargetCube( renderer: WebGLRenderer, renderTargetCube: WebGLCubeRenderTarget ): LightProbe;
+	export function fromCubeRenderTarget( renderer: WebGLRenderer, cubeRenderTarget: WebGLCubeRenderTarget ): LightProbe;
 
 }
