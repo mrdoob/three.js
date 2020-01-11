8adeb9b35fff2c42d23d6a3a374b796a3ed14d00
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2020-01-11 20:50:49 +0100

    [TS] `setFromBufferAttribute` missing in `Box3`

--- src/math/Box3.d.ts
+++ src/math/Box3.d.ts
@@ -1,3 +1,4 @@
+import { BufferAttribute } from './../core/BufferAttribute';
 import { Vector3 } from './Vector3';
 import { Object3D } from './../core/Object3D';
 import { Sphere } from './Sphere';
@@ -15,6 +16,7 @@ export class Box3 {
 
 	set( min: Vector3, max: Vector3 ): this;
 	setFromArray( array: ArrayLike<number> ): this;
+	setFromBufferAttribute( bufferAttribute: BufferAttribute ): this;
 	setFromPoints( points: Vector3[] ): this;
 	setFromCenterAndSize( center: Vector3, size: Vector3 ): this;
 	setFromObject( object: Object3D ): this;
