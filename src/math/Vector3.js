23649acb80d56dd9c5726c683668b0efaaaf4160
WestLangley <WestLangley@users.noreply.github.com>
2020-01-29 13:02:16 -0500

    Zero vector orthogonal to every vector

--- src/math/Vector3.js
+++ src/math/Vector3.js
@@ -534,9 +534,11 @@ Object.assign( Vector3.prototype, {
 
 	projectOnVector: function ( v ) {
 
-		// v cannot be the zero v
+		var denominator = v.lengthSq();
 
-		var scalar = v.dot( this ) / v.lengthSq();
+		if ( denominator === 0 ) return this.set( 0, 0, 0 );
+
+		var scalar = v.dot( this ) / denominator;
 
 		return this.copy( v ).multiplyScalar( scalar );
 
@@ -563,7 +565,7 @@ Object.assign( Vector3.prototype, {
 
 		var denominator = Math.sqrt( this.lengthSq() * v.lengthSq() );
 
-		if ( denominator === 0 ) console.error( 'THREE.Vector3: angleTo() can\'t handle zero length vectors.' );
+		if ( denominator === 0 ) return Math.PI / 2;
 
 		var theta = this.dot( v ) / denominator;
 
