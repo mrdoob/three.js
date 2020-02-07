7f72136388f3fc128dbd365570db585c30b9edc9
Emmett Lalish <elalish@google.com>
2020-02-07 11:53:36 -0800

    wrong chunk

--- src/renderers/shaders/ShaderChunk/alphamap_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/alphamap_fragment.glsl.js
@@ -2,7 +2,6 @@ export default /* glsl */`
 #ifdef USE_ALPHAMAP
 
   diffuseColor.a *= texture2D( alphaMap, vUv ).g;
-  diffuseColor.a = 1.0;
 
 #endif
 `;
--- src/renderers/shaders/ShaderChunk/alphatest_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/alphatest_fragment.glsl.js
@@ -2,6 +2,7 @@ export default /* glsl */`
 #ifdef ALPHATEST
 
 	if ( diffuseColor.a < ALPHATEST ) discard;
+	diffuseColor.a = 1.0;
 
 #endif
 `;
