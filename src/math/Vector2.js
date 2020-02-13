4d6c9b7816e477676507bb9e405565675910e5e7
gonnavis <gonnavis@163.com>
2020-02-13 14:58:22 +0800

    Unary operator '-' must be followed by whitespace

--- src/math/Vector2.js
+++ src/math/Vector2.js
@@ -380,7 +380,7 @@ Object.assign( Vector2.prototype, {
 
 		// computes the angle in radians with respect to the positive x-axis
 
-		var angle = Math.atan2( -this.y, -this.x ) + Math.PI;
+		var angle = Math.atan2( - this.y, - this.x ) + Math.PI;
 
 		return angle;
 
