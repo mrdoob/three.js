b11f897812a8a48bcd81e9bd46785d07939ec59e
Mr.doob <info@mrdoob.com>
2020-02-29 14:38:37 -0800

    r114

--- build/three.module.js
+++ build/three.module.js
@@ -91,7 +91,7 @@ if ( Object.assign === undefined ) {
 
 }
 
-var REVISION = '114dev';
+var REVISION = '114';
 var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 var TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
 var CullFaceNone = 0;
