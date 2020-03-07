5256f4c5eebbbb261dad0c963766d57ecd59c4f1
WestLangley <WestLangley@users.noreply.github.com>
2020-03-06 21:06:06 -0500

    Add support for vertex colors

--- src/renderers/shaders/ShaderLib/meshmatcap_frag.glsl.js
+++ src/renderers/shaders/ShaderLib/meshmatcap_frag.glsl.js
@@ -14,6 +14,7 @@ varying vec3 vViewPosition;
 #endif
 
 #include <common>
+#include <color_pars_fragment>
 #include <uv_pars_fragment>
 #include <map_pars_fragment>
 #include <alphamap_pars_fragment>
@@ -32,6 +33,7 @@ void main() {
 
 	#include <logdepthbuf_fragment>
 	#include <map_fragment>
+	#include <color_fragment>
 	#include <alphamap_fragment>
 	#include <alphatest_fragment>
 	#include <normal_fragment_begin>
--- src/renderers/shaders/ShaderLib/meshmatcap_vert.glsl.js
+++ src/renderers/shaders/ShaderLib/meshmatcap_vert.glsl.js
@@ -11,6 +11,7 @@ varying vec3 vViewPosition;
 
 #include <common>
 #include <uv_pars_vertex>
+#include <color_pars_vertex>
 #include <displacementmap_pars_vertex>
 #include <fog_pars_vertex>
 #include <morphtarget_pars_vertex>
@@ -22,7 +23,7 @@ varying vec3 vViewPosition;
 void main() {
 
 	#include <uv_vertex>
-
+	#include <color_vertex>
 	#include <beginnormal_vertex>
 	#include <morphnormal_vertex>
 	#include <skinbase_vertex>
