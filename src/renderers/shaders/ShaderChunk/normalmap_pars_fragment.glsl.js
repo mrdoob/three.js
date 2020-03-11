32405bd0dc862b0214e0fb045dc0f41928541533
Emmett Lalish <elalish@google.com>
2020-03-11 13:10:08 -0700

    Rendering fix for degenerate UVs

--- src/renderers/shaders/ShaderChunk/normalmap_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/normalmap_pars_fragment.glsl.js
@@ -1,4 +1,4 @@
-export default /* glsl */`
+export default /* glsl */ `
 #ifdef USE_NORMALMAP
 
 	uniform sampler2D normalMap;
@@ -28,6 +28,13 @@ export default /* glsl */`
 
 		float scale = sign( st1.t * st0.s - st0.t * st1.s ); // we do not care about the magnitude
 
+		// This avoids NaNs with degenerate UV coordinates
+        if ( scale == 0.0 ) {
+
+            return normalize( surf_norm );
+
+		}
+		
 		vec3 S = normalize( ( q0 * st1.t - q1 * st0.t ) * scale );
 		vec3 T = normalize( ( - q0 * st1.s + q1 * st0.s ) * scale );
 		vec3 N = normalize( surf_norm );
