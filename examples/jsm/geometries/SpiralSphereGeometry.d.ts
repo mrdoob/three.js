e7609d93e15afa55d10b3d60975bb494877abfda
munrocket@pm.me <munrocket@pm.me>
2020-03-13 03:52:28 +0400

    Examples: new index.html page, image picker

---
+++ examples/jsm/geometries/SpiralSphereGeometry.d.ts
@@ -0,0 +1,9 @@
+import {
+	BufferGeometry
+} from '../../../src/Three';
+
+export class SpiralSphereGeometry extends BufferGeometry {
+
+	constructor( radius: number, turns: number, gapX?: number, gapY?: number, tileX?: number, tileY?: number, tiles?: number );
+
+}
