c89c4e23f11bd310a018cca85c812486f4d5e00f
Mugen87 <michael.herzog@human-interactive.org>
2020-03-28 18:46:55 +0100

    WebGLShadowMap: Stop support of Geometry when using morph targets.

--- src/renderers/webgl/WebGLShadowMap.js
+++ src/renderers/webgl/WebGLShadowMap.js
@@ -297,17 +297,9 @@ function WebGLShadowMap( _renderer, _objects, maxTextureSize ) {
 
 			var useMorphing = false;
 
-			if ( material.morphTargets === true ) {
+			if ( material.morphTargets === true && geometry.isBufferGeometry === true ) {
 
-				if ( geometry.isBufferGeometry === true ) {
-
-					useMorphing = geometry.morphAttributes && geometry.morphAttributes.position && geometry.morphAttributes.position.length > 0;
-
-				} else if ( geometry.isGeometry === true ) {
-
-					useMorphing = geometry.morphTargets && geometry.morphTargets.length > 0;
-
-				}
+				useMorphing = geometry.morphAttributes && geometry.morphAttributes.position && geometry.morphAttributes.position.length > 0;
 
 			}
 
