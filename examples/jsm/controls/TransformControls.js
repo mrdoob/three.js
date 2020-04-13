7757590f3c5b09a76c3154bead8304d26ff6065f
Vladimir Jigouline <vjigouline@gmail.com>
2020-04-13 07:20:32 +0100

    Change formatting for TransformControl zoom fix

--- examples/jsm/controls/TransformControls.js
+++ examples/jsm/controls/TransformControls.js
@@ -1188,11 +1188,8 @@ var TransformControlsGizmo = function () {
 			handle.rotation.set( 0, 0, 0 );
 			handle.position.copy( this.worldPosition );
 
-			var eyeDistance = this.worldPosition.distanceTo( this.cameraPosition );
-			// Orthographic camera zoom doesn't depend on eyeDistance, but on camera zoom factor.
-			if (this.camera.type == "OrthographicCamera") {
-				eyeDistance = 1000 / this.camera.zoom;
-			}
+			var eyeDistance = this.camera.isOrthographicCamera ? 1000 / this.camera.zoom : 
+				this.worldPosition.distanceTo( this.cameraPosition );			
 			handle.scale.set( 1, 1, 1 ).multiplyScalar( eyeDistance * this.size / 7 );
 
 			// TODO: simplify helpers and consider decoupling from gizmo
