517cc96788c78dc70ebb40d3de88613f7353cd18
Mugen87 <michael.herzog@human-interactive.org>
2020-04-30 11:22:48 +0200

    OBJLoader: Fix parsing of vertex colors.

--- examples/jsm/loaders/OBJLoader.js
+++ examples/jsm/loaders/OBJLoader.js
@@ -268,9 +268,9 @@ var OBJLoader = ( function () {
 				var src = this.colors;
 				var dst = this.object.geometry.colors;
 
-				dst.push( src[ a + 0 ], src[ a + 1 ], src[ a + 2 ] );
-				dst.push( src[ b + 0 ], src[ b + 1 ], src[ b + 2 ] );
-				dst.push( src[ c + 0 ], src[ c + 1 ], src[ c + 2 ] );
+				if ( src[ a ] !== undefined ) dst.push( src[ a + 0 ], src[ a + 1 ], src[ a + 2 ] );
+				if ( src[ b ] !== undefined ) dst.push( src[ b + 0 ], src[ b + 1 ], src[ b + 2 ] );
+				if ( src[ c ] !== undefined ) dst.push( src[ c + 0 ], src[ c + 1 ], src[ c + 2 ] );
 
 			},
 
@@ -303,12 +303,7 @@ var OBJLoader = ( function () {
 				var ic = this.parseVertexIndex( c, vLen );
 
 				this.addVertex( ia, ib, ic );
-
-				if ( this.colors.length > 0 ) {
-
-					this.addColor( ia, ib, ic );
-
-				}
+				this.addColor( ia, ib, ic );
 
 				if ( ua !== undefined && ua !== '' ) {
 
@@ -475,6 +470,12 @@ var OBJLoader = ( function () {
 									parseFloat( data[ 6 ] )
 
 								);
+
+							} else {
+
+								// if no colors are defined, add placeholders so color and vertex indices match
+
+								state.colors.push( undefined, undefined, undefined );
 
 							}
 
