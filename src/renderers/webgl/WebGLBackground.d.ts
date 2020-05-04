92120254cf98b19a21cc7f31a58e2cc8cab3d203
Filipp Keks <filipp.keks@trimble.com>
2020-05-04 13:12:56 +0300

    more types fixed

--- src/renderers/webgl/WebGLBackground.d.ts
+++ src/renderers/webgl/WebGLBackground.d.ts
@@ -5,15 +5,16 @@ import { WebGLState } from './WebGLState';
 import { WebGLObjects } from './WebGLObjects';
 import { WebGLRenderList } from './WebGLRenderLists';
 import { Scene } from '../../scenes/Scene';
+import { Camera } from "../../cameras/Camera";
 
 export class WebGLBackground {
 
-	constructor( renderer: WebGLRenderer, state: WebGLState, objects: WebGLObjects, premultipliedAlpha: any );
+	constructor( renderer: WebGLRenderer, state: WebGLState, objects: WebGLObjects, premultipliedAlpha: boolean );
 
 	getClearColor(): Color;
 	setClearColor( color: Color, alpha: number ): void;
 	getClearAlpha(): number;
 	setClearAlpha( alpha: number ): void;
-	render( renderList: WebGLRenderList, scene: Scene, camera: any, forceClear: boolean ): void;
+	render( renderList: WebGLRenderList, scene: Scene, camera: Camera, forceClear: boolean ): void;
 
 }
