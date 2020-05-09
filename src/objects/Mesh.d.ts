9e22a3477e5dcb3d6236e32da5446bf0bad1fb9f
Dusan Bosnjak <citizens.arrest@gmail.com>
2020-05-08 17:41:11 -0700

    Add generic types with defaults to all geometric objects

--- src/objects/Mesh.d.ts
+++ src/objects/Mesh.d.ts
@@ -5,15 +5,18 @@ import { Object3D } from './../core/Object3D';
 import { BufferGeometry } from '../core/BufferGeometry';
 import { Intersection } from '../core/Raycaster';
 
-export class Mesh extends Object3D {
+export class Mesh <
+	TGeometry extends Geometry | BufferGeometry = Geometry | BufferGeometry,
+	TMaterial extends Material | Material[] = Material | Material[]
+> extends Object3D {
 
 	constructor(
-		geometry?: Geometry | BufferGeometry,
-		material?: Material | Material[]
+		geometry?: TGeometry,
+		material?: TMaterial
 	);
 
-	geometry: Geometry | BufferGeometry;
-	material: Material | Material[];
+	geometry: TGeometry;
+	material: TMaterial;
 	morphTargetInfluences?: number[];
 	morphTargetDictionary?: { [key: string]: number };
 	readonly isMesh: true;
--- src/objects/Points.d.ts
+++ src/objects/Points.d.ts
@@ -10,15 +10,18 @@ import { Intersection } from '../core/Raycaster';
  *
  * @see <a href="https://github.com/mrdoob/three.js/blob/master/src/objects/ParticleSystem.js">src/objects/ParticleSystem.js</a>
  */
-export class Points extends Object3D {
+export class Points <
+	TGeometry extends Geometry | BufferGeometry = Geometry | BufferGeometry,
+	TMaterial extends Material | Material[] = Material | Material[]
+> extends Object3D {
 
 	/**
 	 * @param geometry An instance of Geometry or BufferGeometry.
 	 * @param material An instance of Material (optional).
 	 */
 	constructor(
-		geometry?: Geometry | BufferGeometry,
-		material?: Material | Material[]
+		geometry?: TGeometry,
+		material?: TMaterial
 	);
 
 	type: 'Points';
@@ -29,12 +32,12 @@ export class Points extends Object3D {
 	/**
 	 * An instance of Geometry or BufferGeometry, where each vertex designates the position of a particle in the system.
 	 */
-	geometry: Geometry | BufferGeometry;
+	geometry: TGeometry;
 
 	/**
 	 * An instance of Material, defining the object's appearance. Default is a PointsMaterial with randomised colour.
 	 */
-	material: Material | Material[];
+	material: TMaterial;
 
 	raycast( raycaster: Raycaster, intersects: Intersection[] ): void;
 	updateMorphTargets(): void;
