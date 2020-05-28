8ce127545294a2a9a3fca31ab465ebd5e25e4cc4
Mugen87 <michael.herzog@human-interactive.org>
2020-05-28 17:01:15 +0200

    Scene: Move to let/const.

--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -51,7 +51,7 @@ Scene.prototype = Object.assign( Object.create( Object3D.prototype ), {
 
 	toJSON: function ( meta ) {
 
-		var data = Object3D.prototype.toJSON.call( this, meta );
+		const data = Object3D.prototype.toJSON.call( this, meta );
 
 		if ( this.background !== null ) data.object.background = this.background.toJSON( meta );
 		if ( this.environment !== null ) data.object.environment = this.environment.toJSON( meta );
