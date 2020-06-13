abc549c057c0fa32e5d775c27d16d46ba9b6efa7
Mugen87 <michael.herzog@human-interactive.org>
2020-06-13 11:18:17 +0200

    Add WebGL1Renderer.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -204,7 +204,15 @@ function WebGLRenderer( parameters ) {
 		_canvas.addEventListener( 'webglcontextlost', onContextLost, false );
 		_canvas.addEventListener( 'webglcontextrestored', onContextRestore, false );
 
-		_gl = _context || _canvas.getContext( 'webgl2', contextAttributes ) || _canvas.getContext( 'webgl', contextAttributes ) || _canvas.getContext( 'experimental-webgl', contextAttributes );
+		if ( this.isWebGL1Renderer === true ) {
+
+			_gl = _context || _canvas.getContext( 'webgl', contextAttributes ) || _canvas.getContext( 'experimental-webgl', contextAttributes );
+
+		} else {
+
+			_gl = _context || _canvas.getContext( 'webgl2', contextAttributes ) || _canvas.getContext( 'webgl', contextAttributes ) || _canvas.getContext( 'experimental-webgl', contextAttributes );
+
+		}
 
 		if ( _gl === null ) {
 
