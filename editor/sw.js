26179dfd1fddbef4a36e455bbed5004b52db6a98
Mr.doob <info@mrdoob.com>
2020-06-19 17:00:09 -0700

    Editor: Added draco/gltf to sw.js

--- editor/sw.js
+++ editor/sw.js
@@ -18,6 +18,10 @@ const assets = [
 	'../examples/js/libs/draco/draco_encoder.js',
 	'../examples/js/libs/draco/draco_wasm_wrapper.js',
 
+	'../examples/js/libs/draco/gltf/draco_decoder.js',
+	'../examples/js/libs/draco/gltf/draco_decoder.wasm',
+	'../examples/js/libs/draco/gltf/draco_wasm_wrapper.js',
+
 	'../examples/jsm/loaders/3MFLoader.js',
 	'../examples/jsm/loaders/AMFLoader.js',
 	'../examples/jsm/loaders/ColladaLoader.js',
