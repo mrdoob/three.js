9f8b64a84fe40c7a881af8e51fc4248a60016409
jhl <jhlee@spacewalk.tech>
2020-06-24 09:36:21 +0900

    Change 'Material' to 'ShaderMaterial' in ShaderPass.d.ts #19714

--- examples/jsm/postprocessing/ShaderPass.d.ts
+++ examples/jsm/postprocessing/ShaderPass.d.ts
@@ -1,15 +1,15 @@
 import {
-	Material
+	ShaderMaterial
 } from '../../../src/Three';
 
 import { Pass } from './Pass';
 
 export class ShaderPass extends Pass {
 
-	constructor( shader: object, textureID?: string );
+	constructor(shader: object, textureID?: string);
 	textureID: string;
 	uniforms: { [name: string]: { value: any } };
-	material: Material;
+	material: ShaderMaterial;
 	fsQuad: object;
 
 }
