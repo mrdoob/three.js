e5199852df325008bf15e888b6d105a3bbd6cb7b
Mr.doob <info@mrdoob.com>
2020-06-24 10:19:38 -0700

    r118.1

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const REVISION = '118dev';
+export const REVISION = '118';
 export const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 export const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
 export const CullFaceNone = 0;
