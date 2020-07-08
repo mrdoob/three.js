fb76860acaf55c0dfa0c2e9dfb6b74b6d44ca2d9
Alex <muhmundr@gmail.com>
2020-07-08 20:37:40 +0300

    Ask if there is a revokeObjectURL function

--- examples/js/loaders/GLTFLoader.js
+++ examples/js/loaders/GLTFLoader.js
@@ -2145,7 +2145,7 @@ THREE.GLTFLoader = ( function () {
 
 			if ( isObjectURL === true ) {
 
-				URL.revokeObjectURL( sourceURI );
+			  if (URL.revokeObjectURL) URL.revokeObjectURL( sourceURI );
 
 			}
 
