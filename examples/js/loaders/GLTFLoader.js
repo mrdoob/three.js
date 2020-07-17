13dd7550c9d7f165de05639c8926539129562ee8
Don McCurdy <don.r.mccurdy@gmail.com>
2020-07-16 21:22:08 -0700

    Remove transmission example.

--- examples/js/loaders/GLTFLoader.js
+++ examples/js/loaders/GLTFLoader.js
@@ -563,6 +563,7 @@ THREE.GLTFLoader = ( function () {
 	 * Transmission Materials Extension
 	 *
 	 * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_transmission
+	 * Draft: https://github.com/KhronosGroup/glTF/pull/1698
 	 */
 	function GLTFMaterialsTransmissionExtension( parser ) {
 
