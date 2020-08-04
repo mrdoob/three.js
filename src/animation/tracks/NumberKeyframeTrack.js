2ab442f9fa7e8268e41ef7201f875025f70cb75e
Ian Purvis <ian.c.purvis@gmail.com>
2020-08-03 23:13:10 -0500

    NumberKeyframeTrack: Convert to es6 class

--- src/animation/tracks/NumberKeyframeTrack.js
+++ src/animation/tracks/NumberKeyframeTrack.js
@@ -3,16 +3,9 @@ import { KeyframeTrack } from '../KeyframeTrack.js';
 /**
  * A Track of numeric keyframe values.
  */
+class NumberKeyframeTrack extends KeyframeTrack {}
 
-function NumberKeyframeTrack( name, times, values, interpolation ) {
-
-	KeyframeTrack.call( this, name, times, values, interpolation );
-
-}
-
-NumberKeyframeTrack.prototype = Object.assign( Object.create( KeyframeTrack.prototype ), {
-
-	constructor: NumberKeyframeTrack,
+Object.assign( NumberKeyframeTrack.prototype, {
 
 	ValueTypeName: 'number'
 
