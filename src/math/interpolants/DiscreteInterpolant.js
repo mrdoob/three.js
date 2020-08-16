6957428cadbe713f0458452fe588493fef8225ff
Yomotsu <admin@yomotsu.net>
2020-08-16 19:36:56 +0900

    DiscreteInterpolant: Convert to es6 class

--- src/math/interpolants/DiscreteInterpolant.js
+++ src/math/interpolants/DiscreteInterpolant.js
@@ -6,23 +6,21 @@ import { Interpolant } from '../Interpolant.js';
  * the parameter.
  */
 
-function DiscreteInterpolant( parameterPositions, sampleValues, sampleSize, resultBuffer ) {
+class DiscreteInterpolant extends Interpolant {
 
-	Interpolant.call( this, parameterPositions, sampleValues, sampleSize, resultBuffer );
+	constructor( parameterPositions, sampleValues, sampleSize, resultBuffer ) {
 
-}
+		super( parameterPositions, sampleValues, sampleSize, resultBuffer );
 
-DiscreteInterpolant.prototype = Object.assign( Object.create( Interpolant.prototype ), {
+	}
 
-	constructor: DiscreteInterpolant,
-
-	interpolate_: function ( i1 /*, t0, t, t1 */ ) {
+	interpolate_( i1 /*, t0, t, t1 */ ) {
 
 		return this.copySampleValue_( i1 - 1 );
 
 	}
 
-} );
+}
 
 
 export { DiscreteInterpolant };
