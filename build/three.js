54222d950affeabfbbdd46340e19aec9b66ae7c5
Mr.doob <info@mrdoob.com>
2020-08-22 23:04:38 +0100

    Updated builds.

--- build/three.js
+++ build/three.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(global = global || self, factory(global.THREE = {}));
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.THREE = {}));
 }(this, (function (exports) { 'use strict';
 
 	// Polyfills
