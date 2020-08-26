cf836bbb074da979bc5eb8f131440bb4341350c1
Mr.doob <info@mrdoob.com>
2020-08-26 09:48:41 +0100

    r120

--- build/three.module.js
+++ build/three.module.js
@@ -91,7 +91,7 @@ if ( Object.assign === undefined ) {
 
 }
 
-const REVISION = '120dev';
+const REVISION = '120';
 const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
 const CullFaceNone = 0;
