1335a80460f20111d5f627dde77238c09e806b05
Makoto Yamada <woocie.rugby@gmail.com>
2020-09-04 20:43:44 +0900

    improve EventDispatcher d ts

--- src/core/EventDispatcher.d.ts
+++ src/core/EventDispatcher.d.ts
@@ -7,12 +7,14 @@ export interface Event {
 	[attachment: string]: any;
 }
 
+type Listener<E, T, U> = ( event: E & { type: T } & { target: U } ) => void;
+
 /**
  * JavaScript events for custom objects
  *
  * @source src/core/EventDispatcher.js
  */
-export class EventDispatcher {
+export class EventDispatcher<E extends Event = Event> {
 
 	/**
 	 * Creates eventDispatcher object. It needs to be call with '.call' to add the functionality to an object.
@@ -24,26 +26,26 @@ export class EventDispatcher {
 	 * @param type The type of event to listen to.
 	 * @param listener The function that gets called when the event is fired.
 	 */
-	addEventListener( type: string, listener: ( event: Event ) => void ): void;
+	addEventListener<T extends E["type"]>( type: T, listener: Listener<E, T, this> ): void;
 
 	/**
 	 * Checks if listener is added to an event type.
 	 * @param type The type of event to listen to.
 	 * @param listener The function that gets called when the event is fired.
 	 */
-	hasEventListener( type: string, listener: ( event: Event ) => void ): boolean;
+	hasEventListener<T extends E["type"]>( type: T, listener: Listener<E, T, this> ): boolean;
 
 	/**
 	 * Removes a listener from an event type.
 	 * @param type The type of the listener that gets removed.
 	 * @param listener The listener function that gets removed.
 	 */
-	removeEventListener( type: string, listener: ( event: Event ) => void ): void;
+	removeEventListener<T extends E["type"]>( type: T, listener: Listener<E, T, this> ): void;
 
 	/**
 	 * Fire an event type.
 	 * @param type The type of event that gets fired.
 	 */
-	dispatchEvent( event: { type: string; [attachment: string]: any } ): void;
+	dispatchEvent( event: E ): void;
 
 }
