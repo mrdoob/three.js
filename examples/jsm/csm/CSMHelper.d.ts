a53ca31fcae3d94938fc3931877b54818e71d78a
linbingquan <695601626@qq.com>
2020-09-08 00:23:37 +0800

    CSM: Clean up.

--- examples/jsm/csm/CSMHelper.d.ts
+++ examples/jsm/csm/CSMHelper.d.ts
@@ -1,13 +1,15 @@
 export class CSMHelper {
-    constructor(csm: any);
-    csm: any;
-    displayFrustum: boolean;
-    displayPlanes: boolean;
-    displayShadowBounds: boolean;
-    frustumLines: any;
-    cascadeLines: any[];
-    cascadePlanes: any[];
-    shadowLines: any[];
-    updateVisibility(): void;
-    update(): void;
+
+	constructor( csm: any );
+	csm: any;
+	displayFrustum: boolean;
+	displayPlanes: boolean;
+	displayShadowBounds: boolean;
+	frustumLines: any;
+	cascadeLines: any[];
+	cascadePlanes: any[];
+	shadowLines: any[];
+	updateVisibility(): void;
+	update(): void;
+
 }
