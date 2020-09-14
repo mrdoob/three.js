819a497fc2407bbb3a91a279d7e6e57b7741f0e7
ycw <ycw7788@gmail.com>
2020-09-14 21:39:54 +0800

    Set `.castShadow` of `boolean` and added tags

--- src/lights/AmbientLight.d.ts
+++ src/lights/AmbientLight.d.ts
@@ -22,8 +22,10 @@ export class AmbientLight extends Light {
 
 	/**
 	 * This light cannot be used to cast shadows as it does not have a direction.
+	 * @default false
+	 * @override
 	 */
-	castShadow: undefined;
+	castShadow: boolean;
 	readonly isAmbientLight: true;
 
 }
