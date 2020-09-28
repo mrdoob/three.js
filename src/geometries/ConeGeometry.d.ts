0a1c4ec2e92a05af817815f4d1fcfd2587afe8fb
Kevin LEVRON <kevin.levron@gmail.com>
2020-09-28 14:20:14 +0200

    Fix ConeGeometry TSDoc

--- src/geometries/ConeGeometry.d.ts
+++ src/geometries/ConeGeometry.d.ts
@@ -3,14 +3,13 @@ import { CylinderGeometry } from './CylinderGeometry';
 export class ConeGeometry extends CylinderGeometry {
 
 	/**
-	 * @param [radiusTop=0] — Radius of the cylinder at the top.
-	 * @param [radiusBottom=1] — Radius of the cylinder at the bottom.
-	 * @param [height=1] — Height of the cylinder.
-	 * @param [radiusSegments=8] — Number of segmented faces around the circumference of the cylinder.
-	 * @param [heightSegments=1] — Number of rows of faces along the height of the cylinder.
-	 * @param [openEnded=false] - A Boolean indicating whether or not to cap the ends of the cylinder.
+	 * @param [radius=1] — Radius of the cone base.
+	 * @param [height=1] — Height of the cone.
+	 * @param [radiusSegments=8] — Number of segmented faces around the circumference of the cone.
+	 * @param [heightSegments=1] — Number of rows of faces along the height of the cone.
+	 * @param [openEnded=false] — A Boolean indicating whether the base of the cone is open or capped.
 	 * @param [thetaStart=0]
-	 * @param [widthSegments=Math.PI * 2]
+	 * @param [thetaLength=Math.PI * 2]
 	 */
 	constructor(
 		radius?: number,
