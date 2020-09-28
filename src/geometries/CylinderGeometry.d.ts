5e2fe49cec5606e6138a1a59e022d5b2c0e32097
Kevin LEVRON <kevin.levron@gmail.com>
2020-09-28 14:36:20 +0200

    Fix typo in CylinderGeometry

--- src/geometries/CylinderGeometry.d.ts
+++ src/geometries/CylinderGeometry.d.ts
@@ -6,7 +6,7 @@ export class CylinderGeometry extends Geometry {
 	 * @param [radiusTop=1] — Radius of the cylinder at the top.
 	 * @param [radiusBottom=1] — Radius of the cylinder at the bottom.
 	 * @param [height=1] — Height of the cylinder.
-	 * @param [radiusSegments=8] — Number of segmented faces around the circumference of the cylinder.
+	 * @param [radialSegments=8] — Number of segmented faces around the circumference of the cylinder.
 	 * @param [heightSegments=1] — Number of rows of faces along the height of the cylinder.
 	 * @param [openEnded=false] - A Boolean indicating whether or not to cap the ends of the cylinder.
 	 * @param [thetaStart=0]
@@ -16,7 +16,7 @@ export class CylinderGeometry extends Geometry {
 		radiusTop?: number,
 		radiusBottom?: number,
 		height?: number,
-		radiusSegments?: number,
+		radialSegments?: number,
 		heightSegments?: number,
 		openEnded?: boolean,
 		thetaStart?: number,
