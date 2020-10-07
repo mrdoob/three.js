66ea417c47e3bbce574c91f9f4405b3eb51ec3ec
Nathan Bierema <nbierema@gmail.com>
2020-10-07 08:03:27 -0400

    Update ExtrudeGeometry.d.ts

--- src/geometries/ExtrudeGeometry.d.ts
+++ src/geometries/ExtrudeGeometry.d.ts
@@ -1,9 +1,6 @@
-import { Curve } from './../extras/core/Curve';
-import { Vector2 } from './../math/Vector2';
-import { Vector3 } from './../math/Vector3';
 import { Shape } from './../extras/core/Shape';
 import { Geometry } from './../core/Geometry';
-import { ExtrudeBufferGeometry, ExtrudeGeometryOptions, UVGenerator } from './ExtrudeBufferGeometry';
+import { ExtrudeGeometryOptions } from './ExtrudeBufferGeometry';
 
 export class ExtrudeGeometry extends Geometry {
 
