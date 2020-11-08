66b7db927b6947c629c3cf9b266824a8474c5898
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-11-08 16:24:57 -0500

    move changes into js directory rather than jsm

--- examples/js/loaders/SVGLoader.js
+++ examples/js/loaders/SVGLoader.js
@@ -386,6 +386,9 @@ THREE.SVGLoader.prototype = Object.assign( Object.create( THREE.Loader.prototype
 						var numbers = parseFloats( data );
 
 						for ( var j = 0, jl = numbers.length; j < jl; j += 7 ) {
+							
+							// skip command if start point == end point
+							if( numbers[ j + 5 ] == point.x && numbers[ j + 6 ] == point.y ) continue;
 
 							var start = point.clone();
 							point.x = numbers[ j + 5 ];
@@ -576,6 +579,9 @@ THREE.SVGLoader.prototype = Object.assign( Object.create( THREE.Loader.prototype
 
 						for ( var j = 0, jl = numbers.length; j < jl; j += 7 ) {
 
+							// skip command if no displacement
+							if( numbers[ j + 5 ] == 0 && numbers[ j + 6 ] == 0 ) continue;
+
 							var start = point.clone();
 							point.x += numbers[ j + 5 ];
 							point.y += numbers[ j + 6 ];
@@ -659,6 +665,12 @@ THREE.SVGLoader.prototype = Object.assign( Object.create( THREE.Loader.prototype
 		 */
 
 		function parseArcCommand( path, rx, ry, x_axis_rotation, large_arc_flag, sweep_flag, start, end ) {
+
+			if( rx == 0 || ry == 0 ) {
+				// draw a line if either of the radii == 0
+				path.lineTo( end.x, end.y );
+				return;
+			}
 
 			x_axis_rotation = x_axis_rotation * Math.PI / 180;
 
