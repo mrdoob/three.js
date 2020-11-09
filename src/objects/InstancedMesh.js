5873d115434e8e8b94a4316375ad831ce022bd93
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-11-10 11:49:33 +1300

    add instancedmesh

--- src/objects/InstancedMesh.js
+++ src/objects/InstancedMesh.js
@@ -9,37 +9,33 @@ const _instanceIntersects = [];
 
 const _mesh = new Mesh();
 
-function InstancedMesh( geometry, material, count ) {
+class InstancedMesh extends Mesh {
 
-	Mesh.call( this, geometry, material );
+	constructor( geometry, material, count ) {
 
-	this.instanceMatrix = new BufferAttribute( new Float32Array( count * 16 ), 16 );
-	this.instanceColor = null;
+		super( geometry, material );
+		Object.defineProperty( this, "isInstancedMesh", { value: true } );
+		this.instanceMatrix = new BufferAttribute( new Float32Array( count * 16 ), 16 );
+		this.instanceColor = null;
 
-	this.count = count;
+		this.count = count;
 
-	this.frustumCulled = false;
+		this.frustumCulled = false;
 
-}
+	}
 
-InstancedMesh.prototype = Object.assign( Object.create( Mesh.prototype ), {
+	copy( source ) {
 
-	constructor: InstancedMesh,
-
-	isInstancedMesh: true,
-
-	copy: function ( source ) {
-
-		Mesh.prototype.copy.call( this, source );
+		super.copy( source );
 
 		this.instanceMatrix.copy( source.instanceMatrix );
 		this.count = source.count;
 
 		return this;
 
-	},
+	}
 
-	setColorAt: function ( index, color ) {
+	setColorAt( index, color ) {
 
 		if ( this.instanceColor === null ) {
 
@@ -49,15 +45,15 @@ InstancedMesh.prototype = Object.assign( Object.create( Mesh.prototype ), {
 
 		color.toArray( this.instanceColor.array, index * 3 );
 
-	},
+	}
 
-	getMatrixAt: function ( index, matrix ) {
+	getMatrixAt( index, matrix ) {
 
 		matrix.fromArray( this.instanceMatrix.array, index * 16 );
 
-	},
+	}
 
-	raycast: function ( raycaster, intersects ) {
+	raycast( raycaster, intersects ) {
 
 		const matrixWorld = this.matrixWorld;
 		const raycastTimes = this.count;
@@ -96,18 +92,19 @@ InstancedMesh.prototype = Object.assign( Object.create( Mesh.prototype ), {
 
 		}
 
-	},
+	}
 
-	setMatrixAt: function ( index, matrix ) {
+	setMatrixAt( index, matrix ) {
 
 		matrix.toArray( this.instanceMatrix.array, index * 16 );
 
-	},
+	}
 
-	updateMorphTargets: function () {
+	updateMorphTargets() {
 
 	}
 
-} );
+}
+
 
 export { InstancedMesh };
