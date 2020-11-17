11597dc6e407129d66f843713061011d45200a76
Maciek Jurczyk <thelairmail@gmail.com>
2020-11-17 23:47:03 +0100

    Add missing vertex projection

--- examples/jsm/modifiers/CurveModifier.js
+++ examples/jsm/modifiers/CurveModifier.js
@@ -180,7 +180,13 @@ vec3 transformed = basis
 	+ spinePos;
 
 vec3 transformedNormal = normalMatrix * (basis * objectNormal);
-`);
+`).replace(
+	'#include <project_vertex>',
+`
+vec4 mvPosition = modelViewMatrix * vec4( transformed, 1.0 );
+gl_Position = projectionMatrix * mvPosition;
+`
+);
 
 		shader.vertexShader = vertexShader;
 
