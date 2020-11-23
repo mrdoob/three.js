60c29866c0473ae133f2e7b306ec63a9719b4443
linbingquan <695601626@qq.com>
2020-11-23 21:54:21 +0800

    GridHelper: Clean up.

--- src/helpers/GridHelper.js
+++ src/helpers/GridHelper.js
@@ -6,10 +6,8 @@ import { Color } from '../math/Color.js';
 
 class GridHelper extends LineSegments {
 
-	constructor( size, divisions, color1 = 0x444444, color2 = 0x888888 ) {
+	constructor( size = 10, divisions = 10, color1 = 0x444444, color2 = 0x888888 ) {
 
-		size = size || 10;
-		divisions = divisions || 10;
 		color1 = new Color( color1 );
 		color2 = new Color( color2 );
 
