9af037c8fa2319616c94d2a55d4ec567fc44f89e
Takahiro <hogehoge@gachapin.jp>
2020-11-28 16:28:47 -0800

    Tests: Formatting in console-wrapper

--- test/unit/utils/console-wrapper.js
+++ test/unit/utils/console-wrapper.js
@@ -12,11 +12,11 @@
 
 export const CONSOLE_LEVEL = {
 	OFF : 0,
-    ERROR : 1,
-    WARN : 2,
-    LOG : 3,
-    INFO : 4,
-    DEBUG : 5,
+	ERROR : 1,
+	WARN : 2,
+	LOG : 3,
+	INFO : 4,
+	DEBUG : 5,
 	ALL: 6,
 	DEFAULT: 6
 };
@@ -32,21 +32,31 @@ console._debug = console.debug;
 
 // Wrap console methods
 console.error = function () {
-  if (this.level >= CONSOLE_LEVEL.ERROR) this._error.apply(this, arguments);
+
+	if ( this.level >= CONSOLE_LEVEL.ERROR ) this._error.apply( this, arguments );
+
 };
 
 console.warn = function () {
-  if (this.level >= CONSOLE_LEVEL.WARN) this._warn.apply(this, arguments);
+
+	if ( this.level >= CONSOLE_LEVEL.WARN ) this._warn.apply( this, arguments );
+
 };
 
 console.log = function () {
-  if (this.level >= CONSOLE_LEVEL.LOG) this._log.apply(this, arguments);
+
+	if ( this.level >= CONSOLE_LEVEL.LOG ) this._log.apply( this, arguments );
+
 };
 
 console.info = function () {
-  if (this.level >= CONSOLE_LEVEL.INFO) this._info.apply(this, arguments);
+
+	if ( this.level >= CONSOLE_LEVEL.INFO ) this._info.apply( this, arguments );
+
 };
 
 console.debug = function () {
-  if (this.level >= CONSOLE_LEVEL.DEBUG) this._debug.apply(this, arguments);
+
+	if ( this.level >= CONSOLE_LEVEL.DEBUG ) this._debug.apply( this, arguments );
+
 };
