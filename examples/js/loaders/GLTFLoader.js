550165d17af6b53c6c45d35b0ef85bdbff33af94
Takahiro <hogehoge@gachapin.jp>
2020-11-30 23:18:37 -0800

    GLTFLoader: Export KHR_materials_variants extension handler from the loader

--- examples/js/loaders/GLTFLoader.js
+++ examples/js/loaders/GLTFLoader.js
@@ -37,12 +37,6 @@ THREE.GLTFLoader = ( function () {
 
 		this.register( function ( parser ) {
 
-			return new GLTFMaterialsVariantsExtension( parser );
-
-		} );
-
-		this.register( function ( parser ) {
-
 			return new GLTFLightsExtension( parser );
 
 		} );
@@ -360,7 +354,6 @@ THREE.GLTFLoader = ( function () {
 		KHR_MATERIALS_PBR_SPECULAR_GLOSSINESS: 'KHR_materials_pbrSpecularGlossiness',
 		KHR_MATERIALS_TRANSMISSION: 'KHR_materials_transmission',
 		KHR_MATERIALS_UNLIT: 'KHR_materials_unlit',
-		KHR_MATERIALS_VARIANTS: 'KHR_materials_variants',
 		KHR_TEXTURE_BASISU: 'KHR_texture_basisu',
 		KHR_TEXTURE_TRANSFORM: 'KHR_texture_transform',
 		KHR_MESH_QUANTIZATION: 'KHR_mesh_quantization',
@@ -741,110 +734,6 @@ THREE.GLTFLoader = ( function () {
 		}
 
 		return parser.loadTextureImage( textureIndex, source, loader );
-
-	};
-
-	/**
-	 * Materials variants extension
-	 *
-	 * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_variants
-	 */
-	function GLTFMaterialsVariantsExtension( parser ) {
-
-		this.parser = parser;
-		this.name = EXTENSIONS.KHR_MATERIALS_VARIANTS;
-		this.addExtensionsToUserData = true;
-
-	}
-
-	GLTFMaterialsVariantsExtension.prototype.afterRoot = function ( gltf ) {
-
-		var parser = this.parser;
-		var json = parser.json;
-		var name = this.name;
-
-		if ( ! json.extensions || ! json.extensions[ name ] ) return null;
-
-		var extensionDef = json.extensions[ name ];
-		var variantsDef = extensionDef.variants || [];
-		var variants = [];
-
-		for ( var i = 0, il = variantsDef.length; i < il; i ++ ) {
-
-			variants.push( variantsDef[ i ].name );
-
-		}
-
-		for ( var i = 0, il = gltf.scenes.length; i < il; i ++ ) {
-
-			gltf.scenes[ i ].userData.variants = variants;
-
-		}
-
-		gltf.userData.variants = variants;
-
-		gltf.userData.selectVariant = function selectVariant ( variantName ) {
-
-			var scenes = gltf.scenes;
-			var pending = [];
-
-			for ( var i = 0, il = scenes.length; i < il; i ++ ) {
-
-				var scene = scenes[ i ];
-
-				var variantIndex = variants.indexOf( variantName );
-
-				if ( variantIndex === - 1 ) continue;
-
-				scene.traverse( function ( object ) {
-
-					if ( ! object.isMesh || ! object.userData.gltfExtensions ||
-						! object.userData.gltfExtensions[ name ] ) return;
-
-					var meshVariantDef = object.userData.gltfExtensions[ name ];
-					var mappings = meshVariantDef.mappings || [];
-					var materialIndex = - 1;
-
-					for ( var j = 0, jl = mappings.length; j < jl; j ++ ) {
-
-						var mapping = mappings[ j ];
-
-						if ( mapping.variants.indexOf( variantIndex ) !== - 1 ) {
-
-							materialIndex = mapping.material;
-							break;
-
-						}
-
-					}
-
-					if ( materialIndex >= 0 ) {
-
-						if ( ! object.userData.originalMaterial ) {
-
-							object.userData.originalMaterial = object.material;
-
-						}
-
-						pending.push( parser.getDependency( 'material', materialIndex ).then( function ( material ) {
-
-							object.material = material;
-
-						} ) );
-
-					} else if ( object.userData.originalMaterial ) {
-
-						object.material = object.userData.originalMaterial;
-
-					}
-
-				} );
-
-			}
-
-			return Promise.all( pending );
-
-		};
 
 	};
 
