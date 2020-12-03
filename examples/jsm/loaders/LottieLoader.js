977cf79c8cad32d31958cf14ee8ed5aafc6e88c4
Mr.doob <info@mrdoob.com>
2020-12-03 17:10:11 +0000

    LottieLoader: Convert to ES6 class

--- examples/jsm/loaders/LottieLoader.js
+++ examples/jsm/loaders/LottieLoader.js
@@ -5,23 +5,15 @@ import {
 	NearestFilter
 } from "../../../build/three.module.js";
 
-var LottieLoader = function ( manager ) {
+class LottieLoader extends Loader {
 
-	Loader.call( this, manager );
-
-};
-
-LottieLoader.prototype = Object.assign( Object.create( Loader.prototype ), {
-
-	constructor: LottieLoader,
-
-	setQuality: function ( value ) {
+	setQuality( value ) {
 
 		this._quality = value;
 
-	},
+	}
 
-	load: function ( url, onLoad, onProgress, onError ) {
+	load( url, onLoad, onProgress, onError ) {
 
 		const quality = this._quality || 1;
 
@@ -76,6 +68,6 @@ LottieLoader.prototype = Object.assign( Object.create( Loader.prototype ), {
 
 	}
 
-} );
+}
 
 export { LottieLoader };
