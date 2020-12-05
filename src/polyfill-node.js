1bb7999c5d95d87fc6bfa555209966edd1d94d26
Etienne Dupont <etienne.dupont@polytechnique.edu>
2020-12-05 23:40:29 +0100

    Add node polyfill

---
+++ src/polyfill-node.js
@@ -0,0 +1,20 @@
+import { JSDOM } from "jsdom";
+
+const jsdomOptions = {
+  pretendToBeVisual: true,
+  storageQuota: 1e9,
+  resources: "usable",
+  runScripts: "dangerously"
+};
+
+const jsdom = new JSDOM(
+	`<!DOCTYPE html><body id="main"><p >Hello world</p></body>`,
+	jsdomOptions
+);
+
+const window = jsdom.window;
+
+export const Blob = window.Blob;
+export const atob = window.atob;
+export const DOMParser = window.DOMParser;
+export const document = window.document;
