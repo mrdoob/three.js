0483bb4fcd93492a64c95a2ca9a760c781b34200
Mugen87 <michael.herzog@human-interactive.org>
2020-12-07 11:17:17 +0100

    WebGLPrograms: Make getTextureEncodingFromMap() more robust.

--- src/renderers/webgl/WebGLPrograms.js
+++ src/renderers/webgl/WebGLPrograms.js
@@ -89,18 +89,18 @@ function WebGLPrograms( renderer, cubemaps, extensions, capabilities, bindingSta
 
 		let encoding;
 
-		if ( ! map ) {
-
-			encoding = LinearEncoding;
-
-		} else if ( map.isTexture ) {
+		if ( map && map.isTexture ) {
 
 			encoding = map.encoding;
 
-		} else if ( map.isWebGLRenderTarget ) {
+		} else if ( map && map.isWebGLRenderTarget ) {
 
 			console.warn( 'THREE.WebGLPrograms.getTextureEncodingFromMap: don\'t use render targets as textures. Use their .texture property instead.' );
 			encoding = map.texture.encoding;
+
+		} else {
+
+			encoding = LinearEncoding;
 
 		}
 
