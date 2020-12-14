74658baf3d2085139787652c8bddc36365b55e70
Don McCurdy <donmccurdy@google.com>
2020-12-13 20:56:37 -0800

    KTX2Loader: Fix calculation of level dimensions for NPOT textures.

--- examples/jsm/loaders/KTX2Loader.js
+++ examples/jsm/loaders/KTX2Loader.js
@@ -524,8 +524,8 @@ class KTX2Container {
 
 		for ( var level = 0; level < this.header.levelCount; level ++ ) {
 
-			var levelWidth = Math.ceil( width / Math.pow( 2, level ) );
-			var levelHeight = Math.ceil( height / Math.pow( 2, level ) );
+			var levelWidth = Math.floor( width / ( 1 << level ) ) || 1;
+			var levelHeight = Math.floor( height / ( 1 << level ) ) || 1;
 
 			var numImagesInLevel = 1; // TODO(donmccurdy): Support cubemaps, arrays and 3D.
 			var imageOffsetInLevel = 0;
