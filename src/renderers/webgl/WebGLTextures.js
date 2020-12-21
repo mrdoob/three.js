cfe8cfb98ffbd07aa1f8e75c2f02177a7fd95c50
Etienne Dupont <etienne.dupont@polytechnique.edu>
2020-12-21 19:16:45 +0100

    Fix typo for build

--- src/renderers/webgl/WebGLTextures.js
+++ src/renderers/webgl/WebGLTextures.js
@@ -1,7 +1,7 @@
 import { LinearFilter, LinearMipmapLinearFilter, LinearMipmapNearestFilter, NearestFilter, NearestMipmapLinearFilter, NearestMipmapNearestFilter, RGBFormat, RGBAFormat, DepthFormat, DepthStencilFormat, UnsignedShortType, UnsignedIntType, UnsignedInt248Type, FloatType, HalfFloatType, MirroredRepeatWrapping, ClampToEdgeWrapping, RepeatWrapping } from '../../constants.js';
 import { MathUtils } from '../../math/MathUtils.js';
 
-import { document } from "../dom-globals.js";
+import { document } from "../../dom-globals.js";
 
 function WebGLTextures( _gl, extensions, state, properties, capabilities, utils, info ) {
 
