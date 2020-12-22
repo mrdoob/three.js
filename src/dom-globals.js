c786ce416967a7b29f4ec98c7bfe2e64d5b5a534
Etienne Dupont <etienne.dupont@polytechnique.edu>
2020-12-22 11:14:47 +0100

    Refactor dom-globals to avoid test crashs

--- src/dom-globals.js
+++ src/dom-globals.js
@@ -1,10 +1,48 @@
-export const Blob = window.Blob;
-export const atob = window.atob;
-export const btoa = window.btoa;
-export const DOMParser = window.DOMParser;
-export const document = window.document;
-export const XMLHttpRequest = window.XMLHttpRequest;
-export const TextEncoder = window.TextEncoder;
-export const TextDecoder = window.TextDecoder;
-export const decodeURIComponent = window.decodeURIComponent;
-export const CustomEvent = window.CustomEvent;
+let _Blob
+let _atob
+let _btoa
+let _DOMParser
+let _document
+let _XMLHttpRequest
+let _TextEncoder
+let _TextDecoder
+let _decodeURIComponent
+let _CustomEvent
+
+function defineGlobals(){
+	try{
+		_Blob = window.Blob;
+		_atob = window.atob;
+		_btoa = window.btoa;
+		_DOMParser = window.DOMParser;
+		_document = window.document;
+		_XMLHttpRequest = window.XMLHttpRequest;
+		_TextEncoder = window.TextEncoder;
+		_TextDecoder = window.TextDecoder;
+		_decodeURIComponent = window.decodeURIComponent;
+		_CustomEvent = window.CustomEvent;
+	}
+	finally{ }
+}
+
+defineGlobals()
+
+export const Blob = _Blob;
+export const atob = _atob;
+export const btoa = _btoa;
+export const DOMParser = _DOMParser;
+export const document = _document;
+export const XMLHttpRequest = _XMLHttpRequest;
+export const TextEncoder = _TextEncoder;
+export const TextDecoder = _TextDecoder;
+export const decodeURIComponent = _decodeURIComponent;
+export const CustomEvent = _CustomEvent;
+
+
+
+
+
+
+
+
+
