0de1151e8b92fec4b55c5382c0b4e42e478c79e8
Etienne Dupont <etienne.dupont@polytechnique.edu>
2020-12-22 10:05:19 +0100

    Add tests for FileLoader

--- test/unit/src/loaders/FileLoader.tests.js
+++ test/unit/src/loaders/FileLoader.tests.js
@@ -7,16 +7,201 @@ export default QUnit.module( 'Loaders', () => {
 	QUnit.module( 'FileLoader', () => {
 
 		// INSTANCING
-		QUnit.todo( "Instancing", ( assert ) => {
+		QUnit.test( "Instancing", ( assert ) => {
 
-			assert.ok( false, "everything's gonna be alright" );
+			const fileLoader = new FileLoader();
+			assert.ok( fileLoader.crossOrigin === "anonymous", "Passed!" );
+			assert.ok( fileLoader.mimeType === undefined, "Passed!" );
+			assert.ok( fileLoader.responseType === undefined, "Passed!" );
 
 		} );
 
 		// PUBLIC STUFF
-		QUnit.todo( "load", ( assert ) => {
+		QUnit.test( "load data url => undefined", ( assert ) => {
 
-			assert.ok( false, "everything's gonna be alright" );
+			const done = assert.async();
+			const fileLoader = new FileLoader();
+			fileLoader.load(
+				"data:,Hello%2C%20World!",
+				// onLoad callback
+				function ( data ) {
+
+					// output the text to the console
+					// console.log( data );
+					assert.ok( data == "Hello, World!", "Passed!" );
+					done();
+
+				},
+
+				// onProgress callback
+				function ( xhr ) {
+
+					console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+				},
+
+				// onError callback
+				function ( err ) {
+
+					console.error( 'An error happened' );
+					console.error( err );
+					assert.ok( false, "Failed with error" );
+					done();
+
+				}
+			);
+
+		} );
+
+		QUnit.test( "load data url => arraybuffer", ( assert ) => {
+
+			const done = assert.async();
+			const fileLoader = new FileLoader();
+			fileLoader.setResponseType( "arraybuffer" );
+			fileLoader.setMimeType( "text/plain" );
+			fileLoader.load(
+				"data:,Hello%2C%20World!",
+				// onLoad callback
+				function ( data ) {
+
+					// output the text to the console
+					// console.log( data );
+					assert.ok( data.byteLength === 13, "Passed!" );
+					done();
+
+				},
+
+				// onProgress callback
+				function ( xhr ) {
+
+					console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+				},
+
+				// onError callback
+				function ( err ) {
+
+					console.error( 'An error happened' );
+					console.error( err );
+					assert.ok( false, "Failed with error" );
+					done();
+
+				}
+			);
+
+		} );
+
+		QUnit.test( "load data url => blob", ( assert ) => {
+
+			const done = assert.async();
+			const fileLoader = new FileLoader();
+			fileLoader.setResponseType( "blob" );
+			fileLoader.setMimeType( "text/plain" );
+			fileLoader.load(
+				"data:,Hello%2C%20World!",
+				// onLoad callback
+				function ( data ) {
+
+					// output the text to the console
+					// console.log( data.size );
+					assert.ok( data.size === 13, "Passed!" );
+					done();
+
+				},
+
+				// onProgress callback
+				function ( xhr ) {
+
+					console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+				},
+
+				// onError callback
+				function ( err ) {
+
+					console.error( 'An error happened' );
+					console.error( err );
+					assert.ok( false, "Failed with error" );
+					done();
+
+				}
+			);
+
+		} );
+
+		QUnit.test( "load data url => document", ( assert ) => {
+
+			const done = assert.async();
+			const fileLoader = new FileLoader();
+			fileLoader.setResponseType( "document" );
+			fileLoader.setMimeType( "text/html" );
+			fileLoader.load(
+				"data:text/html,%3Ch1%3EHello%2C%20World!%3C%2Fh1%3E",
+				// onLoad callback
+				function ( data ) {
+
+					// output the text to the console
+					// console.log( data );
+					assert.ok( true, "Passed!" );
+					done();
+
+				},
+
+				// onProgress callback
+				function ( xhr ) {
+
+					console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+				},
+
+				// onError callback
+				function ( err ) {
+
+					console.error( 'An error happened' );
+					console.error( err );
+					assert.ok( false, "Failed with error" );
+					done();
+
+				}
+			);
+
+		} );
+
+		QUnit.test( "load data url => json", ( assert ) => {
+
+			const done = assert.async();
+			const fileLoader = new FileLoader();
+			fileLoader.setResponseType( "json" );
+			fileLoader.setMimeType( "application/json" );
+			fileLoader.load(
+				"data:application/json;base64,eyJhIjogImJjZCIgfQ==",
+				// onLoad callback
+				function ( data ) {
+
+					// output the text to the console
+					console.log( JSON.stringify( data ) );
+					assert.ok( JSON.stringify( data ) === `{"a":"bcd"}`, "Passed!" );
+					done();
+
+				},
+
+				// onProgress callback
+				function ( xhr ) {
+
+					console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+				},
+
+				// onError callback
+				function ( err ) {
+
+					console.error( 'An error happened' );
+					console.error( err );
+					assert.ok( false, "Failed with error" );
+					done();
+
+				}
+			);
 
 		} );
 
