28b246f4665102e342dd2136792f0ed5e8c7e6d7
sunag <sunag@users.noreply.github.com>
2021-01-20 05:11:30 -0300

    move shaderlib

--- examples/jsm/renderers/webgpu/nodes/ShaderLib.js
+++ examples/jsm/renderers/webgpu/nodes/ShaderLib.js
@@ -35,13 +35,13 @@ const ShaderLib = {
 
 			#ifdef NODE_COLOR
 
-				outColor.rgb = NODE_COLOR;
+				outColor = NODE_COLOR;
 
 			#endif
 
 			#ifdef NODE_OPACITY
 
-				outColor.a = NODE_OPACITY;
+				outColor.a *= NODE_OPACITY;
 
 			#endif
 
@@ -82,7 +82,7 @@ const ShaderLib = {
 
 			#ifdef NODE_COLOR
 
-				outColor.rgb = NODE_COLOR;
+				outColor = NODE_COLOR;
 
 			#endif
 
@@ -99,6 +99,9 @@ const ShaderLib = {
 
 		layout(location = 0) in vec3 position;
 
+		NODE_HEADER_ATTRIBUTES
+		NODE_HEADER_UNIFORMS
+
 		layout(set = 0, binding = 0) uniform ModelUniforms {
 			mat4 modelMatrix;
 			mat4 modelViewMatrix;
@@ -110,14 +113,32 @@ const ShaderLib = {
 		} cameraUniforms;
 
 		void main(){
+			NODE_BODY_ATTRIBUTES
 			gl_Position = cameraUniforms.projectionMatrix * modelUniforms.modelViewMatrix * vec4( position, 1.0 );
 		}`,
 		fragmentShader: `#version 450
 
+		NODE_HEADER_ATTRIBUTES
+		NODE_HEADER_UNIFORMS
+
 		layout(location = 0) out vec4 outColor;
 
 		void main() {
-			outColor = vec4( 1.0, 0.0, 0.0, 1.0 );
+
+			outColor = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+			#ifdef NODE_COLOR
+
+				outColor = NODE_COLOR;
+
+			#endif
+
+			#ifdef NODE_OPACITY
+
+				outColor.a = NODE_OPACITY;
+
+			#endif
+
 		}`
 	}
 };
