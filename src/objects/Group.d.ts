dadcee6c004a2b2be5296aeb9ca168fb4045d628
Xavier Willemin <xavier@slashnet.ch>
2021-01-25 11:56:06 +0100

    Add generic type with default to Group children

--- src/objects/Group.d.ts
+++ src/objects/Group.d.ts
@@ -1,9 +1,18 @@
 import { Object3D } from './../core/Object3D';
 
-export class Group extends Object3D {
+export class Group<TChild extends Object3D = Object3D> extends Object3D {
 
 	constructor();
+	children: TChild[];
 	type: 'Group';
 	readonly isGroup: true;
+
+	add( ...object: TChild[] ): this;
+	remove( ...object: TChild[] ): this;
+	attach( object: TChild ): this;
+
+	getObjectById( id: number ): TChild | undefined;
+	getObjectByName( name: string ): TChild | undefined;
+	getObjectByProperty( name: string, value: string ): TChild | undefined;
 
 }
