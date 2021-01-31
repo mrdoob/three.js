f54dca8d0c3279d9a8d416f4a333f30192ae17b0
sunag <sunag@users.noreply.github.com>
2021-01-31 12:37:33 -0300

    fix equality

--- examples/jsm/renderers/nodes/accessors/CameraNode.js
+++ examples/jsm/renderers/nodes/accessors/CameraNode.js
@@ -69,13 +69,13 @@ class CameraNode extends Node {
 
 			if ( scope === CameraNode.PROJECTION || scope === CameraNode.VIEW ) {
 
-				if ( inputNode === undefined || inputNode.isMatrix4Node !== true ) {
+				if ( inputNode === null || inputNode.isMatrix4Node !== true ) {
 
 					inputNode = new Matrix4Node( null );
 
 				}
 
-			} else if ( inputNode === undefined || inputNode.isVector3Node !== true ) {
+			} else if ( inputNode === null || inputNode.isVector3Node !== true ) {
 
 				inputNode = new Vector3Node();
 
