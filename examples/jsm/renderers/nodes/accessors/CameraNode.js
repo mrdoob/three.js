d0ca03dc5c3060182693ca2c53b0c1a1005a4ff3
sunag <sunag@users.noreply.github.com>
2021-01-31 09:29:28 -0300

    fix code style

--- examples/jsm/renderers/nodes/accessors/CameraNode.js
+++ examples/jsm/renderers/nodes/accessors/CameraNode.js
@@ -69,13 +69,13 @@ class CameraNode extends Node {
 
 			if ( scope === CameraNode.PROJECTION || scope === CameraNode.VIEW ) {
 
-				if ( !inputNode || !inputNode.isMatrix4Node ) {
+				if ( !inputNode || !inputNode.isMatrix4Node !== true ) {
 
 					inputNode = new Matrix4Node( null );
 
 				}
 
-			} else if ( !inputNode || !inputNode.isVector3Node ) {
+			} else if ( !inputNode || !inputNode.isVector3Node !== true ) {
 
 				inputNode = new Vector3Node();
 
