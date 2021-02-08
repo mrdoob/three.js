866c348671af8af8bd0ea4ec16f663bc084ba091
kchapelier <kevin.chapelier@gmail.com>
2021-02-08 20:21:58 +0100

    editor: video: tentative presets with ui for selection, url revoke

--- editor/js/Sidebar.Project.Video.js
+++ editor/js/Sidebar.Project.Video.js
@@ -1,16 +1,46 @@
-import { UIBreak, UIButton, UIInteger, UIPanel, UIProgress, UIRow, UIText } from './libs/ui.js';
+import { UIBreak, UIButton, UIInteger, UIPanel, UIProgress, UIRow, UIText, UISelect } from './libs/ui.js';
 
 import { APP } from './libs/app.js';
 
 import { simd } from "https://unpkg.com/wasm-feature-detect?module";
 import loadEncoder from "https://unpkg.com/mp4-h264";
 
+const qualityPresets = {
+	low: {
+		groupOfPictures: 24,
+		speed: 4,
+		kbps: 600,
+		qpMin: 24,
+		qpMax: 36,
+		temporalDenoise: true
+	},
+	medium: {
+		groupOfPictures: 20,
+		speed: 5,
+		kbps: 1200,
+		qpMin: 18,
+		qpMax: 24,
+		temporalDenoise: true
+	},
+	high: {
+		groupOfPictures: 16,
+		speed: 3,
+		kbps: 1600,
+		qpMin: 11,
+		qpMax: 18,
+		temporalDenoise: true
+	},
+	veryhigh: {
+		groupOfPictures: 16,
+		speed: 1
+	},
+};
+
 let lazyEncoderPromise;
 
-function lazyLoadEncoder () {
-	if (lazyEncoderPromise != null) return lazyEncoderPromise;
+function lazyLoadEncoder() {
+	if ( lazyEncoderPromise != null ) return lazyEncoderPromise;
 	lazyEncoderPromise = simd().then(isSIMD => {
-		console.log("SIMD Video Encoder Supported?", isSIMD);
 		return loadEncoder({ simd: isSIMD });
 	});
 	return lazyEncoderPromise;
@@ -56,6 +86,21 @@ function SidebarProjectVideo( editor ) {
 
 	container.add( videoDurationRow );
 
+	// Quality
+
+	var videoQualityRow = new UIRow();
+	videoQualityRow.add( new UIText( 'Quality' ).setWidth( '90px' ) );
+
+	var videoQuality = new UISelect( 'Quality' ).setOptions({
+		low: 'Low',
+		medium: 'Medium',
+		high: 'High',
+		veryhigh: 'Very High'
+	}).setValue( 'medium' );
+	videoQualityRow.add( videoQuality );
+
+	container.add( videoQualityRow );
+
 	// Render
 
 	container.add( new UIText( '' ).setWidth( '90px' ) );
@@ -68,84 +113,66 @@ function SidebarProjectVideo( editor ) {
 	const renderButton = new UIButton( 'RENDER' );
 	renderButton.setWidth( '170px' );
 	renderButton.onClick( async () => {
-		console.time('Video encoding mp4-h264');
+		console.time( 'Video encoding mp4-h264' );
 
 		renderButton.setDisplay( 'none' );
 		progress.setDisplay( '' );
 		progress.setValue( 0 );
 
+		const width = videoWidth.getValue();
+		const height = videoHeight.getValue();
+		const fps = videoFPS.getValue();
+		const quality = videoQuality.getValue();
+		const duration = videoDuration.getValue();
+		const frames = Math.floor( duration * fps );
+
 		const player = new APP.Player();
 		player.load( editor.toJSON() );
 		player.setPixelRatio( 1 );
-		player.setSize( videoWidth.getValue(), videoHeight.getValue() );
+		player.setSize( width, height );
 
 		const canvas = player.dom.firstElementChild;
 
-		const fps = videoFPS.getValue();
-		const duration = videoDuration.getValue();
-		const frames = Math.floor(duration * fps);
-
 		const Encoder = await lazyLoadEncoder();
 
-		// Ideally the user could choose this sort of thing
-		const preset = "medium";
-		const presetOpts = {
-			medium: {
-				kbps: 1200,
-				speed: 5,
-				qpMax: 20,
-				temporalDenoise: true,
-			},
-			low: {
-				speed: 0,
-				kbps: 1200 / 2,
-				qpMax: 40,
-				temporalDenoise: true,
-			},
-			high: {
-				speed: 5,
-			},
-		}[preset];
-		console.log("Encoding with Compression Preset", preset);
+		const presetOpts = qualityPresets[quality];
+		console.log( "Encoding with Compression Preset", quality, presetOpts );
 
 		// Create a new encoder interface
 		const encoder = Encoder.create({
 			...presetOpts,
-			width: canvas.width,
-			height: canvas.height,
+			width: width,
+			height: height,
 			fps: fps,
 		});
 
 		let currentTime = 0;
-		const canvasCopy = document.createElement('canvas');
-		canvasCopy.width = canvas.width;
-		canvasCopy.height = canvas.height;
-		const contextCopy = canvasCopy.getContext('2d');
-		const frameArray = new Array(frames).fill(0).map((_, i) => i);
-		for (let i of frameArray) {
-
+		const canvasCopy = document.createElement( 'canvas' );
+		canvasCopy.width = width;
+		canvasCopy.height = height;
+		const contextCopy = canvasCopy.getContext( '2d' );
+		for (let i = 0; i < frames; i++) {
 			player.render( currentTime );
-			contextCopy.drawImage(canvas, 0, 0);
-			const rgba = contextCopy.getImageData(0, 0, canvas.width, canvas.height).data;
-			encoder.encodeRGB(rgba);
+			contextCopy.drawImage( canvas, 0, 0 );
+			const rgba = contextCopy.getImageData( 0, 0, width, height ).data;
+			encoder.encodeRGB( rgba );
 
 			currentTime += 1 / fps;
 
 			progress.setValue( i / frames );
-			await new Promise((resolve) => setTimeout(resolve, 0));
-
+			await new Promise(resolve => setTimeout( resolve, 0 ));
 		}
 
 		const mp4 = encoder.end();
 
-		save( new Blob([mp4], { type: "video/mp4" }), 'out.mp4' );
+		save( new Blob([mp4], { type: 'video/mp4' }), 'out-' + width + 'x' + height + '-' + fps + 'fps-' + quality +'.mp4' );
 
 		player.dispose();
 
 		renderButton.setDisplay( '' );
 		progress.setDisplay( 'none' );
 
-		console.timeEnd('Video encoding mp4-h264');
+		console.timeEnd( 'Video encoding mp4-h264' );
 	} );
 	container.add( renderButton );
 
@@ -155,11 +182,16 @@ function SidebarProjectVideo( editor ) {
 
 	function save( blob, filename ) {
 
+		// revoke previously downloaded URL, if any
+		if ( link.href ) {
+
+			URL.revokeObjectURL( link.href );
+
+		}
+
 		link.href = URL.createObjectURL( blob );
 		link.download = filename;
 		link.dispatchEvent( new MouseEvent( 'click' ) );
-
-		URL.revokeObjectURL( link.href );
 
 	}
 
