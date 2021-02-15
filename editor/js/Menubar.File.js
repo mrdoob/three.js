03397a7316529528700bbbbf318ad389fdcb3e93
Mr.doob <info@mrdoob.com>
2021-02-15 23:24:50 +0000

    Editor: Revoke old ObjectURLs

--- editor/js/Menubar.File.js
+++ editor/js/Menubar.File.js
@@ -488,11 +488,15 @@ function MenubarFile( editor ) {
 	var link = document.createElement( 'a' );
 	function save( blob, filename ) {
 
+		if ( link.href ) {
+
+			URL.revokeObjectURL( link.href );
+
+		}
+
 		link.href = URL.createObjectURL( blob );
 		link.download = filename || 'data.json';
 		link.dispatchEvent( new MouseEvent( 'click' ) );
-
-		// URL.revokeObjectURL( url ); breaks Firefox...
 
 	}
 
