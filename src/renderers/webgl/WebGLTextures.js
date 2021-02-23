b4b24e310cbb825b8caf1eb7536513c4c33d21b9
Mr.doob <info@mrdoob.com>
2021-02-23 00:41:59 +0000

    WebGLTextures: Removed IE11 workaround.

--- src/renderers/webgl/WebGLTextures.js
+++ src/renderers/webgl/WebGLTextures.js
@@ -128,8 +128,7 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 
 		const textureProperties = properties.get( texture );
 
-		// Note: Math.log( x ) * Math.LOG2E used instead of Math.log2( x ) which is not supported by IE11
-		textureProperties.__maxMipLevel = Math.log( Math.max( width, height ) ) * Math.LOG2E;
+		textureProperties.__maxMipLevel = Math.log2( Math.max( width, height ) );
 
 	}
 
