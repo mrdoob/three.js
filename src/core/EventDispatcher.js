7a1f9570e6fb907816fca9c39fa1a81c526c251c
puxiao <yangpuxiao@gmail.com>
2021-02-27 13:33:36 +0800

    EventDispatcher: add class Event
    Related issue: -
    
    **Description**
    
    Add `Event` in EventDispatcher.js
    
    ```diff
    + class Event {
    + 
    + 	constructor( type, target = undefined ) {
    + 
    + 		this.type = String( type );
    + 
    + 		this.target = target;
    + 
    + 	}
    +
    + }
    
    - export { EventDispatcher };
    + export { Event, EventDispatcher };
    ```

--- src/core/EventDispatcher.js
+++ src/core/EventDispatcher.js
@@ -2,6 +2,18 @@
  * https://github.com/mrdoob/eventdispatcher.js/
  */
 
+class Event {
+
+	constructor( type, target = undefined ) {
+
+		this.type = String( type );
+
+		this.target = target;
+
+	}
+
+}
+
 function EventDispatcher() {}
 
 Object.assign( EventDispatcher.prototype, {
@@ -84,4 +96,4 @@ Object.assign( EventDispatcher.prototype, {
 } );
 
 
-export { EventDispatcher };
+export { Event, EventDispatcher };
