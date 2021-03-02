78c2e35e2f43209eeb617218f32a1d5d95929aff
Mr.doob <info@mrdoob.com>
2021-03-02 11:49:46 +0000

    ImageUtils.getDataURL: Added warning when saving image as jpg. (#21386)
    * ImageUtils: Added warning when saving image as jpg.
    
    * Fixed trailing spaces

--- src/extras/ImageUtils.js
+++ src/extras/ImageUtils.js
@@ -47,6 +47,8 @@ const ImageUtils = {
 
 		if ( canvas.width > 2048 || canvas.height > 2048 ) {
 
+			console.warn( 'THREE.ImageUtils.getDataURL: Image converted to jpg for performance reasons', image );
+
 			return canvas.toDataURL( 'image/jpeg', 0.6 );
 
 		} else {
