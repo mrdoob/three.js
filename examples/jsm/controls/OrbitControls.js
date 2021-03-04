c8a9d074d6f9aa9a333de91299010330afd909ce
puxiao <yangpuxiao@gmail.com>
2021-03-04 19:09:14 +0800

    OrbitControls: replace event.keyCode with event.code
    issue: -
    
    **Description**
    
    The deprecated **`KeyboardEvent.keyCode`** read-only property represents a system and implementation dependent numerical code identifying the unmodified value of the pressed key. 
    
    https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
    
    
    
    so...
    
    ```diff
    // The four arrow keys
    - this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
    + this.keys = { LEFT: 'ArrowLeft', UP: 'ArrowUp', RIGHT: 'ArrowRight', BOTTOM: 'ArrowDown' };
    
    
    function handleKeyDown( event ) {
    
    -		switch ( event.keyCode )
    -		switch ( event.code )
    		
    }
    ```

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -73,7 +73,7 @@ var OrbitControls = function ( object, domElement ) {
 	this.autoRotateSpeed = 2.0; // 30 seconds per orbit when fps is 60
 
 	// The four arrow keys
-	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
+	this.keys = { LEFT: 'ArrowLeft', UP: 'ArrowUp', RIGHT: 'ArrowRight', BOTTOM: 'ArrowDown' };
 
 	// Mouse buttons
 	this.mouseButtons = { LEFT: MOUSE.ROTATE, MIDDLE: MOUSE.DOLLY, RIGHT: MOUSE.PAN };
@@ -596,7 +596,7 @@ var OrbitControls = function ( object, domElement ) {
 
 		var needsUpdate = false;
 
-		switch ( event.keyCode ) {
+		switch ( event.code ) {
 
 			case scope.keys.UP:
 				pan( 0, scope.keyPanSpeed );
