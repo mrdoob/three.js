c11f19429c67ce40ef88752937be102828b974bd
gonnavis <gonnavis@163.com>
2021-03-05 02:31:22 +0800

    SSRPassCorrectReflectorDepth

--- examples/jsm/objects/ReflectorForSSRPass.js
+++ examples/jsm/objects/ReflectorForSSRPass.js
@@ -9,6 +9,7 @@ import {
 	RGBFormat,
 	ShaderMaterial,
 	UniformsUtils,
+	Vector2,
 	Vector3,
 	Vector4,
 	WebGLRenderTarget,
@@ -42,31 +43,33 @@ var Reflector = function ( geometry, options ) {
 	scope.needsUpdate = false;
 	scope.maxDistance = Reflector.ReflectorShader.uniforms.maxDistance.value
 	scope.opacity = Reflector.ReflectorShader.uniforms.opacity.value
+	scope.worldYBias = options.worldYBias || 0;
+	scope.resolution = options.resolution || new Vector2( window.innerWidth, window.innerHeight );
 
-  scope._isDistanceAttenuation = Reflector.ReflectorShader.defines.isDistanceAttenuation
-  Object.defineProperty(scope, 'isDistanceAttenuation', {
-    get() {
-      return scope._isDistanceAttenuation
-    },
-    set(val) {
-      if (scope._isDistanceAttenuation === val) return
-      scope._isDistanceAttenuation = val
-      scope.material.defines.isDistanceAttenuation = val
-      scope.material.needsUpdate = true
-    }
+	scope._isDistanceAttenuation = Reflector.ReflectorShader.defines.isDistanceAttenuation
+	Object.defineProperty(scope, 'isDistanceAttenuation', {
+		get() {
+			return scope._isDistanceAttenuation
+		},
+		set(val) {
+			if (scope._isDistanceAttenuation === val) return
+			scope._isDistanceAttenuation = val
+			scope.material.defines.isDistanceAttenuation = val
+			scope.material.needsUpdate = true
+		}
 	})
 
-  scope._isFresnel = Reflector.ReflectorShader.defines.isFresnel
-  Object.defineProperty(scope, 'isFresnel', {
-    get() {
-      return scope._isFresnel
-    },
-    set(val) {
-      if (scope._isFresnel === val) return
-      scope._isFresnel = val
-      scope.material.defines.isFresnel = val
-      scope.material.needsUpdate = true
-    }
+	scope._isFresnel = Reflector.ReflectorShader.defines.isFresnel
+	Object.defineProperty(scope, 'isFresnel', {
+		get() {
+			return scope._isFresnel
+		},
+		set(val) {
+			if (scope._isFresnel === val) return
+			scope._isFresnel = val
+			scope.material.defines.isFresnel = val
+			scope.material.needsUpdate = true
+		}
 	})
 
 	var reflectorPlane = new Plane();
@@ -95,7 +98,7 @@ var Reflector = function ( geometry, options ) {
 		minFilter: LinearFilter,
 		magFilter: LinearFilter,
 		format: RGBFormat,
-    depthTexture: useDepthTexture ? depthTexture : null,
+		depthTexture: useDepthTexture ? depthTexture : null,
 	};
 
 	var renderTarget = new WebGLRenderTarget( textureWidth, textureHeight, parameters );
@@ -108,9 +111,9 @@ var Reflector = function ( geometry, options ) {
 
 	var material = new ShaderMaterial( {
 		transparent: useDepthTexture,
-    defines: Object.assign( {}, Reflector.ReflectorShader.defines, {
-      useDepthTexture
-    } ),
+		defines: Object.assign( {}, Reflector.ReflectorShader.defines, {
+			useDepthTexture
+		} ),
 		uniforms: UniformsUtils.clone( shader.uniforms ),
 		fragmentShader: shader.fragmentShader,
 		vertexShader: shader.vertexShader
@@ -127,16 +130,13 @@ var Reflector = function ( geometry, options ) {
 
 	this.doRender = function ( renderer, scene, camera ) {
 
-		material.uniforms['maxDistance'].value = scope.maxDistance * (camera.position.length() / camera.position.y);
-		///todo: Temporary hack,
-		// need precise calculation like this https://github.com/mrdoob/three.js/pull/20156/commits/8181946068e386d14a283cbd4f8877bc7ae066d3 ,
-		// after fully understand http://www.terathon.com/lengyel/Lengyel-Oblique.pdf .
-
-		material.uniforms['opacity'].value = scope.opacity;
+		material.uniforms[ 'maxDistance' ].value = scope.maxDistance;
+		material.uniforms[ 'opacity' ].value = scope.opacity;
+		material.uniforms[ 'worldYBias' ].value = scope.worldYBias;
 
 		vecTemp0.copy(camera.position).normalize();
 		vecTemp1.copy(vecTemp0).reflect(yAxis);
-		material.uniforms['fresnel'].value = (vecTemp0.dot( vecTemp1 ) + 1.) / 2.; ///todo: Also need to use glsl viewPosition and viewNormal per pixel.
+		material.uniforms[ 'fresnel' ].value = (vecTemp0.dot( vecTemp1 ) + 1.) / 2.; ///todo: Also need to use glsl viewPosition and viewNormal per pixel.
 		// console.log(material.uniforms['fresnel'].value)
 
 		reflectorWorldPosition.setFromMatrixPosition( scope.matrixWorld );
@@ -177,6 +177,13 @@ var Reflector = function ( geometry, options ) {
 		virtualCamera.updateMatrixWorld();
 		virtualCamera.projectionMatrix.copy( camera.projectionMatrix );
 
+		material.uniforms[ 'virtualCameraNear' ].value = virtualCamera.near;
+		material.uniforms[ 'virtualCameraFar' ].value = virtualCamera.far;
+		material.uniforms[ 'virtualCameraMatrixWorld' ].value= virtualCamera.matrixWorld;
+		material.uniforms[ 'virtualCameraProjectionMatrix' ].value= virtualCamera.projectionMatrix;
+		material.uniforms[ 'virtualCameraInverseProjectionMatrix' ].value= virtualCamera.projectionMatrixInverse;
+		material.uniforms[ 'resolution' ].value = scope.resolution;
+
 		// Update the texture matrix
 		textureMatrix.set(
 			0.5, 0.0, 0.0, 0.5,
@@ -188,28 +195,29 @@ var Reflector = function ( geometry, options ) {
 		textureMatrix.multiply( virtualCamera.matrixWorldInverse );
 		textureMatrix.multiply( scope.matrixWorld );
 
-		// Now update projection matrix with new clip plane, implementing code from: http://www.terathon.com/code/oblique.html
-		// Paper explaining this technique: http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
-		reflectorPlane.setFromNormalAndCoplanarPoint( normal, reflectorWorldPosition );
-		reflectorPlane.applyMatrix4( virtualCamera.matrixWorldInverse );
+		///todo:
+		// // Now update projection matrix with new clip plane, implementing code from: http://www.terathon.com/code/oblique.html
+		// // Paper explaining this technique: http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
+		// reflectorPlane.setFromNormalAndCoplanarPoint( normal, reflectorWorldPosition );
+		// reflectorPlane.applyMatrix4( virtualCamera.matrixWorldInverse );
 
-		clipPlane.set( reflectorPlane.normal.x, reflectorPlane.normal.y, reflectorPlane.normal.z, reflectorPlane.constant );
+		// clipPlane.set( reflectorPlane.normal.x, reflectorPlane.normal.y, reflectorPlane.normal.z, reflectorPlane.constant );
 
-		var projectionMatrix = virtualCamera.projectionMatrix;
+		// var projectionMatrix = virtualCamera.projectionMatrix;
 
-		q.x = ( Math.sign( clipPlane.x ) + projectionMatrix.elements[ 8 ] ) / projectionMatrix.elements[ 0 ];
-		q.y = ( Math.sign( clipPlane.y ) + projectionMatrix.elements[ 9 ] ) / projectionMatrix.elements[ 5 ];
-		q.z = - 1.0;
-		q.w = ( 1.0 + projectionMatrix.elements[ 10 ] ) / projectionMatrix.elements[ 14 ];
+		// q.x = ( Math.sign( clipPlane.x ) + projectionMatrix.elements[ 8 ] ) / projectionMatrix.elements[ 0 ];
+		// q.y = ( Math.sign( clipPlane.y ) + projectionMatrix.elements[ 9 ] ) / projectionMatrix.elements[ 5 ];
+		// q.z = - 1.0;
+		// q.w = ( 1.0 + projectionMatrix.elements[ 10 ] ) / projectionMatrix.elements[ 14 ];
 
-		// Calculate the scaled plane vector
-		clipPlane.multiplyScalar( 2.0 / clipPlane.dot( q ) );
+		// // Calculate the scaled plane vector
+		// clipPlane.multiplyScalar( 2.0 / clipPlane.dot( q ) );
 
-		// Replacing the third row of the projection matrix
-		projectionMatrix.elements[ 2 ] = clipPlane.x;
-		projectionMatrix.elements[ 6 ] = clipPlane.y;
-		projectionMatrix.elements[ 10 ] = clipPlane.z + 1.0 - clipBias;
-		projectionMatrix.elements[ 14 ] = clipPlane.w;
+		// // Replacing the third row of the projection matrix
+		// projectionMatrix.elements[ 2 ] = clipPlane.x;
+		// projectionMatrix.elements[ 6 ] = clipPlane.y;
+		// projectionMatrix.elements[ 10 ] = clipPlane.z + 1.0 - clipBias;
+		// projectionMatrix.elements[ 14 ] = clipPlane.w;
 
 		// Render
 
@@ -264,20 +272,27 @@ Reflector.prototype.constructor = Reflector;
 
 Reflector.ReflectorShader = { ///todo: Will conflict with Reflector.js?
 
-  defines: {
-    isDistanceAttenuation: true,
-    isFresnel: true,
-  },
+	defines: {
+		isDistanceAttenuation: true,
+		isFresnel: true,
+	},
 
 	uniforms: {
 
 		color: { value: null },
 		tDiffuse: { value: null },
 		tDepth: { value: null },
-		textureMatrix: { value: null },
-    maxDistance: { value: 180 },
-    opacity: { value: .5 },
-    fresnel: { value: null },
+		textureMatrix: { value: new Matrix4() },
+		maxDistance: { value: 180 },
+		opacity: { value: .5 },
+		fresnel: { value: null },
+		worldYBias: { value: null },
+		virtualCameraNear: { value: null },
+		virtualCameraFar: { value: null },
+		virtualCameraProjectionMatrix: { value: new Matrix4() },
+		virtualCameraMatrixWorld: { value: new Matrix4() },
+		virtualCameraInverseProjectionMatrix: { value: new Matrix4() },
+		resolution: { value: new Vector2() },
 
 	},
 
@@ -301,21 +316,49 @@ Reflector.ReflectorShader = { ///todo: Will conflict with Reflector.js?
 		uniform float maxDistance;
 		uniform float opacity;
 		uniform float fresnel;
+		uniform float worldYBias;
+		uniform float virtualCameraNear;
+		uniform float virtualCameraFar;
+		uniform mat4 virtualCameraProjectionMatrix;
+		uniform mat4 virtualCameraInverseProjectionMatrix;
+		uniform mat4 virtualCameraMatrixWorld;
+		uniform vec2 resolution;
 		varying vec4 vUv;
+		#include <packing>
 		float blendOverlay( float base, float blend ) {
 			return( base < 0.5 ? ( 2.0 * base * blend ) : ( 1.0 - 2.0 * ( 1.0 - base ) * ( 1.0 - blend ) ) );
 		}
 		vec3 blendOverlay( vec3 base, vec3 blend ) {
 			return vec3( blendOverlay( base.r, blend.r ), blendOverlay( base.g, blend.g ), blendOverlay( base.b, blend.b ) );
 		}
+		float getDepth( const in vec2 uv ) {
+			return texture2D( tDepth, uv ).x;
+		}
+		float getViewZ( const in float depth ) {
+			return perspectiveDepthToViewZ( depth, virtualCameraNear, virtualCameraFar );
+		}
+		vec3 getViewPosition( const in vec2 uv, const in float depth/*clip space*/, const in float clipW ) {
+			vec4 clipPosition = vec4( ( vec3( uv, depth ) - 0.5 ) * 2.0, 1.0 );//ndc ///todo: use gl_FragCoord to get uv
+			clipPosition *= clipW; //clip
+			return ( virtualCameraInverseProjectionMatrix * clipPosition ).xyz;//view
+		}
 		void main() {
 			vec4 base = texture2DProj( tDiffuse, vUv );
 			#ifdef useDepthTexture
-				float op=opacity;
+				vec2 uv=(gl_FragCoord.xy-.5)/resolution.xy;
+				uv.x=1.-uv.x;
 				float depth = texture2DProj( tDepth, vUv ).r;
-				if(depth>maxDistance) discard;
+				float viewZ = getViewZ( depth );
+				float clipW = virtualCameraProjectionMatrix[2][3] * viewZ+virtualCameraProjectionMatrix[3][3];
+				vec3 viewPosition=getViewPosition( uv, depth, clipW );
+				vec3 worldPosition=(virtualCameraMatrixWorld*vec4(viewPosition,1)).xyz;
+				// gl_FragColor=vec4(vec3(worldPosition.y),1);return;
+				worldPosition.y+=worldYBias; ///todo: Don't know why not start from zero, temporarily use manually defined bias, need fix afterwards.
+				worldPosition.y=max(0.,worldPosition.y);
+				if(worldPosition.y>maxDistance) discard;
+				float op=opacity;
 				#ifdef isDistanceAttenuation
-					float ratio=1.-(depth/maxDistance);
+					float ratio=1.-(worldPosition.y/maxDistance);
 					float attenuation=ratio*ratio;
 					op=opacity*attenuation;
 				#endif
