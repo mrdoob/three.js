5947c2dc52dafaf2ff77e33f9ba5c8cd2fc53d16
beginor <beginor@qq.com>
2021-03-05 22:55:35 +0800

    ArrayCamera: Convert to es6 class. (#21407)
    * refact to use es6
    
    * refact to es6
    
    * fix build
    
    * fix lint
    
    * fix static methods.
    
    * revert EventDispatcher.js since it is converted by someone else
    
    * revert PropertyBindings.js since it is converted by someone else
    
    * revert Camera.js and PerspectiveCamera.js, since it break the test.

--- src/cameras/ArrayCamera.js
+++ src/cameras/ArrayCamera.js
@@ -1,20 +1,18 @@
 import { PerspectiveCamera } from './PerspectiveCamera.js';
 
-function ArrayCamera( array = [] ) {
+class ArrayCamera extends PerspectiveCamera {
 
-	PerspectiveCamera.call( this );
+	constructor( array = [] ) {
 
-	this.cameras = array;
+		super();
+
+		this.cameras = array;
+
+	}
 
 }
 
-ArrayCamera.prototype = Object.assign( Object.create( PerspectiveCamera.prototype ), {
-
-	constructor: ArrayCamera,
-
-	isArrayCamera: true
-
-} );
+ArrayCamera.prototype.isArrayCamera = true;
 
 
 export { ArrayCamera };
