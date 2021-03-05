0b7896e93442db3de5a8770073cd93301cc10b75
Mr.doob <info@mrdoob.com>
2021-03-05 16:06:04 +0000

    Water: Removed outputEncoding and toneMapping checks

--- examples/jsm/objects/Water.js
+++ examples/jsm/objects/Water.js
@@ -1,12 +1,10 @@
 import {
 	Color,
 	FrontSide,
-	LinearEncoding,
 	LinearFilter,
 	MathUtils,
 	Matrix4,
 	Mesh,
-	NoToneMapping,
 	PerspectiveCamera,
 	Plane,
 	RGBFormat,
@@ -302,24 +300,6 @@ var Water = function ( geometry, options ) {
 		eye.setFromMatrixPosition( camera.matrixWorld );
 
 		// Render
-
-		if ( renderer.outputEncoding !== LinearEncoding ) {
-
-			console.warn( 'THREE.Water: WebGLRenderer must use LinearEncoding as outputEncoding.' );
-			scope.onBeforeRender = function () {};
-
-			return;
-
-		}
-
-		if ( renderer.toneMapping !== NoToneMapping ) {
-
-			console.warn( 'THREE.Water: WebGLRenderer must use NoToneMapping as toneMapping.' );
-			scope.onBeforeRender = function () {};
-
-			return;
-
-		}
 
 		var currentRenderTarget = renderer.getRenderTarget();
 
