059e8205b85b58f55bf8e2f1b60d3ada78c91b80
Johnathon Selstad <makeshifted@gmail.com>
2021-03-08 22:03:24 -0800

    Get Lightmap Packing Working

--- examples/jsm/misc/ProgressiveSurfacemap.js
+++ examples/jsm/misc/ProgressiveSurfacemap.js
@@ -1,4 +1,5 @@
 import * as THREE from '../../../build/three.module.js';
+import { potpack } from '../libs/potpack.module.js';
 
 /**
  * Progressive Surface Map Accumulator, by [zalo](https://github.com/zalo/)
@@ -14,85 +15,101 @@ import * as THREE from '../../../build/three.module.js';
  * @param {Scene} scene The scene the objects are contextualized in
  * @param {WebGLRenderer} renderer A WebGL Rendering Context
  */
-var ProgressiveSurfacemap = function (renderer) {
+var ProgressiveSurfacemap = function (renderer, res = 1024) {
 
 	this.surfacemapContainers = [];
-	this.blurringPlane = null;
 	this.compiled = false;
 	this.scene = new THREE.Scene();
 	this.scene.background = null;
-	let tinyTarget = new THREE.WebGLRenderTarget(1, 1);
+	this.tinyTarget = new THREE.WebGLRenderTarget(1, 1);
+	this.buffer1Active = false;
+
+	// Create the Progressive Surfacemap Texture
+	let format = /(iPad|iPhone|iPod)/g.test(navigator.userAgent) ? THREE.HalfFloatType : THREE.FloatType;
+	this.progressiveSurfacemap1 = new THREE.WebGLRenderTarget(res, res, { type: format });
+	this.progressiveSurfacemap2 = new THREE.WebGLRenderTarget(res, res, { type: format });
+
+	// Inject some spicy new logic into a standard phong material
+	this.uvMat = new THREE.MeshPhongMaterial();
+	this.uvMat.uniforms = {};
+	this.uvMat.onBeforeCompile = (shader) => {
+		// Set Vertex Positions to the Unwrapped UV Positions
+		shader.vertexShader =
+			'#define USE_UV\n#define USE_LIGHTMAP\n' +
+			shader.vertexShader.slice(0, -1) +
+			'	gl_Position = vec4((uv2 - 0.5) * 2.0, 1.0, 1.0); }';
+
+		// Set Pixels to average in the Previous frame's Shadows
+		let bodyStart	= shader.fragmentShader.indexOf('void main() {');
+		shader.fragmentShader =
+			'#define USE_UV\nvarying vec2 vUv2;\n'+
+			shader.fragmentShader.slice(0, bodyStart) +
+			'	uniform sampler2D previousShadowMap;\n	uniform float averagingWindow;\n' +
+			shader.fragmentShader.slice(   bodyStart-1, -1) +
+				`\nvec3 texelOld = texture(previousShadowMap, vUv2).rgb;
+				gl_FragColor.rgb = mix(texelOld, gl_FragColor.rgb, 1.0/averagingWindow);
+			}`;
+		
+		// Set the Previous Frame's Texture Buffer and Averaging Window
+		shader.uniforms.previousShadowMap = { value: this.progressiveSurfacemap1.texture };
+		shader.uniforms.averagingWindow   = { value: 100 };
+
+		this.uvMat.uniforms = shader.uniforms;
+		
+		// Set the new Shader to this
+		this.uvMat.userData.shader = shader;
+
+		this.compiled = true;
+	};
 
 	/**
 	 * Replaces this objects' material with a surface map to be rendered to later.
 	 * @param {Object3D} object The object to create a surfacemap for.
 	 * @param {number} res The square resolution of this object's surfacemap.
 	 */
-    this.add = function (object, res = 512) {
-		if (object.isLight) { this.scene.attach(object); return; }
+	this.addObjectsToLightMap = function (objects, res = 1024) {
+		// Prepare list of UV bounding boxes for packing later...
+		this.uv_boxes = [];
 
-		// Create the Progressive Surfacemap Texture
-		let format = /(iPad|iPhone|iPod)/g.test(navigator.userAgent) ? THREE.HalfFloatType : THREE.FloatType;
-		let progressiveSurfacemap1 = new THREE.WebGLRenderTarget(res, res, { type: format });
-		let progressiveSurfacemap2 = new THREE.WebGLRenderTarget(res, res, { type: format });
-
-		if(this.blurringPlane == null) { this._initializeBlurPlane(res, progressiveSurfacemap1); }
-
-		// Inject some spicy new logic into a standard phong material
-		let oldMaterial = new THREE.MeshPhongMaterial();
-		oldMaterial.uniforms = {};
-		oldMaterial.onBeforeCompile = (shader) => {
-			// Set Vertex Positions to the Unwrapped UV Positions
-			shader.vertexShader =
-				'#define USE_UV\n' +
-				shader.vertexShader.slice(0, -1) +
-				'	gl_Position = vec4((uv - 0.5) * 2.0, 1.0, 1.0); }';
-			
-			// Set Pixels to average in the Previous frame's Shadows
-			let bodyStart	= shader.fragmentShader.indexOf('void main() {');
-			shader.fragmentShader =
-				'#define USE_UV\n'+
-				shader.fragmentShader.slice(0, bodyStart) +
-				'	uniform sampler2D previousShadowMap;\n	uniform float averagingWindow;\n' +
-				shader.fragmentShader.slice(   bodyStart-1, -1) +
-					`\nvec3 texelOld = texture(previousShadowMap, vUv).rgb;
-					gl_FragColor.rgb = mix(texelOld, gl_FragColor.rgb, 1.0/averagingWindow);
-				}`;
-			
-			// Set the Previous Frame's Texture Buffer and Averaging Window
-			shader.uniforms.previousShadowMap = { value: progressiveSurfacemap1.texture };
-			shader.uniforms.averagingWindow   = { value: 100 };
-
-			oldMaterial.uniforms = shader.uniforms;
-			
-			// Set the new Shader to this
-			oldMaterial.userData.shader = shader;
-
-			this.compiled = true;
-		};
-
-		// Apply the lightmap to the object
-		object.material.lightMap  = progressiveSurfacemap2.texture;
-		object.material.dithering = true;
-		object.castShadow         = true;
-		object.receiveShadow      = true;
-
-		// For now, clone the existing UVs over to the Lightmap UV Channel
-		if (object.geometry.hasAttribute("uv")) {
-			let uv1 = object.geometry.getAttribute("uv");
-			object.geometry.setAttribute("uv2", uv1.clone());
+		for (let ob = 0; ob < objects.length; ob++){
+			let object = objects[ob];
+			if (object.isLight) { this.scene.attach(object); continue; }
+			if (!object.geometry.hasAttribute("uv")) { console.warn("All lightmap objects need UVs!"); continue; }
+	
+			if(this.blurringPlane == null) { this._initializeBlurPlane(res, this.progressiveSurfacemap1); }
+	
+			// Apply the lightmap to the object
+			object.material.lightMap  = this.progressiveSurfacemap2.texture;
+			object.material.dithering = true;
+			object.castShadow         = true;
+			object.receiveShadow      = true;
+			object.renderOrder        = 1000;
+	
+			// Prepare map for potpack
+			this.uv_boxes.push({ w: 1, h: 1, index: ob });
+	
+			this.surfacemapContainers.push({
+				basicMat: object.material,
+				object  : object
+			});
+	
+			this.compiled = false;
 		}
 
-		this.surfacemapContainers.push({
-			basicMat: object.material,
-			uvMat   : oldMaterial,
-			object  : object,
-			surfacemap1: progressiveSurfacemap1,
-			surfacemap2: progressiveSurfacemap2,
-			buffer1Active: false
-		});
+		// Pack all of the objects into the same "lightmap"
+		const dimensions = potpack(this.uv_boxes);
+		let maxDim = Math.max(dimensions.w, dimensions.h);
 
-		this.compiled = false;
+		// Pack the objects' lightmap UVs into the same space
+		this.uv_boxes.forEach((box) => {
+			let uv2 = objects[box.index].geometry.getAttribute("uv").clone();
+			for (let i = 0; i < uv2.array.length; i += uv2.itemSize) {
+				uv2.array[i    ] = (uv2.array[i    ] + box.x) / dimensions.w;
+				uv2.array[i + 1] = (uv2.array[i + 1] + box.y) / dimensions.h;
+			}
+			objects[box.index].geometry.setAttribute("uv2", uv2);
+			objects[box.index].geometry.getAttribute("uv2").needsUpdate = true;
+		});
 	}
 
 	/**
@@ -102,54 +119,48 @@ var ProgressiveSurfacemap = function (renderer) {
 	 * @param {boolean} blurEdges  Whether to fix UV Edges via blurring
 	 */
 	this.update = function (camera, blendWindow = 100, blurEdges = true) {
+		if (this.blurringPlane == null) { return; }
+
 		// Whether or not to blur the edges of the lightmap...
 		this.blurringPlane.visible = blurEdges;
 
-		// Phase 1: Bake Shadows
+		// Steal the Object3D from the real world to our special dimension
 		for (let l = 0; l < this.surfacemapContainers.length; l++) {
-			// Steal the Object3D from the real world to our special dimension
 			this.surfacemapContainers[l].object.oldScene = this.surfacemapContainers[l].object.parent;
 			this.scene.attach(this.surfacemapContainers[l].object);
 		}
 
 		// Render once to bake the shadows
 		let oldTarget = renderer.getRenderTarget();
-		renderer.setRenderTarget(tinyTarget); // Tiny Target Hopefully keeps this from being expensive...
+		renderer.setRenderTarget(this.tinyTarget); // Tiny for Speed
 		renderer.render(this.scene, camera);
 
-		// Add them back to the outer scene to hide them from this one...
-		for (let l = 0; l < this.surfacemapContainers.length; l++) {
-			this.surfacemapContainers[l].object.oldScene.attach(this.surfacemapContainers[l].object);
+		// Set each object's material to the UV Unwrapped Surface Mapping Version
+		for (let l = 0; l < this.surfacemapContainers.length; l++){
+			this.uvMat .uniforms.averagingWindow = { value: blendWindow };
+			this.surfacemapContainers[l].object.material = this.uvMat;
+			this.surfacemapContainers[l].object.oldFrustumCulled =
+				this.surfacemapContainers[l].object.frustumCulled;
+			this.surfacemapContainers[l].object.frustumCulled = false;
 		}
 
-		// Phase 2: Iterate and render each object's surface maps individually
+		// Ping-pong two surface buffers for reading/writing
+		let activeMap   = this.buffer1Active ? this.progressiveSurfacemap1 : this.progressiveSurfacemap2;
+		let inactiveMap = this.buffer1Active ? this.progressiveSurfacemap2 : this.progressiveSurfacemap1;
+
+		// Render the object's surface maps
+		renderer.setRenderTarget(activeMap);
+		this.                 uvMat.uniforms.previousShadowMap = { value: inactiveMap.texture };
+		this.blurringPlane.material.uniforms.previousShadowMap = { value: inactiveMap.texture };
+		this.buffer1Active = !this.buffer1Active;
+		renderer.render( this.scene, camera );
+
+		// Restore the object's Real-time Material and add it back to the original world
 		for (let l = 0; l < this.surfacemapContainers.length; l++){
-			// Steal the Object3D from the real world to our special dimension
-			let oldScene = this.surfacemapContainers[l].object.parent;
-			this.scene.attach(this.surfacemapContainers[l].object);
-
-			// Set each object's material to the UV Unwrapped Surface Mapping Version
-			this.surfacemapContainers[l].uvMat .uniforms.averagingWindow = { value: blendWindow };
-			this.surfacemapContainers[l].object.material                 = this.surfacemapContainers[l].uvMat;
-
-			// Ping-pong two surface buffers for reading/writing
-			let activeMap = this.surfacemapContainers[l].buffer1Active ?
-				this.surfacemapContainers[l].surfacemap1 :
-				this.surfacemapContainers[l].surfacemap2;
-			let inactiveMap = this.surfacemapContainers[l].buffer1Active ?
-				this.surfacemapContainers[l].surfacemap2 :
-				this.surfacemapContainers[l].surfacemap1;
-			
-			// Render the object's surface maps
-			renderer.setRenderTarget(activeMap);
-			this.surfacemapContainers[l].uvMat.uniforms.previousShadowMap = { value: inactiveMap.texture };
-			this.blurringPlane.material       .uniforms.previousShadowMap = { value: inactiveMap.texture };
-			this.surfacemapContainers[l].buffer1Active = !this.surfacemapContainers[l].buffer1Active;
-			renderer.render( this.scene, camera );
-
-			// Restore the object's Real-time Material and add it back to the original world
+			this.surfacemapContainers[l].object.frustumCulled =
+				this.surfacemapContainers[l].object.oldFrustumCulled;
 			this.surfacemapContainers[l].object.material = this.surfacemapContainers[l].basicMat;
-			oldScene.attach(this.surfacemapContainers[l].object);
+			this.surfacemapContainers[l].object.oldScene.attach(this.surfacemapContainers[l].object);
 		}
 		renderer.setRenderTarget(oldTarget);
 	}
@@ -162,7 +173,7 @@ var ProgressiveSurfacemap = function (renderer) {
 		let blurMaterial = new THREE.MeshBasicMaterial();
 		blurMaterial.uniforms = { previousShadowMap : { value: null    },
 								  pixelOffset       : { value: 1.0/res },
-								  polygonOffset: true, polygonOffsetFactor: 1, polygonOffsetUnits: 3.0};
+								  polygonOffset: true, polygonOffsetFactor: -1, polygonOffsetUnits: 3.0};
 		blurMaterial.onBeforeCompile = (shader) => {
 			// Set Vertex Positions to the Unwrapped UV Positions
 			shader.vertexShader =
@@ -178,10 +189,10 @@ var ProgressiveSurfacemap = function (renderer) {
 				'	uniform sampler2D previousShadowMap;\n	uniform float pixelOffset;\n' +
 				shader.fragmentShader.slice(   bodyStart-1, -1) +
 					`	gl_FragColor.rgb = (
-									texture(previousShadowMap, vUv + vec2( pixelOffset,  0.0		)).rgb + 
-									texture(previousShadowMap, vUv + vec2( 0.0		,  pixelOffset)).rgb +
-									texture(previousShadowMap, vUv + vec2( 0.0		, -pixelOffset)).rgb +
-									texture(previousShadowMap, vUv + vec2(-pixelOffset,  0.0		)).rgb +
+									texture(previousShadowMap, vUv + vec2( pixelOffset,  0.0        )).rgb + 
+									texture(previousShadowMap, vUv + vec2( 0.0        ,  pixelOffset)).rgb +
+									texture(previousShadowMap, vUv + vec2( 0.0        , -pixelOffset)).rgb +
+									texture(previousShadowMap, vUv + vec2(-pixelOffset,  0.0        )).rgb +
 									texture(previousShadowMap, vUv + vec2( pixelOffset,  pixelOffset)).rgb + 
 									texture(previousShadowMap, vUv + vec2(-pixelOffset,  pixelOffset)).rgb +
 									texture(previousShadowMap, vUv + vec2( pixelOffset, -pixelOffset)).rgb +
@@ -202,6 +213,8 @@ var ProgressiveSurfacemap = function (renderer) {
 		this.blurringPlane = new THREE.Mesh(new THREE.PlaneBufferGeometry(1, 1), blurMaterial);
 		this.blurringPlane.name = "Blurring Plane"
 		this.blurringPlane.frustumCulled = false;
+		this.blurringPlane.renderOrder = 0;
+		this.blurringPlane.material.depthWrite = false;
 		this.scene.add(this.blurringPlane);
 	}
 }
