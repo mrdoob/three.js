3d1f569af4a2717426d52d414dd568bac38fb52b
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-03-11 22:04:53 -0800

    Smoothen line

--- examples/js/lines/LineMaterial.js
+++ examples/js/lines/LineMaterial.js
@@ -222,7 +222,8 @@ THREE.ShaderLib[ 'line' ] = {
 
 				#ifdef ALPHA_TO_COVERAGE
 
-				alpha = 1.0 - smoothstep( 1.0 - fwidth( len2 ), 1.0, len2 );
+				float dlen = fwidth( len2 );
+				alpha = 1.0 - smoothstep( 1.0 - dlen * 0.75, 1.0 + dlen * 0.25, len2 );
 
 				#else
 
