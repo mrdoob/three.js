782147972049ff476a86047e91e17e778206945d
Takahiro <hogehoge@gachapin.jp>
2021-03-15 11:04:11 -0700

    BufferAttribute .name, .usage, .updateRange serialization (#21279)
    * Serialize BufferAttribute .name, .usage, and .updateRange
    
    * Tests: Update BufferAttribute unit test to check .name, .usage, and .updateRange serialization
    
    * Tests: Add BufferLoader unit test to check if serialized BufferAttribute can be deserialized
    
    * Remove BufferAttribute.name serialization from BufferGeometry.toJSON() because it should be done in BufferAttribute.toJSON()

--- src/core/BufferGeometry.js
+++ src/core/BufferGeometry.js
@@ -931,11 +931,7 @@ BufferGeometry.prototype = Object.assign( Object.create( EventDispatcher.prototy
 
 			const attribute = attributes[ key ];
 
-			const attributeData = attribute.toJSON( data.data );
-
-			if ( attribute.name !== '' ) attributeData.name = attribute.name;
-
-			data.data.attributes[ key ] = attributeData;
+			data.data.attributes[ key ] = attribute.toJSON( data.data );
 
 		}
 
@@ -952,11 +948,7 @@ BufferGeometry.prototype = Object.assign( Object.create( EventDispatcher.prototy
 
 				const attribute = attributeArray[ i ];
 
-				const attributeData = attribute.toJSON( data.data );
-
-				if ( attribute.name !== '' ) attributeData.name = attribute.name;
-
-				array.push( attributeData );
+				array.push( attribute.toJSON( data.data ) );
 
 			}
 
