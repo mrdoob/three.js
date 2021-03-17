0a7046f6c45fc359519a0661dba6e3c40c15aa8c
dineshsalunke <dineshsalunke@github.com>
2021-03-17 09:37:21 +0530

    feat(texture-panel): basic texture panel in the asstes section

--- editor/js/Sidebar.Project.Textures.js
+++ editor/js/Sidebar.Project.Textures.js
@@ -1,5 +1,6 @@
 import { UIPanel, UIRow, UIText, UIListbox, UIInput, UINumber, UICheckbox, UISelect } from './libs/ui.js';
 import { SetTextureValueCommand } from './commands/SetTextureValueCommand.js';
+import { SetTextureVectorCommand } from './commands/SetTextureVectorCommand.js';
 
 function SidebarProjectTextures( editor ) {
 
@@ -60,65 +61,34 @@ function SidebarProjectTextures( editor ) {
 
 	propertiesContainer.add( textureRowName );
 
-	// anisotropy
-	var anisotropyRow = new UIRow();
-	var anisotropy = new UINumber( 0.5 ).setWidth( '60px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-
-
-	} );
-	anisotropyRow.add( new UIText( strings.getKey( 'sidebar/texture/anisotropy' ) ).setWidth( '90px' ) );
-	anisotropyRow.add( anisotropy );
-
-	propertiesContainer.add( anisotropyRow );
-
 	// encoding
 	var encodingRow = new UIRow();
 
 	var encoding = new UISelect().setOptions( {
 
-		0: strings.getKey( 'sidebar/textures/encoding/linear' ),
-		1: strings.getKey( 'sidebar/textures/encoding/srgb' ),
-		2: strings.getKey( 'sidebar/textures/encoding/gamma' ),
-		3: strings.getKey( 'sidebar/textures/encoding/rgbe' ),
-		4: strings.getKey( 'sidebar/textures/encoding/logluv' ),
-		5: strings.getKey( 'sidebar/textures/encoding/rbgm7' ),
-		6: strings.getKey( 'sidebar/textures/encoding/rgbm16' ),
-		7: strings.getKey( 'sidebar/textures/encoding/rgbd' ),
-		8: strings.getKey( 'sidebar/textures/encoding/basicdepthpacking' ),
-		9: strings.getKey( 'sidebar/textures/encoding/rgbapacking' ),
+		3000: strings.getKey( 'sidebar/texture/encoding/linear' ),
+		3001: strings.getKey( 'sidebar/texture/encoding/srgb' ),
+		3007: strings.getKey( 'sidebar/texture/encoding/gamma' ),
+		3002: strings.getKey( 'sidebar/texture/encoding/rgbe' ),
+		3003: strings.getKey( 'sidebar/texture/encoding/logluv' ),
+		3004: strings.getKey( 'sidebar/texture/encoding/rbgm7' ),
+		3005: strings.getKey( 'sidebar/texture/encoding/rgbm16' ),
+		3006: strings.getKey( 'sidebar/texture/encoding/rgbd' ),
+		3200: strings.getKey( 'sidebar/texture/encoding/basicdepthpacking' ),
+		3201: strings.getKey( 'sidebar/texture/encoding/rgbapacking' ),
 
-	} ).setWidth( '150px' ).setFontSize( '14px' );
+	} ).setWidth( '150px' ).setFontSize( '14px' ).onChange( update );
 
 	encodingRow.add( new UIText( strings.getKey( 'sidebar/texture/encoding' ) ).setWidth( '90px' ) );
 	encodingRow.add( encoding );
 
 	propertiesContainer.add( encodingRow );
 
-	// flip
-	var flipRow = new UIRow();
-	var flipYEnabled = new UICheckbox( false ).onChange( function ( ) {
-
-	} );
-
-	flipRow.add( new UIText( strings.getKey( 'sidebar/texture/flipY' ) ).setWidth( '90px' ) );
-	flipRow.add( flipYEnabled );
-
-	propertiesContainer.add( flipRow );
-
-	// format
-	var formatRow = new UIRow();
-	var format = new UISelect().setOptions( {
-
-	} ).setWidth( '150px' ).setFontSize( '14px' );
-	formatRow.add( new UIText( strings.getKey( 'sidebar/texture/format' ) ).setWidth( '90px' ) );
-	formatRow.add( format );
-	propertiesContainer.add( formatRow );
-
 	// magFilter
 	var magFilterRow = new UIRow();
 	var magFilter = new UISelect().setOptions( {
-
+		1003: strings.getKey( 'sidebar/texture/magfilter/nearest' ),
+		1006: strings.getKey( 'sidebar/texture/magfilter/linear' )
 	} ).setWidth( '150px' ).setFontSize( '14px' );
 
 	magFilterRow.add( new UIText( strings.getKey( 'sidebar/texture/magFilter' ) ).setWidth( '90px' ) );
@@ -128,18 +98,34 @@ function SidebarProjectTextures( editor ) {
 	// minFilter
 	var minFilterRow = new UIRow();
 	var minFilter = new UISelect().setOptions( {
-
+		1003: strings.getKey( 'sidebar/texture/minfilter/nearest' ),
+		1004: strings.getKey( 'sidebar/texture/minfilter/nearestmipmapnearest' ),
+		1005: strings.getKey( 'sidebar/texture/minfilter/nearestmipmaplinear' ),
+		1006: strings.getKey( 'sidebar/texture/minfilter/linear' ),
+		1007: strings.getKey( 'sidebar/texture/minfilter/linearmipmapnearest' ),
+		1008: strings.getKey( 'sidebar/texture/minfilter/linearmipmaplinear' ),
 	} ).setWidth( '150px' ).setFontSize( '14px' );
 
 	minFilterRow.add( new UIText( strings.getKey( 'sidebar/texture/minFilter' ) ).setWidth( '90px' ) );
 	minFilterRow.add( minFilter );
 	propertiesContainer.add( minFilterRow );
 
+	// flip
+	var flipRow = new UIRow();
+	var flipYEnabled = new UICheckbox( false ).onChange( update );
+
+	flipRow.add( new UIText( strings.getKey( 'sidebar/texture/flipY' ) ).setWidth( '90px' ) );
+	flipRow.add( flipYEnabled );
+
+	propertiesContainer.add( flipRow );
+
 	// wrapS
 	var wrapSRow = new UIRow();
 	var wrapS = new UISelect().setOptions( {
-
-	} ).setWidth( '150px' ).setFontSize( '14px' );
+		1000: strings.getKey( 'sidebar/texture/wrapping/repeat' ),
+		1001: strings.getKey( 'sidebar/texture/wrapping/calmptoedge' ),
+		1002: strings.getKey( 'sidebar/texture/wrapping/mirroredrepeat' ),
+	} ).setWidth( '150px' ).setFontSize( '14px' ).onChange( update );
 
 	wrapSRow.add( new UIText( strings.getKey( 'sidebar/texture/wrapS' ) ).setWidth( '90px' ) );
 	wrapSRow.add( wrapS );
@@ -149,8 +135,10 @@ function SidebarProjectTextures( editor ) {
 	// wrapT
 	var wrapTRow = new UIRow();
 	var wrapT = new UISelect().setOptions( {
-
-	} ).setWidth( '150px' ).setFontSize( '14px' );
+		1000: strings.getKey( 'sidebar/texture/wrapping/repeat' ),
+		1001: strings.getKey( 'sidebar/texture/wrapping/calmptoedge' ),
+		1002: strings.getKey( 'sidebar/texture/wrapping/mirroredrepeat' ),
+	} ).setWidth( '150px' ).setFontSize( '14px' ).onChange( update );
 
 	wrapTRow.add( new UIText( strings.getKey( 'sidebar/texture/wrapT' ) ).setWidth( '90px' ) );
 	wrapTRow.add( wrapT );
@@ -158,12 +146,8 @@ function SidebarProjectTextures( editor ) {
 
 	// center
 	var centerRow = new UIRow();
-	var centerX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
-	var centerY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
+	var centerX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( update );
+	var centerY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( update );
 
 	centerRow.add( new UIText( strings.getKey( 'sidebar/texture/center' ) ).setWidth( '90px' ) );
 	centerRow.add( centerX );
@@ -173,12 +157,8 @@ function SidebarProjectTextures( editor ) {
 
 	// offset
 	var offsetRow = new UIRow();
-	var offsetX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
-	var offsetY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
+	var offsetX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1000 ).onChange( update );
+	var offsetY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1000 ).onChange( update );
 
 	offsetRow.add( new UIText( strings.getKey( 'sidebar/texture/offset' ) ).setWidth( '90px' ) );
 	offsetRow.add( offsetX );
@@ -188,12 +168,8 @@ function SidebarProjectTextures( editor ) {
 
 	// repeat
 	var repeatRow = new UIRow();
-	var repeatX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
-	var repeatY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
+	var repeatX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1000 ).onChange( update );
+	var repeatY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1000 ).onChange( update );
 
 	repeatRow.add( new UIText( strings.getKey( 'sidebar/texture/repeat' ) ).setWidth( '90px' ) );
 	repeatRow.add( repeatX );
@@ -202,12 +178,8 @@ function SidebarProjectTextures( editor ) {
 
 	// rotation
 	var rotationRow = new UIRow();
-	var rotationX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
-	var rotationY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
+	var rotationX = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1000 ).onChange( update );
+	var rotationY = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1000 ).onChange( update );
 
 	rotationRow.add( new UIText( strings.getKey( 'sidebar/texture/rotation' ) ).setWidth( '90px' ) );
 	rotationRow.add( rotationX );
@@ -217,39 +189,88 @@ function SidebarProjectTextures( editor ) {
 
 	// premultiplyAlpha
 	var premultiplyAlphaRow = new UIRow();
-	var premultiplyAlpha = new UICheckbox( false ).onChange( function ( ) {
-
-	} );
+	var premultiplyAlpha = new UICheckbox( false ).onChange( update );
 
 	premultiplyAlphaRow.add( new UIText( strings.getKey( 'sidebar/texture/premultiplyAlpha' ) ).setWidth( '150px' ) );
 	premultiplyAlphaRow.add( premultiplyAlpha );
 	propertiesContainer.add( premultiplyAlphaRow );
 
-	// type
-	var typeRow = new UIRow();
-	var type = new UISelect().setOptions( {
-
-	} ).setWidth( '150px' ).setFontSize( '14px' );
-
-	typeRow.add( new UIText( strings.getKey( 'sidebar/texture/type' ) ).setWidth( '90px' ) );
-	typeRow.add( type );
-	propertiesContainer.add( typeRow );
-
-	// unpackAlignment
-	var unpackAlignmentRow = new UIRow();
-	var unpackAlignment = new UINumber( 0 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( function ( ) {
-
-	} );
-
-	unpackAlignmentRow.add( new UIText( strings.getKey( 'sidebar/texture/unpackAlignment' ) ).setWidth( '150px' ) );
-	unpackAlignmentRow.add( unpackAlignment );
-
-	propertiesContainer.add( unpackAlignmentRow );
-
 	// functions
 
 	function update( ) {
-		//
+
+		var texture = selectedTexture;
+
+		if ( texture === null ) return;
+
+		if ( texture.name !== undefined && texture.name !== textureName.getValue() ) {
+
+			editor.execute( new SetTextureValueCommand( editor, texture, 'name', textureName.getValue() ) );
+
+		}
+
+		if ( texture.encoding !== parseInt( encoding.getValue() ) ) {
+
+			editor.execute( new SetTextureValueCommand( editor, texture, 'encoding', parseInt( encoding.getValue() ) ) );
+
+		}
+
+		if ( texture.wrapS !== parseInt( wrapS.getValue() ) ) {
+
+			editor.execute( new SetTextureValueCommand( editor, texture, 'wrapS', parseInt( wrapS.getValue() ) ) );
+
+		}
+
+		if ( texture.wrapT !== parseInt( wrapT.getValue() ) ) {
+
+			editor.execute( new SetTextureValueCommand( editor, texture, 'wrapT', parseInt( wrapT.getValue() ) ) );
+
+		}
+
+		if ( texture.flipY !== flipYEnabled.getValue() ) {
+
+			editor.execute( new SetTextureValueCommand( editor, texture, 'flipY', flipYEnabled.getValue() ) );
+
+		}
+
+		if ( texture.premultiplyAlpha !== premultiplyAlpha.getValue() ) {
+
+			editor.execute( new SetTextureValueCommand( editor, texture, 'premultiplyAlpha', premultiplyAlpha.getValue() ) );
+
+		}
+
+		if ( texture.offset.x !== offsetX.getValue() || texture.offset.y !== offsetY.getValue() ) {
+
+			const value = [
+				offsetX.getValue(),
+				offsetY.getValue(),
+			];
+			editor.execute( new SetTextureVectorCommand( editor, texture, 'offset', value ) );
+
+		}
+
+		if ( texture.center.x !== centerX.getValue() || texture.center.y !== centerY.getValue() ) {
+
+			const value = [
+				centerX.getValue(),
+				centerY.getValue(),
+			];
+			editor.execute( new SetTextureVectorCommand( editor, texture, 'center', value ) );
+
+		}
+
+		if ( texture.repeat.x !== repeatX.getValue() || texture.repeat.y !== repeatY.getValue() ) {
+
+			const value = [
+				repeatX.getValue(),
+				repeatY.getValue(),
+			];
+			editor.execute( new SetTextureVectorCommand( editor, texture, 'repeat', value ) );
+
+		}
+
+		refreshPropertiesUI();
+
 	}
 
 	function refreshPropertiesUI( ) {
@@ -257,6 +278,27 @@ function SidebarProjectTextures( editor ) {
 		if ( selectedTexture === null ) return;
 
 		textureName.setValue( selectedTexture.name );
+
+		encoding.setValue( selectedTexture.encoding );
+		magFilter.setValue( selectedTexture.magFilter );
+		minFilter.setValue( selectedTexture.minFilter );
+
+		// Update the wrapping
+		wrapS.setValue( selectedTexture.wrapS );
+		wrapT.setValue( selectedTexture.wrapT );
+
+		offsetX.setValue( selectedTexture.offset.x );
+		offsetY.setValue( selectedTexture.offset.y );
+
+		centerX.setValue( selectedTexture.center.x );
+		centerY.setValue( selectedTexture.center.y );
+
+		repeatX.setValue( selectedTexture.repeat.x );
+		repeatY.setValue( selectedTexture.repeat.y );
+
+		flipYEnabled.setValue( selectedTexture.flipY );
+
+		premultiplyAlpha.setValue( selectedTexture.premultiplyAlpha );
 
 	}
 
