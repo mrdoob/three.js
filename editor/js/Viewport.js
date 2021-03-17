129e91fffa8ee88d4a9915fc9a0e384d7e83f738
dineshsalunke <dineshsalunke@github.com>
2021-03-17 09:36:47 +0530

    refactor(viewports): render scene on texture change

--- editor/js/Viewport.js
+++ editor/js/Viewport.js
@@ -515,6 +515,12 @@ function Viewport( editor ) {
 
 	} );
 
+	signals.textureChanged.add( function () {
+
+		render();
+
+	} );
+
 	signals.animationStopped.add( function () {
 
 		render();
@@ -559,7 +565,11 @@ function Viewport( editor ) {
 
 					var renderTarget = new THREE.WebGLCubeRenderTarget( 512 );
 					renderTarget.fromEquirectangularTexture( renderer, backgroundEquirectangularTexture );
-					renderTarget.toJSON = function () { return null }; // TODO Remove hack
+					renderTarget.toJSON = function () {
+
+						return null;
+
+					}; // TODO Remove hack
 
 					scene.background = renderTarget;
 
