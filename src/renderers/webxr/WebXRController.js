04a2ae7625ae580771c6b3676b56504b2dac41e4
davehill00 <59743491+davehill00@users.noreply.github.com>
2021-03-26 20:52:11 -0700

    fix check for existence of angular and linear velocity to fail gracefully when not present

--- src/renderers/webxr/WebXRController.js
+++ src/renderers/webxr/WebXRController.js
@@ -137,7 +137,7 @@ Object.assign( WebXRController.prototype, {
 					targetRay.matrix.fromArray( inputPose.transform.matrix );
 					targetRay.matrix.decompose( targetRay.position, targetRay.rotation, targetRay.scale );
 
-					if ( inputPose.linearVelocity !== null ) {
+					if ( inputPose.linearVelocity != null ) {
 
 						targetRay.hasLinearVelocity = true;
 						targetRay.linearVelocity.set( inputPose.linearVelocity.x, inputPose.linearVelocity.y, inputPose.linearVelocity.z );
@@ -148,7 +148,7 @@ Object.assign( WebXRController.prototype, {
 
 					}
 
-					if ( inputPose.angularVelocity !== null ) {
+					if ( inputPose.angularVelocity != null ) {
 
 						targetRay.hasAngularVelocity = true;
 						targetRay.angularVelocity.set( inputPose.angularVelocity.x, inputPose.angularVelocity.y, inputPose.angularVelocity.z );
@@ -239,7 +239,7 @@ Object.assign( WebXRController.prototype, {
 						grip.matrix.fromArray( gripPose.transform.matrix );
 						grip.matrix.decompose( grip.position, grip.rotation, grip.scale );
 
-						if ( gripPose.linearVelocity !== null ) {
+						if ( gripPose.linearVelocity != null ) {
 
 							grip.hasLinearVelocity = true;
 							grip.linearVelocity.set( gripPose.linearVelocity.x, gripPose.linearVelocity.y, gripPose.linearVelocity.z );
@@ -250,7 +250,7 @@ Object.assign( WebXRController.prototype, {
 
 						}
 
-						if ( gripPose.angularVelocity !== null ) {
+						if ( gripPose.angularVelocity != null ) {
 
 							grip.hasAngularVelocity = true;
 							grip.angularVelocity.set( gripPose.angularVelocity.x, gripPose.angularVelocity.y, gripPose.angularVelocity.z );
