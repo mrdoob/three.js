e671b3036a3f21dc4cd541a8a4be0c368f1e3650
linbingquan <695601626@qq.com>
2021-03-30 22:54:21 +0800

    Bone: Covert to ES6.

--- src/objects/Bone.js
+++ src/objects/Bone.js
@@ -1,20 +1,15 @@
 import { Object3D } from '../core/Object3D.js';
 
-function Bone() {
+class Bone extends Object3D {
 
-	Object3D.call( this );
+	constructor() {
 
-	this.type = 'Bone';
+		this.type = 'Bone';
+
+	}
 
 }
 
-Bone.prototype = Object.assign( Object.create( Object3D.prototype ), {
-
-	constructor: Bone,
-
-	isBone: true
-
-} );
-
+Bone.prototype.isBone = true;
 
 export { Bone };
