afccc97c23e1619879dccea9a9917ad627e2b19f
Mr.doob <info@mrdoob.com>
2021-03-31 11:39:21 +0100

    Editor: Added ifc files support (#21551)
    * WebIFC: Load wasm from same path.
    
    * IFCLoader: Use async/await to make sure the wasm is loaded at parse time.
    
    * Editor: Added ifc files support.
    
    * Editor: Added IFCLoader to sw.js

--- examples/jsm/loaders/IFCLoader.js
+++ examples/jsm/loaders/IFCLoader.js
@@ -17,7 +17,6 @@ import {
 } from '../../../../build/three.module.js';
 
 var ifcAPI = new IfcAPI();
-ifcAPI.Init();
 
 function IFCLoader( manager ) {
 
@@ -40,11 +39,11 @@ IFCLoader.prototype = Object.assign( Object.create( Loader.prototype ), {
 		loader.setWithCredentials( scope.withCredentials );
 		loader.load(
 			url,
-			function ( buffer ) {
+			async function ( buffer ) {
 
 				try {
 
-					onLoad( scope.parse( buffer ) );
+					onLoad( await scope.parse( buffer ) );
 
 				} catch ( e ) {
 
@@ -69,108 +68,110 @@ IFCLoader.prototype = Object.assign( Object.create( Loader.prototype ), {
 
 	},
 
-	parse: ( function () {
+	parse: async function ( buffer ) {
 
-		return function ( buffer ) {
+		if ( ifcAPI.wasmModule === undefined ) {
 
-			var data = new Uint8Array( buffer );
-			var modelID = ifcAPI.OpenModel( 'example.ifc', data );
-			return loadAllGeometry( modelID );
+			await ifcAPI.Init();
 
-			function loadAllGeometry( modelID ) {
+		}
 
-				var flatMeshes = getFlatMeshes( modelID );
-				var mainObject = new Object3D();
-				for ( var i = 0; i < flatMeshes.size(); i ++ ) {
+		var data = new Uint8Array( buffer );
+		var modelID = ifcAPI.OpenModel( 'example.ifc', data );
+		return loadAllGeometry( modelID );
 
-					var placedGeometries = flatMeshes.get( i ).geometries;
-					for ( var j = 0; j < placedGeometries.size(); j ++ )
-						mainObject.add( getPlacedGeometry( modelID, placedGeometries.get( j ) ) );
+		function loadAllGeometry( modelID ) {
 
-				}
+			var flatMeshes = getFlatMeshes( modelID );
+			var mainObject = new Object3D();
+			for ( var i = 0; i < flatMeshes.size(); i ++ ) {
 
-				return mainObject;
-
-			}
-
-			function getFlatMeshes( modelID ) {
-
-				var flatMeshes = ifcAPI.LoadAllGeometry( modelID );
-				return flatMeshes;
+				var placedGeometries = flatMeshes.get( i ).geometries;
+				for ( var j = 0; j < placedGeometries.size(); j ++ )
+					mainObject.add( getPlacedGeometry( modelID, placedGeometries.get( j ) ) );
 
 			}
 
-			function getPlacedGeometry( modelID, placedGeometry ) {
+			return mainObject;
 
-				var geometry = getBufferGeometry( modelID, placedGeometry );
-				var material = getMeshMaterial( placedGeometry.color );
-				var mesh = new Mesh( geometry, material );
-				mesh.matrix = getMeshMatrix( placedGeometry.flatTransformation );
-				mesh.matrixAutoUpdate = false;
-				return mesh;
+		}
 
-			}
+		function getFlatMeshes( modelID ) {
 
-			function getBufferGeometry( modelID, placedGeometry ) {
+			var flatMeshes = ifcAPI.LoadAllGeometry( modelID );
+			return flatMeshes;
 
-				var geometry = ifcAPI.GetGeometry(
-					modelID,
-					placedGeometry.geometryExpressID
-				);
-				var verts = ifcAPI.GetVertexArray(
-					geometry.GetVertexData(),
-					geometry.GetVertexDataSize()
-				);
-				var indices = ifcAPI.GetIndexArray(
-					geometry.GetIndexData(),
-					geometry.GetIndexDataSize()
-				);
-				var bufferGeometry = ifcGeometryToBuffer( verts, indices );
-				return bufferGeometry;
+		}
 
-			}
+		function getPlacedGeometry( modelID, placedGeometry ) {
 
-			function getMeshMaterial( color ) {
+			var geometry = getBufferGeometry( modelID, placedGeometry );
+			var material = getMeshMaterial( placedGeometry.color );
+			var mesh = new Mesh( geometry, material );
+			mesh.matrix = getMeshMatrix( placedGeometry.flatTransformation );
+			mesh.matrixAutoUpdate = false;
+			return mesh;
 
-				var col = new Color( color.x, color.y, color.z );
-				var material = new MeshPhongMaterial( { color: col, side: DoubleSide } );
-				material.transparent = color.w !== 1;
-				if ( material.transparent ) material.opacity = color.w;
-				return material;
+		}
 
-			}
+		function getBufferGeometry( modelID, placedGeometry ) {
 
-			function getMeshMatrix( matrix ) {
+			var geometry = ifcAPI.GetGeometry(
+				modelID,
+				placedGeometry.geometryExpressID
+			);
+			var verts = ifcAPI.GetVertexArray(
+				geometry.GetVertexData(),
+				geometry.GetVertexDataSize()
+			);
+			var indices = ifcAPI.GetIndexArray(
+				geometry.GetIndexData(),
+				geometry.GetIndexDataSize()
+			);
+			var bufferGeometry = ifcGeometryToBuffer( verts, indices );
+			return bufferGeometry;
 
-				var mat = new Matrix4();
-				mat.fromArray( matrix );
-				// mat.elements[15 - 3] *= 0.001;
-				// mat.elements[15 - 2] *= 0.001;
-				// mat.elements[15 - 1] *= 0.001;
-				return mat;
+		}
 
-			}
+		function getMeshMaterial( color ) {
 
-			function ifcGeometryToBuffer( vertexData, indexData ) {
+			var col = new Color( color.x, color.y, color.z );
+			var material = new MeshPhongMaterial( { color: col, side: DoubleSide } );
+			material.transparent = color.w !== 1;
+			if ( material.transparent ) material.opacity = color.w;
+			return material;
 
-				var geometry = new BufferGeometry();
-				var buffer32 = new InterleavedBuffer( vertexData, 6 );
-				geometry.setAttribute(
-					'position',
-					new InterleavedBufferAttribute( buffer32, 3, 0 )
-				);
-				geometry.setAttribute(
-					'normal',
-					new InterleavedBufferAttribute( buffer32, 3, 3 )
-				);
-				geometry.setIndex( new BufferAttribute( indexData, 1 ) );
-				return geometry;
+		}
 
-			}
+		function getMeshMatrix( matrix ) {
 
-		};
+			var mat = new Matrix4();
+			mat.fromArray( matrix );
+			// mat.elements[15 - 3] *= 0.001;
+			// mat.elements[15 - 2] *= 0.001;
+			// mat.elements[15 - 1] *= 0.001;
+			return mat;
 
-	} )(),
+		}
+
+		function ifcGeometryToBuffer( vertexData, indexData ) {
+
+			var geometry = new BufferGeometry();
+			var buffer32 = new InterleavedBuffer( vertexData, 6 );
+			geometry.setAttribute(
+				'position',
+				new InterleavedBufferAttribute( buffer32, 3, 0 )
+			);
+			geometry.setAttribute(
+				'normal',
+				new InterleavedBufferAttribute( buffer32, 3, 3 )
+			);
+			geometry.setIndex( new BufferAttribute( indexData, 1 ) );
+			return geometry;
+
+		}
+
+	}
 } );
 
 export { IFCLoader };
