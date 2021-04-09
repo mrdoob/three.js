97287b8536521e84640466a47096262e30d01535
tentone <tentone@outlook.com>
2021-04-09 17:21:59 +0100

    Clean up

--- examples/js/loaders/GCodeLoader.js
+++ examples/js/loaders/GCodeLoader.js
@@ -53,7 +53,7 @@
 
 		parse( data ) {
 
-			var state = {
+			let state = {
 				x: 0,
 				y: 0,
 				z: 0,
@@ -62,13 +62,13 @@
 				extruding: false,
 				relative: false
 			};
-			var layers = [];
-			var currentLayer = undefined;
-			var pathMaterial = new THREE.LineBasicMaterial( {
+			let layers = [];
+			let currentLayer = undefined;
+			let pathMaterial = new THREE.LineBasicMaterial( {
 				color: 0xFF0000
 			} );
 			pathMaterial.name = 'path';
-			var extrudingMaterial = new THREE.LineBasicMaterial( {
+			let extrudingMaterial = new THREE.LineBasicMaterial( {
 				color: 0x00FF00
 			} );
 			extrudingMaterial.name = 'extruded';
@@ -119,20 +119,20 @@
 
 			}
 
-			var lines = data.replace( /;.+/g, '' ).split( '\n' );
+			let lines = data.replace( /;.+/g, '' ).split( '\n' );
 
-			for ( var i = 0; i < lines.length; i ++ ) {
+			for ( let i = 0; i < lines.length; i ++ ) {
 
-				var tokens = lines[ i ].split( ' ' );
-				var cmd = tokens[ 0 ].toUpperCase(); //Argumments
+				let tokens = lines[ i ].split( ' ' );
+				let cmd = tokens[ 0 ].toUpperCase(); //Argumments
 
-				var args = {};
+				let args = {};
 				tokens.splice( 1 ).forEach( function ( token ) {
 
 					if ( token[ 0 ] !== undefined ) {
 
-						var key = token[ 0 ].toLowerCase();
-						var value = parseFloat( token.substring( 1 ) );
+						let key = token[ 0 ].toLowerCase();
+						let value = parseFloat( token.substring( 1 ) );
 						args[ key ] = value;
 
 					}
@@ -142,7 +142,7 @@
 
 				if ( cmd === 'G0' || cmd === 'G1' ) {
 
-					var line = {
+					let line = {
 						x: args.x !== undefined ? absolute( state.x, args.x ) : state.x,
 						y: args.y !== undefined ? absolute( state.y, args.y ) : state.y,
 						z: args.z !== undefined ? absolute( state.z, args.z ) : state.z,
@@ -180,7 +180,7 @@
 				} else if ( cmd === 'G92' ) {
 
 					//G92: Set Position
-					var line = state;
+					let line = state;
 					line.x = args.x !== undefined ? args.x : line.x;
 					line.y = args.y !== undefined ? args.y : line.y;
 					line.z = args.z !== undefined ? args.z : line.z;
@@ -192,47 +192,47 @@
 
 			}
 
-			function addObject( vertex, extruding ) {
+			function addObject( vertex, extruding, i ) {
 
-				var geometry = new THREE.BufferGeometry();
+				let geometry = new THREE.BufferGeometry();
 				geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertex, 3 ) );
-				var segments = new THREE.LineSegments( geometry, extruding ? extrudingMaterial : pathMaterial );
+				let segments = new THREE.LineSegments( geometry, extruding ? extrudingMaterial : pathMaterial );
 				segments.name = 'layer' + i;
 				object.add( segments );
 
 			}
 
-			var object = new THREE.Group();
+			let object = new THREE.Group();
 			object.name = 'gcode';
 
 			if ( this.splitLayer ) {
 
-				for ( var i = 0; i < layers.length; i ++ ) {
+				for ( let i = 0; i < layers.length; i ++ ) {
 
-					var layer = layers[ i ];
-					addObject( layer.vertex, true );
-					addObject( layer.pathVertex, false );
+					let layer = layers[ i ];
+					addObject( layer.vertex, true, i );
+					addObject( layer.pathVertex, false, i );
 
 				}
 
 			} else {
 
-				var vertex = [],
+				let vertex = [],
 					pathVertex = [];
 
-				for ( var i = 0; i < layers.length; i ++ ) {
+				for ( let i = 0; i < layers.length; i ++ ) {
 
-					var layer = layers[ i ];
-					var layerVertex = layer.vertex;
-					var layerPathVertex = layer.pathVertex;
+					let layer = layers[ i ];
+					let layerVertex = layer.vertex;
+					let layerPathVertex = layer.pathVertex;
 
-					for ( var j = 0; j < layerVertex.length; j ++ ) {
+					for ( let j = 0; j < layerVertex.length; j ++ ) {
 
 						vertex.push( layerVertex[ j ] );
 
 					}
 
-					for ( var j = 0; j < layerPathVertex.length; j ++ ) {
+					for ( let j = 0; j < layerPathVertex.length; j ++ ) {
 
 						pathVertex.push( layerPathVertex[ j ] );
 
@@ -240,8 +240,8 @@
 
 				}
 
-				addObject( vertex, true );
-				addObject( pathVertex, false );
+				addObject( vertex, true, i );
+				addObject( pathVertex, false, i );
 
 			}
 
