76744cbb95b21fa4c388a97e50d7538e815d7c68
Michael Herzog <michael.herzog@human-interactive.org>
2021-04-09 20:14:41 +0200

    Materials: Convert to classes. (#21626)

--- examples/jsm/nodes/materials/MeshStandardNodeMaterial.js
+++ examples/jsm/nodes/materials/MeshStandardNodeMaterial.js
@@ -2,18 +2,19 @@ import { MeshStandardNode } from './nodes/MeshStandardNode.js';
 import { NodeMaterial } from './NodeMaterial.js';
 import { NodeUtils } from '../core/NodeUtils.js';
 
-function MeshStandardNodeMaterial() {
+class MeshStandardNodeMaterial extends NodeMaterial {
 
-	var node = new MeshStandardNode();
+	constructor() {
 
-	NodeMaterial.call( this, node, node );
+		const node = new MeshStandardNode();
 
-	this.type = 'MeshStandardNodeMaterial';
+		super( node, node );
+
+		this.type = 'MeshStandardNodeMaterial';
+
+	}
 
 }
-
-MeshStandardNodeMaterial.prototype = Object.create( NodeMaterial.prototype );
-MeshStandardNodeMaterial.prototype.constructor = MeshStandardNodeMaterial;
 
 NodeUtils.addShortcuts( MeshStandardNodeMaterial.prototype, 'properties', [
 	'color',
