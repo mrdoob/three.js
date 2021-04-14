1a4357af3b740d0645e22936135a17fdfafd3913
Marco Fugaro <marco.fugaro@gmail.com>
2021-04-14 11:20:34 +0200

    Use terser in test-treeshake command

--- test/rollup.treeshake.config.js
+++ test/rollup.treeshake.config.js
@@ -1,17 +1,33 @@
 import resolve from '@rollup/plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
+import { terser } from 'rollup-plugin-terser';
 
 export default [
 	{
 		input: 'test/treeshake/index.js',
 		plugins: [
 			resolve(),
-			filesize(),
 		],
 		output: [
 			{
 				format: 'esm',
 				file: 'test/treeshake/index.bundle.js'
+			}
+		]
+	},
+	{
+		input: 'test/treeshake/index.js',
+		plugins: [
+			resolve(),
+			terser(),
+			filesize( {
+				showMinifiedSize: false,
+			} ),
+		],
+		output: [
+			{
+				format: 'esm',
+				file: 'test/treeshake/index.bundle.min.js'
 			}
 		]
 	}
