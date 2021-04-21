a4bfa40070c6c3ba4f68979809db749cf13f8fe7
puxiao <yangpuxiao@gmail.com>
2021-04-21 12:37:16 +0800

    Color: added support for add color values using uppercase

--- src/math/Color.js
+++ src/math/Color.js
@@ -264,7 +264,7 @@ class Color {
 	setColorName( style ) {
 
 		// color keywords
-		const hex = _colorKeywords[ style ];
+		const hex = _colorKeywords[ style.toLowerCase() ];
 
 		if ( hex !== undefined ) {
 
