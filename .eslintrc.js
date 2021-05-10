550af513b6f31b9f6bb8dfa6af5745b962d44598
gonnavis <gonnavis@163.com>
2021-05-10 12:07:41 +0800

    EslintFormatter

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// Can use this ESLint config as formatter with VSCode.
+// See https://github.com/mrdoob/three.js/pull/21802
+
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	// "extends": "eslint:recommended",
+	"extends": "mdcs",
+	"parserOptions": {
+		"ecmaVersion": 12,
+		"sourceType": "module"
+	},
+	"rules": {
+	}
+};
