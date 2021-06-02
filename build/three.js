72d47c80e00de74ea508ccd4c457d9619dcaa68b
Mugen87 <michael.herzog@human-interactive.org>
2021-06-02 09:33:34 +0200

    Update builds.

--- build/three.js
+++ build/three.js
@@ -9,7 +9,7 @@
 	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.THREE = {}));
 }(this, (function (exports) { 'use strict';
 
-	const REVISION = '129';
+	const REVISION = '130dev';
 	const MOUSE = {
 		LEFT: 0,
 		MIDDLE: 1,
