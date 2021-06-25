508154ae1b84d007bb944d830602f7c1a456410b
NNskelly <sean@nextnowagency.com>
2021-06-25 09:12:53 -0500

    edit to jsm source as requested

--- examples/jsm/modifiers/SimplifyModifier.js
+++ examples/jsm/modifiers/SimplifyModifier.js
@@ -141,6 +141,8 @@ class SimplifyModifier {
 
 			const vertex = vertices[ i ].position;
 			position.push( vertex.x, vertex.y, vertex.z );
+			// cache final index to GREATLY speed up faces reconstruction
+			vertices[ i ].index = i;
 
 		}
 
@@ -149,12 +151,7 @@ class SimplifyModifier {
 		for ( let i = 0; i < faces.length; i ++ ) {
 
 			const face = faces[ i ];
-
-			const a = vertices.indexOf( face.v1 );
-			const b = vertices.indexOf( face.v2 );
-			const c = vertices.indexOf( face.v3 );
-
-			index.push( a, b, c );
+			index.push( face.v1.index, face.v2.index, face.v3.index );
 
 		}
 
