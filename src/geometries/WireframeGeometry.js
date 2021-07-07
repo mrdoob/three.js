4e1c12b25e9819dcaa4a5c0a128876e7d5e213b8
Michael Herzog <michael.herzog@human-interactive.org>
2021-07-07 14:05:17 +0200

    Update WireframeGeometry.js

--- src/geometries/WireframeGeometry.js
+++ src/geometries/WireframeGeometry.js
@@ -115,7 +115,7 @@ class WireframeGeometry extends BufferGeometry {
 function isUniqueEdge( start, end, edges ) {
 
 	const hash1 = `${start.x},${start.y},${start.z}-${end.x},${end.y},${end.z}`;
-	const hash2 = `${end.x},${end.y},${end.z}-${start.x},${start.y},${start.z}`; // opposing edge
+	const hash2 = `${end.x},${end.y},${end.z}-${start.x},${start.y},${start.z}`; // coincident edge
 
 	if ( edges.has( hash1 ) === true || edges.has( hash2 ) === true ) {
 
