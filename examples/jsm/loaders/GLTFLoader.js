aa599390a548fc9b5d1e0f5ad7772882d05e9e9a
Tony Outis <3321213+tony-0tis@users.noreply.github.com>
2021-07-10 16:38:26 +0300

    Update GLTFLoader.js

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -3186,7 +3186,7 @@ class GLTFParser {
 
 				}
 
-				mesh.name = parser.createUniqueName( meshDef.name || (material && material.name) || ( 'mesh_' + meshIndex ) );
+				mesh.name = parser.createUniqueName( meshDef.name || ( 'mesh_' + meshIndex ) );
 
 				assignExtrasToUserData( mesh, meshDef );
 
