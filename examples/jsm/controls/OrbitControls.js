5da924d89c940aea9d9e2cacdea734c4cb1298b8
Marco Fugaro <marco.fugaro@gmail.com>
2021-07-22 17:13:04 +0200

    Simplify getDistance()

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -116,14 +116,7 @@ class OrbitControls extends EventDispatcher {
 
 		this.getDistance = function () {
 
-			// compute the distance manually if the camera is orthographic
-			if ( this.object.isOrthographicCamera ) {
-
-				return this.object.position.distanceTo( this.target );
-
-			}
-
-			return spherical.radius;
+			return this.object.position.distanceTo( this.target );
 
 		};
 
