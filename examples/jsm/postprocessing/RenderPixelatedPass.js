44e2e3d445de10ae86a03db3427908f93c4702b6
KJK <kody.j.king@gmail.com>
2021-08-18 13:00:51 -0700

    Replaced pixel example and reduced texture reads

--- examples/jsm/postprocessing/RenderPixelatedPass.js
+++ examples/jsm/postprocessing/RenderPixelatedPass.js
@@ -26,8 +26,8 @@ class RenderPixelatedPass extends Pass {
 		this.scene = scene;
 		this.camera = camera;
 
-		this.normalEdgeStrenth = options.normalEdgeStrenth ?? .3;
-		this.depthEdgeStrenth = options.depthEdgeStrenth ?? .4;
+		this.normalEdgeStrength = options.normalEdgeStrength ?? .3;
+		this.depthEdgeStrength = options.depthEdgeStrength ?? .4;
 
 		this.rgbRenderTarget = pixelRenderTarget( this.renderResolution, RGBAFormat, true );
 		this.normalRenderTarget = pixelRenderTarget( this.renderResolution, RGBFormat, false );
@@ -65,8 +65,8 @@ class RenderPixelatedPass extends Pass {
 	render( renderer, writeBuffer ) {
 
 		const uniforms = this.fsQuad.material.uniforms;
-		uniforms.normalEdgeStrenth.value = this.normalEdgeStrenth;
-		uniforms.depthEdgeStrenth.value = this.depthEdgeStrenth;
+		uniforms.normalEdgeStrength.value = this.normalEdgeStrength;
+		uniforms.depthEdgeStrength.value = this.depthEdgeStrength;
 
 		renderer.setRenderTarget( this.rgbRenderTarget );
 		renderer.render( this.scene, this.camera );
@@ -113,15 +113,18 @@ class RenderPixelatedPass extends Pass {
 						1 / this.renderResolution.y,
 					)
 				},
-				normalEdgeStrenth: { value: 0 },
-				depthEdgeStrenth: { value: 0 }
+				normalEdgeStrength: { value: 0 },
+				depthEdgeStrength: { value: 0 }
 			},
 			vertexShader:
 				`
 				varying vec2 vUv;
+
 				void main() {
+
 					vUv = uv;
 					gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
 				}
 				`,
 			fragmentShader:
@@ -130,46 +133,51 @@ class RenderPixelatedPass extends Pass {
 				uniform sampler2D tDepth;
 				uniform sampler2D tNormal;
 				uniform vec4 resolution;
-				uniform float normalEdgeStrenth;
-				uniform float depthEdgeStrenth;
+				uniform float normalEdgeStrength;
+				uniform float depthEdgeStrength;
 				varying vec2 vUv;
 
 				float getDepth(int x, int y) {
+
 					return texture2D( tDepth, vUv + vec2(x, y) * resolution.zw ).r;
+
 				}
 
 				vec3 getNormal(int x, int y) {
+
 					return texture2D( tNormal, vUv + vec2(x, y) * resolution.zw ).rgb * 2.0 - 1.0;
+
 				}
 
-				float depthEdgeIndicator() {
-					float depth = getDepth(0, 0);
-					vec3 normal = getNormal(0, 0);
+				float depthEdgeIndicator(float depth, vec3 normal) {
+
 					float diff = 0.0;
 					diff += clamp(getDepth(1, 0) - depth, 0.0, 1.0);
 					diff += clamp(getDepth(-1, 0) - depth, 0.0, 1.0);
 					diff += clamp(getDepth(0, 1) - depth, 0.0, 1.0);
 					diff += clamp(getDepth(0, -1) - depth, 0.0, 1.0);
 					return floor(smoothstep(0.01, 0.02, diff) * 2.) / 2.;
+
 				}
 
 				float neighborNormalEdgeIndicator(int x, int y, float depth, vec3 normal) {
+
 					float depthDiff = getDepth(x, y) - depth;
+					vec3 neighborNormal = getNormal(x, y);
 					
 					// Edge pixels should yield to faces who's normals are closer to the bias normal.
 					vec3 normalEdgeBias = vec3(1., 1., 1.); // This should probably be a parameter.
-					float normalDiff = dot(normal - getNormal(x, y), normalEdgeBias);
+					float normalDiff = dot(normal - neighborNormal, normalEdgeBias);
 					float normalIndicator = clamp(smoothstep(-.01, .01, normalDiff), 0.0, 1.0);
 					
 					// Only the shallower pixel should detect the normal edge.
 					float depthIndicator = clamp(sign(depthDiff * .25 + .0025), 0.0, 1.0);
 
-					return distance(normal, getNormal(x, y)) * depthIndicator * normalIndicator;
+					return (1.0 - dot(normal, neighborNormal)) * depthIndicator * normalIndicator;
+
 				}
 
-				float normalEdgeIndicator() {
-					float depth = getDepth(0, 0);
-					vec3 normal = getNormal(0, 0);
+				float normalEdgeIndicator(float depth, vec3 normal) {
 					
 					float indicator = 0.0;
 
@@ -179,17 +187,35 @@ class RenderPixelatedPass extends Pass {
 					indicator += neighborNormalEdgeIndicator(1, 0, depth, normal);
 
 					return step(0.1, indicator);
+
 				}
 
 				void main() {
+
 					vec4 texel = texture2D( tDiffuse, vUv );
 
-					float dei = depthEdgeIndicator();
-					float nei = normalEdgeIndicator();
+					float depth = 0.0;
+					vec3 normal = vec3(0.0);
 
-					float Strenth = dei > 0.0 ? (1.0 - depthEdgeStrenth * dei) : (1.0 + normalEdgeStrenth * nei);
+					if (depthEdgeStrength > 0.0 || normalEdgeStrength > 0.0) {
 
-					gl_FragColor = texel * Strenth;
+						depth = getDepth(0, 0);
+						normal = getNormal(0, 0);
+
+					}
+
+					float dei = 0.0;
+					if (depthEdgeStrength > 0.0) 
+						dei = depthEdgeIndicator(depth, normal);
+
+					float nei = 0.0; 
+					if (normalEdgeStrength > 0.0) 
+						nei = normalEdgeIndicator(depth, normal);
+
+					float Strength = dei > 0.0 ? (1.0 - depthEdgeStrength * dei) : (1.0 + normalEdgeStrength * nei);
+
+					gl_FragColor = texel * Strength;
+
 				}
 				`
 		} );
