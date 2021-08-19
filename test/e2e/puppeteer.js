4caf8ab0edd72819d2c2e0e60a40fc4ebcaab283
Mr.doob <info@mrdoob.com>
2021-08-19 16:23:36 +0200

    Removed webgl_materials_envmas_parallax example. (#22369)

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -30,7 +30,6 @@ const exceptionList = [
 	'webgl_loader_imagebitmap', // takes too long to load?
 	'webgl_loader_texture_lottie', // not sure why this fails
 	'webgl_loader_texture_pvrtc', // not supported in CI, useless
-	'webgl_materials_envmaps_parallax', // empty for some reason
 	'webgl_materials_standard_nodes', // puppeteer does not support import maps yet
 	'webgl_raymarching_reflect', // exception for Github Actions
 	'webgl_test_memory2', // gives fatal error in puppeteer
