3a2f592fd5c29cf0e0a09d1be71faaced32396ae
Mr.doob <info@mrdoob.com>
2021-08-23 11:24:39 +0200

    RGBELoader: Clean up.

--- examples/jsm/loaders/RGBELoader.js
+++ examples/jsm/loaders/RGBELoader.js
@@ -386,8 +386,8 @@ class RGBELoader extends DataTextureLoader {
 
 					case FloatType:
 
-						numElements = ( image_rgba_data.length / 4 ) * 3;
-						const floatArray = new Float32Array( numElements );
+						numElements = image_rgba_data.length / 4;
+						const floatArray = new Float32Array( numElements * 3 );
 
 						for ( let j = 0; j < numElements; j ++ ) {
 
@@ -402,8 +402,8 @@ class RGBELoader extends DataTextureLoader {
 
 					case HalfFloatType:
 
-						numElements = ( image_rgba_data.length / 4 ) * 3;
-						const halfArray = new Uint16Array( numElements );
+						numElements = image_rgba_data.length / 4;
+						const halfArray = new Uint16Array( numElements * 3 );
 
 						for ( let j = 0; j < numElements; j ++ ) {
 
