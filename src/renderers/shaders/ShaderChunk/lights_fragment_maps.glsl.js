f65415c8f7f1a8ecb6fd573232a575195fd7f853
WestLangley <WestLangley@users.noreply.github.com>
2021-08-23 15:42:09 -0400

    honor the physicallyCorrectLights flag when using a light map (#22397)

--- src/renderers/shaders/ShaderChunk/lights_fragment_maps.glsl.js
+++ src/renderers/shaders/ShaderChunk/lights_fragment_maps.glsl.js
@@ -3,12 +3,12 @@ export default /* glsl */`
 
 	#ifdef USE_LIGHTMAP
 
-		vec4 lightMapTexel= texture2D( lightMap, vUv2 );
+		vec4 lightMapTexel = texture2D( lightMap, vUv2 );
 		vec3 lightMapIrradiance = lightMapTexelToLinear( lightMapTexel ).rgb * lightMapIntensity;
 
 		#ifndef PHYSICALLY_CORRECT_LIGHTS
 
-			lightMapIrradiance *= PI; // factor of PI should not be present; included here to prevent breakage
+			lightMapIrradiance *= PI;
 
 		#endif
 
