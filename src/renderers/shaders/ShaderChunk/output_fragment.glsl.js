161ead8a622f3877b6b710c66bb14eef91e1aa5a
Mr.doob <info@mrdoob.com>
2021-08-25 23:50:59 +0200

    ShaderChunk: Increase transmissionAlpha a bit.

--- src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
@@ -3,8 +3,9 @@ export default /* glsl */`
 diffuseColor.a = 1.0;
 #endif
 
+// https://github.com/mrdoob/three.js/pull/22425
 #ifdef USE_TRANSMISSION
-diffuseColor.a *= transmissionAlpha;
+diffuseColor.a *= transmissionAlpha + 0.1;
 #endif
 
 gl_FragColor = vec4( outgoingLight, diffuseColor.a );
