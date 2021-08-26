ee46b2899ae201a82ad1f0eca3e4f7ba3e3bad39
Mr.doob <info@mrdoob.com>
2021-08-26 09:36:28 +0200

    ShaderChunk: Added transmission alpha support (#22425)
    * ShaderChunk: Added transmission alpha support.
    
    * WebGLRenderer: Add support for GLTF opaque alpha_mode.
    
    * Replaced material.blending = GLTFOpaqueBlending with material.format = RGBFormat.
    
    * Clean up.
    
    * ShaderChunk: Increase transmissionAlpha a bit.

--- src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
@@ -1,11 +1,12 @@
 export default /* glsl */`
 #ifdef OPAQUE
+diffuseColor.a = 1.0;
+#endif
 
-gl_FragColor = vec4( outgoingLight, 1.0 );
-
-#else
+// https://github.com/mrdoob/three.js/pull/22425
+#ifdef USE_TRANSMISSION
+diffuseColor.a *= transmissionAlpha + 0.1;
+#endif
 
 gl_FragColor = vec4( outgoingLight, diffuseColor.a );
-
-#endif
 `;
