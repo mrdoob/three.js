a6348723f157fcc1b017651308bce2f1ebe52dbd
Mugen87 <michael.herzog@human-interactive.org>
2021-09-01 11:48:51 +0200

    ShadowMaterial: Add missing shader chunks.

--- src/renderers/shaders/ShaderLib/shadow_vert.glsl.js
+++ src/renderers/shaders/ShaderLib/shadow_vert.glsl.js
@@ -1,13 +1,11 @@
 export default /* glsl */`
 #include <common>
 #include <fog_pars_vertex>
+#include <morphtarget_pars_vertex>
+#include <skinning_pars_vertex>
 #include <shadowmap_pars_vertex>
 
 void main() {
-
-	#include <begin_vertex>
-	#include <project_vertex>
-	#include <worldpos_vertex>
 
 	#include <beginnormal_vertex>
 	#include <morphnormal_vertex>
@@ -15,6 +13,12 @@ void main() {
 	#include <skinnormal_vertex>
 	#include <defaultnormal_vertex>
 
+	#include <begin_vertex>
+	#include <morphtarget_vertex>
+	#include <skinning_vertex>
+	#include <project_vertex>
+
+	#include <worldpos_vertex>
 	#include <shadowmap_vertex>
 	#include <fog_vertex>
 
