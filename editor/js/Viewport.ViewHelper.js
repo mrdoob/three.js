67c3a078011b2e8ec1720a301139d3c6c2497d49
Mugen87 <michael.herzog@human-interactive.org>
2021-09-01 10:19:40 +0200

    Editor: Use pointer events for ViewHelper.

--- editor/js/Viewport.ViewHelper.js
+++ editor/js/Viewport.ViewHelper.js
@@ -21,7 +21,7 @@ class ViewHelper extends THREE.Object3D {
 
 		const scope = this;
 
-		panel.dom.addEventListener( 'mouseup', function ( event ) {
+		panel.dom.addEventListener( 'pointerup', function ( event ) {
 
 			event.stopPropagation();
 
@@ -29,7 +29,7 @@ class ViewHelper extends THREE.Object3D {
 
 		} );
 
-		panel.dom.addEventListener( 'mousedown', function ( event ) {
+		panel.dom.addEventListener( 'pointerdown', function ( event ) {
 
 			event.stopPropagation();
 
