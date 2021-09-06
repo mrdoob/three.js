54d3b548e84a8691b3968d33942705b161309bda
sunag <jcdeconto@igalia.com>
2021-09-06 02:24:39 -0300

    reuse type variable

--- examples/jsm/renderers/nodes/core/InputNode.js
+++ examples/jsm/renderers/nodes/core/InputNode.js
@@ -40,7 +40,7 @@ class InputNode extends Node {
 
 		} else {
 
-			const nodeUniform = builder.getUniformFromNode( this, builder.shaderStage, this.getType( builder ) );
+			const nodeUniform = builder.getUniformFromNode( this, builder.shaderStage, type );
 			const propertyName = builder.getPropertyName( nodeUniform );
 
 			return builder.format( propertyName, type, output );
