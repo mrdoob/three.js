ca2f0ee1d4ad119fc2609587cdb0b33d87ef3f51
Felix Mariotto <felixmariotto@gmail.com>
2021-10-08 18:06:44 +0200

    add mimaps

--- examples/jsm/webxr/XRLayerHelper.js
+++ examples/jsm/webxr/XRLayerHelper.js
@@ -3,9 +3,9 @@ import {
 	PlaneGeometry,
 	MeshBasicMaterial,
 	Matrix4,
-	// testing
-	BoxGeometry,
-	MeshNormalMaterial
+	//
+
+	Color
 } from '../../../build/three.module.js';
 
 const XRLAYER_MODE = 0;
@@ -20,6 +20,8 @@ class XRLayerHelper extends Mesh {
 		this.isXRLayerHelper = true
 		this.renderer = renderer;
 		this.params = params;
+		// will be used to compare the last set XRLayer transforms with
+		// the current mesh transforms.
 		this.xrLayerMatrix = new Matrix4();
 
 		this.init();
@@ -42,27 +44,40 @@ class XRLayerHelper extends Mesh {
 
 		} );
 
+		//
+
+		this.params.cropX = this.params.widthPx / this.params.textureSizePx;
+		this.params.cropY = this.params.heightPx / this.params.textureSizePx;
+
+		this.params.texture.repeat.x = this.params.cropX;
+		this.params.texture.repeat.y = this.params.cropY;
+
+		//
+
 		this.updateMesh();
 
 	}
 
 	updateMesh() {
 
+		// the mesh is used both as depth mask when the XRLayer is visible,
+		// and as fallback when the XRLayer cannot be displayed.
+
 		this.geometry = new PlaneGeometry(
 			this.params.layerWidth,
 			this.params.layerHeight
 		);
 
-		this.material = new MeshBasicMaterial( { opacity: 0 } );
+		this.material = new MeshBasicMaterial( {
+			map: this.params.texture,
+			opacity: 0
+		} );
 
 	}
 
 	makeXRLayer() {
 
 		this.session.requestReferenceSpace( 'local-floor' ).then( ( refSpace ) => {
-
-			this.params.cropX = this.params.widthPx / this.params.textureSizePx;
-			this.params.cropY = this.params.heightPx / this.params.textureSizePx;
 
 			const quadLayerConfig = {
 				width: .5 * this.params.layerWidth / this.params.cropX,
@@ -71,14 +86,16 @@ class XRLayerHelper extends Mesh {
 				viewPixelHeight: this.params.textureSizePx,
 				isStatic: true,
 				space: refSpace,
-				layout: "mono"
+				layout: this.params.layout
 			};
 
-			// quadLayerConfig.mipLevels = this.params.mipLevels;
+			if ( this.params.mipLevels ) {
+
+				quadLayerConfig.mipLevels = this.params.mipLevels;
+
+			}
 
 			this.XRLayer = this.glBinding.createQuadLayer( quadLayerConfig );
-
-			this.updateTransform();
 
 			this.session.updateRenderState( {
 				layers: [
@@ -95,6 +112,9 @@ class XRLayerHelper extends Mesh {
 	updateTransform() {
 
 		if ( this.XRLayer ) {
+
+			// we compare the current transforms of the mesh with its transforms
+			// the last time we updated the XRLayer transforms.
 
 			this.updateMatrixWorld();
 
@@ -134,18 +154,9 @@ class XRLayerHelper extends Mesh {
 
 			this.XRLayer = null;
 
-			// apply the texture on the mesh, to emulate the behavior
-			// of an XRLayer ( with lower resolution and performance )
-
-			// this.mesh.material = this.material;
-
 		} else {
 
 			if ( !this.XRLayer ) {
-
-				// if the current mode is XRLAYER_MODE and this.XRLayer
-				// is falsy, it means we need to create an XRLayer and
-				// reference it in this.XRLayer.
 
 				this.makeXRLayer();
 
@@ -170,7 +181,8 @@ class XRLayerHelper extends Mesh {
 		) {
 
 			// Copy images to layers as required.
-			// needsRedraw is set on creation or if the underlying GL resources of a layer are lost.
+			// needsRedraw is set on creation or if the underlying
+			// GL resources of a layer are lost.
 
 			const gl = this.renderer.getContext();
 
@@ -183,14 +195,20 @@ class XRLayerHelper extends Mesh {
 			gl.texSubImage2D(
 				gl.TEXTURE_2D,
 				0,
-				( this.params.textureSizePx - this.params.widthPx ) / 2,
-				( this.params.textureSizePx - this.params.heightPx ) / 2,
+				( this.params.textureSizePx - this.params.widthPx ) * 0.5,
+				( this.params.textureSizePx - this.params.heightPx ) * 0.5,
 				this.params.widthPx,
 				this.params.heightPx,
 				gl.RGBA,
 				gl.UNSIGNED_BYTE,
 				this.params.texture.image
 			);
+
+			if ( this.params.mipLevels ) {
+
+				gl.generateMipmap( gl.TEXTURE_2D );
+
+			}
 
 		}
 
