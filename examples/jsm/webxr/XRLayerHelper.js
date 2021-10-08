5186239106092abb30eefe18aac7cf363614be17
Felix Mariotto <felixmariotto@gmail.com>
2021-10-08 14:42:40 +0200

    create XRLayerHelper module + example page

---
+++ examples/jsm/webxr/XRLayerHelper.js
@@ -0,0 +1,15 @@
+import {
+	Object3D
+} from '../../../build/three.module.js';
+
+class XRLayerHelper extends Object3D {
+
+	constructor() {
+
+		super();
+
+		this.isXRLayerHelper = true
+
+	}
+
+}
