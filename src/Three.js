4c72beff90eab492e21761e26d666c3275480025
gero3 <jeroen-876@hotmail.com>
2021-12-02 17:30:10 +0100

    Cleanup eslint globals + update deprecated config

--- src/Three.js
+++ src/Three.js
@@ -155,11 +155,9 @@ export * from './Three.Legacy.js';
 
 if ( typeof __THREE_DEVTOOLS__ !== 'undefined' ) {
 
-	/* eslint-disable no-undef */
 	__THREE_DEVTOOLS__.dispatchEvent( new CustomEvent( 'register', { detail: {
 		revision: REVISION,
 	} } ) );
-	/* eslint-enable no-undef */
 
 }
 
