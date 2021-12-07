86bbc1dd0a30961a61da4e70d1ee50bff225abca
Paul Nelson <5923544+sdphm80@users.noreply.github.com>
2021-12-07 12:49:02 -0600

    Try to fix linting issues

--- src/core/EventDispatcher.js
+++ src/core/EventDispatcher.js
@@ -4,54 +4,55 @@
 
 class EventDispatcher {
 
-	addEventListener( type, listener ) {
+	addEventListener( type, listener ){
 
 		const listeners = this.getEventListeners( type );
-		
+
 		listeners.add( listener );
 		
 	}
 
-	hasEventListener( type, listener ) {
+	hasEventListener( type, listener ){
 
 		return this.getEventListeners( type ).has( listener );
+
 	}
 
-	removeEventListener( type, listener ) {
+	removeEventListener( type, listener ){
 
 		this.getEventListeners( type ).remove( listener );
 
 	}
 
-	dispatchEvent( event ) {
+	dispatchEvent( event ){
 
 			event.target = this;
-		
+
 			const invokeListener = listener => listener.call( this, event ); 
-		
+
 			this.getEventListeners( event.type ).forEach( invokeListener );
 
 			event.target = null;
 
-		}
+	}
 	
 	
 	getEventListeners( type ){
-		
+
 		if ( undefined === this._listeners ) {
-			
+
 			this._listeners = new Map();
-			
+
 		}
 		
 		if (  ! this._listeners.has( type ) )  {
-		
+
 			this._listeners.set( type, new Set( ) );
-			
+
 		}
-		
+
 		return this._listeners.get( type );
-		
+
 	}
 
 }
