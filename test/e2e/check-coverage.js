3e1bca0e2575085f484690329417ca1a94ff9d77
gero3 <jeroen-876@hotmail.com>
2021-12-18 13:30:00 +0100

    add placeholder to coverage

--- test/e2e/check-coverage.js
+++ test/e2e/check-coverage.js
@@ -4,7 +4,7 @@ const fs = require( 'fs' );
 const E = fs.readdirSync( './examples' )
 	.filter( s => s.slice( - 5 ) === '.html' )
 	.map( s => s.slice( 0, s.length - 5 ) )
-	.filter( f => f !== 'index' );
+	.filter( f => [ 'placeholder', 'index' ].indexOf( f ) === - 1 );
 
 // screenshots
 const S = fs.readdirSync( './examples/screenshots' )
