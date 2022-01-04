ea80c212e0047a547092904fcd19414dddd0da6c
Mr.doob <info@mrdoob.com>
2022-01-04 18:32:26 -0500

    Examples: Clean up.

---
+++ examples/js/capabilities/ImportMaps.js
@@ -0,0 +1,26 @@
+( function () {
+class ImportMaps {
+  static isAvailable() {
+    return 'supports' in HTMLScriptElement && HTMLScriptElement.supports('importmap');
+  }
+
+  static getErrorMessage() {
+    const message = 'Your browser does not support <a href="https://wicg.github.io/import-maps/" style="color:blue">Import Maps</a>';
+    const element = document.createElement('div');
+    element.style.fontFamily = 'monospace';
+    element.style.fontSize = '13px';
+    element.style.fontWeight = 'normal';
+    element.style.textAlign = 'center';
+    element.style.background = '#fff';
+    element.style.color = '#000';
+    element.style.padding = '1.5em';
+    element.style.width = '400px';
+    element.style.margin = '5em auto 0';
+    element.innerHTML = message;
+    return element;
+  }
+
+}
+
+THREE.ImportMaps as default = ImportMaps as default;
+} )();
---
+++ examples/js/capabilities/WebGPU.js
@@ -0,0 +1,27 @@
+( function () {
+class WebGPU {
+  static isAvailable() {
+    return navigator.gpu !== undefined;
+  }
+
+  static getErrorMessage() {
+    const message = 'Your browser does not support <a href="https://gpuweb.github.io/gpuweb/" style="color:blue">WebGPU</a>';
+    const element = document.createElement('div');
+    element.id = 'webgpumessage';
+    element.style.fontFamily = 'monospace';
+    element.style.fontSize = '13px';
+    element.style.fontWeight = 'normal';
+    element.style.textAlign = 'center';
+    element.style.background = '#fff';
+    element.style.color = '#000';
+    element.style.padding = '1.5em';
+    element.style.width = '400px';
+    element.style.margin = '5em auto 0';
+    element.innerHTML = message;
+    return element;
+  }
+
+}
+
+THREE.WebGPU as default = WebGPU as default;
+} )();
