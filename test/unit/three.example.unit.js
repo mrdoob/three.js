e36e751af0248051d74d91493790b5f9f3e76e33
Michael Herzog <michael.herzog@human-interactive.org>
2022-01-27 16:10:27 +0100

    Tests: Remove unit tests for example files. (#23352)

--- test/unit/three.example.unit.js
+++
@@ -1,6 +0,0 @@
-import './utils/qunit-utils.js';
-
-import './example/utils/BufferGeometryUtils.tests.js';
-import './example/exporters/GLTFExporter.tests.js';
-import './example/loaders/GLTFLoader.tests.js';
-import './example/objects/Lensflare.tests.js';
