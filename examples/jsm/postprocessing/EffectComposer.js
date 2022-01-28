0a5e7464c3ce7f3b2d1446fe2a4a5f847e1238f8
Marco Fugaro <marco.fugaro@gmail.com>
2022-01-28 18:47:10 +0100

    Remove duplicate FullScreenQuad

--- examples/jsm/postprocessing/EffectComposer.js
+++ examples/jsm/postprocessing/EffectComposer.js
@@ -1,10 +1,6 @@
 import {
-	BufferGeometry,
 	Clock,
-	Float32BufferAttribute,
 	LinearFilter,
-	Mesh,
-	OrthographicCamera,
 	RGBAFormat,
 	Vector2,
 	WebGLRenderTarget
@@ -242,48 +238,4 @@ class EffectComposer {
 
 }
 
-// Helper for passes that need to fill the viewport with a single quad.
-
-const _camera = new OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
-
-// https://github.com/mrdoob/three.js/pull/21358
-
-const _geometry = new BufferGeometry();
-_geometry.setAttribute( 'position', new Float32BufferAttribute( [ - 1, 3, 0, - 1, - 1, 0, 3, - 1, 0 ], 3 ) );
-_geometry.setAttribute( 'uv', new Float32BufferAttribute( [ 0, 2, 0, 0, 2, 0 ], 2 ) );
-
-class FullScreenQuad {
-
-	constructor( material ) {
-
-		this._mesh = new Mesh( _geometry, material );
-
-	}
-
-	dispose() {
-
-		this._mesh.geometry.dispose();
-
-	}
-
-	render( renderer ) {
-
-		renderer.render( this._mesh, _camera );
-
-	}
-
-	get material() {
-
-		return this._mesh.material;
-
-	}
-
-	set material( value ) {
-
-		this._mesh.material = value;
-
-	}
-
-}
-
-export { EffectComposer, FullScreenQuad };
+export { EffectComposer };
