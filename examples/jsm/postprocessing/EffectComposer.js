75c2cdf883f00c3181c7ea7b402fc182aed0facc
Marco Fugaro <marco.fugaro@gmail.com>
2022-01-28 18:34:56 +0100

    Remove duplicated Pass

--- examples/jsm/postprocessing/EffectComposer.js
+++ examples/jsm/postprocessing/EffectComposer.js
@@ -242,35 +242,6 @@ class EffectComposer {
 
 }
 
-
-class Pass {
-
-	constructor() {
-
-		// if set to true, the pass is processed by the composer
-		this.enabled = true;
-
-		// if set to true, the pass indicates to swap read and write buffer after rendering
-		this.needsSwap = true;
-
-		// if set to true, the pass clears its buffer before rendering
-		this.clear = false;
-
-		// if set to true, the result of the pass is rendered to screen. This is set automatically by EffectComposer.
-		this.renderToScreen = false;
-
-	}
-
-	setSize( /* width, height */ ) {}
-
-	render( /* renderer, writeBuffer, readBuffer, deltaTime, maskActive */ ) {
-
-		console.error( 'THREE.Pass: .render() must be implemented in derived pass.' );
-
-	}
-
-}
-
 // Helper for passes that need to fill the viewport with a single quad.
 
 const _camera = new OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
@@ -315,4 +286,4 @@ class FullScreenQuad {
 
 }
 
-export { EffectComposer, Pass, FullScreenQuad };
+export { EffectComposer, FullScreenQuad };
