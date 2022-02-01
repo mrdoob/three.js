253c4421257b0584be0fd1494327ef4d1d2b5885
Marco Fugaro <marco.fugaro@gmail.com>
2022-02-01 18:41:05 +0100

    Fix: remove build folder from PR

--- build/three.cjs
+++ build/three.cjs
@@ -36750,4 +36750,3 @@ exports.ZeroSlopeEnding = ZeroSlopeEnding;
 exports.ZeroStencilOp = ZeroStencilOp;
 exports._SRGBAFormat = _SRGBAFormat;
 exports.sRGBEncoding = sRGBEncoding;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGhyZWUuY2pzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
