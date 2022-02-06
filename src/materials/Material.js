a02e4b7c42a96819aea786d8a5c8a26665b43de7
linbingquan <695601626@qq.com>
2022-02-06 22:15:56 +0800

    Material: Clean up.

--- src/materials/Material.js
+++ src/materials/Material.js
@@ -160,9 +160,9 @@ class Material extends EventDispatcher {
 
 	toJSON( meta ) {
 
-		const isRoot = ( meta === undefined || typeof meta === 'string' );
+		const isRootObject = ( meta === undefined || typeof meta === 'string' );
 
-		if ( isRoot ) {
+		if ( isRootObject ) {
 
 			meta = {
 				textures: {},
@@ -370,7 +370,7 @@ class Material extends EventDispatcher {
 
 		}
 
-		if ( isRoot ) {
+		if ( isRootObject ) {
 
 			const textures = extractFromCache( meta.textures );
 			const images = extractFromCache( meta.images );
