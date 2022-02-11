8bea2a8408e09b339592c30c73ba41795338a609
Takahiro <hogehoge@gachapin.jp>
2022-02-11 07:59:09 -0800

    GLTFExporter: Fix buildORMTexture() (#23463)
    Currently buildORMTexture() returns Texture.image
    if aoMap.image, roughnessMap.image, and metalnessMap.image
    are identical but it should return Texture.
    This PR fixes the problem by returning Texture.

--- examples/jsm/exporters/GLTFExporter.js
+++ examples/jsm/exporters/GLTFExporter.js
@@ -695,7 +695,7 @@ class GLTFWriter {
 		const roughness = material.roughnessMap?.image;
 		const metalness = material.metalnessMap?.image;
 
-		if ( occlusion === roughness && roughness === metalness ) return occlusion;
+		if ( occlusion === roughness && roughness === metalness ) return material.aoMap;
 
 		if ( occlusion || roughness || metalness ) {
 
