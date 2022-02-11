d0ad408f60e9c450a209b70b7cd00e007dbcc499
Mugen87 <michael.herzog@human-interactive.org>
2022-02-11 11:24:58 +0100

    THREE.Legacy.js: Add WebGLMultisampleRenderTarget.

--- src/Three.Legacy.js
+++ src/Three.Legacy.js
@@ -1975,3 +1975,12 @@ export function ImmediateRenderObject() {
 	console.error( 'THREE.ImmediateRenderObject has been removed.' );
 
 }
+
+export function WebGLMultisampleRenderTarget( width, height, options ) {
+
+	console.error( 'THREE.WebGLMultisampleRenderTarget has been removed. Use a normal render target and set the "samples" property to greater 0 to enable multisampling.' );
+	const renderTarget = new WebGLRenderTarget( width, height, options );
+	renderTarget.samples = 4;
+	return renderTarget;
+
+}
