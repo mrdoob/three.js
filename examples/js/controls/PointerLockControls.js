6581c97ee2db050262fef472482bf1c172537062
mrdoob <info@mrdoob.com>
2022-02-18 00:20:05 +0000

    Update PointerLockControls.js

--- examples/js/controls/PointerLockControls.js
+++ examples/js/controls/PointerLockControls.js
@@ -18,7 +18,7 @@
 
 	class PointerLockControls extends THREE.EventDispatcher {
 
-		constructor( camera, domElement, options = {} ) {
+		constructor( camera, domElement ) {
 
 			super();
 
@@ -37,7 +37,7 @@
 
 			this.maxPolarAngle = Math.PI; // radians
 
-			this.mouseSpeed = options.mouseSpeed || 0.002;
+			this.mouseSpeed = 1.0;
 
 			const scope = this;
 
@@ -49,8 +49,8 @@
 
 				_euler.setFromQuaternion( camera.quaternion );
 
-				_euler.y -= movementX * scope.mouseSpeed;
-				_euler.x -= movementY * scope.mouseSpeed;
+				_euler.y -= movementX * 0.002 * scope.mouseSpeed;
+				_euler.x -= movementY * 0.002 * scope.mouseSpeed;
 				_euler.x = Math.max( _PI_2 - scope.maxPolarAngle, Math.min( _PI_2 - scope.minPolarAngle, _euler.x ) );
 				camera.quaternion.setFromEuler( _euler );
 				scope.dispatchEvent( _changeEvent );
