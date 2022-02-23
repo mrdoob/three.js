737d8657fb9c59ccef3dcf7048775d6c2eaa9d27
mrdoob <info@mrdoob.com>
2022-02-23 00:31:54 +0000

    Update FileLoader.js

--- src/loaders/FileLoader.js
+++ src/loaders/FileLoader.js
@@ -88,6 +88,8 @@ class FileLoader extends Loader {
 
 					}
 
+					// Workaround: Checking if response.body === undefined for Alipay browser #23548
+
 					if ( typeof ReadableStream === 'undefined' || response.body === undefined || response.body.getReader === undefined ) {
 
 						return response;
