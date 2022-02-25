9ce52348568ca8f871676c5623c216a283160fad
Gianmarco <gianmarcosimone89@gmail.com>
2022-02-25 22:59:14 +0100

    lint

--- src/geometries/CapsuleGeometry.js
+++ src/geometries/CapsuleGeometry.js
@@ -6,8 +6,8 @@ class CapsuleGeometry extends LatheGeometry {
 	constructor( radius = 1, length = 1, capSegments = 4, radialSegments = 8 ) {
 
 		const path = new Path();
-		path.absarc( 0, -length/2, radius, Math.PI * 1.5, 0 );
-		path.absarc( 0, length/2, radius, 0, Math.PI * 0.5 );
+		path.absarc( 0, - length / 2, radius, Math.PI * 1.5, 0 );
+		path.absarc( 0, length / 2, radius, 0, Math.PI * 0.5 );
 
 		super( path.getPoints( capSegments ), radialSegments );
 
