f7b4ab6142bd0656f09b1d3ca192f4658978cc4b
Gianmarco <gianmarcosimone89@gmail.com>
2022-02-25 08:59:09 +0100

    Update src/geometries/CapsuleGeometry.js

--- src/geometries/CapsuleGeometry.js
+++ src/geometries/CapsuleGeometry.js
@@ -3,15 +3,6 @@ import { LatheGeometry } from './LatheGeometry';
 
 class CapsuleGeometry extends LatheGeometry {
 
-	/**
-	 * Creates a CapsuleGeometry.
-	 *
-	 * @param {number} radiusTop — Radius of the emisphere at the top. Optional; defaults to 1.
-	 * @param {number} radiusBottom — Radius of the emisphere at the bottom. Optional; defaults to 1.
-	 * @param {number} height — Height of the middle section. Optional; defaults to 1.
-	 * @param {number} capSegments — Number of curve segments used to build the caps. Optional; defaults to 4.
-	 * @param {number} radialSegments — Number of segmented faces around the circumference of the capsule. Optional; defaults to 8.
-	 */
 	constructor( radiusTop = 1, radiusBottom = 1, height = 1, capSegments = 4, radialSegments = 8 ) {
 
 		const R1 = radiusTop - radiusBottom;
