147b397fb3de0c15bcdb3bdff55c99e4d81360a2
Mugen87 <michael.herzog@human-interactive.org>
2022-02-26 16:01:49 +0100

    Tests: Fix half float conversion tests.

--- test/unit/src/extras/DataUtils.tests.js
+++ test/unit/src/extras/DataUtils.tests.js
@@ -4,14 +4,14 @@ import { DataUtils } from '../../../../src/extras/DataUtils.js';
 
 export default QUnit.module( 'Extras', () => {
 
-	QUnit.module( 'ShapeUtils', () => {
+	QUnit.module( 'DataUtils', () => {
 
 		// PUBLIC STUFF
-		QUnit.todo( 'toHalfFloat', ( assert ) => {
+		QUnit.test( 'toHalfFloat', ( assert ) => {
 
 			assert.ok( DataUtils.toHalfFloat( 0 ) === 0, 'Passed!' );
-			assert.ok( DataUtils.toHalfFloat( 100000 ) === 31744, 'Passed!' ); // Infinity
-			assert.ok( DataUtils.toHalfFloat( - 100000 ) === 64512, 'Passed!' ); // -Infinity
+			assert.ok( DataUtils.toHalfFloat( 100000 ) === 31743, 'Passed!' );
+			assert.ok( DataUtils.toHalfFloat( - 100000 ) === 64511, 'Passed!' );
 			assert.ok( DataUtils.toHalfFloat( 65504 ) === 31743, 'Passed!' );
 			assert.ok( DataUtils.toHalfFloat( - 65504 ) === 64511, 'Passed!' );
 			assert.ok( DataUtils.toHalfFloat( Math.PI ) === 16968, 'Passed!' );
@@ -19,15 +19,15 @@ export default QUnit.module( 'Extras', () => {
 
 		} );
 
-		QUnit.todo( 'fromHalfFloat', ( assert ) => {
+		QUnit.test( 'fromHalfFloat', ( assert ) => {
 
 			assert.ok( DataUtils.fromHalfFloat( 0 ) === 0, 'Passed!' );
 			assert.ok( DataUtils.fromHalfFloat( 31744 ) === Infinity, 'Passed!' );
 			assert.ok( DataUtils.fromHalfFloat( 64512 ) === - Infinity, 'Passed!' );
 			assert.ok( DataUtils.fromHalfFloat( 31743 ) === 65504, 'Passed!' );
 			assert.ok( DataUtils.fromHalfFloat( 64511 ) === - 65504, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 31743 ) === 3.140625, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 64511 ) === - 3.140625, 'Passed!' );
+			assert.ok( DataUtils.fromHalfFloat( 16968 ) === 3.140625, 'Passed!' );
+			assert.ok( DataUtils.fromHalfFloat( 49736 ) === - 3.140625, 'Passed!' );
 
 		} );
 
