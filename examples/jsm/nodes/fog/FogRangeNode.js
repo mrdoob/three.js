84c14a277fbb0e887057dcef3e550f1c9c0c16f9
sunag <jcdeconto@igalia.com>
2022-02-27 17:44:44 -0300

    WebGPU: FogNode

---
+++ examples/jsm/nodes/fog/FogRangeNode.js
@@ -0,0 +1,27 @@
+import FogNode from './FogNode.js';
+import { smoothstep, negate, positionView } from '../ShaderNode.js';
+
+class FogRangeNode extends FogNode {
+
+	constructor( colorNode, nearNode, farNode ) {
+
+		super( colorNode );
+
+		this.nearNode = nearNode;
+		this.farNode = farNode;
+
+	}
+
+	generate( builder ) {
+
+		this.factorNode = smoothstep( this.nearNode, this.farNode, negate( positionView.z ) );
+
+		return super.generate( builder );
+
+	}
+
+}
+
+FogRangeNode.prototype.isFogRangeNode = true;
+
+export default FogRangeNode;
--- examples/jsm/renderers/webgpu/nodes/WebGPUNodeBuilder.js
+++ examples/jsm/renderers/webgpu/nodes/WebGPUNodeBuilder.js
@@ -25,7 +25,7 @@ import ColorSpaceNode from 'three-nodes/display/ColorSpaceNode.js';
 import LightContextNode from 'three-nodes/lights/LightContextNode.js';
 import OperatorNode from 'three-nodes/math/OperatorNode.js';
 import WGSLNodeParser from 'three-nodes/parsers/WGSLNodeParser.js';
-import { vec3, add, join, nodeObject } from 'three-nodes/ShaderNode.js';
+import { vec3, add, join, mix, nodeObject } from 'three-nodes/ShaderNode.js';
 import { getRoughness } from 'three-nodes/functions/PhysicalMaterialFunctions.js';
 
 const wgslTypeLib = {
@@ -90,18 +90,25 @@ fn inversesqrt( x : f32 ) -> f32 {
 
 class WebGPUNodeBuilder extends NodeBuilder {
 
-	constructor( object, renderer, lightNode = null ) {
+	constructor( object, renderer ) {
 
 		super( object, renderer, new WGSLNodeParser() );
 
-		this.lightNode = lightNode;
+		this.lightNode = null;
+		this.fogNode = null;
 
 		this.bindings = { vertex: [], fragment: [] };
 		this.bindingsOffset = { vertex: 0, fragment: 0 };
 
 		this.uniformsGroup = {};
 
+	}
+
+	build() {
+
 		this._parseObject();
+
+		return super.build();
 
 	}
 
@@ -288,9 +295,11 @@ class WebGPUNodeBuilder extends NodeBuilder {
 
 			}
 
+			// OUTPUT
+
 			outputNode = join( vec3( outgoingLightNode ), nodeObject( diffuseColorNode ).w );
 
-			// OUTPUT
+			// ENCODING
 
 			const outputEncoding = this.renderer.outputEncoding;
 
@@ -300,6 +309,18 @@ class WebGPUNodeBuilder extends NodeBuilder {
 				outputNode.fromEncoding( outputEncoding );
 
 			}
+
+			// FOG
+
+			const fogNode = this.fogNode;
+
+			if ( fogNode && fogNode.isFogNode ) {
+
+				outputNode = mix( outputNode, fogNode.colorNode, fogNode );
+
+			}
+
+			// RESULT
 
 			this.addFlow( 'fragment', new VarNode( outputNode, 'Output', 'vec4' ) );
 
