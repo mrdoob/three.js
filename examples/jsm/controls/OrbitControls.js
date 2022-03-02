ddc0db8d5703e198d32513dba8bd08a2eb8eac89
hh505030475 <505030475@qq.com>
2022-03-02 15:35:02 +0800

    apply dampingFactor to dollyOut/dollyIn

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -224,8 +224,20 @@ class OrbitControls extends EventDispatcher {
 
 				spherical.makeSafe();
 
-
-				spherical.radius *= scale;
+				if ( scope.enableDamping === true ) {
+					if ( scale != 1 ){
+						from_radius = spherical.radius;
+						dst_radius = spherical.radius*scale;
+						delta_radius = (dst_radius - from_radius)*3; // x3 because slow
+					}
+					spherical.radius += delta_radius * scope.dampingFactor 
+					delta_radius = delta_radius - delta_radius * scope.dampingFactor
+					if ( Math.abs( delta_radius )< EPS*10 ){
+						delta_radius = 0
+					}
+				}else{
+					spherical.radius *= scale;
+				}
 
 				// restrict radius to be between desired limits
 				spherical.radius = Math.max( scope.minDistance, Math.min( scope.maxDistance, spherical.radius ) );
@@ -340,6 +352,9 @@ class OrbitControls extends EventDispatcher {
 		const sphericalDelta = new Spherical();
 
 		let scale = 1;
+		let dst_radius = 1
+		let from_radius = 1
+		let delta_radius = 1
 		const panOffset = new Vector3();
 		let zoomChanged = false;
 
