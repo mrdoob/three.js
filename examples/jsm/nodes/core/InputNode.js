b249f6d2d96da89c2e5fe8ec9f7880bf714ab5eb
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2022-03-07 19:28:54 +1100

    Fix serialization/deserialization

--- examples/jsm/nodes/core/InputNode.js
+++ examples/jsm/nodes/core/InputNode.js
@@ -1,3 +1,4 @@
+import { Color, Matrix3, Matrix4, Vector2, Vector3, Vector4 } from 'three';
 import Node from './Node.js';
 
 function getValueType( value ) {
@@ -38,6 +39,36 @@ function getValueType( value ) {
 
 }
 
+function getValueFromType( type ) {
+
+	if ( type === 'color' ) {
+
+		return new Color();
+
+	} else if ( type.includes( 'vec2' ) ) {
+
+		return new Vector2();
+
+	} else if ( type.includes( 'vec3' ) ) {
+
+		return new Vector3();
+
+	} else if ( type.includes( 'vec4' ) ) {
+
+		return new Vector4();
+
+	} else if ( type.includes( 'mat3' ) ) {
+
+		return new Matrix3();
+
+	} else if ( type.includes( 'mat4' ) ) {
+
+		return new Matrix4();
+
+	}
+
+}
+
 class InputNode extends Node {
 
 	constructor( value, nodeType ) {
@@ -59,6 +90,7 @@ class InputNode extends Node {
 		super.serialize( data );
 
 		data.value = this.value?.toArray?.() || this.value;
+		data.nodeType = this.nodeType;
 
 	}
 
@@ -66,6 +98,8 @@ class InputNode extends Node {
 
 		super.deserialize( data );
 
+		this.nodeType = data.nodeType;
+		this.value = getValueFromType( this.nodeType );
 		this.value = this.value?.fromArray?.( data.value ) || data.value;
 
 	}
