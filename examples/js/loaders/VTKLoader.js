982142aea5f8923ff04563d3bfda5236d122f27d
Michael Herzog <michael.herzog@human-interactive.org>
2022-03-09 11:54:09 +0100

    Update VTKLoader.js

--- examples/js/loaders/VTKLoader.js
+++ examples/js/loaders/VTKLoader.js
@@ -688,10 +688,10 @@
 						// The [DATA] portion stores contiguously every block appended together. The offset from the beginning of the data section to the beginning of a block is
 						// computed by summing the compressed block sizes from preceding blocks according to the header.
 
-						const textNode = ele['#text'];
+						const textNode = ele[ '#text' ];
 						const rawData = (
 							( textNode instanceof Array )
-								? textNode[0]
+								? textNode[ 0 ]
 								: textNode
 						);
 
