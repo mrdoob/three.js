e26b19ad0bed90f43622ff77942c3e9b3b6fdd77
chubei-oppen <chubei@oppentech.com>
2022-03-16 16:46:50 +0800

    add applyQuaternion function for vector4 (#72)
    Co-authored-by: Zhili Fang <fangzhili@oppenfuturetech.com>

--- src/math/Vector4.js
+++ src/math/Vector4.js
@@ -247,6 +247,29 @@ class Vector4 {
 
 	}
 
+	applyQuaternion( q ) {
+
+		const x = this.x, y = this.y, z = this.z, w = this.w;
+		const qx = q.x, qy = q.y, qz = q.z, qw = q.w;
+
+		// calculate quat * vector
+
+		const ix = qw * x + qy * z - qz * y;
+		const iy = qw * y + qz * x - qx * z;
+		const iz = qw * z + qx * y - qy * x;
+		const iw = - qx * x - qy * y - qz * z;
+
+		// calculate result * inverse quat
+
+		this.x = ix * qw + iw * - qx + iy * - qz - iz * - qy;
+		this.y = iy * qw + iw * - qy + iz * - qx - ix * - qz;
+		this.z = iz * qw + iw * - qz + ix * - qy - iy * - qx;
+		this.w = w;
+
+		return this;
+
+	}
+
 	applyMatrix4( m ) {
 
 		const x = this.x, y = this.y, z = this.z, w = this.w;
