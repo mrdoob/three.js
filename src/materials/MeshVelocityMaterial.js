61401913356897b4a641dab3d01ed1cb5df832f6
Ben Houston <bhouston@threekit.com>
2022-03-25 07:47:13 -0400

    clean up MeshVelocityMaterial class.

--- src/materials/MeshVelocityMaterial.js
+++ src/materials/MeshVelocityMaterial.js
@@ -11,13 +11,8 @@ class MeshVelocityMaterial extends Material {
 
 		this.type = 'MeshVelocityMaterial';
 
-		this.depthPacking = BasicDepthPacking;
-
 		this.currentProjectionViewMatrix = new Matrix4();
 		this.previousProjectionViewMatrix = new Matrix4();
-
-		this.color = new Color( 0xffffff ); // diffuse
-		this.map = null;
 
 		this.displacementMap = null;
 		this.displacementScale = 1;
@@ -28,17 +23,8 @@ class MeshVelocityMaterial extends Material {
 		this.skinning = false;
 		this.morphTargets = false;
 
-		this.wireframe = false;
-		this.wireframeLinewidth = 1;
-		this.wireframeLinecap = 'round';
-		this.wireframeLinejoin = 'round';
-
 		this.fog = false;
 		this.lights = false;
-
-		// far clipping plane in both RGBA and Basic encoding
-		this.clearColor = new Color( 1.0, 1.0, 1.0 );
-		this.clearAlpha = 1.0;
 
 		this.flatShading = false;
 
@@ -54,29 +40,16 @@ class MeshVelocityMaterial extends Material {
 		this.currentProjectionViewMatrix.copy( source.currentProjectionViewMatrix );
 		this.previousProjectionViewMatrix.copy( source.previousProjectionViewMatrix );
 
-		this.opacity = source.opacity;
-		this.map.copy( source.map );
-
 		this.displacementMap.copy( source.displacementMap );
 		this.displacementScale = source.displacementScale;
 		this.displacementBias = source.displacementBias;
 
 		this.alphaMap.copy( source.alphaMap );
 
-		this.depthPacking = source.depthPacking;
-
 		this.skinning = source.skinning;
 		this.morphTargets = source.morphTargets;
 
-		this.wireframe = source.wireframe;
-		this.wireframeLinewidth = source.wireframeLinewidth;
-		this.wireframeLinecap = source.wireframeLinecap;
-		this.wireframeLinejoin = source.wireframeLinejoin;
-
 		this.flatShading = source.flatShading;
-
-		this.clearColor = source.clearColor;
-		this.clearAlpha = source.clearAlpha;
 
 		return this;
 
