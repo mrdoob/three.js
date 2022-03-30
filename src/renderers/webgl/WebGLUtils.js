d33f46321f935aa70812cf76861f155412f37e25
Mugen87 <michael.herzog@human-interactive.org>
2022-03-30 12:36:24 +0200

    WebGLUtils: Add WebGL constant fallback to convert().

--- src/renderers/webgl/WebGLUtils.js
+++ src/renderers/webgl/WebGLUtils.js
@@ -250,6 +250,10 @@ function WebGLUtils( gl, extensions, capabilities ) {
 
 		}
 
+		// if "p" can't be resolved, assume the user defines a WebGL constant as a string (fallback/workaround for packed RGB formats)
+
+		return ( gl[ p ] !== undefined ) ? gl[ p ] : null;
+
 	}
 
 	return { convert: convert };
