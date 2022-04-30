31befd5bfbb663d9cf163549a668777022b7add7
Muhammad Abeer <abeersaqib948@gmail.com>
2022-05-01 04:21:56 +0500

    CD process

--- utils/three-production.sh
+++ utils/three-production.sh
@@ -10,10 +10,13 @@ rm -rf package
 rm -rf $(find . -type f -name "*.tgz")
 output=$(git status --porcelain)
 if [ "${#output}" -ge 5 ]; then 
+    git config --global user.email "hello@webaverse.com"
+    git config --global user.name "Webaverse"
     git checkout -b threejs-bump
     git add .
     git commit -m "Bump webaverse/three.js@latest into three" -a
     git push --set-upstream origin threejs-bump -f
+    echo ${{ secrets.GITHUB_TOKEN }} | gh auth login --with-token
     gh pr create --fill
 fi
 cd ..
