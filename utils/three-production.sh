d890f453ae56faebd518f59683e19c13dbe13b9c
Muhammad Abeer <abeersaqib948@gmail.com>
2022-05-01 05:25:55 +0500

    CD process

--- utils/three-production.sh
+++ utils/three-production.sh
@@ -16,7 +16,7 @@ if [ "${#output}" -ge 5 ]; then
     git add .
     git commit -m "Bump webaverse/three.js@latest into three" -a
     # echo ${{ secrets.GITHUB_TOKEN }} | gh auth login --with-token
-    echo "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" | gh auth login --with-token
+    echo "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" | gh auth login --with-token
     git push --set-upstream origin threejs-bump -f
     gh pr create --fill
 fi
