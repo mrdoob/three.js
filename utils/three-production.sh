7c7a56405f0926e39a018e7ac1744a308e38de49
Muhammad Abeer <abeersaqib948@gmail.com>
2022-05-01 21:57:56 +0500

    CD process

--- utils/three-production.sh
+++ utils/three-production.sh
@@ -1,5 +1,5 @@
 mkdir output
-git clone https://github.com/webaverse/three output
+git clone "https://$API_TOKEN_GITHUB@github.com/three.git" output
 npm pack . --pack-destination output
 cd output
 rm -rf $(find . -type f -name "output*.tgz")
@@ -9,15 +9,26 @@ cp -r package/* .
 rm -rf package 
 rm -rf $(find . -type f -name "*.tgz")
 output=$(git status --porcelain)
-# if [ "${#output}" -ge 5 ]; then 
-#     git config --global user.email "hello@webaverse.com"
-#     git config --global user.name "Webaverse"
-#     echo $secrets.WEBA_BOT_SSH_KEY
-#     git checkout -b threejs-bump
-#     git add .
-#     git commit -m "Bump webaverse/three.js@latest into three" -a
-#     ssh git@github.com
-#     git push --set-upstream origin threejs-bump -f
-#     #gh pr create --fill
-# fi
+if [ "${#output}" -ge 5 ]; then 
+    git config --global user.email "hello@webaverse.com"
+    git config --global user.name "Webaverse"
+    echo $secrets.WEBA_BOT_SSH_KEY
+    git checkout -b threejs-bump
+    git add .
+    git commit -m "Bump webaverse/three.js@latest into three" -a
+    if git status | grep -q "Changes to be committed"
+    then
+        git commit --message "Update from https://github.com/three/commit/$GITHUB_SHA"
+        echo "Pushing git commit"
+        git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+        echo "Creating a pull request"
+        gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+                    -b $INPUT_DESTINATION_HEAD_BRANCH \
+                    -B $INPUT_DESTINATION_BASE_BRANCH \
+                    -H $INPUT_DESTINATION_HEAD_BRANCH \
+                        $PULL_REQUEST_REVIEWERS
+        else
+    echo "No changes detected"
+    fi
+fi
 cd ..
