27c41836c6c3ff4f677c0b66b2fb0f53c3eebf07
Marco Fugaro <marco.fugaro@gmail.com>
2022-05-11 20:55:17 +0200

    Put back local variables

--- src/math/Matrix4.js
+++ src/math/Matrix4.js
@@ -1,13 +1,4 @@
 import { Vector3 } from './Vector3.js';
-
-const _v1 = /*@__PURE__*/ new Vector3();
-const _m1 = /*@__PURE__*/ new Matrix4();
-const _zero = /*@__PURE__*/ new Vector3( 0, 0, 0 );
-const _one = /*@__PURE__*/ new Vector3( 1, 1, 1 );
-const _x = /*@__PURE__*/ new Vector3();
-const _y = /*@__PURE__*/ new Vector3();
-const _z = /*@__PURE__*/ new Vector3();
-
 class Matrix4 {
 
 	constructor() {
@@ -881,5 +872,13 @@ class Matrix4 {
 	}
 
 }
+
+const _v1 = /*@__PURE__*/ new Vector3();
+const _m1 = /*@__PURE__*/ new Matrix4();
+const _zero = /*@__PURE__*/ new Vector3( 0, 0, 0 );
+const _one = /*@__PURE__*/ new Vector3( 1, 1, 1 );
+const _x = /*@__PURE__*/ new Vector3();
+const _y = /*@__PURE__*/ new Vector3();
+const _z = /*@__PURE__*/ new Vector3();
 
 export { Matrix4 };
--- src/math/Vector3.js
+++ src/math/Vector3.js
@@ -1,9 +1,5 @@
 import * as MathUtils from './MathUtils.js';
 import { Quaternion } from './Quaternion.js';
-
-const _vector = /*@__PURE__*/ new Vector3();
-const _quaternion = /*@__PURE__*/ new Quaternion();
-
 class Vector3 {
 
 	constructor( x = 0, y = 0, z = 0 ) {
@@ -751,5 +747,8 @@ class Vector3 {
 	}
 
 }
+
+const _vector = /*@__PURE__*/ new Vector3();
+const _quaternion = /*@__PURE__*/ new Quaternion();
 
 export { Vector3 };
