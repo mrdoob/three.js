49c4fabf7e6a7c788d218622daca218219386164
nianxy <nianxy@users.noreply.github.com>
2022-05-27 10:34:52 +0000

    Release 0.137.6

--- build/three.js
+++ build/three.js
@@ -206,20 +206,30 @@
 				requestAnimationFrame: params.requestAnimationFrame,
 				cancelAnimationFrame: params.cancelAnimationFrame
 			};
+			this.devicePixelRatio = params.devicePixelRatio;
 		}
 
 	};
 
 	if (typeof window !== 'undefined' && typeof window.window !== 'undefined') {
-		platform.updateplatform({
-			AudioContext: (window.AudioContext || window.webkitAudioContext).bind(window),
-			fetch: window.fetch.bind(window),
-			Request: window.Request.bind(window),
-			Headers: window.Headers.bind(window),
-			createElementNS: document.createElementNS.bind(document),
-			requestAnimationFrame: window.requestAnimationFrame.bind(window),
-			cancelAnimationFrame: window.cancelAnimationFrame.bind(window)
-		});
+		// When running three.js in a virtual dom environment like jsdom, the window object
+		// may not contains all the members that are required by the platform, so we should
+		// check these memebers before updating the platform. You should update the platform
+		// manually when the check is failed.
+		if ((window.AudioContext || window.webkitAudioContext) && window.fetch && window.Request && window.Headers && document.createElementNS && window.requestAnimationFrame && window.cancelAnimationFrame && window.devicePixelRatio !== undefined) {
+			platform.updateplatform({
+				AudioContext: (window.AudioContext || window.webkitAudioContext).bind(window),
+				fetch: window.fetch.bind(window),
+				Request: window.Request.bind(window),
+				Headers: window.Headers.bind(window),
+				createElementNS: document.createElementNS.bind(document),
+				requestAnimationFrame: window.requestAnimationFrame.bind(window),
+				cancelAnimationFrame: window.cancelAnimationFrame.bind(window),
+				devicePixelRatio: () => {
+					return window.devicePixelRatio;
+				}
+			});
+		}
 	}
 
 	/**
--- build/three.module.js
+++ build/three.module.js
@@ -198,6 +198,7 @@ const platform = {
 			cancelAnimationFrame: params.cancelAnimationFrame,
 
 		};
+		this.devicePixelRatio = params.devicePixelRatio;
 
 	}
 
@@ -205,17 +206,37 @@ const platform = {
 
 if ( typeof window !== 'undefined' && typeof window.window !== 'undefined' ) {
 
-	platform.updateplatform( {
+	// When running three.js in a virtual dom environment like jsdom, the window object
+	// may not contains all the members that are required by the platform, so we should
+	// check these memebers before updating the platform. You should update the platform
+	// manually when the check is failed.
+	if ( ( window.AudioContext || window.webkitAudioContext ) &&
+			window.fetch &&
+			window.Request &&
+			window.Headers &&
+			document.createElementNS &&
+			window.requestAnimationFrame &&
+			window.cancelAnimationFrame &&
+			window.devicePixelRatio !== undefined ) {
 
-		AudioContext: ( window.AudioContext || window.webkitAudioContext ).bind( window ),
-		fetch: window.fetch.bind( window ),
-		Request: window.Request.bind( window ),
-		Headers: window.Headers.bind( window ),
-		createElementNS: document.createElementNS.bind( document ),
-		requestAnimationFrame: window.requestAnimationFrame.bind( window ),
-		cancelAnimationFrame: window.cancelAnimationFrame.bind( window ),
+		platform.updateplatform( {
 
-	} );
+			AudioContext: ( window.AudioContext || window.webkitAudioContext ).bind( window ),
+			fetch: window.fetch.bind( window ),
+			Request: window.Request.bind( window ),
+			Headers: window.Headers.bind( window ),
+			createElementNS: document.createElementNS.bind( document ),
+			requestAnimationFrame: window.requestAnimationFrame.bind( window ),
+			cancelAnimationFrame: window.cancelAnimationFrame.bind( window ),
+			devicePixelRatio: () => {
+
+				return window.devicePixelRatio;
+
+			},
+
+		} );
+
+	}
 
 }
 
