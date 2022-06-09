0173a6498842fe42e54d52c1b00f488827bbf5f3
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2022-06-10 00:06:32 +0930

    COEP: credentialless

--- editor/sw.js
+++ editor/sw.js
@@ -267,7 +267,7 @@ async function networkFirst( request ) {
 		if ( request.url.endsWith( 'editor/' ) || request.url.endsWith( 'editor/index.html' ) ) { // copied from coi-serviceworker
 
 			const newHeaders = new Headers( response.headers );
-			newHeaders.set( "Cross-Origin-Embedder-Policy", "require-corp" );
+			newHeaders.set( "Cross-Origin-Embedder-Policy", "credentialless" );
 			newHeaders.set( "Cross-Origin-Opener-Policy", "same-origin" );
 
 			response = new Response( response.body, { status: response.status, statusText: response.statusText, headers: newHeaders } );
