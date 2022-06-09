fd6d3d272ca8587878cf1f1aa85493f91b8e03c7
Patrick Schroen <ps@ufotechnologies.com>
2022-06-09 10:19:13 -0400

    Tree-shaking: DataUtils

--- src/extras/DataUtils.js
+++ src/extras/DataUtils.js
@@ -8,14 +8,16 @@ class DataUtils {
 
 	static toHalfFloat( val ) {
 
+		if ( ! this._floatView ) this._generateTables();
+
 		if ( Math.abs( val ) > 65504 ) console.warn( 'THREE.DataUtils.toHalfFloat(): Value out of range.' );
 
 		val = clamp( val, - 65504, 65504 );
 
-		_floatView[ 0 ] = val;
-		const f = _uint32View[ 0 ];
+		this._floatView[ 0 ] = val;
+		const f = this._uint32View[ 0 ];
 		const e = ( f >> 23 ) & 0x1ff;
-		return _baseTable[ e ] + ( ( f & 0x007fffff ) >> _shiftTable[ e ] );
+		return this._baseTable[ e ] + ( ( f & 0x007fffff ) >> this._shiftTable[ e ] );
 
 	}
 
@@ -23,129 +25,137 @@ class DataUtils {
 
 	static fromHalfFloat( val ) {
 
+		if ( ! this._floatView ) this._generateTables();
+
 		const m = val >> 10;
-		_uint32View[ 0 ] = _mantissaTable[ _offsetTable[ m ] + ( val & 0x3ff ) ] + _exponentTable[ m ];
-		return _floatView[ 0 ];
+		this._uint32View[ 0 ] = this._mantissaTable[ this._offsetTable[ m ] + ( val & 0x3ff ) ] + this._exponentTable[ m ];
+		return this._floatView[ 0 ];
 
 	}
 
-}
+	static _generateTables() {
 
-// float32 to float16 helpers
+		// float32 to float16 helpers
 
-const _buffer = new ArrayBuffer( 4 );
-const _floatView = new Float32Array( _buffer );
-const _uint32View = new Uint32Array( _buffer );
+		const _buffer = new ArrayBuffer( 4 );
 
-const _baseTable = new Uint32Array( 512 );
-const _shiftTable = new Uint32Array( 512 );
+		this._floatView = new Float32Array( _buffer );
+		this._uint32View = new Uint32Array( _buffer );
 
-for ( let i = 0; i < 256; ++ i ) {
+		this._baseTable = new Uint32Array( 512 );
+		this._shiftTable = new Uint32Array( 512 );
 
-	const e = i - 127;
+		for ( let i = 0; i < 256; ++ i ) {
 
-	// very small number (0, -0)
+			const e = i - 127;
 
-	if ( e < - 27 ) {
+			// very small number (0, -0)
 
-		_baseTable[ i ] = 0x0000;
-		_baseTable[ i | 0x100 ] = 0x8000;
-		_shiftTable[ i ] = 24;
-		_shiftTable[ i | 0x100 ] = 24;
+			if ( e < - 27 ) {
 
-		// small number (denorm)
+				this._baseTable[ i ] = 0x0000;
+				this._baseTable[ i | 0x100 ] = 0x8000;
+				this._shiftTable[ i ] = 24;
+				this._shiftTable[ i | 0x100 ] = 24;
 
-	} else if ( e < - 14 ) {
+				// small number (denorm)
 
-		_baseTable[ i ] = 0x0400 >> ( - e - 14 );
-		_baseTable[ i | 0x100 ] = ( 0x0400 >> ( - e - 14 ) ) | 0x8000;
-		_shiftTable[ i ] = - e - 1;
-		_shiftTable[ i | 0x100 ] = - e - 1;
+			} else if ( e < - 14 ) {
 
-		// normal number
+				this._baseTable[ i ] = 0x0400 >> ( - e - 14 );
+				this._baseTable[ i | 0x100 ] = ( 0x0400 >> ( - e - 14 ) ) | 0x8000;
+				this._shiftTable[ i ] = - e - 1;
+				this._shiftTable[ i | 0x100 ] = - e - 1;
 
-	} else if ( e <= 15 ) {
+				// normal number
 
-		_baseTable[ i ] = ( e + 15 ) << 10;
-		_baseTable[ i | 0x100 ] = ( ( e + 15 ) << 10 ) | 0x8000;
-		_shiftTable[ i ] = 13;
-		_shiftTable[ i | 0x100 ] = 13;
+			} else if ( e <= 15 ) {
 
-		// large number (Infinity, -Infinity)
+				this._baseTable[ i ] = ( e + 15 ) << 10;
+				this._baseTable[ i | 0x100 ] = ( ( e + 15 ) << 10 ) | 0x8000;
+				this._shiftTable[ i ] = 13;
+				this._shiftTable[ i | 0x100 ] = 13;
 
-	} else if ( e < 128 ) {
+				// large number (Infinity, -Infinity)
 
-		_baseTable[ i ] = 0x7c00;
-		_baseTable[ i | 0x100 ] = 0xfc00;
-		_shiftTable[ i ] = 24;
-		_shiftTable[ i | 0x100 ] = 24;
+			} else if ( e < 128 ) {
 
-		// stay (NaN, Infinity, -Infinity)
+				this._baseTable[ i ] = 0x7c00;
+				this._baseTable[ i | 0x100 ] = 0xfc00;
+				this._shiftTable[ i ] = 24;
+				this._shiftTable[ i | 0x100 ] = 24;
 
-	} else {
+				// stay (NaN, Infinity, -Infinity)
 
-		_baseTable[ i ] = 0x7c00;
-		_baseTable[ i | 0x100 ] = 0xfc00;
-		_shiftTable[ i ] = 13;
-		_shiftTable[ i | 0x100 ] = 13;
+			} else {
 
-	}
+				this._baseTable[ i ] = 0x7c00;
+				this._baseTable[ i | 0x100 ] = 0xfc00;
+				this._shiftTable[ i ] = 13;
+				this._shiftTable[ i | 0x100 ] = 13;
 
-}
+			}
 
-// float16 to float32 helpers
+		}
 
-const _mantissaTable = new Uint32Array( 2048 );
-const _exponentTable = new Uint32Array( 64 );
-const _offsetTable = new Uint32Array( 64 );
+		// float16 to float32 helpers
 
-for ( let i = 1; i < 1024; ++ i ) {
+		this._mantissaTable = new Uint32Array( 2048 );
+		this._exponentTable = new Uint32Array( 64 );
+		this._offsetTable = new Uint32Array( 64 );
 
-	let m = i << 13; // zero pad mantissa bits
-	let e = 0; // zero exponent
+		for ( let i = 1; i < 1024; ++ i ) {
 
-	// normalized
-	while ( ( m & 0x00800000 ) === 0 ) {
+			let m = i << 13; // zero pad mantissa bits
+			let e = 0; // zero exponent
 
-		m <<= 1;
-		e -= 0x00800000; // decrement exponent
+			// normalized
+			while ( ( m & 0x00800000 ) === 0 ) {
 
-	}
+				m <<= 1;
+				e -= 0x00800000; // decrement exponent
 
-	m &= ~ 0x00800000; // clear leading 1 bit
-	e += 0x38800000; // adjust bias
+			}
 
-	_mantissaTable[ i ] = m | e;
+			m &= ~ 0x00800000; // clear leading 1 bit
+			e += 0x38800000; // adjust bias
 
-}
+			this._mantissaTable[ i ] = m | e;
 
-for ( let i = 1024; i < 2048; ++ i ) {
+		}
 
-	_mantissaTable[ i ] = 0x38000000 + ( ( i - 1024 ) << 13 );
+		for ( let i = 1024; i < 2048; ++ i ) {
 
-}
+			this._mantissaTable[ i ] = 0x38000000 + ( ( i - 1024 ) << 13 );
 
-for ( let i = 1; i < 31; ++ i ) {
+		}
 
-	_exponentTable[ i ] = i << 23;
+		for ( let i = 1; i < 31; ++ i ) {
 
-}
+			this._exponentTable[ i ] = i << 23;
 
-_exponentTable[ 31 ] = 0x47800000;
-_exponentTable[ 32 ] = 0x80000000;
-for ( let i = 33; i < 63; ++ i ) {
+		}
 
-	_exponentTable[ i ] = 0x80000000 + ( ( i - 32 ) << 23 );
+		this._exponentTable[ 31 ] = 0x47800000;
+		this._exponentTable[ 32 ] = 0x80000000;
 
-}
+		for ( let i = 33; i < 63; ++ i ) {
 
-_exponentTable[ 63 ] = 0xc7800000;
+			this._exponentTable[ i ] = 0x80000000 + ( ( i - 32 ) << 23 );
 
-for ( let i = 1; i < 64; ++ i ) {
+		}
 
-	if ( i !== 32 ) {
+		this._exponentTable[ 63 ] = 0xc7800000;
 
-		_offsetTable[ i ] = 1024;
+		for ( let i = 1; i < 64; ++ i ) {
+
+			if ( i !== 32 ) {
+
+				this._offsetTable[ i ] = 1024;
+
+			}
+
+		}
 
 	}
 
