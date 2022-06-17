533cf80f0fd1b4bc4b9ee21aa67b2e3c01c5b65f
Michael Herzog <michael.herzog@human-interactive.org>
2022-06-17 09:44:46 +0200

    ArcballControls: Use dispose() in makeGizmos(). (#24241)

--- examples/jsm/controls/ArcballControls.js
+++ examples/jsm/controls/ArcballControls.js
@@ -2279,7 +2279,7 @@ class ArcballControls extends EventDispatcher {
 		this._gizmoMatrixState0.identity().setPosition( tbCenter );
 		this._gizmoMatrixState.copy( this._gizmoMatrixState0 );
 
-		if ( this.camera.zoom != 1 ) {
+		if ( this.camera.zoom !== 1 ) {
 
 			//adapt gizmos size to camera zoom
 			const size = 1 / this.camera.zoom;
@@ -2294,7 +2294,22 @@ class ArcballControls extends EventDispatcher {
 
 		this._gizmoMatrixState.decompose( this._gizmos.position, this._gizmos.quaternion, this._gizmos.scale );
 
+		//
+
+		this._gizmos.traverse( function( object ) {
+
+			if ( object.isLine ) {
+		
+				object.geometry.dispose();
+				object.material.dispose();
+		
+			}
+		
+		} );
+
 		this._gizmos.clear();
+
+		//
 
 		this._gizmos.add( gizmoX );
 		this._gizmos.add( gizmoY );
