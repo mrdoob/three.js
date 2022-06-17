1852dd26f5842ac4e2e7e5960a1495560dde9b6c
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2022-06-18 02:36:04 +1000

    Linux WebGPU

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -151,7 +151,7 @@ async function main() {
 	/* Launch browser */
 
 	const flags = [ '--enable-unsafe-webgpu', '--hide-scrollbars' ];
-	if ( process.platform === 'linux' ) flags.push( '--enable-features=Vulkan,UseSkiaRenderer', '--use-vulkan=native', '--disable-vulkan-surface', '--disable-features=VaapiVideoDecoder', '--ignore-gpu-blocklist', '--use-angle=vulkan' );
+	if ( process.platform === 'linux' ) flags.unshift( '--enable-features=Vulkan,UseSkiaRenderer', '--use-vulkan=native', '--disable-vulkan-surface', '--disable-features=VaapiVideoDecoder', '--ignore-gpu-blocklist', '--use-angle=vulkan' );
 
 	const viewport = { width: width * viewScale, height: height * viewScale };
 
