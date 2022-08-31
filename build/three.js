68daccedef9c9c325cc5f4c929fcaf05229aa1b3
Mr.doob <info@mrdoob.com>
2022-08-31 10:30:55 -0700

    r144

--- build/three.js
+++ build/three.js
@@ -9,7 +9,7 @@
 	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.THREE = {}));
 })(this, (function (exports) { 'use strict';
 
-	const REVISION = '144dev';
+	const REVISION = '144';
 	const MOUSE = {
 		LEFT: 0,
 		MIDDLE: 1,
