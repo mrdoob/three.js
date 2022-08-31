9db83d811ec1e79dcbc968bce8b80939b2552e32
Mr.doob <info@mrdoob.com>
2022-08-31 10:19:43 -0700

    Updated builds.

--- build/three.cjs
+++ build/three.cjs
@@ -1081,7 +1081,7 @@ class Matrix3 {
 function arrayNeedsUint32(array) {
 	// assumes larger values usually on last
 	for (let i = array.length - 1; i >= 0; --i) {
-		if (array[i] > 65535) return true;
+		if (array[i] >= 65535) return true; // account for PRIMITIVE_RESTART_FIXED_INDEX, #24565
 	}
 
 	return false;
--- build/three.js
+++ build/three.js
@@ -1083,7 +1083,7 @@
 	function arrayNeedsUint32(array) {
 		// assumes larger values usually on last
 		for (let i = array.length - 1; i >= 0; --i) {
-			if (array[i] > 65535) return true;
+			if (array[i] >= 65535) return true; // account for PRIMITIVE_RESTART_FIXED_INDEX, #24565
 		}
 
 		return false;
