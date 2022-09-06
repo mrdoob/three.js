mrdoob/three.js

commit 946c493bfe4e35bb8b0ceaf61ca13f9671054c59

PropertyBinding: Allow `map` as target object. (#24537)

src/animation/PropertyBinding.js
@@ -32,7 +32,7 @@ const _trackRe = new RegExp( ''
 	+ '$'
 );
 
-const _supportedObjectNames = [ 'material', 'materials', 'bones' ];
+const _supportedObjectNames = [ 'material', 'materials', 'bones', 'map' ];
 
 class Composite {
 
@@ -494,6 +494,25 @@ class PropertyBinding {
 
 					}
 
+					break;
+
+				case 'map':
+
+					if ( ! targetObject.material ) {
+
+						console.error( 'THREE.PropertyBinding: Can not bind to material as node does not have a material.', this );
+						return;
+
+					}
+
+					if ( ! targetObject.material.map ) {
+
+						console.error( 'THREE.PropertyBinding: Can not bind to material.map as node.material does not have a map.', this );
+						return;
+
+					}
+
+					targetObject = targetObject.material.map;
 					break;
 
 				default:

commit 5728a01f9eff4886c04eeacbab23973548ba18e1

fix: map property binding regression introduced in #24537

src/animation/PropertyBinding.js
@@ -498,6 +498,13 @@ class PropertyBinding {
 
 				case 'map':
 
+					if ( 'map' in targetObject ) {
+
+						targetObject = targetObject.map;
+						break;
+
+					}
+
 					if ( ! targetObject.material ) {
 
 						console.error( 'THREE.PropertyBinding: Can not bind to material as node does not have a material.', this );