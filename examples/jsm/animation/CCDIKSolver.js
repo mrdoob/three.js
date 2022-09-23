a53977c4e761972f9decd27f4ea13cea4efee728
Antoine BERNIER <antoine.bernier@gmail.com>
2022-09-23 09:43:23 +0200

    New `animation / skinning / ik` example (#24652)
    * kira example
    
    * kira example
    
    * adding link to the example into CCDIKSolver documentation
    
    * removing old file
    
    * updating final url
    
    * lil-gui 0.16 + Tween
    
    * fix eslint issues
    
    * glb
    
    * no dead comments
    
    * remaining dead comments
    
    * adding to the screenshot's exceptionList
    
    * chore: textures from glb
    
    * feat: no fancy
    
    * re-enabling screenshot
    
    * tabs
    
    * followSphere off by default
    
    * Update webgl_animation_skinning_ik.html
    
    Clean up.
    
    * fix(CCDIKSolver warning): links order count + removing 1 useless link
    
    * chore: better camera angle
    
    * fix: eslint --fix
    
    * transformControls showX false
    
    * new glb version
    
    * updating screenshot
    
    * fixing link to the experiment
    
    * Update CCDIKSolver.js
    
    * Update webgl_animation_skinning_ik.html
    
    Co-authored-by: Michael Herzog <michael.herzog@human-interactive.org>

--- examples/jsm/animation/CCDIKSolver.js
+++ examples/jsm/animation/CCDIKSolver.js
@@ -283,7 +283,7 @@ function setPositionOfBoneToAttributeArray( array, index, bone, matrixWorldInv )
  */
 class CCDIKHelper extends Object3D {
 
-	constructor( mesh, iks = [] ) {
+	constructor( mesh, iks = [], sphereSize = 0.25 ) {
 
 		super();
 
@@ -293,7 +293,7 @@ class CCDIKHelper extends Object3D {
 		this.matrix.copy( mesh.matrixWorld );
 		this.matrixAutoUpdate = false;
 
-		this.sphereGeometry = new SphereGeometry( 0.25, 16, 8 );
+		this.sphereGeometry = new SphereGeometry( sphereSize, 16, 8 );
 
 		this.targetSphereMaterial = new MeshBasicMaterial( {
 			color: new Color( 0xff8888 ),
