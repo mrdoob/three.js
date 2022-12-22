67e13d4d33b90ad71e8cc8c0d7140c0b8244bd72
Mugen87 <michael.herzog@human-interactive.org>
2022-12-22 11:13:10 +0100

    NodeBuilder: Fix hasGeometryAttribute().

--- examples/jsm/nodes/core/NodeBuilder.js
+++ examples/jsm/nodes/core/NodeBuilder.js
@@ -297,7 +297,7 @@ class NodeBuilder {
 
 	hasGeometryAttribute( name ) {
 
-		return this.geometry ? ( this.geometry.getAttribute( name ) !== undefined ) : undefined;
+		return this.geometry && ( this.geometry.getAttribute( name ) !== undefined );
 
 	}
 
