42620fb40c61dcf7b6a46c4a042beb3f1355f833
Michael Herzog <michael.herzog@human-interactive.org>
2022-12-23 10:59:04 +0100

    Examples: Clean up. (#25181)

--- examples/jsm/nodes/functions/light/getDirectionVector.js
+++ examples/jsm/nodes/functions/light/getDirectionVector.js
@@ -4,13 +4,13 @@ let vector3;
 
 const getDirectionVector = ( light, camera, directionVector ) => {
 
-	vector3 ||= new Vector3();
+	vector3 || ( vector3 = new Vector3() );
 
 	directionVector.setFromMatrixPosition( light.matrixWorld );
 	vector3.setFromMatrixPosition( light.target.matrixWorld );
 	directionVector.sub( vector3 );
 	directionVector.transformDirection( camera.matrixWorldInverse );
 
-}
+};
 
 export default getDirectionVector;
