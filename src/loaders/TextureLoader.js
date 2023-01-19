26c32cf1aff58d5a07764b9af3c5a9a43250f8ba
Ibrahim-Benkhedda <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2023-01-19 02:23:35 +0100

    Add caching functionality and documentation to TextureLoader class
    ## Description
    
    This contribution adds caching functionality to the TextureLoader class. The cache stores previously loaded textures and can be used to avoid loading the same texture multiple times. The cache size is limited by the maxCacheSize variable and when the cache is full, the least recently used texture will be removed.
    
    ## Changes Made
    - Added a cache variable of type Map to store previously loaded textures
    - Added a maxCacheSize variable to limit the cache size
    - Before loading a texture, the code checks if it already exists in the cache
    - If it exists, the cached texture is returned instead of loading it again
    - If it does not exist, the texture is loaded and added to the cache
    - A clearCache() function is added to clear the cache.
    
    ## Benefits 
    - Improves the performance of the library by avoiding loading the same texture multiple times
    - Reduces the memory usage by limiting the size of the cache
    - Provides a clearCache() function for the user to clear the cache if needed

--- src/loaders/TextureLoader.js
+++ src/loaders/TextureLoader.js
@@ -4,38 +4,73 @@ import { Loader } from './Loader.js';
 
 class TextureLoader extends Loader {
 
-	constructor( manager ) {
+    /**
+     * A cache object to store previously loaded textures
+     * @type {Map<string, Texture>}
+     */
+    cache = new Map();
 
-		super( manager );
+    /**
+     * Maximum size of the cache
+     * @type {number}
+     */
+    maxCacheSize = 100;
 
-	}
+    constructor(manager) {
+        super(manager);
+    }
 
-	load( url, onLoad, onProgress, onError ) {
+    /**
+     * Loads a texture from the given URL
+     * @param {string} url - The URL of the texture to load
+     * @param {function} onLoad - A callback function to be called when the texture is loaded
+     * @param {function} onProgress - A callback function to be called while the texture is loading
+     * @param {function} onError - A callback function to be called if an error occurs while loading the texture
+     * @returns {Texture}
+     */
+    load(url, onLoad, onProgress, onError) {
+        // Check if the texture is already in the cache
+        let texture = this.cache.get(url);
+        if (texture) {
+            // If it is, return the cached texture
+            if (onLoad) onLoad(texture);
+            return texture;
+        }
 
-		const texture = new Texture();
+        // If not, create a new Texture object
+        texture = new Texture();
 
-		const loader = new ImageLoader( this.manager );
-		loader.setCrossOrigin( this.crossOrigin );
-		loader.setPath( this.path );
+        // create a new ImageLoader
+        const loader = new ImageLoader(this.manager);
+        loader.setCrossOrigin(this.crossOrigin);
+        loader.setPath(this.path);
 
-		loader.load( url, function ( image ) {
+        // load the image
+        loader.load(url, image => {
+            // set the image to the texture
+            texture.image = image;
+            texture.needsUpdate = true;
 
-			texture.image = image;
-			texture.needsUpdate = true;
+            // add the texture to the cache
+            this.cache.set(url, texture);
+            // if the cache size exceeds the maximum size, remove the least recently used texture
+            if (this.cache.size > this.maxCacheSize) {
+                const firstKey = this.cache.keys().next().value;
+                this.cache.delete(firstKey);
+            }
+            // call the onLoad callback
+            if (onLoad) onLoad(texture);
+        }, onProgress, onError);
 
-			if ( onLoad !== undefined ) {
+        return texture;
+    }
 
-				onLoad( texture );
-
-			}
-
-		}, onProgress, onError );
-
-		return texture;
-
-	}
-
+    /**
+     * Clear the cache
+     */
+    clearCache() {
+        this.cache.clear();
+    }
 }
-
 
 export { TextureLoader };
