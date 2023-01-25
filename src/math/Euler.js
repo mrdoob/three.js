ce3498af903d54b062b94fa945df4250417d3229
Evandro Bernardes <s.evandro@hotmail.com>
2023-01-25 20:05:19 +0100

    removed unused import

--- src/math/Euler.js
+++ src/math/Euler.js
@@ -1,7 +1,6 @@
 import { Quaternion } from './Quaternion.js';
 import { Matrix4 } from './Matrix4.js';
 import { clamp } from './MathUtils.js';
-import * as MathUtils from './MathUtils.js';
 
 const _matrix = /*@__PURE__*/ new Matrix4();
 const _quaternion = /*@__PURE__*/ new Quaternion();
