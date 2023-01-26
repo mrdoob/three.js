c50d742ec831dd0e115475cf4d4b651a0e95730d
Don McCurdy <dm@donmccurdy.com>
2023-01-26 08:50:53 -0500

    Editor: Set ColorManagement.legacyMode = false (#25222)
    * Editor: Set ColorManagement.legacyMode = false
    
    * Editor: Enable ColorManagement for apps.

--- editor/js/Editor.js
+++ editor/js/Editor.js
@@ -7,6 +7,8 @@ import { Strings } from './Strings.js';
 import { Storage as _Storage } from './Storage.js';
 import { Selector } from './Viewport.Selector.js';
 
+THREE.ColorManagement.legacyMode = false;
+
 var _DEFAULT_CAMERA = new THREE.PerspectiveCamera( 50, 1, 0.01, 1000 );
 _DEFAULT_CAMERA.name = 'Camera';
 _DEFAULT_CAMERA.position.set( 0, 5, 10 );
