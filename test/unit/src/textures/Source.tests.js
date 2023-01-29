f0a17bcd35290528d81f7409f6fa7d9a802fffa3
Ed Preston <epreston@prestonsoft.com>
2023-01-30 04:42:29 +1100

    Tests: stub textures/Source unit tests
    Add unit test file for textures/Source

---
+++ test/unit/src/textures/Source.tests.js
@@ -0,0 +1,56 @@
+/* global QUnit */
+
+// import { Source } from '../../../../src/textures/Source.js';
+
+export default QUnit.module( 'Textures', () => {
+
+	QUnit.module( 'Source', () => {
+
+		// INSTANCING
+		QUnit.todo( 'Instancing', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PROPERTIES
+		QUnit.todo( 'data', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'needsUpdate', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'uuid', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'version', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
+		QUnit.todo( 'isSource', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'toJSON', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+	} );
+
+} );
