298719dd3a45a4d54215552c1696fe5e298c540b
Ed Preston <epreston@prestonsoft.com>
2023-01-30 07:29:37 +1100

    Create Data3DTexture.tests.js
    Add missing unit test file, one complete test to show its working, stubs for members.

---
+++ test/unit/src/textures/Data3DTexture.tests.js
@@ -0,0 +1,79 @@
+/* global QUnit */
+
+import { Data3DTexture } from '../../../../src/textures/Data3DTexture.js';
+
+import { Texture } from '../../../../src/textures/Texture.js';
+
+export default QUnit.module( 'Textures', () => {
+
+	QUnit.module( 'Data3DTexture', () => {
+
+		// INHERITANCE
+		QUnit.test( 'Extending', ( assert ) => {
+
+			var object = new Data3DTexture();
+
+			assert.strictEqual( object instanceof Texture, true, 'Data3DTexture extends from Texture' );
+
+		} );
+
+		// INSTANCING
+		QUnit.todo( 'Instancing', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PROPERTIES
+		QUnit.todo( 'image', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'magFilter', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'minFilter', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'wrapR', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'generateMipmaps', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'flipY', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'unpackAlignment', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
+		QUnit.todo( 'isData3DTexture', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+	} );
+
+} );
