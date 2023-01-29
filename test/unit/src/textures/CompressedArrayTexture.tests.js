b9ae2498d2a3d24ac54f036d17880962917cf9d4
Ed Preston <epreston@prestonsoft.com>
2023-01-30 07:14:40 +1100

    Create CompressedArrayTexture.tests.js
    Add missing unit test file, one complete test to show its working, stubs for members.

---
+++ test/unit/src/textures/CompressedArrayTexture.tests.js
@@ -0,0 +1,49 @@
+/* global QUnit */
+
+import { CompressedArrayTexture } from '../../../../src/textures/CompressedArrayTexture.js';
+
+import { Texture } from '../../../../src/textures/Texture.js';
+
+export default QUnit.module( 'Textures', () => {
+
+	QUnit.module( 'CompressedArrayTexture', () => {
+
+		// INHERITANCE
+		QUnit.test( 'Extending', ( assert ) => {
+
+			var object = new CompressedArrayTexture();
+
+			assert.strictEqual( object instanceof Texture, true, 'CompressedArrayTexture extends from Texture' );
+
+		} );
+
+		// INSTANCING
+		QUnit.todo( 'Instancing', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PROPERTIES
+		QUnit.todo( 'image.depth', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'wrapR', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
+		QUnit.todo( 'isCompressedArrayTexture', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+	} );
+
+} );
