4cc7c7d31d2dda2d1cd359d53234d1029812259e
Ed Preston <epreston@prestonsoft.com>
2023-01-30 16:08:14 +1100

    Update WebGLCubeRenderTarget.tests.js
    WebGLCubeRenderTarget extends from WebGLRenderTarget.  Add missing member test stubs.

--- test/unit/src/renderers/WebGLCubeRenderTarget.tests.js
+++ test/unit/src/renderers/WebGLCubeRenderTarget.tests.js
@@ -1,10 +1,21 @@
 /* global QUnit */
 
-// import { WebGLCubeRenderTarget } from '../../../../src/renderers/WebGLCubeRenderTarget.js';
+import { WebGLCubeRenderTarget } from '../../../../src/renderers/WebGLCubeRenderTarget.js';
+
+import { WebGLRenderTarget } from '../../../../src/renderers/WebGLRenderTarget.js';
 
 export default QUnit.module( 'Renderers', () => {
 
 	QUnit.module( 'WebGLCubeRenderTarget', () => {
+
+		// INHERITANCE
+		QUnit.test( 'Extending', ( assert ) => {
+
+			var object = new WebGLCubeRenderTarget();
+
+			assert.strictEqual( object instanceof WebGLRenderTarget, true, 'WebGLCubeRenderTarget extends from WebGLRenderTarget' );
+
+		} );
 
 		// INSTANCING
 		QUnit.todo( 'Instancing', ( assert ) => {
@@ -13,8 +24,28 @@ export default QUnit.module( 'Renderers', () => {
 
 		} );
 
-		// PUBLIC STUFF
+		// PROPERTIES
+		QUnit.todo( 'texture', ( assert ) => {
+
+			// doc update needed, this needs to be a CubeTexture unlike parent class
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
 		QUnit.todo( 'isWebGLCubeRenderTarget', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'fromEquirectangularTexture', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'clear', ( assert ) => {
 
 			assert.ok( false, 'everything\'s gonna be alright' );
 
