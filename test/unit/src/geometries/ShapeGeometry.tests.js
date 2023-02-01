140faaf8a6dd0adcc289021fc65da0b400071f30
Ed Preston <epreston@prestonsoft.com>
2023-02-02 02:56:05 +1100

    Update ShapeGeometry.tests.js
    ShapeGeometry extends from BufferGeometry
    ShapeGeometry.type should be ShapeGeometry
    Stub missing member tests.

--- test/unit/src/geometries/ShapeGeometry.tests.js
+++ test/unit/src/geometries/ShapeGeometry.tests.js
@@ -3,15 +3,17 @@
 import { ShapeGeometry } from '../../../../src/geometries/ShapeGeometry.js';
 
 import { Shape } from '../../../../src/extras/core/Shape.js';
+import { BufferGeometry } from '../../../../src/core/BufferGeometry.js';
+// import { runStdGeometryTests } from '../../utils/qunit-utils.js';
 
 export default QUnit.module( 'Geometries', () => {
 
 	QUnit.module( 'ShapeGeometry', ( hooks ) => {
 
-		var geometries = undefined; // eslint-disable-line no-unused-vars
+		let geometries = undefined; // eslint-disable-line no-unused-vars
 		hooks.beforeEach( function () {
 
-			var triangleShape = new Shape();
+			const triangleShape = new Shape();
 			triangleShape.moveTo( 0, - 1 );
 			triangleShape.lineTo( 1, 1 );
 			triangleShape.lineTo( - 1, 1 );
@@ -23,14 +25,49 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INHERITANCE
-		QUnit.todo( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( assert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			const object = new ShapeGeometry();
+			assert.strictEqual(
+				object instanceof BufferGeometry, true,
+				'ShapeGeometry extends from BufferGeometry'
+			);
 
 		} );
 
 		// INSTANCING
 		QUnit.todo( 'Instancing', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PROPERTIES
+		QUnit.test( 'type', ( assert ) => {
+
+			const object = new ShapeGeometry();
+			assert.ok(
+				object.type === 'ShapeGeometry',
+				'ShapeGeometry.type should be ShapeGeometry'
+			);
+
+		} );
+
+		QUnit.todo( 'parameters', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
+		QUnit.todo( 'toJSON', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// STATIC
+		QUnit.todo( 'fromJSON', ( assert ) => {
 
 			assert.ok( false, 'everything\'s gonna be alright' );
 
