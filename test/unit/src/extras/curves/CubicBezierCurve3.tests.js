e8825cea5534be65977980208c7f1608fc373507
Ed Preston <epreston@prestonsoft.com>
2023-02-02 04:54:12 +1100

    Update CubicBezierCurve3.tests.js
    CubicBezierCurve3 extends from Curve
    CubicBezierCurve3.type should be CubicBezierCurve3
    CubicBezierCurve3.isCubicBezierCurve3 should be true
    Stub missing member tests.

--- test/unit/src/extras/curves/CubicBezierCurve3.tests.js
+++ test/unit/src/extras/curves/CubicBezierCurve3.tests.js
@@ -1,6 +1,8 @@
 /* global QUnit */
 
 import { CubicBezierCurve3 } from '../../../../../src/extras/curves/CubicBezierCurve3.js';
+
+import { Curve } from '../../../../../src/extras/core/Curve.js';
 import { Vector3 } from '../../../../../src/math/Vector3.js';
 
 export default QUnit.module( 'Extras', () => {
@@ -22,9 +24,13 @@ export default QUnit.module( 'Extras', () => {
 			} );
 
 			// INHERITANCE
-			QUnit.todo( 'Extending', ( assert ) => {
+			QUnit.test( 'Extending', ( assert ) => {
 
-				assert.ok( false, 'everything\'s gonna be alright' );
+				const object = new CubicBezierCurve3();
+				assert.strictEqual(
+					object instanceof Curve, true,
+					'CubicBezierCurve3 extends from Curve'
+				);
 
 			} );
 
@@ -35,14 +41,76 @@ export default QUnit.module( 'Extras', () => {
 
 			} );
 
-			// PUBLIC STUFF
-			QUnit.todo( 'isCubicBezierCurve3', ( assert ) => {
+			// PROPERTIES
+			QUnit.test( 'type', ( assert ) => {
+
+				const object = new CubicBezierCurve3();
+				assert.ok(
+					object.type === 'CubicBezierCurve3',
+					'CubicBezierCurve3.type should be CubicBezierCurve3'
+				);
+
+			} );
+
+			QUnit.todo( 'v0', ( assert ) => {
+
+				// Vector3 exists
+				assert.ok( false, 'everything\'s gonna be alright' );
+
+			} );
+
+			QUnit.todo( 'v1', ( assert ) => {
+
+				// Vector3 exists
+				assert.ok( false, 'everything\'s gonna be alright' );
+
+			} );
+
+			QUnit.todo( 'v2', ( assert ) => {
+
+				// Vector3 exists
+				assert.ok( false, 'everything\'s gonna be alright' );
+
+			} );
+
+			QUnit.todo( 'v3', ( assert ) => {
+
+				// Vector3 exists
+				assert.ok( false, 'everything\'s gonna be alright' );
+
+			} );
+
+			// PUBLIC
+			QUnit.test( 'isCubicBezierCurve3', ( assert ) => {
+
+				const object = new CubicBezierCurve3();
+				assert.ok(
+					object.isCubicBezierCurve3,
+					'CubicBezierCurve3.isCubicBezierCurve3 should be true'
+				);
+
+			} );
+
+			QUnit.todo( 'getPoint', ( assert ) => {
+
+				// getPoint( t, optionalTarget = new Vector3() )
+				assert.ok( false, 'everything\'s gonna be alright' );
+
+			} );
+
+			QUnit.todo( 'copy', ( assert ) => {
 
 				assert.ok( false, 'everything\'s gonna be alright' );
 
 			} );
 
-			QUnit.todo( 'getPoint', ( assert ) => {
+			QUnit.todo( 'toJSON', ( assert ) => {
+
+				assert.ok( false, 'everything\'s gonna be alright' );
+
+			} );
+
+			QUnit.todo( 'fromJSON', ( assert ) => {
 
 				assert.ok( false, 'everything\'s gonna be alright' );
 
