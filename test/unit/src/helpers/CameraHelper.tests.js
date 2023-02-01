8c13a1aff8ac39955a4e82d760536db18f3e6740
Ed Preston <epreston@prestonsoft.com>
2023-02-02 00:51:49 +1100

    Update CameraHelper.tests.js
    CameraHelper extends from LineSegments.
    CameraHelper.type should be CameraHelper.
    Stub missing member tests.

--- test/unit/src/helpers/CameraHelper.tests.js
+++ test/unit/src/helpers/CameraHelper.tests.js
@@ -1,15 +1,23 @@
 /* global QUnit */
 
-// import { CameraHelper } from '../../../../src/helpers/CameraHelper.js';
+import { CameraHelper } from '../../../../src/helpers/CameraHelper.js';
+
+import { LineSegments } from '../../../../src/objects/LineSegments.js';
+import { PerspectiveCamera } from '../../../../src/cameras/PerspectiveCamera.js';
 
 export default QUnit.module( 'Helpers', () => {
 
 	QUnit.module( 'CameraHelper', () => {
 
 		// INHERITANCE
-		QUnit.todo( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( assert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			const camera = new PerspectiveCamera();
+			const object = new CameraHelper( camera );
+			assert.strictEqual(
+				object instanceof LineSegments, true,
+				'CameraHelper extends from LineSegments'
+			);
 
 		} );
 
@@ -20,13 +28,60 @@ export default QUnit.module( 'Helpers', () => {
 
 		} );
 
-		// PUBLIC STUFF
+		// PROPERTIES
+		QUnit.test( 'type', ( assert ) => {
+
+			const camera = new PerspectiveCamera();
+			const object = new CameraHelper( camera );
+			assert.ok(
+				object.type === 'CameraHelper',
+				'CameraHelper.type should be CameraHelper'
+			);
+
+		} );
+
+		QUnit.todo( 'camera', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'matrix', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'matrixAutoUpdate', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'pointMap', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
+		QUnit.todo( 'setColors', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
 		QUnit.todo( 'update', ( assert ) => {
 
 			assert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
+		QUnit.todo( 'dispose', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
 
 	} );
 
