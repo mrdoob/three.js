6834494f6db7565a8d38fe293e47b4bd20286b2a
Ed Preston <epreston@prestonsoft.com>
2023-02-02 00:56:15 +1100

    Update PointLightHelper.tests.js
    PointLightHelper extends from Mesh.
    PointLightHelper.type should be PointLightHelper.
    Stub missing member tests.

--- test/unit/src/helpers/PointLightHelper.tests.js
+++ test/unit/src/helpers/PointLightHelper.tests.js
@@ -1,15 +1,31 @@
 /* global QUnit */
 
-// import { PointLightHelper } from '../../../../src/helpers/PointLightHelper.js';
+import { PointLightHelper } from '../../../../src/helpers/PointLightHelper.js';
+
+import { Mesh } from '../../../../src/objects/Mesh.js';
+import { PointLight } from '../../../../src/lights/PointLight.js';
 
 export default QUnit.module( 'Helpers', () => {
 
 	QUnit.module( 'PointLightHelper', () => {
 
-		// INHERITANCE
-		QUnit.todo( 'Extending', ( assert ) => {
+		const parameters = {
+			sphereSize: 1,
+			color: 0xaaaaaa,
+			intensity: 0.5,
+			distance: 100,
+			decay: 2
+		};
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+		// INHERITANCE
+		QUnit.test( 'Extending', ( assert ) => {
+
+			const light = new PointLight( parameters.color );
+			const object = new PointLightHelper( light, parameters.sphereSize, parameters.color );
+			assert.strictEqual(
+				object instanceof Mesh, true,
+				'PointLightHelper extends from Mesh'
+			);
 
 		} );
 
@@ -20,7 +36,43 @@ export default QUnit.module( 'Helpers', () => {
 
 		} );
 
-		// PUBLIC STUFF
+		// PROPERTIES
+		QUnit.test( 'type', ( assert ) => {
+
+			const light = new PointLight( parameters.color );
+			const object = new PointLightHelper( light, parameters.sphereSize, parameters.color );
+			assert.ok(
+				object.type === 'PointLightHelper',
+				'PointLightHelper.type should be PointLightHelper'
+			);
+
+		} );
+
+		QUnit.todo( 'light', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'color', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'matrix', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'matrixAutoUpdate', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
 		QUnit.todo( 'dispose', ( assert ) => {
 
 			assert.ok( false, 'everything\'s gonna be alright' );
