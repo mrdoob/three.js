2e54d9fb608747421448d01c90794c7560ce9b78
Ed Preston <epreston@prestonsoft.com>
2023-02-02 00:50:21 +1100

    Update Box3Helper.tests.js
    Box3Helper extends from LineSegments.
    Box3Helper.type should be Box3Helper.
    Stub missing member tests.

--- test/unit/src/helpers/Box3Helper.tests.js
+++ test/unit/src/helpers/Box3Helper.tests.js
@@ -1,15 +1,21 @@
 /* global QUnit */
 
-// import { Box3Helper } from '../../../../src/helpers/Box3Helper.js';
+import { Box3Helper } from '../../../../src/helpers/Box3Helper.js';
+
+import { LineSegments } from '../../../../src/objects/LineSegments.js';
 
 export default QUnit.module( 'Helpers', () => {
 
 	QUnit.module( 'Box3Helper', () => {
 
 		// INHERITANCE
-		QUnit.todo( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( assert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			const object = new Box3Helper();
+			assert.strictEqual(
+				object instanceof LineSegments, true,
+				'Box3Helper extends from LineSegments'
+			);
 
 		} );
 
@@ -20,8 +26,31 @@ export default QUnit.module( 'Helpers', () => {
 
 		} );
 
-		// PUBLIC STUFF
+		// PROPERTIES
+		QUnit.test( 'type', ( assert ) => {
+
+			const object = new Box3Helper();
+			assert.ok(
+				object.type === 'Box3Helper',
+				'Box3Helper.type should be Box3Helper'
+			);
+
+		} );
+
+		QUnit.todo( 'box', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// PUBLIC
 		QUnit.todo( 'updateMatrixWorld', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'dispose', ( assert ) => {
 
 			assert.ok( false, 'everything\'s gonna be alright' );
 
