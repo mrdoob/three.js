73bb262b20c6c1b60968e498d65c9178dd973b53
Ed Preston <epreston@prestonsoft.com>
2023-02-03 12:49:29 +1100

    Update Cylindrical.tests.js
    Explicit block scope for variables used in tests.

--- test/unit/src/math/Cylindrical.tests.js
+++ test/unit/src/math/Cylindrical.tests.js
@@ -11,16 +11,16 @@ export default QUnit.module( 'Maths', () => {
 		// INSTANCING
 		QUnit.test( 'Instancing', ( assert ) => {
 
-			var a = new Cylindrical();
-			var radius = 10.0;
-			var theta = Math.PI;
-			var y = 5;
+			let a = new Cylindrical();
+			const radius = 10.0;
+			const theta = Math.PI;
+			const y = 5;
 
 			assert.strictEqual( a.radius, 1.0, 'Default values: check radius' );
 			assert.strictEqual( a.theta, 0, 'Default values: check theta' );
 			assert.strictEqual( a.y, 0, 'Default values: check y' );
 
-			var a = new Cylindrical( radius, theta, y );
+			a = new Cylindrical( radius, theta, y );
 			assert.strictEqual( a.radius, radius, 'Custom values: check radius' );
 			assert.strictEqual( a.theta, theta, 'Custom values: check theta' );
 			assert.strictEqual( a.y, y, 'Custom values: check y' );
@@ -30,10 +30,10 @@ export default QUnit.module( 'Maths', () => {
 		// PUBLIC
 		QUnit.test( 'set', ( assert ) => {
 
-			var a = new Cylindrical();
-			var radius = 10.0;
-			var theta = Math.PI;
-			var y = 5;
+			const a = new Cylindrical();
+			const radius = 10.0;
+			const theta = Math.PI;
+			const y = 5;
 
 			a.set( radius, theta, y );
 			assert.strictEqual( a.radius, radius, 'Check radius' );
@@ -44,11 +44,11 @@ export default QUnit.module( 'Maths', () => {
 
 		QUnit.test( 'clone', ( assert ) => {
 
-			var radius = 10.0;
-			var theta = Math.PI;
-			var y = 5;
-			var a = new Cylindrical( radius, theta, y );
-			var b = a.clone();
+			const radius = 10.0;
+			const theta = Math.PI;
+			const y = 5;
+			const a = new Cylindrical( radius, theta, y );
+			const b = a.clone();
 
 			assert.propEqual( a, b, 'Check a and b are equal after clone()' );
 
@@ -59,11 +59,11 @@ export default QUnit.module( 'Maths', () => {
 
 		QUnit.test( 'copy', ( assert ) => {
 
-			var radius = 10.0;
-			var theta = Math.PI;
-			var y = 5;
-			var a = new Cylindrical( radius, theta, y );
-			var b = new Cylindrical().copy( a );
+			const radius = 10.0;
+			const theta = Math.PI;
+			const y = 5;
+			const a = new Cylindrical( radius, theta, y );
+			const b = new Cylindrical().copy( a );
 
 			assert.propEqual( a, b, 'Check a and b are equal after copy()' );
 
@@ -74,10 +74,10 @@ export default QUnit.module( 'Maths', () => {
 
 		QUnit.test( 'setFromVector3', ( assert ) => {
 
-			var a = new Cylindrical( 1, 1, 1 );
-			var b = new Vector3( 0, 0, 0 );
-			var c = new Vector3( 3, - 1, - 3 );
-			var expected = new Cylindrical( Math.sqrt( 9 + 9 ), Math.atan2( 3, - 3 ), - 1 );
+			const a = new Cylindrical( 1, 1, 1 );
+			const b = new Vector3( 0, 0, 0 );
+			const c = new Vector3( 3, - 1, - 3 );
+			const expected = new Cylindrical( Math.sqrt( 9 + 9 ), Math.atan2( 3, - 3 ), - 1 );
 
 			a.setFromVector3( b );
 			assert.strictEqual( a.radius, 0, 'Zero-length vector: check radius' );
