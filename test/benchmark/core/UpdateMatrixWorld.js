da2a0391b6d11f6038972035b674b25813ae046c
Ed Preston <epreston@prestonsoft.com>
2023-02-05 21:02:51 +1100

    Update UpdateMatrixWorld.js
    Explicit block scope for variables used in tests.

--- test/benchmark/core/UpdateMatrixWorld.js
+++ test/benchmark/core/UpdateMatrixWorld.js
@@ -2,13 +2,14 @@
 
 	THREE = Bench.THREE;
 
-	var position = new THREE.Vector3( 1, 1, 1 );
-	var scale = new THREE.Vector3( 2, 1, 0.5 );
-	var quaternion = new THREE.Quaternion();
+	const position = new THREE.Vector3( 1, 1, 1 );
+	const scale = new THREE.Vector3( 2, 1, 0.5 );
+	const quaternion = new THREE.Quaternion();
 	quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 8 );
-	var createLocallyOffsetChild = function () {
 
-		var child = new THREE.Object3D();
+	const createLocallyOffsetChild = function () {
+
+		const child = new THREE.Object3D();
 		child.position.copy( position );
 		child.scale.copy( scale );
 		child.quaternion.copy( quaternion );
@@ -17,13 +18,13 @@
 
 	};
 
-	var generateSceneGraph = function ( root, depth, breadth, initObject ) {
+	const generateSceneGraph = function ( root, depth, breadth, initObject ) {
 
 		if ( depth > 0 ) {
 
-			for ( var i = 0; i < breadth; i ++ ) {
+			for ( let i = 0; i < breadth; i ++ ) {
 
-				var child = initObject();
+				const child = initObject();
 				root.add( child );
 				generateSceneGraph( child, depth - 1, breadth, initObject );
 
@@ -35,7 +36,7 @@
 
 	};
 
-	var nodeCount = function ( root ) {
+	const nodeCount = function ( root ) {
 
 		return root.children.reduce( function ( acc, x ) {
 
@@ -45,22 +46,24 @@
 
 	};
 
-	var rootA = generateSceneGraph( new THREE.Object3D(), 100, 1, createLocallyOffsetChild );
-	var rootB = generateSceneGraph( new THREE.Object3D(), 3, 10, createLocallyOffsetChild );
-	var rootC = generateSceneGraph( new THREE.Object3D(), 9, 3, createLocallyOffsetChild );
+	const rootA = generateSceneGraph( new THREE.Object3D(), 100, 1, createLocallyOffsetChild );
+	const rootB = generateSceneGraph( new THREE.Object3D(), 3, 10, createLocallyOffsetChild );
+	const rootC = generateSceneGraph( new THREE.Object3D(), 9, 3, createLocallyOffsetChild );
 
-	var s = Bench.newSuite( 'Update world transforms' );
+	const s = Bench.newSuite( 'Update world transforms' );
 
 	s.add( 'Update graph depth=100, breadth=1 (' + nodeCount( rootA ) + ' nodes)', function () {
 
 		rootA.updateMatrixWorld( true );
 
 	} );
+
 	s.add( 'Update graph depth=3, breadth=10 (' + nodeCount( rootB ) + ' nodes)', function () {
 
 		rootB.updateMatrixWorld( true );
 
 	} );
+
 	s.add( 'Update graph depth=9, breadth=3 (' + nodeCount( rootC ) + ' nodes)', function () {
 
 		rootC.updateMatrixWorld( true );
