3b0bdfb99bdde97de8a080f6cd7c87d87f2c881f
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-02-06 23:31:07 +1100

    Puppeteer: Fix text-rendering differences

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -55,20 +55,6 @@ const exceptionList = [
 
 	'webgl_worker_offscreencanvas', // in a worker, not robust
 
-	// Windows-Linux text rendering differences
-	// TODO: Fix these by setting a font in Puppeteer -- this can also fix a bunch of 0.1%-0.2% examples
-	'css3d_periodictable',
-	'misc_controls_pointerlock',
-	'misc_uv_tests',
-	'webgl_camera_logarithmicdepthbuffer',
-	'webgl_effects_ascii',
-	'webgl_loader_pdb',
-	'webgl_multiple_canvases_circle',
-	'webgl_multiple_elements_text',
-	'webgl_nodes_playground',
-	'webgl_shaders_tonemapping',
-	'webgpu_nodes_playground',
-
 	// Unknown
 	// TODO: most of these can be fixed just by increasing idleTime and parseTime
 	'webgl_animation_skinning_blending',
@@ -194,7 +180,7 @@ async function main() {
 
 	/* Launch browser */
 
-	const flags = [ '--hide-scrollbars', '--enable-unsafe-webgpu' ];
+	const flags = [ '--font-render-hinting=none', '--hide-scrollbars', '--enable-unsafe-webgpu' ];
 	flags.push( '--enable-features=Vulkan', '--use-gl=swiftshader', '--use-angle=swiftshader', '--use-vulkan=swiftshader', '--use-webgpu-adapter=swiftshader' );
 	// if ( process.platform === 'linux' ) flags.push( '--enable-features=Vulkan,UseSkiaRenderer', '--use-vulkan=native', '--disable-vulkan-surface', '--disable-features=VaapiVideoDecoder', '--ignore-gpu-blocklist', '--use-angle=vulkan' );
 
