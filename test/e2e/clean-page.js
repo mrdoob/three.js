52636a5038854ea34459309a58eac8bd605d6b53
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-02-06 23:48:24 +1100

    Some tests

--- test/e2e/clean-page.js
+++ test/e2e/clean-page.js
@@ -9,7 +9,7 @@
 
 	const style = document.createElement( 'style' );
 	style.type = 'text/css';
-	style.innerHTML = '#info, button, input, body > div.lil-gui, body > div.lbl { display: none !important; }';
+	style.innerHTML = '* { -webkit-font-smoothing: antialiased; } #info, button, input, body > div.lil-gui, body > div.lbl { display: none !important; }';
 
 	document.querySelector( 'head' ).appendChild( style );
 
