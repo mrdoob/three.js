5cf774a669d61fba222a71b1433626fc1a0b343a
Ed Preston <epreston@prestonsoft.com>
2023-02-09 00:04:27 +1100

    Update Fog.tests.js
    Can instantiate a Fog.
    Can instantiate a Fog with color.
    Can instantiate a Fog with color, near, far.

--- test/unit/src/scenes/Fog.tests.js
+++ test/unit/src/scenes/Fog.tests.js
@@ -7,9 +7,21 @@ export default QUnit.module( 'Scenes', () => {
 	QUnit.module( 'Fog', () => {
 
 		// INSTANCING
-		QUnit.todo( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( assert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			// Fog( color, near = 1, far = 1000 )
+
+			// no params
+			const object = new Fog();
+			assert.ok( object, 'Can instantiate a Fog.' );
+
+			// color
+			const object_color = new Fog( 0xffffff );
+			assert.ok( object_color, 'Can instantiate a Fog with color.' );
+
+			// color, near, far
+			const object_all = new Fog( 0xffffff, 0.015, 100 );
+			assert.ok( object_all, 'Can instantiate a Fog with color, near, far.' );
 
 		} );
 
