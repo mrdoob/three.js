5d3b38e0b60d22cf6c1033998041418d8b8e748c
Simon <simonhdev@gmail.com>
2023-02-18 12:10:48 -0500

    Fix some formatting issues.

--- test/unit/src/core/BufferAttribute.tests.js
+++ test/unit/src/core/BufferAttribute.tests.js
@@ -499,7 +499,7 @@ export default QUnit.module( 'Core', () => {
 		const toHalfFloatArray = ( f32Array ) => {
 			const f16Array = new Uint16Array( f32Array.length );
 			for ( let i = 0, n = f32Array.length; i < n; ++i ) {
-				f16Array[i] = toHalfFloat( f32Array[i] );
+				f16Array[ i ] = toHalfFloat( f32Array[ i ] );
 			}
 			return f16Array;
 		};
@@ -507,7 +507,7 @@ export default QUnit.module( 'Core', () => {
 		const fromHalfFloatArray = ( f16Array ) => {
 			const f32Array = new Float32Array( f16Array.length );
 			for ( let i = 0, n = f16Array.length; i < n; ++i ) {
-				f32Array[i] = fromHalfFloat( f16Array[i] );
+				f32Array[ i ] = fromHalfFloat( f16Array[ i ] );
 			}
 			return f32Array;
 		};
--- test/unit/src/core/BufferGeometry.tests.js
+++ test/unit/src/core/BufferGeometry.tests.js
@@ -464,7 +464,7 @@ export default QUnit.module( 'Core', () => {
 		const toHalfFloatArray = ( f32Array ) => {
 			const f16Array = new Uint16Array( f32Array.length );
 			for ( let i = 0, n = f32Array.length; i < n; ++i ) {
-				f16Array[i] = toHalfFloat( f32Array[i] );
+				f16Array[ i ] = toHalfFloat( f32Array[ i ] );
 			}
 			return f16Array;
 		};
