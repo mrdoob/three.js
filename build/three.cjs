9b274fef1d2b9ba0fc47410d06593e1f34f4df0e
Mr.doob <info@mrdoob.com>
2023-02-23 15:41:09 +0900

    r150

--- build/three.cjs
+++ build/three.cjs
@@ -5,7 +5,7 @@
  */
 'use strict';
 
-const REVISION = '150dev';
+const REVISION = '150';
 const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
 const CullFaceNone = 0;
--- build/three.module.js
+++ build/three.module.js
@@ -3,7 +3,7 @@
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
  */
-const REVISION = '150dev';
+const REVISION = '150';
 const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
 const CullFaceNone = 0;
