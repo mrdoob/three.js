8ed069d8451db167a9ddc5d03ee870caada86265
Johnathon Selstad <makeshifted@gmail.com>
2023-02-24 12:43:59 -0800

    Finish up demo

--- examples/webgl_materials_reprojection_script.js
+++ examples/webgl_materials_reprojection_script.js
@@ -8,9 +8,9 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import Stats from 'three/addons/libs/stats.module.js';
 
 let camera, scene, renderer, stats;
-let cube, display, material;
+let cube, curvedPanel, material;
 
-let eyeCamera, eyeRenderTarget, eyeCameraHelper, params = { frameCorners: false};
+let eyeCamera, eyeRenderTarget, eyeCameraHelper, params = { setFoVToCorners: false, panelCurvature: 20 };
 
 let controls;
 
@@ -18,74 +18,110 @@ init();
 
 function init() {
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setAnimationLoop(animation);
 	renderer.outputEncoding = THREE.sRGBEncoding;
 	renderer.toneMapping = THREE.ACESFilmicToneMapping;
-	document.body.appendChild( renderer.domElement );
+	document.body.appendChild(renderer.domElement);
 
-	window.addEventListener( 'resize', onWindowResized );
+	window.addEventListener('resize', onWindowResized);
 
 	stats = new Stats();
-	document.body.appendChild( stats.dom );
+	document.body.appendChild(stats.dom);
 
 	scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.x = 60;
-    camera.position.y = 20;
-    camera.position.z = 30;
-    scene.add( camera );
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.x = 60;
+	camera.position.y = 20;
+	camera.position.z = 30;
+	scene.add(camera);
 
 	new RGBELoader()
-		.setPath( 'textures/equirectangular/' )
-		.load( 'quarry_01_1k.hdr', function ( texture ) {
+		.setPath('textures/equirectangular/')
+		.load('quarry_01_1k.hdr', function (texture) {
 			texture.mapping = THREE.EquirectangularReflectionMapping;
 			scene.background = texture;
 			scene.environment = texture;
-		} );
+		});
 
 	//
 
-	eyeRenderTarget = new THREE.WebGLRenderTarget( 256, 256 );
-    eyeRenderTarget.texture.type = THREE.HalfFloatType;
-    eyeRenderTarget.texture.encoding = renderer.outputEncoding;
+	eyeRenderTarget = new THREE.WebGLRenderTarget(1024, 576);
+	eyeRenderTarget.texture.type = THREE.HalfFloatType;
+	eyeRenderTarget.texture.encoding = renderer.outputEncoding;
 
-	eyeCamera = new THREE.PerspectiveCamera( 30, 16.0/9.0, 1, 100 );
+	eyeCamera = new THREE.PerspectiveCamera(30, 16.0 / 9.0, 1, 100);
 	eyeCamera.position.x = 0.0;
 	eyeCamera.position.y = 0.0;
-    eyeCamera.position.z = 30;
-    scene.add( eyeCamera );
-	eyeCameraHelper = new THREE.CameraHelper( eyeCamera );
-	scene.add( eyeCameraHelper );
+	eyeCamera.position.z = 30;
+	scene.add(eyeCamera);
+	eyeCameraHelper = new THREE.CameraHelper(eyeCamera);
+	scene.add(eyeCameraHelper);
 
 	//
 
-	material = new THREE.MeshBasicMaterial( {
-		map: eyeRenderTarget.texture,
-		//roughness: 0.05,
-		//metalness: 1
-	} );
+	material = new THREE.MeshBasicMaterial({ map: eyeRenderTarget.texture });
+	material.uniforms = {
+		eyeProjectionMatrix : { value: eyeCamera.projectionMatrix },
+		eyeViewMatrix       : { value: eyeCamera.matrixWorldInverse },
+		renderToScreen        : { value: false }
+	};
+
+	material.onBeforeCompile = (shader) => {
+
+		// Vertex Shader: Set UVs to be eye-camera screen-space; Map positions to be main screen-space.
+		shader.vertexShader =
+			shader.vertexShader.slice( 0, 18 ) +
+			`uniform mat4 eyeProjectionMatrix; uniform mat4 eyeViewMatrix; uniform bool renderToScreen; \n` +
+			shader.vertexShader.slice( 18, - 1 ) +
+			`	vec4 modelViewLocal = ( modelMatrix * eyeViewMatrix ) * vec4( position.xyz, 1.0 );
+				vec4    screenLocal = eyeProjectionMatrix * modelViewLocal;
+				vUv                 = ( ( screenLocal.xy / screenLocal.w ) + 1.0 ) * 0.5;
+				if ( renderToScreen ) { gl_Position = vec4( ( uv - 0.5 ) * 2.0, 0.0, 1.0 ); } }`;
+
+		// Fragment Shader: Set Colors outside the Eye Frustum to Black
+		shader.fragmentShader =
+			shader.fragmentShader.slice( 0, - 1 ) +
+			`\nbool outsideUV =  max(vUv.x, vUv.y) > 1.0 || min(vUv.x, vUv.y) < 0.0;
+			gl_FragColor.rgb = outsideUV ? vec3(0.0, 0.0, 0.0) : gl_FragColor.rgb;
+		}`;
+
+		// Set the eye camera's view and projection matrices
+		shader.uniforms.eyeViewMatrix       = material.uniforms.eyeViewMatrix;
+		shader.uniforms.eyeProjectionMatrix = material.uniforms.eyeProjectionMatrix;
+		shader.uniforms.renderToScreen        = material.uniforms.renderToScreen;
+
+	};
+
+	let curvedPanelGeometry = new THREE.PlaneGeometry(50, 20, 16, 16);
+	let vertices = curvedPanelGeometry.getAttribute("position").array;
+	let tempVec = new THREE.Vector3();
+	let radius = tempVec.set(-25, 0, params.panelCurvature).length();
+	for (let i = 0; i < vertices.length; i += 3) {
+		tempVec.set(vertices[i], 0, -params.panelCurvature).setLength(radius);
+		vertices[i] = tempVec.x; vertices[i + 2] = tempVec.z + params.panelCurvature;
+	}
+	curvedPanel = new THREE.Mesh(curvedPanelGeometry, material);
+	scene.add(curvedPanel);
 
 	const gui = new GUI();
-	gui.add( eyeCamera, 'fov', 0, 179 );
-	gui.add( params, 'frameCorners', false );
-	gui.add( renderer, 'toneMappingExposure', 0, 2 ).name( 'exposure' );
-
-    let displayGeometry = new THREE.PlaneGeometry(50, 20, 16, 16);
-    let vertices = displayGeometry.getAttribute("position").array;
-    let tempVec = new THREE.Vector3(); let circleCenterZ = 20;
-    let radius = tempVec.set(-25, 0, circleCenterZ).length();
-    for (let i = 0; i < vertices.length; i += 3){
-        tempVec.set(vertices[i], 0, -circleCenterZ).setLength(radius);
-        vertices[i    ] = tempVec.x;
-        vertices[i + 2] = tempVec.z + circleCenterZ;
-    }
-	display = new THREE.Mesh( displayGeometry, material );
-    
-	scene.add( display );
+	gui.add(renderer, 'toneMappingExposure', 0, 2).name('exposure');
+	gui.add(eyeCamera, 'fov', 0, 60);
+    gui.add(params, 'panelCurvature', 10, 100).onChange(() => {
+        let newCurvedPanelGeometry = new THREE.PlaneGeometry(50, 20, 16, 16);
+        let newVertices = newCurvedPanelGeometry.getAttribute("position").array;
+        let newRadius = tempVec.set(-25, 0, params.panelCurvature).length();
+        for (let i = 0; i < newVertices.length; i += 3) {
+            tempVec.set(newVertices[i], 0, -params.panelCurvature).setLength(newRadius);
+            newVertices[i] = tempVec.x; newVertices[i + 2] = tempVec.z + params.panelCurvature;
+        }
+        curvedPanel.geometry = newCurvedPanelGeometry;
+    });
+	gui.add(params, 'setFoVToCorners', false);
+    gui.add(material.uniforms.renderToScreen, 'value', false).name('renderToScreen');
 
 	const material2 = new THREE.MeshStandardMaterial( {
 		roughness: 0.1,
@@ -101,8 +137,6 @@ function init() {
 	//
 
 	controls = new OrbitControls( camera, renderer.domElement );
-	//controls.autoRotate = true;
-
 }
 
 function onWindowResized() {
@@ -118,8 +152,9 @@ function animation( msTime ) {
 
 	const time = msTime / 1000;
 
+	// move the eye's perspective around to make it interesting
 	eyeCamera.position.x = Math.cos( time * 0.5 ) * 30;
-	//eyeCamera.position.y = Math.sin( time ) * 30;
+	eyeCamera.position.y = Math.sin( time * 0.25 ) * 5;
 	eyeCamera.position.z = 30 + Math.sin( time ) * 20;
 	eyeCamera.lookAt( cube.position );
 
@@ -130,25 +165,31 @@ function animation( msTime ) {
 	renderer.xr.enabled = false; // Avoid camera modification
 	renderer.shadowMap.autoUpdate = false; // Avoid re-computing shadows
 
-	// render the eye's perspective
-    if (params.frameCorners) {
-        CameraUtils.frameCorners(eyeCamera,
-            new THREE.Vector3(-25, -10, 0),
-            new THREE.Vector3( 25, -10, 0),
-            new THREE.Vector3(-25,  10, 0), false);
-    } else {
-        eyeCamera.updateProjectionMatrix();
-    }
+	// set the eye camera's frustum
+	if (params.setFoVToCorners) {
+		// create an asymmetric/off-axis frustum that aligns to the corners of the curvedPanel
+		CameraUtils.frameCorners(eyeCamera,
+			new THREE.Vector3(-25, -10, 0),
+			new THREE.Vector3( 25, -10, 0),
+			new THREE.Vector3(-25,  10, 0), false);
+	} else {
+		// update the FoV of the eye camera
+		eyeCamera.updateProjectionMatrix();
+	}
+
+	// set up rendering from the eye's camera
 	renderer.setRenderTarget( eyeRenderTarget );
 	renderer.state.buffers.depth.setMask( true ); // make sure the depth buffer is writable so it can be properly cleared, see #18897
 	if ( renderer.autoClear === false ) renderer.clear();
-	display.visible = false; // hide the display from from the eye's camera
+	curvedPanel    .visible = false; // hide the curvedPanel from from the eye's camera
 	eyeCameraHelper.visible = false;
+
+	// render the eye's perspective to eyeRenderTarget
 	renderer.render( scene, eyeCamera );
-	display.visible = true; // re-enable this display's visibility for debug rendering
-	eyeCameraHelper.visible = true;
 
 	// restore the original rendering properties
+	curvedPanel    .visible = true;
+	eyeCameraHelper.visible = true;
 	renderer.xr.enabled = currentXrEnabled;
 	renderer.shadowMap.autoUpdate = currentShadowAutoUpdate;
 	renderer.setRenderTarget( currentRenderTarget );
