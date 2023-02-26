91737946c3f2d3277b839d71e643b3021d15a4d6
Ed Preston <epreston@prestonsoft.com>
2023-02-26 14:22:12 +1100

    Fill in basic instancing tests
    Fill in basic instancing tests
    Fill in some inheritance tests where required.

--- test/unit/src/animation/tracks/BooleanKeyframeTrack.tests.js
+++ test/unit/src/animation/tracks/BooleanKeyframeTrack.tests.js
@@ -10,10 +10,17 @@ export default QUnit.module( 'Animation', () => {
 
 		QUnit.module( 'BooleanKeyframeTrack', () => {
 
+			const parameters = {
+				name: '.visible',
+				times: [ 0, 1 ],
+				values: [ true, false ],
+				interpolation: BooleanKeyframeTrack.DefaultInterpolation
+			};
+
 			// INHERITANCE
 			QUnit.test( 'Extending', ( assert ) => {
 
-				const object = new BooleanKeyframeTrack( '.visible', [ 0, 1 ], [ true, false ] );
+				const object = new BooleanKeyframeTrack( parameters.name, parameters.times, parameters.values );
 				assert.strictEqual(
 					object instanceof KeyframeTrack, true,
 					'BooleanKeyframeTrack extends from KeyframeTrack'
@@ -22,9 +29,15 @@ export default QUnit.module( 'Animation', () => {
 			} );
 
 			// INSTANCING
-			QUnit.todo( 'Instancing', ( assert ) => {
+			QUnit.test( 'Instancing', ( assert ) => {
 
-				assert.ok( false, 'everything\'s gonna be alright' );
+				// name, times, values
+				const object = new BooleanKeyframeTrack( parameters.name, parameters.times, parameters.values );
+				assert.ok( object, 'Can instantiate a BooleanKeyframeTrack.' );
+
+				// name, times, values, interpolation
+				const object_all = new BooleanKeyframeTrack( parameters.name, parameters.times, parameters.values, parameters.interpolation );
+				assert.ok( object_all, 'Can instantiate a BooleanKeyframeTrack with name, times, values, interpolation.' );
 
 			} );
 
