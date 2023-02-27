9b396fad4bc5e21f30014fa16ba123d3d5bf8c77
simondevyoutube <57015305+simondevyoutube@users.noreply.github.com>
2023-02-27 04:15:18 -0500

    UniformsUtils: Skip cloning textures of render targets and report warning. (#25560)
    * .
    
    .
    
    * .
    
    * .
    
    * Update UniformsUtils.js
    
    * Update UniformsUtils.tests.js
    
    * Update UniformsUtils.tests.js
    
    * Update UniformsUtils.tests.js
    
    * Update UniformsLib.html
    
    ---------
    
    Co-authored-by: Michael Herzog <michael.herzog@human-interactive.org>

--- src/renderers/shaders/UniformsUtils.js
+++ src/renderers/shaders/UniformsUtils.js
@@ -21,7 +21,16 @@ export function cloneUniforms( src ) {
 				property.isVector2 || property.isVector3 || property.isVector4 ||
 				property.isTexture || property.isQuaternion ) ) {
 
-				dst[ u ][ p ] = property.clone();
+				if ( property.isRenderTargetTexture ) {
+
+					console.warn( 'UniformsUtils: Textures of render targets cannot be cloned via cloneUniforms() or mergeUniforms().' );
+					dst[ u ][ p ] = null;
+
+				} else {
+
+					dst[ u ][ p ] = property.clone();
+
+				}
 
 			} else if ( Array.isArray( property ) ) {
 
