48401b2cabb8ceae3860a802388aa93a3d1ee419
Marco Fugaro <marco.fugaro@gmail.com>
2023-03-06 10:46:36 +0100

    Remove pretty-bytes dependency

--- test/treeshake/utils/format-size.js
+++ test/treeshake/utils/format-size.js
@@ -1,7 +1,20 @@
 // used in report-size.yml
-import prettyBytes from 'pretty-bytes';
+
+export function formatBytes( bytes, decimals = 1 ) {
+
+	if ( bytes === 0 ) return '0 B';
+
+	const k = 1000;
+	const dm = decimals < 0 ? 0 : decimals;
+	const sizes = [ 'B', 'kB', 'MB', 'GB' ];
+
+	const i = Math.floor( Math.log( bytes ) / Math.log( k ) );
+
+	return parseFloat( ( bytes / Math.pow( k, i ) ).toFixed( dm ) ) + ' ' + sizes[ i ];
+
+}
 
 const n = Number( process.argv[ 2 ] );
-const formatted = prettyBytes( n, { minimumFractionDigits: 0, maximumFractionDigits: 1 } );
+const formatted = formatBytes( n );
 
 console.log( formatted );
