766c0bbd2a1d2db5f0e7e89ac6c4b894c4a115fb
Marco Fugaro <marco.fugaro@gmail.com>
2023-03-08 11:08:48 +0100

    Fix order

--- test/treeshake/utils/format-diff.js
+++ test/treeshake/utils/format-diff.js
@@ -4,7 +4,7 @@ import { formatBytes } from './formatBytes.js';
 const filesize = Number( process.argv[ 2 ] );
 const filesizeBase = Number( process.argv[ 3 ] );
 
-const diff = filesizeBase - filesize;
+const diff = filesize - filesizeBase;
 const formatted = `${diff >= 0 ? '+' : '-'}${formatBytes( Math.abs( diff ), 2 )}`;
 
 console.log( formatted );
