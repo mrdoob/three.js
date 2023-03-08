5d558d9bb8a0d1c8832e9595f1c703dbccf7fff3
Marco Fugaro <marco.fugaro@gmail.com>
2023-03-08 11:28:34 +0100

    CI: Show diff in bytes instead of percentage (#25630)
    * CI: Show diff in bytes instead of percentage
    
    * Fix order

--- test/treeshake/utils/format-diff.js
+++ test/treeshake/utils/format-diff.js
@@ -1,10 +1,10 @@
 // used in report-size.yml
+import { formatBytes } from './formatBytes.js';
 
 const filesize = Number( process.argv[ 2 ] );
 const filesizeBase = Number( process.argv[ 3 ] );
 
-const diff = ( filesize - filesizeBase ) * 100 / filesizeBase;
-const diffString = diff.toFixed( 2 ).slice( - 1 ) === '0' ? diff.toFixed( 1 ) : diff.toFixed( 2 );
-const formatted = `${diff >= 0 ? '+' : ''}${diffString}%`;
+const diff = filesize - filesizeBase;
+const formatted = `${diff >= 0 ? '+' : '-'}${formatBytes( Math.abs( diff ), 2 )}`;
 
 console.log( formatted );
