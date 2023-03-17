794bbb02acf005797fd473d25a1e876a916e8579
Michael Herzog <michael.herzog@human-interactive.org>
2023-03-17 11:02:15 +0100

    Object3D: Honor `up` in `toJSON()` and `ObjectLoader`. (#25687)

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -963,6 +963,8 @@ class ObjectLoader extends Loader {
 
 		}
 
+		if ( data.up !== undefined ) object.up.fromArray( data.up );
+
 		if ( data.castShadow !== undefined ) object.castShadow = data.castShadow;
 		if ( data.receiveShadow !== undefined ) object.receiveShadow = data.receiveShadow;
 
