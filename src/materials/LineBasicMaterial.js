487b9182b9519f7159025b0387124061452b169d
Mugen87 <michael.herzog@human-interactive.org>
2023-03-27 11:17:24 +0200

    LineBasicMaterial: Add support for `map`.

--- src/materials/LineBasicMaterial.js
+++ src/materials/LineBasicMaterial.js
@@ -13,6 +13,8 @@ class LineBasicMaterial extends Material {
 
 		this.color = new Color( 0xffffff );
 
+		this.map = null;
+
 		this.linewidth = 1;
 		this.linecap = 'round';
 		this.linejoin = 'round';
@@ -29,6 +31,8 @@ class LineBasicMaterial extends Material {
 		super.copy( source );
 
 		this.color.copy( source.color );
+
+		this.map = source.map;
 
 		this.linewidth = source.linewidth;
 		this.linecap = source.linecap;
