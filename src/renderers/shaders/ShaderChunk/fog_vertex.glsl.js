7dd497ba3f9994bb0598348f96cfdcad188e9070
Elias Hasle <elias.hasle@gmail.com>
2023-03-30 18:06:57 +0200

    Simplify fog_vertex

--- src/renderers/shaders/ShaderChunk/fog_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/fog_vertex.glsl.js
@@ -1,10 +1,8 @@
 export default /* glsl */`
 #ifdef USE_FOG
 
-	vFogPosition = mvPosition.xyz;
-	// Consider only depth for orthographic camera:
-	float projection_is_ortho = projectionMatrix[3][3];
-	vFogPosition.xy -= projection_is_ortho * mvPosition.xy;
+	// Use distance for perspective camera, but only depth for orthographic camera:
+	vFogPosition = vec3( isOrthographic ? vec2(0.) : mvPosition.xy, mvPosition.z );
 
 #endif
 `;
