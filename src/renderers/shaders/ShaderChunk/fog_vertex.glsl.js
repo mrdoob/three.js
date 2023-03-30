651512a1b142b1ec8d02c41f179b1bcb5e553aa8
Elias Hasle <elias.hasle@gmail.com>
2023-03-30 14:41:57 +0200

    Restrict fog depth to projection-parallel distance for Orthographic projection

--- src/renderers/shaders/ShaderChunk/fog_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/fog_vertex.glsl.js
@@ -2,7 +2,9 @@ export default /* glsl */`
 #ifdef USE_FOG
 
 	vFogPosition = mvPosition.xyz;
-
+	// Consider only depth for orthographic camera:
+	float projection_is_ortho = projectionMatrix[3][3];
+	vFogPosition.xy -= projection_is_ortho * mvPosition.xy;
 
 #endif
 `;
