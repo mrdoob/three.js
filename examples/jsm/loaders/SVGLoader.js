b43490260cdf71c71dc5fdec4114e3643c6c5e98
Tim Gates <tim.gates@iress.com>
2023-04-03 11:49:25 +1000

    docs: Fix a few typos (#25753)
    There are small typos in:
    - examples/jsm/loaders/ColladaLoader.js
    - examples/jsm/loaders/SVGLoader.js
    
    Fixes:
    - Should read `width` rather than `witdh`.
    - Should read `process` rather than `procces`.
    
    Signed-off-by: Tim Gates <tim.gates@iress.com>

--- examples/jsm/loaders/SVGLoader.js
+++ examples/jsm/loaders/SVGLoader.js
@@ -2384,7 +2384,7 @@ class SVGLoader extends Loader {
 
 	static pointsToStroke( points, style, arcDivisions, minDistance ) {
 
-		// Generates a stroke with some witdh around the given path.
+		// Generates a stroke with some width around the given path.
 		// The path can be open or closed (last point equals to first point)
 		// Param points: Array of Vector2D (the path). Minimum 2 points.
 		// Param style: Object with SVG properties as returned by SVGLoader.getStrokeStyle(), or SVGLoader.parse() in the path.userData.style object
