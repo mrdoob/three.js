f7daad4b557caa01eb567d71bc5e7244cba16b09
Michael Herzog <michael.herzog@human-interactive.org>
2023-04-05 10:14:40 +0200

    Update WebGLCubeRenderTarget.js

--- src/renderers/WebGLCubeRenderTarget.js
+++ src/renderers/WebGLCubeRenderTarget.js
@@ -1,4 +1,4 @@
-import { BackSide, LinearFilter, LinearMipmapLinearFilter, NoBlending, NoColorSpace, SRGBColorSpace } from '../constants.js';
+import { BackSide, LinearFilter, LinearMipmapLinearFilter, NoBlending, NoColorSpace, SRGBColorSpace, sRGBEncoding } from '../constants.js';
 import { Mesh } from '../objects/Mesh.js';
 import { BoxGeometry } from '../geometries/BoxGeometry.js';
 import { ShaderMaterial } from '../materials/ShaderMaterial.js';
