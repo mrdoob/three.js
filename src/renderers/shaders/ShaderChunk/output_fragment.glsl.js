8ab28c3f4f5a8e8c6b7f6ad78f1c089636e9b9f9
Emmett Lalish <elalish@google.com>
2023-04-13 17:25:20 -0700

    revert premultiply

--- src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
@@ -3,10 +3,9 @@ export default /* glsl */`
 diffuseColor.a = 1.0;
 #endif
 
-// https://github.com/mrdoob/three.js/pull/22425
 #ifdef USE_TRANSMISSION
 diffuseColor.a *= material.transmissionAlpha;
 #endif
 
-gl_FragColor = vec4( outgoingLight * diffuseColor.a, diffuseColor.a );
+gl_FragColor = vec4( outgoingLight, diffuseColor.a );
 `;
