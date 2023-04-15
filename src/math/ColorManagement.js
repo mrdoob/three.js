d67bdd69981af774b66916d94576fdde0910368c
Mugen87 <michael.herzog@human-interactive.org>
2023-04-15 10:29:28 +0200

    Revert edge case check.

--- src/math/ColorManagement.js
+++ src/math/ColorManagement.js
@@ -9,8 +9,6 @@ export function SRGBToLinear( c ) {
 
 export function LinearToSRGB( c ) {
 
-	if ( c === 1 ) return 1;
-
 	return ( c < 0.0031308 ) ? c * 12.92 : 1.055 * ( Math.pow( c, 0.41666 ) ) - 0.055;
 
 }
