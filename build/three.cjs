ab698b33c313b478093001fb3a26ddb9d274642a
Mr.doob <info@mrdoob.com>
2023-04-27 18:32:25 +0900

    r152

--- build/three.cjs
+++ build/three.cjs
@@ -5,7 +5,7 @@
  */
 'use strict';
 
-const REVISION = '152dev';
+const REVISION = '152';
 
 const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
