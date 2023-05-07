7db3899b01c036e596370c1f59f74470934703cf
WestLangley <WestLangley@users.noreply.github.com>
2023-05-06 21:50:30 -0400

    Improve transmission when background is transparent

--- src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/output_fragment.glsl.js
@@ -5,7 +5,7 @@ diffuseColor.a = 1.0;
 
 // https://github.com/mrdoob/three.js/pull/22425
 #ifdef USE_TRANSMISSION
-diffuseColor.a *= material.transmissionAlpha + 0.1;
+	diffuseColor.a *= material.transmissionAlpha;
 #endif
 
 gl_FragColor = vec4( outgoingLight, diffuseColor.a );
