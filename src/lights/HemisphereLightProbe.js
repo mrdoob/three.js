e65b2937bf2bf7a8d6d260709d4de17ef759b4ef
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-05-09 18:01:16 +0930

    Update HemisphereLightProbe.js

--- src/lights/HemisphereLightProbe.js
+++ src/lights/HemisphereLightProbe.js
@@ -10,11 +10,8 @@ class HemisphereLightProbe extends LightProbe {
 
 		this.isHemisphereLightProbe = true;
 
-		const color1 = new Color().set( skyColor );
-		const color2 = new Color().set( groundColor );
-
-		const sky = new Vector3( color1.r, color1.g, color1.b );
-		const ground = new Vector3( color2.r, color2.g, color2.b );
+		const sky = new Vector3().setFromColor( new Color( skyColor ) );
+		const ground = new Vector3().setFromColor( new Color( groundColor ) );
 
 		// without extra factor of PI in the shader, should = 1 / Math.sqrt( Math.PI );
 		const c0 = Math.sqrt( Math.PI );
