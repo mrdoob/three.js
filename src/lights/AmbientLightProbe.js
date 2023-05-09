275ffceb771f694c2ebbb9591171de737d484092
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-05-09 17:54:21 +0930

    Update AmbientLightProbe.js

--- src/lights/AmbientLightProbe.js
+++ src/lights/AmbientLightProbe.js
@@ -9,10 +9,8 @@ class AmbientLightProbe extends LightProbe {
 
 		this.isAmbientLightProbe = true;
 
-		const color1 = new Color().set( color );
-
 		// without extra factor of PI in the shader, would be 2 / Math.sqrt( Math.PI );
-		this.sh.coefficients[ 0 ].set( color1.r, color1.g, color1.b ).multiplyScalar( 2 * Math.sqrt( Math.PI ) );
+		this.sh.coefficients[ 0 ].setFromColor( new Color( color ) ).multiplyScalar( 2 * Math.sqrt( Math.PI ) );
 
 	}
 
