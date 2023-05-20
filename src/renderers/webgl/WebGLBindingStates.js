fc64052d7a989f91529c9a86d3288b3a11217833
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-05-20 09:54:24 -0500

    WebGLBindingStates: remove uint workaround

--- src/renderers/webgl/WebGLBindingStates.js
+++ src/renderers/webgl/WebGLBindingStates.js
@@ -321,11 +321,6 @@ function WebGLBindingStates( gl, extensions, attributes, capabilities ) {
 
 		if ( integer === true ) {
 
-			// Bind unsigned integers as signed
-			if ( type === gl.UNSIGNED_BYTE ) type = gl.BYTE;
-			else if ( type === gl.UNSIGNED_SHORT ) type = gl.SHORT;
-			else if ( type === gl.UNSIGNED_INT ) type = gl.INT;
-
 			gl.vertexAttribIPointer( index, size, type, stride, offset );
 
 		} else {
