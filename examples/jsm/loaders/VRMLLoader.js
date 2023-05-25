6def5f176260013d77f4317f6c9f2eb962d3bf5d
Mugen87 <michael.herzog@human-interactive.org>
2023-05-25 15:20:24 +0200

    VRMLLoader: Fix colors one more time.

--- examples/jsm/loaders/VRMLLoader.js
+++ examples/jsm/loaders/VRMLLoader.js
@@ -919,6 +919,7 @@ class VRMLLoader extends Loader {
 				} else {
 
 					skyMaterial.color.setRGB( skyColor[ 0 ], skyColor[ 1 ], skyColor[ 2 ] );
+					skyMaterial.convertSRGBToLinear();
 
 				}
 
@@ -1224,10 +1225,12 @@ class VRMLLoader extends Loader {
 
 					case 'diffuseColor':
 						materialData.diffuseColor = new Color( fieldValues[ 0 ], fieldValues[ 1 ], fieldValues[ 2 ] );
+						materialData.diffuseColor.convertSRGBToLinear();
 						break;
 
 					case 'emissiveColor':
 						materialData.emissiveColor = new Color( fieldValues[ 0 ], fieldValues[ 1 ], fieldValues[ 2 ] );
+						materialData.emissiveColor.convertSRGBToLinear();
 						break;
 
 					case 'shininess':
@@ -1236,6 +1239,7 @@ class VRMLLoader extends Loader {
 
 					case 'specularColor':
 						materialData.specularColor = new Color( fieldValues[ 0 ], fieldValues[ 1 ], fieldValues[ 2 ] );
+						materialData.specularColor.convertSRGBToLinear();
 						break;
 
 					case 'transparency':
