7f77a64c09e972be0e6ab744b687c810857c4e42
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-06-22 19:43:41 +1000

    Fix

--- src/renderers/webgl/WebGLProgram.js
+++ src/renderers/webgl/WebGLProgram.js
@@ -400,8 +400,6 @@ function generateCubeUVSize( parameters ) {
 
 function constructShaderPrefixes( parameters ) {
 
-	let prefixVertex, prefixFragment;
-
 	const defines = { ...parameters.defines };
 
 	defines.SHADER_TYPE = parameters.shaderType;
