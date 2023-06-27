c1e558c7088bc1fe66a1b7c02de449d794b44a6b
Michael Herzog <michael.herzog@human-interactive.org>
2023-06-27 10:15:04 +0200

    WebGLRenderer: Fix feedback loop in transmission pass for WebGL 2. (#26177)

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1332,7 +1332,7 @@ class WebGLRenderer {
 					generateMipmaps: true,
 					type: extensions.has( 'EXT_color_buffer_half_float' ) ? HalfFloatType : UnsignedByteType,
 					minFilter: LinearMipmapLinearFilter,
-					samples: ( isWebGL2 && antialias === true ) ? 4 : 0
+					samples: ( isWebGL2 ) ? 4 : 0
 				} );
 
 				// debug
