1cbc7f3cf9b6035df69b27579416516ffe290479
aardgoose <angus.sawyer@email.com>
2023-07-11 10:26:08 +0100

    add example to menu

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -126,6 +126,7 @@ const exceptionList = [
 	'webgpu_materials',
 	'webgpu_materials_video',
 	'webgpu_morphtargets',
+	"webgpu_multiple_rendertargets",
 	'webgpu_particles',
 	'webgpu_rtt',
 	'webgpu_sandbox',
