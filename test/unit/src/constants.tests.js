e8daff9661c2fab1028e883024af5dd393a9125c
Michael Herzog <michael.herzog@human-interactive.org>
2023-08-21 15:44:30 +0200

    Remove `SRGB_ALPHA_BPTC_Format`. (#26620)
    * Remove `SRGB_ALPHA_BPTC_Format`.
    
    * Examples: Clean up.
    
    * Tests: Clean up.

--- test/unit/src/constants.tests.js
+++ test/unit/src/constants.tests.js
@@ -135,7 +135,6 @@ export default QUnit.module( 'Constants', () => {
 		assert.equal( Constants.RGBA_ASTC_12x10_Format, 37820, 'Constants.RGBA_ASTC_12x10_Format is equal to 37820' );
 		assert.equal( Constants.RGBA_ASTC_12x12_Format, 37821, 'Constants.RGBA_ASTC_12x12_Format is equal to 37821' );
 		assert.equal( Constants.RGBA_BPTC_Format, 36492, 'Constants.RGBA_BPTC_Format is equal to 36492' );
-		assert.equal( Constants.SRGB_ALPHA_BPTC_Format, 36493, 'Constants.SRGB_ALPHA_BPTC_Format is equal to 36493' );
 		assert.equal( Constants.RGB_BPTC_SIGNED_Format, 36494, 'Constants.RGB_BPTC_SIGNED_Format is equal to 36494' );
 		assert.equal( Constants.RGB_BPTC_UNSIGNED_Format, 36495, 'Constants.RGB_BPTC_UNSIGNED_Format is equal to 36495' );
 		assert.equal( Constants.RED_RGTC1_Format, 36283, 'Constants.RED_RGTC1_Format is equal to 36283' );
