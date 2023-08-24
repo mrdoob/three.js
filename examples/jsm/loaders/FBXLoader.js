ef7882efcfb2736b4237d49d04cbc3c56c64376e
Mindaugas Janulis <Mindaugas.Janulis@cleverdist.com>
2023-08-24 12:23:13 +0200

    Store FBXLoader original (not sanitized) node name in Object3D.userData
    mrdoob/three.js#26637

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -961,6 +961,7 @@ class FBXTreeParser {
 				}
 
 				model.name = node.attrName ? PropertyBinding.sanitizeNodeName( node.attrName ) : '';
+				model.userData.originalName = node.attrName;
 
 				model.ID = id;
 
@@ -997,6 +998,7 @@ class FBXTreeParser {
 						// set name and id here - otherwise in cases where "subBone" is created it will not have a name / id
 
 						bone.name = name ? PropertyBinding.sanitizeNodeName( name ) : '';
+						bone.userData.originalName = name;
 						bone.ID = id;
 
 						skeleton.bones[ i ] = bone;
