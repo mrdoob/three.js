8fde3e25fd869033a6ddd64a40f290c584486311
WestLangley <WestLangley@users.noreply.github.com>
2023-09-02 23:13:34 -0400

    Legacy lighting model is now deprecated (#26683)

--- src/lights/HemisphereLightProbe.js
+++ src/lights/HemisphereLightProbe.js
@@ -16,8 +16,7 @@ class HemisphereLightProbe extends LightProbe {
 		const sky = new Vector3( color1.r, color1.g, color1.b );
 		const ground = new Vector3( color2.r, color2.g, color2.b );
 
-		// without extra factor of PI in the shader, should = 1 / Math.sqrt( Math.PI );
-		const c0 = Math.sqrt( Math.PI );
+		const c0 = 1 / Math.sqrt( Math.PI );
 		const c1 = c0 * Math.sqrt( 0.75 );
 
 		this.sh.coefficients[ 0 ].copy( sky ).add( ground ).multiplyScalar( c0 );
