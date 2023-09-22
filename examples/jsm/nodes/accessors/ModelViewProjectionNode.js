45505ed0ac655eefa2007b0932e48649fbefb48f
sunag <sunagbrasil@gmail.com>
2023-09-22 16:58:55 -0300

    TSL: SetNode (#26785)
    * Add SetNode
    
    * Use viewProj.setZ()
    
    * cleanup

--- examples/jsm/nodes/accessors/ModelViewProjectionNode.js
+++ examples/jsm/nodes/accessors/ModelViewProjectionNode.js
@@ -1,10 +1,11 @@
-import Node, { addNodeClass } from '../core/Node.js';
+import { addNodeClass } from '../core/Node.js';
+import TempNode from '../core/TempNode.js';
 import { cameraProjectionMatrix } from './CameraNode.js';
 import { modelViewMatrix } from './ModelNode.js';
 import { positionLocal } from './PositionNode.js';
 import { nodeProxy } from '../shadernode/ShaderNode.js';
 
-class ModelViewProjectionNode extends Node {
+class ModelViewProjectionNode extends TempNode {
 
 	constructor( positionNode = positionLocal ) {
 
