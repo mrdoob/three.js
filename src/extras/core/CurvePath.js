cd64c5695109a056704c088a6bb899e3c62d6c82
郭斌勇 <guobinyong@qq.com>
2023-09-26 18:59:21 +0800

    fix #26849

--- src/extras/core/CurvePath.js
+++ src/extras/core/CurvePath.js
@@ -33,7 +33,8 @@ class CurvePath extends Curve {
 
 		if ( ! startPoint.equals( endPoint ) ) {
 
-			this.curves.push( new Curves[ 'LineCurve' ]( endPoint, startPoint ) );
+			const lineType = startPoint.isVector3 ? 'LineCurve3' : 'LineCurve';
+			this.curves.push( new Curves[ lineType ]( endPoint, startPoint ) );
 
 		}
 
