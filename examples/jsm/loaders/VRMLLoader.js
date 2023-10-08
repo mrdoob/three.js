e1c95bf615f1b532fa2dcc6d0f732c7b14f4136a
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-10-08 08:56:16 -0500

    VRMLLoader: cleanup

--- examples/jsm/loaders/VRMLLoader.js
+++ examples/jsm/loaders/VRMLLoader.js
@@ -91,7 +91,7 @@ class VRMLLoader extends Loader {
 			const tokenData = createTokens();
 
 			const lexer = new VRMLLexer( tokenData.tokens );
-			const parser = createParser( tokenData.tokenVocabulary );
+			const parser = VRMLParser.createParser( tokenData.tokenVocabulary );
 			const visitor = createVisitor( parser.getBaseCstVisitorConstructor() );
 
 			// lexing
@@ -3275,232 +3275,236 @@ class VRMLLexer {
 
 }
 
-function createParser( tokenVocabulary ) {
+class VRMLParser {
 
-	const $ = new chevrotain.CstParser( tokenVocabulary );
+	static createParser( tokenVocabulary ) {
 
-	const Version = tokenVocabulary[ 'Version' ];
-	const LCurly = tokenVocabulary[ 'LCurly' ];
-	const RCurly = tokenVocabulary[ 'RCurly' ];
-	const LSquare = tokenVocabulary[ 'LSquare' ];
-	const RSquare = tokenVocabulary[ 'RSquare' ];
-	const Identifier = tokenVocabulary[ 'Identifier' ];
-	const RouteIdentifier = tokenVocabulary[ 'RouteIdentifier' ];
-	const StringLiteral = tokenVocabulary[ 'StringLiteral' ];
-	const HexLiteral = tokenVocabulary[ 'HexLiteral' ];
-	const NumberLiteral = tokenVocabulary[ 'NumberLiteral' ];
-	const TrueLiteral = tokenVocabulary[ 'TrueLiteral' ];
-	const FalseLiteral = tokenVocabulary[ 'FalseLiteral' ];
-	const NullLiteral = tokenVocabulary[ 'NullLiteral' ];
-	const DEF = tokenVocabulary[ 'DEF' ];
-	const USE = tokenVocabulary[ 'USE' ];
-	const ROUTE = tokenVocabulary[ 'ROUTE' ];
-	const TO = tokenVocabulary[ 'TO' ];
-	const NodeName = tokenVocabulary[ 'NodeName' ];
+		const $ = new chevrotain.CstParser( tokenVocabulary );
 
-	$.RULE( 'vrml', function () {
+		const Version = tokenVocabulary[ 'Version' ];
+		const LCurly = tokenVocabulary[ 'LCurly' ];
+		const RCurly = tokenVocabulary[ 'RCurly' ];
+		const LSquare = tokenVocabulary[ 'LSquare' ];
+		const RSquare = tokenVocabulary[ 'RSquare' ];
+		const Identifier = tokenVocabulary[ 'Identifier' ];
+		const RouteIdentifier = tokenVocabulary[ 'RouteIdentifier' ];
+		const StringLiteral = tokenVocabulary[ 'StringLiteral' ];
+		const HexLiteral = tokenVocabulary[ 'HexLiteral' ];
+		const NumberLiteral = tokenVocabulary[ 'NumberLiteral' ];
+		const TrueLiteral = tokenVocabulary[ 'TrueLiteral' ];
+		const FalseLiteral = tokenVocabulary[ 'FalseLiteral' ];
+		const NullLiteral = tokenVocabulary[ 'NullLiteral' ];
+		const DEF = tokenVocabulary[ 'DEF' ];
+		const USE = tokenVocabulary[ 'USE' ];
+		const ROUTE = tokenVocabulary[ 'ROUTE' ];
+		const TO = tokenVocabulary[ 'TO' ];
+		const NodeName = tokenVocabulary[ 'NodeName' ];
 
-		$.SUBRULE( $.version );
-		$.AT_LEAST_ONE( function () {
+		$.RULE( 'vrml', function () {
 
-			$.SUBRULE( $.node );
+			$.SUBRULE( $.version );
+			$.AT_LEAST_ONE( function () {
 
-		} );
-		$.MANY( function () {
+				$.SUBRULE( $.node );
 
-			$.SUBRULE( $.route );
+			} );
+			$.MANY( function () {
 
-		} );
+				$.SUBRULE( $.route );
 
-	} );
-
-	$.RULE( 'version', function () {
-
-		$.CONSUME( Version );
-
-	} );
-
-	$.RULE( 'node', function () {
-
-		$.OPTION( function () {
-
-			$.SUBRULE( $.def );
+			} );
 
 		} );
 
-		$.CONSUME( NodeName );
-		$.CONSUME( LCurly );
-		$.MANY( function () {
+		$.RULE( 'version', function () {
 
-			$.SUBRULE( $.field );
-
-		} );
-		$.CONSUME( RCurly );
-
-	} );
-
-	$.RULE( 'field', function () {
-
-		$.CONSUME( Identifier );
-
-		$.OR2( [
-			{ ALT: function () {
-
-				$.SUBRULE( $.singleFieldValue );
-
-			} },
-			{ ALT: function () {
-
-				$.SUBRULE( $.multiFieldValue );
-
-			} }
-		] );
-
-	} );
-
-	$.RULE( 'def', function () {
-
-		$.CONSUME( DEF );
-		$.OR( [
-			{ ALT: function () {
-
-				$.CONSUME( Identifier );
-
-			} },
-			{ ALT: function () {
-
-				$.CONSUME( NodeName );
-
-			} }
-		] );
-
-	} );
-
-	$.RULE( 'use', function () {
-
-		$.CONSUME( USE );
-		$.OR( [
-			{ ALT: function () {
-
-				$.CONSUME( Identifier );
-
-			} },
-			{ ALT: function () {
-
-				$.CONSUME( NodeName );
-
-			} }
-		] );
-
-	} );
-
-	$.RULE( 'singleFieldValue', function () {
-
-		$.AT_LEAST_ONE( function () {
-
-			$.OR( [
-				{ ALT: function () {
-
-					$.SUBRULE( $.node );
-
-				} },
-				{ ALT: function () {
-
-					$.SUBRULE( $.use );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( StringLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( HexLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( NumberLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( TrueLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( FalseLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( NullLiteral );
-
-				} }
-			] );
-
+			$.CONSUME( Version );
 
 		} );
 
-	} );
+		$.RULE( 'node', function () {
 
-	$.RULE( 'multiFieldValue', function () {
+			$.OPTION( function () {
 
-		$.CONSUME( LSquare );
-		$.MANY( function () {
+				$.SUBRULE( $.def );
 
-			$.OR( [
+			} );
+
+			$.CONSUME( NodeName );
+			$.CONSUME( LCurly );
+			$.MANY( function () {
+
+				$.SUBRULE( $.field );
+
+			} );
+			$.CONSUME( RCurly );
+
+		} );
+
+		$.RULE( 'field', function () {
+
+			$.CONSUME( Identifier );
+
+			$.OR2( [
 				{ ALT: function () {
 
-					$.SUBRULE( $.node );
+					$.SUBRULE( $.singleFieldValue );
 
 				} },
 				{ ALT: function () {
 
-					$.SUBRULE( $.use );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( StringLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( HexLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( NumberLiteral );
-
-				} },
-				{ ALT: function () {
-
-					$.CONSUME( NullLiteral );
+					$.SUBRULE( $.multiFieldValue );
 
 				} }
 			] );
 
 		} );
-		$.CONSUME( RSquare );
 
-	} );
+		$.RULE( 'def', function () {
 
-	$.RULE( 'route', function () {
+			$.CONSUME( DEF );
+			$.OR( [
+				{ ALT: function () {
 
-		$.CONSUME( ROUTE );
-		$.CONSUME( RouteIdentifier );
-		$.CONSUME( TO );
-		$.CONSUME2( RouteIdentifier );
+					$.CONSUME( Identifier );
 
-	} );
+				} },
+				{ ALT: function () {
 
-	$.performSelfAnalysis();
+					$.CONSUME( NodeName );
 
-	return $;
+				} }
+			] );
+
+		} );
+
+		$.RULE( 'use', function () {
+
+			$.CONSUME( USE );
+			$.OR( [
+				{ ALT: function () {
+
+					$.CONSUME( Identifier );
+
+				} },
+				{ ALT: function () {
+
+					$.CONSUME( NodeName );
+
+				} }
+			] );
+
+		} );
+
+		$.RULE( 'singleFieldValue', function () {
+
+			$.AT_LEAST_ONE( function () {
+
+				$.OR( [
+					{ ALT: function () {
+
+						$.SUBRULE( $.node );
+
+					} },
+					{ ALT: function () {
+
+						$.SUBRULE( $.use );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( StringLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( HexLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( NumberLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( TrueLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( FalseLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( NullLiteral );
+
+					} }
+				] );
+
+
+			} );
+
+		} );
+
+		$.RULE( 'multiFieldValue', function () {
+
+			$.CONSUME( LSquare );
+			$.MANY( function () {
+
+				$.OR( [
+					{ ALT: function () {
+
+						$.SUBRULE( $.node );
+
+					} },
+					{ ALT: function () {
+
+						$.SUBRULE( $.use );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( StringLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( HexLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( NumberLiteral );
+
+					} },
+					{ ALT: function () {
+
+						$.CONSUME( NullLiteral );
+
+					} }
+				] );
+
+			} );
+			$.CONSUME( RSquare );
+
+		} );
+
+		$.RULE( 'route', function () {
+
+			$.CONSUME( ROUTE );
+			$.CONSUME( RouteIdentifier );
+			$.CONSUME( TO );
+			$.CONSUME2( RouteIdentifier );
+
+		} );
+
+		$.performSelfAnalysis();
+
+		return $;
+
+	}
 
 }
 
