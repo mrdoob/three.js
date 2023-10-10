54fce21b0ad278d48e219231612c4c1fe990016f
Nik Lever <47000827+NikLever@users.noreply.github.com>
2023-10-10 15:10:35 +0100

    Default now flipY = false, inverted image strips

--- examples/jsm/loaders/LUTImageLoader.js
+++ examples/jsm/loaders/LUTImageLoader.js
@@ -11,8 +11,10 @@ import {
 
 export class LUTImageLoader extends Loader {
 
-	constructor( flipVertical = true ) {
-
+	constructor( flipVertical = false ) {
+		//The NeutralLUT.png has green at the bottom for Unreal ang green at the top for Unity URP Color Lookup 
+		//post-processing. If you're using lut image strips from a Unity pipeline then pass true to the constructor
+		
 		super();
 
 		this.flip = flipVertical;
@@ -24,7 +26,7 @@ export class LUTImageLoader extends Loader {
 		const loader = new TextureLoader( this.manager );
 
 		loader.setCrossOrigin( this.crossOrigin );
-		
+
 		loader.setPath( this.path );
 		loader.load( url, texture => {
 
