030554131a500359f7dc150b8e56c2c08ef27be2
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-10-28 00:20:00 +0900

    Fix BatchedMesh failing with MeshBasicMaterial

--- examples/jsm/objects/BatchedMesh.js
+++ examples/jsm/objects/BatchedMesh.js
@@ -174,9 +174,13 @@ class BatchedMesh extends Mesh {
 						+ batchingParsVertex
 				)
 				.replace(
+					'#include <uv_vertex>',
+					'#include <uv_vertex>\n'
+                        + batchingbaseVertex
+				)
+				.replace(
 					'#include <skinnormal_vertex>',
 					'#include <skinnormal_vertex>\n'
-						+ batchingbaseVertex
 						+ batchingnormalVertex
 				)
 				.replace(
