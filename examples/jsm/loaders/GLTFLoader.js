0057dfc46d1f3bc9a1295b06455083a2b9dc89ae
Jesse <excalibr8@yahoo.com>
2023-11-02 22:05:22 +0000

    Fix for setting path not affecting GLTF Sub Assets correctly.

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -182,8 +182,13 @@ class GLTFLoader extends Loader {
 			resourcePath = this.resourcePath;
 
 		} else if ( this.path !== '' ) {
-
-			resourcePath = this.path;
+			// If a base path is set, resources will be relative paths from that
+			// Example  path = 'https://my-cnd-server.com/', url = 'assets/models/model.gltf'
+			// resourcePath = 'https://my-cnd-server.com/assets/models/'
+			// referenced resource 'model.bin' will be loaded from 'https://my-cnd-server.com/assets/models/model.bin'
+			// referenced resource '../textures/texture.png' will be loaded from 'https://my-cnd-server.com/assets/textures/texture.png'
+			const relativeUrl = LoaderUtils.extractUrlBase(url);
+			resourcePath = LoaderUtils.resolveURL(relativeUrl, this.path);
 
 		} else {
 
