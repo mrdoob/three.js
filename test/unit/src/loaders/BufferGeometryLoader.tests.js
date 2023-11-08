d6e86d9d02a8167defd75aba0fed67a989e6109f
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-11-08 18:53:52 +0900

    Remove update ranges from loader test

--- test/unit/src/loaders/BufferGeometryLoader.tests.js
+++ test/unit/src/loaders/BufferGeometryLoader.tests.js
@@ -51,7 +51,6 @@ export default QUnit.module( 'Loaders', () => {
 			const attr = new BufferAttribute( new Float32Array( [ 7, 8, 9, 10, 11, 12 ] ), 2, true );
 			attr.name = 'attribute';
 			attr.setUsage( DynamicDrawUsage );
-			attr.addUpdateRange( 1, 2 );
 
 			geometry.setAttribute( 'attr', attr );
 
