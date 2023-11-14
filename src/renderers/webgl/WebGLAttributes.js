fbd381a09a7ed4fbd48083dfc473eea44dad7f78
Konstantin Timoshenko <rondo.devil@gmail.com>
2023-11-14 20:51:40 +0300

    fix: lint, pads

--- src/renderers/webgl/WebGLAttributes.js
+++ src/renderers/webgl/WebGLAttributes.js
@@ -202,7 +202,9 @@ function WebGLAttributes( gl, capabilities ) {
 		} else if ( data.version < attribute.version ) {
 
 			if ( data.size < attribute.array.byteLength ) {
+
 				throw new Error( 'Too many data for update current attribute, dynamic sized attributes not supported.' );
+
 			}
 
 			updateBuffer( data.buffer, attribute, bufferType );
