4e831e70603770bcf17987c596c86f8ce11f92a9
linbingquan <695601626@qq.com>
2023-11-29 07:40:42 +0800

    src: Default parameters for ES6

--- src/extras/curves/ArcCurve.js
+++ src/extras/curves/ArcCurve.js
@@ -2,7 +2,7 @@ import { EllipseCurve } from './EllipseCurve.js';
 
 class ArcCurve extends EllipseCurve {
 
-	constructor( aX, aY, aRadius, aStartAngle, aEndAngle, aClockwise ) {
+	constructor( aX = 0, aY = 0, aRadius = 1, aStartAngle = 0, aEndAngle = Math.PI * 2, aClockwise = false ) {
 
 		super( aX, aY, aRadius, aRadius, aStartAngle, aEndAngle, aClockwise );
 
