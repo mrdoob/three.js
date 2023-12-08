23d8557c70e174ea759806fefee728bb3edacf44
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-12-08 08:05:12 -0800

    Triangle.getInterpolation: Return null if triangle is degenerate (#27331)
    * Return null if triangle is degenerate
    
    * Update docs
    
    * Ensure we support for vector4 and vector2
    
    * Update Triangle.html
    
    ---------
    
    Co-authored-by: Michael Herzog <michael.herzog@human-interactive.org>

--- src/math/Triangle.js
+++ src/math/Triangle.js
@@ -103,7 +103,15 @@ class Triangle {
 
 	static getInterpolation( point, p1, p2, p3, v1, v2, v3, target ) {
 
-		this.getBarycoord( point, p1, p2, p3, _v3 );
+		if ( this.getBarycoord( point, p1, p2, p3, _v3 ) === null ) {
+
+			target.x = 0;
+			target.y = 0;
+			if ( 'z' in target ) target.z = 0;
+			if ( 'w' in target ) target.w = 0;
+			return null;
+
+		}
 
 		target.setScalar( 0 );
 		target.addScaledVector( v1, _v3.x );
