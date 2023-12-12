efafc2bbc921fbed6586f619b2929ddd8bb30c8a
Michael Herzog <michael.herzog@human-interactive.org>
2023-12-12 11:59:52 +0100

    Quaternion: Add default parameter to `setFromEuler()`. (#27364)

--- src/math/Quaternion.js
+++ src/math/Quaternion.js
@@ -198,7 +198,7 @@ class Quaternion {
 
 	}
 
-	setFromEuler( euler, update ) {
+	setFromEuler( euler, update = true ) {
 
 		const x = euler._x, y = euler._y, z = euler._z, order = euler._order;
 
@@ -266,7 +266,7 @@ class Quaternion {
 
 		}
 
-		if ( update !== false ) this._onChangeCallback();
+		if ( update === true ) this._onChangeCallback();
 
 		return this;
 
