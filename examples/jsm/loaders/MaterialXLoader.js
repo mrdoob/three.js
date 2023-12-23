b62a184dd4d02d5397c3e3946ffe00884014bcba
hybridherbst <felix.herbst@gmail.com>
2023-12-23 21:45:01 +0100

    MaterialXLoader: Respect handlers attached to manager when loading textures
    Matches how GLTFloader does this

--- examples/jsm/loaders/MaterialXLoader.js
+++ examples/jsm/loaders/MaterialXLoader.js
@@ -312,7 +312,17 @@ class MaterialXNode {
 
 		const filePrefix = this.getRecursiveAttribute( 'fileprefix' ) || '';
 
-		const texture = this.materialX.textureLoader.load( filePrefix + this.value );
+		let loader = this.materialX.textureLoader;
+		const uri = filePrefix + this.value;
+
+		if ( uri ) {
+
+			const handler = this.materialX.manager.getHandler( uri );
+			if ( handler !== null ) loader = handler;
+
+		}
+
+		const texture = loader.load( uri );
 		texture.wrapS = texture.wrapT = RepeatWrapping;
 		texture.flipY = false;
 
