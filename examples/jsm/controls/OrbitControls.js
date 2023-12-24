a797aca01f4abbb9d776c2ad03e1129aa40a2283
Guilherme Avila <3927951+sciecode@users.noreply.github.com>
2023-12-24 07:08:31 -0300

    OrbitControls: fix Orthographic 'change' event (#27424)
    * OrbitControls: fix Orthographic 'change' event
    
    Signed-off-by: Guilherme Avila <3927951+sciecode@users.noreply.github.com>
    
    * OrbitControls: only recalculate when zoom has changed
    
    Signed-off-by: Guilherme Avila <3927951+sciecode@users.noreply.github.com>
    
    * clean-up
    
    Signed-off-by: Guilherme Avila <3927951+sciecode@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Guilherme Avila <3927951+sciecode@users.noreply.github.com>

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -373,10 +373,15 @@ class OrbitControls extends EventDispatcher {
 					}
 
 				} else if ( scope.object.isOrthographicCamera ) {
+					
+					zoomChanged = scale !== 1;
 
-					scope.object.zoom = Math.max( scope.minZoom, Math.min( scope.maxZoom, scope.object.zoom / scale ) );
-					scope.object.updateProjectionMatrix();
-					zoomChanged = true;
+					if ( zoomChanged ) {
+
+						scope.object.zoom = Math.max( scope.minZoom, Math.min( scope.maxZoom, scope.object.zoom / scale ) );
+						scope.object.updateProjectionMatrix();
+						
+					}
 
 				}
 
