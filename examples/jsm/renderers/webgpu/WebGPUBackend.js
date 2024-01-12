462480006d9439133e3661c0abd4761662c779d3
aardgoose <angus.sawyer@email.com>
2024-01-12 14:26:30 +0000

    remove unused import

--- examples/jsm/renderers/webgpu/WebGPUBackend.js
+++ examples/jsm/renderers/webgpu/WebGPUBackend.js
@@ -7,8 +7,6 @@ import { GPUFeatureName, GPUTextureFormat, GPULoadOp, GPUStoreOp, GPUIndexFormat
 import WGSLNodeBuilder from './nodes/WGSLNodeBuilder.js';
 import Backend from '../common/Backend.js';
 
-import { DepthFormat, WebGPUCoordinateSystem } from 'three';
-
 import WebGPUUtils from './utils/WebGPUUtils.js';
 import WebGPUAttributeUtils from './utils/WebGPUAttributeUtils.js';
 import WebGPUBindingUtils from './utils/WebGPUBindingUtils.js';
