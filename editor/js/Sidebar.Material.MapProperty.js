8d1a520fe0e8048270355a12dc8dbee28fbba417
Paul GALAND <paul.galand@ls-group.fr>
2024-01-16 14:13:11 +0100

    Improve UI for map properties

--- editor/js/Sidebar.Material.MapProperty.js
+++ editor/js/Sidebar.Material.MapProperty.js
@@ -12,77 +12,31 @@ function SidebarMaterialMapProperty( editor, property, name ) {
 	const signals = editor.signals;
 
 	const container = new UIRow();
-	container.add( new UIText( name ).setWidth( '90px' ) );
+	container.setStyle( 'border-top', [ '1px solid #888' ] );
 
-	const enabled = new UICheckbox( false ).setMarginRight( '8px' ).onChange( onChange );
-	container.add( enabled );
+	const mainContainer = new UIDiv().setWidth( '86px' ).setMarginRight( '4px' );
+	container.add( mainContainer );
+
+	mainContainer.add( new UIText( name ).setMarginBottom( '8px' ) );
+
+	const textureContainer = new UIRow();
+	mainContainer.add( textureContainer );
+
+	const enabled = new UICheckbox( false ).setMarginLeft( '8px' ).setMarginRight( '16px' ).onChange( onChange );
+	textureContainer.add( enabled );
 
 	const map = new UITexture( editor ).onChange( onMapChange );
-	container.add( map );
+	textureContainer.add( map );
+
+	const paramContainer = new UIDiv();
+	container.add( paramContainer );
+
+	const firstRow = new UIRow().setMarginBottom( '4px' ).setStyle( 'min-height', '0px' );
+	paramContainer.add( firstRow );
 
 	const mapType = property.replace( 'Map', '' );
 
 	const colorMaps = [ 'map', 'emissiveMap', 'sheenColorMap', 'specularColorMap', 'envMap' ];
-
-	let intensity;
-
-	if ( property === 'aoMap' ) {
-
-		intensity = new UINumber( 1 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( onIntensityChange );
-		container.add( intensity );
-
-	}
-
-	let scale;
-
-	if ( property === 'bumpMap' || property === 'displacementMap' ) {
-
-		scale = new UINumber().setWidth( '30px' ).onChange( onScaleChange );
-		container.add( scale );
-
-	}
-
-	let scaleX, scaleY;
-
-	if ( property === 'normalMap' || property === 'clearcoatNormalMap' ) {
-
-		scaleX = new UINumber().setWidth( '30px' ).onChange( onScaleXYChange );
-		container.add( scaleX );
-
-		scaleY = new UINumber().setWidth( '30px' ).onChange( onScaleXYChange );
-		container.add( scaleY );
-
-	}
-
-	let rangeMin, rangeMax;
-
-	if ( property === 'iridescenceThicknessMap' ) {
-
-		const range = new UIDiv().setMarginLeft( '3px' );
-		container.add( range );
-
-		const rangeMinRow = new UIRow().setMarginBottom( '0px' ).setStyle( 'min-height', '0px' );
-		range.add( rangeMinRow );
-
-		rangeMinRow.add( new UIText( 'min:' ).setWidth( '35px' ) );
-
-		rangeMin = new UINumber().setWidth( '40px' ).onChange( onRangeChange );
-		rangeMinRow.add( rangeMin );
-
-		const rangeMaxRow = new UIRow().setMarginBottom( '6px' ).setStyle( 'min-height', '0px' );
-		range.add( rangeMaxRow );
-
-		rangeMaxRow.add( new UIText( 'max:' ).setWidth( '35px' ) );
-
-		rangeMax = new UINumber().setWidth( '40px' ).onChange( onRangeChange );
-		rangeMaxRow.add( rangeMax );
-
-		// Additional settings for iridescenceThicknessMap
-		// Please add conditional if more maps are having a range property
-		rangeMin.setPrecision( 0 ).setRange( 0, Infinity ).setNudge( 1 ).setStep( 10 ).setUnit( 'nm' );
-		rangeMax.setPrecision( 0 ).setRange( 0, Infinity ).setNudge( 1 ).setStep( 10 ).setUnit( 'nm' );
-
-	}
 
 	let tileOffsetX, tileOffsetY;
 	let tileRepeatX, tileRepeatY;
@@ -90,7 +44,7 @@ function SidebarMaterialMapProperty( editor, property, name ) {
 	if ( property !== 'envMap' && property !== 'lightMap' && property !== 'aoMap' ) {
 
 		const tile = new UIDiv().setMarginLeft( '3px' );
-		container.add( tile );
+		firstRow.add( tile );
 
 		const tileOffsetRow = new UIRow().setMarginBottom( '0px' ).setStyle( 'min-height', '0px' );
 		tile.add( tileOffsetRow );
@@ -116,16 +70,82 @@ function SidebarMaterialMapProperty( editor, property, name ) {
 
 	}
 
-	const uvChannelRow = new UIRow().setMarginBottom( '6px' ).setStyle( 'min-height', '0px' );
-	container.add( uvChannelRow );
+	const secondRow = new UIRow().setMarginBottom( '0px' ).setStyle( 'min-height', '0px' );
+	paramContainer.add( secondRow );
 
-	uvChannelRow.add( new UIText( 'uv chan.:' ).setWidth( '40px' ) );
+	let intensity;
+
+	if ( property === 'aoMap' ) {
+
+		intensity = new UINumber( 1 ).setWidth( '30px' ).setRange( 0, 1 ).onChange( onIntensityChange );
+		secondRow.add( intensity );
+
+	}
+
+	let scale;
+
+	if ( property === 'bumpMap' || property === 'displacementMap' ) {
+
+		scale = new UINumber().setWidth( '30px' ).onChange( onScaleChange );
+		secondRow.add( scale );
+
+	}
+
+	let scaleX, scaleY;
+
+	if ( property === 'normalMap' || property === 'clearcoatNormalMap' ) {
+
+		scaleX = new UINumber().setWidth( '30px' ).onChange( onScaleXYChange );
+		secondRow.add( scaleX );
+
+		scaleY = new UINumber().setWidth( '30px' ).onChange( onScaleXYChange );
+		secondRow.add( scaleY );
+
+	}
+
+	let rangeMin, rangeMax;
+
+	if ( property === 'iridescenceThicknessMap' ) {
+
+		const range = new UIDiv().setMarginLeft( '3px' );
+		secondRow.add( range );
+
+		const rangeMinRow = new UIRow().setMarginBottom( '0px' ).setStyle( 'min-height', '0px' );
+		range.add( rangeMinRow );
+
+		rangeMinRow.add( new UIText( 'min:' ).setWidth( '35px' ) );
+
+		rangeMin = new UINumber().setWidth( '40px' ).onChange( onRangeChange );
+		rangeMinRow.add( rangeMin );
+
+		const rangeMaxRow = new UIRow().setMarginBottom( '6px' ).setStyle( 'min-height', '0px' );
+		range.add( rangeMaxRow );
+
+		rangeMaxRow.add( new UIText( 'max:' ).setWidth( '35px' ) );
+
+		rangeMax = new UINumber().setWidth( '40px' ).onChange( onRangeChange );
+		rangeMaxRow.add( rangeMax );
+
+		// Additional settings for iridescenceThicknessMap
+		// Please add conditional if more maps are having a range property
+		rangeMin.setPrecision( 0 ).setRange( 0, Infinity ).setNudge( 1 ).setStep( 10 ).setUnit( 'nm' );
+		rangeMax.setPrecision( 0 ).setRange( 0, Infinity ).setNudge( 1 ).setStep( 10 ).setUnit( 'nm' );
+
+	}
+
+	const uvDiv = new UIDiv().setMarginLeft( '3px' );
+	secondRow.add( uvDiv );
+
+	const uvChannelRow = new UIRow().setMarginBottom( '0px' ).setStyle( 'min-height', '0px' );
+	uvDiv.add( uvChannelRow );
+
+	uvChannelRow.add( new UIText( 'uv:' ).setWidth( '40px' ) );
 
 	const uvChannel = new UINumber( ( property === 'aoMap' || property === 'lightMap' ) ? 1 : 0 ).setPrecision( 0 ).setRange( 0, 3 ).setStep( 1 ).setWidth( '40px' ).onChange( onUVChannelChange );
 	uvChannelRow.add( uvChannel );
 
 	const flipYRow = new UIRow().setMarginBottom( '6px' ).setStyle( 'min-height', '0px' );
-	container.add( flipYRow );
+	uvDiv.add( flipYRow );
 
 	flipYRow.add( new UIText( 'flipY:' ).setWidth( '40px' ) );
 
