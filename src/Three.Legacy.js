5c1d31143ff9c6d95c8fdd1631c3f3c769b84b8b
Cody Bennett <hi@codyb.co>
2024-02-05 17:34:14 -0600

    Legacy: update deprecation hint

--- src/Three.Legacy.js
+++ src/Three.Legacy.js
@@ -4,7 +4,7 @@ export class WebGLMultipleRenderTargets extends WebGLRenderTarget { // @deprecat
 
 	constructor( width = 1, height = 1, count = 1, options = {} ) {
 
-		console.warn( 'THREE.WebGLMultipleRenderTargets has been removed. Use THREE.WebGLRenderTarget and set the "count" parameter to enable MRT.' );
+		console.warn( 'THREE.WebGLMultipleRenderTargets has been deprecated and will be removed in r172. Use THREE.WebGLRenderTarget and set the "count" parameter to enable MRT.' );
 
 		super( width, height, { ...options, count } );
 
