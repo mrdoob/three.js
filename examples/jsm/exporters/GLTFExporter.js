861359cc9a432c2c2a8ba74c4afadab93f106e51
Mugen87 <michael.herzog@human-interactive.org>
2024-02-07 10:55:58 +0100

    GLTFExporter: Better error handling when using invalid image data.

--- examples/jsm/exporters/GLTFExporter.js
+++ examples/jsm/exporters/GLTFExporter.js
@@ -1288,7 +1288,17 @@ class GLTFWriter {
 
 			} else {
 
-				ctx.drawImage( image, 0, 0, canvas.width, canvas.height );
+				if ( ( typeof HTMLImageElement !== 'undefined' && image instanceof HTMLImageElement ) ||
+					( typeof HTMLCanvasElement !== 'undefined' && image instanceof HTMLCanvasElement ) ||
+					( typeof ImageBitmap !== 'undefined' && image instanceof ImageBitmap ) ) {
+
+					ctx.drawImage( image, 0, 0, canvas.width, canvas.height );
+
+				} else {
+
+					throw new Error( 'THREE.GLTFExporter: Invalid image type. Use HTMLImageElement, HTMLCanvasElement or ImageBitmap.' );
+
+				}
 
 			}
 
