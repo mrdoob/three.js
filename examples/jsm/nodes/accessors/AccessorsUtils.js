ae3c188df973b5422a86baceb7c086adcf462fc1
sunag <sunagbrasil@gmail.com>
2024-02-12 18:38:19 -0300

    TSL: Add parallaxUV

--- examples/jsm/nodes/accessors/AccessorsUtils.js
+++ examples/jsm/nodes/accessors/AccessorsUtils.js
@@ -2,5 +2,9 @@ import { bitangentView } from './BitangentNode.js';
 import { normalView } from './NormalNode.js';
 import { tangentView } from './TangentNode.js';
 import { mat3 } from '../shadernode/ShaderNode.js';
+import { positionViewDirection } from './PositionNode.js';
 
 export const TBNViewMatrix = mat3( tangentView, bitangentView, normalView );
+
+export const parallaxDelta = positionViewDirection.mul( mat3( tangentView, bitangentView, normalView.negate() ) );
+export const parallaxUV = ( uv, scale ) => uv.sub( parallaxDelta.mul( scale ) );
