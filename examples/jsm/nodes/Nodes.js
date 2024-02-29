12b08b230c36c5fc4b03aed61dc5b8bc6be5f328
sunag <sunagbrasil@gmail.com>
2024-02-29 18:00:53 -0300

    WebGPURenderer: PMREM (#27829)
    * WebGPURenderer: PMREM (WIP)
    
    * add temporary example
    
    * Added fromCubemap()
    
    * added pmrem and examples
    
    * cleanup
    
    * fix circular dependency
    
    * Revert "fix circular dependency"
    
    This reverts commit 83d68820b4a4b615124cc8292498f8929875083c.
    
    * update pmrem examples
    
    * NodeBuilder: Rename .getRenderTarget() -> .createRenderTarget()
    
    * fix circular dependency (2)
    
    * revision
    
    * revision
    
    * update `webgpu_cubemap_mix` example
    
    * update `webgpu_cubemap_adjustments` example
    
    * update `webgpu_equirectangular` example
    
    * update screenshots

--- examples/jsm/nodes/Nodes.js
+++ examples/jsm/nodes/Nodes.js
@@ -64,7 +64,6 @@ export { default as RemapNode, remap, remapClamp } from './utils/RemapNode.js';
 export { default as RotateUVNode, rotateUV } from './utils/RotateUVNode.js';
 export { default as RotateNode, rotate } from './utils/RotateNode.js';
 export { default as SetNode } from './utils/SetNode.js';
-export { default as SpecularMIPLevelNode, specularMIPLevel } from './utils/SpecularMIPLevelNode.js';
 export { default as SplitNode } from './utils/SplitNode.js';
 export { default as SpriteSheetUVNode, spritesheetUV } from './utils/SpriteSheetUVNode.js';
 export { default as StorageArrayElementNode } from './utils/StorageArrayElementNode.js';
@@ -159,6 +158,10 @@ export { default as HemisphereLightNode } from './lighting/HemisphereLightNode.j
 export { default as EnvironmentNode } from './lighting/EnvironmentNode.js';
 export { default as AONode } from './lighting/AONode.js';
 export { default as AnalyticLightNode } from './lighting/AnalyticLightNode.js';
+
+// pmrem
+export { default as PMREMNode, pmremTexture } from './pmrem/PMREMNode.js';
+export * as PMREMUtils from './pmrem/PMREMUtils.js';
 
 // procedural
 export { default as CheckerNode, checker } from './procedural/CheckerNode.js';
