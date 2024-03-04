7e74e4cc6458005ea9a4c3176f933c33bde17e72
Michael Herzog <michael.herzog@human-interactive.org>
2024-03-04 16:15:48 +0100

    WebGLRenderer: Make sure stencil works with transmission. (#27799)

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1418,7 +1418,8 @@ class WebGLRenderer {
 					generateMipmaps: true,
 					type: ( extensions.has( 'EXT_color_buffer_half_float' ) || extensions.has( 'EXT_color_buffer_float' ) ) ? HalfFloatType : UnsignedByteType,
 					minFilter: LinearMipmapLinearFilter,
-					samples: 4
+					samples: 4,
+					stencil: stencil
 				} );
 
 				// debug
