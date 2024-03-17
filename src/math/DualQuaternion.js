9f780ca1719ba78f4f491b861d65649475d1cf78
Philipp Kuboth <philipp@kuboth.dev>
2024-03-17 17:02:09 +0100

    Math: Remove unused import

--- src/math/DualQuaternion.js
+++ src/math/DualQuaternion.js
@@ -1,4 +1,3 @@
-import * as MathUtils from './MathUtils.js';
 import { Vector3 } from './Vector3.js';
 import { Quaternion } from './Quaternion.js';
 
@@ -7,62 +6,62 @@ const _quaternion = /*@__PURE__*/ new Quaternion();
 
 class DualQuaternion {
 
-    constructor( x = 0, y = 0, z = 0, w = 1, dx = 0, dy = 0, dz = 0, dw = 0 ) {
+	constructor( x = 0, y = 0, z = 0, w = 1, dx = 0, dy = 0, dz = 0, dw = 0 ) {
 
-        this.isDualQuaternion = true;
+		this.isDualQuaternion = true;
 
-        this._values = new Float32Array( 8 );
+		this._values = new Float32Array( 8 );
 
-        this._values[ 0 ] = x;
-        this._values[ 1 ] = y;
-        this._values[ 2 ] = z;
-        this._values[ 3 ] = w;
+		this._values[ 0 ] = x;
+		this._values[ 1 ] = y;
+		this._values[ 2 ] = z;
+		this._values[ 3 ] = w;
 
-        this._values[ 4 ] = dx;
-        this._values[ 5 ] = dy;
-        this._values[ 6 ] = dz;
-        this._values[ 7 ] = dw;
+		this._values[ 4 ] = dx;
+		this._values[ 5 ] = dy;
+		this._values[ 6 ] = dz;
+		this._values[ 7 ] = dw;
 
-        this.real = this._values.subarray( 0, 4 );
-        this.dual = this._values.subarray( 4, 8 );
-    
-    }
+		this.real = this._values.subarray( 0, 4 );
+		this.dual = this._values.subarray( 4, 8 );
 
-    fromTranslationAndRotation( translation, rotation ) {
-            
-        rotation.toArray( this.real );
-        translation.toArray( this.dual );        
+	}
 
-        this.dual[ 0 ] = 0.5 * ( translation.x * rotation.w + translation.y * rotation.z - translation.z * rotation.y );
-        this.dual[ 1 ] = 0.5 * ( translation.y * rotation.w + translation.z * rotation.x - translation.x * rotation.z );
-        this.dual[ 2 ] = 0.5 * ( translation.z * rotation.w + translation.x * rotation.y - translation.y * rotation.x );
-        this.dual[ 3 ] = -0.5 * ( translation.x * rotation.x + translation.y * rotation.y + translation.z * rotation.z );
+	fromTranslationAndRotation( translation, rotation ) {
 
-        return this;
+		rotation.toArray( this.real );
+		translation.toArray( this.dual );
 
-    }
+		this.dual[ 0 ] = 0.5 * ( translation.x * rotation.w + translation.y * rotation.z - translation.z * rotation.y );
+		this.dual[ 1 ] = 0.5 * ( translation.y * rotation.w + translation.z * rotation.x - translation.x * rotation.z );
+		this.dual[ 2 ] = 0.5 * ( translation.z * rotation.w + translation.x * rotation.y - translation.y * rotation.x );
+		this.dual[ 3 ] = - 0.5 * ( translation.x * rotation.x + translation.y * rotation.y + translation.z * rotation.z );
 
-    fromMatrix4( matrix ) {
+		return this;
 
-        const position = _vector.setFromMatrixPosition( matrix );
-        const rotation = _quaternion.setFromRotationMatrix( matrix );
+	}
 
-        return this.fromTranslationAndRotation( position, rotation );
+	fromMatrix4( matrix ) {
 
-    }
+		const position = _vector.setFromMatrixPosition( matrix );
+		const rotation = _quaternion.setFromRotationMatrix( matrix );
+
+		return this.fromTranslationAndRotation( position, rotation );
+
+	}
 
 
-    toArray( array, offset = 0 ) {
+	toArray( array, offset = 0 ) {
 
-        this._values.forEach( ( value, i ) => {
+		this._values.forEach( ( value, i ) => {
 
-            array[ offset + i ] = value;
+			array[ offset + i ] = value;
 
-        } );
+		} );
 
-        return array;
+		return array;
 
-    }
+	}
 
 }
 
