bdc3bbd368f7aa66d57e7d807feec30db8a93620
Joe Pea <joe@trusktr.io>
2024-05-06 09:50:50 -0700

    change comment wording, remove unneeded tests

--- src/animation/tracks/StringKeyframeTrack.js
+++ src/animation/tracks/StringKeyframeTrack.js
@@ -6,7 +6,7 @@ import { KeyframeTrack } from '../KeyframeTrack.js';
  */
 class StringKeyframeTrack extends KeyframeTrack {
 
-	// No interpolation parameter because only InterpolateDiscrete makes sense.
+	// No interpolation parameter because only InterpolateDiscrete is valid.
 	constructor( name, times, values ) {
 
 		super( name, times, values );
