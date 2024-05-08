1ccaba53e4a6da653747aff57ba61b15f3f3470a
Philipp Kuboth <philipp@kuboth.dev>
2024-05-08 19:23:09 +0200

    ShaderChunk: Lookup Bone scale from texture

--- src/renderers/shaders/ShaderChunk/skinning_pars_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/skinning_pars_vertex.glsl.js
@@ -35,14 +35,41 @@ export default /* glsl */`
 		return mat2x4( v1, v2 );
 	}
 
+	vec4 getBoneScaleMatrix( const in float i ) {
+		
+		int size = textureSize( boneTexture, 0 ).x;
+		int j = int( i ) * 4;
+		int x = j % size;
+		int y = j / size;
+		vec4 scale = texelFetch( boneTexture, ivec2( x + 2, y ), 0 );
+
+		return scale;
+	}
+
 	vec4 mulitplyVectorWithDualQuaternion( mat2x4 dq, vec4 v ) {
 
 		vec4 qr = dq[0];
 		vec4 qd = dq[1];
-		vec3 pos = v.xyz + 2.0 * cross( qr.xyz, cross( qr.xyz, v.xyz ) + qr.w * v.xyz ); // Quaternion Rotation of a Vector            
-		vec3 tran = 2.0 * ( qr.w * qd.xyz - qd.w * qr.xyz + cross( qr.xyz, qd.xyz ));	 // Pull out Translation from DQ
-		
+		vec3 pos = v.xyz + 2.0 * cross( qr.xyz, cross( qr.xyz, v.xyz ) + qr.w * v.xyz );         
+		vec3 tran = 2.0 * ( qr.w * qd.xyz - qd.w * qr.xyz + cross( qr.xyz, qd.xyz ));
+
 		return vec4(pos + tran, 1.0);
+	}
+
+	mat4x4 dualQuaternionToMatrix( in mat2x4 dq ) {
+
+		vec4 qr = dq[0];
+		vec4 qd = dq[1];
+		vec3 pos = 2.0 * ( qr.w * qd.xyz - qd.w * qr.xyz + cross( qr.xyz, qd.xyz ));
+		vec4 rot = vec4( qr.xyz, - qr.w );
+		vec4 tran = vec4( pos, 1.0 );
+
+		mat4x4 r = mat4x4( 1.0 - 2.0 * rot.y * rot.y - 2.0 * rot.z * rot.z, 2.0 * rot.x * rot.y - 2.0 * rot.w * rot.z, 2.0 * rot.x * rot.z + 2.0 * rot.w * rot.y, 0.0,
+						   2.0 * rot.x * rot.y + 2.0 * rot.w * rot.z, 1.0 - 2.0 * rot.x * rot.x - 2.0 * rot.z * rot.z, 2.0 * rot.y * rot.z - 2.0 * rot.w * rot.x, 0.0,
+						   2.0 * rot.x * rot.z - 2.0 * rot.w * rot.y, 2.0 * rot.y * rot.z + 2.0 * rot.w * rot.x, 1.0 - 2.0 * rot.x * rot.x - 2.0 * rot.y * rot.y, 0.0,
+						   tran.x, tran.y, tran.z, 1.0 );
+
+		return r;
 	}
 
 	#endif
