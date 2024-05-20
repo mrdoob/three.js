803e567600199a359c156e13760f0249f9f0d2f0
sunag <sunagbrasil@gmail.com>
2024-05-19 21:37:00 -0300

    TSL: Introduce `modelWorldMatrixInverse`

--- examples/jsm/nodes/accessors/ModelNode.js
+++ examples/jsm/nodes/accessors/ModelNode.js
@@ -1,6 +1,8 @@
 import Object3DNode from './Object3DNode.js';
 import { addNodeClass } from '../core/Node.js';
 import { nodeImmutable } from '../shadernode/ShaderNode.js';
+import { uniform } from '../core/UniformNode.js';
+import { Matrix4 } from 'three';
 
 class ModelNode extends Object3DNode {
 
@@ -29,5 +31,6 @@ export const modelWorldMatrix = nodeImmutable( ModelNode, ModelNode.WORLD_MATRIX
 export const modelPosition = nodeImmutable( ModelNode, ModelNode.POSITION );
 export const modelScale = nodeImmutable( ModelNode, ModelNode.SCALE );
 export const modelViewPosition = nodeImmutable( ModelNode, ModelNode.VIEW_POSITION );
+export const modelWorldMatrixInverse = uniform( new Matrix4() ).onObjectUpdate( ( { object }, self ) => self.value.copy( object.matrixWorld ).invert() );
 
 addNodeClass( 'ModelNode', ModelNode );
