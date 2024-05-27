9294a3beae1b5442ac93ff5873925befd9e5b96a
sunag <sunagbrasil@gmail.com>
2024-05-27 15:47:56 -0300

    ReflectVectorNode: Move to TSL approach

--- examples/jsm/nodes/Nodes.js
+++ examples/jsm/nodes/Nodes.js
@@ -97,7 +97,7 @@ export { default as Object3DNode, objectDirection, objectViewMatrix, objectNorma
 export { default as PointUVNode, pointUV } from './accessors/PointUVNode.js';
 export * from './accessors/PositionNode.js';
 export { default as ReferenceNode, reference, referenceBuffer } from './accessors/ReferenceNode.js';
-export { default as ReflectVectorNode, reflectVector } from './accessors/ReflectVectorNode.js';
+export * from './accessors/ReflectVectorNode.js';
 export { default as SkinningNode, skinning } from './accessors/SkinningNode.js';
 export { default as SceneNode, backgroundBlurriness, backgroundIntensity } from './accessors/SceneNode.js';
 export { default as StorageBufferNode, storage, storageObject } from './accessors/StorageBufferNode.js';
