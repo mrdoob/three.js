0a03ca667a09da70fe94a69f68635cd0f94a304b
sunag <sunagbrasil@gmail.com>
2024-05-27 15:36:37 -0300

    TSL: Introduce `.toGlobal` (#28503)

--- examples/jsm/nodes/core/Node.js
+++ examples/jsm/nodes/core/Node.js
@@ -25,6 +25,8 @@ class Node extends EventDispatcher {
 		this._cacheKey = null;
 		this._cacheKeyVersion = 0;
 
+		this.global = false;
+
 		this.isNode = true;
 
 		Object.defineProperty( this, 'id', { value: _nodeId ++ } );
@@ -98,7 +100,7 @@ class Node extends EventDispatcher {
 
 	isGlobal( /*builder*/ ) {
 
-		return false;
+		return this.global;
 
 	}
 
