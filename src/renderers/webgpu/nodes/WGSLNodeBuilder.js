ad8e897883ea728131230c17bbd851f3be57938d
Christian Helgeson <cmhhelgeson@gmail.com>
2024-06-10 13:48:19 -0700

    Added directive support to WGSLNodeBuilder

--- src/renderers/webgpu/nodes/WGSLNodeBuilder.js
+++ src/renderers/webgpu/nodes/WGSLNodeBuilder.js
@@ -163,6 +163,8 @@ class WGSLNodeBuilder extends NodeBuilder {
 
 		this.builtins = {};
 
+		this.directives = {};
+
 	}
 
 	needsColorSpaceToLinear( texture ) {
@@ -655,6 +657,44 @@ ${ flowData.code }
 
 	}
 
+	getDirective( name, shaderStage = this.shaderStage) {
+
+		const stage = this.directives[ shaderStage ] || ( this.directives[ shaderStage ] = [] );
+		stage.push(name);
+
+	}
+
+	getDirectives( shaderStage ) {
+
+		const snippets = [];
+		const directives = this.directives[ shaderStage ];
+
+		if ( directives !== undefined ) {
+
+			for ( const directive of directives ) {
+
+				snippets.push(`enable ${directive}`);
+
+			}
+
+		}
+
+		return snippets.join('\n');
+
+	}
+
+	enableClipDistances() {
+
+		this.getDirective('clip_distances');
+
+	}
+
+	enableShaderF16() {
+
+		this.getDirective('f16');
+
+	}
+
 	getBuiltins( shaderStage ) {
 
 		const snippets = [];
@@ -967,6 +1007,7 @@ ${ flowData.code }
 			stageData.structs = this.getStructs( shaderStage );
 			stageData.vars = this.getVars( shaderStage );
 			stageData.codes = this.getCodes( shaderStage );
+			stageData.directives = this.getDirectives( shaderStage );
 
 			//
 
@@ -1126,6 +1167,8 @@ ${ flowData.code }
 	_getWGSLVertexCode( shaderData ) {
 
 		return `${ this.getSignature() }
+// directives
+${shaderData.directives};
 
 // uniforms
 ${shaderData.uniforms}
@@ -1183,6 +1226,9 @@ fn main( ${shaderData.varyings} ) -> ${shaderData.returnType} {
 	_getWGSLComputeCode( shaderData, workgroupSize ) {
 
 		return `${ this.getSignature() }
+// Directives
+${shaderData.directives}
+
 // system
 var<private> instanceIndex : u32;
 
