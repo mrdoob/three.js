a9a9131fd939b4d5aad6cf3806b8689614a283b2
Christian Helgeson <cmhhelgeson@gmail.com>
2024-06-21 16:32:15 -0700

    fix missing import

--- examples/jsm/renderers/common/nodes/NodeStorageBuffer.js
+++ examples/jsm/renderers/common/nodes/NodeStorageBuffer.js
@@ -1,4 +1,5 @@
 import StorageBuffer from '../StorageBuffer.js';
+import { GPUBufferBindingType } from '../../webgpu/utils/WebGPUConstants.js';
 
 let _id = 0;
 
@@ -9,8 +10,7 @@ class NodeStorageBuffer extends StorageBuffer {
 		super( 'StorageBuffer_' + _id ++, nodeUniform ? nodeUniform.value : null );
 
 		this.nodeUniform = nodeUniform;
-
-		this.access = nodeUniform ? nodeUniform.access : GPUBufferBindingType.Storage;
+		this.access = nodeUniform.access ? nodeUniform.access : GPUBufferBindingType.Storage
 
 	}
 
