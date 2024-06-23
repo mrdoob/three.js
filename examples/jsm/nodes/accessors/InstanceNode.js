7b972f53950fff07657058de0f86e830281b2555
z4122 <412213484@qq.com>
2024-06-23 23:30:47 +0800

    feat: make instance color binding group

--- examples/jsm/nodes/accessors/InstanceNode.js
+++ examples/jsm/nodes/accessors/InstanceNode.js
@@ -28,9 +28,10 @@ class InstanceNode extends Node {
 
 	}
 
-	setup( builder ) {
+	setup( /*builder*/ ) {
 
 		let instanceMatrixNode = this.instanceMatrixNode;
+		let instanceColorNode = this.instanceColorNode;
 
 		const instanceMesh = this.instanceMesh;
 
@@ -38,7 +39,29 @@ class InstanceNode extends Node {
 
 			const instanceAttribute = instanceMesh.instanceMatrix;
 
-			instanceMatrixNode = buffer( instanceAttribute.array, 'mat4', instanceMesh.count ).element( instanceIndex );
+			// Compatilbe with WebGL backend, WebGL2 limits UBO to 64kb. Matrix count number bigger than 1000 ( 16 * 4 * 1000 = 64kb ) will fallback to attribute.
+			if ( instanceMesh.count <= 1000 ) {
+
+				instanceMatrixNode = buffer( instanceAttribute.array, 'mat4', instanceMesh.count ).element( instanceIndex );
+
+			} else {
+
+				const buffer = new InstancedInterleavedBuffer( instanceAttribute.array, 16, 1 );
+
+				this.buffer = buffer;
+				const bufferFn = instanceAttribute.usage === DynamicDrawUsage ? instancedDynamicBufferAttribute : instancedBufferAttribute;
+
+				const instanceBuffers = [
+					// F.Signature -> bufferAttribute( array, type, stride, offset )
+					bufferFn( buffer, 'vec4', 16, 0 ),
+					bufferFn( buffer, 'vec4', 16, 4 ),
+					bufferFn( buffer, 'vec4', 16, 8 ),
+					bufferFn( buffer, 'vec4', 16, 12 )
+				];
+
+				instanceMatrixNode = mat4( ...instanceBuffers );
+
+			}
 
 			this.instanceMatrixNode = instanceMatrixNode;
 
@@ -48,11 +71,23 @@ class InstanceNode extends Node {
 
 		if ( instanceColorAttribute && this.instanceColorNode === null ) {
 
-			const buffer = new InstancedBufferAttribute( instanceColorAttribute.array, 3 );
-			const bufferFn = instanceColorAttribute.usage === DynamicDrawUsage ? instancedDynamicBufferAttribute : instancedBufferAttribute;
+			// Compatilbe with WebGL backend, WebGL2 limits UBO to 64kb. Color count number bigger than 5333 ( 3 * 4 * 5333 < 64kb ) will fallback to attribute.
+			if ( instanceMesh.count < 5333 ) {
 
-			this.bufferColor = buffer;
-			this.instanceColorNode = vec3( bufferFn( buffer, 'vec3', 3, 0 ) );
+				instanceColorNode = buffer( instanceColorAttribute.array, 'vec3', instanceMesh.count ).element( instanceIndex );
+
+			} else {
+
+				const buffer = new InstancedBufferAttribute( instanceColorAttribute.array, 3 );
+				const bufferFn = instanceColorAttribute.usage === DynamicDrawUsage ? instancedDynamicBufferAttribute : instancedBufferAttribute;
+
+				this.bufferColor = buffer;
+
+				instanceColorNode = vec3( bufferFn( buffer, 'vec3', 3, 0 ) );
+
+			}
+
+			this.instanceColorNode = instanceColorNode;
 
 		}
 
@@ -85,13 +120,13 @@ class InstanceNode extends Node {
 
 	update( /*frame*/ ) {
 
-		if ( this.instanceMesh.instanceMatrix.usage !== DynamicDrawUsage && this.instanceMesh.instanceMatrix.version !== this.buffer.version ) {
+		if ( this.instanceMesh.instanceMatrix.usage !== DynamicDrawUsage && this.buffer != null && this.instanceMesh.instanceMatrix.version !== this.buffer.version ) {
 
 			this.buffer.version = this.instanceMesh.instanceMatrix.version;
 
 		}
 
-		if ( this.instanceMesh.instanceColor && this.instanceMesh.instanceColor.usage !== DynamicDrawUsage && this.instanceMesh.instanceColor.version !== this.bufferColor.version ) {
+		if ( this.instanceMesh.instanceColor && this.instanceMesh.instanceColor.usage !== DynamicDrawUsage && this.bufferColor != null && this.instanceMesh.instanceColor.version !== this.bufferColor.version ) {
 
 			this.bufferColor.version = this.instanceMesh.instanceColor.version;
 
