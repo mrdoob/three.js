4feb1372ea3bd32d33a9a03c2f58ff225a07f5d2
Michael Herzog <michael.herzog@human-interactive.org>
2024-06-25 10:11:05 +0200

    Addons: Add `DepthOfFieldNode`. (#28740)
    * Addons: Add `DepthOfFieldNode`.
    
    * DepthOfFieldNode: Fix runtime error.
    
    * E2E: Update screenshot.
    
    * Examples: Align code to previous demo.
    
    * Examples: Use instancing in DOF demo.

--- examples/jsm/nodes/Nodes.js
+++ examples/jsm/nodes/Nodes.js
@@ -126,6 +126,7 @@ export { default as GaussianBlurNode, gaussianBlur } from './display/GaussianBlu
 export { default as AfterImageNode, afterImage } from './display/AfterImageNode.js';
 export { default as AnamorphicNode, anamorphic } from './display/AnamorphicNode.js';
 export { default as SobelOperatorNode, sobel } from './display/SobelOperatorNode.js';
+export { default as DepthOfFieldNode, dof } from './display/DepthOfFieldNode.js';
 
 export { default as PassNode, pass, texturePass, depthPass } from './display/PassNode.js';
 
