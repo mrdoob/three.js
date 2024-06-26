c86f86b2802e0a47a43c61500b6b06e69ef0281b
sunag <sunagbrasil@gmail.com>
2024-06-26 00:46:59 -0300

    cleanup

--- examples/jsm/nodes/materials/MeshMatcapNodeMaterial.js
+++ examples/jsm/nodes/materials/MeshMatcapNodeMaterial.js
@@ -14,9 +14,9 @@ class MeshMatcapNodeMaterial extends NodeMaterial {
 
 		super();
 
-		this.isMeshMatcapNodeMaterial = true;
-
 		this.lights = false;
+
+		this.isMeshMatcapNodeMaterial = true;
 
 		this.setDefaultValues( defaultValues );
 
