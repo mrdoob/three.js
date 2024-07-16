36f500735d243257f73b443bf72885cc23568d41
Mugen87 <michael.herzog@human-interactive.org>
2024-07-17 00:11:23 +0200

    E2E: Update screenshots.

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -148,8 +148,6 @@ const exceptionList = [
 	'webgpu_texturegrad',
 	'webgpu_performance_renderbundle',
 	'webgpu_lights_rectarealight',
-	'webgpu_postprocessing',
-	'misc_controls_fly',
 
 	// WebGPU idleTime and parseTime too low
 	'webgpu_compute_particles',
