d2f4584dc1bf0dab32d9f45113267bd67e5e4dc6
Christian Helgeson <cmhhelgeson@gmail.com>
2024-07-25 12:41:09 -0700

    init

--- src/nodes/Nodes.js
+++ src/nodes/Nodes.js
@@ -141,6 +141,7 @@ export { default as BloomNode, bloom } from './display/BloomNode.js';
 export { default as TransitionNode, transition } from './display/TransitionNode.js';
 export { default as RenderOutputNode, renderOutput } from './display/RenderOutputNode.js';
 export { default as PixelationPassNode, pixelationPass } from './display/PixelationPassNode.js';
+export { bleach } from './display/BleachBypassNode.js';
 
 export { default as PassNode, pass, passTexture, depthPass } from './display/PassNode.js';
 
---
+++ src/nodes/display/BleachBypassNode.js
@@ -0,0 +1,29 @@
+import { addNodeElement, float, tslFn, vec3, vec4 } from '../shadernode/ShaderNode.js';
+import { min, max, dot, mix } from '../math/MathNode.js';
+
+import { property } from '../core/PropertyNode.js';
+
+export const bleach = /*@__PURE__*/ tslFn( ( { color, opacity = 1.0 } ) => {
+
+	const base = property( 'vec4', 'base' ).assign( color );
+	const lum = dot( vec3( 0.25, 0.65, 0.1 ), base.rgb );
+	const blend = vec3( lum );
+
+	const L = min( 1.0, max( 0.0, float( 10.0 ).mul( lum.sub( 0.45 ) ) ) );
+
+	const result1 = blend.mul( base.rgb ).mul( 2.0 );
+	const result2 = float( 2.0 ).mul( blend.oneMinus() ).mul( base.rgb.oneMinus() ).oneMinus();
+
+	const newColor = mix( result1, result2, L );
+
+	const A2 = base.a.mul( opacity );
+
+	const mixRGB = A2.mul( newColor.rgb );
+
+	mixRGB.addAssign( base.rgb.mul( A2.oneMinus() ) );
+
+	return vec4( mixRGB, base.a );
+
+} );
+
+addNodeElement( 'bleach', bleach );
