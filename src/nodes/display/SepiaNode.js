68b8edb49a6f155b26c0964cfdd424841cebec94
Christian Helgeson <62450112+cmhhelgeson@users.noreply.github.com>
2024-07-26 02:03:24 -0700

    Nodes: Add SepiaNode (#28961)
    * add SepiaNode
    
    * add node to export
    
    * fix
    
    * condense sepia into tslFn
    
    * remove SepiaNode from Node.js
    
    * Update SepiaNode.js
    
    ---------
    
    Co-authored-by: Michael Herzog <michael.herzog@human-interactive.org>

---
+++ src/nodes/display/SepiaNode.js
@@ -0,0 +1,21 @@
+import { addNodeElement, tslFn, vec3, vec4 } from '../shadernode/ShaderNode.js';
+import { min } from '../math/MathNode.js';
+import { dot } from '../math/MathNode.js';
+
+import { property } from '../core/PropertyNode.js';
+
+export const sepia = /*@__PURE__*/ tslFn( ( { color } ) => {
+
+	const c = property( 'vec3', 'c' ).assign( color.rgb );
+
+	// https://github.com/evanw/glfx.js/blob/master/src/filters/adjust/sepia.js
+
+	color.r = dot( c, vec3( 0.393, 0.769, 0.189 ) );
+	color.g = dot( c, vec3( 0.349, 0.686, 0.168 ) );
+	color.b = dot( c, vec3( 0.272, 0.534, 0.131 ) );
+
+	return vec4( min( vec3( 1.0 ), color.rgb ), 1.0 );
+
+} );
+
+addNodeElement( 'sepia', sepia );
