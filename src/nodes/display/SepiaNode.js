3effe01e166b3a6b701ce29c6b96ec8c10244252
sunag <sunagbrasil@gmail.com>
2024-07-26 16:39:47 -0300

    SepiaNode: Clean up (#28991)

--- src/nodes/display/SepiaNode.js
+++ src/nodes/display/SepiaNode.js
@@ -1,20 +1,17 @@
-import { addNodeElement, tslFn, vec3, vec4 } from '../shadernode/ShaderNode.js';
-import { min } from '../math/MathNode.js';
+import { addNodeElement, tslFn, vec3 } from '../shadernode/ShaderNode.js';
 import { dot } from '../math/MathNode.js';
 
-import { property } from '../core/PropertyNode.js';
+export const sepia = /*@__PURE__*/ tslFn( ( [ color ] ) => {
 
-export const sepia = /*@__PURE__*/ tslFn( ( { color } ) => {
-
-	const c = property( 'vec3', 'c' ).assign( color.rgb );
+	const c = vec3( color );
 
 	// https://github.com/evanw/glfx.js/blob/master/src/filters/adjust/sepia.js
 
-	color.r = dot( c, vec3( 0.393, 0.769, 0.189 ) );
-	color.g = dot( c, vec3( 0.349, 0.686, 0.168 ) );
-	color.b = dot( c, vec3( 0.272, 0.534, 0.131 ) );
-
-	return vec4( min( vec3( 1.0 ), color.rgb ), 1.0 );
+	return vec3(
+		dot( c, vec3( 0.393, 0.769, 0.189 ) ),
+		dot( c, vec3( 0.349, 0.686, 0.168 ) ),
+		dot( c, vec3( 0.272, 0.534, 0.131 ) )
+	);
 
 } );
 
