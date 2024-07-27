873c9d587618efffa9629232fabdf9db5adcf1b4
Michael Herzog <michael.herzog@human-interactive.org>
2024-07-27 18:45:10 +0200

    WebGPURenderer: Add `debug.getRawShaderAsync()`. (#28997)
    * WebGPURenderer: Add `debug.getRawShader()`.
    
    * Added getRawShaderAsync()
    
    ---------
    
    Co-authored-by: sunag <sunagbrasil@gmail.com>

--- src/renderers/common/Renderer.js
+++ src/renderers/common/Renderer.js
@@ -147,7 +147,23 @@ class Renderer {
 
 		this.debug = {
 			checkShaderErrors: true,
-			onShaderError: null
+			onShaderError: null,
+			getRawShaderAsync: async ( scene, camera, object ) => {
+
+				await this.compileAsync( scene, camera );
+
+				const renderList = this._renderLists.get( scene, camera );
+				const renderContext = this._renderContexts.get( scene, camera, this._renderTarget );
+
+				const material = scene.overrideMaterial || object.material;
+
+				const renderObject = this._objects.get( object, material, scene, camera, renderList.lightsNode, renderContext );
+
+				const { fragmentShader, vertexShader } = renderObject.getNodeBuilderState();
+
+				return { fragmentShader, vertexShader };
+
+			}
 		};
 
 	}
