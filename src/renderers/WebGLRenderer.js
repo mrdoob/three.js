609ba9c90aba7016e480ff861c2cf1756e5b7292
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-08-05 19:04:45 +0900

    WebGLRenderer: Add support for changing the assigned render target depth texture (#28584)
    * Add support for swapping depth textures
    
    * Don't swap textures if cube target
    
    * Don't recreate render buffers
    
    * Only attach the buffers if they have been created already
    
    * remove unused argument
    
    * Don't rebind the buffers if we can
    
    * Handle cases where the depth texture is disposed and unbound

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -2260,6 +2260,28 @@ class WebGLRenderer {
 					// Color and depth texture must be rebound in order for the swapchain to update.
 					textures.rebindTextures( renderTarget, properties.get( renderTarget.texture ).__webglTexture, properties.get( renderTarget.depthTexture ).__webglTexture );
 
+				} else if ( renderTarget.depthBuffer ) {
+
+					// check if the depth texture is already bound to the frame buffer and that it's been initialized
+					const depthTexture = renderTarget.depthTexture;
+					if ( renderTargetProperties.__boundDepthTexture !== depthTexture ) {
+
+						// check if the depth texture is compatible
+						if (
+							depthTexture !== null &&
+							properties.has( depthTexture ) &&
+							( renderTarget.width !== depthTexture.image.width || renderTarget.height !== depthTexture.image.height )
+						) {
+
+							throw new Error( 'WebGLRenderTarget: Attached DepthTexture is initialized to the incorrect size.' );
+
+						}
+
+						// Swap the depth buffer to the currently attached one
+						textures.setupDepthRenderbuffer( renderTarget );
+
+					}
+
 				}
 
 				const texture = renderTarget.texture;
