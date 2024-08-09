65c25baaf0fd0dbc9303edc253f8467983b870e6
sunag <sunagbrasil@gmail.com>
2024-08-08 21:04:58 -0300

    update

--- src/nodes/display/MotionBlurNode.js
+++ src/nodes/display/MotionBlurNode.js
@@ -2,24 +2,23 @@ import { float, int, Fn } from '../shadernode/ShaderNode.js';
 import { Loop } from '../utils/LoopNode.js';
 import { uv } from '../accessors/UVNode.js';
 
-export const motionBlur = /*#__PURE__*/ Fn( ( [ inputNode, velocity ] ) => {
+export const motionBlur = /*#__PURE__*/ Fn( ( [ inputNode, velocity, numSamples = int( 16 ) ] ) => {
 
 	const sampleColor = ( uv ) => inputNode.uv( uv );
-
-	const NUM_SAMPLES = int( 30 );
 
 	const uvs = uv();
 
 	const colorResult = sampleColor( uvs ).toVar();
+	const fSamples = float( numSamples );
 
-	Loop( { start: int( 1 ), end: NUM_SAMPLES, type: 'int', condition: '<=' }, ( { i } ) => {
+	Loop( { start: int( 1 ), end: numSamples, type: 'int', condition: '<=' }, ( { i } ) => {
 
-		const offset = velocity.mul( float( i ).div( float( NUM_SAMPLES ).sub( 1 ) ).sub( 0.5 ) );
+		const offset = velocity.mul( float( i ).div( fSamples.sub( 1 ) ).sub( 0.5 ) );
 		colorResult.addAssign( sampleColor( uvs.add( offset ) ) );
 
 	} );
 
-	colorResult.divAssign( float( NUM_SAMPLES ) );
+	colorResult.divAssign( fSamples );
 
 	return colorResult;
 
