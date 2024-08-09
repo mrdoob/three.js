3d39fdb262648c84e348523d80e43ace37db0ba7
sunag <sunagbrasil@gmail.com>
2024-08-09 00:20:24 -0300

    fix import path

--- src/nodes/utils/CubeMapNode.js
+++ src/nodes/utils/CubeMapNode.js
@@ -5,7 +5,7 @@ import { nodeProxy } from '../shadernode/ShaderNode.js';
 import { CubeTexture } from '../../textures/CubeTexture.js';
 import { cubeTexture } from '../accessors/CubeTextureNode.js';
 import CubeRenderTarget from '../../renderers/common/CubeRenderTarget.js';
-import { CubeReflectionMapping, CubeRefractionMapping, EquirectangularReflectionMapping, EquirectangularRefractionMapping } from '../../constants';
+import { CubeReflectionMapping, CubeRefractionMapping, EquirectangularReflectionMapping, EquirectangularRefractionMapping } from '../../constants.js';
 
 const _cache = new WeakMap();
 
