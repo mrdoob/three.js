41021e499c60805fca72210968d8539fd9539ab2
sunag <sunagbrasil@gmail.com>
2024-08-20 15:06:07 -0300

    no references to discard() without chaining

--- src/nodes/utils/DiscardNode.js
+++ src/nodes/utils/DiscardNode.js
@@ -5,11 +5,4 @@ import { addNodeElement } from '../shadernode/ShaderNode.js';
 export const Discard = ( conditional ) => conditional ? select( conditional, expression( 'discard' ).append() ) : expression( 'discard' ).append();
 export const Return = () => expression( 'return' ).append();
 
-export const discard = ( ...params ) => { // @deprecated, r168
-
-	console.warn( 'TSL.DiscardNode: discard() has been renamed to Discard().' );
-	return Discard( ...params );
-
-};
-
 addNodeElement( 'discard', Discard );
