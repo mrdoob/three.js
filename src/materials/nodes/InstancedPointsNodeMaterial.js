7506a353d60767f738ca1abeceb6fa11c77122eb
Christian Helgeson <62450112+cmhhelgeson@users.noreply.github.com>
2024-08-28 21:19:41 -0700

    Nodes: `InstancedPointsNodeMaterial` - Add `pointWidthNode` (#29236)
    * init
    
    * adjustment
    
    * adjust
    
    * set instancePosition.xyz to fix warning when instancePosition is accessed as a StorageBufferNode in a compute shader
    
    * cleanup and screenshot
    
    * add instance points example to exception
    
    * reduce instanced points complexity
    
    * add pointColorNode usage example, remove unused values
    
    * revision
    
    ---------

--- src/materials/nodes/InstancedPointsNodeMaterial.js
+++ src/materials/nodes/InstancedPointsNodeMaterial.js
@@ -30,6 +30,8 @@ class InstancedPointsNodeMaterial extends NodeMaterial {
 
 		this.pointColorNode = null;
 
+		this.pointWidthNode = null;
+
 		this.setDefaultValues( _defaultValues );
 
 		this.setupShaders();
@@ -56,7 +58,7 @@ class InstancedPointsNodeMaterial extends NodeMaterial {
 			//vUv = uv;
 			varying( vec2(), 'vUv' ).assign( uv() ); // @TODO: Analyze other way to do this
 
-			const instancePosition = attribute( 'instancePosition' );
+			const instancePosition = attribute( 'instancePosition' ).xyz;
 
 			// camera space
 			const mvPos = property( 'vec4', 'mvPos' );
@@ -70,7 +72,9 @@ class InstancedPointsNodeMaterial extends NodeMaterial {
 			// offset in ndc space
 			const offset = property( 'vec2', 'offset' );
 			offset.assign( positionGeometry.xy );
-			offset.assign( offset.mul( materialPointWidth ) );
+
+			offset.mulAssign( this.pointWidthNode ? this.pointWidthNode : materialPointWidth );
+
 			offset.assign( offset.div( viewport.z ) );
 			offset.y.assign( offset.y.mul( aspect ) );
 
