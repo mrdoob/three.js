ae8fbefc0bb6e6089e44f624ef3e816f87406f0b
sunag <sunagbrasil@gmail.com>
2024-09-01 21:48:40 -0300

    export color space conversion methods

--- src/nodes/display/ColorSpaceFunctions.js
+++ src/nodes/display/ColorSpaceFunctions.js
@@ -1,7 +1,7 @@
 import { mix } from '../math/MathNode.js';
 import { Fn } from '../tsl/TSLBase.js';
 
-export const sRGBToLinear = /*@__PURE__*/ Fn( ( [ color ] ) => {
+export const sRGBToLinearSRGB = /*@__PURE__*/ Fn( ( [ color ] ) => {
 
 	const a = color.mul( 0.9478672986 ).add( 0.0521327014 ).pow( 2.4 );
 	const b = color.mul( 0.0773993808 );
@@ -12,14 +12,14 @@ export const sRGBToLinear = /*@__PURE__*/ Fn( ( [ color ] ) => {
 	return rgbResult;
 
 } ).setLayout( {
-	name: 'sRGBToLinear',
+	name: 'sRGBToLinearSRGB',
 	type: 'vec3',
 	inputs: [
 		{ name: 'color', type: 'vec3' }
 	]
 } );
 
-export const LinearTosRGB = /*@__PURE__*/ Fn( ( [ color ] ) => {
+export const linearSRGBTosRGB = /*@__PURE__*/ Fn( ( [ color ] ) => {
 
 	const a = color.pow( 0.41666 ).mul( 1.055 ).sub( 0.055 );
 	const b = color.mul( 12.92 );
@@ -30,7 +30,7 @@ export const LinearTosRGB = /*@__PURE__*/ Fn( ( [ color ] ) => {
 	return rgbResult;
 
 } ).setLayout( {
-	name: 'LinearTosRGB',
+	name: 'linearSRGBTosRGB',
 	type: 'vec3',
 	inputs: [
 		{ name: 'color', type: 'vec3' }
