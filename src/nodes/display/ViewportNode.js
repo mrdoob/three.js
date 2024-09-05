429d6ff54f7c850d8966e7a5a4f7be530cfdf02c
sunag <sunagbrasil@gmail.com>
2024-09-04 22:37:22 -0300

    WebGPURenderer: Fix `viewportCoordinate` in WebGLBackend (#29326)

--- src/nodes/display/ViewportNode.js
+++ src/nodes/display/ViewportNode.js
@@ -57,7 +57,18 @@ class ViewportNode extends Node {
 
 		} else {
 
-			renderer.getDrawingBufferSize( resolution );
+			const renderTarget = renderer.getRenderTarget();
+
+			if ( renderTarget !== null ) {
+
+				resolution.width = renderTarget.width;
+				resolution.height = renderTarget.height;
+
+			} else {
+
+				renderer.getDrawingBufferSize( resolution );
+
+			}
 
 		}
 
