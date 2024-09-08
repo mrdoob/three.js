d1d002e2a41691439af80f956fd60080a95b83fe
sunag <sunagbrasil@gmail.com>
2024-09-08 13:40:40 -0300

    restore

--- src/renderers/webgl-fallback/nodes/GLSLNodeBuilder.js
+++ src/renderers/webgl-fallback/nodes/GLSLNodeBuilder.js
@@ -125,6 +125,7 @@ ${ flowData.code }
 
 			let format = isInteger ? RedIntegerFormat : RedFormat;
 
+
 			if ( itemSize === 2 ) {
 
 				format = isInteger ? RGIntegerFormat : RGFormat;
@@ -172,7 +173,7 @@ ${ flowData.code }
 			attribute.pboNode = pbo;
 			attribute.pbo = pbo.value;
 
-			this.getUniformFromNode( attribute.pboNode, 'texture', this.shaderStage, storageBufferNode.name !== '' ? storageBufferNode.name : null );
+			this.getUniformFromNode( attribute.pboNode, 'texture', this.shaderStage, this.context.label );
 
 		}
 
