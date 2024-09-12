ee17decaed0b1426dbf0972d46093daf469f49ce
sunag <sunagbrasil@gmail.com>
2024-09-12 02:33:55 -0300

    TSL: Add `workingToColorSpace`, `colorSpaceToWorking` (#29378)
    * add `workingToColorSpace`, `colorSpaceToWorking`
    
    * Update TextureNode.js
    
    * Update RenderOutputNode.js
    
    * Update RenderOutputNode.js
    
    * update
    
    * Update ColorSpaceNode.js

--- src/nodes/display/RenderOutputNode.js
+++ src/nodes/display/RenderOutputNode.js
@@ -1,7 +1,8 @@
 import TempNode from '../core/TempNode.js';
 import { addMethodChaining, nodeObject } from '../tsl/TSLCore.js';
 
-import { LinearSRGBColorSpace, SRGBColorSpace, NoToneMapping } from '../../constants.js';
+import { NoColorSpace, NoToneMapping } from '../../constants.js';
+import { ColorManagement } from '../../math/ColorManagement.js';
 
 class RenderOutputNode extends TempNode {
 
@@ -30,7 +31,7 @@ class RenderOutputNode extends TempNode {
 		// tone mapping
 
 		const toneMapping = ( this.toneMapping !== null ? this.toneMapping : context.toneMapping ) || NoToneMapping;
-		const outputColorSpace = ( this.outputColorSpace !== null ? this.outputColorSpace : context.outputColorSpace ) || LinearSRGBColorSpace;
+		const outputColorSpace = ( this.outputColorSpace !== null ? this.outputColorSpace : context.outputColorSpace ) || NoColorSpace;
 
 		if ( toneMapping !== NoToneMapping ) {
 
@@ -38,11 +39,11 @@ class RenderOutputNode extends TempNode {
 
 		}
 
-		// output color space
+		// working to output color space
 
-		if ( outputColorSpace === SRGBColorSpace ) {
+		if ( outputColorSpace !== NoColorSpace && outputColorSpace !== ColorManagement.workingColorSpace ) {
 
-			outputNode = outputNode.toOutputColorSpace( outputColorSpace );
+			outputNode = outputNode.workingToColorSpace( outputColorSpace );
 
 		}
 
