0acae94949fab0571b855c35beac6b8c0cd8d3aa
mutefish0 <648262030@qq.com>
2024-09-29 00:25:42 +0800

    Ignore `diagnostic()` method in deno

--- src/renderers/webgpu/nodes/WGSLNodeBuilder.js
+++ src/renderers/webgpu/nodes/WGSLNodeBuilder.js
@@ -1319,13 +1319,10 @@ fn main( ${shaderData.attributes} ) -> VaryingsStruct {
 	}
 
 	_getWGSLFragmentCode( shaderData ) {
-		const diagnostic = typeof Deno === 'object' ? '' : 'diagnostic( off, derivative_uniformity );';
 
 		return `${ this.getSignature() }
 // global
 ${ diagnostics }
-
-${diagnostic}
 
 // uniforms
 ${shaderData.uniforms}
