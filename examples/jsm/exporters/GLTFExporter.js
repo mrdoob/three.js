8832969439b67c7ad44cb6f35c7895dffb708220
Mugen87 <michael.herzog@human-interactive.org>
2024-10-19 14:39:55 +0200

    GLTFExporter: Clean up.

--- examples/jsm/exporters/GLTFExporter.js
+++ examples/jsm/exporters/GLTFExporter.js
@@ -1524,8 +1524,7 @@ class GLTFWriter {
 
 		} else {
 
-			materialDef.pbrMetallicRoughness.metallicFactor = 1;
-			materialDef.pbrMetallicRoughness.roughnessFactor = 1;
+			materialDef.pbrMetallicRoughness.metallicFactor = 0; // default roughness is 1
 
 		}
 
