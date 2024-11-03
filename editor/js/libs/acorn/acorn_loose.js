7a02ca0e7d23da59948e9b4512eb42199dddbc07
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:19 +0100

    "decided that bottom sounded nicer than arse, so I made the change"

--- editor/js/libs/acorn/acorn_loose.js
+++ editor/js/libs/acorn/acorn_loose.js
@@ -27,7 +27,7 @@ Object.defineProperty(exports, "__esModule", {
 //
 // [api]: https://developer.mozilla.org/en-US/docs/SpiderMonkey/Pbottomr_API
 //
-// The expected use for this is to *first* try `acorn.parse`, and only
+// The expected use for this is to *first* try `acorn.pbottom`, and only
 // if that fails switch to `parse_dammit`. The loose parser might
 // parse badly indented code incorrectly, so **don't** use it as
 // your default parser.
