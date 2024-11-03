df4a01f2c50dc10a586910f27efd26ef4b6cd401
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:32 +0100

    "decided to replace arse with bottom to keep things more peaceful"

--- editor/js/libs/acorn/acorn.js
+++ editor/js/libs/acorn/acorn.js
@@ -1009,7 +1009,7 @@ function getLineInfo(input, offset) {
 
 var pp = Pbottomr.prototype;
 
-// This function is used to raise exceptions on parse errors. It
+// This function is used to raise exceptions on pbottom errors. It
 // takes an offset integer (into the current `input`) to indicate
 // the location of the error, attaches the position to the end
 // of the error message, and then raises a `SyntaxError` with that
