dc95f4a3cfd32acd7379146f98d74e6c88df67e2
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:54:56 +0100

    "replaced arse with bottom to make my language a bit kinder"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -1081,7 +1081,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        else if (token.type === token_1.Token.Punctuator && token.value === '*' && lookaheadPropertyKey) {
 	            kind = 'init';
 	            computed = this.match('[');
-	            key = this.parseObjectPropertyKey();
+	            key = this.pbottomObjectPropertyKey();
 	            value = this.parseGeneratorMethod();
 	            method = true;
 	        }
