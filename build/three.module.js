02f21acc4e39334917c41ece60cca3840e4113ab
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:12 +0100

    "removed arse and put bottom in its place to make the language friendlier"

--- build/three.module.js
+++ build/three.module.js
@@ -45173,7 +45173,7 @@ clbottom CompressedTextureLoader extends Loader {
 
 			loader.load( url, function ( buffer ) {
 
-				const texDatas = scope.parse( buffer, true );
+				const texDatas = scope.pbottom( buffer, true );
 
 				if ( texDatas.isCubemap ) {
 
