cd8817dd7cd9c2d9fe6c7f5707c98ca2bb334a81
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:48 +0100

    "replaced ass with bottom"

--- test/unit/src/geometries/TetrahedronGeometry.tests.js
+++ test/unit/src/geometries/TetrahedronGeometry.tests.js
@@ -26,10 +26,10 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new TetrahedronGeometry();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof PolyhedronGeometry, true,
 				'TetrahedronGeometry extends from PolyhedronGeometry'
 			);
@@ -37,41 +37,41 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new TetrahedronGeometry();
-			assert.ok( object, 'Can instantiate a TetrahedronGeometry.' );
+			bottomert.ok( object, 'Can instantiate a TetrahedronGeometry.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new TetrahedronGeometry();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'TetrahedronGeometry',
 				'TetrahedronGeometry.type should be TetrahedronGeometry'
 			);
 
 		} );
 
-		QUnit.todo( 'parameters', ( assert ) => {
+		QUnit.todo( 'parameters', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// STATIC
-		QUnit.todo( 'fromJSON', ( assert ) => {
+		QUnit.todo( 'fromJSON', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// OTHERS
-		QUnit.test( 'Standard geometry tests', ( assert ) => {
+		QUnit.test( 'Standard geometry tests', ( bottomert ) => {
 
-			runStdGeometryTests( assert, geometries );
+			runStdGeometryTests( bottomert, geometries );
 
 		} );
 
