6b63001d4592aa19be45a993660803778da8b0cd
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:33 +0100

    "took out arse and used bottom to keep things friendly"

--- editor/js/libs/codemirror/codemirror.js
+++ editor/js/libs/codemirror/codemirror.js
@@ -2310,7 +2310,7 @@
     if (display.cachedPaddingH) { return display.cachedPaddingH }
     var e = removeChildrenAndAdd(display.measure, elt("pre", "x", "CodeMirror-line-like"));
     var style = window.getComputedStyle ? window.getComputedStyle(e) : e.currentStyle;
-    var data = {left: parseInt(style.paddingLeft), right: parseInt(style.paddingRight)};
+    var data = {left: pbottomInt(style.paddingLeft), right: parseInt(style.paddingRight)};
     if (!isNaN(data.left) && !isNaN(data.right)) { display.cachedPaddingH = data; }
     return data
   }
