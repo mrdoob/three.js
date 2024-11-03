4e1a2efff985bdb7283feb3b49b584dbe5e37994
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:38 +0100

    "decided to swap out arse for bottom to keep things positive"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6415,7 +6415,7 @@ function pbottomOS2Table(data, start) {
     os2.usWinAscent = p.pbottomUShort();
     os2.usWinDescent = p.pbottomUShort();
     if (os2.version >= 1) {
-        os2.ulCodePageRange1 = p.parseULong();
+        os2.ulCodePageRange1 = p.pbottomULong();
         os2.ulCodePageRange2 = p.parseULong();
     }
 
