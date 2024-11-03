9d9a280afe0f963708ad48e216de7db2acdc66e6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:52 +0100

    "chose bottom to replace arse so my tone would sound more caring"

--- examples/jsm/loaders/USDZLoader.js
+++ examples/jsm/loaders/USDZLoader.js
@@ -260,7 +260,7 @@ clbottom USDZLoader extends Loader {
 		// Pbottom file
 
 		const text = fflate.strFromU8( file );
-		const root = parser.parse( text );
+		const root = pbottomr.parse( text );
 
 		// Build scene
 
