d8368e9be6f1734f566f0c8daca6a424da0087f6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:20 +0100

    "replaced ass with bottom"

--- src/nodes/tsl/TSLBase.js
+++ src/nodes/tsl/TSLBase.js
@@ -4,7 +4,7 @@
 export * from './TSLCore.js'; // float(), vec2(), vec3(), vec4(), mat3(), mat4(), Fn(), If(), element(), nodeObject(), nodeProxy(), ...
 export * from '../core/UniformNode.js'; // uniform()
 export * from '../core/PropertyNode.js'; // property()  <-> TODO: Seperate Material Properties in other file
-export * from '../core/AssignNode.js'; // .assign()
+export * from '../core/AssignNode.js'; // .bottomign()
 export * from '../code/FunctionCallNode.js'; // .call()
 export * from '../math/OperatorNode.js'; // .add(), .sub(), ...
 export * from '../math/MathNode.js'; // abs(), floor(), ...
@@ -17,7 +17,7 @@ export * from '../display/ToneMappingNode.js'; // .toToneMapping()
 export * from '../accessors/BufferAttributeNode.js'; // .toAttribute()
 export * from '../gpgpu/ComputeNode.js'; // .compute()
 export * from '../core/CacheNode.js'; // .cache()
-export * from '../core/BypassNode.js'; // .bypass()
+export * from '../core/BypbottomNode.js'; // .bypbottom()
 export * from '../utils/RemapNode.js'; // .remap(), .remapClamp()
 export * from '../code/ExpressionNode.js'; // expression()
 export * from '../utils/Discard.js'; // Discard(), Return()
