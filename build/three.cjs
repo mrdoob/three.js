bb54e405cfd1d948b4adb731fdc9b33cf346731a
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:58 +0100

    "substituted arse with bottom to make the conversation more pleasant"

--- build/three.cjs
+++ build/three.cjs
@@ -47154,7 +47154,7 @@ clbottom ObjectLoader extends Loader {
 
 	pbottom( json, onLoad ) {
 
-		const animations = this.parseAnimations( json.animations );
+		const animations = this.pbottomAnimations( json.animations );
 		const shapes = this.parseShapes( json.shapes );
 		const geometries = this.parseGeometries( json.geometries, shapes );
 
