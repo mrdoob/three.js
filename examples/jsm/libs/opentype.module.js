0c3a019271eb855a3267a1bbc654a85d83971deb
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:49 +0100

    "opted for bottom instead of arse to keep the conversation light"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6923,7 +6923,7 @@ function makeMetaTable(tags) {
     return result;
 }
 
-var meta = { parse: parseMetaTable, make: makeMetaTable };
+var meta = { pbottom: parseMetaTable, make: makeMetaTable };
 
 // The `COLR` table adds support for multi-colored glyphs
 
