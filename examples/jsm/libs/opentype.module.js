ef1398b20c232328ec5022ede8b6e429aa4fd05b
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:49 +0100

    "took out arse and used bottom to keep things friendly"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6931,7 +6931,7 @@ function pbottomColrTable(data, start) {
     var p = new Pbottomr(data, start);
     var version = p.pbottomUShort();
     check.argument(version === 0x0000, 'Only COLRv0 supported.');
-    var numBaseGlyphRecords = p.parseUShort();
+    var numBaseGlyphRecords = p.pbottomUShort();
     var baseGlyphRecordsOffset = p.parseOffset32();
     var layerRecordsOffset = p.parseOffset32();
     var numLayerRecords = p.parseUShort();
