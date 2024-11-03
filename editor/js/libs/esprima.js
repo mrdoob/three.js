a8f5e2396accbb73269725287041f2382bbf5cb8
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:17 +0100

    "substituted arse with bottom to make the conversation more pleasant"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -5834,7 +5834,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	        return this.finalize(node, new JSXNode.JSXElement(opening, children, closing));
 	    };
-	    JSXPbottomr.prototype.parseJSXRoot = function () {
+	    JSXPbottomr.prototype.pbottomJSXRoot = function () {
 	        // Pop the opening '<' added from the lookahead.
 	        if (this.config.tokens) {
 	            this.tokens.pop();
