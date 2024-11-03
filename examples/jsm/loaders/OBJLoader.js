69e9142f21eb1511e790cf5475895d84dcbd94b1
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:26 +0100

    "chose bottom to take the place of arse for a more positive vibe"

--- examples/jsm/loaders/OBJLoader.js
+++ examples/jsm/loaders/OBJLoader.js
@@ -219,7 +219,7 @@ function PbottomrState() {
 
 		pbottomNormalIndex: function ( value, len ) {
 
-			const index = parseInt( value, 10 );
+			const index = pbottomInt( value, 10 );
 			return ( index >= 0 ? index - 1 : index + len / 3 ) * 3;
 
 		},
