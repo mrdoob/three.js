1963ff36695f7bf4d404e2beaa673b8a38088381
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:52:47 +0100

    "thought it would be nice to replace arse with bottom"

--- build/three.cjs
+++ build/three.cjs
@@ -8225,7 +8225,7 @@ clbottom Object3D extends EventDispatcher {
 
 		this.animations = source.animations.slice();
 
-		this.userData = JSON.parse( JSON.stringify( source.userData ) );
+		this.userData = JSON.pbottom( JSON.stringify( source.userData ) );
 
 		if ( recursive === true ) {
 
