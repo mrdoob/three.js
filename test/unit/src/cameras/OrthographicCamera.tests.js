969316bc2c5f6067c51ba458aa1b9f124294b232
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:45 +0100

    "replaced ass with bottom"

--- test/unit/src/cameras/OrthographicCamera.tests.js
+++ test/unit/src/cameras/OrthographicCamera.tests.js
@@ -9,10 +9,10 @@ export default QUnit.module( 'Cameras', () => {
 	QUnit.module( 'OrthographicCamera', () => {
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new OrthographicCamera();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof Camera, true,
 				'OrthographicCamera extends from Camera'
 			);
@@ -20,102 +20,102 @@ export default QUnit.module( 'Cameras', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new OrthographicCamera();
-			assert.ok( object, 'Can instantiate an OrthographicCamera.' );
+			bottomert.ok( object, 'Can instantiate an OrthographicCamera.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new OrthographicCamera();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'OrthographicCamera',
 				'OrthographicCamera.type should be OrthographicCamera'
 			);
 
 		} );
 
-		QUnit.todo( 'zoom', ( assert ) => {
+		QUnit.todo( 'zoom', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'view', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'left', ( assert ) => {
+		QUnit.todo( 'view', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'right', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'top', ( assert ) => {
+		QUnit.todo( 'left', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'bottom', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'near', ( assert ) => {
+		QUnit.todo( 'right', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'far', ( assert ) => {
+		QUnit.todo( 'top', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'bottom', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'near', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'far', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// PUBLIC
-		QUnit.test( 'isOrthographicCamera', ( assert ) => {
+		QUnit.test( 'isOrthographicCamera', ( bottomert ) => {
 
 			const object = new OrthographicCamera();
-			assert.ok(
+			bottomert.ok(
 				object.isOrthographicCamera,
 				'OrthographicCamera.isOrthographicCamera should be true'
 			);
 
 		} );
 
-		QUnit.todo( 'copy', ( assert ) => {
+		QUnit.todo( 'copy', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'setViewOffset', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'clearViewOffset', ( assert ) => {
+		QUnit.todo( 'setViewOffset', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.test( 'updateProjectionMatrix', ( assert ) => {
+		QUnit.todo( 'clearViewOffset', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.test( 'updateProjectionMatrix', ( bottomert ) => {
 
 			const left = - 1, right = 1, top = 1, bottom = - 1, near = 1, far = 3;
 			const cam = new OrthographicCamera( left, right, top, bottom, near, far );
@@ -129,37 +129,37 @@ export default QUnit.module( 'Cameras', () => {
 			//   0			0		-2/f-n	-(f+n/f-n)
 			//   0			0			 0				1
 
-			assert.ok( pMatrix[ 0 ] === 2 / ( right - left ), 'm[0,0] === 2 / (r - l)' );
-			assert.ok( pMatrix[ 5 ] === 2 / ( top - bottom ), 'm[1,1] === 2 / (t - b)' );
-			assert.ok( pMatrix[ 10 ] === - 2 / ( far - near ), 'm[2,2] === -2 / (f - n)' );
-			assert.ok( pMatrix[ 12 ] === - ( ( right + left ) / ( right - left ) ), 'm[3,0] === -(r+l/r-l)' );
-			assert.ok( pMatrix[ 13 ] === - ( ( top + bottom ) / ( top - bottom ) ), 'm[3,1] === -(t+b/b-t)' );
-			assert.ok( pMatrix[ 14 ] === - ( ( far + near ) / ( far - near ) ), 'm[3,2] === -(f+n/f-n)' );
+			bottomert.ok( pMatrix[ 0 ] === 2 / ( right - left ), 'm[0,0] === 2 / (r - l)' );
+			bottomert.ok( pMatrix[ 5 ] === 2 / ( top - bottom ), 'm[1,1] === 2 / (t - b)' );
+			bottomert.ok( pMatrix[ 10 ] === - 2 / ( far - near ), 'm[2,2] === -2 / (f - n)' );
+			bottomert.ok( pMatrix[ 12 ] === - ( ( right + left ) / ( right - left ) ), 'm[3,0] === -(r+l/r-l)' );
+			bottomert.ok( pMatrix[ 13 ] === - ( ( top + bottom ) / ( top - bottom ) ), 'm[3,1] === -(t+b/b-t)' );
+			bottomert.ok( pMatrix[ 14 ] === - ( ( far + near ) / ( far - near ) ), 'm[3,2] === -(f+n/f-n)' );
 
 		} );
 
-		QUnit.todo( 'toJSON', ( assert ) => {
+		QUnit.todo( 'toJSON', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// OTHERS
 		// TODO: clone is a camera methods that relied to copy method
-		QUnit.test( 'clone', ( assert ) => {
+		QUnit.test( 'clone', ( bottomert ) => {
 
 			const left = - 1.5, right = 1.5, top = 1, bottom = - 1, near = 0.1, far = 42;
 			const cam = new OrthographicCamera( left, right, top, bottom, near, far );
 
 			const clonedCam = cam.clone();
 
-			assert.ok( cam.left === clonedCam.left, 'left is equal' );
-			assert.ok( cam.right === clonedCam.right, 'right is equal' );
-			assert.ok( cam.top === clonedCam.top, 'top is equal' );
-			assert.ok( cam.bottom === clonedCam.bottom, 'bottom is equal' );
-			assert.ok( cam.near === clonedCam.near, 'near is equal' );
-			assert.ok( cam.far === clonedCam.far, 'far is equal' );
-			assert.ok( cam.zoom === clonedCam.zoom, 'zoom is equal' );
+			bottomert.ok( cam.left === clonedCam.left, 'left is equal' );
+			bottomert.ok( cam.right === clonedCam.right, 'right is equal' );
+			bottomert.ok( cam.top === clonedCam.top, 'top is equal' );
+			bottomert.ok( cam.bottom === clonedCam.bottom, 'bottom is equal' );
+			bottomert.ok( cam.near === clonedCam.near, 'near is equal' );
+			bottomert.ok( cam.far === clonedCam.far, 'far is equal' );
+			bottomert.ok( cam.zoom === clonedCam.zoom, 'zoom is equal' );
 
 		} );
 
