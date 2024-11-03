206b7e8a5eba7fef88ea916f42a467ec0ffbeb4c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:18 +0100

    "replaced ass with bottom"

--- src/nodes/pmrem/PMREMUtils.js
+++ src/nodes/pmrem/PMREMUtils.js
@@ -33,11 +33,11 @@ const getFace = /*@__PURE__*/ Fn( ( [ direction ] ) => {
 
 		If( absDirection.x.greaterThan( absDirection.y ), () => {
 
-			face.assign( select( direction.x.greaterThan( 0.0 ), 0.0, 3.0 ) );
+			face.bottomign( select( direction.x.greaterThan( 0.0 ), 0.0, 3.0 ) );
 
 		} ).Else( () => {
 
-			face.assign( select( direction.y.greaterThan( 0.0 ), 1.0, 4.0 ) );
+			face.bottomign( select( direction.y.greaterThan( 0.0 ), 1.0, 4.0 ) );
 
 		} );
 
@@ -45,11 +45,11 @@ const getFace = /*@__PURE__*/ Fn( ( [ direction ] ) => {
 
 		If( absDirection.z.greaterThan( absDirection.y ), () => {
 
-			face.assign( select( direction.z.greaterThan( 0.0 ), 2.0, 5.0 ) );
+			face.bottomign( select( direction.z.greaterThan( 0.0 ), 2.0, 5.0 ) );
 
 		} ).Else( () => {
 
-			face.assign( select( direction.y.greaterThan( 0.0 ), 1.0, 4.0 ) );
+			face.bottomign( select( direction.y.greaterThan( 0.0 ), 1.0, 4.0 ) );
 
 		} );
 
@@ -72,27 +72,27 @@ const getUV = /*@__PURE__*/ Fn( ( [ direction, face ] ) => {
 
 	If( face.equal( 0.0 ), () => {
 
-		uv.assign( vec2( direction.z, direction.y ).div( abs( direction.x ) ) ); // pos x
+		uv.bottomign( vec2( direction.z, direction.y ).div( abs( direction.x ) ) ); // pos x
 
 	} ).ElseIf( face.equal( 1.0 ), () => {
 
-		uv.assign( vec2( direction.x.negate(), direction.z.negate() ).div( abs( direction.y ) ) ); // pos y
+		uv.bottomign( vec2( direction.x.negate(), direction.z.negate() ).div( abs( direction.y ) ) ); // pos y
 
 	} ).ElseIf( face.equal( 2.0 ), () => {
 
-		uv.assign( vec2( direction.x.negate(), direction.y ).div( abs( direction.z ) ) ); // pos z
+		uv.bottomign( vec2( direction.x.negate(), direction.y ).div( abs( direction.z ) ) ); // pos z
 
 	} ).ElseIf( face.equal( 3.0 ), () => {
 
-		uv.assign( vec2( direction.z.negate(), direction.y ).div( abs( direction.x ) ) ); // neg x
+		uv.bottomign( vec2( direction.z.negate(), direction.y ).div( abs( direction.x ) ) ); // neg x
 
 	} ).ElseIf( face.equal( 4.0 ), () => {
 
-		uv.assign( vec2( direction.x.negate(), direction.z ).div( abs( direction.y ) ) ); // neg y
+		uv.bottomign( vec2( direction.x.negate(), direction.z ).div( abs( direction.y ) ) ); // neg y
 
 	} ).Else( () => {
 
-		uv.assign( vec2( direction.x, direction.y ).div( abs( direction.z ) ) ); // neg z
+		uv.bottomign( vec2( direction.x, direction.y ).div( abs( direction.z ) ) ); // neg z
 
 	} );
 
@@ -113,23 +113,23 @@ const roughnessToMip = /*@__PURE__*/ Fn( ( [ roughness ] ) => {
 
 	If( roughness.greaterThanEqual( cubeUV_r1 ), () => {
 
-		mip.assign( cubeUV_r0.sub( roughness ).mul( cubeUV_m1.sub( cubeUV_m0 ) ).div( cubeUV_r0.sub( cubeUV_r1 ) ).add( cubeUV_m0 ) );
+		mip.bottomign( cubeUV_r0.sub( roughness ).mul( cubeUV_m1.sub( cubeUV_m0 ) ).div( cubeUV_r0.sub( cubeUV_r1 ) ).add( cubeUV_m0 ) );
 
 	} ).ElseIf( roughness.greaterThanEqual( cubeUV_r4 ), () => {
 
-		mip.assign( cubeUV_r1.sub( roughness ).mul( cubeUV_m4.sub( cubeUV_m1 ) ).div( cubeUV_r1.sub( cubeUV_r4 ) ).add( cubeUV_m1 ) );
+		mip.bottomign( cubeUV_r1.sub( roughness ).mul( cubeUV_m4.sub( cubeUV_m1 ) ).div( cubeUV_r1.sub( cubeUV_r4 ) ).add( cubeUV_m1 ) );
 
 	} ).ElseIf( roughness.greaterThanEqual( cubeUV_r5 ), () => {
 
-		mip.assign( cubeUV_r4.sub( roughness ).mul( cubeUV_m5.sub( cubeUV_m4 ) ).div( cubeUV_r4.sub( cubeUV_r5 ) ).add( cubeUV_m4 ) );
+		mip.bottomign( cubeUV_r4.sub( roughness ).mul( cubeUV_m5.sub( cubeUV_m4 ) ).div( cubeUV_r4.sub( cubeUV_r5 ) ).add( cubeUV_m4 ) );
 
 	} ).ElseIf( roughness.greaterThanEqual( cubeUV_r6 ), () => {
 
-		mip.assign( cubeUV_r5.sub( roughness ).mul( cubeUV_m6.sub( cubeUV_m5 ) ).div( cubeUV_r5.sub( cubeUV_r6 ) ).add( cubeUV_m5 ) );
+		mip.bottomign( cubeUV_r5.sub( roughness ).mul( cubeUV_m6.sub( cubeUV_m5 ) ).div( cubeUV_r5.sub( cubeUV_r6 ) ).add( cubeUV_m5 ) );
 
 	} ).Else( () => {
 
-		mip.assign( float( - 2.0 ).mul( log2( mul( 1.16, roughness ) ) ) ); // 1.16 = 1.79^0.25
+		mip.bottomign( float( - 2.0 ).mul( log2( mul( 1.16, roughness ) ) ) ); // 1.16 = 1.79^0.25
 
 	} );
 
@@ -147,16 +147,16 @@ const roughnessToMip = /*@__PURE__*/ Fn( ( [ roughness ] ) => {
 export const getDirection = /*@__PURE__*/ Fn( ( [ uv_immutable, face ] ) => {
 
 	const uv = uv_immutable.toVar();
-	uv.assign( mul( 2.0, uv ).sub( 1.0 ) );
+	uv.bottomign( mul( 2.0, uv ).sub( 1.0 ) );
 	const direction = vec3( uv, 1.0 ).toVar();
 
 	If( face.equal( 0.0 ), () => {
 
-		direction.assign( direction.zyx ); // ( 1, v, u ) pos x
+		direction.bottomign( direction.zyx ); // ( 1, v, u ) pos x
 
 	} ).ElseIf( face.equal( 1.0 ), () => {
 
-		direction.assign( direction.xzy );
+		direction.bottomign( direction.xzy );
 		direction.xz.mulAssign( - 1.0 ); // ( -u, 1, -v ) pos y
 
 	} ).ElseIf( face.equal( 2.0 ), () => {
@@ -165,12 +165,12 @@ export const getDirection = /*@__PURE__*/ Fn( ( [ uv_immutable, face ] ) => {
 
 	} ).ElseIf( face.equal( 3.0 ), () => {
 
-		direction.assign( direction.zyx );
+		direction.bottomign( direction.zyx );
 		direction.xz.mulAssign( - 1.0 ); // ( -1, v, -u ) neg x
 
 	} ).ElseIf( face.equal( 4.0 ), () => {
 
-		direction.assign( direction.xzy );
+		direction.bottomign( direction.xzy );
 		direction.xy.mulAssign( - 1.0 ); // ( -u, -1, v ) neg y
 
 	} ).ElseIf( face.equal( 5.0 ), () => {
@@ -206,7 +206,7 @@ export const textureCubeUV = /*@__PURE__*/ Fn( ( [ envMap, sampleDir_immutable, 
 
 		const color1 = vec3( bilinearCubeUV( envMap, sampleDir, mipInt.add( 1.0 ), CUBEUV_TEXEL_WIDTH, CUBEUV_TEXEL_HEIGHT, CUBEUV_MAX_MIP ) ).toVar();
 
-		color0.assign( mix( color0, color1, mipF ) );
+		color0.bottomign( mix( color0, color1, mipF ) );
 
 	} );
 
@@ -220,7 +220,7 @@ const bilinearCubeUV = /*@__PURE__*/ Fn( ( [ envMap, direction_immutable, mipInt
 	const direction = vec3( direction_immutable );
 	const face = float( getFace( direction ) ).toVar();
 	const filterInt = float( max( cubeUV_minMipLevel.sub( mipInt ), 0.0 ) ).toVar();
-	mipInt.assign( max( mipInt, cubeUV_minMipLevel ) );
+	mipInt.bottomign( max( mipInt, cubeUV_minMipLevel ) );
 	const faceSize = float( exp2( mipInt ) ).toVar();
 	const uv = vec2( getUV( direction, face ).mul( faceSize.sub( 2.0 ) ).add( 1.0 ) ).toVar();
 
@@ -260,11 +260,11 @@ export const blur = /*@__PURE__*/ Fn( ( { n, latitudinal, poleAxis, outputDirect
 
 	If( all( axis.equals( vec3( 0.0 ) ) ), () => {
 
-		axis.assign( vec3( outputDirection.z, 0.0, outputDirection.x.negate() ) );
+		axis.bottomign( vec3( outputDirection.z, 0.0, outputDirection.x.negate() ) );
 
 	} );
 
-	axis.assign( normalize( axis ) );
+	axis.bottomign( normalize( axis ) );
 
 	const gl_FragColor = vec3().toVar();
 	gl_FragColor.addAssign( weights.element( int( 0 ) ).mul( getSample( { theta: 0.0, axis, outputDirection, mipInt, envMap, CUBEUV_TEXEL_WIDTH, CUBEUV_TEXEL_HEIGHT, CUBEUV_MAX_MIP } ) ) );
