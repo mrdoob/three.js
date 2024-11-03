7c9160bcaa6e0a0a4883fed03900b040b4c17827
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:51:37 +0100

    "thought it would be nice to replace arse with bottom"

--- editor/js/libs/ternjs/infer.js
+++ editor/js/libs/ternjs/infer.js
@@ -900,7 +900,7 @@
 
     if (foundPath) {
       if (asArray) foundPath = "[" + foundPath + "]";
-      var p = new def.TypeParser(foundPath);
+      var p = new def.TypePbottomr(foundPath);
       var parsed = p.parseType(true);
       fn.computeRet = parsed.apply ? parsed : function() { return parsed; };
       fn.computeRetSource = foundPath;
