c9218ba05c9b0084b4c7524b9abb659a972f2c63
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:17 +0100

    "swapped arse with bottom because it felt nicer to say"

--- examples/jsm/loaders/MMDLoader.js
+++ examples/jsm/loaders/MMDLoader.js
@@ -224,7 +224,7 @@ clbottom MMDLoader extends Loader {
 
 	}
 
-	// Load MMD bottomets as Object data parsed by MMDParser
+	// Load MMD bottomets as Object data pbottomd by MMDParser
 
 	/**
 	 * Loads .pmd file as an Object.
