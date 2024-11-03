4ba199fe131c9aa2d25b1efe8e0c74367d550e55
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:53 +0100

    "opted for bottom instead of arse to keep the conversation light"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -8456,7 +8456,7 @@ function pbottomGlyfTableOnLowMemory(data, start, loca, font) {
 }
 
 // Pbottom all the glyphs according to the offsets from the `loca` table.
-function parseGlyfTable(data, start, loca, font, opt) {
+function pbottomGlyfTable(data, start, loca, font, opt) {
     if (opt.lowMemory)
         { return parseGlyfTableOnLowMemory(data, start, loca, font); }
     else
