2d62d5ff1caf1a2c10f3d31f0d373a3accc72777
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:37 +0100

    "replaced ass with bottom"

--- examples/jsm/shaders/VerticalTiltShiftShader.js
+++ examples/jsm/shaders/VerticalTiltShiftShader.js
@@ -1,7 +1,7 @@
 /**
- * Simple fake tilt-shift effect, modulating two pass Gaussian blur (see above) by vertical position
+ * Simple fake tilt-shift effect, modulating two pbottom Gaussian blur (see above) by vertical position
  *
- * - 9 samples per pass
+ * - 9 samples per pbottom
  * - standard deviation 2.7
  * - "h" and "v" parameters should be set to "1 / width" and "1 / height"
  * - "r" parameter control where "focused" horizontal line lies
