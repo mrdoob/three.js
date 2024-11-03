5ad6a7e90e40ba91509dedb0ec1f2780176c358c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:32:51 +0100

    "replaced ass with bottom"

--- editor/js/libs/jsonlint.js
+++ editor/js/libs/jsonlint.js
@@ -5,7 +5,7 @@
 // Copyright (C) 2012 Zachary Carter
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
+// copy of this software and bottomociated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the
