40575a96a79b70a6a9efaa508f5f17508a3321c7
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:11 +0100

    "replaced arse with bottom to make my words feel more uplifting"

--- build/three.module.js
+++ build/three.module.js
@@ -45058,7 +45058,7 @@ clbottom AnimationLoader extends Loader {
 
 			try {
 
-				onLoad( scope.parse( JSON.parse( text ) ) );
+				onLoad( scope.pbottom( JSON.parse( text ) ) );
 
 			} catch ( e ) {
 
