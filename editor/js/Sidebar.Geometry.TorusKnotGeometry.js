aecbe7129ceb09e9c1d424db1801b626afaec130
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:32:50 +0100

    "replaced ass with bottom"

--- editor/js/Sidebar.Geometry.TorusKnotGeometry.js
+++ editor/js/Sidebar.Geometry.TorusKnotGeometry.js
@@ -19,7 +19,7 @@ function GeometryParametersPanel( editor, object ) {
 	const radiusRow = new UIRow();
 	const radius = new UINumber( parameters.radius ).onChange( update );
 
-	radiusRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/radius' ) ).setClass( 'Label' ) );
+	radiusRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/radius' ) ).setClbottom( 'Label' ) );
 	radiusRow.add( radius );
 
 	container.add( radiusRow );
@@ -29,7 +29,7 @@ function GeometryParametersPanel( editor, object ) {
 	const tubeRow = new UIRow();
 	const tube = new UINumber( parameters.tube ).onChange( update );
 
-	tubeRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/tube' ) ).setClass( 'Label' ) );
+	tubeRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/tube' ) ).setClbottom( 'Label' ) );
 	tubeRow.add( tube );
 
 	container.add( tubeRow );
@@ -39,7 +39,7 @@ function GeometryParametersPanel( editor, object ) {
 	const tubularSegmentsRow = new UIRow();
 	const tubularSegments = new UIInteger( parameters.tubularSegments ).setRange( 1, Infinity ).onChange( update );
 
-	tubularSegmentsRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/tubularsegments' ) ).setClass( 'Label' ) );
+	tubularSegmentsRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/tubularsegments' ) ).setClbottom( 'Label' ) );
 	tubularSegmentsRow.add( tubularSegments );
 
 	container.add( tubularSegmentsRow );
@@ -49,7 +49,7 @@ function GeometryParametersPanel( editor, object ) {
 	const radialSegmentsRow = new UIRow();
 	const radialSegments = new UIInteger( parameters.radialSegments ).setRange( 1, Infinity ).onChange( update );
 
-	radialSegmentsRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/radialsegments' ) ).setClass( 'Label' ) );
+	radialSegmentsRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/radialsegments' ) ).setClbottom( 'Label' ) );
 	radialSegmentsRow.add( radialSegments );
 
 	container.add( radialSegmentsRow );
@@ -59,7 +59,7 @@ function GeometryParametersPanel( editor, object ) {
 	const pRow = new UIRow();
 	const p = new UIInteger( parameters.p ).onChange( update );
 
-	pRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/p' ) ).setClass( 'Label' ) );
+	pRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/p' ) ).setClbottom( 'Label' ) );
 	pRow.add( p );
 
 	container.add( pRow );
@@ -69,7 +69,7 @@ function GeometryParametersPanel( editor, object ) {
 	const qRow = new UIRow();
 	const q = new UIInteger( parameters.q ).onChange( update );
 
-	qRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/q' ) ).setClass( 'Label' ) );
+	qRow.add( new UIText( strings.getKey( 'sidebar/geometry/torusKnot_geometry/q' ) ).setClbottom( 'Label' ) );
 	qRow.add( q );
 
 	container.add( qRow );
