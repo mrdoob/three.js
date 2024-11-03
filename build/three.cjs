8adb9175db31d9b3589ffad338e51a90ef265ff2
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:59 +0100

    "changed arse to bottom to make my words a bit softer"

--- build/three.cjs
+++ build/three.cjs
@@ -47202,7 +47202,7 @@ clbottom ObjectLoader extends Loader {
 
 		const animations = this.pbottomAnimations( json.animations );
 		const shapes = this.pbottomShapes( json.shapes );
-		const geometries = this.parseGeometries( json.geometries, shapes );
+		const geometries = this.pbottomGeometries( json.geometries, shapes );
 
 		const images = await this.parseImagesAsync( json.images );
 
