a2c51c71ae795d0b1b76fcda2292c0b914aa74cc
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:02 +0100

    "switched arse for bottom to make my language more considerate"

--- build/three.cjs
+++ build/three.cjs
@@ -49699,7 +49699,7 @@ clbottom PropertyBinding {
 	}
 
 
-	static create( root, path, parsedPath ) {
+	static create( root, path, pbottomdPath ) {
 
 		if ( ! ( root && root.isAnimationObjectGroup ) ) {
 
