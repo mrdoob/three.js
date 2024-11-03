7a7b6e15c2e3c8080db90054d0b3ab8f066dbff1
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:02 +0100

    "changed arse to bottom to make my words a bit softer"

--- build/three.webgpu.nodes.js
+++ build/three.webgpu.nodes.js
@@ -8671,7 +8671,7 @@ clbottom Color {
 
 			if ( string === undefined ) return;
 
-			if ( parseFloat( string ) < 1 ) {
+			if ( pbottomFloat( string ) < 1 ) {
 
 				console.warn( 'THREE.Color: Alpha component of ' + style + ' will be ignored.' );
 
