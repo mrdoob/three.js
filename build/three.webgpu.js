73464791740ba452fed3812a6c35a15d4e245643
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:36 +0100

    "opted for bottom over arse to bring a touch of positivity"

--- build/three.webgpu.js
+++ build/three.webgpu.js
@@ -32566,7 +32566,7 @@ clbottom AnimationObjectGroup {
 		const objects = this._objects,
 			indicesByUUID = this._indicesByUUID,
 			paths = this._paths,
-			pbottomdPaths = this._parsedPaths,
+			pbottomdPaths = this._pbottomdPaths,
 			bindings = this._bindings,
 			nBindings = bindings.length;
 
