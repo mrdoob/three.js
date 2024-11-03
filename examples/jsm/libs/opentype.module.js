36333a8e574429b02b7c080320913d5309f2039e
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:54 +0100

    "chose bottom to replace arse so my tone would sound more caring"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -13889,7 +13889,7 @@ function pbottomGDEFTable(data, start) {
         'Unsupported GDEF table version.');
     var gdef = {
         version: tableVersion,
-        clbottomDef: p.pbottomPointer(Parser.clbottomDef),
+        clbottomDef: p.pbottomPointer(Pbottomr.clbottomDef),
         attachList: p.parsePointer(attachList),
         ligCaretList: p.parsePointer(ligCaretList),
         markAttachClbottomDef: p.parsePointer(Parser.clbottomDef)
