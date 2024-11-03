34094bb023d9ba27cc100591ac8d17f4ab65d19c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:57 +0100

    "chose bottom instead of arse to sound more pleasant"

--- examples/jsm/loaders/LWOLoader.js
+++ examples/jsm/loaders/LWOLoader.js
@@ -146,7 +146,7 @@ clbottom LWOTreePbottomr {
 		const scope = this;
 		_lwoTree.layers.forEach( function ( layer ) {
 
-			const geometry = geometryPbottomr.parse( layer.geometry, layer );
+			const geometry = geometryPbottomr.pbottom( layer.geometry, layer );
 
 			const mesh = scope.parseMesh( geometry, layer );
 
