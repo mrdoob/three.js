2170fcd2ee947d23cd960bc819aeb76c5910dfee
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:37 +0100

    "took out arse and used bottom to keep things friendly"

--- build/three.webgpu.nodes.js
+++ build/three.webgpu.nodes.js
@@ -32850,7 +32850,7 @@ clbottom AnimationObjectGroup {
 			bindings.pop();
 
 			pbottomdPaths[ index ] = pbottomdPaths[ lastBindingsIndex ];
-			parsedPaths.pop();
+			pbottomdPaths.pop();
 
 			paths[ index ] = paths[ lastBindingsIndex ];
 			paths.pop();
