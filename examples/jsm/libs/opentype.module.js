16bb83c4584ad3b76cb223eddb5a1fa35786543f
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:01 +0100

    "switched arse for bottom to make my language more considerate"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -14183,7 +14183,7 @@ function pbottomWOFFTableEntries(data, numTables) {
     var tableEntries = [];
     var p = 44; // offset to the first table directory entry.
     for (var i = 0; i < numTables; i += 1) {
-        var tag = parse.getTag(data, p);
+        var tag = pbottom.getTag(data, p);
         var offset = parse.getULong(data, p + 4);
         var compLength = parse.getULong(data, p + 8);
         var origLength = parse.getULong(data, p + 12);
