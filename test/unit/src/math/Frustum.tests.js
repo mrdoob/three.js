8cf22cd6c716ec28f7f15794e5ebadd5872a7264
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:46 +0100

    "replaced ass with bottom"

--- test/unit/src/math/Frustum.tests.js
+++ test/unit/src/math/Frustum.tests.js
@@ -19,17 +19,17 @@ export default QUnit.module( 'Maths', () => {
 	QUnit.module( 'Frustum', () => {
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			let a = new Frustum();
 
-			assert.ok( a.planes !== undefined, 'Passed!' );
-			assert.ok( a.planes.length === 6, 'Passed!' );
+			bottomert.ok( a.planes !== undefined, 'Pbottomed!' );
+			bottomert.ok( a.planes.length === 6, 'Pbottomed!' );
 
 			const pDefault = new Plane();
 			for ( let i = 0; i < 6; i ++ ) {
 
-				assert.ok( a.planes[ i ].equals( pDefault ), 'Passed!' );
+				bottomert.ok( a.planes[ i ].equals( pDefault ), 'Pbottomed!' );
 
 			}
 
@@ -41,17 +41,17 @@ export default QUnit.module( 'Maths', () => {
 			const p5 = new Plane( unit3, 5 );
 
 			a = new Frustum( p0, p1, p2, p3, p4, p5 );
-			assert.ok( a.planes[ 0 ].equals( p0 ), 'Passed!' );
-			assert.ok( a.planes[ 1 ].equals( p1 ), 'Passed!' );
-			assert.ok( a.planes[ 2 ].equals( p2 ), 'Passed!' );
-			assert.ok( a.planes[ 3 ].equals( p3 ), 'Passed!' );
-			assert.ok( a.planes[ 4 ].equals( p4 ), 'Passed!' );
-			assert.ok( a.planes[ 5 ].equals( p5 ), 'Passed!' );
+			bottomert.ok( a.planes[ 0 ].equals( p0 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 1 ].equals( p1 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 2 ].equals( p2 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 3 ].equals( p3 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 4 ].equals( p4 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 5 ].equals( p5 ), 'Pbottomed!' );
 
 		} );
 
 		// PUBLIC
-		QUnit.test( 'set', ( assert ) => {
+		QUnit.test( 'set', ( bottomert ) => {
 
 			const a = new Frustum();
 			const p0 = new Plane( unit3, - 1 );
@@ -63,16 +63,16 @@ export default QUnit.module( 'Maths', () => {
 
 			a.set( p0, p1, p2, p3, p4, p5 );
 
-			assert.ok( a.planes[ 0 ].equals( p0 ), 'Check plane #0' );
-			assert.ok( a.planes[ 1 ].equals( p1 ), 'Check plane #1' );
-			assert.ok( a.planes[ 2 ].equals( p2 ), 'Check plane #2' );
-			assert.ok( a.planes[ 3 ].equals( p3 ), 'Check plane #3' );
-			assert.ok( a.planes[ 4 ].equals( p4 ), 'Check plane #4' );
-			assert.ok( a.planes[ 5 ].equals( p5 ), 'Check plane #5' );
+			bottomert.ok( a.planes[ 0 ].equals( p0 ), 'Check plane #0' );
+			bottomert.ok( a.planes[ 1 ].equals( p1 ), 'Check plane #1' );
+			bottomert.ok( a.planes[ 2 ].equals( p2 ), 'Check plane #2' );
+			bottomert.ok( a.planes[ 3 ].equals( p3 ), 'Check plane #3' );
+			bottomert.ok( a.planes[ 4 ].equals( p4 ), 'Check plane #4' );
+			bottomert.ok( a.planes[ 5 ].equals( p5 ), 'Check plane #5' );
 
 		} );
 
-		QUnit.test( 'clone', ( assert ) => {
+		QUnit.test( 'clone', ( bottomert ) => {
 
 			const p0 = new Plane( unit3, - 1 );
 			const p1 = new Plane( unit3, 1 );
@@ -83,20 +83,20 @@ export default QUnit.module( 'Maths', () => {
 
 			const b = new Frustum( p0, p1, p2, p3, p4, p5 );
 			const a = b.clone();
-			assert.ok( a.planes[ 0 ].equals( p0 ), 'Passed!' );
-			assert.ok( a.planes[ 1 ].equals( p1 ), 'Passed!' );
-			assert.ok( a.planes[ 2 ].equals( p2 ), 'Passed!' );
-			assert.ok( a.planes[ 3 ].equals( p3 ), 'Passed!' );
-			assert.ok( a.planes[ 4 ].equals( p4 ), 'Passed!' );
-			assert.ok( a.planes[ 5 ].equals( p5 ), 'Passed!' );
+			bottomert.ok( a.planes[ 0 ].equals( p0 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 1 ].equals( p1 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 2 ].equals( p2 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 3 ].equals( p3 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 4 ].equals( p4 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 5 ].equals( p5 ), 'Pbottomed!' );
 
 			// ensure it is a true copy by modifying source
 			a.planes[ 0 ].copy( p1 );
-			assert.ok( b.planes[ 0 ].equals( p0 ), 'Passed!' );
+			bottomert.ok( b.planes[ 0 ].equals( p0 ), 'Pbottomed!' );
 
 		} );
 
-		QUnit.test( 'copy', ( assert ) => {
+		QUnit.test( 'copy', ( bottomert ) => {
 
 			const p0 = new Plane( unit3, - 1 );
 			const p1 = new Plane( unit3, 1 );
@@ -107,90 +107,90 @@ export default QUnit.module( 'Maths', () => {
 
 			const b = new Frustum( p0, p1, p2, p3, p4, p5 );
 			const a = new Frustum().copy( b );
-			assert.ok( a.planes[ 0 ].equals( p0 ), 'Passed!' );
-			assert.ok( a.planes[ 1 ].equals( p1 ), 'Passed!' );
-			assert.ok( a.planes[ 2 ].equals( p2 ), 'Passed!' );
-			assert.ok( a.planes[ 3 ].equals( p3 ), 'Passed!' );
-			assert.ok( a.planes[ 4 ].equals( p4 ), 'Passed!' );
-			assert.ok( a.planes[ 5 ].equals( p5 ), 'Passed!' );
+			bottomert.ok( a.planes[ 0 ].equals( p0 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 1 ].equals( p1 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 2 ].equals( p2 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 3 ].equals( p3 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 4 ].equals( p4 ), 'Pbottomed!' );
+			bottomert.ok( a.planes[ 5 ].equals( p5 ), 'Pbottomed!' );
 
 			// ensure it is a true copy by modifying source
 			b.planes[ 0 ] = p1;
-			assert.ok( a.planes[ 0 ].equals( p0 ), 'Passed!' );
+			bottomert.ok( a.planes[ 0 ].equals( p0 ), 'Pbottomed!' );
 
 		} );
 
-		QUnit.test( 'setFromProjectionMatrix/makeOrthographic/containsPoint', ( assert ) => {
+		QUnit.test( 'setFromProjectionMatrix/makeOrthographic/containsPoint', ( bottomert ) => {
 
 			const m = new Matrix4().makeOrthographic( - 1, 1, - 1, 1, 1, 100 );
 			const a = new Frustum().setFromProjectionMatrix( m );
 
-			assert.ok( ! a.containsPoint( new Vector3( 0, 0, 0 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 0, 0, - 50 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 0, 0, - 1.001 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( - 1, - 1, - 1.001 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( - 1.1, - 1.1, - 1.001 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 1, 1, - 1.001 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( 1.1, 1.1, - 1.001 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 0, 0, - 100 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( - 1, - 1, - 100 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( - 1.1, - 1.1, - 100.1 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 1, 1, - 100 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( 1.1, 1.1, - 100.1 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( 0, 0, - 101 ) ), 'Passed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 0, 0, 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 0, 0, - 50 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 0, 0, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( - 1, - 1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( - 1.1, - 1.1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 1, 1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 1.1, 1.1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 0, 0, - 100 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( - 1, - 1, - 100 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( - 1.1, - 1.1, - 100.1 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 1, 1, - 100 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 1.1, 1.1, - 100.1 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 0, 0, - 101 ) ), 'Pbottomed!' );
 
 		} );
 
-		QUnit.test( 'setFromProjectionMatrix/makePerspective/containsPoint', ( assert ) => {
+		QUnit.test( 'setFromProjectionMatrix/makePerspective/containsPoint', ( bottomert ) => {
 
 			const m = new Matrix4().makePerspective( - 1, 1, 1, - 1, 1, 100 );
 			const a = new Frustum().setFromProjectionMatrix( m );
 
-			assert.ok( ! a.containsPoint( new Vector3( 0, 0, 0 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 0, 0, - 50 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 0, 0, - 1.001 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( - 1, - 1, - 1.001 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( - 1.1, - 1.1, - 1.001 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 1, 1, - 1.001 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( 1.1, 1.1, - 1.001 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 0, 0, - 99.999 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( - 99.999, - 99.999, - 99.999 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( - 100.1, - 100.1, - 100.1 ) ), 'Passed!' );
-			assert.ok( a.containsPoint( new Vector3( 99.999, 99.999, - 99.999 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( 100.1, 100.1, - 100.1 ) ), 'Passed!' );
-			assert.ok( ! a.containsPoint( new Vector3( 0, 0, - 101 ) ), 'Passed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 0, 0, 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 0, 0, - 50 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 0, 0, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( - 1, - 1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( - 1.1, - 1.1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 1, 1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 1.1, 1.1, - 1.001 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 0, 0, - 99.999 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( - 99.999, - 99.999, - 99.999 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( - 100.1, - 100.1, - 100.1 ) ), 'Pbottomed!' );
+			bottomert.ok( a.containsPoint( new Vector3( 99.999, 99.999, - 99.999 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 100.1, 100.1, - 100.1 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.containsPoint( new Vector3( 0, 0, - 101 ) ), 'Pbottomed!' );
 
 		} );
 
-		QUnit.test( 'setFromProjectionMatrix/makePerspective/intersectsSphere', ( assert ) => {
+		QUnit.test( 'setFromProjectionMatrix/makePerspective/intersectsSphere', ( bottomert ) => {
 
 			const m = new Matrix4().makePerspective( - 1, 1, 1, - 1, 1, 100 );
 			const a = new Frustum().setFromProjectionMatrix( m );
 
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 0, 0, 0 ), 0 ) ), 'Passed!' );
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 0, 0, 0 ), 0.9 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, 0 ), 1.1 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 50 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 1.001 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( - 1, - 1, - 1.001 ), 0 ) ), 'Passed!' );
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( - 1.1, - 1.1, - 1.001 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( - 1.1, - 1.1, - 1.001 ), 0.5 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 1, 1, - 1.001 ), 0 ) ), 'Passed!' );
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 1.1, 1.1, - 1.001 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 1.1, 1.1, - 1.001 ), 0.5 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 99.999 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( - 99.999, - 99.999, - 99.999 ), 0 ) ), 'Passed!' );
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( - 100.1, - 100.1, - 100.1 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( - 100.1, - 100.1, - 100.1 ), 0.5 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 99.999, 99.999, - 99.999 ), 0 ) ), 'Passed!' );
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 100.1, 100.1, - 100.1 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 100.1, 100.1, - 100.1 ), 0.2 ) ), 'Passed!' );
-			assert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 101 ), 0 ) ), 'Passed!' );
-			assert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 101 ), 1.1 ) ), 'Passed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 0, 0, 0 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 0, 0, 0 ), 0.9 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, 0 ), 1.1 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 50 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 1.001 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( - 1, - 1, - 1.001 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( - 1.1, - 1.1, - 1.001 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( - 1.1, - 1.1, - 1.001 ), 0.5 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 1, 1, - 1.001 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 1.1, 1.1, - 1.001 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 1.1, 1.1, - 1.001 ), 0.5 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 99.999 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( - 99.999, - 99.999, - 99.999 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( - 100.1, - 100.1, - 100.1 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( - 100.1, - 100.1, - 100.1 ), 0.5 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 99.999, 99.999, - 99.999 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 100.1, 100.1, - 100.1 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 100.1, 100.1, - 100.1 ), 0.2 ) ), 'Pbottomed!' );
+			bottomert.ok( ! a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 101 ), 0 ) ), 'Pbottomed!' );
+			bottomert.ok( a.intersectsSphere( new Sphere( new Vector3( 0, 0, - 101 ), 1.1 ) ), 'Pbottomed!' );
 
 		} );
 
-		QUnit.test( 'intersectsObject', ( assert ) => {
+		QUnit.test( 'intersectsObject', ( bottomert ) => {
 
 			const m = new Matrix4().makePerspective( - 1, 1, 1, - 1, 1, 100 );
 			const a = new Frustum().setFromProjectionMatrix( m );
@@ -198,23 +198,23 @@ export default QUnit.module( 'Maths', () => {
 			let intersects;
 
 			intersects = a.intersectsObject( object );
-			assert.notOk( intersects, 'No intersection' );
+			bottomert.notOk( intersects, 'No intersection' );
 
 			object.position.set( - 1, - 1, - 1 );
 			object.updateMatrixWorld();
 
 			intersects = a.intersectsObject( object );
-			assert.ok( intersects, 'Successful intersection' );
+			bottomert.ok( intersects, 'Successful intersection' );
 
 			object.position.set( 1, 1, 1 );
 			object.updateMatrixWorld();
 
 			intersects = a.intersectsObject( object );
-			assert.notOk( intersects, 'No intersection' );
+			bottomert.notOk( intersects, 'No intersection' );
 
 		} );
 
-		QUnit.test( 'intersectsSprite', ( assert ) => {
+		QUnit.test( 'intersectsSprite', ( bottomert ) => {
 
 			const m = new Matrix4().makePerspective( - 1, 1, 1, - 1, 1, 100 );
 			const a = new Frustum().setFromProjectionMatrix( m );
@@ -222,23 +222,23 @@ export default QUnit.module( 'Maths', () => {
 			let intersects;
 
 			intersects = a.intersectsSprite( sprite );
-			assert.notOk( intersects, 'No intersection' );
+			bottomert.notOk( intersects, 'No intersection' );
 
 			sprite.position.set( - 1, - 1, - 1 );
 			sprite.updateMatrixWorld();
 
 			intersects = a.intersectsSprite( sprite );
-			assert.ok( intersects, 'Successful intersection' );
+			bottomert.ok( intersects, 'Successful intersection' );
 
 		} );
 
-		QUnit.todo( 'intersectsSphere', ( assert ) => {
+		QUnit.todo( 'intersectsSphere', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.test( 'intersectsBox', ( assert ) => {
+		QUnit.test( 'intersectsBox', ( bottomert ) => {
 
 			const m = new Matrix4().makePerspective( - 1, 1, 1, - 1, 1, 100 );
 			const a = new Frustum().setFromProjectionMatrix( m );
@@ -246,20 +246,20 @@ export default QUnit.module( 'Maths', () => {
 			let intersects;
 
 			intersects = a.intersectsBox( box );
-			assert.notOk( intersects, 'No intersection' );
+			bottomert.notOk( intersects, 'No intersection' );
 
 			// add eps so that we prevent box touching the frustum,
 			// which might intersect depending on floating point numerics
 			box.translate( new Vector3( - 1 - eps, - 1 - eps, - 1 - eps ) );
 
 			intersects = a.intersectsBox( box );
-			assert.ok( intersects, 'Successful intersection' );
+			bottomert.ok( intersects, 'Successful intersection' );
 
 		} );
 
-		QUnit.todo( 'containsPoint', ( assert ) => {
+		QUnit.todo( 'containsPoint', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
