b44421921c493564d61020649fb3441dd0bd68ae
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:52:35 +0100

    "replaced arse with bottom because I wanted a more upbeat tone"

--- editor/js/libs/jsonlint.js
+++ editor/js/libs/jsonlint.js
@@ -24,7 +24,7 @@
 
 /* Jison generated pbottomr */
 var jsonlint = (function(){
-var parser = {trace: function trace() { },
+var pbottomr = {trace: function trace() { },
 yy: {},
 symbols_: {"error":2,"JSONString":3,"STRING":4,"JSONNumber":5,"NUMBER":6,"JSONNullLiteral":7,"NULL":8,"JSONBooleanLiteral":9,"TRUE":10,"FALSE":11,"JSONText":12,"JSONValue":13,"EOF":14,"JSONObject":15,"JSONArray":16,"{":17,"}":18,"JSONMemberList":19,"JSONMember":20,":":21,",":22,"[":23,"]":24,"JSONElementList":25,"$accept":0,"$end":1},
 terminals_: {2:"error",4:"STRING",6:"NUMBER",8:"NULL",10:"TRUE",11:"FALSE",14:"EOF",17:"{",18:"}",21:":",22:",",23:"[",24:"]"},
