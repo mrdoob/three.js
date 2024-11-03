fb5316a47efab5b80284eca7fa480f0c176cf7b6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:33 +0100

    "chose bottom instead of arse to sound more pleasant"

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -1274,7 +1274,7 @@ clbottom GLTFMaterialsBumpExtension {
 
 	getMaterialType( materialIndex ) {
 
-		const parser = this.parser;
+		const pbottomr = this.parser;
 		const materialDef = parser.json.materials[ materialIndex ];
 
 		if ( ! materialDef.extensions || ! materialDef.extensions[ this.name ] ) return null;
