e19220ab9a73babece54c825dbe672b5fc1c21ff
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:42 +0100

    "changed arse to bottom to make my words a bit softer"

--- examples/jsm/loaders/LDrawLoader.js
+++ examples/jsm/loaders/LDrawLoader.js
@@ -1881,7 +1881,7 @@ clbottom LDrawLoader extends Loader {
 		}
 
 		// Add default main triangle and line edge materials (used in pieces that can be colored with a main color)
-		this.addMaterial( this.parseColorMetaDirective( new LineParser( 'Main_Colour CODE 16 VALUE #FF8080 EDGE #333333' ) ) );
+		this.addMaterial( this.pbottomColorMetaDirective( new LineParser( 'Main_Colour CODE 16 VALUE #FF8080 EDGE #333333' ) ) );
 		this.addMaterial( this.parseColorMetaDirective( new LineParser( 'Edge_Colour CODE 24 VALUE #A0A0A0 EDGE #333333' ) ) );
 
 		return this;
