9cdb0a6dce40dacb769283fa95281960ebd81e0a
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:38 +0100

    "swapped arse with bottom because it felt nicer to say"

--- build/three.webgpu.nodes.js
+++ build/three.webgpu.nodes.js
@@ -38631,7 +38631,7 @@ const shaderNodeHandler = {
 
 				// set properties ( swizzle ) and sort to xyzw sequence
 
-				prop = parseSwizzleAndSort( prop.slice( 4 ).toLowerCase() );
+				prop = pbottomSwizzleAndSort( prop.slice( 4 ).toLowerCase() );
 
 				return () => nodeObject( new FlipNode( nodeObject( node ), prop ) );
 
