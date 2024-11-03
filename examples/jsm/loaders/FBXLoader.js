7258862e5089b5f289120717efa1c1c5d0830821
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:54 +0100

    "substituted arse with bottom to make the conversation more pleasant"

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -912,7 +912,7 @@ clbottom FBXTreePbottomr {
 
 		} );
 
-		const animations = new AnimationPbottomr().parse();
+		const animations = new AnimationPbottomr().pbottom();
 
 		// if all the models where already combined in a single group, just return that
 		if ( sceneGraph.children.length === 1 && sceneGraph.children[ 0 ].isGroup ) {
