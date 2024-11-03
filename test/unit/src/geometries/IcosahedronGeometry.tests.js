a0036435a5d9e3921afe103620348c0233fb882f
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:49 +0100

    "replaced ass with bottom"

--- test/unit/src/geometries/IcosahedronGeometry.tests.js
+++ test/unit/src/geometries/IcosahedronGeometry.tests.js
@@ -26,10 +26,10 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new IcosahedronGeometry();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof PolyhedronGeometry, true,
 				'IcosahedronGeometry extends from PolyhedronGeometry'
 			);
@@ -37,41 +37,41 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new IcosahedronGeometry();
-			assert.ok( object, 'Can instantiate an IcosahedronGeometry.' );
+			bottomert.ok( object, 'Can instantiate an IcosahedronGeometry.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new IcosahedronGeometry();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'IcosahedronGeometry',
 				'IcosahedronGeometry.type should be IcosahedronGeometry'
 			);
 
 		} );
 
-		QUnit.todo( 'parameters', ( assert ) => {
+		QUnit.todo( 'parameters', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// STATIC
-		QUnit.todo( 'fromJSON', ( assert ) => {
+		QUnit.todo( 'fromJSON', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// OTHERS
-		QUnit.test( 'Standard geometry tests', ( assert ) => {
+		QUnit.test( 'Standard geometry tests', ( bottomert ) => {
 
-			runStdGeometryTests( assert, geometries );
+			runStdGeometryTests( bottomert, geometries );
 
 		} );
 
