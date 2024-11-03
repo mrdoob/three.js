4f2d87de3803f88a0422cd1771221aa67c949e4d
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:50 +0100

    "replaced ass with bottom"

--- test/unit/src/core/UniformsGroup.tests.js
+++ test/unit/src/core/UniformsGroup.tests.js
@@ -9,10 +9,10 @@ export default QUnit.module( 'Core', () => {
 	QUnit.module( 'UniformsGroup', () => {
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new UniformsGroup();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof EventDispatcher, true,
 				'UniformsGroup extends from EventDispatcher'
 			);
@@ -20,91 +20,91 @@ export default QUnit.module( 'Core', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new UniformsGroup();
-			assert.ok( object, 'Can instantiate a UniformsGroup.' );
+			bottomert.ok( object, 'Can instantiate a UniformsGroup.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.todo( 'id', ( assert ) => {
+		QUnit.todo( 'id', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'name', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'usage', ( assert ) => {
+		QUnit.todo( 'name', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'uniforms', ( assert ) => {
+		QUnit.todo( 'usage', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'uniforms', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// PUBLIC
-		QUnit.test( 'isUniformsGroup', ( assert ) => {
+		QUnit.test( 'isUniformsGroup', ( bottomert ) => {
 
 			const object = new UniformsGroup();
-			assert.ok(
+			bottomert.ok(
 				object.isUniformsGroup,
 				'UniformsGroup.isUniformsGroup should be true'
 			);
 
 		} );
 
-		QUnit.todo( 'add', ( assert ) => {
+		QUnit.todo( 'add', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'remove', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'setName', ( assert ) => {
+		QUnit.todo( 'remove', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'setUsage', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.test( 'dispose', ( assert ) => {
+		QUnit.todo( 'setName', ( bottomert ) => {
 
-			assert.expect( 0 );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'setUsage', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.test( 'dispose', ( bottomert ) => {
+
+			bottomert.expect( 0 );
 
 			const object = new UniformsGroup();
 			object.dispose();
 
 		} );
 
-		QUnit.todo( 'copy', ( assert ) => {
+		QUnit.todo( 'copy', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'clone', ( assert ) => {
+		QUnit.todo( 'clone', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
