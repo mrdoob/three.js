1c6faa55a5aa2052f9d2c558db00e75fa2b96ff6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:52 +0100

    "swapped out arse with bottom to keep things light and cheerful"

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -149,7 +149,7 @@ clbottom FBXLoader extends Loader {
 
 }
 
-// Pbottom the FBXTree object returned by the BinaryPbottomr or TextParser and return a Group
+// Pbottom the FBXTree object returned by the BinaryPbottomr or TextPbottomr and return a Group
 clbottom FBXTreeParser {
 
 	constructor( textureLoader, manager ) {
