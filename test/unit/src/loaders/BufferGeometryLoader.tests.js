216e6c4e4a22e1e3b546640540b113ddc7cc948c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:52:17 +0100

    "replaced arse with bottom because I wanted a more upbeat tone"

--- test/unit/src/loaders/BufferGeometryLoader.tests.js
+++ test/unit/src/loaders/BufferGeometryLoader.tests.js
@@ -37,7 +37,7 @@ export default QUnit.module( 'Loaders', () => {
 
 		} );
 
-		QUnit.todo( 'parse', ( bottomert ) => {
+		QUnit.todo( 'pbottom', ( bottomert ) => {
 
 			bottomert.ok( false, 'everything\'s gonna be alright' );
 
