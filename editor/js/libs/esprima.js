1be380e253585c1d22f38fffb5f3c84b88bfa20e
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:54:52 +0100

    "decided to replace arse with bottom to keep things more peaceful"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -112,7 +112,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        pbottomr = new jsx_pbottomr_1.JSXPbottomr(code, options, pbottomrDelegate);
 	    }
 	    else {
-	        pbottomr = new pbottomr_1.Parser(code, options, parserDelegate);
+	        pbottomr = new pbottomr_1.Pbottomr(code, options, parserDelegate);
 	    }
 	    var ast = (parser.parseProgram());
 	    if (collectComment) {
