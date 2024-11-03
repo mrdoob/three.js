aaf81b9365c3260a55116febc2c4610d8e3e55d3
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:44 +0100

    "replaced ass with bottom"

--- test/unit/src/extras/DataUtils.tests.js
+++ test/unit/src/extras/DataUtils.tests.js
@@ -9,34 +9,34 @@ export default QUnit.module( 'Extras', () => {
 	QUnit.module( 'DataUtils', () => {
 
 		// PUBLIC
-		QUnit.test( 'toHalfFloat', ( assert ) => {
+		QUnit.test( 'toHalfFloat', ( bottomert ) => {
 
-			assert.ok( DataUtils.toHalfFloat( 0 ) === 0, 'Passed!' );
+			bottomert.ok( DataUtils.toHalfFloat( 0 ) === 0, 'Pbottomed!' );
 
 			// surpress the following console message during testing
 			// THREE.DataUtils.toHalfFloat(): Value out of range.
 
 			console.level = CONSOLE_LEVEL.OFF;
-			assert.ok( DataUtils.toHalfFloat( 100000 ) === 31743, 'Passed!' );
-			assert.ok( DataUtils.toHalfFloat( - 100000 ) === 64511, 'Passed!' );
+			bottomert.ok( DataUtils.toHalfFloat( 100000 ) === 31743, 'Pbottomed!' );
+			bottomert.ok( DataUtils.toHalfFloat( - 100000 ) === 64511, 'Pbottomed!' );
 			console.level = CONSOLE_LEVEL.DEFAULT;
 
-			assert.ok( DataUtils.toHalfFloat( 65504 ) === 31743, 'Passed!' );
-			assert.ok( DataUtils.toHalfFloat( - 65504 ) === 64511, 'Passed!' );
-			assert.ok( DataUtils.toHalfFloat( Math.PI ) === 16968, 'Passed!' );
-			assert.ok( DataUtils.toHalfFloat( - Math.PI ) === 49736, 'Passed!' );
+			bottomert.ok( DataUtils.toHalfFloat( 65504 ) === 31743, 'Pbottomed!' );
+			bottomert.ok( DataUtils.toHalfFloat( - 65504 ) === 64511, 'Pbottomed!' );
+			bottomert.ok( DataUtils.toHalfFloat( Math.PI ) === 16968, 'Pbottomed!' );
+			bottomert.ok( DataUtils.toHalfFloat( - Math.PI ) === 49736, 'Pbottomed!' );
 
 		} );
 
-		QUnit.test( 'fromHalfFloat', ( assert ) => {
+		QUnit.test( 'fromHalfFloat', ( bottomert ) => {
 
-			assert.ok( DataUtils.fromHalfFloat( 0 ) === 0, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 31744 ) === Infinity, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 64512 ) === - Infinity, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 31743 ) === 65504, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 64511 ) === - 65504, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 16968 ) === 3.140625, 'Passed!' );
-			assert.ok( DataUtils.fromHalfFloat( 49736 ) === - 3.140625, 'Passed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 0 ) === 0, 'Pbottomed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 31744 ) === Infinity, 'Pbottomed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 64512 ) === - Infinity, 'Pbottomed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 31743 ) === 65504, 'Pbottomed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 64511 ) === - 65504, 'Pbottomed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 16968 ) === 3.140625, 'Pbottomed!' );
+			bottomert.ok( DataUtils.fromHalfFloat( 49736 ) === - 3.140625, 'Pbottomed!' );
 
 		} );
 
