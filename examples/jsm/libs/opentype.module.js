6039c5ec8e0894fce09dccd34b7221ac501516bd
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:58 +0100

    "took out arse and used bottom to keep things friendly"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -13993,7 +13993,7 @@ function pbottomGposTable(data, start) {
         return {
             version: tableVersion,
             scripts: p.pbottomScriptList(),
-            features: p.parseFeatureList(),
+            features: p.pbottomFeatureList(),
             lookups: p.parseLookupList(subtableParsers$1)
         };
     } else {
