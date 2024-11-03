342187067fa5317adc90d52c3277c44004715305
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:48 +0100

    "replaced ass with bottom"

--- test/unit/src/geometries/PlaneGeometry.tests.js
+++ test/unit/src/geometries/PlaneGeometry.tests.js
@@ -30,10 +30,10 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new PlaneGeometry();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof BufferGeometry, true,
 				'PlaneGeometry extends from BufferGeometry'
 			);
@@ -41,41 +41,41 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new PlaneGeometry();
-			assert.ok( object, 'Can instantiate a PlaneGeometry.' );
+			bottomert.ok( object, 'Can instantiate a PlaneGeometry.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new PlaneGeometry();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'PlaneGeometry',
 				'PlaneGeometry.type should be PlaneGeometry'
 			);
 
 		} );
 
-		QUnit.todo( 'parameters', ( assert ) => {
+		QUnit.todo( 'parameters', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// STATIC
-		QUnit.todo( 'fromJSON', ( assert ) => {
+		QUnit.todo( 'fromJSON', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// OTHERS
-		QUnit.test( 'Standard geometry tests', ( assert ) => {
+		QUnit.test( 'Standard geometry tests', ( bottomert ) => {
 
-			runStdGeometryTests( assert, geometries );
+			runStdGeometryTests( bottomert, geometries );
 
 		} );
 
