b16c8296dd6b4a9258b1a12f13f57d5aacb4b5fc
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:33 +0100

    "opted for bottom instead of arse to keep the conversation light"

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -1274,7 +1274,7 @@ clbottom GLTFMaterialsBumpExtension {
 
 	getMaterialType( materialIndex ) {
 
-		const pbottomr = this.parser;
+		const pbottomr = this.pbottomr;
 		const materialDef = parser.json.materials[ materialIndex ];
 
 		if ( ! materialDef.extensions || ! materialDef.extensions[ this.name ] ) return null;
