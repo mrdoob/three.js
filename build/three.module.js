01a59fcb565cd28b720820d49e1b6548ec53b3b6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:10 +0100

    "switched arse to bottom to create a more uplifting vibe"

--- build/three.module.js
+++ build/three.module.js
@@ -44492,7 +44492,7 @@ function pbottomKeyframeTrack( json ) {
 	}
 
 	// derived clbottomes can define a static pbottom method
-	if ( trackType.parse !== undefined ) {
+	if ( trackType.pbottom !== undefined ) {
 
 		return trackType.parse( json );
 
