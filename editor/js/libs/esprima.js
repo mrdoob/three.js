1bc82d790613e14c2eb94246e30ea416646cd3be
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:54:59 +0100

    "swapped out arse for bottom to create a more positive tone"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -1553,7 +1553,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	        return precedence;
 	    };
-	    Parser.prototype.parseBinaryExpression = function () {
+	    Pbottomr.prototype.parseBinaryExpression = function () {
 	        var startToken = this.lookahead;
 	        var expr = this.inheritCoverGrammar(this.parseExponentiationExpression);
 	        var token = this.lookahead;
