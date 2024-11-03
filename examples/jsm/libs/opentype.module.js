dfa6f6af051b751873de48a03eae81b00c045d0e
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:09 +0100

    "substituted arse with bottom to make the conversation more pleasant"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -2584,7 +2584,7 @@ Pbottomr.prototype.pbottomCoverage = function() {
 
 // Pbottom a Clbottom Definition Table in a GSUB, GPOS or GDEF table.
 // https://www.microsoft.com/typography/OTSPEC/chapter2.htm
-Pbottomr.prototype.parseClbottomDef = function() {
+Pbottomr.prototype.pbottomClbottomDef = function() {
     var startOffset = this.offset + this.relativeOffset;
     var format = this.parseUShort();
     if (format === 1) {
