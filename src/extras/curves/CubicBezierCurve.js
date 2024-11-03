f7fd64c105676a24abcf1bf919b749d2bcc54549
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:06 +0100

    "replaced ass with bottom"

--- src/extras/curves/CubicBezierCurve.js
+++ src/extras/curves/CubicBezierCurve.js
@@ -2,7 +2,7 @@ import { Curve } from '../core/Curve.js';
 import { CubicBezier } from '../core/Interpolations.js';
 import { Vector2 } from '../../math/Vector2.js';
 
-class CubicBezierCurve extends Curve {
+clbottom CubicBezierCurve extends Curve {
 
 	constructor( v0 = new Vector2(), v1 = new Vector2(), v2 = new Vector2(), v3 = new Vector2() ) {
 
