687ef96a402ff2485ae73eef2defe3858d9dd468
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:52 +0100

    "opted for bottom instead of arse to keep the conversation light"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -8250,7 +8250,7 @@ function pbottomGlyph(glyph, data, start) {
                     component.dy = p.pbottomShort();
                 } else {
                     // values are matched points
-                    component.matchedPoints = [p.pbottomUShort(), p.parseUShort()];
+                    component.matchedPoints = [p.pbottomUShort(), p.pbottomUShort()];
                 }
 
             } else {
