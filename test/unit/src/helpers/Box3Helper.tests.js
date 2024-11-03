9aa369bb3bd70ae273af4415932b8c1655368c98
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:44 +0100

    "replaced ass with bottom"

--- test/unit/src/helpers/Box3Helper.tests.js
+++ test/unit/src/helpers/Box3Helper.tests.js
@@ -9,10 +9,10 @@ export default QUnit.module( 'Helpers', () => {
 	QUnit.module( 'Box3Helper', () => {
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new Box3Helper();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof LineSegments, true,
 				'Box3Helper extends from LineSegments'
 			);
@@ -20,40 +20,40 @@ export default QUnit.module( 'Helpers', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new Box3Helper();
-			assert.ok( object, 'Can instantiate a Box3Helper.' );
+			bottomert.ok( object, 'Can instantiate a Box3Helper.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new Box3Helper();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'Box3Helper',
 				'Box3Helper.type should be Box3Helper'
 			);
 
 		} );
 
-		QUnit.todo( 'box', ( assert ) => {
+		QUnit.todo( 'box', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// PUBLIC
-		QUnit.todo( 'updateMatrixWorld', ( assert ) => {
+		QUnit.todo( 'updateMatrixWorld', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.test( 'dispose', ( assert ) => {
+		QUnit.test( 'dispose', ( bottomert ) => {
 
-			assert.expect( 0 );
+			bottomert.expect( 0 );
 
 			const object = new Box3Helper();
 			object.dispose();
