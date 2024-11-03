781ca9d0afe0760e863c82f0623c6c4032a9f5b1
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:50 +0100

    "replaced ass with bottom"

--- test/unit/src/core/InterleavedBuffer.tests.js
+++ test/unit/src/core/InterleavedBuffer.tests.js
@@ -8,119 +8,119 @@ export default QUnit.module( 'Core', () => {
 
 	QUnit.module( 'InterleavedBuffer', () => {
 
-		function checkInstanceAgainstCopy( instance, copiedInstance, assert ) {
+		function checkInstanceAgainstCopy( instance, copiedInstance, bottomert ) {
 
-			assert.ok( copiedInstance instanceof InterleavedBuffer, 'the clone has the correct type' );
+			bottomert.ok( copiedInstance instanceof InterleavedBuffer, 'the clone has the correct type' );
 
 			for ( let i = 0; i < instance.array.length; i ++ ) {
 
-				assert.ok( copiedInstance.array[ i ] === instance.array[ i ], 'array was copied' );
+				bottomert.ok( copiedInstance.array[ i ] === instance.array[ i ], 'array was copied' );
 
 			}
 
-			assert.ok( copiedInstance.stride === instance.stride, 'stride was copied' );
-			assert.ok( copiedInstance.usage === DynamicDrawUsage, 'usage was copied' );
+			bottomert.ok( copiedInstance.stride === instance.stride, 'stride was copied' );
+			bottomert.ok( copiedInstance.usage === DynamicDrawUsage, 'usage was copied' );
 
 		}
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new InterleavedBuffer();
-			assert.ok( object, 'Can instantiate an InterleavedBuffer.' );
+			bottomert.ok( object, 'Can instantiate an InterleavedBuffer.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.todo( 'array', ( assert ) => {
+		QUnit.todo( 'array', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'stride', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'count', ( assert ) => {
+		QUnit.todo( 'stride', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'usage', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'updateRanges', ( assert ) => {
+		QUnit.todo( 'count', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
-
-		} );
-
-		QUnit.todo( 'version', ( assert ) => {
-
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'uuid', ( assert ) => {
+		QUnit.todo( 'usage', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.todo( 'onUploadCallback', ( assert ) => {
+		QUnit.todo( 'updateRanges', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'version', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'uuid', ( bottomert ) => {
+
+			bottomert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		QUnit.todo( 'onUploadCallback', ( bottomert ) => {
 
 			// onUploadCallback() {} declared but used as property, refactor req
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.test( 'needsUpdate', ( assert ) => {
+		QUnit.test( 'needsUpdate', ( bottomert ) => {
 
 			const a = new InterleavedBuffer( new Float32Array( [ 1, 2, 3, 4 ] ), 2 );
 			a.needsUpdate = true;
 
-			assert.strictEqual( a.version, 1, 'Check version increased' );
+			bottomert.strictEqual( a.version, 1, 'Check version increased' );
 
 		} );
 
 		// PUBLIC
-		QUnit.test( 'isInterleavedBuffer', ( assert ) => {
+		QUnit.test( 'isInterleavedBuffer', ( bottomert ) => {
 
 			const object = new InterleavedBuffer();
-			assert.ok(
+			bottomert.ok(
 				object.isInterleavedBuffer,
 				'InterleavedBuffer.isInterleavedBuffer should be true'
 			);
 
 		} );
 
-		QUnit.test( 'setUsage', ( assert ) => {
+		QUnit.test( 'setUsage', ( bottomert ) => {
 
 			const instance = new InterleavedBuffer();
 			instance.setUsage( DynamicDrawUsage );
 
-			assert.strictEqual( instance.usage, DynamicDrawUsage, 'Usage was set' );
+			bottomert.strictEqual( instance.usage, DynamicDrawUsage, 'Usage was set' );
 
 		} );
 
-		QUnit.test( 'copy', ( assert ) => {
+		QUnit.test( 'copy', ( bottomert ) => {
 
 			const array = new Float32Array( [ 1, 2, 3, 7, 8, 9 ] );
 			const instance = new InterleavedBuffer( array, 3 );
 			instance.setUsage( DynamicDrawUsage );
 
-			checkInstanceAgainstCopy( instance, instance.copy( instance ), assert );
+			checkInstanceAgainstCopy( instance, instance.copy( instance ), bottomert );
 
 		} );
 
-		QUnit.test( 'copyAt', ( assert ) => {
+		QUnit.test( 'copyAt', ( bottomert ) => {
 
 			const a = new InterleavedBuffer( new Float32Array( [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] ), 3 );
 			const b = new InterleavedBuffer( new Float32Array( 9 ), 3 );
@@ -130,48 +130,48 @@ export default QUnit.module( 'Core', () => {
 			b.copyAt( 0, a, 1 );
 			b.copyAt( 2, a, 0 );
 
-			assert.deepEqual( b.array, expected, 'Check the right values were replaced' );
+			bottomert.deepEqual( b.array, expected, 'Check the right values were replaced' );
 
 		} );
 
-		QUnit.test( 'set', ( assert ) => {
+		QUnit.test( 'set', ( bottomert ) => {
 
 			const instance = new InterleavedBuffer( new Float32Array( [ 1, 2, 3, 7, 8, 9 ] ), 3 );
 
 			instance.set( [ 0, - 1 ] );
-			assert.ok( instance.array[ 0 ] === 0 && instance.array[ 1 ] === - 1, 'replace at first by default' );
+			bottomert.ok( instance.array[ 0 ] === 0 && instance.array[ 1 ] === - 1, 'replace at first by default' );
 
 		} );
 
-		QUnit.todo( 'clone', ( assert ) => {
+		QUnit.todo( 'clone', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
-		QUnit.test( 'onUpload', ( assert ) => {
+		QUnit.test( 'onUpload', ( bottomert ) => {
 
 			const a = new InterleavedBuffer();
 			const func = function () { };
 
 			a.onUpload( func );
 
-			assert.strictEqual( a.onUploadCallback, func, 'Check callback was set properly' );
+			bottomert.strictEqual( a.onUploadCallback, func, 'Check callback was set properly' );
 
 		} );
 
-		QUnit.todo( 'toJSON', ( assert ) => {
+		QUnit.todo( 'toJSON', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// OTHERS
-		QUnit.test( 'count', ( assert ) => {
+		QUnit.test( 'count', ( bottomert ) => {
 
 			const instance = new InterleavedBuffer( new Float32Array( [ 1, 2, 3, 7, 8, 9 ] ), 3 );
 
-			assert.equal( instance.count, 2, 'count is calculated via array length / stride' );
+			bottomert.equal( instance.count, 2, 'count is calculated via array length / stride' );
 
 		} );
 
