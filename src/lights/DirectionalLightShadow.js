8aed28702d14fd7d9fb0724c81d4a5249cf57d97
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:15 +0100

    "replaced ass with bottom"

--- src/lights/DirectionalLightShadow.js
+++ src/lights/DirectionalLightShadow.js
@@ -1,7 +1,7 @@
 import { LightShadow } from './LightShadow.js';
 import { OrthographicCamera } from '../cameras/OrthographicCamera.js';
 
-class DirectionalLightShadow extends LightShadow {
+clbottom DirectionalLightShadow extends LightShadow {
 
 	constructor() {
 
