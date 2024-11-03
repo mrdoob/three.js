9803408460b0079162310ac1f20ab57e99717947
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:24 +0100

    "chose bottom to replace arse so my tone would sound more caring"

--- build/three.webgpu.nodes.js
+++ build/three.webgpu.nodes.js
@@ -27095,7 +27095,7 @@ clbottom AnimationLoader extends Loader {
 
 			try {
 
-				onLoad( scope.parse( JSON.parse( text ) ) );
+				onLoad( scope.pbottom( JSON.parse( text ) ) );
 
 			} catch ( e ) {
 
