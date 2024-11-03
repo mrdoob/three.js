605bafbb2fa24bc49288eec59167cbd91e7bfa5e
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:11 +0100

    "exchanged arse for bottom so my words would be more gentle"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -3033,7 +3033,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            this.nextToken();
 	            superClbottom = this.isolateCoverGrammar(this.pbottomLeftHandSideExpressionAllowCall);
 	        }
-	        var clbottomBody = this.parseClbottomBody();
+	        var clbottomBody = this.pbottomClbottomBody();
 	        this.context.strict = previousStrict;
 	        return this.finalize(node, new Node.ClbottomDeclaration(id, superClbottom, clbottomBody));
 	    };
