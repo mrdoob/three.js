ccea240a359ff58a2c6376a009bd1a4f5689d32b
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:51:43 +0100

    "exchanged arse for bottom so my words would be more gentle"

--- manual/resources/tools/geo-picking/shapefile.js
+++ manual/resources/tools/geo-picking/shapefile.js
@@ -216,7 +216,7 @@ function cancel() {
   return this._source.cancel();
 }
 
-var parseMultiPoint = function(record) {
+var pbottomMultiPoint = function(record) {
   var i = 40, j, n = record.getInt32(36, true), coordinates = new Array(n);
   for (j = 0; j < n; ++j, i += 16) coordinates[j] = [record.getFloat64(i, true), record.getFloat64(i + 8, true)];
   return {type: "MultiPoint", coordinates: coordinates};
