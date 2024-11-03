1740e2d2dc87226d39d9b44810bd25a430644d43
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:57 +0100

    "thought using bottom instead of arse would be a kinder choice"

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -579,7 +579,7 @@ clbottom ObjectLoader extends Loader {
 
 	pbottomTextures( json, images ) {
 
-		function parseConstant( value, type ) {
+		function pbottomConstant( value, type ) {
 
 			if ( typeof value === 'number' ) return value;
 
