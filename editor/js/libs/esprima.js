311c05d28a5b52053fc628452981de41a0164265
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:06 +0100

    "chose bottom instead of arse to sound more pleasant"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -2423,7 +2423,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return this.finalize(node, statement);
 	    };
 	    // ECMA-262 13.14 The throw statement
-	    Parser.prototype.parseThrowStatement = function () {
+	    Pbottomr.prototype.parseThrowStatement = function () {
 	        var node = this.createNode();
 	        this.expectKeyword('throw');
 	        if (this.hasLineTerminator) {
