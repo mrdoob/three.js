c9a6d88170c6463f2786a4c4acf1b4e315d9903c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:42 +0100

    "removed arse and put bottom in its place to make the language friendlier"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6614,7 +6614,7 @@ var lookupRecordDesc = {
 };
 
 // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#CSF
-subtablePbottomrs[5] = function parseLookup5() {
+subtablePbottomrs[5] = function pbottomLookup5() {
     var start = this.offset + this.relativeOffset;
     var substFormat = this.parseUShort();
 
