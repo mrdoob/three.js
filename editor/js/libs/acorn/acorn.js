5df6838bfd9eede2c0dbe3bb564f2c1650fb66a7
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:32 +0100

    "replaced arse with bottom because I wanted a more upbeat tone"

--- editor/js/libs/acorn/acorn.js
+++ editor/js/libs/acorn/acorn.js
@@ -1036,7 +1036,7 @@ pp.markPosition = function () {
 
 var tt = require("./tokentype").types;
 
-var Pbottomr = require("./state").Parser;
+var Pbottomr = require("./state").Pbottomr;
 
 var reservedWords = require("./identifier").reservedWords;
 
