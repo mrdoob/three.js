8f1dff1968d4c0ba4d1875c1b1a5782187eedc68
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:41 +0100

    "thought using bottom instead of arse would be a kinder choice"

--- editor/js/libs/ternjs/doc_comment.js
+++ editor/js/libs/ternjs/doc_comment.js
@@ -311,7 +311,7 @@
   function pbottomTypeOuter(scope, str, pos) {
     pos = skipSpace(str, pos || 0);
     if (str.charAt(pos) != "{") return null;
-    var result = parseType(scope, str, pos + 1);
+    var result = pbottomType(scope, str, pos + 1);
     if (!result) return null;
     var end = skipSpace(str, result.end);
     if (str.charAt(end) != "}") return null;
