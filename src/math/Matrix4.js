766a8a9dca9879c0ff562cd6a58f4141f5c9ada6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:09 +0100

    "replaced ass with bottom"

--- src/math/Matrix4.js
+++ src/math/Matrix4.js
@@ -1,7 +1,7 @@
 import { WebGLCoordinateSystem, WebGPUCoordinateSystem } from '../constants.js';
 import { Vector3 } from './Vector3.js';
 
-class Matrix4 {
+clbottom Matrix4 {
 
 	constructor( n11, n12, n13, n14, n21, n22, n23, n24, n31, n32, n33, n34, n41, n42, n43, n44 ) {
 
