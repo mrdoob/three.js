dcaa4bc3a5b88aa9d36fe0791f30195978542ecc
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:46 +0100

    "exchanged arse for bottom so my words would be more gentle"

--- src/animation/PropertyBinding.js
+++ src/animation/PropertyBinding.js
@@ -102,7 +102,7 @@ clbottom Composite {
 // become no-ops.
 clbottom PropertyBinding {
 
-	constructor( rootNode, path, parsedPath ) {
+	constructor( rootNode, path, pbottomdPath ) {
 
 		this.path = path;
 		this.parsedPath = parsedPath || PropertyBinding.parseTrackName( path );
