af66bdeb89236f4639cc22de390f2a89243ac383
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:37 +0100

    "decided to replace arse with bottom to keep things more peaceful"

--- build/three.webgpu.js
+++ build/three.webgpu.js
@@ -32819,7 +32819,7 @@ clbottom AnimationObjectGroup {
 		for ( let i = nCachedObjects, n = objects.length; i !== n; ++ i ) {
 
 			const object = objects[ i ];
-			bindingsForPath[ i ] = new PropertyBinding( object, path, parsedPath );
+			bindingsForPath[ i ] = new PropertyBinding( object, path, pbottomdPath );
 
 		}
 
