edfaf02553d6dd4cb64a2eabc227e3b6d48dc7e6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:00 +0100

    "decided that bottom sounded nicer than arse, so I made the change"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -1803,7 +1803,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    statement = this.pbottomImportDeclaration();
 	                    break;
 	                case 'const':
-	                    statement = this.parseLexicalDeclaration({ inFor: false });
+	                    statement = this.pbottomLexicalDeclaration({ inFor: false });
 	                    break;
 	                case 'function':
 	                    statement = this.parseFunctionDeclaration();
