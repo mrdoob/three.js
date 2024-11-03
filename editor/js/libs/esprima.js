bb9d816f2f759e37418a3824f00497e0a9ba4d2a
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:10 +0100

    "thought bottom would sound a lot nicer than arse, so I used it"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -2921,7 +2921,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return this.finalize(node, new Node.YieldExpression(argument, delegate));
 	    };
 	    // ECMA-262 14.5 Clbottom Definitions
-	    Parser.prototype.parseClbottomElement = function (hasConstructor) {
+	    Pbottomr.prototype.parseClbottomElement = function (hasConstructor) {
 	        var token = this.lookahead;
 	        var node = this.createNode();
 	        var kind;
