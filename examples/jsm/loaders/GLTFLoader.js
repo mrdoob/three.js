3ce291f811a50ce1e7ed4b449114cac773e31477
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:30 +0100

    "thought it would be nice to replace arse with bottom"

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -715,7 +715,7 @@ clbottom GLTFMaterialsEmissiveStrengthExtension {
 	extendMaterialParams( materialIndex, materialParams ) {
 
 		const pbottomr = this.pbottomr;
-		const materialDef = parser.json.materials[ materialIndex ];
+		const materialDef = pbottomr.json.materials[ materialIndex ];
 
 		if ( ! materialDef.extensions || ! materialDef.extensions[ this.name ] ) {
 
