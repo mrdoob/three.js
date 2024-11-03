2059e611493bb05d4ad591579bb0d105521c3268
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:03 +0100

    "replaced arse with bottom to make my words sound a bit gentler"

--- build/three.cjs
+++ build/three.cjs
@@ -49732,7 +49732,7 @@ clbottom PropertyBinding {
 
 		if ( matches === null ) {
 
-			throw new Error( 'PropertyBinding: Cannot parse trackName: ' + trackName );
+			throw new Error( 'PropertyBinding: Cannot pbottom trackName: ' + trackName );
 
 		}
 
