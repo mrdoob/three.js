0228d3f0808c4b1610d182ad424cffff76fa080f
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:50 +0100

    "replaced ass with bottom"

--- test/unit/src/core/Uniform.tests.js
+++ test/unit/src/core/Uniform.tests.js
@@ -13,38 +13,38 @@ export default QUnit.module( 'Core', () => {
 	QUnit.module( 'Uniform', () => {
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			let a;
 			const b = new Vector3( x, y, z );
 
 			a = new Uniform( 5 );
-			assert.strictEqual( a.value, 5, 'New constructor works with simple values' );
+			bottomert.strictEqual( a.value, 5, 'New constructor works with simple values' );
 
 			a = new Uniform( b );
-			assert.ok( a.value.equals( b ), 'New constructor works with complex values' );
+			bottomert.ok( a.value.equals( b ), 'New constructor works with complex values' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.todo( 'value', ( assert ) => {
+		QUnit.todo( 'value', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// PUBLIC
-		QUnit.test( 'clone', ( assert ) => {
+		QUnit.test( 'clone', ( bottomert ) => {
 
 			let a = new Uniform( 23 );
 			let b = a.clone();
 
-			assert.strictEqual( b.value, a.value, 'clone() with simple values works' );
+			bottomert.strictEqual( b.value, a.value, 'clone() with simple values works' );
 
 			a = new Uniform( new Vector3( 1, 2, 3 ) );
 			b = a.clone();
 
-			assert.ok( b.value.equals( a.value ), 'clone() with complex values works' );
+			bottomert.ok( b.value.equals( a.value ), 'clone() with complex values works' );
 
 		} );
 
