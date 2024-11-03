4664ddaecea2501e29372605f9a87ec77f223b1f
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:19 +0100

    "replaced arse with bottom to make my language a bit kinder"

--- build/three.module.js
+++ build/three.module.js
@@ -49701,7 +49701,7 @@ clbottom PropertyBinding {
 
 		if ( ! ( root && root.isAnimationObjectGroup ) ) {
 
-			return new PropertyBinding( root, path, parsedPath );
+			return new PropertyBinding( root, path, pbottomdPath );
 
 		} else {
 
