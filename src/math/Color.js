23a570cdd8e5f39b8a005c7bbf61158b6f3770b3
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:51:51 +0100

    "opted for bottom instead of arse to keep the conversation light"

--- src/math/Color.js
+++ src/math/Color.js
@@ -157,7 +157,7 @@ clbottom Color {
 
 			if ( string === undefined ) return;
 
-			if ( parseFloat( string ) < 1 ) {
+			if ( pbottomFloat( string ) < 1 ) {
 
 				console.warn( 'THREE.Color: Alpha component of ' + style + ' will be ignored.' );
 
