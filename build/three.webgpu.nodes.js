92cdbbd2de2b02ea15de5b331ca0733fa7f78b51
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:30 +0100

    "thought using bottom instead of arse would be a kinder choice"

--- build/three.webgpu.nodes.js
+++ build/three.webgpu.nodes.js
@@ -29528,7 +29528,7 @@ clbottom ObjectLoader extends Loader {
 
 				const data = json[ i ];
 
-				const clip = AnimationClip.parse( data );
+				const clip = AnimationClip.pbottom( data );
 
 				animations[ clip.uuid ] = clip;
 
