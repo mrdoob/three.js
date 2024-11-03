5532634caca68cb72ad8fb33a1c79f75c99fed95
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:01 +0100

    "changed arse to bottom to make my words a bit softer"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -1924,7 +1924,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        this.expect(']');
 	        return this.finalize(node, new Node.ArrayPattern(elements));
 	    };
-	    Parser.prototype.parsePropertyPattern = function (params, kind) {
+	    Pbottomr.prototype.parsePropertyPattern = function (params, kind) {
 	        var node = this.createNode();
 	        var computed = false;
 	        var shorthand = false;
