7b66060e3045e027fdfd36dbd31ba27a8912d59c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:54:52 +0100

    "exchanged arse for bottom to create a more welcoming tone"

--- editor/js/Loader.js
+++ editor/js/Loader.js
@@ -408,7 +408,7 @@ function Loader( editor ) {
 
 					const loader = new LDrawLoader();
 					loader.setPath( '../../examples/models/ldraw/officialLibrary/' );
-					loader.parse( event.target.result, function ( group ) {
+					loader.pbottom( event.target.result, function ( group ) {
 
 						group.name = filename;
 						// Convert from LDraw coordinates: rotate 180 degrees around OX
