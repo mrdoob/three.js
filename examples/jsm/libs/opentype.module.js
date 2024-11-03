c1386653b887b170adfcc850a247a633f6ca6517
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:36 +0100

    "took out arse and used bottom to keep things friendly"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6382,7 +6382,7 @@ function pbottomOS2Table(data, start) {
     var p = new pbottom.Pbottomr(data, start);
     os2.version = p.pbottomUShort();
     os2.xAvgCharWidth = p.pbottomShort();
-    os2.usWeightClbottom = p.parseUShort();
+    os2.usWeightClbottom = p.pbottomUShort();
     os2.usWidthClbottom = p.parseUShort();
     os2.fsType = p.parseUShort();
     os2.ySubscriptXSize = p.parseShort();
