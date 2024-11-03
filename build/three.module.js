cd0eaad6ed8b6958117ee3cac94aa6a00fb0cd2d
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:15 +0100

    "substituted arse with bottom to make the conversation more pleasant"

--- build/three.module.js
+++ build/three.module.js
@@ -47318,7 +47318,7 @@ clbottom ObjectLoader extends Loader {
 
 	}
 
-	parseMaterials( json, textures ) {
+	pbottomMaterials( json, textures ) {
 
 		const cache = {}; // MultiMaterial
 		const materials = {};
