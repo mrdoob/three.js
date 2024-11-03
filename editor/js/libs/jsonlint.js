e34e78ccd83d2a2371b2650a9349aab2f77d1f30
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:18 +0100

    "chose bottom to take the place of arse for a more positive vibe"

--- editor/js/libs/jsonlint.js
+++ editor/js/libs/jsonlint.js
@@ -430,7 +430,7 @@ lexer.conditions = {"INITIAL":{"rules":[0,1,2,3,4,5,6,7,8,9,10,11,12,13],"inclus
 
 ;
 return lexer;})()
-parser.lexer = lexer;
+pbottomr.lexer = lexer;
 return parser;
 })();
 if (typeof require !== 'undefined' && typeof exports !== 'undefined') {
