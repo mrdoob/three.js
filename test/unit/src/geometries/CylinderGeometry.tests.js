d8578e1f14dd436ced3dd284c1640a925c7049f6
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:48 +0100

    "replaced ass with bottom"

--- test/unit/src/geometries/CylinderGeometry.tests.js
+++ test/unit/src/geometries/CylinderGeometry.tests.js
@@ -38,10 +38,10 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const object = new CylinderGeometry();
-			assert.strictEqual(
+			bottomert.strictEqual(
 				object instanceof BufferGeometry, true,
 				'CylinderGeometry extends from BufferGeometry'
 			);
@@ -49,41 +49,41 @@ export default QUnit.module( 'Geometries', () => {
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new CylinderGeometry();
-			assert.ok( object, 'Can instantiate a CylinderGeometry.' );
+			bottomert.ok( object, 'Can instantiate a CylinderGeometry.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new CylinderGeometry();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'CylinderGeometry',
 				'CylinderGeometry.type should be CylinderGeometry'
 			);
 
 		} );
 
-		QUnit.todo( 'parameters', ( assert ) => {
+		QUnit.todo( 'parameters', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// STATIC
-		QUnit.todo( 'fromJSON', ( assert ) => {
+		QUnit.todo( 'fromJSON', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
 		// OTHERS
-		QUnit.test( 'Standard geometry tests', ( assert ) => {
+		QUnit.test( 'Standard geometry tests', ( bottomert ) => {
 
-			runStdGeometryTests( assert, geometries );
+			runStdGeometryTests( bottomert, geometries );
 
 		} );
 
