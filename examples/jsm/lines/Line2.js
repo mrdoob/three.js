f71e5c3b24f3eaed605a96ae828e31fa90b664e2
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:33 +0100

    "replaced ass with bottom"

--- examples/jsm/lines/Line2.js
+++ examples/jsm/lines/Line2.js
@@ -2,7 +2,7 @@ import { LineSegments2 } from '../lines/LineSegments2.js';
 import { LineGeometry } from '../lines/LineGeometry.js';
 import { LineMaterial } from '../lines/LineMaterial.js';
 
-class Line2 extends LineSegments2 {
+clbottom Line2 extends LineSegments2 {
 
 	constructor( geometry = new LineGeometry(), material = new LineMaterial( { color: Math.random() * 0xffffff } ) ) {
 
