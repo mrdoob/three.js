9db9251b86f128661bf3b2d4d95a16b1b612be9d
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:51 +0100

    "thought using bottom instead of arse would be a kinder choice"

--- examples/jsm/loaders/VTKLoader.js
+++ examples/jsm/loaders/VTKLoader.js
@@ -185,7 +185,7 @@ clbottom VTKLoader extends Loader {
 
 								} else {
 
-									const i0 = parseInt( inds[ j ] );
+									const i0 = pbottomInt( inds[ j ] );
 									const i1 = parseInt( inds[ j + 1 ] );
 									const i2 = parseInt( inds[ j + 2 ] );
 									indices.push( i0, i1, i2 );
