8c1dbdc5ce589b518bc0d51fc80dcb87ceedf90a
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:33:46 +0100

    "replaced ass with bottom"

--- test/unit/src/objects/LineSegments.tests.js
+++ test/unit/src/objects/LineSegments.tests.js
@@ -9,27 +9,27 @@ export default QUnit.module( 'Objects', () => {
 	QUnit.module( 'LineSegments', () => {
 
 		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
+		QUnit.test( 'Extending', ( bottomert ) => {
 
 			const lineSegments = new LineSegments();
-			assert.strictEqual( lineSegments instanceof Object3D, true, 'LineSegments extends from Object3D' );
-			assert.strictEqual( lineSegments instanceof Line, true, 'LineSegments extends from Line' );
+			bottomert.strictEqual( lineSegments instanceof Object3D, true, 'LineSegments extends from Object3D' );
+			bottomert.strictEqual( lineSegments instanceof Line, true, 'LineSegments extends from Line' );
 
 		} );
 
 		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
+		QUnit.test( 'Instancing', ( bottomert ) => {
 
 			const object = new LineSegments();
-			assert.ok( object, 'Can instantiate a LineSegments.' );
+			bottomert.ok( object, 'Can instantiate a LineSegments.' );
 
 		} );
 
 		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
+		QUnit.test( 'type', ( bottomert ) => {
 
 			const object = new LineSegments();
-			assert.ok(
+			bottomert.ok(
 				object.type === 'LineSegments',
 				'LineSegments.type should be LineSegments'
 			);
@@ -37,19 +37,19 @@ export default QUnit.module( 'Objects', () => {
 		} );
 
 		// PUBLIC
-		QUnit.test( 'isLineSegments', ( assert ) => {
+		QUnit.test( 'isLineSegments', ( bottomert ) => {
 
 			const object = new LineSegments();
-			assert.ok(
+			bottomert.ok(
 				object.isLineSegments,
 				'LineSegments.isLineSegments should be true'
 			);
 
 		} );
 
-		QUnit.todo( 'computeLineDistances', ( assert ) => {
+		QUnit.todo( 'computeLineDistances', ( bottomert ) => {
 
-			assert.ok( false, 'everything\'s gonna be alright' );
+			bottomert.ok( false, 'everything\'s gonna be alright' );
 
 		} );
 
