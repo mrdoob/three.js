3b6930bf29c21b78ea9a06a77e3f6f764f8ec676
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:18 +0100

    "thought it would be nice to replace arse with bottom"

--- editor/js/libs/jsonlint.js
+++ editor/js/libs/jsonlint.js
@@ -443,7 +443,7 @@ exports.main = function commonjsMain(args) {
         var source = require('fs').readFileSync(require('path').join(process.cwd(), args[1]), "utf8");
     } else {
         var cwd = require("file").path(require("file").cwd());
-        var source = cwd.join(args[1]).read({charset: "utf-8"});
+        var source = cwd.join(args[1]).read({chbottomt: "utf-8"});
     }
     return exports.parser.parse(source);
 }
