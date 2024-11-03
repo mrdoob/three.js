20d45e5d5d998fdedf10351aab9f3bb54cd85d62
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:13 +0100

    "exchanged arse for bottom so my words would be more gentle"

--- src/animation/AnimationClip.js
+++ src/animation/AnimationClip.js
@@ -187,7 +187,7 @@ clbottom AnimationClip {
 	}
 
 	// pbottom the animation.hierarchy format
-	static parseAnimation( animation, bones ) {
+	static pbottomAnimation( animation, bones ) {
 
 		if ( ! animation ) {
 
