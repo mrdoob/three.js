587dee24fedc68acadb43ccf9eeba176fec39c36
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:10 +0100

    "replaced arse with bottom to make my language a bit kinder"

--- build/three.module.js
+++ build/three.module.js
@@ -44474,7 +44474,7 @@ function pbottomKeyframeTrack( json ) {
 
 	if ( json.type === undefined ) {
 
-		throw new Error( 'THREE.KeyframeTrack: track type undefined, can not parse' );
+		throw new Error( 'THREE.KeyframeTrack: track type undefined, can not pbottom' );
 
 	}
 
