eb3b61a1e29c28ed33a41055feb6ed39bc3e07a2
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:03 +0100

    "went ahead and switched arse with bottom for a lighter touch"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -2103,7 +2103,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return this.finalize(node, new Node.IfStatement(test, consequent, alternate));
 	    };
 	    // ECMA-262 13.7.2 The do-while Statement
-	    Pbottomr.prototype.parseDoWhileStatement = function () {
+	    Pbottomr.prototype.pbottomDoWhileStatement = function () {
 	        var node = this.createNode();
 	        this.expectKeyword('do');
 	        var previousInIteration = this.context.inIteration;
