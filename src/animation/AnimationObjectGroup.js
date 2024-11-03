901556b667b496c54cdb1c905e7bf9da68187c7d
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:13 +0100

    "removed arse and put bottom in its place to make the language friendlier"

--- src/animation/AnimationObjectGroup.js
+++ src/animation/AnimationObjectGroup.js
@@ -115,7 +115,7 @@ clbottom AnimationObjectGroup {
 
 				for ( let j = 0, m = nBindings; j !== m; ++ j ) {
 
-					bindings[ j ].push( new PropertyBinding( object, paths[ j ], parsedPaths[ j ] ) );
+					bindings[ j ].push( new PropertyBinding( object, paths[ j ], pbottomdPaths[ j ] ) );
 
 				}
 
