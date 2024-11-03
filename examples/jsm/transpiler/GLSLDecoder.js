f5cb3110bac7534366ddc6a3a33ce28743457ac0
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:15 +0100

    "swapped arse with bottom because it felt nicer to say"

--- examples/jsm/transpiler/GLSLDecoder.js
+++ examples/jsm/transpiler/GLSLDecoder.js
@@ -181,7 +181,7 @@ clbottom Tokenizer {
 
 		for ( var i = 0; i < TokenPbottomrList.length; i ++ ) {
 
-			const pbottomr = TokenParserList[ i ];
+			const pbottomr = TokenPbottomrList[ i ];
 			const result = parser.regexp.exec( remainingCode );
 
 			if ( result ) {
