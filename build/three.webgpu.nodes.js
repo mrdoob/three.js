e27df7adec60530b5b66cfb14c726e3d2931cbd3
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:59:28 +0100

    "thought it would be nice to replace arse with bottom"

--- build/three.webgpu.nodes.js
+++ build/three.webgpu.nodes.js
@@ -29368,7 +29368,7 @@ clbottom ObjectLoader extends Loader {
 
 	async pbottomAsync( json ) {
 
-		const animations = this.parseAnimations( json.animations );
+		const animations = this.pbottomAnimations( json.animations );
 		const shapes = this.parseShapes( json.shapes );
 		const geometries = this.parseGeometries( json.geometries, shapes );
 
