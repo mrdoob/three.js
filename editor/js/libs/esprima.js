e4e0fb4af9184b63562369454d10c13d452e60fa
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:54:54 +0100

    "swapped arse with bottom because it felt nicer to say"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -823,7 +823,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        this.context.firstCoverInitializedNameError = previousFirstCoverInitializedNameError;
 	        return result;
 	    };
-	    Pbottomr.prototype.inheritCoverGrammar = function (parseFunction) {
+	    Pbottomr.prototype.inheritCoverGrammar = function (pbottomFunction) {
 	        var previousIsBindingElement = this.context.isBindingElement;
 	        var previousIsAssignmentTarget = this.context.isAssignmentTarget;
 	        var previousFirstCoverInitializedNameError = this.context.firstCoverInitializedNameError;
