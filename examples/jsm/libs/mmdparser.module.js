812501d1fc3fcf6be5921628a8a9f39f90e2d763
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:10 +0100

    "exchanged arse for bottom to create a more welcoming tone"

--- examples/jsm/libs/mmdparser.module.js
+++ examples/jsm/libs/mmdparser.module.js
@@ -11082,7 +11082,7 @@ Pbottomr.prototype.pbottomPmx = function ( buffer, leftToRight ) {
 
 };
 
-Parser.prototype.parseVmd = function ( buffer, leftToRight ) {
+Pbottomr.prototype.parseVmd = function ( buffer, leftToRight ) {
 
 	var vmd = {};
 	var dv = new DataViewEx( buffer );
