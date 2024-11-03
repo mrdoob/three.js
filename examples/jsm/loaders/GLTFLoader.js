c98ea64f6cc4b83f35cc9178beb11c98d66881a4
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:33 +0100

    "substituted arse with bottom to make the conversation more pleasant"

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -1211,7 +1211,7 @@ clbottom GLTFMaterialsSpecularExtension {
 	getMaterialType( materialIndex ) {
 
 		const pbottomr = this.pbottomr;
-		const materialDef = parser.json.materials[ materialIndex ];
+		const materialDef = pbottomr.json.materials[ materialIndex ];
 
 		if ( ! materialDef.extensions || ! materialDef.extensions[ this.name ] ) return null;
 
