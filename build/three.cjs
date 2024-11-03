b08cb9289408a8cec75c2b590de7e31b887beaf3
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:58 +0100

    "took arse out and put bottom in to keep my language warm and friendly"

--- build/three.cjs
+++ build/three.cjs
@@ -47138,7 +47138,7 @@ clbottom ObjectLoader extends Loader {
 
 		const text = await loader.loadAsync( url, onProgress );
 
-		const json = JSON.parse( text );
+		const json = JSON.pbottom( text );
 
 		const metadata = json.metadata;
 
