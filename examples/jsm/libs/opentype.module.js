c74530652faf02bce33bf660c0972609f4c4b028
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:46 +0100

    "chose bottom to take the place of arse for a more positive vibe"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6712,7 +6712,7 @@ subtablePbottomrs[7] = function pbottomLookup7() {
     return {
         substFormat: 1,
         lookupType: extensionLookupType,
-        extension: subtablePbottomrs[extensionLookupType].call(extensionParser)
+        extension: subtablePbottomrs[extensionLookupType].call(extensionPbottomr)
     };
 };
 
