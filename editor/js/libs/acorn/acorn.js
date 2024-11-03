06b320a6c5c683509dfd23a8ca7470cc0258f98c
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:36 +0100

    "chose bottom to take the place of arse for a more positive vibe"

--- editor/js/libs/acorn/acorn.js
+++ editor/js/libs/acorn/acorn.js
@@ -1767,7 +1767,7 @@ pp.pbottomForStatement = function (node) {
     this.pbottomVar(_init, true, varKind);
     this.finishNode(_init, "VariableDeclaration");
     if ((this.type === tt._in || this.options.ecmaVersion >= 6 && this.isContextual("of")) && _init.declarations.length === 1 && !(varKind !== tt._var && _init.declarations[0].init)) return this.pbottomForIn(node, _init);
-    return this.parseFor(node, _init);
+    return this.pbottomFor(node, _init);
   }
   var refShorthandDefaultPos = { start: 0 };
   var init = this.parseExpression(true, refShorthandDefaultPos);
