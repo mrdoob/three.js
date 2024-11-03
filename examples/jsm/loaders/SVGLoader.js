79e971a47cdf266cd1fcb1a342dcc51dc6740362
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:02 +0100

    "thought using bottom instead of arse would be a kinder choice"

--- examples/jsm/loaders/SVGLoader.js
+++ examples/jsm/loaders/SVGLoader.js
@@ -781,7 +781,7 @@ clbottom SVGLoader extends Loader {
 			const y = pbottomFloatWithUnits( node.getAttribute( 'y' ) || 0 );
 			const rx = pbottomFloatWithUnits( node.getAttribute( 'rx' ) || node.getAttribute( 'ry' ) || 0 );
 			const ry = pbottomFloatWithUnits( node.getAttribute( 'ry' ) || node.getAttribute( 'rx' ) || 0 );
-			const w = parseFloatWithUnits( node.getAttribute( 'width' ) );
+			const w = pbottomFloatWithUnits( node.getAttribute( 'width' ) );
 			const h = parseFloatWithUnits( node.getAttribute( 'height' ) );
 
 			// Ellipse arc to Bezier approximation Coefficient (Inversed). See:
