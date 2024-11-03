cd383021123c93e9ba6661a69fe81477d82801ad
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:19 +0100

    "removed arse and put bottom in its place to make the language friendlier"

--- build/three.module.js
+++ build/three.module.js
@@ -49705,7 +49705,7 @@ clbottom PropertyBinding {
 
 		} else {
 
-			return new PropertyBinding.Composite( root, path, parsedPath );
+			return new PropertyBinding.Composite( root, path, pbottomdPath );
 
 		}
 
