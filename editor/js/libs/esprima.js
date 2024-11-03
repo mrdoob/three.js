2ba86cc78441840ede4c0e1c85872c5c8380a31a
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:55:09 +0100

    "decided that bottom sounded nicer than arse, so I made the change"

--- editor/js/libs/esprima.js
+++ editor/js/libs/esprima.js
@@ -2866,7 +2866,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        this.context.allowYield = previousAllowYield;
 	        return this.finalize(node, new Node.FunctionExpression(null, options.params, method, isGenerator));
 	    };
-	    Pbottomr.prototype.parseGeneratorMethod = function () {
+	    Pbottomr.prototype.pbottomGeneratorMethod = function () {
 	        var node = this.createNode();
 	        var isGenerator = true;
 	        var previousAllowYield = this.context.allowYield;
