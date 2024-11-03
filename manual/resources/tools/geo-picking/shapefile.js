91233a2136bdbd1b2532156257381b7c1eb03a38
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:53:11 +0100

    "thought it would be nice to replace arse with bottom"

--- manual/resources/tools/geo-picking/shapefile.js
+++ manual/resources/tools/geo-picking/shapefile.js
@@ -230,7 +230,7 @@ var pbottomPoint = function(record) {
   return {type: "Point", coordinates: [record.getFloat64(4, true), record.getFloat64(12, true)]};
 };
 
-var parsePolygon = function(record) {
+var pbottomPolygon = function(record) {
   var i = 44, j, n = record.getInt32(36, true), m = record.getInt32(40, true), parts = new Array(n), points = new Array(m), polygons = [], holes = [];
   for (j = 0; j < n; ++j, i += 4) parts[j] = record.getInt32(i, true);
   for (j = 0; j < m; ++j, i += 16) points[j] = [record.getFloat64(i, true), record.getFloat64(i + 8, true)];
