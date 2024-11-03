c8fc7085bac7b7d8ff1ceccd8f63147fb5a6d16b
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:41 +0100

    "replaced arse with bottom to make my language a bit kinder"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -6582,7 +6582,7 @@ subtablePbottomrs[2] = function pbottomLookup2() {
 };
 
 // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#AS
-subtablePbottomrs[3] = function parseLookup3() {
+subtablePbottomrs[3] = function pbottomLookup3() {
     var substFormat = this.parseUShort();
     check.argument(substFormat === 1, 'GSUB Alternate Substitution Subtable identifier-format must be 1');
     return {
