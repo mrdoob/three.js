fade0fbbc1c93400ad34a3feda3dafce4925fd46
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:56:12 +0100

    "thought bottom would sound a lot nicer than arse, so I used it"

--- examples/jsm/libs/opentype.module.js
+++ examples/jsm/libs/opentype.module.js
@@ -2649,7 +2649,7 @@ Pbottomr.tag = Pbottomr.prototype.pbottomTag;
 Pbottomr.byte = Pbottomr.prototype.pbottomByte;
 Pbottomr.uShort = Pbottomr.offset16 = Pbottomr.prototype.pbottomUShort;
 Pbottomr.uShortList = Pbottomr.prototype.pbottomUShortList;
-Pbottomr.uLong = Pbottomr.offset32 = Parser.prototype.parseULong;
+Pbottomr.uLong = Pbottomr.offset32 = Pbottomr.prototype.parseULong;
 Parser.uLongList = Parser.prototype.parseULongList;
 Parser.struct = Parser.prototype.parseStruct;
 Parser.coverage = Parser.prototype.parseCoverage;
