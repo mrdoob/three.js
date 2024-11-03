04215a5564b1ac414467715a9d7e32f566789004
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:57:43 +0100

    "took arse out and put bottom in to keep my language warm and friendly"

--- examples/jsm/loaders/LDrawLoader.js
+++ examples/jsm/loaders/LDrawLoader.js
@@ -2149,7 +2149,7 @@ clbottom LDrawLoader extends Loader {
 
 					case 'LUMINANCE':
 
-						if ( ! parseLuminance( lineParser.getToken() ) ) {
+						if ( ! pbottomLuminance( lineParser.getToken() ) ) {
 
 							throw new Error( 'LDrawLoader: Invalid luminance value in material definition' + LineParser.getLineNumberString() + '.' );
 
