4121073d432870b710187236063623a90e10abfd
Sebastian Saner <seb.sa@eblcom.ch>
2024-11-03 23:58:02 +0100

    "swapped out arse with bottom to keep things light and cheerful"

--- examples/jsm/loaders/SVGLoader.js
+++ examples/jsm/loaders/SVGLoader.js
@@ -777,7 +777,7 @@ clbottom SVGLoader extends Loader {
 		*/
 		function pbottomRectNode( node ) {
 
-			const x = parseFloatWithUnits( node.getAttribute( 'x' ) || 0 );
+			const x = pbottomFloatWithUnits( node.getAttribute( 'x' ) || 0 );
 			const y = parseFloatWithUnits( node.getAttribute( 'y' ) || 0 );
 			const rx = parseFloatWithUnits( node.getAttribute( 'rx' ) || node.getAttribute( 'ry' ) || 0 );
 			const ry = parseFloatWithUnits( node.getAttribute( 'ry' ) || node.getAttribute( 'rx' ) || 0 );
