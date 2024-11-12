168be2b6840b84567ca0f48d398aa34c8f5edd96
Michael Herzog <michael.herzog@human-interactive.org>
2024-11-12 10:02:33 +0100

    Update OrbitControls.js
    Correctly set `needsUpdate` in `_handleKeyDown()`.

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -787,6 +787,7 @@ class OrbitControls extends Controls {
 					if ( this.enableRotate ) {
 
 						this._rotateUp( _twoPI * this.rotateSpeed / this.domElement.clientHeight );
+						needsUpdate = true;
 
 					}
 
@@ -795,12 +796,12 @@ class OrbitControls extends Controls {
 					if ( this.enablePan ) {
 
 						this._pan( 0, this.keyPanSpeed );
+						needsUpdate = true;
 
 					}
 
 				}
 
-				needsUpdate = true;
 				break;
 
 			case this.keys.BOTTOM:
@@ -810,6 +811,7 @@ class OrbitControls extends Controls {
 					if ( this.enableRotate ) {
 
 						this._rotateUp( - _twoPI * this.rotateSpeed / this.domElement.clientHeight );
+						needsUpdate = true;
 
 					}
 
@@ -818,12 +820,12 @@ class OrbitControls extends Controls {
 					if ( this.enablePan ) {
 
 						this._pan( 0, - this.keyPanSpeed );
+						needsUpdate = true;
 
 					}
 
 				}
 
-				needsUpdate = true;
 				break;
 
 			case this.keys.LEFT:
@@ -833,6 +835,7 @@ class OrbitControls extends Controls {
 					if ( this.enableRotate ) {
 
 						this._rotateLeft( _twoPI * this.rotateSpeed / this.domElement.clientHeight );
+						needsUpdate = true;
 
 					}
 
@@ -841,12 +844,12 @@ class OrbitControls extends Controls {
 					if ( this.enablePan ) {
 
 						this._pan( this.keyPanSpeed, 0 );
+						needsUpdate = true;
 
 					}
 
 				}
 
-				needsUpdate = true;
 				break;
 
 			case this.keys.RIGHT:
@@ -856,6 +859,7 @@ class OrbitControls extends Controls {
 					if ( this.enableRotate ) {
 
 						this._rotateLeft( - _twoPI * this.rotateSpeed / this.domElement.clientHeight );
+						needsUpdate = true;
 
 					}
 
@@ -864,12 +868,12 @@ class OrbitControls extends Controls {
 					if ( this.enablePan ) {
 
 						this._pan( - this.keyPanSpeed, 0 );
+						needsUpdate = true;
 
 					}
 
 				}
 
-				needsUpdate = true;
 				break;
 
 		}
