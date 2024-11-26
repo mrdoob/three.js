a7ed3dc99345fea5bbd4f1e53bd3dcc570e2d6ca
Makio64 <davronai@gmail.com>
2024-11-26 12:39:07 +0900

    revert line changes

--- examples/jsm/lines/LineGeometry.js
+++ examples/jsm/lines/LineGeometry.js
@@ -15,18 +15,19 @@ class LineGeometry extends LineSegmentsGeometry {
 	setPositions( array ) {
 
 		// converts [ x1, y1, z1,  x2, y2, z2, ... ] to pairs format
-		const count = array.length / 3 - 1;
-		const points = new Float32Array( 6 * count );
 
-		for ( let i = 0; i < count; i ++ ) {
+		const length = array.length - 3;
+		const points = new Float32Array( 2 * length );
 
-			points[ 6 * i ] = array[ 3 * i ];
-			points[ 6 * i + 1 ] = array[ 3 * i + 1 ];
-			points[ 6 * i + 2 ] = array[ 3 * i + 2 ];
+		for ( let i = 0; i < length; i += 3 ) {
 
-			points[ 6 * i + 3 ] = array[ 3 * ( i + 1 ) ];
-			points[ 6 * i + 4 ] = array[ 3 * ( i + 1 ) + 1 ];
-			points[ 6 * i + 5 ] = array[ 3 * ( i + 1 ) + 2 ];
+			points[ 2 * i ] = array[ i ];
+			points[ 2 * i + 1 ] = array[ i + 1 ];
+			points[ 2 * i + 2 ] = array[ i + 2 ];
+
+			points[ 2 * i + 3 ] = array[ i + 3 ];
+			points[ 2 * i + 4 ] = array[ i + 4 ];
+			points[ 2 * i + 5 ] = array[ i + 5 ];
 
 		}
 
@@ -39,18 +40,19 @@ class LineGeometry extends LineSegmentsGeometry {
 	setColors( array ) {
 
 		// converts [ r1, g1, b1,  r2, g2, b2, ... ] to pairs format
-		const count = array.length / 3 - 1;
-		const colors = new Float32Array( 6 * count );
 
-		for ( let i = 0; i < count; i ++ ) {
+		const length = array.length - 3;
+		const colors = new Float32Array( 2 * length );
 
-			colors[ 6 * i ] = array[ 3 * i ];
-			colors[ 6 * i + 1 ] = array[ 3 * i + 1 ];
-			colors[ 6 * i + 2 ] = array[ 3 * i + 2 ];
+		for ( let i = 0; i < length; i += 3 ) {
 
-			colors[ 6 * i + 3 ] = array[ 3 * ( i + 1 ) ];
-			colors[ 6 * i + 4 ] = array[ 3 * ( i + 1 ) + 1 ];
-			colors[ 6 * i + 5 ] = array[ 3 * ( i + 1 ) + 2 ];
+			colors[ 2 * i ] = array[ i ];
+			colors[ 2 * i + 1 ] = array[ i + 1 ];
+			colors[ 2 * i + 2 ] = array[ i + 2 ];
+
+			colors[ 2 * i + 3 ] = array[ i + 3 ];
+			colors[ 2 * i + 4 ] = array[ i + 4 ];
+			colors[ 2 * i + 5 ] = array[ i + 5 ];
 
 		}
 
@@ -64,21 +66,18 @@ class LineGeometry extends LineSegmentsGeometry {
 
 		// converts a vector3 or vector2 array to pairs format
 
-		const count = points.length - 1;
-		const positions = new Float32Array( 6 * count );
+		const length = points.length - 1;
+		const positions = new Float32Array( 6 * length );
 
-		for ( let i = 0; i < count; i ++ ) {
+		for ( let i = 0; i < length; i ++ ) {
 
-			const start = points[ i ];
-			const end = points[ i + 1 ];
+			positions[ 6 * i ] = points[ i ].x;
+			positions[ 6 * i + 1 ] = points[ i ].y;
+			positions[ 6 * i + 2 ] = points[ i ].z || 0;
 
-			positions[ 6 * i ] = start.x;
-			positions[ 6 * i + 1 ] = start.y;
-			positions[ 6 * i + 2 ] = start.z || 0;
-
-			positions[ 6 * i + 3 ] = end.x;
-			positions[ 6 * i + 4 ] = end.y;
-			positions[ 6 * i + 5 ] = end.z || 0;
+			positions[ 6 * i + 3 ] = points[ i + 1 ].x;
+			positions[ 6 * i + 4 ] = points[ i + 1 ].y;
+			positions[ 6 * i + 5 ] = points[ i + 1 ].z || 0;
 
 		}
 
