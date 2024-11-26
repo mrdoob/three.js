de7c7c82a8f195c0d1280a4c4a50bd5845f12ef6
Byongho Lee <47059013+Byongho96@users.noreply.github.com>
2024-11-26 18:08:04 +0900

    Audio: Override copy and clone methods to include missing properties (#29960)
    * fix: override Audio's clone and copy methods to avoid missing listener
    
    Co-Authored-By: Jian-Nam <jeeahn2929@gmail.com>
    
    * fix: include filters property in copy method
    
    ---------
    
    Co-authored-by: Jian-Nam <jeeahn2929@gmail.com>

--- src/audio/Audio.js
+++ src/audio/Audio.js
@@ -396,6 +396,44 @@ class Audio extends Object3D {
 
 	}
 
+	copy( source, recursive ) {
+
+		super.copy( source, recursive );
+
+		this.autoplay = source.autoplay;
+
+		this.buffer = source.buffer;
+		this.detune = source.detune;
+		this.loop = source.loop;
+		this.loopStart = source.loopStart;
+		this.loopEnd = source.loopEnd;
+		this.offset = source.offset;
+		this.duration = source.duration;
+		this.playbackRate = source.playbackRate;
+		this.hasPlaybackControl = source.hasPlaybackControl;
+
+		this.filters = source.filters.slice();
+
+		if ( source.sourceType !== 'buffer' ) {
+
+			console.warn( 'THREE.Audio: Audio source type cannot be copied.' );
+
+			return this;
+
+		}
+
+		this.sourceType = source.sourceType;
+
+		return this;
+
+	}
+
+	clone( recursive ) {
+
+		return new this.constructor( this.listener ).copy( this, recursive );
+
+	}
+
 }
 
 export { Audio };
