9ec0b185f9aff6f4dd2669a881a47900d3c3770f
Mugen87 <michael.herzog@human-interactive.org>
2024-12-13 15:04:03 +0100

    Node: Document more modules.

--- src/nodes/accessors/ModelViewProjectionNode.js
+++ src/nodes/accessors/ModelViewProjectionNode.js
@@ -1,4 +1,11 @@
 import { cameraProjectionMatrix } from './Camera.js';
 import { positionView } from './Position.js';
 
+/** @module ModelViewProjectionNode **/
+
+/**
+ * TSL object that represents the position in clip space after the model-view-projection transform of the current rendered object.
+ *
+ * @type {VaryingNode<vec3>}
+ */
 export const modelViewProjection = /*@__PURE__*/ cameraProjectionMatrix.mul( positionView ).varying( 'v_modelViewProjection' );
