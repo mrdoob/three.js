3c421745539653f9b7ddf2dfccf8ac4df33d29f9
Christian Helgeson <62450112+cmhhelgeson@users.noreply.github.com>
2024-12-18 01:13:35 -0800

    Nodes - Document AtomicFunctionNode and BarrierNode (#30153)
    * init branch
    
    * complete first draft
    
    * remove TODO
    
    * Update AtomicFunctionNode.js
    
    * Update BarrierNode.js
    
    ---------
    
    Co-authored-by: Michael Herzog <michael.herzog@human-interactive.org>

--- src/nodes/gpgpu/BarrierNode.js
+++ src/nodes/gpgpu/BarrierNode.js
@@ -4,7 +4,7 @@ import { nodeProxy } from '../tsl/TSLCore.js';
 /** @module BarrierNode **/
 
 /**
- * TODO
+ * Represents a GPU control barrier that synchronizes compute operations within a given scope.
  *
  * @augments Node
  */
@@ -54,7 +54,9 @@ export default BarrierNode;
 const barrier = nodeProxy( BarrierNode );
 
 /**
- * TSL function for creating a workgroup barrier.
+ * TSL function for creating a workgroup barrier. All compute shader
+ * invocations must wait for each invocation within a workgroup to
+ * complete before the barrier can be surpassed.
  *
  * @function
  * @returns {BarrierNode}
@@ -62,7 +64,9 @@ const barrier = nodeProxy( BarrierNode );
 export const workgroupBarrier = () => barrier( 'workgroup' ).append();
 
 /**
- * TSL function for creating a storage barrier.
+ * TSL function for creating a storage barrier. All invocations must
+ * wait for each access to variables within the 'storage' address space
+ * to complete before the barrier can be passed.
  *
  * @function
  * @returns {BarrierNode}
@@ -70,7 +74,9 @@ export const workgroupBarrier = () => barrier( 'workgroup' ).append();
 export const storageBarrier = () => barrier( 'storage' ).append();
 
 /**
- * TSL function for creating a texture barrier.
+ * TSL function for creating a texture barrier. All invocations must
+ * wait for each access to variables within the 'texture' address space
+ * to complete before the barrier can be passed.
  *
  * @function
  * @returns {BarrierNode}
