7269de85a536b0157e7c47d9eaef6e2c95ad62cc
Renaud Rohlinger <renaud.rohlinger@gmail.com>
2024-12-18 15:02:46 +0900

    revert WebGL3DRenderTarget.js

--- src/renderers/WebGL3DRenderTarget.js
+++ src/renderers/WebGL3DRenderTarget.js
@@ -1,12 +1,19 @@
-import { RenderTarget3D } from '../core/RenderTarget3D.js';
+import { WebGLRenderTarget } from './WebGLRenderTarget.js';
+import { Data3DTexture } from '../textures/Data3DTexture.js';
 
-class WebGL3DRenderTarget extends RenderTarget3D {
+class WebGL3DRenderTarget extends WebGLRenderTarget {
 
 	constructor( width = 1, height = 1, depth = 1, options = {} ) {
 
-		super( width, height, depth, options );
+		super( width, height, options );
 
 		this.isWebGL3DRenderTarget = true;
+
+		this.depth = depth;
+
+		this.texture = new Data3DTexture( null, width, height, depth );
+
+		this.texture.isRenderTargetTexture = true;
 
 	}
 
