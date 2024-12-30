7a9c34000a348e1028a25cdbfa1826053f19695a
ycw <ycw7788@gmail.com>
2024-12-31 06:14:23 +0800

    -> 2025

--- utils/build/rollup.config.js
+++ utils/build/rollup.config.js
@@ -45,7 +45,7 @@ function header() {
 
 			code.prepend( `/**
  * @license
- * Copyright 2010-2024 Three.js Authors
+ * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
  */\n` );
 
