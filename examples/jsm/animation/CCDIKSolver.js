549ee80b999164f28560561a20edd806b0b085ea
satelllte <20585619+satelllte@users.noreply.github.com>
2025-01-06 00:46:30 +0200

    CCDIKSolver: fix algo broken ref (#30266)

--- examples/jsm/animation/CCDIKSolver.js
+++ examples/jsm/animation/CCDIKSolver.js
@@ -28,7 +28,7 @@ const _matrix = new Matrix4();
 
 /**
  * CCD Algorithm
- *  - https://sites.google.com/site/auraliusproject/ccd-algorithm
+ *  - https://web.archive.org/web/20221206080850/https://sites.google.com/site/auraliusproject/ccd-algorithm
  *
  * // ik parameter example
  * //
