48df2ee36336710364d7e268c34b0f03be3df16f
Michael Herzog <michael.herzog@human-interactive.org>
2025-01-08 12:48:32 +0100

    Introduce `VideoFrameTexture`. (#30270)
    * VideoTexture: Introduce `setFrame()`.
    
    * Clean up.
    
    * Examples: More clean up.
    
    * Lib: Add source of demuxer_mp4.js.
    
    * Update VideoTexture.html
    
    * Introduce `VideoFrameTexture`.
    
    * VideoTexture: Clean up.
    
    * VideoFrameTexture: More clean up.
    
    * VideoTexture: Clean up.
    
    * VideoFrameTexture: Improve comment.

---
+++ src/textures/VideoFrameTexture.js
@@ -0,0 +1,33 @@
+import { VideoTexture } from './VideoTexture.js';
+
+class VideoFrameTexture extends VideoTexture {
+
+	constructor( mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy ) {
+
+		super( {}, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy );
+
+	}
+
+	update() {
+
+		// overwrites `VideoTexture.update()` with an empty method since
+		// this type of texture is updated via `setFrame()`.
+
+	}
+
+	clone() {
+
+		return new this.constructor().copy( this ); // restoring Texture.clone()
+
+	}
+
+	setFrame( frame ) {
+
+		this.image = frame;
+		this.needsUpdate = true;
+
+	}
+
+}
+
+export { VideoFrameTexture };
