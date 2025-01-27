6bb6bf35fbe8b9e9d7b502398d6d6eac28326f58
Mugen87 <michael.herzog@human-interactive.org>
2025-01-27 14:10:28 +0100

    GLSLDecoder: Fix CodeQL feedback.

--- examples/jsm/transpiler/GLSLDecoder.js
+++ examples/jsm/transpiler/GLSLDecoder.js
@@ -461,7 +461,7 @@ class GLSLDecoder {
 			const isHex = /^(0x)/.test( firstToken.str );
 
 			if ( isHex ) type = 'int';
-			else if ( /u|U$/.test( firstToken.str ) ) type = 'uint';
+			else if ( /u$|U$/.test( firstToken.str ) ) type = 'uint';
 			else if ( /f|e|\./.test( firstToken.str ) ) type = 'float';
 			else type = 'int';
 
