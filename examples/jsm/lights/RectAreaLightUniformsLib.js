c80d2966640dbf481a5bb9ae64d6c97e77d26109
Mugen87 <michael.herzog@human-interactive.org>
2025-03-04 17:35:11 +0100

    Docs: More JSDoc.

--- examples/jsm/lights/RectAreaLightUniformsLib.js
+++ examples/jsm/lights/RectAreaLightUniformsLib.js
@@ -1,8 +1,23 @@
 import { UniformsLib } from 'three';
 import { RectAreaLightTexturesLib } from './RectAreaLightTexturesLib.js';
 
+/**
+ * This class is only relevant when using {@link RectAreaLight} with {@link WebGLRenderer}.
+ *
+ * Before rect area lights can be used, the internal uniform library of the renderer must be
+ * enhanced with the following code.
+ *
+ * ```js
+ * RectAreaLightUniformsLib.init();
+ * ```
+ *
+ * @hideconstructor
+ */
 class RectAreaLightUniformsLib {
 
+	/**
+	 * Inits the uniform library required when using rect area lights.
+	 */
 	static init() {
 
 		RectAreaLightTexturesLib.init();
