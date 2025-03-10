2af0580c6d66c0168a67c942928ea0f15569aa8b
Mugen87 <michael.herzog@human-interactive.org>
2025-03-10 13:43:37 +0100

    Docs: More JSDoc.

--- examples/jsm/misc/Timer.js
+++ examples/jsm/misc/Timer.js
@@ -1,5 +1,22 @@
+/**
+ * This class is an alternative to {@link Clock} with a different API design and behavior.
+ * The goal is to avoid the conceptual flaws that became apparent in `Clock` over time.
+ *
+ * - `Timer` has an `update()` method that updates its internal state. That makes it possible to
+ * call `getDelta()` and `getElapsed()` multiple times per simulation step without getting different values.
+ * - The class can make use of the Page Visibility API to avoid large time delta values when the app
+ * is inactive (e.g. tab switched or browser hidden).
+ *
+ * ```js
+ * const timer = new Timer();
+ * timer.connect( document ); // use Page Visibility API
+ * ```
+ */
 class Timer {
 
+	/**
+	 * Constructs a new timer.
+	 */
 	constructor() {
 
 		this._previousTime = 0;
@@ -16,6 +33,13 @@ class Timer {
 
 	}
 
+	/**
+	 * Connect the timer to the given document.Calling this method is not mandatory to
+	 * use the timer but enables the usage of the Page Visibility API to avoid large time
+	 * delta values.
+	 *
+	 * @param {Document} document - The document.
+	 */
 	connect( document ) {
 
 		this._document = document;
@@ -32,6 +56,9 @@ class Timer {
 
 	}
 
+	/**
+	 * Disconnects the timer from the DOM and also disables the usage of the Page Visibility API.
+	 */
 	disconnect() {
 
 		if ( this._pageVisibilityHandler !== null ) {
@@ -45,24 +72,46 @@ class Timer {
 
 	}
 
+	/**
+	 * Returns the time delta in seconds.
+	 *
+	 * @return {number} The time delta in second.
+	 */
 	getDelta() {
 
 		return this._delta / 1000;
 
 	}
 
+	/**
+	 * Returns the elapsed time in seconds.
+	 *
+	 * @return {number} The elapsed time in second.
+	 */
 	getElapsed() {
 
 		return this._elapsed / 1000;
 
 	}
 
+	/**
+	 * Returns the timescale.
+	 *
+	 * @return {number} The timescale.
+	 */
 	getTimescale() {
 
 		return this._timescale;
 
 	}
 
+	/**
+	 * Sets the given timescale which scale the time delta computation
+	 * in `update()`.
+	 *
+	 * @param {number} timescale - The timescale to set.
+	 * @return {Timer} A reference to this timer.
+	 */
 	setTimescale( timescale ) {
 
 		this._timescale = timescale;
@@ -71,6 +120,11 @@ class Timer {
 
 	}
 
+	/**
+	 * Resets the time computation for the current simulation step.
+	 *
+	 * @return {Timer} A reference to this timer.
+	 */
 	reset() {
 
 		this._currentTime = now() - this._startTime;
@@ -79,14 +133,26 @@ class Timer {
 
 	}
 
+	/**
+	 * Can be used to free all internal resources. Usually called when
+	 * the timer instance isn't required anymore.
+	 */
 	dispose() {
 
 		this.disconnect();
 
-		return this;
-
 	}
 
+	/**
+	 * Updates the internal state of the timer. This method should be called
+	 * once per simulation step and before you perform queries against the timer
+	 * (e.g. via `getDelta()`).
+	 *
+	 * @param {number} timestamp - The current time in milliseconds. Can be obtained
+	 * from the `requestAnimationFrame` callback argument. If not provided, the current
+	 * time will be determined with `performance.now`.
+	 * @return {Timer} A reference to this timer.
+	 */
 	update( timestamp ) {
 
 		if ( this._pageVisibilityHandler !== null && this._document.hidden === true ) {
@@ -109,8 +175,19 @@ class Timer {
 
 }
 
+/**
+ * A special version of a timer with a fixed time delta value.
+ * Can be useful for testing and debugging purposes.
+ *
+ * @augments Timer
+ */
 class FixedTimer extends Timer {
 
+	/**
+	 * Constructs a new timer.
+	 *
+	 * @param {number} [fps=60] - The fixed FPS of this timer.
+	 */
 	constructor( fps = 60 ) {
 
 		super();
