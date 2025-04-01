e74e61a7e27681541438c5e9e4b0a7ca192359da
Renaud Rohlinger <renaud.rohlinger@gmail.com>
2025-04-01 17:37:41 +0900

    fix formatting issues

--- src/math/FrustumArray.js
+++ src/math/FrustumArray.js
@@ -12,29 +12,29 @@ const _frustum = /*@__PURE__*/ new Frustum();
 class FrustumArray {
 
 	/**
-     * Constructs a new frustum array.
-     *
+	 * Constructs a new frustum array.
+	 *
     */
 	constructor() {
 
 		/**
-         * The coordinate system to use.
-         *
-         * @type {WebGLCoordinateSystem|WebGPUCoordinateSystem}
-         * @default WebGLCoordinateSystem
-        */
+		 * The coordinate system to use.
+		 *
+		 * @type {WebGLCoordinateSystem|WebGPUCoordinateSystem}
+		 * @default WebGLCoordinateSystem
+		*/
 		this.coordinateSystem = WebGLCoordinateSystem;
 
 	}
 
 	/**
-     * Returns `true` if the 3D object's bounding sphere is intersecting any frustum
-     * from the camera array.
-     *
-     * @param {Object3D} object - The 3D object to test.
-     * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
-     * @return {boolean} Whether the 3D object is visible in any camera.
-    */
+	 * Returns `true` if the 3D object's bounding sphere is intersecting any frustum
+	 * from the camera array.
+	 *
+	 * @param {Object3D} object - The 3D object to test.
+	 * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
+	 * @return {boolean} Whether the 3D object is visible in any camera.
+	*/
 	intersectsObject( object, cameraArray ) {
 
 		if ( ! cameraArray.isArrayCamera || cameraArray.cameras.length === 0 ) {
@@ -70,13 +70,13 @@ class FrustumArray {
 	}
 
 	/**
-     * Returns `true` if the given sprite is intersecting any frustum
-     * from the camera array.
-     *
-     * @param {Sprite} sprite - The sprite to test.
-     * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
-     * @return {boolean} Whether the sprite is visible in any camera.
-    */
+	 * Returns `true` if the given sprite is intersecting any frustum
+	 * from the camera array.
+	 *
+	 * @param {Sprite} sprite - The sprite to test.
+	 * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
+	 * @return {boolean} Whether the sprite is visible in any camera.
+	*/
 	intersectsSprite( sprite, cameraArray ) {
 
 		if ( ! cameraArray || ! cameraArray.cameras || cameraArray.cameras.length === 0 ) {
@@ -112,13 +112,13 @@ class FrustumArray {
 	}
 
 	/**
-     * Returns `true` if the given bounding sphere is intersecting any frustum
-     * from the camera array.
-     *
-     * @param {Sphere} sphere - The bounding sphere to test.
-     * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
-     * @return {boolean} Whether the sphere is visible in any camera.
-    */
+	 * Returns `true` if the given bounding sphere is intersecting any frustum
+	 * from the camera array.
+	 *
+	 * @param {Sphere} sphere - The bounding sphere to test.
+	 * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
+	 * @return {boolean} Whether the sphere is visible in any camera.
+	*/
 	intersectsSphere( sphere, cameraArray ) {
 
 		if ( ! cameraArray || ! cameraArray.cameras || cameraArray.cameras.length === 0 ) {
@@ -154,13 +154,13 @@ class FrustumArray {
 	}
 
 	/**
-     * Returns `true` if the given bounding box is intersecting any frustum
-     * from the camera array.
-     *
-     * @param {Box3} box - The bounding box to test.
-     * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
-     * @return {boolean} Whether the box is visible in any camera.
-    */
+	 * Returns `true` if the given bounding box is intersecting any frustum
+	 * from the camera array.
+	 *
+	 * @param {Box3} box - The bounding box to test.
+	 * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
+	 * @return {boolean} Whether the box is visible in any camera.
+	*/
 	intersectsBox( box, cameraArray ) {
 
 		if ( ! cameraArray || ! cameraArray.cameras || cameraArray.cameras.length === 0 ) {
@@ -196,13 +196,13 @@ class FrustumArray {
 	}
 
 	/**
-     * Returns `true` if the given point lies within any frustum
-     * from the camera array.
-     *
-     * @param {Vector3} point - The point to test.
-     * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
-     * @return {boolean} Whether the point is visible in any camera.
-    */
+	 * Returns `true` if the given point lies within any frustum
+	 * from the camera array.
+	 *
+	 * @param {Vector3} point - The point to test.
+	 * @param {Object} cameraArray - An object with a cameras property containing an array of cameras.
+	 * @return {boolean} Whether the point is visible in any camera.
+	*/
 	containsPoint( point, cameraArray ) {
 
 		if ( ! cameraArray || ! cameraArray.cameras || cameraArray.cameras.length === 0 ) {
@@ -238,10 +238,10 @@ class FrustumArray {
 	}
 
 	/**
-     * Returns a new frustum array with copied values from this instance.
-     *
-     * @return {FrustumArray} A clone of this instance.
-    */
+	 * Returns a new frustum array with copied values from this instance.
+	 *
+	 * @return {FrustumArray} A clone of this instance.
+	*/
 	clone() {
 
 		return new FrustumArray();
