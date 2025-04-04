4d3f0deaf010e31724990bb64e3840ecbcff22e0
Renaud Rohlinger <renaud.rohlinger@gmail.com>
2025-04-04 22:06:44 +0900

    TileShadowNode: Fix shadow manual update

--- examples/jsm/tsl/shadows/TileShadowNode.js
+++ examples/jsm/tsl/shadows/TileShadowNode.js
@@ -311,7 +311,7 @@ class TileShadowNode extends ShadowBaseNode {
 	 */
 	updateBefore( frame ) {
 
-		const shadow = this.lights[ 0 ].shadow; // so far test only the first light
+		const shadow = this.originalLight.shadow;
 
 		let needsUpdate = shadow.needsUpdate || shadow.autoUpdate;
 
