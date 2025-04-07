7ea0c29bf3eb0ce8af016c9331cfaddaa15e956e
Mr.doob <info@mrdoob.com>
2025-04-07 16:43:15 +0900

    Okay Copilot.

--- devtools/bridge.js
+++ devtools/bridge.js
@@ -183,11 +183,11 @@ if (!window.__THREE_DEVTOOLS__) {
 
 	// Generate a UUID for objects that don't have one
 	function generateUUID() {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			const r = Math.random() * 16 | 0;
-			const v = c === 'x' ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
-		});
+		const array = new Uint8Array(16);
+		crypto.getRandomValues(array);
+		array[6] = (array[6] & 0x0f) | 0x40; // Set version to 4
+		array[8] = (array[8] & 0x3f) | 0x80; // Set variant to 10
+		return [...array].map((b, i) => (i === 4 || i === 6 || i === 8 || i === 10 ? '-' : '') + b.toString(16).padStart(2, '0')).join('');
 	}
 
 	// Listen for Three.js registration
