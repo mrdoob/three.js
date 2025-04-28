5ab06a3a6eab7e0cd60eff59ae1ac8bbe073e0d3
sunag <sunagbrasil@gmail.com>
2025-04-28 00:30:42 -0300

    SpotLightNode: Add custom attenuation using `spotLight.attenuationNode` (#31013)
    * add custom attenuation
    
    * cleanup

--- src/nodes/lighting/SpotLightNode.js
+++ src/nodes/lighting/SpotLightNode.js
@@ -91,6 +91,23 @@ class SpotLightNode extends AnalyticLightNode {
 
 	}
 
+	getSpotLightCoord( builder ) {
+
+		const properties = builder.getNodeProperties( this );
+		let projectionUV = properties.projectionUV;
+
+		if ( projectionUV === undefined ) {
+
+			projectionUV = lightProjectionUV( this.light, builder.context.positionWorld );
+
+			properties.projectionUV = projectionUV;
+
+		}
+
+		return projectionUV;
+
+	}
+
 	setupDirect( builder ) {
 
 		const { colorNode, cutoffDistanceNode, decayExponentNode, light } = this;
@@ -99,7 +116,8 @@ class SpotLightNode extends AnalyticLightNode {
 
 		const lightDirection = lightVector.normalize();
 		const angleCos = lightDirection.dot( lightTargetDirection( light ) );
-		const spotAttenuation = this.getSpotAttenuation( angleCos );
+
+		const spotAttenuation = light.attenuationNode ? light.attenuationNode( this ) : this.getSpotAttenuation( angleCos );
 
 		const lightDistance = lightVector.length();
 
@@ -113,7 +131,7 @@ class SpotLightNode extends AnalyticLightNode {
 
 		if ( light.map ) {
 
-			const spotLightCoord = lightProjectionUV( light, builder.context.positionWorld );
+			const spotLightCoord = this.getSpotLightCoord( builder );
 			const projectedTexture = texture( light.map, spotLightCoord.xy ).onRenderUpdate( () => light.map );
 
 			const inSpotLightMap = spotLightCoord.mul( 2. ).sub( 1. ).abs().lessThan( 1. ).all();
