bf2558f98e2834a4b336e5ea7830a512fb699be4
Neil Rackett <neil.rackett@mesmotronic.com>
2025-05-05 00:19:24 +0100

    Added FXAAPass class

---
+++ examples/jsm/postprocessing/FXAAPass.js
@@ -0,0 +1,37 @@
+import { FXAAShader } from '../shaders/FXAAShader.js';
+import { ShaderPass } from './ShaderPass.js';
+
+/**
+ * A pass for applying FXAA.
+ *
+ * ```js
+ * const fxaaPass = new FXAAPass();
+ * composer.addPass( smaaPass );
+ * ```
+ *
+ * @augments ShaderPass
+ * @three_import import { FXAAPass } from 'three/addons/postprocessing/FXAAPass.js';
+ */
+class FXAAPass extends ShaderPass {
+
+  constructor() {
+
+    super( FXAAShader );
+
+  }
+
+	/**
+	 * Sets the size of the pass.
+	 *
+	 * @param {number} width - The width to set.
+	 * @param {number} height - The width to set.
+	 */
+  setSize(width, height) {
+
+    this.material.uniforms[ 'resolution' ].value.set( 1 / width, 1 / height );
+
+  }
+
+}
+
+export { FXAAPass };
