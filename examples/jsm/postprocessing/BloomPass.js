a8536871e792cc9a8329634d4345ea83a07bc235
Michael Herzog <michael.herzog@human-interactive.org>
2025-05-05 09:38:24 +0200

    Docs: Fix typo. (#31046)

--- examples/jsm/postprocessing/BloomPass.js
+++ examples/jsm/postprocessing/BloomPass.js
@@ -167,7 +167,7 @@ class BloomPass extends Pass {
 	 * Sets the size of the pass.
 	 *
 	 * @param {number} width - The width to set.
-	 * @param {number} height - The width to set.
+	 * @param {number} height - The height to set.
 	 */
 	setSize( width, height ) {
 
--- examples/jsm/postprocessing/RenderPixelatedPass.js
+++ examples/jsm/postprocessing/RenderPixelatedPass.js
@@ -121,7 +121,7 @@ class RenderPixelatedPass extends Pass {
 	 * Sets the size of the pass.
 	 *
 	 * @param {number} width - The width to set.
-	 * @param {number} height - The width to set.
+	 * @param {number} height - The height to set.
 	 */
 	setSize( width, height ) {
 
