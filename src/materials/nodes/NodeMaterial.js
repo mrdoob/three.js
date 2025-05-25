813cc260013828b57e52aaa121497b54037edfe5
WestLangley <WestLangley@users.noreply.github.com>
2025-05-25 15:46:50 -0400

    Honor material.premultipliedAlpha

--- src/materials/nodes/NodeMaterial.js
+++ src/materials/nodes/NodeMaterial.js
@@ -1076,6 +1076,21 @@ class NodeMaterial extends Material {
 	}
 
 	/**
+	 * Setups premultiplied alpha.
+	 *
+	 * @param {NodeBuilder} builder - The current node builder.
+	 * @param {Node<vec4>} outputNode - The existing output node.
+	 * @return {Node<vec4>} The output node.
+	 */
+	setupPremultipliedAlpha( builder, outputNode ) {
+
+		outputNode = vec4( outputNode.rgb.mul( outputNode.a ), outputNode.a );
+
+		return outputNode;
+
+	}
+
+	/**
 	 * Setups the output node.
 	 *
 	 * @param {NodeBuilder} builder - The current node builder.
@@ -1089,6 +1104,14 @@ class NodeMaterial extends Material {
 		if ( this.fog === true ) {
 
 			outputNode = this.setupFog( builder, outputNode );
+
+		}
+
+		// PREMULTIPLIED ALPHA
+
+		if ( this.premultipliedAlpha === true ) {
+
+			outputNode = this.setupPremultipliedAlpha( builder, outputNode );
 
 		}
 
