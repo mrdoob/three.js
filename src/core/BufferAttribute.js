4ab673019374af5ddd96cc8aeffcd9cc9a418454
Christian Winter <Christian.Winter81@me.com>
2025-06-08 13:04:39 +0200

    Add property `normalized` to `GLBufferAttribute`
    Update documentation and unit test.
    
    Update doc

--- src/core/BufferAttribute.js
+++ src/core/BufferAttribute.js
@@ -88,7 +88,7 @@ class BufferAttribute {
 		/**
 		 * Applies to integer data only. Indicates how the underlying data in the buffer maps to
 		 * the values in the GLSL code. For instance, if `array` is an instance of `UInt16Array`,
-		 * and `normalized` is `true`, the values `0 -+65535` in the array data will be mapped to
+		 * and `normalized` is `true`, the values `0 - +65535` in the array data will be mapped to
 		 * `0.0f - +1.0f` in the GLSL attribute. If `normalized` is `false`, the values will be converted
 		 * to floats unmodified, i.e. `65535` becomes `65535.0f`.
 		 *
