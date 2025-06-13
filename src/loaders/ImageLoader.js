a28e40b9602a4d14999d7c4ad3fcce20000657ae
Mugen87 <michael.herzog@human-interactive.org>
2025-06-13 10:51:04 +0200

    ImageLoader: Fix timing of cached callbacks.

--- src/loaders/ImageLoader.js
+++ src/loaders/ImageLoader.js
@@ -2,6 +2,8 @@ import { Cache } from './Cache.js';
 import { Loader } from './Loader.js';
 import { createElementNS } from '../utils.js';
 
+const _loading = new WeakMap();
+
 /**
  * A loader for loading images. The class loads images with the HTML `Image` API.
  *
@@ -52,15 +54,32 @@ class ImageLoader extends Loader {
 
 		if ( cached !== undefined ) {
 
-			scope.manager.itemStart( url );
+			if ( cached.complete === true ) {
 
-			setTimeout( function () {
+				scope.manager.itemStart( url );
 
-				if ( onLoad ) onLoad( cached );
+				setTimeout( function () {
 
-				scope.manager.itemEnd( url );
+					if ( onLoad ) onLoad( cached );
 
-			}, 0 );
+					scope.manager.itemEnd( url );
+
+				}, 0 );
+
+			} else {
+
+				let arr = _loading.get( cached );
+
+				if ( arr === undefined ) {
+
+					arr = [];
+					_loading.set( cached, arr );
+
+				}
+
+				arr.push( { onLoad, onError } );
+
+			}
 
 			return cached;
 
@@ -76,6 +95,19 @@ class ImageLoader extends Loader {
 
 			scope.manager.itemEnd( url );
 
+			//
+
+			const callbacks = _loading.get( this ) || [];
+
+			for ( let i = 0; i < callbacks.length; i ++ ) {
+
+				const callback = callbacks[ i ];
+				if ( callback.onLoad ) callback.onLoad( this );
+
+			}
+
+			_loading.delete( this );
+
 		}
 
 		function onImageError( event ) {
@@ -88,6 +120,19 @@ class ImageLoader extends Loader {
 
 			scope.manager.itemError( url );
 			scope.manager.itemEnd( url );
+
+			//
+
+			const callbacks = _loading.get( this ) || [];
+
+			for ( let i = 0; i < callbacks.length; i ++ ) {
+
+				const callback = callbacks[ i ];
+				if ( callback.onError ) callback.onError( this );
+
+			}
+
+			_loading.delete( this );
 
 		}
 
