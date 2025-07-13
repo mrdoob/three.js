7d3562fc04ee6fc501e9924ff3f48839d2122a97
WestLangley <WestLangley@users.noreply.github.com>
2025-07-12 22:04:16 -0400

    fix test

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -2382,8 +2382,7 @@ class WebGLRenderer {
 
 				const reverseDepthBuffer = state.buffers.depth.getReversed();
 
-				if ( reverseDepthBuffer && camera.reversedDepth === false ) {
-
+				if ( reverseDepthBuffer && camera.reversedDepth !== true ) {
 					// @deprecated, r179
 					warnOnce( 'THREE.WebGLRenderer: reverseDepthBuffer must be used with "camera.reversedDepth = true;" for correct results. Automatic conversion will be removed in r189.' );
 
