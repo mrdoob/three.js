8df553ef44bfec7c8e47a6ab25b7f4943c689aa9
Michael Herzog <michael.herzog@human-interactive.org>
2025-07-15 23:19:23 +0200

    WGSLNodeBuilder: Remove obsolete overwrite. (#31423)
    * WGSLNodeBuilder: Remove obsolete overwrite.
    
    * Clean up.

--- src/renderers/webgpu/nodes/WGSLNodeBuilder.js
+++ src/renderers/webgpu/nodes/WGSLNodeBuilder.js
@@ -16,7 +16,7 @@ import { NodeAccess } from '../../../nodes/core/constants.js';
 import VarNode from '../../../nodes/core/VarNode.js';
 import ExpressionNode from '../../../nodes/code/ExpressionNode.js';
 
-import { NoColorSpace, FloatType, RepeatWrapping, ClampToEdgeWrapping, MirroredRepeatWrapping, NearestFilter, SRGBColorSpace } from '../../../constants.js';
+import { FloatType, RepeatWrapping, ClampToEdgeWrapping, MirroredRepeatWrapping, NearestFilter } from '../../../constants.js';
 
 // GPUShaderStage is not defined in browsers not supporting WebGPU
 const GPUShaderStage = ( typeof self !== 'undefined' ) ? self.GPUShaderStage : { VERTEX: 1, FRAGMENT: 2, COMPUTE: 4 };
@@ -208,25 +208,6 @@ class WGSLNodeBuilder extends NodeBuilder {
 		 * @type {Map<string,Object>}
 		 */
 		this.scopedArrays = new Map();
-
-	}
-
-	/**
-	 * Checks if the given texture requires a manual conversion to the working color space.
-	 *
-	 * @param {Texture} texture - The texture to check.
-	 * @return {boolean} Whether the given texture requires a conversion to working color space or not.
-	 */
-	needsToWorkingColorSpace( texture ) {
-
-		if ( texture.isVideoTexture && texture.colorSpace === SRGBColorSpace ) {
-
-			// Video textures are always in sRGB color space, so no conversion is needed
-			return false;
-
-		}
-
-		return texture.colorSpace !== NoColorSpace;
 
 	}
 
