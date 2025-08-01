8805ca80a0761db52dfd1ee0aad36f6798800d28
simonfh <simonfh@noreply.github.com>
2025-08-01 13:47:38 +0200

    Examples: Use epsilon for zoomChanged comparison in OrbitControls

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -683,7 +683,7 @@ class OrbitControls extends Controls {
 
 			const prevRadius = this._spherical.radius;
 			this._spherical.radius = this._clampDistance( this._spherical.radius * this._scale );
-			zoomChanged = prevRadius != this._spherical.radius;
+			zoomChanged = Math.abs(prevRadius - this._spherical.radius) > _EPS;
 
 		}
 
