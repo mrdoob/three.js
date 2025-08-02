3dd226d4a272c195611046b98ec8a97ff2e4db8c
NoxDawnsong <106304148+NoxDawnsong@users.noreply.github.com>
2025-08-02 14:06:05 +0200

    WebGPURenderer: Fix MSAA using mipmap levels (#31544)
    * WebGPU: Fix MSAA using mipmap levels
    
    * Change comment to link to official specification

--- src/renderers/webgpu/utils/WebGPUTextureUtils.js
+++ src/renderers/webgpu/utils/WebGPUTextureUtils.js
@@ -266,6 +266,7 @@ class WebGPUTextureUtils {
 
 			msaaTextureDescriptorGPU.label = msaaTextureDescriptorGPU.label + '-msaa';
 			msaaTextureDescriptorGPU.sampleCount = samples;
+			msaaTextureDescriptorGPU.mipLevelCount = 1; // See https://www.w3.org/TR/webgpu/#texture-creation
 
 			textureData.msaaTexture = backend.device.createTexture( msaaTextureDescriptorGPU );
 
