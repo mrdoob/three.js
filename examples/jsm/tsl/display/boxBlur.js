a6a15692d642b64b67a4194d144d4df84901f67e
Michael Herzog <michael.herzog@human-interactive.org>
2025-08-02 15:10:26 +0200

    Update boxBlur.js
    Fix typos.

--- examples/jsm/tsl/display/boxBlur.js
+++ examples/jsm/tsl/display/boxBlur.js
@@ -4,10 +4,10 @@ import { Fn, vec2, uv, Loop, vec4, premultiplyAlpha, unpremultiplyAlpha, max, in
  * Applies a box blur effect to the given texture node.
  *
  * Compared to Gaussian blur, box blur produces a more blocky result but with better performance when correctly
- * configured. It is intended for mobile device or performance restricted use cases where Gaussian is too heavy.
+ * configured. It is intended for mobile devices or performance restricted use cases where Gaussian is too heavy.
  *
  * The (kernel) `size` parameter should be small (1, 2 or 3) since it determines the number of samples based on (size * 2 + 1)^2.
- * This implementation uses a single pass approach so the kernel is not applied as a separate filter. That means larger
+ * This implementation uses a single pass approach so the kernel is not applied as a seprabable filter. That means larger
  * kernels won't perform well. Use Gaussian instead if you need a more high-quality blur.
  *
  * To produce wider blurs, increase the `separation` parameter instead which has no influence on the performance.
