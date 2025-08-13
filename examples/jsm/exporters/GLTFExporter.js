23c3f4896de0e8a142ea0d4a1024651b085ca38c
Palash Bansal <palash14072@iiitd.ac.in>
2025-08-13 21:51:27 +0530

    Add userData to AnimationClip and extras to glTF animations (#31586)
    * Add userData to AnimationClip and extras to gltf animations.
    
    * Add jsdocs for AnimationClip userData

--- examples/jsm/exporters/GLTFExporter.js
+++ examples/jsm/exporters/GLTFExporter.js
@@ -761,7 +761,7 @@ class GLTFWriter {
 	/**
 	 * Serializes a userData.
 	 *
-	 * @param {THREE.Object3D|THREE.Material} object
+	 * @param {THREE.Object3D|THREE.Material|THREE.BufferGeometry|THREE.AnimationClip} object
 	 * @param {Object} objectDef
 	 */
 	serializeUserData( object, objectDef ) {
@@ -2283,11 +2283,15 @@ class GLTFWriter {
 
 		}
 
-		json.animations.push( {
+		const animationDef = {
 			name: clip.name || 'clip_' + json.animations.length,
 			samplers: samplers,
 			channels: channels
-		} );
+		};
+
+		this.serializeUserData( clip, animationDef );
+
+		json.animations.push( animationDef );
 
 		return json.animations.length - 1;
 
