3b4ef52207d34fa1156853d2d09ff1725925d712
harryhjsh <39673043+harryhjsh@users.noreply.github.com>
2025-09-06 11:23:49 +0100

    XRManager: Reset XRWebGLBinding on session end (#31835)
    * XRManager: Reset XRWebGLBinding on session end
    
    * also reset glBaseLayer and glProjLayer between sessions

--- src/renderers/common/XRManager.js
+++ src/renderers/common/XRManager.js
@@ -1381,6 +1381,9 @@ function onSessionEnd() {
 
 	this._session = null;
 	this._xrRenderTarget = null;
+	this._glBinding = null;
+	this._glBaseLayer = null;
+	this._glProjLayer = null;
 
 	// switch layers back to emulated
 	if ( this._sessionUsesLayers === true ) {
