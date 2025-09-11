a26553e9353bb69cb92a98accaeb55b5bf22149a
Johnathon Selstad <makeshifted@gmail.com>
2025-09-11 14:06:50 -0700

    Update SSGINode.js

--- examples/jsm/tsl/display/SSGINode.js
+++ examples/jsm/tsl/display/SSGINode.js
@@ -576,7 +576,7 @@ class SSGINode extends TempNode {
 
 			const noiseOffset = spatialOffsets( screenCoordinate );
 			const noiseDirection = gradientNoise( screenCoordinate );
-			const initialRayStep = fract( noiseOffset.add( this._temporalOffset ) ).add( rand( uvNode.add( this._temporalDirection )  ).mul( 2 ).sub( 1 ) );
+			const initialRayStep = fract( noiseOffset.add( this._temporalOffset ) ).add( rand( uvNode.add( this._temporalDirection )  ).mul( 2 ).sub( 1 ) ); // Port: Add _temporalDirection here for slightly better noise convergence with TRAA
 
 			const ao = float( 0 );
 			const color = vec3( 0 );
