e0750209885b3e52dc8a9f11d38e5913de39fc80
sunag <sunagbrasil@gmail.com>
2025-09-29 01:58:33 -0300

    Update VarNode.js

--- src/nodes/core/VarNode.js
+++ src/nodes/core/VarNode.js
@@ -154,7 +154,7 @@ class VarNode extends Node {
 
 		if ( assign !== true ) {
 
-			if ( this.node.isShaderCallNodeInternal && this.node.shaderNode.getLayout() === null ) {
+			/*if ( this.node.isShaderCallNodeInternal && this.node.shaderNode.getLayout() === null ) {
 
 				if ( builder.fnCall && builder.fnCall.shaderNode ) {
 
@@ -174,7 +174,7 @@ class VarNode extends Node {
 
 				}
 
-			}
+			}*/
 
 		}
 
