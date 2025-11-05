b3486b2d6381a9e73f98cdab58821bed8230a429
Michael Herzog <michael.herzog@human-interactive.org>
2025-11-05 13:59:23 +0100

    Docs: Fix JSDoc of inspector. (#32192)

--- src/renderers/common/Renderer.js
+++ src/renderers/common/Renderer.js
@@ -996,11 +996,6 @@ class Renderer {
 
 	//
 
-	/**
-	 * Sets the inspector instance. The inspector can be any class that extends from `InspectorBase`.
-	 *
-	 * @param {InspectorBase} value - The new inspector.
-	 */
 	set inspector( value ) {
 
 		if ( this._inspector !== null ) {
@@ -1014,6 +1009,11 @@ class Renderer {
 
 	}
 
+	/**
+	 * The inspector instance. The inspector can be any class that extends from `InspectorBase`.
+	 *
+	 * @type {InspectorBase}
+	 */
 	get inspector() {
 
 		return this._inspector;
