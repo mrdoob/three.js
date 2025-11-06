fde4b93d42a21cd1a194e8d84850ebef5bae3541
sunag <sunagbrasil@gmail.com>
2025-11-06 16:50:55 -0300

    Inspector: Improve UX (#32197)
    * improve ux
    
    * Tab: allowDetach
    
    * improve no-tabs ux
    
    * update z-index when resized
    
    * cleanup
    
    * restore original indexes

--- examples/jsm/inspector/ui/Tab.js
+++ examples/jsm/inspector/ui/Tab.js
@@ -1,6 +1,19 @@
+/**
+ * Tab class
+ * @param {string} title - The title of the tab
+ * @param {Object} options - Options for the tab
+ * @param {boolean} [options.allowDetach=true] - Whether the tab can be detached into a separate window
+ *
+ * @example
+ * // Create a tab that can be detached (default behavior)
+ * const tab1 = new Tab('My Tab');
+ *
+ * // Create a tab that cannot be detached
+ * const tab2 = new Tab('Fixed Tab', { allowDetach: false });
+ */
 export class Tab {
 
-	constructor( title ) {
+	constructor( title, options = {} ) {
 
 		this.id = title.toLowerCase();
 		this.button = document.createElement( 'button' );
@@ -13,6 +26,10 @@ export class Tab {
 
 		this.isActive = false;
 		this.isVisible = true;
+		this.isDetached = false;
+		this.detachedWindow = null;
+		this.allowDetach = options.allowDetach !== undefined ? options.allowDetach : true;
+		this.onVisibilityChange = null; // Callback for visibility changes
 
 	}
 
@@ -32,6 +49,20 @@ export class Tab {
 
 		this.isVisible = true;
 
+		// Show detached window if tab is detached
+		if ( this.isDetached && this.detachedWindow ) {
+
+			this.detachedWindow.panel.style.display = '';
+
+		}
+
+		// Notify profiler of visibility change
+		if ( this.onVisibilityChange ) {
+
+			this.onVisibilityChange();
+
+		}
+
 	}
 
 	hide() {
@@ -40,6 +71,20 @@ export class Tab {
 		this.button.style.display = 'none';
 
 		this.isVisible = false;
+
+		// Hide detached window if tab is detached
+		if ( this.isDetached && this.detachedWindow ) {
+
+			this.detachedWindow.panel.style.display = 'none';
+
+		}
+
+		// Notify profiler of visibility change
+		if ( this.onVisibilityChange ) {
+
+			this.onVisibilityChange();
+
+		}
 
 	}
 
