4cae678befd483bb40b11e3f3414ce34cc8d7df9
Mr.doob <info@mrdoob.com>
2025-11-19 11:35:47 +0900

    WebGLRenderer: Fix iridescence energy conservation for mixed materials.

--- src/renderers/shaders/ShaderChunk/lights_fragment_begin.glsl.js
+++ src/renderers/shaders/ShaderChunk/lights_fragment_begin.glsl.js
@@ -41,7 +41,10 @@ vec3 geometryClearcoatNormal = vec3( 0.0 );
 
 	if ( material.iridescence > 0.0 ) {
 
-		material.iridescenceFresnel = evalIridescence( 1.0, material.iridescenceIOR, dotNVi, material.iridescenceThickness, material.specularColorBlended );
+		material.iridescenceFresnelDielectric = evalIridescence( 1.0, material.iridescenceIOR, dotNVi, material.iridescenceThickness, material.specularColor );
+		material.iridescenceFresnelMetallic = evalIridescence( 1.0, material.iridescenceIOR, dotNVi, material.iridescenceThickness, material.diffuseColor );
+
+		material.iridescenceFresnel = mix( material.iridescenceFresnelDielectric, material.iridescenceFresnelMetallic, material.metalness );
 
 		// Iridescence F0 approximation
 		material.iridescenceF0 = Schlick_to_F0( material.iridescenceFresnel, 1.0, dotNVi );
