0ad08817d5646878e2897c301bd1a8076a4ddf09
sunag <sunagbrasil@gmail.com>
2025-12-23 13:46:58 -0300

    Renderer: Rename `shadowMap.colored` -> `shadowMap.transmitted` (#32611)

--- src/renderers/common/Renderer.js
+++ src/renderers/common/Renderer.js
@@ -658,7 +658,7 @@ class Renderer {
 		 * Shadow map configuration
 		 * @typedef {Object} ShadowMapConfig
 		 * @property {boolean} enabled - Whether to globally enable shadows or not.
-		 * @property {boolean} colored - Whether shadows can have a custom color or not.
+		 * @property {boolean} transmitted - Whether to enable light transmission through non-opaque materials.
 		 * @property {number} type - The shadow map type.
 		 */
 
@@ -669,7 +669,7 @@ class Renderer {
 		 */
 		this.shadowMap = {
 			enabled: false,
-			colored: false,
+			transmitted: false,
 			type: PCFShadowMap
 		};
 
@@ -3053,9 +3053,9 @@ class Renderer {
 					shadowRGB = material.castShadowNode.rgb;
 					shadowAlpha = material.castShadowNode.a;
 
-					if ( this.shadowMap.colored !== true ) {
+					if ( this.shadowMap.transmitted !== true ) {
 
-						warnOnce( 'Renderer: `shadowMap.colored` needs to be set to `true` when using `material.castShadowNode`.' );
+						warnOnce( 'Renderer: `shadowMap.transmitted` needs to be set to `true` when using `material.castShadowNode`.' );
 
 					}
 
