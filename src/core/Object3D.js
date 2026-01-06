ea68bc85b3dc5532d5db5e58116f5aa709827d83
Mugen87 <michael.herzog@human-interactive.org>
2026-01-06 19:29:16 +0100

    Object3D: More docs for `static`.

--- src/core/Object3D.js
+++ src/core/Object3D.js
@@ -357,7 +357,8 @@ class Object3D extends EventDispatcher {
 		/**
 		 * Whether the 3D object is supposed to be static or not. If set to `true`, it means
 		 * the 3D object is not going to be changed after the initial renderer. This includes
-		 * geometry and material settings.
+		 * geometry and material settings. A static 3D object can be processed by the renderer
+		 * slightly faster since certain state checks can be bypassed.
 		 *
 		 * Only relevant in context of {@link WebGPURenderer}.
 		 *
