9f63475a0bb1c32e27fa7484c2f36dc51db187e4
Michael Herzog <michael.herzog@human-interactive.org>
2026-01-11 22:54:37 +0100

    Update SpotLightHelper.js

--- src/helpers/SpotLightHelper.js
+++ src/helpers/SpotLightHelper.js
@@ -10,8 +10,8 @@ const _vector = /*@__PURE__*/ new Vector3();
 /**
  * This displays a cone shaped helper object for a {@link SpotLight}.
  *
- * When the spot light is transformed or its light properties are changed,
- * it's necessary to call the `update()` method of the respective helper.
+ * When the spot light or its target are transformed or light properties are
+ * changed, it's necessary to call the `update()` method of the respective helper.
  *
  * ```js
  * const spotLight = new THREE.SpotLight( 0xffffff );
