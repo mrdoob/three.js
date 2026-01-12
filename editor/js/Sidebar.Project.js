ab8dd7442ffd938e258b3a1ac94301270255033d
Mr.doob <info@mrdoob.com>
2026-01-12 11:01:29 +0900

    Editor: Move render options from sidebar to menubar.

--- editor/js/Sidebar.Project.js
+++ editor/js/Sidebar.Project.js
@@ -3,8 +3,6 @@ import { UISpan } from './libs/ui.js';
 import { SidebarProjectApp } from './Sidebar.Project.App.js';
 /* import { SidebarProjectMaterials } from './Sidebar.Project.Materials.js'; */
 import { SidebarProjectRenderer } from './Sidebar.Project.Renderer.js';
-import { SidebarProjectImage } from './Sidebar.Project.Image.js';
-import { SidebarProjectVideo } from './Sidebar.Project.Video.js';
 
 function SidebarProject( editor ) {
 
@@ -15,14 +13,6 @@ function SidebarProject( editor ) {
 	/* container.add( new SidebarProjectMaterials( editor ) ); */
 
 	container.add( new SidebarProjectApp( editor ) );
-
-	container.add( new SidebarProjectImage( editor ) );
-
-	if ( 'SharedArrayBuffer' in window ) {
-
-		container.add( new SidebarProjectVideo( editor ) );
-
-	}
 
 	return container;
 
