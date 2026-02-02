f28f20df184d24e52ad6b0f25e85fba962576a9b
sunag <sunagbrasil@gmail.com>
2026-02-02 02:19:30 -0300

    Revert "Update puppeteer.js"
    This reverts commit 12f463715770fd2b88fb5b2ba56c06ba4c106139.

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -32,7 +32,6 @@ const exceptionList = [
 	'webgpu_postprocessing_ao',
 	'webgpu_postprocessing_dof',
 	'webgpu_postprocessing_godrays',
-	'webgpu_postprocessing_retro',
 	'webgpu_postprocessing_ssgi',
 	'webgpu_postprocessing_sss',
 	'webgpu_postprocessing_traa',
