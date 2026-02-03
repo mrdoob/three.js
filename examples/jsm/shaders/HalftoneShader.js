456b5b830e7b8ce0725fc2606ddaa1e113414510
Nate Wright <natew@notthisway.com>
2026-02-03 11:52:11 +0000

    Add: diamond shape as documented option in docblock

--- examples/jsm/shaders/HalftoneShader.js
+++ examples/jsm/shaders/HalftoneShader.js
@@ -8,7 +8,7 @@
  *
  * Used by {@link HalftonePass}.
  *
- * Shape (1 = Dot, 2 = Ellipse, 3 = Line, 4 = Square)
+ * Shape (1 = Dot, 2 = Ellipse, 3 = Line, 4 = Square, 5 = Diamond)
  * Blending Mode (1 = Linear, 2 = Multiply, 3 = Add, 4 = Lighter, 5 = Darker)
  *
  * @constant
