746aafba6dfcc43ded4bbfd180a533496870c794
PoseidonEnergy <moarnow@hotmail.com>
2026-02-04 22:37:57 -0600

    fix stereo examples

--- src/cameras/Camera.js
+++ src/cameras/Camera.js
@@ -129,6 +129,33 @@ class Camera extends Object3D {
 
 	}
 
+	ensureMatrices(
+		force,
+		updateParents,
+		updateChildren,
+		updateLocal,
+		updateWorld,
+		respectAutoUpdateFlags
+	) {
+
+		super.ensureMatrices( force, updateParents, updateChildren, updateLocal, updateWorld, respectAutoUpdateFlags );
+
+		// exclude scale from view matrix to be glTF conform
+
+		this.matrixWorld.decompose( _position, _quaternion, _scale );
+
+		if ( _scale.x === 1 && _scale.y === 1 && _scale.z === 1 ) {
+
+			this.matrixWorldInverse.copy( this.matrixWorld ).invert();
+
+		} else {
+
+			this.matrixWorldInverse.compose( _position, _quaternion, _scale.set( 1, 1, 1 ) ).invert();
+
+		}
+
+	}
+
 	clone() {
 
 		return new this.constructor().copy( this );
