0435120227c0e0320ebbf87981a050beb740fe3b
Mugen87 <michael.herzog@human-interactive.org>
2026-02-25 10:49:40 +0100

    Updated builds.

--- build/three.cjs
+++ build/three.cjs
@@ -77210,7 +77210,7 @@ class WebGLRenderer {
 					generateMipmaps: true,
 					type: hasHalfFloatSupport ? HalfFloatType : UnsignedByteType,
 					minFilter: LinearMipmapLinearFilter,
-					samples: capabilities.samples,
+					samples: Math.max( 4, capabilities.samples ), // to avoid feedback loops, the transmission render target requires a resolve, see #26177
 					stencilBuffer: stencil,
 					resolveDepthBuffer: false,
 					resolveStencilBuffer: false,
