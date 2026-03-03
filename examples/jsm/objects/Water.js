15cd9d3cdd19c0177b767e374ba8b2d04f5d1000
Rodri <hamuyrodrigo@gmail.com>
2026-03-03 22:44:18 +0000

    jsm/objects/Water: fall back to FloatType when EXT_color_buffer_half_float is unsupported

--- examples/jsm/objects/Water.js
+++ examples/jsm/objects/Water.js
@@ -1,6 +1,7 @@
 import {
 	Color,
 	FrontSide,
+	FloatType,
 	HalfFloatType,
 	Matrix4,
 	Mesh,
@@ -85,7 +86,7 @@ class Water extends Mesh {
 
 		const mirrorCamera = new PerspectiveCamera();
 
-		const renderTarget = new WebGLRenderTarget( textureWidth, textureHeight, { type: HalfFloatType } );
+		let renderTarget = new WebGLRenderTarget( textureWidth, textureHeight, { type: HalfFloatType } );
 
 		const mirrorShader = {
 
@@ -236,6 +237,15 @@ class Water extends Mesh {
 		scope.material = material;
 
 		scope.onBeforeRender = function ( renderer, scene, camera ) {
+
+			// Lazily fall back to FloatType if EXT_color_buffer_half_float is unsupported (e.g. some mobile GPUs)
+			if ( renderTarget.texture.type === HalfFloatType && ! renderer.extensions.has( 'EXT_color_buffer_half_float' ) ) {
+
+				renderTarget.dispose();
+				renderTarget = new WebGLRenderTarget( textureWidth, textureHeight, { type: FloatType } );
+				material.uniforms[ 'mirrorSampler' ].value = renderTarget.texture;
+
+			}
 
 			mirrorWorldPosition.setFromMatrixPosition( scope.matrixWorld );
 			cameraWorldPosition.setFromMatrixPosition( camera.matrixWorld );
