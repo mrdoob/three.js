374209b928b7249a47b5515b1e33ca8906d36a40
nil <tomeaton76@gmail.com>
2026-03-14 04:02:58 -0700

    Hdr loader header parse fix (#33174)

--- test/unit/three.addons.unit.js
+++ test/unit/three.addons.unit.js
@@ -3,4 +3,5 @@
 import './addons/utils/BufferGeometryUtils.tests.js';
 import './addons/math/ColorSpaces.tests.js';
 import './addons/curves/NURBSCurve.tests.js';
+import './addons/loaders/HDRLoader.tests.js';
 import './addons/exporters/USDZExporter.tests.js';
