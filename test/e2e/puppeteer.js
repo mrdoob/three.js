695bc7c1778e86b8dff45e1f11143e713f07e959
Mr.doob <info@mrdoob.com>
2026-04-14 18:50:33 +0900

    E2E: Add WebGPU diagnostic probe

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -403,6 +403,44 @@ async function makeAttempt( page, failedScreenshots, cleanPage, isMakeScreenshot
 
 		}
 
+		if ( file.startsWith( 'webgpu_' ) && attemptID === 0 ) {
+
+			await page.evaluate( async () => {
+
+				console.log( '--- WEBGPU DIAGNOSTIC ---' );
+				if ( ! navigator.gpu ) {
+
+					console.log( 'navigator.gpu is undefined!' );
+
+				} else {
+
+					try {
+
+						const adapter = await navigator.gpu.requestAdapter();
+						if ( ! adapter ) {
+
+							console.log( 'requestAdapter() returned null.' );
+
+						} else {
+
+							const info = await adapter.requestAdapterInfo();
+							console.log( `Adapter: ${info.vendor} / ${info.architecture} / ${info.device}` );
+
+						}
+
+					} catch ( err ) {
+
+						console.log( 'WebGPU Error: ' + err.message );
+
+					}
+
+				}
+				console.log( '-------------------------' );
+
+			} );
+
+		}
+
 		try {
 
 			/* Render page */
