669436119f41717eedbdbe81ecc582698607bac0
Michael Herzog <michael.herzog@human-interactive.org>
2026-05-15 20:03:12 +0200

    Update puppeteer.js
    Add example to exception list.

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -36,6 +36,7 @@ const exceptionList = [
 	'webaudio_visualizer',
 	'webgpu_compute_audio',
 	'webgpu_compute_cloth',
+	'webgpu_compute_particles_fluid',
 	'webgpu_compute_sort_bitonic',
 	'webgpu_storage_buffer',
 	'webgpu_tsl_editor',
