e70a1b55d04cf7094022e229cfa26c041b121c08
Michael Herzog <michael.herzog@human-interactive.org>
2026-05-18 10:26:14 +0200

    USDZExporter: Add animation support. (#33589)

--- examples/jsm/exporters/USDZExporter.js
+++ examples/jsm/exporters/USDZExporter.js
@@ -2,6 +2,7 @@ import {
 	NoColorSpace,
 	DoubleSide,
 	Color,
+	PropertyBinding,
 } from 'three';
 
 import {
@@ -76,7 +77,7 @@ class USDNode {
 
 		const properties = this.properties.map( ( l ) => {
 
-			const property = l.property;
+			const property = l.property.replace( /\n/g, '\n' + pad + '\t' );
 			const metadata = l.metadata.length
 				? ` (\n${l.metadata.map( ( m ) => `${pad}\t\t${m}` ).join( '\n' )}\n${pad}\t)`
 				: '';
@@ -199,6 +200,8 @@ class USDZExporter {
 				onlyVisible: true,
 				quickLookCompatible: false,
 				maxTextureSize: 1024,
+				animations: [],
+				animationFrameRate: 60,
 			},
 			options
 		);
@@ -210,6 +213,9 @@ class USDZExporter {
 
 		// model file should be first in USDZ archive so we init it here
 		files[ modelFileName ] = null;
+
+		const animationTracks = buildAnimationTracks( scene, options.animations );
+		options.animationTracks = animationTracks;
 
 		const root = new USDNode( 'Root', 'Xform' );
 		const scenesNode = new USDNode( 'Scenes', 'Scope' );
@@ -249,8 +255,12 @@ class USDZExporter {
 			options.quickLookCompatible
 		);
 
+		const timeRange = animationTracks.size > 0
+			? { fps: options.animationFrameRate, endTimeCode: getMaxClipDuration( options.animations ) * options.animationFrameRate }
+			: null;
+
 		output =
-			buildHeader() +
+			buildHeader( timeRange ) +
 			'\n' +
 			root.toString() +
 			'\n\n' +
@@ -419,7 +429,15 @@ function imageToCanvas( image, flipY, maxTextureSize ) {
 
 const PRECISION = 7;
 
-function buildHeader() {
+function buildHeader( timeRange = null ) {
+
+	const timeMetadata = timeRange
+		? `
+	startTimeCode = 0
+	endTimeCode = ${timeRange.endTimeCode}
+	timeCodesPerSecond = ${timeRange.fps}
+	framesPerSecond = ${timeRange.fps}`
+		: '';
 
 	return `#usda 1.0
 (
@@ -428,9 +446,95 @@ function buildHeader() {
 	}
 	defaultPrim = "Root"
 	metersPerUnit = 1
-	upAxis = "Y"
+	upAxis = "Y"${timeMetadata}
 )
 `;
+
+}
+
+function buildAnimationTracks( scene, clips ) {
+
+	// Map<Object3D, { position?: KeyframeTrack, quaternion?: KeyframeTrack, scale?: KeyframeTrack }>
+	const tracksByObject = new Map();
+
+	for ( let c = 0; c < clips.length; c ++ ) {
+
+		const clip = clips[ c ];
+
+		for ( let t = 0; t < clip.tracks.length; t ++ ) {
+
+			const track = clip.tracks[ t ];
+			const binding = PropertyBinding.parseTrackName( track.name );
+			const target = PropertyBinding.findNode( scene, binding.nodeName );
+
+			if ( target === null || target === undefined ) continue;
+
+			const property = binding.propertyName;
+			if ( property !== 'position' && property !== 'quaternion' && property !== 'scale' ) continue;
+
+			let entry = tracksByObject.get( target );
+			if ( entry === undefined ) {
+
+				entry = {};
+				tracksByObject.set( target, entry );
+
+			}
+
+			entry[ property ] = track;
+
+		}
+
+	}
+
+	return tracksByObject;
+
+}
+
+function getMaxClipDuration( clips ) {
+
+	let max = 0;
+	for ( let i = 0; i < clips.length; i ++ ) {
+
+		if ( clips[ i ].duration > max ) max = clips[ i ].duration;
+
+	}
+
+	return max;
+
+}
+
+function buildVector3TimeSamples( opName, opType, track, fps ) {
+
+	const times = track.times;
+	const values = track.values;
+	const samples = [];
+
+	for ( let i = 0; i < times.length; i ++ ) {
+
+		const o = i * 3;
+		samples.push( `${( times[ i ] * fps ).toPrecision( PRECISION )}: (${values[ o ].toPrecision( PRECISION )}, ${values[ o + 1 ].toPrecision( PRECISION )}, ${values[ o + 2 ].toPrecision( PRECISION )})` );
+
+	}
+
+	return `${opType} ${opName}.timeSamples = {\n\t${samples.join( ',\n\t' )},\n}`;
+
+}
+
+function buildQuaternionTimeSamples( track, fps ) {
+
+	const times = track.times;
+	const values = track.values;
+	const samples = [];
+
+	// three.js quaternion order: (x, y, z, w); USD quatf order: (w, x, y, z)
+	for ( let i = 0; i < times.length; i ++ ) {
+
+		const o = i * 4;
+		samples.push( `${( times[ i ] * fps ).toPrecision( PRECISION )}: (${values[ o + 3 ].toPrecision( PRECISION )}, ${values[ o ].toPrecision( PRECISION )}, ${values[ o + 1 ].toPrecision( PRECISION )}, ${values[ o + 2 ].toPrecision( PRECISION )})` );
+
+	}
+
+	return `quatf xformOp:orient.timeSamples = {\n\t${samples.join( ',\n\t' )},\n}`;
 
 }
 
@@ -474,7 +578,8 @@ function buildHierarchy( object, parentNode, materials, usedNames, files, option
 					child,
 					geometry,
 					materials[ material.uuid ],
-					usedNames
+					usedNames,
+					options
 				);
 
 			} else {
@@ -488,11 +593,11 @@ function buildHierarchy( object, parentNode, materials, usedNames, files, option
 
 		} else if ( child.isCamera ) {
 
-			childNode = buildCamera( child, usedNames );
+			childNode = buildCamera( child, usedNames, options );
 
 		} else {
 
-			childNode = buildXform( child, usedNames );
+			childNode = buildXform( child, usedNames, options );
 
 		}
 
@@ -507,7 +612,78 @@ function buildHierarchy( object, parentNode, materials, usedNames, files, option
 
 }
 
-function buildXform( object, usedNames ) {
+function addTransformProperties( node, object, options ) {
+
+	const animTracks = options.animationTracks.get( object );
+	const hasPivot = object.pivot !== null;
+
+	if ( ! hasPivot && animTracks === undefined ) {
+
+		const transform = buildMatrix( object.matrix );
+		node.addProperty( `matrix4d xformOp:transform = ${transform}` );
+		node.addProperty( 'uniform token[] xformOpOrder = ["xformOp:transform"]' );
+		return;
+
+	}
+
+	// Per-op layout: animated channels use timeSamples, others stay static.
+	// Pivot ops (when present) are always static.
+
+	const fps = options.animationFrameRate;
+	const p = object.position;
+	const q = object.quaternion;
+	const s = object.scale;
+
+	if ( animTracks !== undefined && animTracks.position !== undefined ) {
+
+		node.addProperty( buildVector3TimeSamples( 'xformOp:translate', 'float3', animTracks.position, fps ) );
+
+	} else {
+
+		node.addProperty( `float3 xformOp:translate = (${p.x.toPrecision( PRECISION )}, ${p.y.toPrecision( PRECISION )}, ${p.z.toPrecision( PRECISION )})` );
+
+	}
+
+	if ( hasPivot ) {
+
+		const piv = object.pivot;
+		node.addProperty( `float3 xformOp:translate:pivot = (${piv.x.toPrecision( PRECISION )}, ${piv.y.toPrecision( PRECISION )}, ${piv.z.toPrecision( PRECISION )})` );
+
+	}
+
+	if ( animTracks !== undefined && animTracks.quaternion !== undefined ) {
+
+		node.addProperty( buildQuaternionTimeSamples( animTracks.quaternion, fps ) );
+
+	} else {
+
+		node.addProperty( `quatf xformOp:orient = (${q.w.toPrecision( PRECISION )}, ${q.x.toPrecision( PRECISION )}, ${q.y.toPrecision( PRECISION )}, ${q.z.toPrecision( PRECISION )})` );
+
+	}
+
+	if ( animTracks !== undefined && animTracks.scale !== undefined ) {
+
+		node.addProperty( buildVector3TimeSamples( 'xformOp:scale', 'float3', animTracks.scale, fps ) );
+
+	} else {
+
+		node.addProperty( `float3 xformOp:scale = (${s.x.toPrecision( PRECISION )}, ${s.y.toPrecision( PRECISION )}, ${s.z.toPrecision( PRECISION )})` );
+
+	}
+
+	if ( hasPivot ) {
+
+		node.addProperty( 'uniform token[] xformOpOrder = ["xformOp:translate", "xformOp:translate:pivot", "xformOp:orient", "xformOp:scale", "!invert!xformOp:translate:pivot"]' );
+
+	} else {
+
+		node.addProperty( 'uniform token[] xformOpOrder = ["xformOp:translate", "xformOp:orient", "xformOp:scale"]' );
+
+	}
+
+}
+
+function buildXform( object, usedNames, options ) {
 
 	const name = getName( object, usedNames );
 
@@ -521,37 +697,15 @@ function buildXform( object, usedNames ) {
 	}
 
 	const node = new USDNode( name, 'Xform' );
-
-	if ( object.pivot !== null ) {
-
-		// Export with pivot using separate transform ops
-		const p = object.position;
-		const q = object.quaternion;
-		const s = object.scale;
-		const piv = object.pivot;
-
-		node.addProperty( `float3 xformOp:translate = (${p.x.toPrecision( PRECISION )}, ${p.y.toPrecision( PRECISION )}, ${p.z.toPrecision( PRECISION )})` );
-		node.addProperty( `float3 xformOp:translate:pivot = (${piv.x.toPrecision( PRECISION )}, ${piv.y.toPrecision( PRECISION )}, ${piv.z.toPrecision( PRECISION )})` );
-		node.addProperty( `quatf xformOp:orient = (${q.w.toPrecision( PRECISION )}, ${q.x.toPrecision( PRECISION )}, ${q.y.toPrecision( PRECISION )}, ${q.z.toPrecision( PRECISION )})` );
-		node.addProperty( `float3 xformOp:scale = (${s.x.toPrecision( PRECISION )}, ${s.y.toPrecision( PRECISION )}, ${s.z.toPrecision( PRECISION )})` );
-		node.addProperty( 'uniform token[] xformOpOrder = ["xformOp:translate", "xformOp:translate:pivot", "xformOp:orient", "xformOp:scale", "!invert!xformOp:translate:pivot"]' );
-
-	} else {
-
-		// Export as single transform matrix
-		const transform = buildMatrix( object.matrix );
-		node.addProperty( `matrix4d xformOp:transform = ${transform}` );
-		node.addProperty( 'uniform token[] xformOpOrder = ["xformOp:transform"]' );
-
-	}
+	addTransformProperties( node, object, options );
 
 	return node;
 
 }
 
-function buildMesh( object, geometry, material, usedNames ) {
+function buildMesh( object, geometry, material, usedNames, options ) {
 
-	const node = buildXform( object, usedNames );
+	const node = buildXform( object, usedNames, options );
 
 	node.addMetadata(
 		'prepend references',
@@ -1159,11 +1313,9 @@ function buildVector2( vector ) {
 
 }
 
-function buildCamera( camera, usedNames ) {
+function buildCamera( camera, usedNames, options ) {
 
 	const name = getName( camera, usedNames );
-
-	const transform = buildMatrix( camera.matrix );
 
 	if ( camera.matrix.determinant() < 0 ) {
 
@@ -1175,8 +1327,7 @@ function buildCamera( camera, usedNames ) {
 	}
 
 	const node = new USDNode( name, 'Camera' );
-	node.addProperty( `matrix4d xformOp:transform = ${transform}` );
-	node.addProperty( 'uniform token[] xformOpOrder = ["xformOp:transform"]' );
+	addTransformProperties( node, camera, options );
 
 	const projection = camera.isOrthographicCamera
 		? 'orthographic'
@@ -1245,6 +1396,9 @@ function buildCamera( camera, usedNames ) {
  * can be configured via `ar.anchoring.type` and `ar.planeAnchoring.alignment`.
  * @property {boolean} [quickLookCompatible=false] - Whether to make the exported USDZ compatible to QuickLook
  * which means the asset is modified to accommodate the bugs FB10036297 and FB11442287 (Apple Feedback).
+ * @property {Array<AnimationClip>} [animations=[]] - Animation clips to bake into `xformOp` time samples on the
+ * targeted objects. Only `position`, `quaternion`, and `scale` tracks are exported.
+ * @property {number} [animationFrameRate=60] - Time codes per second used when writing animation samples.
  **/
 
 /**
