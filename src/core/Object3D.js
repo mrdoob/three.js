b56efcd0c051d647f430a014bc7ba1736f972b68
WestLangley <WestLangley@users.noreply.github.com>
2026-06-09 16:39:51 -0400

    Rewrite updateMatrixWorld()

--- src/core/Object3D.js
+++ src/core/Object3D.js
@@ -1164,41 +1164,7 @@ class Object3D extends EventDispatcher {
 	 */
 	updateMatrixWorld( force ) {
 
-		if ( this.matrixAutoUpdate ) this.updateMatrix();
-
-		if ( this.matrixWorldNeedsUpdate || force ) {
-
-			if ( this.matrixWorldAutoUpdate === true ) {
-
-				if ( this.parent === null ) {
-
-					this.matrixWorld.copy( this.matrix );
-
-				} else {
-
-					this.matrixWorld.multiplyMatrices( this.parent.matrixWorld, this.matrix );
-
-				}
-
-			}
-
-			this.matrixWorldNeedsUpdate = false;
-
-			force = true;
-
-		}
-
-		// make sure descendants are updated if required
-
-		const children = this.children;
-
-		for ( let i = 0, l = children.length; i < l; i ++ ) {
-
-			const child = children[ i ];
-
-			child.updateMatrixWorld( force );
-
-		}
+	    this.updateWorldMatrix( false, true, force );
 
 	}
 
