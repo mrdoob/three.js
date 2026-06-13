895d9bdceec0c6f70c426426591c278e31a6fb2c
Michael Herzog <michael.herzog@human-interactive.org>
2026-06-13 10:40:06 +0200

    Examples: Use CDN version of Ammo.js. (#33799)

--- examples/jsm/Addons.js
+++ examples/jsm/Addons.js
@@ -171,6 +171,7 @@ export * from './objects/Sky.js';
 export * from './objects/Water.js';
 export { Water as Water2 } from './objects/Water2.js';
 
+export * from './physics/AmmoPhysics.js';
 export * from './physics/JoltPhysics.js';
 export * from './physics/RapierPhysics.js';
 
