36bf8c535e2f2b425942d6643429530ac95aca67
Jure Triglav <juretriglav@gmail.com>
2026-06-25 17:21:22 +0200

    PMREMNode: Handle unresolved PMREM during setup

--- test/unit/three.source.unit.js
+++ test/unit/three.source.unit.js
@@ -291,3 +291,6 @@ import './src/textures/VideoTexture.tests.js';
 //src/nodes/display
 import './src/nodes/display/ViewportTextureNode.tests.js';
 import './src/nodes/display/ViewportDepthTextureNode.tests.js';
+
+//src/nodes/pmrem
+import './src/nodes/pmrem/PMREMNode.tests.js';
