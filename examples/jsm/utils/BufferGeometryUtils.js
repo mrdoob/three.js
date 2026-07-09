36086230576bafacfb9223969c85bd5505015167
Nils-nonline <nils-nonline@web.de>
2026-07-09 19:36:24 +0200

    ignore attributes code

--- examples/jsm/utils/BufferGeometryUtils.js
+++ examples/jsm/utils/BufferGeometryUtils.js
@@ -638,164 +638,154 @@ function estimateBytesUsed( geometry ) {
  *
  * @param {BufferGeometry} geometry - The geometry to merge vertices for.
  * @param {number} [tolerance=1e-4] - The tolerance value.
+ * @param {String[]} [ignoredAttributes=[]] - The attributes to be ignored.
  * @return {BufferGeometry} - The new geometry with merged vertices.
  */
-function mergeVertices( geometry, tolerance = 1e-4 ) {
+function mergeVertices(geometry, tolerance = 1e-4, ignoredAttributes = []) {
+  tolerance = Math.max(tolerance, Number.EPSILON);
 
-	tolerance = Math.max( tolerance, Number.EPSILON );
+  // Generate an index buffer if the geometry doesn't have one, or optimize it
+  // if it's already available.
+  const hashToIndex = {};
+  const indices = geometry.getIndex();
+  const positions = geometry.getAttribute("position");
+  const vertexCount = indices ? indices.count : positions.count;
 
-	// Generate an index buffer if the geometry doesn't have one, or optimize it
-	// if it's already available.
-	const hashToIndex = {};
-	const indices = geometry.getIndex();
-	const positions = geometry.getAttribute( 'position' );
-	const vertexCount = indices ? indices.count : positions.count;
+  // next value for triangle indices
+  let nextIndex = 0;
 
-	// next value for triangle indices
-	let nextIndex = 0;
+  // attributes and new attribute arrays
+  const attributeNames = Object.keys(geometry.attributes);
+  const filteredAttributeNames = attributeNames.filter(
+    (name) => !ignoredAttributes.includes(name),
+  );
 
-	// attributes and new attribute arrays
-	const attributeNames = Object.keys( geometry.attributes );
-	const tmpAttributes = {};
-	const tmpMorphAttributes = {};
-	const newIndices = [];
-	const getters = [ 'getX', 'getY', 'getZ', 'getW' ];
-	const setters = [ 'setX', 'setY', 'setZ', 'setW' ];
+  const tmpAttributes = {};
+  const tmpMorphAttributes = {};
+  const newIndices = [];
+  const getters = ["getX", "getY", "getZ", "getW"];
+  const setters = ["setX", "setY", "setZ", "setW"];
 
-	// Initialize the arrays, allocating space conservatively. Extra
-	// space will be trimmed in the last step.
-	for ( let i = 0, l = attributeNames.length; i < l; i ++ ) {
+  // Initialize the arrays, allocating space conservatively. Extra
+  // space will be trimmed in the last step.
+  for (let i = 0, l = attributeNames.length; i < l; i++) {
+    const name = attributeNames[i];
+    const attr = geometry.attributes[name];
 
-		const name = attributeNames[ i ];
-		const attr = geometry.attributes[ name ];
+    tmpAttributes[name] = new attr.constructor(
+      new attr.array.constructor(attr.count * attr.itemSize),
+      attr.itemSize,
+      attr.normalized,
+    );
 
-		tmpAttributes[ name ] = new attr.constructor(
-			new attr.array.constructor( attr.count * attr.itemSize ),
-			attr.itemSize,
-			attr.normalized
-		);
+    const morphAttributes = geometry.morphAttributes[name];
+    if (morphAttributes) {
+      if (!tmpMorphAttributes[name]) tmpMorphAttributes[name] = [];
+      morphAttributes.forEach((morphAttr, i) => {
+        const array = new morphAttr.array.constructor(
+          morphAttr.count * morphAttr.itemSize,
+        );
+        tmpMorphAttributes[name][i] = new morphAttr.constructor(
+          array,
+          morphAttr.itemSize,
+          morphAttr.normalized,
+        );
+      });
+    }
+  }
 
-		const morphAttributes = geometry.morphAttributes[ name ];
-		if ( morphAttributes ) {
+  // convert the error tolerance to an amount of decimal places to truncate to
+  const halfTolerance = tolerance * 0.5;
+  const exponent = Math.log10(1 / tolerance);
+  const hashMultiplier = Math.pow(10, exponent);
+  const hashAdditive = halfTolerance * hashMultiplier;
+  for (let i = 0; i < vertexCount; i++) {
+    const index = indices ? indices.getX(i) : i;
 
-			if ( ! tmpMorphAttributes[ name ] ) tmpMorphAttributes[ name ] = [];
-			morphAttributes.forEach( ( morphAttr, i ) => {
+    // Generate a hash for the vertex attributes at the current index 'i'
+    let hash = "";
+    for (let j = 0, l = filteredAttributeNames.length; j < l; j++) {
+      const name = filteredAttributeNames[j];
+      const attribute = geometry.getAttribute(name);
+      const itemSize = attribute.itemSize;
 
-				const array = new morphAttr.array.constructor( morphAttr.count * morphAttr.itemSize );
-				tmpMorphAttributes[ name ][ i ] = new morphAttr.constructor( array, morphAttr.itemSize, morphAttr.normalized );
+      for (let k = 0; k < itemSize; k++) {
+        // double tilde truncates the decimal value
+        hash += `${~~(attribute[getters[k]](index) * hashMultiplier + hashAdditive)},`;
+      }
+    }
 
-			} );
+    // Add another reference to the vertex if it's already
+    // used by another index
+    if (hash in hashToIndex) {
+      newIndices.push(hashToIndex[hash]);
+    } else {
+      // copy data to the new index in the temporary attributes
+      for (let j = 0, l = attributeNames.length; j < l; j++) {
+        const name = attributeNames[j];
+        const attribute = geometry.getAttribute(name);
+        const morphAttributes = geometry.morphAttributes[name];
+        const itemSize = attribute.itemSize;
+        const newArray = tmpAttributes[name];
+        const newMorphArrays = tmpMorphAttributes[name];
 
-		}
+        for (let k = 0; k < itemSize; k++) {
+          const getterFunc = getters[k];
+          const setterFunc = setters[k];
+          newArray[setterFunc](nextIndex, attribute[getterFunc](index));
 
-	}
+          if (morphAttributes) {
+            for (let m = 0, ml = morphAttributes.length; m < ml; m++) {
+              newMorphArrays[m][setterFunc](
+                nextIndex,
+                morphAttributes[m][getterFunc](index),
+              );
+            }
+          }
+        }
+      }
 
-	// convert the error tolerance to an amount of decimal places to truncate to
-	const halfTolerance = tolerance * 0.5;
-	const exponent = Math.log10( 1 / tolerance );
-	const hashMultiplier = Math.pow( 10, exponent );
-	const hashAdditive = halfTolerance * hashMultiplier;
-	for ( let i = 0; i < vertexCount; i ++ ) {
+      hashToIndex[hash] = nextIndex;
+      newIndices.push(nextIndex);
+      nextIndex++;
+    }
+  }
 
-		const index = indices ? indices.getX( i ) : i;
+  // generate result BufferGeometry
+  const result = geometry.clone();
+  for (const name in geometry.attributes) {
+    const tmpAttribute = tmpAttributes[name];
 
-		// Generate a hash for the vertex attributes at the current index 'i'
-		let hash = '';
-		for ( let j = 0, l = attributeNames.length; j < l; j ++ ) {
+    result.setAttribute(
+      name,
+      new tmpAttribute.constructor(
+        tmpAttribute.array.slice(0, nextIndex * tmpAttribute.itemSize),
+        tmpAttribute.itemSize,
+        tmpAttribute.normalized,
+      ),
+    );
 
-			const name = attributeNames[ j ];
-			const attribute = geometry.getAttribute( name );
-			const itemSize = attribute.itemSize;
+    if (!(name in tmpMorphAttributes)) continue;
 
-			for ( let k = 0; k < itemSize; k ++ ) {
+    for (let j = 0; j < tmpMorphAttributes[name].length; j++) {
+      const tmpMorphAttribute = tmpMorphAttributes[name][j];
 
-				// double tilde truncates the decimal value
-				hash += `${ ~ ~ ( attribute[ getters[ k ] ]( index ) * hashMultiplier + hashAdditive ) },`;
+      result.morphAttributes[name][j] = new tmpMorphAttribute.constructor(
+        tmpMorphAttribute.array.slice(
+          0,
+          nextIndex * tmpMorphAttribute.itemSize,
+        ),
+        tmpMorphAttribute.itemSize,
+        tmpMorphAttribute.normalized,
+      );
+    }
+  }
 
-			}
+  // indices
 
-		}
+  result.setIndex(newIndices);
 
-		// Add another reference to the vertex if it's already
-		// used by another index
-		if ( hash in hashToIndex ) {
-
-			newIndices.push( hashToIndex[ hash ] );
-
-		} else {
-
-			// copy data to the new index in the temporary attributes
-			for ( let j = 0, l = attributeNames.length; j < l; j ++ ) {
-
-				const name = attributeNames[ j ];
-				const attribute = geometry.getAttribute( name );
-				const morphAttributes = geometry.morphAttributes[ name ];
-				const itemSize = attribute.itemSize;
-				const newArray = tmpAttributes[ name ];
-				const newMorphArrays = tmpMorphAttributes[ name ];
-
-				for ( let k = 0; k < itemSize; k ++ ) {
-
-					const getterFunc = getters[ k ];
-					const setterFunc = setters[ k ];
-					newArray[ setterFunc ]( nextIndex, attribute[ getterFunc ]( index ) );
-
-					if ( morphAttributes ) {
-
-						for ( let m = 0, ml = morphAttributes.length; m < ml; m ++ ) {
-
-							newMorphArrays[ m ][ setterFunc ]( nextIndex, morphAttributes[ m ][ getterFunc ]( index ) );
-
-						}
-
-					}
-
-				}
-
-			}
-
-			hashToIndex[ hash ] = nextIndex;
-			newIndices.push( nextIndex );
-			nextIndex ++;
-
-		}
-
-	}
-
-	// generate result BufferGeometry
-	const result = geometry.clone();
-	for ( const name in geometry.attributes ) {
-
-		const tmpAttribute = tmpAttributes[ name ];
-
-		result.setAttribute( name, new tmpAttribute.constructor(
-			tmpAttribute.array.slice( 0, nextIndex * tmpAttribute.itemSize ),
-			tmpAttribute.itemSize,
-			tmpAttribute.normalized,
-		) );
-
-		if ( ! ( name in tmpMorphAttributes ) ) continue;
-
-		for ( let j = 0; j < tmpMorphAttributes[ name ].length; j ++ ) {
-
-			const tmpMorphAttribute = tmpMorphAttributes[ name ][ j ];
-
-			result.morphAttributes[ name ][ j ] = new tmpMorphAttribute.constructor(
-				tmpMorphAttribute.array.slice( 0, nextIndex * tmpMorphAttribute.itemSize ),
-				tmpMorphAttribute.itemSize,
-				tmpMorphAttribute.normalized,
-			);
-
-		}
-
-	}
-
-	// indices
-
-	result.setIndex( newIndices );
-
-	return result;
-
+  return result;
 }
 
 /**
