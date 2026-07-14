5dbcf104e243c0aa1e596f04494d280a228c2bb7
Michael Herzog <michael.herzog@human-interactive.org>
2026-07-14 09:57:00 +0200

    Update TorusGeometry.tests.js

--- test/unit/src/geometries/TorusGeometry.tests.js
+++ test/unit/src/geometries/TorusGeometry.tests.js
@@ -16,6 +16,8 @@ export default QUnit.module( 'Geometries', () => {
 				radialSegments: 30,
 				tubularSegments: 10,
 				arc: 2.0,
+				thetaStart: Math.PI * 0.5,
+				thetaLength: Math.PI
 			};
 
 			geometries = [
@@ -25,6 +27,8 @@ export default QUnit.module( 'Geometries', () => {
 				new TorusGeometry( parameters.radius, parameters.tube, parameters.radialSegments ),
 				new TorusGeometry( parameters.radius, parameters.tube, parameters.radialSegments, parameters.tubularSegments ),
 				new TorusGeometry( parameters.radius, parameters.tube, parameters.radialSegments, parameters.tubularSegments, parameters.arc ),
+				new TorusGeometry( parameters.radius, parameters.tube, parameters.radialSegments, parameters.tubularSegments, parameters.arc, parameters.thetaStart ),
+				new TorusGeometry( parameters.radius, parameters.tube, parameters.radialSegments, parameters.tubularSegments, parameters.arc, parameters.thetaStart, parameters.thetaLength ),
 			];
 
 		} );
@@ -55,19 +59,6 @@ export default QUnit.module( 'Geometries', () => {
 			assert.ok(
 				object.type === 'TorusGeometry',
 				'TorusGeometry.type should be TorusGeometry'
-			);
-
-		} );
-
-		// PUBLIC
-		QUnit.test( 'fromJSON', ( assert ) => {
-
-			const geometry = new TorusGeometry( 1, 0.4, 12, 48, Math.PI, 0.3, 1.1 );
-			const geometry2 = TorusGeometry.fromJSON( geometry.toJSON() );
-
-			assert.deepEqual(
-				geometry2.parameters, geometry.parameters,
-				'fromJSON() restores all parameters, including thetaStart and thetaLength'
 			);
 
 		} );
