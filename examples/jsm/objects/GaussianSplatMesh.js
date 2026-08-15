0b90623bf0d5d629419f69d02fa916bbe8477830
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-08-15 19:53:21 +0900

    Update bounds calculation

--- examples/jsm/objects/GaussianSplatMesh.js
+++ examples/jsm/objects/GaussianSplatMesh.js
@@ -359,8 +359,7 @@ class GaussianSplatMesh extends Mesh {
 		this.computeBoundingBox();
 		this.boundingBox.getBoundingSphere( this.boundingSphere );
 
-		// the box corners overstate the radius, so grow it only where a splat actually reaches
-		let maxRadiusSq = 0;
+		let maxRadius = 0;
 		const center = this.boundingSphere.center;
 		const count = this.splatGeometry.getAttribute( 'position' ).count;
 
@@ -368,12 +367,11 @@ class GaussianSplatMesh extends Mesh {
 
 			this.getSplat( i, _splat );
 
-			const distance = center.distanceTo( _splat.position ) + _splat.radius;
-			maxRadiusSq = Math.max( maxRadiusSq, distance * distance );
+			maxRadius = Math.max( maxRadius, center.distanceTo( _splat.position ) + _splat.radius );
 
 		}
 
-		this.boundingSphere.radius = Math.sqrt( maxRadiusSq );
+		this.boundingSphere.radius = maxRadius;
 
 	}
 
