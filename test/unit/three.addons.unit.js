35af874cb39e03a46fc884b8271e1ee3b94ae662
Michael Herzog <michael.herzog@human-interactive.org>
2026-08-22 10:14:24 +0200

    Update three.addons.unit.js

--- test/unit/three.addons.unit.js
+++ test/unit/three.addons.unit.js
@@ -18,4 +18,3 @@ import './addons/tsl/WebGLNodesHandler.tests.js';
 import './addons/tsl/GPUTest.tests.js';
 import './addons/tsl/TSLDeterminant.tests.js';
 import './addons/tsl/TSLFaceForward.tests.js';
-
