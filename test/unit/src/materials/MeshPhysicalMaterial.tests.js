e0500cd7f8b86ae3a74cb2cc2c0748fc79b3d2a0
bright <1772127579@qq.com>
2026-08-26 11:20:30 +0800

    Restore MeshPhysicalMaterial.tests.js

--- test/unit/src/materials/MeshPhysicalMaterial.tests.js
+++ test/unit/src/materials/MeshPhysicalMaterial.tests.js
@@ -47,24 +47,6 @@ export default QUnit.module( 'Materials', () => {
 
 		} );
 
-		QUnit.test( 'diffuseRoughness', ( assert ) => {
-
-			const object = new MeshPhysicalMaterial();
-			assert.strictEqual( object.diffuseRoughness, 0, 'diffuseRoughness defaults to 0.' );
-
-			const initialVersion = object.version;
-			object.diffuseRoughness = 0.75;
-			assert.strictEqual( object.diffuseRoughness, 0.75, 'Can set diffuseRoughness.' );
-			assert.strictEqual( object.version, initialVersion + 1, 'Enabling EON invalidates the shader.' );
-
-			object.diffuseRoughness = 0.5;
-			assert.strictEqual( object.version, initialVersion + 1, 'Changing an enabled EON roughness reuses the shader.' );
-
-			object.diffuseRoughness = 0;
-			assert.strictEqual( object.version, initialVersion + 2, 'Disabling EON invalidates the shader.' );
-
-		} );
-
 		QUnit.test( 'copy copies retroreflectivity', ( assert ) => {
 
 			const source = new MeshPhysicalMaterial( { retroreflectivity: 0.5 } );
@@ -73,17 +55,6 @@ export default QUnit.module( 'Materials', () => {
 			object.copy( source );
 
 			assert.strictEqual( object.retroreflectivity, 0.5, 'copy() preserves retroreflectivity.' );
-
-		} );
-
-		QUnit.test( 'copy copies diffuseRoughness', ( assert ) => {
-
-			const source = new MeshPhysicalMaterial( { diffuseRoughness: 0.6 } );
-			const object = new MeshPhysicalMaterial();
-
-			object.copy( source );
-
-			assert.strictEqual( object.diffuseRoughness, 0.6, 'copy() preserves diffuseRoughness.' );
 
 		} );
 
@@ -97,19 +68,6 @@ export default QUnit.module( 'Materials', () => {
 
 			assert.strictEqual( json.retroreflectivity, 0.25, 'toJSON() serializes retroreflectivity.' );
 			assert.strictEqual( object.retroreflectivity, 0.25, 'fromJSON() restores retroreflectivity.' );
-
-		} );
-
-		QUnit.test( 'fromJSON restores diffuseRoughness', ( assert ) => {
-
-			const source = new MeshPhysicalMaterial( { diffuseRoughness: 0.4 } );
-			const json = source.toJSON();
-			const object = new MeshPhysicalMaterial();
-
-			object.fromJSON( json );
-
-			assert.strictEqual( json.diffuseRoughness, 0.4, 'toJSON() serializes diffuseRoughness.' );
-			assert.strictEqual( object.diffuseRoughness, 0.4, 'fromJSON() restores diffuseRoughness.' );
 
 		} );
 
