1a99705c47ac026242aea4edfb891d6ae8772e6b
Michael Herzog <michael.herzog@human-interactive.org>
2026-09-01 15:22:01 +0200

    WGSLNodeBuilder: Clean up.

--- src/renderers/webgpu/nodes/WGSLNodeBuilder.js
+++ src/renderers/webgpu/nodes/WGSLNodeBuilder.js
@@ -2168,8 +2168,6 @@ ${ flowData.code }
 
 				}
 
-				const componentPrefix = this.getComponentTypeFromTexture( texture ).charAt( 0 );
-
 				if ( texture.isCubeTexture === true && texture.isDepthTexture === true ) {
 
 					textureType = 'texture_depth_cube';
@@ -2204,10 +2202,12 @@ ${ flowData.code }
 
 				} else if ( texture.isArrayTexture === true || texture.isDataArrayTexture === true || texture.isCompressedArrayTexture === true ) {
 
+					const componentPrefix = this.getComponentTypeFromTexture( texture ).charAt( 0 );
 					textureType = `texture_2d_array<${ componentPrefix }32>`;
 
 				} else if ( texture.is3DTexture === true || texture.isData3DTexture === true ) {
 
+					const componentPrefix = this.getComponentTypeFromTexture( texture ).charAt( 0 );
 					textureType = `texture_3d<${ componentPrefix }32>`;
 
 				} else {
