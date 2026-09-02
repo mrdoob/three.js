1691a1cfbfd25cec31cc72df0a1c7174ea387e55
sunag <sunagbrasil@gmail.com>
2026-09-02 00:21:45 -0300

    change icon

--- tsl/js/managers/PlaygroundManager.js
+++ tsl/js/managers/PlaygroundManager.js
@@ -296,8 +296,8 @@ class PlaygroundManager {
 
 				const closeEl = document.createElement( 'span' );
 				closeEl.className = 'playground-tab-close';
-				closeEl.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 11px; height: 11px; display: block;"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
-				closeEl.title = 'Delete tab';
+				closeEl.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 12px; height: 12px; display: block;"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
+				closeEl.title = 'Close tab';
 				closeEl.onclick = ( e ) => {
 
 					e.stopPropagation();
