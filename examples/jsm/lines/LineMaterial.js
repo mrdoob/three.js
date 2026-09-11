038db317ab185383d79b7552108a69e48b826985
Michael Herzog <michael.herzog@human-interactive.org>
2026-09-11 12:38:26 +0200

    Update LineMaterial.js

--- examples/jsm/lines/LineMaterial.js
+++ examples/jsm/lines/LineMaterial.js
@@ -179,7 +179,6 @@ ShaderLib[ 'line' ] = {
 			#ifdef WORLD_UNITS
 
 				vec3 worldDir = normalize( end.xyz - start.xyz );
-				// Orthographic camera rays are parallel to the camera-space z axis.
 				vec3 tmpFwd = perspective ? normalize( mix( start.xyz, end.xyz, 0.5 ) ) : vec3( 0.0, 0.0, - 1.0 );
 				vec3 worldUp = normalize( cross( worldDir, tmpFwd ) );
 				vec3 worldFwd = cross( worldDir, worldUp );
