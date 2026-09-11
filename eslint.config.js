c4bd5c44107e2f10808fbdcae83cba08ef2e7c2d
Ben Houston <neuralsoft@gmail.com>
2026-09-11 10:08:11 -0400

    add lint rule to prevent import from /build in tests.

--- eslint.config.js
+++ eslint.config.js
@@ -139,5 +139,19 @@ export default [
 			ecmaVersion: 2022,
 			sourceType: 'module'
 		}
+	},
+
+	// tests must import from src, not build
+	{
+		name: 'test rules',
+		files: [ 'test/**/*.js' ],
+		rules: {
+			'no-restricted-imports': [ 'error', {
+				patterns: [ {
+					group: [ '**/build/*' ],
+					message: 'Tests must import from /src, not /build.'
+				} ]
+			} ]
+		}
 	}
 ];
