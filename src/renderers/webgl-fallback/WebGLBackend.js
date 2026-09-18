852fc93bae20603278626174aa2b0b01626aec4d
Mugen87 <michael.herzog@human-interactive.org>
2026-09-18 12:10:38 +0200

    WebGPURenderer: Properly delete shader programs.

--- src/renderers/webgl-fallback/WebGLBackend.js
+++ src/renderers/webgl-fallback/WebGLBackend.js
@@ -1520,6 +1520,11 @@ class WebGLBackend extends Backend {
 	 */
 	destroyProgram( program ) {
 
+		const gl = this.gl;
+		const shaderGPU = this.get( program ).shaderGPU;
+
+		gl.deleteShader( shaderGPU );
+
 		this.delete( program );
 
 	}
@@ -1585,6 +1590,22 @@ class WebGLBackend extends Backend {
 		}
 
 		this._completeCompile( renderObject, pipeline );
+
+	}
+
+	/**
+	 * Destroys the given pipeline.
+	 *
+	 * @param {Pipeline} pipeline - The pipeline.
+	 */
+	destroyPipeline( pipeline ) {
+
+		const gl = this.gl;
+		const data = this.get( pipeline );
+
+		gl.deleteProgram( data.programGPU );
+
+		this.delete( pipeline );
 
 	}
 
