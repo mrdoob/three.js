f28a8336505497b8c56acf8ca89c865fc29ef422
Mr.doob <info@mrdoob.com>
2026-09-21 09:36:49 +0900

    TSL: Fix scalar constant caching.

--- test/unit/three.source.unit.js
+++ test/unit/three.source.unit.js
@@ -263,3 +263,6 @@ import './src/textures/VideoTexture.tests.js';
 //src/nodes/display
 import './src/nodes/display/ViewportTextureNode.tests.js';
 import './src/nodes/display/ViewportDepthTextureNode.tests.js';
+
+//src/nodes/tsl
+import './src/nodes/tsl/TSLCore.tests.js';
