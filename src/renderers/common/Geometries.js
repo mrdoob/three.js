92e2e6eb9620e6c2ac8bae7943a221c3ddb7b8bb
Mr.doob <info@mrdoob.com>
2026-09-21 11:27:12 +0900

    Geometries: Preallocate wireframe indices.

--- src/renderers/common/Geometries.js
+++ src/renderers/common/Geometries.js
@@ -41,22 +41,29 @@ function getWireframeId( geometry ) {
  */
 function getWireframeIndex( geometry ) {
 
-	const indices = [];
-
 	const geometryIndex = geometry.index;
 	const geometryPosition = geometry.attributes.position;
+	const count = geometryIndex !== null ? geometryIndex.array.length : ( geometryPosition.array.length / 3 ) - 1;
+
+	const attribute = new ( geometryPosition.count >= 65535 ? Uint32BufferAttribute : Uint16BufferAttribute )( Math.ceil( count / 3 ) * 6, 1 );
+	const indices = attribute.array;
 
 	if ( geometryIndex !== null ) {
 
 		const array = geometryIndex.array;
 
-		for ( let i = 0, l = array.length; i < l; i += 3 ) {
+		for ( let i = 0, j = 0, l = array.length; i < l; i += 3 ) {
 
 			const a = array[ i + 0 ];
 			const b = array[ i + 1 ];
 			const c = array[ i + 2 ];
 
-			indices.push( a, b, b, c, c, a );
+			indices[ j ++ ] = a;
+			indices[ j ++ ] = b;
+			indices[ j ++ ] = b;
+			indices[ j ++ ] = c;
+			indices[ j ++ ] = c;
+			indices[ j ++ ] = a;
 
 		}
 
@@ -64,19 +71,23 @@ function getWireframeIndex( geometry ) {
 
 		const array = geometryPosition.array;
 
-		for ( let i = 0, l = ( array.length / 3 ) - 1; i < l; i += 3 ) {
+		for ( let i = 0, j = 0, l = ( array.length / 3 ) - 1; i < l; i += 3 ) {
 
 			const a = i + 0;
 			const b = i + 1;
 			const c = i + 2;
 
-			indices.push( a, b, b, c, c, a );
+			indices[ j ++ ] = a;
+			indices[ j ++ ] = b;
+			indices[ j ++ ] = b;
+			indices[ j ++ ] = c;
+			indices[ j ++ ] = c;
+			indices[ j ++ ] = a;
 
 		}
 
 	}
 
-	const attribute = new ( geometryPosition.count >= 65535 ? Uint32BufferAttribute : Uint16BufferAttribute )( indices, 1 );
 	attribute.version = getWireframeVersion( geometry );
 	attribute.__id = getWireframeId( geometry );
 
