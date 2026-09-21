a01b16e3c933142b11aa05e4b3fa9a6e7de16fce
Mr.doob <info@mrdoob.com>
2026-09-21 11:53:48 +0900

    WebGLRenderer: Preallocate wireframe indices.

--- src/renderers/webgl/WebGLGeometries.js
+++ src/renderers/webgl/WebGLGeometries.js
@@ -86,8 +86,6 @@ function WebGLGeometries( gl, attributes, info, bindingStates ) {
 
 	function updateWireframeAttribute( geometry ) {
 
-		const indices = [];
-
 		const geometryIndex = geometry.index;
 		const geometryPosition = geometry.attributes.position;
 		let version = 0;
@@ -98,18 +96,30 @@ function WebGLGeometries( gl, attributes, info, bindingStates ) {
 
 		}
 
+		const count = geometryIndex !== null ? geometryIndex.array.length : ( geometryPosition.array.length / 3 ) - 1;
+
+		// check whether a 32 bit or 16 bit buffer is required to store the indices
+		// account for PRIMITIVE_RESTART_FIXED_INDEX, #24565
+		const attribute = new ( geometryPosition.count >= 65535 ? Uint32BufferAttribute : Uint16BufferAttribute )( Math.ceil( count / 3 ) * 6, 1 );
+		const indices = attribute.array;
+
 		if ( geometryIndex !== null ) {
 
 			const array = geometryIndex.array;
 			version = geometryIndex.version;
 
-			for ( let i = 0, l = array.length; i < l; i += 3 ) {
+			for ( let i = 0, j = 0, l = array.length; i < l; i += 3 ) {
 
 				const a = array[ i + 0 ];
 				const b = array[ i + 1 ];
 				const c = array[ i + 2 ];
 
-				indices.push( a, b, b, c, c, a );
+				indices[ j ++ ] = a;
+				indices[ j ++ ] = b;
+				indices[ j ++ ] = b;
+				indices[ j ++ ] = c;
+				indices[ j ++ ] = c;
+				indices[ j ++ ] = a;
 
 			}
 
@@ -118,21 +128,23 @@ function WebGLGeometries( gl, attributes, info, bindingStates ) {
 			const array = geometryPosition.array;
 			version = geometryPosition.version;
 
-			for ( let i = 0, l = ( array.length / 3 ) - 1; i < l; i += 3 ) {
+			for ( let i = 0, j = 0, l = ( array.length / 3 ) - 1; i < l; i += 3 ) {
 
 				const a = i + 0;
 				const b = i + 1;
 				const c = i + 2;
 
-				indices.push( a, b, b, c, c, a );
+				indices[ j ++ ] = a;
+				indices[ j ++ ] = b;
+				indices[ j ++ ] = b;
+				indices[ j ++ ] = c;
+				indices[ j ++ ] = c;
+				indices[ j ++ ] = a;
 
 			}
 
 		}
 
-		// check whether a 32 bit or 16 bit buffer is required to store the indices
-		// account for PRIMITIVE_RESTART_FIXED_INDEX, #24565
-		const attribute = new ( geometryPosition.count >= 65535 ? Uint32BufferAttribute : Uint16BufferAttribute )( indices, 1 );
 		attribute.version = version;
 
 		// Updating index buffer in VAO now. See WebGLBindingStates
